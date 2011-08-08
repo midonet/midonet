d61df6637fd73202e75540ce6eb7b8957a2b50a6
Yoshiaki Tamura <yoshi@midokura.jp>
2011-08-08 17:37:28 +0900

    Introduce TestOpenvSwitchDatabaseConnection.scala.
    Add tests for Open vSwitch database connection.
    
    Signed-off-by: Taku Fukushima <tfukushima@midokura.jp>
    Signed-off-by: Yoshiaki Tamura <yoshi@midokura.jp>

---
+++ src/test/scala/com/midokura/midolman/openvswitch/TestOpenvSwitchDatabaseConnection.scala
@@ -0,0 +1,211 @@
+/**
+ * TestOpenvSwitchDatabaaseConnection.scala - OVSDB connection Test.
+ *
+ * Copyright (c) 2011 Midokura KK. All rights reserved.
+ */
+
+package com.midokura.midolman.openvswitch
+
+
+import org.junit.{AfterClass, BeforeClass, Test}
+import org.junit.Assert._
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitSuite
+
+import com.midokura.midolman.openvswitch._
+
+/**
+ * Test for the Open vSwitch database connection.
+ */
+object TestOpenvSwitchDatabaaseConnection extends JUnitSuite {
+    private final val database = "Open_vSwitch"
+    private final val host = "localhost"
+    private final val port = 12344
+    private final val bridgeName = "testbr"
+    private final val portName = "testport"
+    private final val bridgeExtIdKey = "midolman-vnet"
+    private final val bridgeExtIdValue = "efbf1194-9e25-11e0-b3b3-ba417460eb69"
+    private final val bridgeOfPortNum = 65534
+    private final val ovsdb =
+        new OpenvSwitchDatabaseConnectionImpl(database, host, port)
+    private final var bridgeId: Long = _
+
+    @BeforeClass def initializeTest() {
+        testAddBridge()
+        bridgeId = java.lang.Long.parseLong(ovsdb.getDatapathId(bridgeName),
+                                            16)
+    }
+
+    /**
+     * Test addBridge().
+     */
+    def testAddBridge() = {
+        val bb: BridgeBuilder = ovsdb.addBridge(bridgeName)
+        bb.externalId(bridgeExtIdKey, bridgeExtIdValue)
+        bb.build
+        assertTrue(ovsdb.hasBridge(bridgeName))
+    }
+
+    /**
+     * Test delBridge().
+     */
+    def testDelBridge() = {
+        ovsdb.delBridge(bridgeName)
+        assertFalse(ovsdb.hasBridge(bridgeName))
+    }
+
+    /**
+     * Disconnect the OVSDB connection.
+     */
+    @AfterClass def finalizeTest() = {
+        testDelBridge()
+        assertFalse(ovsdb.hasBridge(bridgeName))
+        ovsdb.close
+    }
+}
+
+class TestOpenvSwitchDatabaaseConnection extends JUnitSuite {
+    import TestOpenvSwitchDatabaaseConnection._
+
+    /**
+     * Test addSystemPort().
+     */
+    @Test def testAddSystemPort() = {
+        var pb = ovsdb.addSystemPort(bridgeName, portName)
+        pb.build
+        assertTrue(ovsdb.hasPort(portName))
+        ovsdb.delPort(portName)
+        assertFalse(ovsdb.hasPort(portName))
+
+        pb = ovsdb.addSystemPort(bridgeId, portName)
+        pb.build
+        assertTrue(ovsdb.hasPort(portName))
+        ovsdb.delPort(portName)
+        assertFalse(ovsdb.hasPort(portName))
+    }
+
+    /**
+     * Test addInternalPort().
+     */
+    @Test def testAddInternalPort() = {
+        var pb = ovsdb.addInternalPort(bridgeName, portName)
+        pb.build
+        assertTrue(ovsdb.hasPort(portName))
+        ovsdb.delPort(portName)
+        assertFalse(ovsdb.hasPort(portName))
+
+        pb = ovsdb.addInternalPort(bridgeId, portName)
+        pb.build
+        assertTrue(ovsdb.hasPort(portName))
+        ovsdb.delPort(portName)
+        assertFalse(ovsdb.hasPort(portName))
+    }
+
+    /**
+     * Test addTapPort().
+     */
+    @Test def testAddTapPort() = {
+        var pb = ovsdb.addTapPort(bridgeName, portName)
+        pb.build
+        assertTrue(ovsdb.hasPort(portName))
+        ovsdb.delPort(portName)
+        assertFalse(ovsdb.hasPort(portName))
+
+        pb = ovsdb.addTapPort(bridgeId, portName)
+        pb.build
+        assertTrue(ovsdb.hasPort(portName))
+        ovsdb.delPort(portName)
+        assertFalse(ovsdb.hasPort(portName))
+    }
+
+    /**
+     * Test addGrePort().
+     */
+    @Test def testAddGrePort() = {
+        var pb = ovsdb.addGrePort(bridgeName, portName, "127.0.0.1")
+        pb.build
+        assertTrue(ovsdb.hasPort(portName))
+        ovsdb.delPort(portName)
+        assertFalse(ovsdb.hasPort(portName))
+
+        pb = ovsdb.addGrePort(bridgeId, portName, "127.0.0.1")
+        pb.build
+        assertTrue(ovsdb.hasPort(portName))
+        ovsdb.delPort(portName)
+        assertFalse(ovsdb.hasPort(portName))
+    }
+
+    /**
+     * Test addOpenflowController().
+     */
+    @Test def testAddOpenflowController() = {
+        val target = "tcp:127.0.0.1"
+        var cb = ovsdb.addBridgeOpenflowController(bridgeName, target)
+        cb.build
+        assertTrue(ovsdb.hasController(target))
+        ovsdb.delBridgeOpenflowControllers(bridgeName)
+        assertFalse(ovsdb.hasController(target))
+
+        cb = ovsdb.addBridgeOpenflowController(bridgeId, target)
+        cb.build
+        assertTrue(ovsdb.hasController(target))
+        ovsdb.delBridgeOpenflowControllers(bridgeId)
+        assertFalse(ovsdb.hasController(target))
+    }
+
+    /**
+     * Test getDatapathExternalId().
+     */
+    @Test def testGetDatapathExternalId() = {
+        assertTrue(ovsdb.hasBridge(bridgeName))
+        assertEquals(ovsdb.getDatapathExternalId(bridgeName, bridgeExtIdKey),
+                     bridgeExtIdValue)
+        assertEquals(ovsdb.getDatapathExternalId(bridgeId, bridgeExtIdKey),
+                     bridgeExtIdValue)
+    }
+
+    /**
+     * Test getPortExternalId().
+     */
+    @Test def testGetPortExternalId() = {
+        assertEquals(ovsdb.getPortExternalId(bridgeName, bridgeOfPortNum,
+                                             bridgeExtIdKey), "")
+        assertEquals(ovsdb.getPortExternalId(bridgeId, bridgeOfPortNum,
+                                             bridgeExtIdKey), "")
+
+        val portExtIdKey = bridgeExtIdKey
+        val portExtIdValue = "002bcb5f-0000-8000-1000-bafbafbafbaf"
+        var pb = ovsdb.addSystemPort(bridgeName, portName)
+        pb.externalId(portExtIdKey, portExtIdValue)
+        pb.build
+        assertTrue(ovsdb.hasPort(portName))
+        assertEquals(ovsdb.getPortExternalId(portName, portExtIdKey),
+                     portExtIdValue)
+        ovsdb.delPort(portName)
+        assertFalse(ovsdb.hasPort(portName))
+    }
+
+    /**
+     * Test getBridgeNamesByExternalId().
+     */
+    @Test def testGetBridgeNamesByExternalId() = {
+        assertTrue(ovsdb.hasBridge(bridgeName))
+        assertTrue(ovsdb.getBridgeNamesByExternalId(
+            bridgeExtIdKey, bridgeExtIdValue).contains(bridgeName))
+    }
+
+    /**
+     * Test getPortNamesByExternalId().
+     */
+    @Test def testGetPortNamesByExternalId() = {
+        val portExtIdKey = bridgeExtIdKey
+        val portExtIdValue = "002bcb5f-0000-8000-1000-bafbafbafbaf"
+        var pb = ovsdb.addSystemPort(bridgeName, portName)
+        pb.externalId(portExtIdKey, portExtIdValue)
+        pb.build
+        assertTrue(ovsdb.getPortNamesByExternalId(
+            portExtIdKey, portExtIdValue).contains(portName))
+        ovsdb.delPort(portName)
+        assertFalse(ovsdb.hasPort(portName))
+    }
+}
