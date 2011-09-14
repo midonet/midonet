1209fb9a414258e4c7a7a54deb9af4dcddfb4600
ddumitriu <ddumitriu@dan-imac.mido>
2011-09-14 14:23:24 +0900

    add test for ZK multi update

--- src/test/java/com/midokura/midolman/zookeeper/ZooKeeperMultiUpdate.java
+++ src/test/java/com/midokura/midolman/zookeeper/ZooKeeperMultiUpdate.java
@@ -6,6 +6,7 @@ import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.Semaphore;
 
 import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.Op;
 import org.apache.zookeeper.OpResult;
 import org.apache.zookeeper.WatchedEvent;
@@ -86,14 +87,40 @@ public class ZooKeeperMultiUpdate {
         Stat s3 = zk.exists("/3", false);
         Assert.assertNotNull(s3);
         
-        zk.setData("/2", null, s2.getVersion());
+        byte b[] = new byte[1000];
+        zk.setData("/2", b, s2.getVersion());
+        
+        long before = System.currentTimeMillis();
+        for (int i=0; i<100; i++) {
+            zk.getData("/2", false, null);
+        }
+        long after = System.currentTimeMillis();
+        
+        log.info("duration {}", after - before);
         
         List<Op> ops2 = new ArrayList<Op>();
         ops2.add(Op.delete("/1", s1.getVersion()));
         ops2.add(Op.delete("/2", s2.getVersion()));
         ops2.add(Op.delete("/3", s3.getVersion()));
         
-        List<OpResult> results2 = zk.multi(ops2);
+        try {
+            List<OpResult> results2 = zk.multi(ops2);
+        } catch (KeeperException e) {
+            log.warn("multi", e);
+        }
+        
+        Assert.assertNotNull(zk.exists("/1", false));
+        Assert.assertNotNull(zk.exists("/2", false));
+        Assert.assertNotNull(zk.exists("/3", false));
+        
+        s2 = zk.exists("/2", false);
+        
+        List<Op> ops3 = new ArrayList<Op>();
+        ops3.add(Op.delete("/1", s1.getVersion()));
+        ops3.add(Op.delete("/2", s2.getVersion()));
+        ops3.add(Op.delete("/3", s3.getVersion()));
+        
+        List<OpResult> results3 = zk.multi(ops3);
         
         Assert.assertNull(zk.exists("/1", false));
         Assert.assertNull(zk.exists("/2", false));
