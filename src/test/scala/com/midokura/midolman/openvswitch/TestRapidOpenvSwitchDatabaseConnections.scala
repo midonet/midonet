96f05834593f244f797edc6c9e15a4897532b8cb
Jacob Mandelson <jlm@midokura.jp>
2011-10-11 13:02:19 -0700

    Convert TestRapidOpenvSwitchDatabaseConnections to use OpenvSwitchDatabaseConnectionMutexer.

--- src/test/scala/com/midokura/midolman/openvswitch/TestRapidOpenvSwitchDatabaseConnections.scala
+++ src/test/scala/com/midokura/midolman/openvswitch/TestRapidOpenvSwitchDatabaseConnections.scala
@@ -11,17 +11,19 @@ import com.midokura.midolman.openvswitch._
 import com.midokura.midolman.openvswitch.OpenvSwitchDatabaseConnectionImpl._
 import com.midokura.midolman.openvswitch.OpenvSwitchDatabaseConsts._
 
-import org.junit.{AfterClass, BeforeClass, Ignore, Test}
+import org.junit.{AfterClass, BeforeClass, Test}
 import org.junit.Assert._
 import org.junit.Assume._
 import org.scalatest.junit.JUnitSuite
 
-import java.io.{File, RandomAccessFile}
-import java.lang.Long
+import java.lang.Long.parseLong
 import java.net.ConnectException
-import java.nio.channels.FileLock
 
-object TestRapidOpenvSwitchDatabaseConnections extends JUnitSuite {
+
+object TestRapidOpenvSwitchDatabaseConnections 
+        extends OpenvSwitchDatabaseConnectionMutexer with JUnitSuite {
+    import OpenvSwitchDatabaseConnectionMutexer._
+
     private final val database = "Open_vSwitch"
     private final val host = "localhost"
     private final val port = 12344
@@ -29,18 +31,13 @@ object TestRapidOpenvSwitchDatabaseConnections extends JUnitSuite {
     private final val portName = "testrapidport"
     private final val bridgeExtIdKey = "midolman-vnet"
     private final val bridgeExtIdValue = "f5451278-fddd-8b9c-d658-b167aa6c00cc"
-    private final val lockfile = new File("/tmp/ovsdbconnection.lock")
-    private var lock: FileLock = _
 
     @BeforeClass def initializeTest() {
-        lockfile.setReadable(true, false)
-        lockfile.setWritable(true, false)
-        lock = new RandomAccessFile(lockfile, "rw").getChannel.lock
-        Console.err.println("Entering testRapidOVSConns")
+        takeLock
     }
 
     @AfterClass def finalizeTest() {
-        lock.release
+        releaseLock
     }
 
     def testAddBridge(conn: OpenvSwitchDatabaseConnection) {
@@ -68,13 +65,13 @@ class TestRapidOpenvSwitchDatabaseConnections extends JUnitSuite {
                 assumeNoException(e)
         }
         testAddBridge(conn1)
-        var bridgeId = Long.parseLong(conn1.getDatapathId(bridgeName), 16)
+        var bridgeId = parseLong(conn1.getDatapathId(bridgeName), 16)
         testDelBridge(conn1)
         assertFalse(conn1.hasBridge(bridgeName))
         conn1.close
         var conn2 = new OpenvSwitchDatabaseConnectionImpl(database, host, port)
         testAddBridge(conn2)
-        bridgeId = Long.parseLong(conn2.getDatapathId(bridgeName), 16)
+        bridgeId = parseLong(conn2.getDatapathId(bridgeName), 16)
         testDelBridge(conn2)
         assertFalse(conn2.hasBridge(bridgeName))
         conn2.close
