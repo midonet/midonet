18d154ea13027dd73017fec188f9fbac70fd3896
Jacob Mandelson <jlm@midokura.jp>
2011-10-13 14:42:20 -0700

    MockDirectory only used by tests, move from main/ to test/.  Create test for zebra.start().

---
+++ src/test/java/com/midokura/midolman/quagga/TestZebraServer.java
@@ -0,0 +1,43 @@
+// Copyright 2011 Midokura Inc.
+
+package com.midokura.midolman.quagga;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.newsclub.net.unix.AFUNIXServerSocket;
+import org.newsclub.net.unix.AFUNIXSocketAddress;
+
+import com.midokura.midolman.openvswitch.MockOpenvSwitchDatabaseConnection;
+import com.midokura.midolman.state.MockDirectory;
+import com.midokura.midolman.state.PortZkManager;
+import com.midokura.midolman.state.RouteZkManager;
+
+
+class TestZebraServer {
+    private ZebraServer zebra;
+ 
+    @Before
+    public static makeZebraServer {
+        MockOpenvSwitchDatabaseConnection ovsdb = 
+                                new MockOpenvSwitchDatabaseConnection();
+        MockDirectory directory = new MockDirectory();
+        String basePath = "/base";
+        PortZkManager portMgr = new PortZkManager(directory, basePath);
+        RouteZkManager routeMgr = new RouteZkManager(directory, basePath);
+        File socketFile = File.createTempFile("testzebra", ".sock");
+        socketFile.delete();
+        AFUNIXServerSocket server = AFUNIXServerSocket.newInstance();
+        AFUNIXSocketAddress address = new AFUNIXSocketAddress(socketFile);
+        zebra = new ZebraServer(server, address, portMgr, routeMgr, ovsdb);
+    }
+
+    @Test
+    public static testZebraStart() {
+        try {
+            zebra.start();
+        } catch (Exception e) {
+            Assert.fail("zebra.start() threw an exception: " + e);
+        }
+    }
+}
