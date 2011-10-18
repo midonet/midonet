8833befaf77e4a2baa0864968bb39c3ac7c37c24
Jacob Mandelson <jlm@midokura.jp>
2011-10-18 15:32:50 -0700

    Check for memcache connection during startup. Refs 269

--- src/main/java/com/midokura/midolman/util/MemcacheCache.java
+++ src/main/java/com/midokura/midolman/util/MemcacheCache.java
@@ -1,23 +1,45 @@
 package com.midokura.midolman.util;
 
 import java.io.IOException;
+import java.util.Date;
 
 import net.spy.memcached.AddrUtil;
 import net.spy.memcached.BinaryConnectionFactory;
 import net.spy.memcached.CASValue;
 import net.spy.memcached.MemcachedClient;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 public class MemcacheCache implements Cache {
+    private static final Logger log =
+                         LoggerFactory.getLogger(MemcacheCache.class);
     
     private MemcachedClient client;
     
     private int expirationSecs;
     
-    public MemcacheCache(String memcacheServers, int expirationSecs) throws IOException {
-        client = new MemcachedClient(new BinaryConnectionFactory(),
-                AddrUtil.getAddresses(memcacheServers));
+    public MemcacheCache(String memcacheServers, int expirationSecs) 
+                throws IOException {
+        boolean success = false;
+        try {
+            client = new MemcachedClient(new BinaryConnectionFactory(),
+                                         AddrUtil.getAddresses(memcacheServers));
         
-        this.expirationSecs = expirationSecs;
+            this.expirationSecs = expirationSecs;
+
+            String testKey = "testkey";
+            String testValue = (new Date()).toString();
+            set(testKey, testValue);
+            String fetchedValue = get(testKey);
+            assert testValue.equals(fetchedValue);
+            client.delete(testKey);   
+            success = true;
+        } finally {
+            if (!success)
+                log.error("Connection to memcached FAILED");
+        }
     }
 
     @Override
