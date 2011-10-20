afab0245fc960b9ba1ecad9021d46bea4214f200
Ryu Ishimoto <ryu@midokura.com>
2011-10-20 19:20:02 +0900

    Added more test frameworks to try out

--- src/test/java/com/midokura/midolman/mgmt/rest_api/v1/MainTest.java
+++ src/test/java/com/midokura/midolman/mgmt/rest_api/v1/MainTest.java
@@ -3,12 +3,20 @@ package com.midokura.midolman.mgmt.rest_api.v1;
 import org.codehaus.jackson.jaxrs.JacksonJaxbJsonProvider;
 import org.junit.Test;
 
+import com.midokura.midolman.mgmt.rest_api.v1.resources.TenantResource;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.config.ClientConfig;
 import com.sun.jersey.api.client.config.DefaultClientConfig;
 import com.sun.jersey.test.framework.JerseyTest;
 import com.sun.jersey.test.framework.WebAppDescriptor;
+import com.sun.jersey.test.framework.spi.container.TestContainerFactory;
+import com.sun.jersey.test.framework.spi.container.embedded.glassfish.EmbeddedGlassFishTestContainerFactory;
+import com.sun.jersey.test.framework.spi.container.external.*;
+import com.sun.jersey.test.framework.spi.container.grizzly.GrizzlyTestContainerFactory;
+import com.sun.jersey.test.framework.spi.container.grizzly.web.GrizzlyWebTestContainerFactory;
+import com.sun.jersey.test.framework.spi.container.http.HTTPContainerFactory;
+import com.sun.jersey.test.framework.spi.container.inmemory.*;
 
 public class MainTest extends JerseyTest {
 
@@ -36,4 +44,11 @@ public class MainTest extends JerseyTest {
 
     }
 
+    @Override
+    protected TestContainerFactory getTestContainerFactory() {
+        return new HTTPContainerFactory();
+//        return new ExternalTestContainerFactory();
+//        return new GrizzlyWebTestContainerFactory();
+
+    }
 }
