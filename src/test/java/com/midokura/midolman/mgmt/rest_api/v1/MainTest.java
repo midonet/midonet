2d22dda445ac47b16b69a17c55c76116fff234f3
Yoshiaki Tamura <yoshi@midokura.com>
2011-10-24 22:36:47 +0900

    Update unit tests.
    This commit is the first version to actually run unit tests using
    Jersey test framework.
    
    Signed-off-by: Yoshiaki Tamura <yoshi@midokura.com>

--- src/test/java/com/midokura/midolman/mgmt/rest_api/v1/MainTest.java
+++ src/test/java/com/midokura/midolman/mgmt/rest_api/v1/MainTest.java
@@ -1,11 +1,16 @@
 package com.midokura.midolman.mgmt.rest_api.v1;
 
+import com.midokura.midolman.mgmt.data.dto.Router;
 import com.midokura.midolman.mgmt.data.dto.Tenant;
 
+import java.net.URI;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriBuilder;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.midokura.midolman.mgmt.rest_api.v1.resources.TenantResource;
 import com.sun.jersey.api.client.ClientResponse;
@@ -21,6 +26,8 @@ import com.sun.jersey.test.framework.spi.container.grizzly2.web.GrizzlyWebTestCo
 
 public class MainTest extends JerseyTest {
 
+    private final static Logger log = LoggerFactory.getLogger(MainTest.class);
+
     static ClientConfig cc;
     static {
         cc = new DefaultClientConfig();
@@ -33,7 +40,9 @@ public class MainTest extends JerseyTest {
                   "com.midokura.midolman.mgmt.rest_api.v1.resources")
               .initParam(JSONConfiguration.FEATURE_POJO_MAPPING, "true")
               .initParam("com.sun.jersey.spi.container.ContainerRequestFilters",
-                         "com.midokura.midolman.mgmt.auth.KeystoneAuthFilter")
+                         "com.midokura.midolman.mgmt.auth.MockAuthFilter")
+              .contextParam("zookeeper-root", "/mock/midolman")
+              .contextParam("zookeeper-mgmt-root", "/mock/midolman-mgmt")
               .contextPath("context").build());
     }
 
@@ -42,17 +51,37 @@ public class MainTest extends JerseyTest {
         WebResource resource;
         ClientResponse response;
 
+        // Init directory.
+        resource = resource().path("admin/init");
+        response = resource.type(
+            MediaType.APPLICATION_JSON).post(ClientResponse.class);
+
         // Add the tenant
+        Tenant tenant = new Tenant();
+        tenant.setId("tenant1");
+
         resource = resource().path("tenants");
         response = resource.type(
-            MediaType.APPLICATION_JSON).post(ClientResponse.class,
-                                             new Tenant());
-        System.out.println(response.getLocation());
-        // Loop if you want to test w/ curl.
-        // try {
-        //     Thread.sleep(100000);
-        // } catch (Exception e) {
-        // }
+            MediaType.APPLICATION_JSON).post(ClientResponse.class, tenant);
+        URI tenantURI = response.getLocation();
+        log.debug("tanant location: {}", tenantURI);
+        
+        // Add a router.
+        Router router = new Router();
+        String routerName = "router1";
+        router.setName(routerName);
+        resource = resource().uri(
+            UriBuilder.fromUri(tenantURI).path("routers").build());
+        response = resource.type(
+            MediaType.APPLICATION_JSON).post(ClientResponse.class, router);
+        URI routerURI = response.getLocation();
+        log.debug("router location: {}", routerURI);
+
+        // Get the router.
+        resource = resource().uri(routerURI);
+        router = resource.type(MediaType.APPLICATION_JSON).get(Router.class);
+        log.debug("router name: " + router.getName());
+        assertEquals(router.getName(), routerName);
     }
 
     @Override
