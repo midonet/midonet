c6a59f4c24d9caa1875ad9856791e8aa2d3cbd3b
Jacob Mandelson <jlm@midokura.jp>
2011-10-25 15:22:40 -0700

    Make testTransact use checkOVSDB.

--- src/test/scala/com/midokura/midolman/openvswitch/TestDummyOVSDB.scala
+++ src/test/scala/com/midokura/midolman/openvswitch/TestDummyOVSDB.scala
@@ -49,23 +49,11 @@ class TestDummyOVSDB {
     }
 
     @Test(timeout=1000) def testTransact() {
-        val portNum = basePortNum - counter.incrementAndGet
-        log.info("testTransact: Using port {}", portNum)
-        val ovsdb = new DummyOVSDB(portNum)
-        spawn {
-            ovsdb.accept(classOf[DummyOVSDBServerConn]).loop
-            ovsdb.close
-        }
-        Thread.sleep(20)
-        val socket = new Socket(localhost, portNum)
-        val outStream = socket.getOutputStream
-        outStream.write("""{"method":"transact","params":["Open_vSwitch",{"op":"select","table":"Bridge","where":[],"columns":["_uuid","name","ports"]}],"id":1}""".getBytes("ASCII"))
-        var buf = new Array[Byte](100)
-        val inStream = socket.getInputStream
-        val bytesRead = inStream.read(buf)
-        val response = new String(buf, 0, bytesRead, "ASCII")
-        assertEquals("""{"id":1,"error":null,"result":[{"rows":[]}]}""", response)
-        outStream.write("""{"method":"die","params":[],"id":0}""".getBytes("ASCII"))
+        checkOVSDB(classOf[DummyOVSDBServerConn],
+                   """{"method":"transact","params":["Open_vSwitch",""" +
+                   """{"op":"select","table":"Bridge","where":[],"columns":""" +
+                   """["_uuid","name","ports"]}],"id":1}""",
+                   """{"id":1,"error":null,"result":[{"rows":[]}]}""")
     }
 
 }
