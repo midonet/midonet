e39e93611e40a3ffb08a844127a38df46861ad47
Mihai Claudiu Toader <mtoader@midokura.com>
2011-12-12 10:44:00 +0200

    Allow midolmanj to start even if it can't access zserv.api socket.
    Midolman will start with reduced functionality (no support for BGP at all) if it can't access/create the zebra control socket. This functionality can be controlled via a setting in the midolman.conf file ("disable_bgp_on_failure" under the midolman stanza). The default value for this setting is false to keep the current functionality intact.
    A NullPortService implementation (which will only log the method calls with an warn level) is used insted of a BGPPortService to allow midolman to not raise NPE's.
    
    Signed-off-by: Mihai Claudiu Toader <mtoader@midokura.com>
    Signed-off-by: Pino de Candia <gdecandia@midokura.com>

---
+++ src/main/java/com/midokura/midolman/portservice/NullPortService.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2011 Midokura Europe SARL
+ */
+package com.midokura.midolman.portservice;
+
+import com.midokura.midolman.layer3.NetworkController;
+import com.midokura.midolman.packets.MAC;
+import com.midokura.midolman.state.StateAccessException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Set;
+import java.util.UUID;
+
+/**
+ * Author: Toader Mihai Claudiu <mtoader@midokura.com>
+ * <p/>
+ * Date: 12/6/11
+ * Time: 3:50 PM
+ */
+public class NullPortService implements PortService {
+
+    private static final Logger log =
+        LoggerFactory.getLogger(NullPortService.class);
+
+    @Override
+    public void clear() {
+        log.warn("clear() called!");
+    }
+
+    @Override
+    public void setController(NetworkController controller) {
+        log.warn("setController() called!");
+    }
+
+    @Override
+    public Set<String> getPorts(UUID portId) throws StateAccessException {
+        log.warn("getPorts({}) called!", portId);
+        return Collections.emptySet();
+    }
+
+    @Override
+    public void addPort(long datapathId, UUID portId, MAC mac)
+        throws StateAccessException {
+        log.warn("addPort({}, {}, {}) called!",
+                 new Object[]{datapathId, portId, mac});
+    }
+
+    @Override
+    public void addPort(long datapathId, UUID portId) throws
+        StateAccessException {
+        log.warn("addPort({}, {}) called!", datapathId, portId);
+    }
+
+    @Override
+    public UUID getRemotePort(String portName) {
+        log.warn("getRemotePort({}) called!", portName);
+        return null;
+    }
+
+    @Override
+    public void configurePort(UUID portId, String portName)
+        throws IOException, StateAccessException {
+        log.warn("configurePort({}, {}) called!", portId, portName);
+    }
+
+    @Override
+    public void configurePort(UUID portId)
+        throws IOException, StateAccessException {
+        log.warn("configurePort({}) called!", portId);
+    }
+
+    @Override
+    public void delPort(UUID portId) {
+        log.warn("delPort({}) called!", portId);
+    }
+
+    @Override
+    public void start(long datapathId, short localPortNum, short remotePortNum)
+        throws StateAccessException, IOException {
+        log.warn("start({}, {}, {}) called!",
+                 new Object[]{datapathId, localPortNum, remotePortNum});
+    }
+
+    @Override
+    public void start(UUID serviceId)
+        throws StateAccessException, IOException {
+        log.warn("start({}) called!", serviceId);
+    }
+
+    @Override
+    public void stop(UUID serviceId) {
+        log.warn("stop({}) called!", serviceId);
+    }
+}
