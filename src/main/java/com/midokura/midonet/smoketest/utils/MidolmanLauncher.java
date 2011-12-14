e5869d854aea4abc3a01e13252495ce546086087
gdecandia <gdecandia@midokura.jp>
2011-12-14 18:12:30 +0100

    Use different log4j.xml for the two Midolmanj controllers.

--- src/main/java/com/midokura/midonet/smoketest/utils/MidolmanLauncher.java
+++ src/main/java/com/midokura/midonet/smoketest/utils/MidolmanLauncher.java
@@ -13,11 +13,13 @@ import java.util.List;
 
 public class MidolmanLauncher {
 
-    private static final String[] MIDOLMANJ_CMD = { "java", "-cp",
-            "./conf:/usr/share/midolman/midolmanj.jar", "-Dmidolman.log.dir=.",
-            "com.midokura.midolman.Midolman", "-c" };
+    private static final String[] MIDOLMANJ_CMD = { "java", "-cp", "CP_HERE",
+            "-Dmidolman.log.dir=.", "com.midokura.midolman.Midolman", "-c",
+            "CONF_HERE" };
+    private static final String cp1 = "./conf:/usr/share/midolman/midolmanj.jar";
+    private static final String cp2 = "./conf2:/usr/share/midolman/midolmanj.jar";
     private static final String conf1 = "./conf/midolman.conf";
-    private static final String conf2 = "./conf/midolman2.conf";
+    private static final String conf2 = "./conf2/midolman.conf";
     // There are cases where this doesn't prevent multiple executions of
     // start method's code. But for test suites this should be fine.
     private static boolean started = false;
@@ -28,11 +30,15 @@ public class MidolmanLauncher {
         started = true;
         List<String> cmd = new ArrayList<String>(Arrays.asList(MIDOLMANJ_CMD));
         // Start MM1
-        cmd.add(conf1);
+        cmd.set(2, cp1);
+        cmd.set(6, conf1);
         ProcessBuilder pb = new ProcessBuilder(cmd);
         pb.start();
         // Start MM2
-        cmd.set(cmd.size()-1, conf2);
+        cmd = new ArrayList<String>(Arrays.asList(MIDOLMANJ_CMD));
+        // Start MM1
+        cmd.set(2, cp2);
+        cmd.set(6, conf2);
         pb = new ProcessBuilder(cmd);
         pb.start();
     }
