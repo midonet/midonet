7d7eaa6e42a88de7e92a24629158e8f4f010c5c1
Ryu Ishimoto <ryu@midokura.com>
2011-12-15 14:34:04 +0900

    Added 'join' String helper method.
    Issue #474
    
    Signed-off-by: Ryu Ishimoto <ryu@midokura.com>

---
+++ src/main/java/com/midokura/util/StringUtil.java
@@ -0,0 +1,52 @@
+/*
+ * @(#)StringUtil        1.6 11/12/15
+ *
+ * Copyright 2011 Midokura KK
+ */
+package com.midokura.util;
+
+/**
+ * Helper class for String operations. Only static methods should exist.
+ *
+ * @version 1.6 15 Dec 2011
+ * @author Ryu Ishimoto
+ */
+public class StringUtil {
+
+    public final static String EMPTY_STRING = "";
+
+    /**
+     * Joins the elements of the provided array into a single String containing
+     * the provided list of elements separated by the provided separator.
+     *
+     * @param list
+     *            List of T elements to concatenate as strings.
+     * @param separator
+     *            Separator character.
+     * @return Concatenated string.
+     */
+    public static <T> String join(T[] list, char separator) {
+
+        if (list == null) {
+            throw new IllegalArgumentException("list cannot be null.");
+        }
+
+        if (list.length == 0) {
+            return EMPTY_STRING;
+        }
+
+        // Using 16 chars(default StringBuilder initial capacity) as the
+        // expected string size for each item.
+        StringBuilder sb = new StringBuilder(list.length * 16);
+
+        for (T obj : list) {
+            sb.append(obj).append(separator);
+        }
+
+        // Remove the last separator char.
+        return sb.deleteCharAt(sb.length() - 1).toString();
+    }
+
+    private StringUtil() {
+    }
+}
---
+++ src/test/java/com/midokura/util/TestStringUtil.java
@@ -0,0 +1,67 @@
+/*
+ * @(#)TestStringUtil        1.6 11/12/15
+ *
+ * Copyright 2011 Midokura KK
+ */
+package com.midokura.util;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class TestStringUtil {
+
+    @Test
+    public void testJoinGoodInputs() throws Exception {
+
+        // Empty list -> empty string
+        String[] list = {};
+        char separator = '/';
+        String result = StringUtil.join(list, separator);
+        Assert.assertEquals(StringUtil.EMPTY_STRING, result);
+
+        // One item -> same item
+        list = new String[] { "foo" };
+        result = StringUtil.join(list, separator);
+        Assert.assertEquals("foo", result);
+
+        // One item of empty string -> same item
+        list = new String[] { StringUtil.EMPTY_STRING };
+        result = StringUtil.join(list, separator);
+        Assert.assertEquals(StringUtil.EMPTY_STRING, result);
+
+        // Multiple items
+        list = new String[] { "foo", "bar", "baz" };
+        result = StringUtil.join(list, separator);
+        Assert.assertEquals("foo/bar/baz", result);
+
+        // Multiple items of empty string
+        list = new String[] { StringUtil.EMPTY_STRING, StringUtil.EMPTY_STRING,
+                StringUtil.EMPTY_STRING };
+        result = StringUtil.join(list, separator);
+        Assert.assertEquals("//", result);
+
+        // Mix of empty string and non-empty string
+        list = new String[] { "foo", StringUtil.EMPTY_STRING, "bar",
+                StringUtil.EMPTY_STRING };
+        result = StringUtil.join(list, separator);
+        Assert.assertEquals("foo//bar/", result);
+
+        // Another mix that ends up creating a path-like string
+        list = new String[] { StringUtil.EMPTY_STRING, "foo", "bar", "baz" };
+        result = StringUtil.join(list, separator);
+        Assert.assertEquals("/foo/bar/baz", result);
+
+        // Test int
+        Integer[] intList = new Integer[] { 1, 10, -100 };
+        result = StringUtil.join(intList, separator);
+        Assert.assertEquals("1/10/-100", result);
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testJoinNullInput() throws Exception {
+        StringUtil.join(null, '/');
+    }
+
+}
