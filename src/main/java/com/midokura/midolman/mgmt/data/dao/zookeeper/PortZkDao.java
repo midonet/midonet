f1a0f7e6b43de4a6feacaa4927153ff4269e2d88
Ryu Ishimoto <ryu@midokura.com>
2012-01-05 17:49:27 +0900

    Added port ZK proxy DAO to for accessing Port ZK data, and its unit tests.
    Signed-off-by: Ryu Ishimoto <ryu@midoukra.com>
    
    To-be-reviewed-by: Pino de Candia <pino@midoura.com>

---
+++ src/main/java/com/midokura/midolman/mgmt/data/dao/zookeeper/PortZkDao.java
@@ -0,0 +1,175 @@
+/*
+ * @(#)PortZkDao        1.6 12/1/6
+ *
+ * Copyright 2012 Midokura KK
+ */
+package com.midokura.midolman.mgmt.data.dao.zookeeper;
+
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.UUID;
+
+import org.apache.zookeeper.Op;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.midokura.midolman.mgmt.data.dto.config.PortMgmtConfig;
+import com.midokura.midolman.mgmt.data.zookeeper.io.PortSerializer;
+import com.midokura.midolman.mgmt.data.zookeeper.path.PathBuilder;
+import com.midokura.midolman.state.PortConfig;
+import com.midokura.midolman.state.PortZkManager;
+import com.midokura.midolman.state.StateAccessException;
+import com.midokura.midolman.state.ZkNodeEntry;
+
+/**
+ * Proxy class to access ZooKeeper for port data.
+ *
+ * @version 1.6 6 Jan 2012
+ * @author Ryu Ishimoto
+ */
+public class PortZkDao {
+
+    private final static Logger log = LoggerFactory.getLogger(PortZkDao.class);
+    private final PortZkManager zkDao;
+    private final PathBuilder pathBuilder;
+    private final PortSerializer serializer;
+
+    /**
+     * Constructor
+     *
+     * @param zkDao
+     *            PortZkManager object to access ZK data.
+     * @param pathBuilder
+     *            PathBuilder object to get path data.
+     * @param serializer
+     *            PortSerializer object.
+     */
+    public PortZkDao(PortZkManager zkDao, PathBuilder pathBuilder,
+            PortSerializer serializer) {
+        this.zkDao = zkDao;
+        this.pathBuilder = pathBuilder;
+        this.serializer = serializer;
+    }
+
+    /**
+     * Checks whether a port exists with the given ID.
+     *
+     * @param id
+     *            Port ID
+     * @return True if port exists.
+     * @throws StateAccessException
+     *             Data access error.
+     */
+    public boolean exists(UUID id) throws StateAccessException {
+        log.debug("PortZkDao.exists entered: id={}", id);
+
+        String path = pathBuilder.getPortPath(id);
+        boolean exists = zkDao.exists(path);
+
+        log.debug("PortZkDao.exists exiting: exists=" + exists);
+        return exists;
+    }
+
+    /**
+     * Get the data for the given port.
+     *
+     * @param id
+     *            ID of the port.
+     * @return PortConfig stored in ZK.
+     * @throws StateAccessException
+     *             Data access error.
+     */
+    public PortConfig getData(UUID id) throws StateAccessException {
+        log.debug("PortPathDao.getData entered: id={}", id);
+
+        ZkNodeEntry<UUID, PortConfig> node = zkDao.get(id);
+        PortConfig config = node.value;
+
+        log.debug("PortPathDao.getData exiting");
+        return config;
+    }
+
+    /**
+     * Get the data for the given port.
+     *
+     * @param id
+     *            ID of the port.
+     * @return PortMgmtConfig stored in ZK.
+     * @throws StateAccessException
+     *             Data access error.
+     */
+    public PortMgmtConfig getMgmtData(UUID id) throws StateAccessException {
+        log.debug("PortPathDao.getMgmtData entered: id={}", id);
+
+        String path = pathBuilder.getPortPath(id);
+        byte[] data = zkDao.get(path);
+        PortMgmtConfig config = serializer.deserialize(data);
+
+        log.debug("PortPathDao.getMgmtData exiting: path=" + path);
+        return config;
+    }
+
+    /**
+     * Get a set of port IDs for a given router.
+     *
+     * @param routerId
+     *            ID of the router.
+     * @return Set of port IDs.
+     * @throws StateAccessException
+     *             Data access error.
+     */
+    public Set<UUID> getRouterPortIds(UUID routerId)
+            throws StateAccessException {
+        log.debug("PortPathDao.getRouterPortIds entered: routerId={}", routerId);
+
+        Set<UUID> ids = new TreeSet<UUID>();
+        List<ZkNodeEntry<UUID, PortConfig>> nodes = zkDao
+                .listRouterPorts(routerId);
+        for (ZkNodeEntry<UUID, PortConfig> node : nodes) {
+            ids.add(node.key);
+        }
+
+        log.debug("PortPathDao.getRouterPortIds exiting: ids count="
+                + ids.size());
+        return ids;
+    }
+
+    /**
+     * Get a set of port IDs for a given bridge.
+     *
+     * @param routerId
+     *            ID of the bridge.
+     * @return Set of port IDs.
+     * @throws StateAccessException
+     *             Data access error.
+     */
+    public Set<UUID> getBridgePortIds(UUID bridgeId)
+            throws StateAccessException {
+        log.debug("PortPathDao.getBridgePortIds entered: bridgeId={}", bridgeId);
+
+        Set<UUID> ids = new TreeSet<UUID>();
+        List<ZkNodeEntry<UUID, PortConfig>> nodes = zkDao
+                .listBridgePorts(bridgeId);
+        for (ZkNodeEntry<UUID, PortConfig> node : nodes) {
+            ids.add(node.key);
+        }
+
+        log.debug("PortPathDao.getBridgePortIds exiting: ids count="
+                + ids.size());
+        return ids;
+    }
+
+    /**
+     * Wrapper for multi ZK API. The paths in the Op list are expected already
+     * to be created.
+     *
+     * @param ops
+     * @throws StateAccessException
+     */
+    public void multi(List<Op> ops) throws StateAccessException {
+        log.debug("PortZkDao.multi entered: ops count={}", ops.size());
+        zkDao.multi(ops);
+        log.debug("PortZkDao.multi exiting.");
+    }
+}
