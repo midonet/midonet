151ac27fd4961f8871a1021c812bdcca55d7ba50
Ryu Ishimoto <ryu@midokura.com>
2012-01-06 18:07:15 +0900

    Added Bridge Op Builder class and its unit tests.
    Signed-off-by: Ryu Ishimoto <ryu@midokura.com>
    
    To-be-reviewed-by: Pino de Candia <pino@midokura.com>

---
+++ src/main/java/com/midokura/midolman/mgmt/data/zookeeper/op/BridgeOpBuilder.java
@@ -0,0 +1,173 @@
+/*
+ * @(#)BridgeOpBuilder        1.6 12/1/6
+ *
+ * Copyright 2012 Midokura KK
+ */
+package com.midokura.midolman.mgmt.data.zookeeper.op;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import org.apache.zookeeper.Op;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.midokura.midolman.mgmt.data.dao.zookeeper.BridgeZkDao;
+import com.midokura.midolman.mgmt.data.dto.config.BridgeMgmtConfig;
+import com.midokura.midolman.mgmt.data.dto.config.BridgeNameMgmtConfig;
+import com.midokura.midolman.state.BridgeZkManager.BridgeConfig;
+import com.midokura.midolman.state.StateAccessException;
+
+/**
+ * Bridge Op builder.
+ *
+ * @version 1.6 6 Jan 2012
+ * @author Ryu Ishimoto
+ */
+public class BridgeOpBuilder {
+
+    private final static Logger log = LoggerFactory
+            .getLogger(BridgeOpBuilder.class);
+    private final BridgeOpPathBuilder pathBuilder;
+    private final PortOpBuilder portOpBuilder;
+    private final BridgeZkDao zkDao;
+
+    /**
+     * Constructor
+     *
+     * @param pathBuilder
+     *            BridgeOpPathBuilder object.
+     * @param portOpBuilder
+     *            PortOpBuilder object.
+     * @param zkDao
+     *            BridgeZkDao object.
+     */
+    public BridgeOpBuilder(BridgeOpPathBuilder pathBuilder,
+            PortOpBuilder portOpBuilder, BridgeZkDao zkDao) {
+        this.pathBuilder = pathBuilder;
+        this.portOpBuilder = portOpBuilder;
+        this.zkDao = zkDao;
+    }
+
+    /**
+     * Build list of Op objects to create a bridge
+     *
+     * @param id
+     *            ID of the bridge
+     * @param config
+     *            BridgeConfig object
+     * @param mgmtConfig
+     *            BridgeMgmtConfig object
+     * @param nameConfig
+     *            BridgeNameMgmtConfig object
+     * @return List of Op objects
+     * @throws StateAccessException
+     *             Data access error
+     */
+    public List<Op> buildCreate(UUID id, BridgeConfig config,
+            BridgeMgmtConfig mgmtConfig, BridgeNameMgmtConfig nameConfig)
+            throws StateAccessException {
+        log.debug("BridgeOpBuilder.buildCreate entered: id={}", id);
+
+        List<Op> ops = new ArrayList<Op>();
+
+        // Create the root bridge path
+        ops.add(pathBuilder.getBridgeCreateOp(id, mgmtConfig));
+
+        // Add the bridge under tenant.
+        ops.add(pathBuilder.getTenantBridgeCreateOp(mgmtConfig.tenantId, id));
+
+        // Add the bridge name.
+        ops.add(pathBuilder.getTenantBridgeNameCreateOp(mgmtConfig.tenantId,
+                mgmtConfig.name, nameConfig));
+
+        // Create Midolman data
+        ops.addAll(pathBuilder.getBridgeCreateOps(id, config));
+
+        log.debug("BridgeOpBuilder.buildCreate exiting: ops count={}",
+                ops.size());
+        return ops;
+    }
+
+    /**
+     * Build list of Op objects to delete a bridge
+     *
+     * @param id
+     *            ID of the port
+     * @param cascade
+     *            True if cascade delete
+     * @return List of Op objects
+     * @throws StateAccessException
+     *             Data error.
+     */
+    public List<Op> buildDelete(UUID id, boolean cascade)
+            throws StateAccessException {
+        log.debug("BridgeOpBuilder.buildDelete entered: id=" + id
+                + ", cascade=" + cascade);
+
+        BridgeMgmtConfig mgmtConfig = zkDao.getData(id);
+
+        List<Op> ops = new ArrayList<Op>();
+
+        // Delete the Midolman side.
+        if (cascade) {
+            ops.addAll(pathBuilder.getBridgeDeleteOps(id));
+        }
+
+        // Delete the ports
+        ops.addAll(portOpBuilder.buildBridgePortsDelete(id));
+
+        // Delete the tenant bridge name
+        ops.add(pathBuilder.getTenantBridgeNameDeleteOp(mgmtConfig.tenantId,
+                mgmtConfig.name));
+
+        // Delete the tenant bridge
+        ops.add(pathBuilder.getTenantBridgeDeleteOp(mgmtConfig.tenantId, id));
+
+        // Delete the root bridge path.
+        ops.add(pathBuilder.getBridgeDeleteOp(id));
+
+        log.debug("BridgeOpBuilder.buildDelete exiting: ops count={}",
+                ops.size());
+        return ops;
+    }
+
+    /**
+     * Build list of Op objects to update a bridge
+     *
+     * @param id
+     *            ID of the bridge
+     * @param name
+     *            Name of the bridge
+     * @return Op list
+     * @throws StateAccessException
+     *             Data error.
+     */
+    public List<Op> buildUpdate(UUID id, String name)
+            throws StateAccessException {
+        log.debug("BridgeOpBuilder.buildUpdate entered: id=" + id + ", name="
+                + name);
+
+        List<Op> ops = new ArrayList<Op>();
+        BridgeMgmtConfig config = zkDao.getData(id);
+
+        // Remove the name of this bridge
+        ops.add(pathBuilder.getTenantBridgeNameDeleteOp(config.tenantId,
+                config.name));
+
+        // Move NameConfig to the new name path.
+        BridgeNameMgmtConfig nameConfig = zkDao.getNameData(config.tenantId,
+                config.name);
+        ops.add(pathBuilder.getTenantBridgeNameCreateOp(config.tenantId, name,
+                nameConfig));
+
+        // Update bridge
+        config.name = name;
+        ops.add(pathBuilder.getBridgeSetDataOp(id, config));
+
+        log.debug("BridgeOpBuilder.buildUpdate exiting: ops count={}",
+                ops.size());
+        return ops;
+    }
+}
---
+++ src/test/java/com/midokura/midolman/mgmt/data/zookeeper/op/TestBridgeOpBuilder.java
@@ -0,0 +1,194 @@
+/*
+ * @(#)TestBridgeOpBuilder        1.6 12/1/6
+ *
+ * Copyright 2012 Midokura KK
+ */
+package com.midokura.midolman.mgmt.data.zookeeper.op;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import junit.framework.Assert;
+
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.Op;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.midokura.midolman.mgmt.data.dao.zookeeper.BridgeZkDao;
+import com.midokura.midolman.mgmt.data.dto.config.BridgeMgmtConfig;
+import com.midokura.midolman.mgmt.data.dto.config.BridgeNameMgmtConfig;
+import com.midokura.midolman.state.BridgeZkManager.BridgeConfig;
+
+public class TestBridgeOpBuilder {
+
+    private BridgeOpPathBuilder pathBuilderMock = null;
+    private BridgeZkDao zkDaoMock = null;
+    private PortOpBuilder portOpBuilderMock = null;
+    private BridgeOpBuilder builder = null;
+    private static final Op dummyCreateOp0 = Op.create("/foo",
+            new byte[] { 0 }, null, CreateMode.PERSISTENT);
+    private static final Op dummyCreateOp1 = Op.create("/bar",
+            new byte[] { 1 }, null, CreateMode.PERSISTENT);
+    private static final Op dummyCreateOp2 = Op.create("/baz",
+            new byte[] { 2 }, null, CreateMode.PERSISTENT);
+    private static final Op dummyDeleteOp0 = Op.delete("/foo", -1);;
+    private static final Op dummyDeleteOp1 = Op.delete("/bar", -1);
+    private static final Op dummyDeleteOp2 = Op.delete("/baz", -1);
+    private static List<Op> dummyCreateOps = null;
+    static {
+        dummyCreateOps = new ArrayList<Op>();
+        dummyCreateOps.add(dummyCreateOp0);
+        dummyCreateOps.add(dummyCreateOp1);
+        dummyCreateOps.add(dummyCreateOp2);
+    }
+    private static List<Op> dummyDeleteOps = null;
+    static {
+        dummyDeleteOps = new ArrayList<Op>();
+        dummyDeleteOps.add(dummyDeleteOp0);
+        dummyDeleteOps.add(dummyDeleteOp1);
+        dummyDeleteOps.add(dummyDeleteOp2);
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        this.pathBuilderMock = mock(BridgeOpPathBuilder.class);
+        this.portOpBuilderMock = mock(PortOpBuilder.class);
+        this.zkDaoMock = mock(BridgeZkDao.class);
+        this.builder = new BridgeOpBuilder(this.pathBuilderMock,
+                this.portOpBuilderMock, this.zkDaoMock);
+    }
+
+    @Test
+    public void TestBuildCreateBridgeSuccess() throws Exception {
+        UUID id = UUID.randomUUID();
+        BridgeConfig config = new BridgeConfig();
+        BridgeMgmtConfig mgmtConfig = new BridgeMgmtConfig();
+        mgmtConfig.tenantId = "foo";
+        mgmtConfig.name = "bar";
+        BridgeNameMgmtConfig nameConfig = new BridgeNameMgmtConfig();
+
+        // Mock the path builder
+        when(pathBuilderMock.getBridgeCreateOp(id, mgmtConfig)).thenReturn(
+                dummyCreateOp0);
+        when(pathBuilderMock.getTenantBridgeCreateOp(mgmtConfig.tenantId, id))
+                .thenReturn(dummyCreateOp1);
+        when(
+                pathBuilderMock.getTenantBridgeNameCreateOp(
+                        mgmtConfig.tenantId, mgmtConfig.name, nameConfig))
+                .thenReturn(dummyCreateOp2);
+        when(pathBuilderMock.getBridgeCreateOps(id, config)).thenReturn(
+                dummyCreateOps);
+
+        List<Op> ops = builder.buildCreate(id, config, mgmtConfig, nameConfig);
+
+        Assert.assertEquals(6, ops.size());
+        Assert.assertEquals(dummyCreateOp0, ops.get(0));
+        Assert.assertEquals(dummyCreateOp1, ops.get(1));
+        Assert.assertEquals(dummyCreateOp2, ops.get(2));
+        Assert.assertEquals(dummyCreateOp0, ops.get(3));
+        Assert.assertEquals(dummyCreateOp1, ops.get(4));
+        Assert.assertEquals(dummyCreateOp2, ops.get(5));
+    }
+
+    @Test
+    public void TestBuildDeleteWithCascadeSuccess() throws Exception {
+        UUID id = UUID.randomUUID();
+        BridgeMgmtConfig mgmtConfig = new BridgeMgmtConfig();
+        mgmtConfig.tenantId = "foo";
+        mgmtConfig.name = "bar";
+
+        // Mock the path builder
+        when(zkDaoMock.getData(id)).thenReturn(mgmtConfig);
+        when(pathBuilderMock.getBridgeDeleteOps(id)).thenReturn(dummyDeleteOps);
+        when(portOpBuilderMock.buildBridgePortsDelete(id)).thenReturn(
+                dummyDeleteOps);
+        when(
+                pathBuilderMock.getTenantBridgeNameDeleteOp(
+                        mgmtConfig.tenantId, mgmtConfig.name)).thenReturn(
+                dummyDeleteOp0);
+        when(pathBuilderMock.getTenantBridgeDeleteOp(mgmtConfig.tenantId, id))
+                .thenReturn(dummyDeleteOp1);
+        when(pathBuilderMock.getBridgeDeleteOp(id)).thenReturn(dummyDeleteOp2);
+
+        List<Op> ops = builder.buildDelete(id, true);
+
+        Assert.assertEquals(9, ops.size());
+        Assert.assertEquals(dummyDeleteOp0, ops.get(0));
+        Assert.assertEquals(dummyDeleteOp1, ops.get(1));
+        Assert.assertEquals(dummyDeleteOp2, ops.get(2));
+        Assert.assertEquals(dummyDeleteOp0, ops.get(3));
+        Assert.assertEquals(dummyDeleteOp1, ops.get(4));
+        Assert.assertEquals(dummyDeleteOp2, ops.get(5));
+        Assert.assertEquals(dummyDeleteOp0, ops.get(6));
+        Assert.assertEquals(dummyDeleteOp1, ops.get(7));
+        Assert.assertEquals(dummyDeleteOp2, ops.get(8));
+    }
+
+    @Test
+    public void TestBuildDeleteWithNoCascadeSuccess() throws Exception {
+        UUID id = UUID.randomUUID();
+        BridgeMgmtConfig mgmtConfig = new BridgeMgmtConfig();
+        mgmtConfig.tenantId = "foo";
+        mgmtConfig.name = "bar";
+
+        // Mock the path builder
+        when(zkDaoMock.getData(id)).thenReturn(mgmtConfig);
+        when(portOpBuilderMock.buildBridgePortsDelete(id)).thenReturn(
+                dummyDeleteOps);
+        when(
+                pathBuilderMock.getTenantBridgeNameDeleteOp(
+                        mgmtConfig.tenantId, mgmtConfig.name)).thenReturn(
+                dummyDeleteOp0);
+        when(pathBuilderMock.getTenantBridgeDeleteOp(mgmtConfig.tenantId, id))
+                .thenReturn(dummyDeleteOp1);
+        when(pathBuilderMock.getBridgeDeleteOp(id)).thenReturn(dummyDeleteOp2);
+
+        List<Op> ops = builder.buildDelete(id, false);
+
+        Assert.assertEquals(6, ops.size());
+        Assert.assertEquals(dummyDeleteOp0, ops.get(0));
+        Assert.assertEquals(dummyDeleteOp1, ops.get(1));
+        Assert.assertEquals(dummyDeleteOp2, ops.get(2));
+        Assert.assertEquals(dummyDeleteOp0, ops.get(3));
+        Assert.assertEquals(dummyDeleteOp1, ops.get(4));
+        Assert.assertEquals(dummyDeleteOp2, ops.get(5));
+    }
+
+    @Test
+    public void TestBuildUpdateSuccess() throws Exception {
+        UUID id = UUID.randomUUID();
+        String name = "foo";
+        BridgeMgmtConfig mgmtConfig = new BridgeMgmtConfig();
+        mgmtConfig.tenantId = "bar";
+        mgmtConfig.name = "baz";
+        BridgeNameMgmtConfig nameConfig = new BridgeNameMgmtConfig();
+
+        // Mock the path builder
+        when(zkDaoMock.getData(id)).thenReturn(mgmtConfig);
+        when(zkDaoMock.getNameData(mgmtConfig.tenantId, mgmtConfig.name))
+                .thenReturn(nameConfig);
+        when(
+                pathBuilderMock.getTenantBridgeNameDeleteOp(
+                        mgmtConfig.tenantId, mgmtConfig.name)).thenReturn(
+                dummyCreateOp0);
+        when(
+                pathBuilderMock.getTenantBridgeNameCreateOp(
+                        mgmtConfig.tenantId, name, nameConfig)).thenReturn(
+                dummyCreateOp1);
+        when(pathBuilderMock.getBridgeSetDataOp(id, mgmtConfig)).thenReturn(
+                dummyCreateOp2);
+
+        List<Op> ops = builder.buildUpdate(id, name);
+
+        Assert.assertEquals(3, ops.size());
+        Assert.assertEquals(dummyCreateOp0, ops.get(0));
+        Assert.assertEquals(dummyCreateOp1, ops.get(1));
+        Assert.assertEquals(dummyCreateOp2, ops.get(2));
+        Assert.assertEquals(name, mgmtConfig.name);
+    }
+}
