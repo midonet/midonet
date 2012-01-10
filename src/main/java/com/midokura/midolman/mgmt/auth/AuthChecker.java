569d0dec2c394683a50869e035281c4b87a0aca1
Ryu Ishimoto <ryu@midokura.com>
2012-01-10 11:46:49 +0900

    Added helper Enum classes for authorization.
    Signed-off-by: Ryu Ishimoto <ryu@midokura.com>

---
+++ src/main/java/com/midokura/midolman/mgmt/auth/AuthChecker.java
@@ -0,0 +1,67 @@
+/*
+ * @(#)AuthChecker        1.6 12/1/8
+ *
+ * Copyright 2012 Midokura KK
+ */
+package com.midokura.midolman.mgmt.auth;
+
+import java.util.UUID;
+
+import javax.ws.rs.core.SecurityContext;
+
+/**
+ * Class to check authorization.
+ *
+ * @version 1.6 8 Jan 2012
+ * @author Ryu Ishimoto
+ */
+public class AuthChecker {
+
+    /**
+     * Checks whether the user sending the request is admin.
+     *
+     * @param context
+     *            Request context.
+     * @return True if admin.
+     */
+    public boolean isAdmin(SecurityContext context) {
+        return (context.isUserInRole(AuthRole.ADMIN.toString()));
+    }
+
+    /**
+     * Checks whether the user sending the request is service provider.
+     *
+     * @param context
+     *            Request context.
+     * @return True if provider.
+     */
+    public boolean isProvider(SecurityContext context) {
+        return (isAdmin(context) || context.isUserInRole(AuthRole.PROVIDER
+                .toString()));
+    }
+
+    /**
+     * Checks whether the user principal ID passed in matches that of the
+     * request. Always returns true for admin.
+     *
+     * @param id
+     *            User principal ID
+     * @return True if the requester ID matches the ID.
+     */
+    public boolean isUserPrincipal(SecurityContext context, String id) {
+        return (isAdmin(context))
+                || context.getUserPrincipal().getName().equals(id);
+    }
+
+    /**
+     * Checks whether the user principal ID passed in matches that of the
+     * request. Always returns true for admin.
+     *
+     * @param id
+     *            User principal ID
+     * @return True if the requester ID matches the ID.
+     */
+    public boolean isUserPrincipal(SecurityContext context, UUID id) {
+        return isUserPrincipal(context, id.toString());
+    }
+}
