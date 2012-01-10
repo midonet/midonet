912354b1484a0662c637f8669eb6275781bb00ea
Ryu Ishimoto <ryu@midokura.com>
2012-01-10 16:52:25 +0900

    Added comments to AdRouteDao
    Signed-off-by; Ryu Ishimoto <ryu@midokura.com>

--- src/main/java/com/midokura/midolman/mgmt/data/dao/AdRouteDao.java
+++ src/main/java/com/midokura/midolman/mgmt/data/dao/AdRouteDao.java
@@ -11,13 +11,48 @@ import java.util.UUID;
 import com.midokura.midolman.mgmt.data.dto.AdRoute;
 import com.midokura.midolman.state.StateAccessException;
 
+/**
+ * Data access class for ad routes.
+ *
+ * @version 1.6 29 Nov 2011
+ * @author Yoshi Tamura
+ */
 public interface AdRouteDao {
 
+    /**
+     * @param adRoute
+     *            AdRoute object to create.
+     * @return New ID of the object created.
+     * @throws StateAccessException
+     *             Data access error.
+     */
     UUID create(AdRoute adRoute) throws StateAccessException;
 
+    /**
+     * Delete a route.
+     *
+     * @param id
+     *            ID of the route to delete.
+     * @throws StateAccessException
+     *             Data access error.
+     */
+    void delete(UUID id) throws StateAccessException;
+
+    /**
+     * @param id
+     *            ID of the AdRoute to retrieve.
+     * @return AdRoute object.
+     * @throws StateAccessException
+     *             Data access error.
+     */
     AdRoute get(UUID id) throws StateAccessException;
 
+    /**
+     * @param bgpId
+     *            ID of the BGP to get the list of AdRoute objects for.
+     * @return A list of AdRoute objects.
+     * @throws StateAccessException
+     *             Data access error.
+     */
     List<AdRoute> list(UUID bgpId) throws StateAccessException;
-
-    void delete(UUID id) throws StateAccessException;
 }
