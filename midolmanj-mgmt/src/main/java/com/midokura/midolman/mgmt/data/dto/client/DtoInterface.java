524f3c4a523441d40fb5226a46cfe7dd4cb13974
Mihai Claudiu Toader <mtoader@midokura.com>
2012-02-10 15:06:29 +0100

    Interface Management - REST API client classes.
     Implementation of the interface management client classes.
    
     Signed-off-by: Mihai Claudiu Toader <mtoader@midokura.com>

---
+++ midolmanj-mgmt/src/main/java/com/midokura/midolman/mgmt/data/dto/client/DtoInterface.java
@@ -0,0 +1,84 @@
+package com.midokura.midolman.mgmt.data.dto.client;
+
+import java.net.URI;
+import java.util.UUID;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ * @author Mihai Claudiu Toader <mtoader@midokura.com>
+ *         Date: 1/31/12
+ */
+@XmlRootElement
+public class DtoInterface {
+
+    public enum Type {
+        Physical, Virtual, Tunnel, Unknown
+    }
+
+    private UUID id;
+    private UUID hostId;
+    private String name;
+    private String mac;
+    private int mtu;
+    private Type type;
+
+    @XmlTransient
+    private URI uri;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public URI getUri() {
+        return uri;
+    }
+
+    public void setUri(URI uri) {
+        this.uri = uri;
+    }
+
+    public UUID getHostId() {
+        return hostId;
+    }
+
+    public void setHostId(UUID hostId) {
+        this.hostId = hostId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMac() {
+        return mac;
+    }
+
+    public void setMac(String mac) {
+        this.mac = mac;
+    }
+
+    public int getMtu() {
+        return mtu;
+    }
+
+    public void setMtu(int mtu) {
+        this.mtu = mtu;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+}
