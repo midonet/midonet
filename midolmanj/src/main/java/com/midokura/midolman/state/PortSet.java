719bde6748a62a604c26d6b2ae1fb0c3216e01db
Jacob Mandelson <jlm@midokura.com>
2012-03-12 23:29:44 -0700

    Use proper constructor & fromString factory method.
    Signed-off-by: Jacob Mandelson <jlm@midokura.com>

--- midolmanj/src/main/java/com/midokura/midolman/state/PortSet.java
+++ midolmanj/src/main/java/com/midokura/midolman/state/PortSet.java
@@ -4,8 +4,14 @@ package com.midokura.midolman.state;
 
 import java.util.UUID;
 
+import org.apache.zookeeper.CreateMode;
+
 
 public class PortSet extends ReplicatedSet<UUID> {
+    public PortSet(Directory d, CreateMode createMode) {
+        super(d, createMode);
+    }
+
     public String encode(UUID item) { return item.toString(); }
-    public UUID decode(String str) { return new UUID(str); }
+    public UUID decode(String str) { return UUID.fromString(str); }
 }
