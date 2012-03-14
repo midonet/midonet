4b55a1f762c01a1c193d9aba2203123d4fce5d23
Jacob Mandelson <jlm@midokura.com>
2012-03-14 13:19:14 -0700

    Add IPv4 (replicated) set.
    Signed-off-by: Jacob Mandelson <jlm@midokura.com>

---
+++ midolmanj/src/main/java/com/midokura/midolman/state/IPv4Set.java
@@ -0,0 +1,16 @@
+// Copyright 2012 Midokura Inc.
+
+package com.midokura.midolman.state;
+
+import org.apache.zookeeper.CreateMode;
+
+import com.midokura.midolman.packets.IntIPv4;
+
+public class IPv4Set extends ReplicatedSet<IntIPv4> {
+    public IPv4Set(Directory d, CreateMode createMode) {
+        super(d, createMode);
+    }
+
+    protected String encode(IntIPv4 item) { return item.toString(); }
+    protected IntIPv4 decode(String str) { return IntIPv4.fromString(str); }
+}
