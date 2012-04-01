179f5871e38902c6557efa684f34493481fc5376
gdecandia <gdecandia@midokura.jp>
2012-04-02 00:13:27 +0900

    Extended BridgeRouterTest to use two bridge and one router.

--- midonet-functional-tests/src/test/java/com/midokura/midonet/functional_test/BridgeRouterTest.java
+++ midonet-functional-tests/src/test/java/com/midokura/midonet/functional_test/BridgeRouterTest.java
@@ -40,15 +40,20 @@ public class BridgeRouterTest {
 
     private final static Logger log = LoggerFactory.getLogger(BridgeRouterTest.class);
 
-    IntIPv4 rtrIp = IntIPv4.fromString("192.168.231.1");
+    IntIPv4 rtrIp1 = IntIPv4.fromString("192.168.231.1");
+    IntIPv4 rtrIp2 = IntIPv4.fromString("192.168.240.1");
     IntIPv4 ip1 = IntIPv4.fromString("192.168.231.2");
-    IntIPv4 subnetAddr = IntIPv4.fromString("192.168.231.0", 24);
+    IntIPv4 ip2 = IntIPv4.fromString("192.168.240.2");
+    IntIPv4 subnetAddr1 = IntIPv4.fromString("192.168.231.0", 24);
+    IntIPv4 subnetAddr2 = IntIPv4.fromString("192.168.240.0", 24);
 
     Router rtr;
-    BridgeRouterLink link;
+    BridgeRouterLink link1;
+    BridgeRouterLink link2;
     Tenant tenant1;
     RouterPort p1;
     TapWrapper tap1;
+    TapWrapper tap2;
     OpenvSwitchDatabaseConnectionImpl ovsdb;
     MidolmanMgmt api;
     MidolmanLauncher midolman;
@@ -78,13 +83,23 @@ public class BridgeRouterTest {
         tenant1 = new Tenant.Builder(api).setName("tenant-ping").build();
         rtr = tenant1.addRouter().setName("rtr1").build();
 
+        // Create a virtual L2 bridge with one tap port.
         Bridge bridge = tenant1.addBridge().setName("br1").build();
-        BridgePort bPort1 = bridge.addPort();
+        BridgePort bPort = bridge.addPort();
         tap1 = new TapWrapper("tapBridge1");
-        ovsBridge.addSystemPort(bPort1.getId(), tap1.getName());
+        ovsBridge.addSystemPort(bPort.getId(), tap1.getName());
 
         // Link the Bridge and Router
-        link = rtr.addBridgeRouterLink(bridge, subnetAddr);
+        link1 = rtr.addBridgeRouterLink(bridge, subnetAddr1);
+
+        // Create another virtual L2 bridge with one tap port.
+        bridge = tenant1.addBridge().setName("br2").build();
+        bPort = bridge.addPort();
+        tap2 = new TapWrapper("tapBridge2");
+        ovsBridge.addSystemPort(bPort.getId(), tap2.getName());
+
+        // Link the Bridge and Router
+        link2 = rtr.addBridgeRouterLink(bridge, subnetAddr2);
 
         sleepBecause("we wait for the network configuration to bootup", 5);
     }
@@ -92,10 +107,13 @@ public class BridgeRouterTest {
     @After
     public void tearDown() throws Exception {
         removeTapWrapper(tap1);
+        removeTapWrapper(tap2);
         removeBridge(ovsBridge);
         stopMidolman(midolman);
-        if (null != link)
-            link.delete();
+        if (null != link1)
+            link1.delete();
+        if (null != link2)
+            link2.delete();
         removeTenant(tenant1);
         stopMidolmanMgmt(api);
     }
@@ -105,10 +123,10 @@ public class BridgeRouterTest {
             throws MalformedPacketException {
         byte[] request;
 
-        // First arp for router's mac.
-        MAC vmMac = MAC.fromString("02:00:00:00:00:c1");
+        // Arp for router's mac on first bridge.
+        MAC vmMac1 = MAC.fromString("02:00:00:00:00:c1");
         assertThat("The ARP request was sent properly",
-                tap1.send(PacketHelper.makeArpRequest(vmMac, ip1, rtrIp)));
+                tap1.send(PacketHelper.makeArpRequest(vmMac1, ip1, rtrIp1)));
 
         byte[] received = tap1.recv();
         assertThat("We expected a package that we didn't get.",
@@ -116,20 +134,45 @@ public class BridgeRouterTest {
         Ethernet eth = new Ethernet();
         eth.deserialize(ByteBuffer.wrap(received, 0, received.length));
         // Now that we know the router's MAC we can create the packet helper.
-        PacketHelper helper = new PacketHelper(vmMac, ip1,
-                eth.getSourceMACAddress(), rtrIp);
-        helper.checkArpReply(received);
+        PacketHelper helper1 = new PacketHelper(vmMac1, ip1,
+                eth.getSourceMACAddress(), rtrIp1);
+        helper1.checkArpReply(received);
 
         // Ping router's port.
-        request = helper.makeIcmpEchoRequest(rtrIp);
+        request = helper1.makeIcmpEchoRequest(rtrIp1);
         assertThat(
-            format("The tap %s should have sent the packet", tap1.getName()),
-            tap1.send(request));
+                format("The tap %s should have sent the packet", tap1.getName()),
+                tap1.send(request));
 
         // Note: Midolman's virtual router currently does not ARP before
         // responding to ICMP echo requests addressed to its own port.
         PacketHelper.checkIcmpEchoReply(request, tap1.recv());
 
         assertNoMorePacketsOnTap(tap1);
+
+        // Arp for router's mac on second bridge.
+        MAC vmMac2 = MAC.fromString("02:00:00:00:00:c2");
+        assertThat("The ARP request was sent properly",
+                tap2.send(PacketHelper.makeArpRequest(vmMac2, ip2, rtrIp2)));
+
+        received = tap2.recv();
+        assertThat("We expected a package that we didn't get.",
+                received, notNullValue());
+        eth = new Ethernet();
+        eth.deserialize(ByteBuffer.wrap(received, 0, received.length));
+        // Now that we know the router's MAC we can create the packet helper.
+        PacketHelper helper2 = new PacketHelper(vmMac2, ip2,
+                eth.getSourceMACAddress(), rtrIp2);
+        helper2.checkArpReply(received);
+
+        // Send a ping request from tap1 to tap2.
+        request = helper1.makeIcmpEchoRequest(ip2);
+        assertThat(
+                format("The tap %s should have sent the packet",
+                        tap1.getName()), tap1.send(request));
+        received = tap2.recv();
+        assertThat("We expected a package that we didn't get.",
+                received, notNullValue());
+        helper2.checkIcmpEchoRequest(request, received);
     }
 }
