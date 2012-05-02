f7e300341eb826580c98c849a89549340f9c47de
Abel Navarro <abel@midokura.com>
2012-05-02 18:26:22 +0200

    Moved RuleEngine functionality into Chains and Chain
    Signed-off-by: Abel Navarro <abel@midokura.com>

---
+++ midolmanj/src/main/java/com/midokura/midolman/rules/Chain.java
@@ -0,0 +1,157 @@
+/*
+ * Copyright 2012 Midokura Pte. Ltd.
+ */
+
+package com.midokura.midolman.rules;
+
+import com.midokura.midolman.openflow.MidoMatch;
+import com.midokura.midolman.state.Directory;
+import com.midokura.midolman.state.RuleZkManager;
+import com.midokura.midolman.state.StateAccessException;
+import com.midokura.midolman.state.ZkNodeEntry;
+import com.midokura.midolman.state.ZkStateSerializationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.*;
+
+public class Chain {
+    private final static Logger log = LoggerFactory.getLogger(Chain.class);
+
+    private class RulesWatcher implements Runnable {
+        @Override
+        public void run() {
+            try {
+                updateRules();
+            } catch (Exception e) {
+                log.warn("RulesWatcher.run", e);
+            }
+        }
+    }
+
+    private UUID chainId;
+    private String chainName;
+    private List<Rule> rules;
+    private RulesWatcher rulesWatcher;
+    private RuleZkManager zkRuleManager;
+    private Chains chains;
+
+    public Chain(UUID chainId, String chainName, Directory zkDirectory, String zkBasePath, Chains chains) {
+        this.chainId = chainId;
+        this.chainName = chainName;
+        this.rules = new LinkedList<Rule>();
+        this.rulesWatcher = new RulesWatcher();
+        this.zkRuleManager = new RuleZkManager(zkDirectory, zkBasePath);
+        this.chains = chains;
+    }
+
+    public String getChainName() {
+        return chainName;
+    }
+
+    public void setChainName(String chainName) {
+        this.chainName = chainName;
+    }
+
+    public void updateRules()
+            throws StateAccessException, ZkStateSerializationException {
+
+        List<Rule> newRules = new ArrayList<Rule>();
+
+        List<ZkNodeEntry<UUID, Rule>> entries = zkRuleManager.list(chainId, rulesWatcher);
+        for (ZkNodeEntry<UUID, Rule> entry : entries) {
+            newRules.add(entry.value);
+        }
+        Collections.sort(newRules);
+
+        rules = newRules;
+
+        // TODO(abel) check nat mappings
+
+        /*
+        updateResources();
+        notifyWatchers();
+        */
+    }
+
+    private class ChainPosition {
+        String chainName; // keep this for debugging.
+        List<Rule> rules;
+        int position;
+
+        public ChainPosition(String chainName, List<Rule> rules, int position) {
+            super();
+            this.chainName = chainName;
+            this.rules = rules;
+            this.position = position;
+        }
+    }
+
+    public RuleResult process(MidoMatch flowMatch,
+                              MidoMatch pktMatch, UUID inPortId, UUID outPortId) {
+
+        if (rules.size() == 0) {
+            log.debug("applyChain {} - empty, return ACCEPT", chainName);
+            return new RuleResult(RuleResult.Action.ACCEPT, null, pktMatch, false);
+        }
+
+        Stack<ChainPosition> chainStack = new Stack<ChainPosition>();
+        chainStack.push(new ChainPosition(chainName, rules, 0));
+        Set<String> traversedChains = new HashSet<String>();
+        traversedChains.add(chainName);
+
+        RuleResult res = new RuleResult(RuleResult.Action.CONTINUE, null,
+                pktMatch.clone(), false);
+        while (!chainStack.empty()) {
+            ChainPosition cp = chainStack.pop();
+            while (cp.position < cp.rules.size()) {
+                // Reset the default action and jumpToChain. Keep the
+                // transformed match and trackConnection.
+                res.action = RuleResult.Action.CONTINUE;
+                res.jumpToChain = null;
+                cp.rules.get(cp.position).process(flowMatch, inPortId,
+                        outPortId, res);
+                cp.position++;
+                if (res.action.equals(RuleResult.Action.ACCEPT)
+                        || res.action.equals(RuleResult.Action.DROP)
+                        || res.action.equals(RuleResult.Action.REJECT)) {
+                    return res;
+                } else if (res.action.equals(RuleResult.Action.JUMP)) {
+                    if (traversedChains.contains(res.jumpToChain)) {
+                        // Avoid jumping to chains we've already seen.
+                        log.warn("applyChain {} cannot jump to chain {} - "
+                                + "already visited.", new Object[] { chainName, res.jumpToChain });
+                        continue;
+                    }
+
+                    Chain nextChain = chains.getChainByName(res.jumpToChain);
+                    if (null == nextChain) {
+                        // Let's just ignore jumps to non-existent chains.
+                        log.warn("ignoring jump to chain {} - not found.", res.jumpToChain);
+                        continue;
+                    }
+
+                    traversedChains.add(res.jumpToChain);
+                    // Remember the calling chain.
+                    chainStack.push(cp);
+                    chainStack.push(new ChainPosition(res.jumpToChain,
+                            nextChain.rules, 0));
+                    break;
+                } else if (res.action.equals(RuleResult.Action.RETURN)) {
+                    // Stop processing this chain; return to the calling chain.
+                    break;
+                } else if (res.action.equals(RuleResult.Action.CONTINUE)) {
+                    // Move on to the next rule in the same chain.
+                    continue;
+                } else {
+                    log.error("Unknown action type {} in rule chain",
+                            res.action);
+                    // TODO: Should we throw an exception?
+                    continue;
+                }
+            }
+        }
+        res.action = RuleResult.Action.ACCEPT;
+        return res;
+    }
+}
--- midolmanj/src/main/java/com/midokura/midolman/rules/RuleEngine.java
+++ midolmanj/src/main/java/com/midokura/midolman/rules/RuleEngine.java
@@ -35,58 +35,22 @@ public class RuleEngine {
 
     private final static Logger log = LoggerFactory.getLogger(RuleEngine.class);
 
-    private class RouterWatcher implements Runnable {
-        @Override
-        public void run() {
-            try {
-                updateChains(true);
-            } catch (Exception e) {
-                log.warn("RouteWatcher.run", e);
-            }
-        }
-    }
-
-    private class RuleChainWatcher implements Runnable {
-        UUID chainId;
-
-        RuleChainWatcher(UUID chainId) {
-            this.chainId = chainId;
-        }
-
-        @Override
-        public void run() {
-            try {
-                updateRules(chainId, this);
-            } catch (Exception e) {
-                log.warn("RuleChainWatcher.run", e);
-            }
-        }
-    }
-
     private UUID rtrId;
     private String rtrIdStr;
-    private ChainZkManager zkChainMgr;
     private RuleZkManager zkRuleMgr;
     private NatMapping natMap;
-    protected Map<String, UUID> chainNameToUUID;
-    protected Map<UUID, String> chainIdToName;
-    protected Map<UUID, List<Rule>> ruleChains;
-    private RouterWatcher rtrWatcher;
     private Set<Callback<UUID>> watchers;
+    Chains chains;
 
     public RuleEngine(Directory zkDir, String zkBasePath, UUID rtrId,
                       NatMapping natMap) throws StateAccessException {
         this.rtrId = rtrId;
         rtrIdStr = rtrId.toString();
-        this.zkChainMgr = new ChainZkManager(zkDir, zkBasePath);
         this.zkRuleMgr = new RuleZkManager(zkDir, zkBasePath);
         this.natMap = natMap;
-        chainNameToUUID = new HashMap<String, UUID>();
-        chainIdToName = new HashMap<UUID, String>();
-        ruleChains = new HashMap<UUID, List<Rule>>();
-        rtrWatcher = new RouterWatcher();
         watchers = new HashSet<Callback<UUID>>();
-        updateChains(false);
+        chains = new Chains(zkDir, zkBasePath, rtrId);
+        chains.updateChains();
     }
 
     public void addWatcher(Callback<UUID> watcher) {
@@ -103,81 +67,7 @@ public class RuleEngine {
             watcher.call(rtrId);
     }
 
-    private void updateChains(boolean notify) throws StateAccessException,
-            ZkStateSerializationException {
-        Collection<ZkNodeEntry<UUID, ChainConfig>> entryList = zkChainMgr.list(
-                rtrId, rtrWatcher);
-        Map<UUID, String> newChainNames = new HashMap<UUID, String>();
-        for (ZkNodeEntry<UUID, ChainConfig> entry : entryList) {
-            newChainNames.put(entry.key, entry.value.name);
-        }
-        Set<UUID> currentChains = new HashSet<UUID>(newChainNames.keySet());
-        Set<UUID> newChains = new HashSet<UUID>(newChainNames.keySet());
-        Set<UUID> oldChains = new HashSet<UUID>(ruleChains.keySet());
-        newChains.removeAll(oldChains);
-        oldChains.removeAll(currentChains);
-
-        // Any old chains that are not in currentChains should be removed.
-        for (UUID chain : oldChains) {
-            ruleChains.remove(chain);
-            String name = chainIdToName.remove(chain);
-            chainNameToUUID.remove(name);
-            log.debug("{} removed a chain named {}", rtrIdStr, name);
-        }
-        // Any brand new chains should be processed.
-        for (UUID chain : newChains) {
-            updateRules(chain, new RuleChainWatcher(chain));
-            String name = newChainNames.get(chain);
-            chainIdToName.put(chain, name);
-            chainNameToUUID.put(name, chain);
-            log.debug("{} added a chain named {}", rtrIdStr, name);
-        }
-        // If no chains were added or deleted we're done. Otherwise, we need to
-        // recompute the resources and notify listeners.
-        if (oldChains.isEmpty() && newChains.isEmpty())
-            return;
-        updateResources();
-        notifyWatchers();
-    }
-
-    private void updateRules(UUID chainId, Runnable watcher)
-            throws StateAccessException, ZkStateSerializationException {
-        // TODO(pino): this implementation doesn't use the UUIDs (it preceeded
-        // the introduction of the UUIDs) but they make it possible to know
-        // exactly which rules changed.
-        List<Rule> curRules = new ArrayList<Rule>();
-        List<ZkNodeEntry<UUID, Rule>> entries = zkRuleMgr
-                .list(chainId, watcher);
-        for (ZkNodeEntry<UUID, Rule> entry : entries)
-            curRules.add(entry.value);
-        Collections.sort(curRules);
-
-        List<Rule> oldRules = ruleChains.get(chainId);
-        if (null != oldRules && oldRules.equals(curRules))
-            // The chain was updated with the same rules. Do nothing.
-            return;
-        log.debug(
-                "{} updating chain {}. Old length {}, new length {}.",
-                new Object[] { rtrIdStr, chainIdToName.get(chainId),
-                        null == oldRules ? 0 : oldRules.size(), curRules.size() });
-
-        // Initialize rules that need it.
-        for (Rule r : curRules) {
-            log.debug("Added rule: {}", r);
-            if (r instanceof NatRule)
-                ((NatRule) r).setNatMapping(natMap);
-        }
-        ruleChains.put(chainId, curRules);
-
-        // If this is a new chain, we're done: the router watcher will handle
-        // notifications.
-        if (null == oldRules)
-            return;
-        // The chain already existed. We handle notifications here.
-        updateResources();
-        notifyWatchers();
-    }
-
+    /*
     private void updateResources() {
         // Tell the NatMapping about all the current NatTargets for SNAT.
         // TODO(pino): the NatMapping should clean up any old targets that
@@ -194,19 +84,7 @@ public class RuleEngine {
         }
         natMap.updateSnatTargets(targets);
     }
-
-    private class ChainPosition {
-        String chainName; // keep this for debugging.
-        List<Rule> rules;
-        int position;
-
-        public ChainPosition(String chainName, List<Rule> rules, int position) {
-            super();
-            this.chainName = chainName;
-            this.rules = rules;
-            this.position = position;
-        }
-    }
+    */
 
     /**
      *
@@ -223,76 +101,8 @@ public class RuleEngine {
      */
     public RuleResult applyChain(String chainName, MidoMatch flowMatch,
             MidoMatch pktMatch, UUID inPortId, UUID outPortId) {
-        List<Rule> chain = null;
-        UUID chainId = chainNameToUUID.get(chainName);
-        if (null != chainId)
-            chain = ruleChains.get(chainId);
-        if (null == chain || chain.size() == 0) {
-            log.debug("applyChain {} - empty, return ACCEPT", chainName);
-            return new RuleResult(Action.ACCEPT, null, pktMatch, false);
-        }
 
-        Stack<ChainPosition> chainStack = new Stack<ChainPosition>();
-        chainStack.push(new ChainPosition(chainName, chain, 0));
-        Set<String> traversedChains = new HashSet<String>();
-        traversedChains.add(chainName);
-
-        RuleResult res = new RuleResult(Action.CONTINUE, null,
-                pktMatch.clone(), false);
-        while (!chainStack.empty()) {
-            ChainPosition cp = chainStack.pop();
-            while (cp.position < cp.rules.size()) {
-                // Reset the default action and jumpToChain. Keep the
-                // transformed match and trackConnection.
-                res.action = Action.CONTINUE;
-                res.jumpToChain = null;
-                cp.rules.get(cp.position).process(flowMatch, inPortId,
-                        outPortId, res);
-                cp.position++;
-                if (res.action.equals(Action.ACCEPT)
-                        || res.action.equals(Action.DROP)
-                        || res.action.equals(Action.REJECT)) {
-                    return res;
-                } else if (res.action.equals(Action.JUMP)) {
-                    if (traversedChains.contains(res.jumpToChain)) {
-                        // Avoid jumping to chains we've already seen.
-                        log.warn("{} applyChain {} cannot jump to chain {} - "
-                                + "already visited.", new Object[] { rtrIdStr,
-                                chainName, res.jumpToChain });
-                        continue;
-                    }
-                    List<Rule> nextChain = null;
-                    chainId = chainNameToUUID.get(res.jumpToChain);
-                    if (null != chainId)
-                        nextChain = ruleChains.get(chainId);
-                    if (null == nextChain) {
-                        // Let's just ignore jumps to non-existent chains.
-                        log.warn("{} ignoring jump to chain {} - not found.",
-                                rtrIdStr, res.jumpToChain);
-                        continue;
-                    }
-                    traversedChains.add(res.jumpToChain);
-                    // Remember the calling chain.
-                    chainStack.push(cp);
-                    chainStack.push(new ChainPosition(res.jumpToChain,
-                            nextChain, 0));
-                    break;
-                } else if (res.action.equals(Action.RETURN)) {
-                    // Stop processing this chain; return to the calling chain.
-                    break;
-                } else if (res.action.equals(Action.CONTINUE)) {
-                    // Move on to the next rule in the same chain.
-                    continue;
-                } else {
-                    log.error("Unknown action type {} in rule chain",
-                            res.action);
-                    // TODO: Should we throw an exception?
-                    continue;
-                }
-            }
-        }
-        res.action = Action.ACCEPT;
-        return res;
+        return chains.process(chainName, flowMatch, pktMatch, inPortId, outPortId);
     }
 
     public void freeFlowResources(OFMatch match) {
