ad37426141583ed275b277972e12d2674711b54f
Ryu Ishimoto <ryu@midokura.com>
2012-05-23 15:34:37 +0900

    Modified unit tests for rule and route resources in mgmt for better maintainability.
    Signed-off-by: Ryu Ishimoto <ryu@midokura.com>

--- midolmanj-mgmt/src/test/java/com/midokura/midolman/mgmt/rest_api/resources/TestRuleResource.java
+++ midolmanj-mgmt/src/test/java/com/midokura/midolman/mgmt/rest_api/resources/TestRuleResource.java
@@ -6,129 +6,88 @@ package com.midokura.midolman.mgmt.rest_api.resources;
 
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
-import java.net.URI;
 import java.util.UUID;
 
 import javax.ws.rs.core.SecurityContext;
 import javax.ws.rs.core.UriInfo;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Answers;
+import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import com.midokura.midolman.mgmt.auth.AuthAction;
 import com.midokura.midolman.mgmt.auth.Authorizer;
 import com.midokura.midolman.mgmt.data.DaoFactory;
 import com.midokura.midolman.mgmt.data.dao.RuleDao;
-import com.midokura.midolman.mgmt.data.dto.Rule;
 import com.midokura.midolman.mgmt.rest_api.jaxrs.ForbiddenHttpException;
 import com.midokura.midolman.state.NoStatePathException;
-import com.midokura.midolman.state.StateAccessException;
 
 @RunWith(MockitoJUnitRunner.class)
 public class TestRuleResource {
 
-    private SecurityContext contextMock = null;
-    private DaoFactory factoryMock = null;
-    private Authorizer authMock = null;
-    private UriInfo uriInfoMock = null;
-    private RuleResource resource = null;
-    private RuleDao daoMock = null;
+    private RuleResource testObject;
+
+    @Mock(answer = Answers.RETURNS_SMART_NULLS)
+    private SecurityContext context;
+
+    @Mock(answer = Answers.RETURNS_SMART_NULLS)
+    private DaoFactory factory;
+
+    @Mock(answer = Answers.RETURNS_SMART_NULLS)
+    private Authorizer auth;
+
+    @Mock(answer = Answers.RETURNS_SMART_NULLS)
+    private UriInfo uriInfo;
+
+    @Mock(answer = Answers.RETURNS_SMART_NULLS)
+    private RuleDao dao;
 
     @Before
     public void setUp() throws Exception {
-        this.contextMock = mock(SecurityContext.class);
-        this.factoryMock = mock(DaoFactory.class);
-        this.authMock = mock(Authorizer.class);
-        this.daoMock = mock(RuleDao.class);
-        this.uriInfoMock = mock(UriInfo.class);
-        this.resource = new RuleResource();
-
-        doReturn(daoMock).when(factoryMock).getRuleDao();
-    }
-
-    @Test
-    public void testDeleteSuccess() throws Exception {
-        UUID id = UUID.randomUUID();
-        doReturn(true).when(authMock).ruleAuthorized(contextMock,
-                AuthAction.WRITE, id);
-        resource.delete(id, contextMock, factoryMock, authMock);
-
-        verify(daoMock, times(1)).delete(id);
+        testObject = new RuleResource();
+        doReturn(dao).when(factory).getRuleDao();
     }
 
     @Test(expected = ForbiddenHttpException.class)
     public void testDeleteUnauthorized() throws Exception {
+        // Set up
         UUID id = UUID.randomUUID();
-        doReturn(false).when(authMock).ruleAuthorized(contextMock,
-                AuthAction.WRITE, id);
-        resource.delete(id, contextMock, factoryMock, authMock);
+        doReturn(false).when(auth).ruleAuthorized(context, AuthAction.WRITE,
+                id);
+
+        // Execute
+        testObject.delete(id, context, factory, auth);
     }
 
     @Test
     public void testDeleteNonExistentData() throws Exception {
+        // Set up
         UUID id = UUID.randomUUID();
-        doReturn(true).when(authMock).ruleAuthorized(contextMock,
-                AuthAction.WRITE, id);
-        doThrow(NoStatePathException.class).when(daoMock).delete(id);
+        doReturn(true).when(auth).ruleAuthorized(context, AuthAction.WRITE,
+                id);
+        doThrow(NoStatePathException.class).when(dao).delete(id);
 
-        resource.delete(id, contextMock, factoryMock, authMock);
+        // Execute
+        testObject.delete(id, context, factory, auth);
 
-        verify(daoMock, times(1)).delete(id);
-    }
-
-    @Test(expected = StateAccessException.class)
-    public void testDeleteDataAccessError() throws Exception {
-        UUID id = UUID.randomUUID();
-        doReturn(true).when(authMock).ruleAuthorized(contextMock,
-                AuthAction.WRITE, id);
-        doThrow(StateAccessException.class).when(daoMock).delete(id);
-        resource.delete(id, contextMock, factoryMock, authMock);
-    }
-
-    private Rule getRule(URI baseUri) {
-        Rule rule = new Rule();
-        rule.setBaseUri(baseUri);
-        return rule;
-    }
-
-    @Test
-    public void testGetSuccess() throws Exception {
-        UUID id = UUID.randomUUID();
-        URI uri = URI.create("http://www.foo.com");
-        Rule rule = getRule(uri);
-        doReturn(true).when(authMock).ruleAuthorized(contextMock,
-                AuthAction.READ, id);
-        doReturn(uri).when(uriInfoMock).getBaseUri();
-        doReturn(rule).when(daoMock).get(id);
-
-        Rule result = resource.get(id, contextMock, uriInfoMock,
-                factoryMock, authMock);
-
-        Assert.assertEquals(rule, result);
-        Assert.assertEquals(uri, result.getBaseUri());
+        // Verify
+        verify(dao, times(1)).delete(id);
     }
 
     @Test(expected = ForbiddenHttpException.class)
     public void testGetUnauthorized() throws Exception {
+        // Set up
         UUID id = UUID.randomUUID();
-        doReturn(false).when(authMock).ruleAuthorized(contextMock,
-                AuthAction.READ, id);
-        resource.get(id, contextMock, uriInfoMock, factoryMock, authMock);
-    }
+        doReturn(false).when(auth).ruleAuthorized(context, AuthAction.READ,
+                id);
 
-    @Test(expected = StateAccessException.class)
-    public void testGetDataAccessError() throws Exception {
-        UUID id = UUID.randomUUID();
-        doReturn(true).when(authMock).ruleAuthorized(contextMock,
-                AuthAction.READ, id);
-        doThrow(StateAccessException.class).when(daoMock).get(id);
-        resource.get(id, contextMock, uriInfoMock, factoryMock, authMock);
+        // Execute
+        testObject.get(id, context, uriInfo, factory, auth);
     }
 }
