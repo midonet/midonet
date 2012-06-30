fffdac9d08792636e42813b53c7fadc21dc7467e
gdecandia <gdecandia@midokura.jp>
2012-06-30 09:38:37 +0200

    Replace Java actors with Scala actors.

---
+++ midolmanj/src/main/scala/com/midokura/midolman/vrn/SimulationActor.scala
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2012 Midokura Europe SARL
+ */
+
+package com.midokura.midolman.vrn
+
+import akka.actor.{Actor, Props}
+import akka.event.Logging
+
+class SimulationActor extends Actor {
+
+}
---
+++ midolmanj/src/main/scala/com/midokura/midolman/vrn/VirtualTopologyActor.scala
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2012 Midokura Europe SARL
+ */
+package com.midokura.midolman.vrn
+
+import akka.actor.{Actor, Props}
+import akka.event.Logging
+
+class VirtualTopologyActor extends Actor {
+
+}
