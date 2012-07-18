472b1a39b1114b3d76f6b9e1728f68f8335522d7
Jacob Mandelson <jlm@midokura.com>
2012-07-18 20:14:35 +0200

    Create skeleton interfaces for the flow management classes used by the VRN's ControllerActor.
    Signed-off-by: Jacob Mandelson <jlm@midokura.com>

--- midolmanj/src/main/scala/com/midokura/midolman/vrn/ControllerActor.scala
+++ midolmanj/src/main/scala/com/midokura/midolman/vrn/ControllerActor.scala
@@ -3,22 +3,64 @@
 package com.midokura.midolman.vrn
 
 import akka.actor.{Actor, ActorRef}
+import compat.Platform.currentTime
+import scala.collection.Iterator
+import scala.collection.immutable.List
 import scala.collection.mutable.{Map, Set}
-import java.util.ArrayList
+import java.util.{ArrayList, UUID}
 
 import com.midokura.util.netlink.dp.Packet
+import com.midokura.util.netlink.dp.{Flow => KernelFlow}
+import com.midokura.util.netlink.dp.flows.FlowAction
 import com.midokura.midolman.openflow.MidoMatch
 
 
 abstract class ControllerActorRequest
 case class PacketIn(packet: Packet) extends ControllerActorRequest
-case class SimulationDone(result: Unit) extends ControllerActorRequest
+case class SimulationDone(result: KernelFlow) extends ControllerActorRequest
+
+//XXX: Move into separate file
+class WildcardFlow(val wcmatch: MidoMatch, val actions: List[FlowAction[_]]) {
+    val creationTime = currentTime
+    private var lastUsedTime = creationTime
+
+    def getLastUsedTime() = lastUsedTime
+    def setLastUsedTime(timestamp: Long) { 
+        if (timestamp > lastUsedTime)
+            lastUsedTime = timestamp
+    }
+}
+
+abstract class WildcardFlowManager {
+    def matchPacket(packet: Packet): KernelFlow
+    def add(flow: WildcardFlow): Unit
+    def remove(wcmatch: MidoMatch): Unit
+    def markUsed(flow: WildcardFlow): Unit
+    def markUnused(flows: Set[WildcardFlow]): Unit
+}
+
+abstract class ExactFlowManager {
+    // Returns evicted WildcardFlows
+    def add(wcflow: WildcardFlow, flow: KernelFlow): Set[WildcardFlow]
+
+    def removeByWildcard(wcflow: WildcardFlow): Unit
+    def contains(flow: KernelFlow): Boolean
+}
+
+abstract class ValidationEngine {
+    def add(wcflow: WildcardFlow, devices: Set[UUID]): Unit
+    def remove(wcflow: WildcardFlow): Unit
+    def getFlows(id: UUID): Set[WildcardFlow]
+    // Older by creation time
+    def getFlowsOlderThan(timestamp: Long): Iterator[WildcardFlow]
+    def getFlowsOlderThan(id: UUID, timestamp: Long): Iterator[WildcardFlow]
+}
+
 
 class ControllerActor(XXX: Unit) extends Actor {
     type FlowID = Int
-    type WCFlow = MidoMatch
-    type KernelFlow = Unit // XXX: A flow as represented by the datapath
-    private var wcFlowToFlowIDs: Map[WCFlow, Set[FlowID]] = _
+    type WildcardFlow = MidoMatch
+    private var wcFlowToFlowIDs: Map[WildcardFlow, Set[FlowID]] = _
     private var flowIDToKernelFlow: Map[FlowID, KernelFlow] = _
     private var installedFlowIDs: ArrayList[FlowID] = _
 
