ae180bfe670d9ac4f36f71efd28dabf080257034
Mihai Claudiu Toader <mtoader@midokura.com>
2012-07-25 15:47:17 +0200

    Don't require the bootclasspath patch unless we are actually using the netlink channel.
    Signed-off-by: Mihai Claudiu Toader <mtoader@midokura.com>

--- midokura-util/src/main/java/com/midokura/netlink/NetlinkSelectorProvider.java
+++ midokura-util/src/main/java/com/midokura/netlink/NetlinkSelectorProvider.java
@@ -4,6 +4,8 @@
 package com.midokura.netlink;
 
 import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.nio.channels.DatagramChannel;
 import java.nio.channels.Pipe;
 import java.nio.channels.ServerSocketChannel;
@@ -63,6 +65,20 @@ public class NetlinkSelectorProvider extends SelectorProvider {
     }
 
     public NetlinkChannel openNetlinkSocketChannel(Netlink.Protocol protocol) {
-        return new NetlinkTracingChannelImpl(this, protocol);
+        final String NAME = "com.midokura.netlink.NetlinkChannelImpl";
+        //        final String NAME = "com.midokura.netlink.NetlinkTracingChannelImpl";
+
+        try {
+            Class<? extends NetlinkChannel> clazz =
+                (Class<? extends NetlinkChannel>) Class.forName(NAME);
+
+            Constructor<? extends NetlinkChannel> constructor =
+                clazz.getConstructor(SelectorProvider.class, Netlink.Protocol.class);
+
+            return constructor.newInstance(this, protocol);
+        } catch (Exception e) {
+            log.error("Exception while instantiating class of type: {}", NAME, e);
+            return null;
+        }
     }
 }
