da51b617895c0420c8a35cf43b7cf2552438c7b0
gdecandia <gdecandia@midokura.jp>
2012-08-15 09:43:45 +0200

    Added a notification callback to the cluster client's MacLearningTable interface.
    Signed-off-by: gdecandia <gdecandia@midokura.jp>

---
+++ midokura-util/src/main/java/com/midokura/util/functors/Callback2.java
@@ -0,0 +1,9 @@
+/*
+ * Copyright 2012 Midokura Europe SARL
+ */
+
+package com.midokura.util.functors;
+
+public interface Callback2<A, B> {
+    void call(A a, B b);
+}
--- midolmanj/src/main/java/com/midokura/midonet/cluster/client/MacLearningTable.java
+++ midolmanj/src/main/java/com/midokura/midonet/cluster/client/MacLearningTable.java
@@ -1,13 +1,13 @@
 /*
  * Copyright 2012 Midokura Europe SARL
  */
-
 package com.midokura.midonet.cluster.client;
 
 import java.util.UUID;
 
 import com.midokura.util.functors.Callback1;
 import com.midokura.packets.MAC;
+import com.midokura.util.functors.Callback2;
 
 /*
  * Non-blocking.
@@ -16,4 +16,5 @@ public interface MacLearningTable {
     void get(MAC mac, Callback1<UUID> cb);
     void add(MAC mac, UUID portID);
     void remove(MAC mac, UUID portID);
+    void notify(Callback2<MAC, UUID> cb);
 }
