8aa32a5072a133646995e0fd4cb70c71fa8a80ba
Marc de Palol <marc@midokura.jp>
2012-08-21 16:52:27 +0200

    Added test to check that the BridgeBuilder's MacPortMap gets modified when using the ClusterClient.

--- midolmanj/src/test/java/com/midonet/cluster/LocalClientImplTest.java
+++ midolmanj/src/test/java/com/midonet/cluster/LocalClientImplTest.java
@@ -15,8 +15,12 @@ import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.KeeperException;
 import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.*;
 
 import com.midokura.midolman.Setup;
 import com.midokura.midolman.config.MidolmanConfig;
@@ -27,7 +31,10 @@ import com.midokura.midolman.guice.config.TypedConfigModule;
 import com.midokura.midolman.guice.reactor.ReactorModule;
 import com.midokura.midolman.guice.zookeeper.MockZookeeperConnectionModule;
 import com.midokura.midolman.state.Directory;
+import com.midokura.midolman.state.MacPortMap;
+import com.midokura.midolman.state.ReplicatedMap;
 import com.midokura.midolman.state.StateAccessException;
+import com.midokura.midolman.state.ZkStateSerializationException;
 import com.midokura.midolman.state.zkManagers.BridgeZkManager;
 import com.midokura.midonet.cluster.Client;
 import com.midokura.midonet.cluster.client.BridgeBuilder;
@@ -35,8 +42,13 @@ import com.midokura.midonet.cluster.client.Builder;
 import com.midokura.midonet.cluster.client.DeviceBuilder;
 import com.midokura.midonet.cluster.client.MacLearningTable;
 import com.midokura.midonet.cluster.client.SourceNatResource;
+import com.midokura.packets.MAC;
+import com.midokura.util.functors.Callback1;
+import com.midokura.util.functors.Callback3;
 
 public class LocalClientImplTest {
+    
+    private Logger log = LoggerFactory.getLogger(LocalClientImplTest.class);
 
     @Inject
     Client client;
@@ -98,6 +110,49 @@ public class LocalClientImplTest {
                    bridgeBuilder.getBuildCallsCount(), equalTo(2));
 
     }
+    
+    @Test
+    public void MacPortMapTest() throws InterruptedException, KeeperException, ZkStateSerializationException, StateAccessException {
+        initializeZKStructure();
+        Setup.createZkDirectoryStructure(zkDir(), zkRoot);
+        UUID bridgeId = getBridgeZkManager().create(
+            new BridgeZkManager.BridgeConfig("test", UUID.randomUUID(), UUID.randomUUID()));
+        TestBridgeBuilder bridgeBuilder = new TestBridgeBuilder();
+        client.getBridge(bridgeId, bridgeBuilder);
+        Thread.sleep(2000);
+        assertThat("Build is called", bridgeBuilder.getBuildCallsCount(), equalTo(1));
+        
+        // and a new packet. 
+        MAC mac = MAC.random(); 
+        UUID portUUID = UUID.randomUUID(); 
+        
+        ///////////
+        // This sends two notifications. 
+        bridgeBuilder.simulateNewPacket(mac,portUUID); 
+        ///////////
+        
+        Thread.sleep(2000);
+        // make sure the  notifications sent what we expected.
+        assertEquals(bridgeBuilder.getNotifiedMAC(), mac); 
+        assertNull(bridgeBuilder.getNotifiedUUID()[0]); 
+        assertEquals(portUUID, bridgeBuilder.getNotifiedUUID()[1]);
+        
+        // make sure the packet is there. 
+        assertEquals(portUUID, bridgeBuilder.getPort(mac)); 
+        
+        // remove the port. 
+        bridgeBuilder.removePort(mac, portUUID); 
+        // make sure the  notifications sent what we expected.
+        Thread.sleep(2000);
+        assertEquals(bridgeBuilder.getNotifiedMAC(), mac); 
+        assertEquals(portUUID, bridgeBuilder.getNotifiedUUID()[0]); 
+        assertNull(bridgeBuilder.getNotifiedUUID()[1]);
+        
+        // make sure that the mac <-> port association has been removed. 
+        assertNull(bridgeBuilder.getPort(mac)); 
+        
+        assertThat("Bridge update was notified", bridgeBuilder.getBuildCallsCount(), equalTo(1));
+    }
 
     void initializeZKStructure() throws InterruptedException, KeeperException {
 
@@ -112,9 +167,46 @@ public class LocalClientImplTest {
         }
     }
 
+    // hint could modify this class so we can get the map from it. 
     class TestBridgeBuilder implements BridgeBuilder {
         int buildCallsCount = 0;
+        MacLearningTable mlTable; 
+        MAC[] notifiedMAC = new MAC[1]; 
+        UUID[] notifiedUUID = new UUID[2]; 
+        
+        public void simulateNewPacket(MAC mac, UUID portId) {
+            mlTable.add(mac, portId); 
+        }
 
+        public void removePort(MAC mac, UUID portId) {
+            mlTable.remove(mac, portId); 
+        }
+        
+        public UUID getPort(MAC mac) {
+            final UUID result[] = new UUID[1]; 
+            
+            mlTable.get(mac, new Callback1<UUID>() {
+                @Override
+                public void call(UUID v) {
+                    result[0] = v; 
+                }
+            });
+            
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+            } 
+            return result[0]; 
+        }
+
+        public MAC getNotifiedMAC() {
+            return notifiedMAC[0]; 
+        }
+        
+        public UUID[] getNotifiedUUID() {
+            return notifiedUUID; 
+        }
+       
         public int getBuildCallsCount() {
             return buildCallsCount;
         }
@@ -125,6 +217,7 @@ public class LocalClientImplTest {
 
         @Override
         public void setMacLearningTable(MacLearningTable table) {
+            mlTable = table; 
         }
 
         @Override
@@ -154,6 +247,15 @@ public class LocalClientImplTest {
         @Override
         public void build() {
             buildCallsCount++;
+            // add the callback 
+            mlTable.notify(new Callback3<MAC,UUID,UUID>() {
+                @Override
+                public void call(MAC mac, UUID oldPortID, UUID newPortID) {
+                    notifiedMAC[0] = mac; 
+                    notifiedUUID[0] = oldPortID; 
+                    notifiedUUID[1] = newPortID; 
+                }
+            }); 
         }
 
     }
