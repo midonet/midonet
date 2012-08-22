8bb8de74385f1c64adfc6e4a9223180815a1f3bb
rossella <rossella@midokura.com>
2012-08-22 13:46:41 +0200

    Changed Set() to Set[Any]() Signed-off-by: Rossella Sblendido <rossella@midokura.com>

--- midolmanj/src/main/scala/com/midokura/midolman/simulation/Coordinator.scala
+++ midolmanj/src/main/scala/com/midokura/midolman/simulation/Coordinator.scala
@@ -22,7 +22,7 @@ class PacketContext(var port: UUID, var mmatch: MidoMatch,
     val flowRemovedCallbacks = mutable.Set[Callback1[MidoMatch]]()
     // This Set will store the tags by which the flow should be indexed
     // The index can be used to remove flows associated with the given tag
-    val flowTags = mutable.Set()
+    val flowTags = mutable.Set[Any]()
 }
 
 case class SimulatePacket(ingress: PacketContext)
