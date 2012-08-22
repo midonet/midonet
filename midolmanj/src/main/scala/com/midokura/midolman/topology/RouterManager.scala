de88a8be9712ae817b281217646f1d71688cb942
Jacob Mandelson <jlm@midokura.com>
2012-08-22 10:48:41 -0700

    Add ArpTable interface between RCU Router and RouterManager.

--- midolmanj/src/main/scala/com/midokura/midolman/topology/RouterManager.scala
+++ midolmanj/src/main/scala/com/midokura/midolman/topology/RouterManager.scala
@@ -17,6 +17,14 @@ import com.midokura.midolman.util.JSONSerializer
 import com.midokura.midonet.cluster.client.ArpCache
 
 
+/* The MacFlowCount is called from the Coordinators' actors and dispatches
+ * to the BridgeManager's actor to get/modify the flow counts.  */
+trait ArpTable {
+    def get(ip: IntIPv4): MAC
+    def set(ip: IntIPv4, mac: MAC)
+}
+
+
 class RouterManager(id: UUID, val mgr: RouterZkManager,
                     val routeMgr: RouteZkManager)
         extends DeviceManager(id) {
@@ -27,6 +35,7 @@ class RouterManager(id: UUID, val mgr: RouterZkManager,
     private val localPortToRouteIDs = mutable.Map[UUID, mutable.Set[UUID]]()
     private val idToRoute = mutable.Map[UUID, Route]()
     private val arpCache: ArpCache = null  //XXX
+    private val arpTable = new ArpTableImpl
 
     case object RefreshTableRoutes
 
@@ -116,7 +125,7 @@ class RouterManager(id: UUID, val mgr: RouterZkManager,
     private def makeNewRouter() = {
         if (chainsReady() && null != rTable)
             context.actorFor("..").tell(
-                new Router(id, cfg, rTable, arpCache, inFilter, outFilter));
+                new Router(id, cfg, rTable, arpTable, inFilter, outFilter));
     }
 
     override def updateConfig() = {
@@ -138,10 +147,36 @@ class RouterManager(id: UUID, val mgr: RouterZkManager,
         }
     }
 
+    def getArpEntry(ip: IntIPv4): MAC = {       
+        //XXX: get the ArpCacheEntry from the ArpCache
+        //XXX: If stale, send and ARP and schedule retries & expiration.
+        //XXX: Return value if not expired.
+    }
+
     override def receive() = super.receive orElse {
         case SetRouterPortLocal(_, portId, local) =>
             updatePortLocality(portId, local)
         case RefreshTableRoutes => refreshTableRoutes()
         case RefreshLocalPortRoutes(portId) => refreshLocalPortRoutes(portId)
+        case SetArpEntry(ip, mac) =>
+            // XXX: Add/replace the IP's ArpCacheEntry in the ArpCache.
+            // XXX: Remove any scheduled ARP retries.
+            // XXX: Reschedule the ARP expiration.
+        case GetArpEntry(ip) => sender ! getArpEntry(ip)
+    }
+
+    private class ArpTableImpl extends ArpTable {
+        def get(ip: IntIPv4) = {
+            implicit val timeout = Timeout(1 minute)
+            // TODO(jlm): When the Await suspends, which execution context
+            // is suspended?  Do we need to pass in the Coordinator's EC
+            // to avoid suspending a JVM thread?
+            Await.result(self ? GetArpEntry(ip),
+                         timeout.duration).asInstanceOf[MAC]
+        }
+
+        def set(ip: IntIPv4, mac: MAC) {
+            self ! SetArpEntry(ip, mac)
+        }
     }
 }
