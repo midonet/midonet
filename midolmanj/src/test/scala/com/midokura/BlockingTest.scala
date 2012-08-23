bad21281641540735c9a1624671e97ee09c6927b
Jacob Mandelson <jlm@midokura.com>
2012-08-23 15:37:59 -0700

    Spawn a thread to complete the promise.
    Signed-off-by: Jacob Mandelson <jlm@midokura.com>

--- midolmanj/src/test/scala/com/midokura/BlockingTest.scala
+++ midolmanj/src/test/scala/com/midokura/BlockingTest.scala
@@ -18,6 +18,7 @@ import akka.dispatch.{Await, Promise}
 import akka.testkit.CallingThreadDispatcher
 import akka.util.duration._
 import compat.Platform
+import concurrent.ops.spawn
 import com.typesafe.config.ConfigFactory
 import org.junit.runner.RunWith
 import org.scalatest.{OneInstancePerTest, Suite}
@@ -38,43 +39,63 @@ class BlockingTest extends Suite with ShouldMatchers with OneInstancePerTest {
         }
     """)
     val system = ActorSystem("BlockingTest", ConfigFactory.load(config))
-    //val system = ActorSystem("BlockingTest")
+    val initialDelay = 1000L
+    val sleepTime = 5300L
+    val margin = 100L
 
-    def testOneThread() {
+    private def spawnPromiseThread(promise: Promise[Int]) {
+        /*
+        val thread = new Thread {
+        */
+        spawn {    
+            Thread.sleep(initialDelay+sleepTime)
+            promise.complete(Right(1234))
+            Thread.sleep(6000)
+        }
+    }
+
+    def testThreadSleep() {
         Thread.sleep(1000)
         val start = Platform.currentTime
         @volatile var time1 = start-1
         @volatile var time2 = start-1
-        var elapsed: Long = 0
+        @volatile var time3 = start-1
+        var elapsed = 0L
         val promise = Promise[Int]()(system.dispatcher)
         //system.dispatcher.id should be === CallingThreadDispatcher.Id
         assert(system.dispatcher.id != CallingThreadDispatcher.Id)
-        system.scheduler.scheduleOnce(1000 milliseconds) { 
+        system.scheduler.scheduleOnce(initialDelay milliseconds) { 
             time1 = Platform.currentTime 
-            Thread.sleep(5300)
+            Thread.sleep(sleepTime)
+            time3 = Platform.currentTime
         }
         system.scheduler.scheduleOnce(1500 milliseconds) { 
             time2 = Platform.currentTime 
         }
-        //new Thread { Thread.sleep(3000); promise.complete(Right(1234)) }
+
+        spawnPromiseThread(promise)
+
+        // The executor's one thread runs the first scheduled operation, and has
+        // to wait for it to complete before it can run the second scheduled
+        // operation, late if the first op blocked.
+
+        elapsed = Platform.currentTime - start
+        elapsed should be > 0L
+        elapsed should be < margin
         elapsed = time1 - start
         elapsed should be === -1
         elapsed = time2 - start
         elapsed should be === -1
-        elapsed = Platform.currentTime - start
-        elapsed should be > 0L
-        elapsed should be < 100L
-        //Await.result(promise, 4000 milliseconds)
         Thread.sleep(2000)
         elapsed = time1 - start
-        elapsed should be >= 1000L
-        elapsed should be <= 1100L
+        elapsed should be >= initialDelay
+        elapsed should be <= initialDelay + margin
         elapsed = time2 - start
         elapsed should be === -1
         Thread.sleep(6000)
         elapsed = time2 - start
-        elapsed should be >= 6300L
-        elapsed should be <= 6400L
+        elapsed should be >= initialDelay + sleepTime
+        elapsed should be <= initialDelay + sleepTime + margin
     }
 
 }
