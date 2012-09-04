0fc08b078ffead578d8747aacac845c104e4681c
Jacob Mandelson <jlm@midokura.com>
2012-09-04 09:33:34 -0700

    Change LoadBalancer::lookup to take a WildcardMatch.
    Signed-off-by: Jacob Mandelson <jlm@midokura.com>

--- midolmanj/src/main/scala/com/midokura/midolman/simulation/LoadBalancer.scala
+++ midolmanj/src/main/scala/com/midokura/midolman/simulation/LoadBalancer.scala
@@ -3,12 +3,12 @@
 package com.midokura.midolman.simulation
 
 import com.midokura.midolman.layer3.{Route, RoutingTable}
-import com.midokura.midolman.openflow.MidoMatch
 import com.midokura.midolman.topology.RoutingTableWrapper
+import com.midokura.sdn.flows.WildcardMatch
 
 
 class LoadBalancer(val rTable: RoutingTableWrapper) {
-    def lookup(mmatch: MidoMatch): Route = {
+    def lookup(mmatch: WildcardMatch): Route = {
         null //XXX
     }
 }
