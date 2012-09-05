261ef2b1938d335922c20e75f36eb950cc01287b
Mihai Claudiu Toader <mtoader@midokura.com>
2012-09-05 19:01:15 +0200

    Added PortSet abstraction to the cluster Client.
    Added basic zk callback implementation.
    
    Signed-off-by: Mihai Claudiu Toader <mtoader@midokura.com>

--- midolmanj/src/main/java/com/midokura/midonet/cluster/Client.java
+++ midolmanj/src/main/java/com/midokura/midonet/cluster/Client.java
@@ -6,12 +6,13 @@ package com.midokura.midonet.cluster;
 
 import java.util.UUID;
 
-import com.midokura.midonet.cluster.client.TunnelZones;
 import com.midokura.midonet.cluster.client.BridgeBuilder;
 import com.midokura.midonet.cluster.client.ChainBuilder;
 import com.midokura.midonet.cluster.client.HostBuilder;
 import com.midokura.midonet.cluster.client.PortBuilder;
+import com.midokura.midonet.cluster.client.PortSetBuilder;
 import com.midokura.midonet.cluster.client.RouterBuilder;
+import com.midokura.midonet.cluster.client.TunnelZones;
 
 public interface Client {
 
@@ -30,4 +31,6 @@ public interface Client {
     void getHost(UUID hostIdentifier, HostBuilder builder);
 
     void getTunnelZones(UUID uuid, TunnelZones.BuildersProvider builders);
+
+    void getPortSet(UUID uuid, PortSetBuilder builder);
 }
