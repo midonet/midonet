5b4a33ab163fb25bfe7899ac0ec2034b4a0a8309
Jacob Mandelson <jlm@midokura.com>
2012-09-05 09:25:36 -0700

    Implement simulation.LoadBalancer
    Signed-off-by: Jacob Mandelson <jlm@midokura.com>

--- midolmanj/src/main/scala/com/midokura/midolman/simulation/LoadBalancer.scala
+++ midolmanj/src/main/scala/com/midokura/midolman/simulation/LoadBalancer.scala
@@ -2,13 +2,17 @@
 
 package com.midokura.midolman.simulation
 
-import com.midokura.midolman.layer3.{Route, RoutingTable}
+import com.midokura.midolman.layer3.Route
 import com.midokura.midolman.topology.RoutingTableWrapper
 import com.midokura.sdn.flows.WildcardMatch
 
 
 class LoadBalancer(val rTable: RoutingTableWrapper) {
     def lookup(mmatch: WildcardMatch): Route = {
-        null //XXX
+        val routes = rTable.lookup(mmatch)
+        if (routes.isEmpty)
+            null
+        else
+            routes.head
     }
 }
