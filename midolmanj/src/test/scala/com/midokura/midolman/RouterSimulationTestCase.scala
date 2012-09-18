02964397146ed23f90d9830fd7ce6e3797764c9c
Guillermo Ontanon <guillermo@midokura.jp>
2012-09-18 17:39:10 +0200

    WIP: fixes in test fixture, working arp test, basic routing test almost there.
    Signed-off-by: Guillermo Ontanon <guillermo@midokura.jp>

--- midolmanj/src/test/scala/com/midokura/midolman/RouterSimulationTestCase.scala
+++ midolmanj/src/test/scala/com/midokura/midolman/RouterSimulationTestCase.scala
@@ -3,23 +3,33 @@
 */
 package com.midokura.midolman
 
+import compat.Platform
 import scala.collection.mutable
 import java.util.UUID
+import akka.util.duration._
+import akka.dispatch.Await
 import akka.testkit.TestProbe
 import org.apache.commons.configuration.HierarchicalConfiguration
 import org.junit.runner.RunWith
 import org.slf4j.LoggerFactory
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.Ignore
+import guice.actors.OutgoingMessage
+
 
 import com.midokura.midonet.cluster.data.{Router => ClusterRouter}
-import com.midokura.packets.{Ethernet, IntIPv4, MAC}
-import com.midokura.midolman.FlowController.{AddWildcardFlow, WildcardFlowAdded}
+import com.midokura.packets._
+import com.midokura.midolman.FlowController._
 import com.midokura.midonet.cluster.data.ports.MaterializedRouterPort
-import com.midokura.midolman.DatapathController.PacketIn
-import com.midokura.midonet.cluster.data.zones.GreTunnelZoneHost
+import simulation.{Router => SimRouter}
 import layer3.Route.{NextHop, NO_GATEWAY}
-
+import topology.VirtualToPhysicalMapper.{HostRequest, LocalPortActive}
+import topology.VirtualTopologyActor.{PortRequest, RouterRequest}
+import com.midokura.midonet.cluster.client.ExteriorRouterPort
+import akka.util.Timeout
+import com.midokura.midolman.FlowController.AddWildcardFlow
+import com.midokura.midolman.FlowController.WildcardFlowAdded
+import com.midokura.midolman.DatapathController.PacketIn
 
 @RunWith(classOf[JUnitRunner])
 class RouterSimulationTestCase extends MidolmanTestCase with
@@ -27,12 +37,12 @@ class RouterSimulationTestCase extends MidolmanTestCase with
     private final val log = LoggerFactory.getLogger(classOf[RouterSimulationTestCase])
 
     private var flowEventsProbe: TestProbe = null
-    private var router: ClusterRouter = null
 
-    private val uplinkGatewayAddr = 0x0a0b0c0d // 10.11.12.13
+    private var router: ClusterRouter = null
+    private val uplinkGatewayAddr = "180.0.1.1"
     private val uplinkNwAddr = 0x000000
-    private val uplinkNwLen = 0
-    private val uplinkPortAddr = "180.0.1.2" // 0xb400102 // 180.0.1.2
+    private val uplinkNwLen = 30
+    private val uplinkPortAddr = "180.0.1.2"
     private val uplinkMacAddr = MAC.fromString("02:0a:08:06:04:02")
     private var uplinkPort: MaterializedRouterPort = null
     private var upLinkRoute: UUID = null
@@ -41,7 +51,7 @@ class RouterSimulationTestCase extends MidolmanTestCase with
     private val portNumToId = mutable.Map[Int, UUID]()
     private val portNumToMac = mutable.Map[Int, MAC]()
     private val portNumToName = mutable.Map[Int, String]()
-
+    private val portNumToSegmentAddr = mutable.Map[Int, Int]()
 
     override protected def fillConfig(config: HierarchicalConfiguration) = {
         config.setProperty("datapath.max_flow_count", "10")
@@ -49,21 +59,23 @@ class RouterSimulationTestCase extends MidolmanTestCase with
     }
 
     override def before() {
-        val tunnelZone = greTunnelZone("default")
+        val host = newHost("myself", hostId())
         router = newRouter("router")
-        val host = newHost("host1", hostId())
         // Create one port that works as an uplink for the router.
-        uplinkPort = newPortOnRouter(router, uplinkMacAddr,
+        uplinkPort = newPortOnRouter(router, uplinkMacAddr, uplinkPortAddr,
                                      uplinkPortAddr, uplinkNwLen,
                                      uplinkPortAddr, uplinkNwLen)
-        materializePort(uplinkPort, host, "uplinkPort")
-        upLinkRoute = newRoute(router, "0.0.0.0", 0, "0.0.0.0", 0, NextHop.PORT,
-            uplinkPort.getId, "10.11.12.13", 1)
 
-        clusterDataClient().tunnelZonesAddMembership(
-            tunnelZone.getId,
-            new GreTunnelZoneHost(host.getId)
-                .setIp(IntIPv4.fromString("10.15.200.1")))
+        initializeDatapath() should not be (null)
+
+        materializePort(uplinkPort, host, "uplinkPort")
+        requestOfType[HostRequest](vtpProbe())
+        requestOfType[OutgoingMessage](vtpProbe())
+        requestOfType[OutgoingMessage](vtpProbe())
+        requestOfType[LocalPortActive](vtpProbe())
+
+        upLinkRoute = newRoute(router, "0.0.0.0", 0, "45.0.0.0", 8, NextHop.PORT,
+            uplinkPort.getId, uplinkGatewayAddr, 1)
 
         router should not be null
         uplinkPort should not be null
@@ -82,12 +94,16 @@ class RouterSimulationTestCase extends MidolmanTestCase with
                 val segmentAddr = new IntIPv4(nwAddr + (j * 4))
 
                 val port = newPortOnRouter(router, macAddr,
+                    new IntIPv4(portAddr).toString,
                     segmentAddr.toString, 30,
                     new IntIPv4(nwAddr).toString, 24)
 
                 port should not be null
 
                 materializePort(port, host, portName)
+                requestOfType[OutgoingMessage](vtpProbe())
+                requestOfType[LocalPortActive](vtpProbe())
+
 
                 log.debug("Created router port {}, {}", portName, macAddr)
 
@@ -96,6 +112,7 @@ class RouterSimulationTestCase extends MidolmanTestCase with
                 portNumToId.put(portNum, port.getId)
                 portNumToMac.put(portNum, macAddr)
                 portNumToName.put(portNum, portName)
+                portNumToSegmentAddr.put(portNum, segmentAddr.addressAsInt)
 
                 // Default route to port based on destination only.  Weight 2.
                 var rt = newRoute(router, "0.0.0.0", 0, segmentAddr.toString, 30,
@@ -133,10 +150,39 @@ class RouterSimulationTestCase extends MidolmanTestCase with
 
         flowEventsProbe = newProbe()
         actors().eventStream.subscribe(flowEventsProbe.ref, classOf[WildcardFlowAdded])
-
-        initializeDatapath() should not be (null)
-
         flowProbe().expectMsgType[DatapathController.DatapathReady].datapath should not be (null)
+    }
+
+    private def feedArpCache(portName: String, srcIp: Int, srcMac: MAC,
+                                               dstIp: Int, dstMac: MAC) {
+        val arp = new ARP()
+        arp.setHardwareType(ARP.HW_TYPE_ETHERNET)
+        arp.setProtocolType(ARP.PROTO_TYPE_IP)
+        arp.setHardwareAddressLength(6)
+        arp.setProtocolAddressLength(4)
+        arp.setOpCode(ARP.OP_REPLY)
+        arp.setSenderHardwareAddress(srcMac)
+        arp.setSenderProtocolAddress(IPv4.toIPv4AddressBytes(srcIp))
+        arp.setTargetHardwareAddress(dstMac)
+        arp.setTargetProtocolAddress(IPv4.toIPv4AddressBytes(dstIp))
+
+        val eth = new Ethernet()
+        eth.setPayload(arp)
+        eth.setSourceMACAddress(srcMac)
+        eth.setDestinationMACAddress(dstMac)
+        eth.setEtherType(ARP.ETHERTYPE)
+        triggerPacketIn(portName, eth)
+    }
+
+    private def expectPacketOnPort(portNum: Int): PacketIn = {
+        dpProbe().expectMsgClass(classOf[PacketIn])
+
+        val pktInMsg = simProbe().expectMsgClass(classOf[PacketIn])
+        pktInMsg should not be null
+        pktInMsg.pktBytes should not be null
+        pktInMsg.wMatch should not be null
+        pktInMsg.wMatch.getInputPortUUID should be(portNumToId(portNum))
+        pktInMsg
     }
 
     def testDropsIPv6() {
@@ -149,13 +195,7 @@ class RouterSimulationTestCase extends MidolmanTestCase with
         eth.setPad(true)
         triggerPacketIn(portNumToName(onPort), eth)
 
-        dpProbe().expectMsgClass(classOf[PacketIn])
-
-        val pktInMsg = simProbe().expectMsgClass(classOf[PacketIn])
-        pktInMsg should not be null
-        pktInMsg.pktBytes should not be null
-        pktInMsg.wMatch should not be null
-        pktInMsg.wMatch.getInputPortUUID should be(portNumToId(onPort))
+        expectPacketOnPort(onPort)
 
         flowEventsProbe.expectMsgClass(classOf[WildcardFlowAdded])
 
@@ -169,11 +209,84 @@ class RouterSimulationTestCase extends MidolmanTestCase with
         addFlowMsg.flow.getActions.size() should equal(0)
     }
 
-    /*
     @Ignore def testForwardToUplink() {
         // Make a packet that comes in on port 23 (dlDst set to port 23's mac,
         // nwSrc inside 10.0.2.12/30) and has a nwDst that matches the uplink
         // port (e.g. anything outside 10.  0.0.0/16).
+        val onPort = 23
+        val eth = Packets.udp(
+            MAC.fromString("01:02:03:04:05:06"),
+            portNumToMac(onPort),
+            new IntIPv4((portNumToSegmentAddr(onPort) + 2)),
+            IntIPv4.fromString("45.44.33.22"),
+            10, 11, "My UDP packet".getBytes)
+        triggerPacketIn(portNumToName(onPort), eth)
+        expectPacketOnPort(onPort)
+        feedArpCache("uplinkPort",
+            IntIPv4.fromString(uplinkGatewayAddr).addressAsInt,
+            MAC.fromString("aa:bb:aa:cc:dd:cc"),
+            IntIPv4.fromString(uplinkPortAddr).addressAsInt,
+            uplinkMacAddr)
+
+        requestOfType[DiscardPacket](flowProbe())
+        val addFlowMsg = requestOfType[AddWildcardFlow](flowProbe())
+        addFlowMsg should not be null
+        addFlowMsg.flow should not be null
+        val flow = addFlowMsg.flow
+        flow.getMatch.getEthernetSource should equal(MAC.fromString("01:02:03:04:05:06"))
+        flow.getMatch.getEthernetDestination should equal(portNumToMac(onPort))
+        flow.getMatch.getEtherType should equal(IPv4.ETHERTYPE)
+        flow.getMatch.getNetworkProtocol should equal(UDP.PROTOCOL_NUMBER)
+        flow.getMatch.getTransportSource should equal(10)
+        flow.getMatch.getTransportDestination should equal(11)
+        // two actions: set hw.dst and emit through port
+        flow.getActions.size() should not equal(0)
+    }
+
+    def testArpRequestFulfilledLocally() {
+        feedArpCache("uplinkPort",
+            IPv4.toIPv4Address(uplinkGatewayAddr),
+            MAC.fromString("aa:bb:aa:cc:dd:cc"),
+            IntIPv4.fromString(uplinkPortAddr).addressAsInt,
+            uplinkMacAddr)
+
+        requestOfType[PortRequest](vtaProbe())
+        requestOfType[OutgoingMessage](vtaProbe()) // ExteriorRouterPort
+        requestOfType[PortRequest](vtaProbe())
+        val port = requestOfType[OutgoingMessage](vtaProbe()).m.asInstanceOf[ExteriorRouterPort]
+
+        requestOfType[RouterRequest](vtaProbe())
+        val router = replyOfType[SimRouter](vtaProbe())
+        requestOfType[PacketIn](simProbe())
+
+        val expiry = Platform.currentTime + 1000
+        val arpPromise = router.arpTable.get(IntIPv4.fromString(uplinkGatewayAddr),
+            port, expiry)(actors().dispatcher, actors())
+        val t = Timeout(3 seconds)
+        val mac = Await.result(arpPromise, t.duration)
+        mac should equal(MAC.fromString("aa:bb:aa:cc:dd:cc"))
+    }
+
+    /*
+    @Ignore def testArpRequestNonLocalAddress() {
+    }
+
+    @Ignore def testArpRequestGeneration() {
+    }
+
+    @Ignore def testArpRequestRetry() {
+    }
+
+    @Ignore def testArpRequestTimeout() {
+    }
+
+    @Ignore def testArpRequestFulfilledLocally() {
+    }
+
+    @Ignore def testArpRequestFulfilledRemotely() {
+    }
+
+    @Ignore def testArpReceivedRequestProcessing() {
     }
 
     @Ignore def testForwardBetweenDownlinks() {
@@ -201,27 +314,6 @@ class RouterSimulationTestCase extends MidolmanTestCase with
     @Ignore def testUnlinkedLogicalPort() {
     }
 
-    @Ignore def testArpRequestNonLocalAddress() {
-    }
-
-    @Ignore def testArpRequestGeneration() {
-    }
-
-    @Ignore def testArpRequestRetry() {
-    }
-
-    @Ignore def testArpRequestTimeout() {
-    }
-
-    @Ignore def testArpRequestFulfilledLocally() {
-    }
-
-    @Ignore def testArpRequestFulfilledRemotely() {
-    }
-
-    @Ignore def testArpReceivedRequestProcessing() {
-    }
-
     @Ignore def testFilterBadSrcForPort() {
     }
 
@@ -230,5 +322,6 @@ class RouterSimulationTestCase extends MidolmanTestCase with
 
     @Ignore def testDnat() {
     }
+
     */
 }
