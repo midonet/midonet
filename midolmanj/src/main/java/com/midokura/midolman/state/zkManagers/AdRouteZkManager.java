4e19cb58d1af48100f939b874764f9df806f9e58
Abel Navarro <abel@midokura.com>
2012-09-28 11:39:03 +0200

    Added async getter methods for AdRoutes
    Signed-off-by: Abel Navarro <abel@midokura.com>

--- midolmanj/src/main/java/com/midokura/midolman/state/zkManagers/AdRouteZkManager.java
+++ midolmanj/src/main/java/com/midokura/midolman/state/zkManagers/AdRouteZkManager.java
@@ -6,14 +6,20 @@ package com.midokura.midolman.state.zkManagers;
 
 import java.net.InetAddress;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
 import com.midokura.midolman.state.Directory;
+import com.midokura.midolman.state.DirectoryCallback;
+import com.midokura.midolman.state.DirectoryCallbackFactory;
 import com.midokura.midolman.state.StateAccessException;
 import com.midokura.midolman.state.ZkManager;
 import com.midokura.midolman.state.ZkStateSerializationException;
+import com.midokura.midonet.cluster.data.BGP;
+import com.midokura.util.functors.CollectionFunctors;
+import com.midokura.util.functors.Functor;
 import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.Op;
 import org.apache.zookeeper.ZooDefs.Ids;
@@ -95,6 +101,46 @@ public class AdRouteZkManager extends ZkManager {
 
     public boolean exists(UUID id) throws StateAccessException {
         return exists(paths.getAdRoutePath(id));
+    }
+
+    public void getAdRouteAsync(final UUID adRouteId, DirectoryCallback <AdRouteConfig> adRouteDirectoryCallback,
+                            final Directory.TypedWatcher watcher) {
+
+        String adRoutePath = paths.getAdRoutePath(adRouteId);
+
+        zk.asyncGet(adRoutePath,
+                DirectoryCallbackFactory.transform(
+                        adRouteDirectoryCallback,
+                        new Functor<byte[], AdRouteConfig>() {
+                            @Override
+                            public AdRouteConfig apply(byte[] arg0) {
+                                try {
+                                    return serializer.deserialize(arg0, AdRouteConfig.class);
+                                } catch (ZkStateSerializationException e) {
+                                    log.warn("Could not deserialize AdRoute data");
+                                }
+                                return null;
+                            }
+                        }),
+                watcher);
+    }
+    public void getAdRouteListAsync(final DirectoryCallback<Set<UUID>>
+                                        adRouteContentsCallback,
+                                Directory.TypedWatcher watcher) {
+        String adRoutePath = paths.getAdRoutesPath();
+
+        zk.asyncGetChildren(
+                adRoutePath,
+                DirectoryCallbackFactory.transform(
+                        adRouteContentsCallback,
+                        new Functor<Set<String>, Set<UUID>>() {
+                            @Override
+                            public Set<UUID> apply(Set<String> arg0) {
+                                return CollectionFunctors.map(
+                                        arg0, strToUUIDMapper, new HashSet<UUID>());
+                            }
+                        }
+                ), watcher);
     }
 
     public List<UUID> list(UUID bgpId, Runnable watcher)
