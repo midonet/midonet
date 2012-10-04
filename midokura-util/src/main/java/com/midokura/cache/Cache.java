3df8833de0cfdd93e688541530f15abf51dcac5a
Jacob Mandelson <jlm@midokura.com>
2012-10-04 16:43:52 -0700

    Make an AsyncCache interface.  Implement AsyncCassandraCache::get.
    Signed-off-by: Jacob Mandelson <jlm@midokura.com>

--- midokura-util/src/main/java/com/midokura/cache/Cache.java
+++ midokura-util/src/main/java/com/midokura/cache/Cache.java
@@ -4,13 +4,10 @@
  */
 package com.midokura.cache;
 
-import com.midokura.util.functors.Callback1;
-
 
 public interface Cache {
     void set(String key, String value);
     String get(String key);
-    void getAsync(String key, Callback1<String> valueCb);
     String getAndTouch(String key);
     int getExpirationSeconds();
 }
