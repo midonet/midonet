7e0e354d57da7e21822921e632233d29db631b7b
gdecandia <gdecandia@midokura.jp>
2012-10-05 07:52:48 +0200

    ArpTable needs to convert all its IntIPv4 keys to have maskLength=32.
    Signed-off-by: gdecandia <gdecandia@midokura.jp>

--- midolmanj/src/main/java/com/midokura/midolman/state/ArpTable.java
+++ midolmanj/src/main/java/com/midokura/midolman/state/ArpTable.java
@@ -2,12 +2,45 @@
 
 package com.midokura.midolman.state;
 
+import org.apache.zookeeper.KeeperException;
+
 import com.midokura.packets.IntIPv4;
 
 public class ArpTable extends ReplicatedMap<IntIPv4, ArpCacheEntry> {
 
     public ArpTable(Directory dir) {
         super(dir);
+    }
+
+    private IntIPv4 unicastIPv4(IntIPv4 ip) {
+        return (ip.getMaskLength() != 32) ?
+            new IntIPv4(ip.getAddress(), 32) : ip;
+    }
+
+    @Override
+    public ArpCacheEntry get(IntIPv4 key) {
+        return super.get(unicastIPv4(key));
+    }
+
+    @Override
+    public boolean containsKey(IntIPv4 key) {
+        return super.containsKey(unicastIPv4(key));
+    }
+
+    @Override
+    public void put(IntIPv4 key, ArpCacheEntry value) {
+        super.put(unicastIPv4(key), value);
+    }
+
+    @Override
+    public ArpCacheEntry removeIfOwner(IntIPv4 key)
+        throws InterruptedException, KeeperException {
+        return super.removeIfOwner(unicastIPv4(key));
+    }
+
+    @Override
+    public synchronized boolean isKeyOwner(IntIPv4 key) {
+        return super.isKeyOwner(unicastIPv4(key));
     }
 
     @Override
