b9f6e020a11fda729f7632f8e829bff13edc39b9
Jacob Mandelson <jlm@midokura.com>
2012-10-10 14:15:50 -0700

    Fix imports.
    Signed-off-by: Jacob Mandelson <jlm@midokura.com>

--- midolmanj/src/main/scala/com/midokura/midolman/DatapathController.scala
+++ midolmanj/src/main/scala/com/midokura/midolman/DatapathController.scala
@@ -5,44 +5,43 @@ package com.midokura.midolman
 
 import akka.actor.{Actor, ActorLogging, ActorRef}
 import akka.dispatch.{Future, Promise}
-import akka.util.duration._
-import akka.util.Timeout
 import akka.pattern.ask
-import monitoring.MonitoringActor
+import akka.util.Timeout
+import akka.util.duration._
 import scala.collection.JavaConversions._
 import scala.collection.{Set => ROSet, mutable, immutable}
 import scala.collection.mutable.ListBuffer
+import java.util.UUID
 
 import com.google.inject.Inject
 
-import java.util.UUID
+import com.midokura.midolman.FlowController.AddWildcardFlow
+import com.midokura.midolman.datapath._
+import com.midokura.midolman.monitoring.MonitoringActor
+import com.midokura.midolman.services.HostIdProviderService
+import com.midokura.midolman.simulation.{Bridge => RCUBridge}
+import com.midokura.midolman.topology._
+import com.midokura.midolman.topology.LocalPortActive
+import com.midokura.midolman.topology.VirtualTopologyActor.{BridgeRequest,
+         PortRequest}
+import com.midokura.midolman.topology.rcu.{Host, PortSet}
+import com.midokura.midonet.cluster.client
+import com.midokura.midonet.cluster.client.{ExteriorPort, TunnelZones}
+import com.midokura.midonet.cluster.data.TunnelZone
 import com.midokura.midonet.cluster.data.zones.{GreTunnelZoneHost,
                                                 GreTunnelZone,
                                                 CapwapTunnelZone,
                                                 CapwapTunnelZoneHost}
-import com.midokura.midonet.cluster.client
-import com.midokura.midonet.cluster.client.{ExteriorPort, TunnelZones}
-import com.midokura.midonet.cluster.data.TunnelZone
-import com.midokura.midolman.services.HostIdProviderService
-import com.midokura.midolman.datapath._
-import com.midokura.midolman.simulation.{Bridge => RCUBridge}
-import topology._
+import com.midokura.netlink.Callback
 import com.midokura.netlink.exceptions.NetlinkException
 import com.midokura.netlink.exceptions.NetlinkException.ErrorCode
 import com.midokura.netlink.protos.OvsDatapathConnection
+import com.midokura.packets.Ethernet
 import com.midokura.sdn.flows.{WildcardFlow, WildcardMatch}
 import com.midokura.sdn.dp.{Flow => KernelFlow, _}
-import com.midokura.sdn.dp.flows.{FlowAction, FlowKeys, FlowActions}
+import com.midokura.sdn.dp.flows.{FlowAction, FlowActions, FlowKeys}
 import com.midokura.sdn.dp.ports._
 import com.midokura.util.functors.Callback0
-import com.midokura.netlink.Callback
-import rcu.Host
-import rcu.PortSet
-import topology.LocalPortActive
-import topology.VirtualTopologyActor.BridgeRequest
-import com.midokura.packets.Ethernet
-import topology.VirtualTopologyActor.PortRequest
-import com.midokura.midolman.FlowController.AddWildcardFlow
 
 
 /**
