8b44d4cb073223d5398134717e75b6a2725a3907
Guillermo Ontanon <guillermo@midokura.jp>
2012-10-17 19:22:11 +0200

    fix: do not create the dp flow if it already existed
    Signed-off-by: Guillermo Ontanon <guillermo@midokura.jp>

--- midolmanj/src/main/scala/com/midokura/midolman/FlowController.scala
+++ midolmanj/src/main/scala/com/midokura/midolman/FlowController.scala
@@ -228,28 +228,30 @@ class FlowController extends Actor with ActorLogging {
                 // Empty action list means DROP. Do nothing.
                 return
             }
-        }
-        // Otherwise, try to create a datapath flow based on an existing
-        // wildcard flow.
-        val dpFlow = flowManager.createDpFlow(packet.getMatch)
-        if (dpFlow != null) {
-            log.debug("A matching wildcard flow returned actions {}",
-                dpFlow.getActions)
-            datapathConnection.flowsCreate(datapath, dpFlow,
-            new ErrorHandlingCallback[Flow] {
-                def onSuccess(data: Flow) {
-                    self ! flowAdded(data)
-                }
+        } else {
+            // Otherwise, try to create a datapath flow based on an existing
+            // wildcard flow.
+            val dpFlow = flowManager.createDpFlow(packet.getMatch)
+            if (dpFlow != null) {
+                log.debug("A matching wildcard flow returned actions {}",
+                    dpFlow.getActions)
+                datapathConnection.flowsCreate(datapath, dpFlow,
+                new ErrorHandlingCallback[Flow] {
+                    def onSuccess(data: Flow) {
+                        self ! flowAdded(data)
+                    }
 
-                def handleError(ex: NetlinkException, timeout: Boolean) {
-                        log.error("Got an exception {} or timeout {} when trying to add flow" +
-                            "with flow match {}", ex, timeout, dpFlow.getMatch)
-                }
-            })
-            actions = dpFlow.getActions
-            // Empty action list means DROP. Do nothing.
-            if (actions.size == 0)
-                return
+                    def handleError(ex: NetlinkException, timeout: Boolean) {
+                            log.error("Got an exception {} or timeout {} when "+
+                                "trying to add flow with flow match {}",
+                                ex, timeout, dpFlow.getMatch)
+                    }
+                })
+                actions = dpFlow.getActions
+                // Empty action list means DROP. Do nothing.
+                if (actions.size == 0)
+                    return
+            }
         }
 
         // If there was a match, execute its actions
