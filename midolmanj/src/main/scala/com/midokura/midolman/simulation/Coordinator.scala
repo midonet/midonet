c51ee05d8cdd33f3d75c359b6a8de592629ce31d
rossella <rossella@midokura.com>
2012-11-06 13:17:22 +0100

    Added missing negation and removed check for arp in packetcontext
    Signed-off-by: Rossella Sblendido <rossella@midokura.com>

--- midolmanj/src/main/scala/com/midokura/midolman/simulation/Coordinator.scala
+++ midolmanj/src/main/scala/com/midokura/midolman/simulation/Coordinator.scala
@@ -225,7 +225,7 @@ class Coordinator(val origMatch: WildcardMatch,
                     dropFlow(temporary = true)
                 } else {
                     pktContext.setInputPort(port.id)
-                    if(numDevicesSimulated == 0 && generatedPacketEgressPort.isDefined) {
+                    if(numDevicesSimulated == 0 && !generatedPacketEgressPort.isDefined) {
                         // add ingressFE only at the beginning of the simulation
                         // and only if it's not a generated packet
                         pktContext.setIngressFE(port.deviceID)
