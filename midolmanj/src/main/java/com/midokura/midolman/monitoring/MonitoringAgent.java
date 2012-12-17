4b969d6a6efcf28d55f0f1c6d9c1770b6b194fec
Guillermo Ontanon <guillermo@midokura.jp>
2012-12-17 05:59:19 -0500

    Monitoring: fixes in the cassandra configuration
     * remove midolman-global cassandra options from the monitoring config
     * make MidolmanConfig extend MonitoringConfig
     * make MonitoringConfig not extend CassandraConfig
     * fix some typos and add all default values to the config file
    
    Signed-off-by: Guillermo Ontanon <guillermo@midokura.jp>

--- midolmanj/src/main/java/com/midokura/midolman/monitoring/MonitoringAgent.java
+++ midolmanj/src/main/java/com/midokura/midolman/monitoring/MonitoringAgent.java
@@ -10,16 +10,15 @@ import static java.lang.String.format;
 
 import akka.actor.ActorRef;
 import com.google.inject.Inject;
-import com.midokura.midolman.monitoring.MonitoringActor;
-import com.midokura.midolman.config.MidolmanConfig;
-import com.midokura.midolman.services.MidolmanActorsService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.midokura.midolman.monitoring.config.MonitoringConfiguration;
+import com.midokura.midolman.config.MidolmanConfig;
+import com.midokura.midolman.monitoring.MonitoringActor;
 import com.midokura.midolman.monitoring.metrics.VMMetricsCollection;
 import com.midokura.midolman.monitoring.metrics.ZookeeperMetricsCollection;
 import com.midokura.midolman.monitoring.store.Store;
+import com.midokura.midolman.services.MidolmanActorsService;
 
 /**
  * This is the main entry point to the monitoring functionality. It's called
@@ -38,9 +37,6 @@ public class MonitoringAgent {
     MidolmanConfig config;
 
     @Inject
-    MonitoringConfiguration configuration;
-
-    @Inject
     VMMetricsCollection vmMetrics;
 
     @Inject
@@ -52,12 +48,11 @@ public class MonitoringAgent {
     MidoReporter reporter;
 
     public void startMonitoringIfEnabled() {
-
-       if (config.getMidolmanEnableMonitoring()) {
+        if (!config.getMidolmanEnableMonitoring())
+            return;
 
         vmMetrics.registerMetrics();
-
-        int zkJmxPort = configuration.getMonitoringZookeeperJMXPort();
+        int zkJmxPort = config.getMonitoringZookeeperJMXPort();
 
         if (zkJmxPort != -1) {
             String zkJmxUrl =
@@ -71,16 +66,13 @@ public class MonitoringAgent {
             store.initialize();
             reporter = new MidoReporter(store, "MidonetMonitoring");
             reporter.start(
-                configuration.getMonitoringCassandraReporterPullTime(),
+                config.getMonitoringCassandraReporterPullTime(),
                 TimeUnit.MILLISECONDS);
         } else {
             log.warn("The metrics publisher to the data store didn't start " +
                          "because the store was not initialized. Most likely " +
                          "the connection to the store failed.");
         }
-
-
-       }
     }
 
     public void stop() {
