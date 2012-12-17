339007b7c69a5452bc171a731a73b6e924a7bdbc
Guillermo Ontanon <guillermo@midokura.jp>
2012-12-17 05:59:19 -0500

    Monitoring: fixes in the cassandra configuration
     * remove midolman-global cassandra options from the monitoring config
     * make MidolmanConfig extend MonitoringConfig
     * make MonitoringConfig not extend CassandraConfig
     * fix some typos and add all default values to the config file
    
    Signed-off-by: Guillermo Ontanon <guillermo@midokura.jp>

--- midolmanj/src/main/java/com/midokura/midolman/monitoring/config/MonitoringConfiguration.java
+++ midolmanj/src/main/java/com/midokura/midolman/monitoring/config/MonitoringConfiguration.java
@@ -5,8 +5,8 @@ package com.midokura.midolman.monitoring.config;
 
 import com.midokura.config.ConfigGroup;
 import com.midokura.config.ConfigInt;
+import com.midokura.config.ConfigBool;
 import com.midokura.config.ConfigString;
-import com.midokura.midolman.config.CassandraConfig;
 
 /**
  * Interface that explains properly all the parameters that we are expecting
@@ -16,34 +16,34 @@ import com.midokura.midolman.config.CassandraConfig;
  *         Date: 5/30/12
  */
 @ConfigGroup(MonitoringConfiguration.GROUP_NAME)
-public interface MonitoringConfiguration extends CassandraConfig {
+public interface MonitoringConfiguration {
 
     public final static String GROUP_NAME = "monitoring";
+
+    /**
+     * @return Whether monitoring is enabled.
+     */
+    @ConfigBool(key = "enable_monitoring", defaultValue = true)
+    boolean getMidolmanEnableMonitoring();
 
     /**
      * @return the cassandra key space we want to use for the monitoring data.
      */
     @ConfigString(key = "cassandra_keyspace",
-                  defaultValue = "midonet_monitoring_keyspace")
+                  defaultValue = "midonet_monitoring")
     String getMonitoringCassandraKeyspace();
 
     /**
      * @return the cassandra column family we want to use for the monitoring data.
      */
     @ConfigString(key = "cassandra_column_family",
-                  defaultValue = "midonet_monitoring_column_family")
+                  defaultValue = "monitoring_data")
     String getMonitoringCassandraColumnFamily();
-
-    /**
-     * @return the cassandra replication factor to use.
-     */
-    @ConfigInt(key = "cassandra_replication_factor", defaultValue = 3)
-    int getMonitoringCassandraReplicationFactor();
 
     /**
      * @return the cassandra expiration timeout (in seconds).
      */
-    @ConfigInt(key = "cassandra_expiration_timeout",
+    @ConfigInt(key = "cassandra_expiration_timeout_minutes",
                defaultValue = 365 * 34 * 60)
     int getMonitoringCassandraExpirationTimeout();
 
