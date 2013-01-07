36e2d7bcd7653e88f04dcde8b11ba3cc3eaaab52
Guillermo Ontanon <guillermo@midokura.jp>
2013-01-07 17:01:43 +0100

    Ethernet.isMcast(): fix NPE
    test for null on the mac address (npe was triggered in unit
    tests only)
    
    Change-Id: I4eafc2f403ef9a4308e70eb00ad9f44cd5ef572b
    Signed-off-by: Guillermo Ontanon <guillermo@midokura.jp>

--- packets/src/main/java/com/midokura/packets/Ethernet.java
+++ packets/src/main/java/com/midokura/packets/Ethernet.java
@@ -292,7 +292,7 @@ public class Ethernet extends BasePacket {
     }
 
     public static boolean isMcast(MAC mac) {
-        return isMcast(mac.getAddress());
+        return (mac == null) ? false : isMcast(mac.getAddress());
     }
 
     private static boolean isMcast(byte[] mac) {
