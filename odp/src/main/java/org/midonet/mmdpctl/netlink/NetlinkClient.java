midonet/midonet

commit e6fabaf3777a7d6eb13a0649da03ad125806bba4

Netlink: move writes to select loop
Motivation: the netlink channel is set to non-blocking because
reads are performed out of the select loop. Writes were not
checking for writeability of the channel, now this check
is done by the select loop.

Implementation: A new BlockingQueue subclass is used to queue the
requests that need to be written, this class (SelectorInputQueue)
wakes up a Selector when an item is added.

The select loop can now take multiple registrations for the same
channel (for different ops obviously), and some of them may be
bound to a BlockingQueue. For these new registrations, the loop
is interested in the ops only when there's data in the queue.

As a consequence the select loop now reconstructs the interest
set before each call to select(), and demultiplexes the ready
ops on return to find the callback associated with each of them.

Like reads, writes are done in batches, the callback writes
as many msgs as possible until either the queue is depleted or
the channel will not take any more. However, we introduce a
maximum number of operations per batch to prevent one operation
from adding too much latency to the other.

Change-Id: I595b736fc7fd3b26e1007da0b722fcf59913579a
Signed-off-by: Guillermo Ontanon <guillermo@midokura.jp>

odp/src/main/java/org/midonet/mmdpctl/netlink/NetlinkClient.java
@@ -61,7 +61,7 @@ public class NetlinkClient {
                     @Override
                     public void handleEvent(SelectionKey key)
                             throws IOException {
-                        ovsConnection.handleEvent(key);
+                        ovsConnection.handleReadEvent(key);
                     }
                 });
 

odp/src/main/java/org/midonet/odp/NetlinkClient.java
@@ -77,7 +77,7 @@ public class NetlinkClient {
                           @Override
                           public void handleEvent(SelectionKey key)
                               throws IOException {
-                              ovsConnection.handleEvent(key);
+                              ovsConnection.handleReadEvent(key);
                           }
                       });
 

midolman/src/main/java/org/midonet/midolman/services/DatapathConnectionService.java
@@ -43,15 +43,26 @@ public class DatapathConnectionService extends AbstractService {
                     @Override
                     public void handleEvent(SelectionKey key)
                         throws IOException {
-                        datapathConnection.handleEvent(key);
+                        datapathConnection.handleReadEvent(key);
                     }
                 });
 
+            selectLoop.registerForInputQueue(
+                    datapathConnection.getSendQueue(),
+                    datapathConnection.getChannel(),
+                    SelectionKey.OP_WRITE,
+                    new SelectListener() {
+                        @Override
+                        public void handleEvent(SelectionKey key)
+                            throws IOException {
+                            datapathConnection.handleWriteEvent(key);
+                        }
+                    });
+
             datapathConnection.initialize().get();
             notifyStarted();
-        } catch (ClosedChannelException e) {
-            notifyFailed(e);
         } catch (Exception e) {
+            log.error("failed to start DatapathConnectionService: {}", e);
             notifyFailed(e);
         }
     }

midonet-util/src/main/java/org/midonet/util/eventloop/SelectLoop.java
@@ -37,10 +37,14 @@ import java.nio.channels.SelectableChannel;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.spi.SelectorProvider;
+import java.util.concurrent.BlockingQueue;
 
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import org.midonet.util.io.SelectorInputQueue;
 
 /**
  * Dirt simple SelectLoop for simple java controller
@@ -58,6 +62,9 @@ public class SelectLoop {
     protected long timeout;
     protected final Object registerLock = new Object();
 
+    private Multimap<SelectableChannel, Registration> registrations =
+        HashMultimap.create();
+
     public SelectLoop() throws IOException {
         dontStop = true;
         selector = SelectorProvider.provider().openSelector();
@@ -73,7 +80,7 @@ public class SelectLoop {
      * @return SelectionKey
      * @throws ClosedChannelException if channel was already closed
      */
-    public SelectionKey register(SelectableChannel ch, int ops,
+    public void register(SelectableChannel ch, int ops,
                                  SelectListener arg)
         throws ClosedChannelException {
         synchronized (registerLock) {
@@ -87,7 +94,7 @@ public class SelectLoop {
             // synchronizing on registerLock and its calling select(),
             // because select() returns immediately if a wakeup() was
             // called while the selector didn't have a select in progress.
-            return ch.register(selector, ops, arg);
+            registrations.put(ch, new Registration(ch, ops, arg, null));
         }
     }
 
@@ -111,7 +118,47 @@ public class SelectLoop {
             // synchronizing on registerLock and its calling select(),
             // because select() returns immediately if a wakeup() was
             // called while the selector didn't have a select in progress.
-            ch.register(selector, ops).cancel();
+            for (Registration reg: registrations.get(ch)) {
+                if (reg.ops == ops) {
+                    registrations.remove(ch, reg);
+                    break;
+                }
+            }
+        }
+    }
+
+    /**
+     * Registers the supplied SelectableChannel with this SelectLoop. The
+     * interest ops will only be listened to when there's data queued in
+     * the given BlockingQueue.
+     *
+     * @param queue the queue
+     * @param ch    the channel
+     * @param ops   interest ops
+     * @param arg   argument that will be returned with the SelectListener
+     */
+    public void registerForInputQueue(SelectorInputQueue<?> queue,
+                                      SelectableChannel ch,
+                                      int ops,
+                                      SelectListener arg) {
+        log.debug("Registering channel bound to input queue");
+        synchronized (registerLock) {
+            selector.wakeup();
+            queue.bind(selector);
+            registrations.put(ch, new Registration(ch, ops, arg, queue));
+        }
+    }
+
+    public void unregisterForInputQueue(SelectorInputQueue<?> queue) {
+        synchronized (registerLock) {
+            selector.wakeup();
+            queue.unbind();
+            for (Registration reg: registrations.values()) {
+                if (reg.queue == queue) {
+                    registrations.remove(reg.ch, reg);
+                    break;
+                }
+            }
         }
     }
 
@@ -128,20 +175,42 @@ public class SelectLoop {
             log.trace("looping");
 
             synchronized (registerLock) {
+                for (SelectableChannel ch: registrations.keySet()) {
+                    int ops = 0;
+                    for (Registration reg: registrations.get(ch)) {
+                        if (reg.queue == null || !reg.queue.isEmpty()) {
+                            ops |= reg.ops;
+                        }
+                    }
+                    if (ops != 0)
+                        ch.register(selector, ops);
+                    else
+                        ch.register(selector, ch.validOps()).cancel();
+                }
             }
+
             nEvents = selector.select(timeout);
             log.trace("got {} events", nEvents);
+
             if (nEvents > 0) {
                 for (SelectionKey sk : selector.selectedKeys()) {
                     if (!sk.isValid())
                         continue;
-
-                    SelectListener callback = (SelectListener) sk.attachment();
-                    callback.handleEvent(sk);
+                    int ops = sk.readyOps();
+                    SelectableChannel ch = sk.channel();
+                    for (Registration reg: registrations.get(ch)) {
+                        if (ops == 0) {
+                            break;
+                        } else if ((reg.ops & ops) != 0) {
+                            reg.listener.handleEvent(sk);
+                            // We report each ready-op once, so after
+                            // dispatching the event we clear it from ops.
+                            ops &= ~reg.ops;
+                        }
+                    }
                 }
                 selector.selectedKeys().clear();
             }
-
         }
     }
 
@@ -163,4 +232,20 @@ public class SelectLoop {
         this.dontStop = false;
         wakeup();
     }
+
+    private class Registration {
+        final SelectableChannel ch;
+        final int ops;
+        final SelectListener listener;
+        final BlockingQueue<?> queue;
+
+        Registration(SelectableChannel ch, int ops, SelectListener arg,
+                     BlockingQueue<?> queue) {
+            this.ch = ch;
+            this.ops = ops;
+            this.listener = arg;
+            this.queue = queue;
+        }
+    }
+
 }

midonet-util/src/main/java/org/midonet/util/io/SelectorInputQueue.java
@@ -1,0 +1,73 @@
+// Copyright 2013 Midokura Inc.
+
+package org.midonet.util.io;
+
+import java.nio.channels.Selector;
+import java.util.Collection;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * A BlockingQueue specialization that can be bound to a Selector. When bound,
+ * the selector will be woken up whenever data is put in the queue. It is meant
+ * to be combined with a selector used to write data to a channel as it arrives
+ * to this queue.
+ */
+public class SelectorInputQueue<E> extends LinkedBlockingQueue<E> {
+    private Selector selector = null;
+
+    public void bind(Selector s) {
+        this.selector = s;
+    }
+
+    public void unbind() {
+        this.selector = null;
+    }
+
+    @Override
+    public boolean add(E e) {
+        if (super.add(e)) {
+            if (selector != null)
+                selector.wakeup();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean offer(E e) {
+        if (super.offer(e)) {
+            if (selector != null)
+                selector.wakeup();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void put(E e) throws InterruptedException {
+        super.put(e);
+        if (selector != null)
+            selector.wakeup();
+    }
+
+    @Override
+    public boolean offer(E e, long timeout, TimeUnit unit) throws InterruptedException {
+        if (super.offer(e, timeout, unit)) {
+            if (selector != null)
+                selector.wakeup();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection c) {
+        if (super.addAll(c)) {
+            if (selector != null)
+                selector.wakeup();
+            return true;
+        }
+        return false;
+    }
+}

netlink/src/main/java/org/midonet/netlink/AbstractNetlinkConnection.java
@@ -26,6 +26,7 @@ import org.midonet.netlink.clib.cLibrary;
 import org.midonet.netlink.exceptions.NetlinkException;
 import org.midonet.netlink.messages.Builder;
 import org.midonet.util.eventloop.Reactor;
+import org.midonet.util.io.SelectorInputQueue;
 import org.midonet.util.throttling.ThrottlingGuard;
 import org.midonet.util.throttling.ThrottlingGuardFactory;
 
@@ -48,6 +49,11 @@ public abstract class AbstractNetlinkConnection {
     private Map<Integer, NetlinkRequest>
         pendingRequests = new ConcurrentHashMap<Integer, NetlinkRequest>();
 
+    // Mostly for testing purposes, this flag tells the netlink connection that
+    // writes should be done on the channel directly, instead of waiting
+    // for a selector to invoke handleWriteEvent()
+    private boolean bypassSendQueue = false;
+
     // Again, mostly for testing purposes. Tweaks the number of IO operations
     // per handle[Write|Read]Event() invokation. Netlink protocol tests will
     // assume one read per call.
@@ -55,6 +61,9 @@ public abstract class AbstractNetlinkConnection {
 
     private NetlinkChannel channel;
     private Reactor reactor;
+
+    private SelectorInputQueue<NetlinkRequest> writeQueue =
+            new SelectorInputQueue<NetlinkRequest>();
 
     public AbstractNetlinkConnection(NetlinkChannel channel, Reactor reactor,
             ThrottlingGuardFactory throttlerFactory) {
@@ -72,12 +81,24 @@ public abstract class AbstractNetlinkConnection {
         return reactor;
     }
 
+    public boolean bypassSendQueue() {
+        return bypassSendQueue;
+    }
+
+    public void bypassSendQueue(final boolean bypass) {
+        this.bypassSendQueue = bypass;
+    }
+
     public void setMaxBatchIoOps(int max) {
         this.maxBatchIoOps = max;
     }
 
     public int getMaxBatchIoOps() {
         return this.maxBatchIoOps;
+    }
+
+    public SelectorInputQueue<NetlinkRequest> getSendQueue() {
+        return writeQueue;
     }
 
     public class RequestBuilder<
@@ -149,61 +170,56 @@ public abstract class AbstractNetlinkConnection {
         final int seq = sequenceGenerator.getAndIncrement();
 
         int totalSize = 20 + payload.remaining();
-        final ByteBuffer request = ByteBuffer.allocate(totalSize + 4);
-        request.order(ByteOrder.nativeOrder());
+        final ByteBuffer buf = ByteBuffer.allocate(totalSize + 4);
+        buf.order(ByteOrder.nativeOrder());
 
-        serializeNetlinkHeader(request, seq, cmdFamily, version, cmd, flags);
+        serializeNetlinkHeader(buf, seq, cmdFamily, version, cmd, flags);
 
         // set the payload
-        request.put(payload);
+        buf.put(payload);
 
         // set the header length
-        request.putInt(0, totalSize);
-        request.flip();
+        buf.putInt(0, totalSize);
+        buf.flip();
 
         final NetlinkRequest netlinkRequest = new NetlinkRequest();
         netlinkRequest.family = cmdFamily;
         netlinkRequest.cmd = cmd;
         netlinkRequest.callback = callback;
+        netlinkRequest.buffers = new ArrayList<ByteBuffer>(1);
+        netlinkRequest.buffers.add(buf);
 
-        getReactor().submit(new Callable<Object>() {
+        // send the request
+        netlinkRequest.timeoutHandler = new Callable<Object>() {
             @Override
             public Object call() throws Exception {
-                pendingRequests.put(seq, netlinkRequest);
-                throttler.tokenIn();
-                // send the request
-                try {
-                    log.debug("Sending message for id {}", seq);
-                    netlinkRequest.timeoutHandler = new Callable<Object>() {
-                        @Override
-                        public Object call() throws Exception {
-                            log.trace("Timeout fired for {}", seq);
-                            NetlinkRequest timedOutRequest =
-                                pendingRequests.remove(seq);
-                            if (timedOutRequest != null)
-                                throttler.tokenOut();
+                log.trace("Timeout fired for {}", seq);
+                NetlinkRequest timedOutRequest =
+                    pendingRequests.remove(seq);
 
-                            if (timedOutRequest != null) {
-                                log.debug("Signalling timeout to the callback: {}", seq);
-                                timedOutRequest.callback.onTimeout();
-                            }
-
-                            return null;
-                        }
-                    };
-                    getReactor().schedule(netlinkRequest.timeoutHandler,
-                                          timeoutMillis, TimeUnit.MILLISECONDS);
-                    channel.write(request);
-                } catch (IOException e) {
-                    netlinkRequest.callback
-                                  .onError(new NetlinkException(
-                                      NetlinkException.ERROR_SENDING_REQUEST,
-                                      e));
+                if (timedOutRequest != null) {
+                    log.debug("Signalling timeout to the callback: {}", seq);
+                    throttler.tokenOut();
+                    timedOutRequest.callback.onTimeout();
                 }
 
                 return null;
             }
-        });
+        };
+        log.trace("Sending message for id {}", seq);
+        pendingRequests.put(seq, netlinkRequest);
+        if (writeQueue.offer(netlinkRequest)) {
+            if (bypassSendQueue())
+                processWriteToChannel();
+            throttler.tokenIn();
+            getReactor().schedule(netlinkRequest.timeoutHandler,
+                                  timeoutMillis, TimeUnit.MILLISECONDS);
+        } else {
+            pendingRequests.remove(seq);
+            netlinkRequest.callback.onError(new NetlinkException(
+                            NetlinkException.ERROR_SENDING_REQUEST,
+                            "Too many pending netlink requests"));
+        }
     }
 
     protected <
@@ -214,7 +230,46 @@ public abstract class AbstractNetlinkConnection {
         return new RequestBuilder<Cmd, Family>(commandFamily, cmd);
     }
 
-    public void handleEvent(final SelectionKey key) throws IOException {
+    public void handleWriteEvent(final SelectionKey key) throws IOException {
+        log.info("Handling write");
+        for (int i = 0; i < maxBatchIoOps; i++) {
+            final int ret = processWriteToChannel();
+            if (ret <= 0) {
+                if (ret < 0) {
+                    log.warn("NETLINK write() error: {}",
+                            cLibrary.lib.strerror(Native.getLastError()));
+                }
+                break;
+            }
+        }
+    }
+
+    private int processWriteToChannel() {
+        final NetlinkRequest request = writeQueue.peek();
+        if (request == null)
+            return 0;
+        if (request.buffers == null || request.buffers.isEmpty())
+            return 0;
+
+        int bytes = 0;
+        try {
+            bytes = channel.write(request.buffers.get(0));
+            if (bytes == 0)
+                return 0;
+        } catch (IOException e) {
+            log.warn("NETLINK write() exception: {}", e);
+            request.callback
+                .onError(new NetlinkException(
+                        NetlinkException.ERROR_SENDING_REQUEST,
+                        e));
+        } finally {
+            request.buffers.clear();
+            writeQueue.poll();
+        }
+        return bytes;
+    }
+
+    public void handleReadEvent(final SelectionKey key) throws IOException {
         try {
             for (int i = 0; i < maxBatchIoOps; i++) {
                 final int ret = processReadFromChannel(key);

odp/src/test/java/org/midonet/odp/protos/AbstractNetlinkProtocolTest.java
@@ -122,7 +122,7 @@ public abstract class AbstractNetlinkProtocolTest<NetlinkConnection extends Abst
 
     protected void fireReply(int amount) throws IOException {
         while ( amount-- > 0 ) {
-            connection.handleEvent(null);
+            connection.handleReadEvent(null);
         }
     }
 

odp/src/test/java/org/midonet/odp/protos/NetlinkConnectionTest.java
@@ -72,6 +72,7 @@ public class NetlinkConnectionTest
         super.setUp(responses);
         connection = new NetlinkConnection(channel, reactor,
             new NoOpThrottlingGuardFactory());
+        connection.bypassSendQueue(true);
         connection.setMaxBatchIoOps(1);
     }
 
@@ -132,7 +133,7 @@ public class NetlinkConnectionTest
         Future<Short> future = connection.getFamilyId("ovs_datapath");
 
         // fire the received message
-        connection.handleEvent(null);
+        connection.handleReadEvent(null);
 
         // validate decoding
         assertThat("The future was completed",

commit b989267e3300ecee615c2006c0e66439fcad9895

mm-dpctl: register channel for writing
This book makes the netlink client used in mm-dpctl register
the channel's input queue for writing to the select loop. Otherwise
writes are put in the queue but never executed.

This bug was introduced by e6fabaf377.

Change-Id: I51fd0a0d0ee4cf13284e948e13c0256c5eb13468
Signed-off-by: Guillermo Ontanon <guillermo@midokura.jp>

odp/src/main/java/org/midonet/mmdpctl/netlink/NetlinkClient.java
@@ -65,6 +65,18 @@ public class NetlinkClient {
                     }
                 });
 
+        loop.registerForInputQueue(
+            ovsConnection.getSendQueue(),
+            ovsConnection.getChannel(),
+            SelectionKey.OP_WRITE,
+            new SelectListener() {
+                @Override
+                public void handleEvent(SelectionKey key)
+                    throws IOException {
+                        ovsConnection.handleWriteEvent(key);
+                    }
+            });
+
         Thread loopThread = new Thread(new Runnable() {
             @Override
             public void run() {