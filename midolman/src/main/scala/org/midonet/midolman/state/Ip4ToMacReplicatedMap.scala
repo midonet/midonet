1871406cb431bf98f1df5870806321250dd96888
Dave Cahill <dcahill@midokura.com>
2013-07-04 07:19:44 +0000

    Ip4ToMacReplicatedMap: extract functions.
    Change-Id: I3dbecfe609aba29d66f7d6425434ac91e36e92d2

--- midolman/src/main/scala/org/midonet/midolman/state/Ip4ToMacReplicatedMap.scala
+++ midolman/src/main/scala/org/midonet/midolman/state/Ip4ToMacReplicatedMap.scala
@@ -11,7 +11,9 @@ import org.midonet.packets.IPv4Addr
 import org.midonet.packets.MAC
 import collection.JavaConversions._
 
-class Ip4ToMacReplicatedMap(dir: Directory) extends ReplicatedMap[IPv4Addr, MAC](dir) {
+class Ip4ToMacReplicatedMap(dir: Directory)
+    extends ReplicatedMap[IPv4Addr, MAC](dir)
+{
     protected def encodeKey(key: IPv4Addr): String = key.toString
     protected def decodeKey(str: String): IPv4Addr = IPv4Addr.fromString(str)
     protected def encodeValue(value: MAC): String = value.toString
@@ -19,68 +21,35 @@ class Ip4ToMacReplicatedMap(dir: Directory) extends ReplicatedMap[IPv4Addr, MAC]
 }
 
 object Ip4ToMacReplicatedMap {
-    def getAsMap(dir: Directory): Map[IPv4Addr, MAC] = {
-        try {
-            val paths: Iterable[java.lang.String] = dir.getChildren("/", null)
-            val m: Map[IPv4Addr, MAC] = new HashMap[IPv4Addr, MAC]
-            for (path <- paths) {
-                val parts: Array[String] = ReplicatedMap.getKeyValueVersion(path)
-                m.put(IPv4Addr.fromString(parts(0)), MAC.fromString(parts(1)))
-            }
-            return m
+
+    def getAsMap(dir: Directory): Map[IPv4Addr, MAC] = convertException {
+        val paths: Iterable[java.lang.String] = dir.getChildren("/", null)
+        val m: Map[IPv4Addr, MAC] = new HashMap[IPv4Addr, MAC]
+        for (path <- paths) {
+            val parts: Array[String] =
+                ReplicatedMap.getKeyValueVersion(path)
+            m.put(IPv4Addr.fromString(parts(0)), MAC.fromString(parts(1)))
         }
-        catch {
-            case e: KeeperException => {
-                throw new StateAccessException(e)
-            }
-            case e: InterruptedException => {
-                throw new StateAccessException(e)
-            }
-        }
+        m
     }
 
-    def hasPersistentEntry(dir: Directory, key: IPv4Addr, value: MAC): Boolean = {
-        val path: String = ReplicatedMap.encodeFullPath(key.toString, value.toString, 1)
-        try {
-            return dir.has(path)
-        }
-        catch {
-            case e: KeeperException => {
-                throw new StateAccessException(e)
-            }
-            case e: InterruptedException => {
-                throw new StateAccessException(e)
-            }
-        }
-    }
+    def hasPersistentEntry(dir: Directory, key: IPv4Addr, value: MAC): Boolean
+        = convertException(dir.has(encodePersistentPath(key, value)))
 
-    def addPersistentEntry(dir: Directory, key: IPv4Addr, value: MAC) {
-        val path: String = ReplicatedMap.encodeFullPath(key.toString, value.toString, 1)
-        try {
-            dir.add(path, null, CreateMode.PERSISTENT)
-        }
-        catch {
-            case e: KeeperException => {
-                throw new StateAccessException(e)
-            }
-            case e: InterruptedException => {
-                throw new StateAccessException(e)
-            }
-        }
-    }
+    def addPersistentEntry(dir: Directory, key: IPv4Addr, value: MAC)
+        = convertException(dir.add(
+            encodePersistentPath(key, value), null, CreateMode.PERSISTENT))
 
-    def deletePersistentEntry(dir: Directory, key: IPv4Addr, value: MAC) {
-        val path: String = ReplicatedMap.encodeFullPath(key.toString, value.toString, 1)
-        try {
-            dir.delete(path)
-        }
-        catch {
-            case e: KeeperException => {
-                throw new StateAccessException(e)
-            }
-            case e: InterruptedException => {
-                throw new StateAccessException(e)
-            }
+    def deletePersistentEntry(dir: Directory, key: IPv4Addr, value: MAC)
+        = convertException(dir.delete(encodePersistentPath(key, value)))
+
+    def encodePersistentPath(k :IPv4Addr, v :MAC) =
+        ReplicatedMap.encodeFullPath(k.toString, v.toString, 1)
+
+    def convertException[T](f :T) :T = {
+        try { f } catch {
+            case e: KeeperException      => throw new StateAccessException(e)
+            case e: InterruptedException => throw new StateAccessException(e)
         }
     }
 }
