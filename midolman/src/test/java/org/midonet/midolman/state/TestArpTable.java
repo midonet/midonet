39277d2fd82cddca081eb0707d49b12cac2d238f
Dave Cahill <dcahill@midokura.com>
2013-07-04 03:00:02 +0000

    TestArpTable: Added basic test
    - Test that encoding in zookeeper stays the same
    - Remove unused imports in TestIp4ToMacReplicatedMap
    
    Change-Id: I8c65b1c5c65472aa4f759f614b1750bbfb8586c5

--- midolman/src/test/java/org/midonet/midolman/state/TestArpTable.java
+++ midolman/src/test/java/org/midonet/midolman/state/TestArpTable.java
@@ -10,34 +10,35 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
 
-public class TestIp4ToMacReplicatedMap {
+public class TestArpTable {
 
-    Ip4ToMacReplicatedMap map;
+    ArpTable map;
     MockDirectory dir;
 
     final IPv4Addr ip = IPv4Addr.fromString("1.2.3.4");
     final byte[] macBytes = { 0, 1, 2, 3, 4, 5 };
     final MAC mac = new MAC(macBytes);
+    final ArpCacheEntry cacheEntry = new ArpCacheEntry(mac,10,100,1000);
 
     @Before
     public void setUp() {
         dir = new MockDirectory();
-        map = new Ip4ToMacReplicatedMap(dir);
+        map = new ArpTable(dir);
         map.start();
     }
 
     @Test
     public void testContainsEntryAfterPut() throws Exception {
-        map.put(ip, mac);
-        assertEquals(mac, map.get(ip));
+        map.put(ip, cacheEntry);
+        assertEquals(cacheEntry, map.get(ip));
     }
 
     @Test
     public void testSerialization() throws Exception {
         assertEquals("1.2.3.4", map.encodeKey(ip));
-        assertEquals("00:01:02:03:04:05", map.encodeValue(mac));
+        assertEquals("00:01:02:03:04:05;10;100;1000", map.encodeValue(cacheEntry));
 
         assertEquals(ip, map.decodeKey("1.2.3.4"));
-        assertEquals(mac, map.decodeValue("00:01:02:03:04:05"));
+        assertEquals(cacheEntry, map.decodeValue("00:01:02:03:04:05;10;100;1000"));
     }
 }
