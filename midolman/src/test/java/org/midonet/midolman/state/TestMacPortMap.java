7f27a64d8dbc3e57ec50d22d656fbcb168b40ab1
Dave Cahill <dcahill@midokura.com>
2013-07-05 15:10:42 +0900

    MacPortMap: Fix #689 by allowing deletes on non-persistent entries
    - Convert MacPortMap to Scala
    - Expanded unit tests, refactored MacPortMap slightly
    
    Change-Id: Ieef2ca93b8da8f03e7607c4ca06bb5b64e1c7ab9

--- midolman/src/test/java/org/midonet/midolman/state/TestMacPortMap.java
+++ midolman/src/test/java/org/midonet/midolman/state/TestMacPortMap.java
@@ -19,6 +19,7 @@ public class TestMacPortMap {
     final byte[] macBytes = { 0, 1, 2, 3, 4, 5 };
     final MAC mac = new MAC(macBytes);
     final UUID port = UUID.fromString("c1fbd793-1ce9-42a1-86dc-65bbcaa945c0");
+    final UUID port2 = UUID.fromString("e3256890-e520-11e2-a28f-0800200c9a66");
 
     @Before
     public void setUp() {
@@ -30,6 +31,27 @@ public class TestMacPortMap {
     @Test
     public void testContainsEntryAfterPut() throws Exception {
         map.put(mac, port);
+        assertEquals(port, map.get(mac));
+
+        // Add learned entry
+        map.put(mac, port);
+        assertEquals(port, map.get(mac));
+
+        // Delete learned (non-persistent) entry
+        MacPortMap.deleteEntry(dir, mac, port);
+        assertEquals(null, map.get(mac));
+
+        // Deleting twice is OK
+        MacPortMap.deleteEntry(dir, mac, port);
+        assertEquals(null, map.get(mac));
+
+        // Re-add learned entry
+        map.put(mac, port);
+        assertEquals(port, map.get(mac));
+
+        // Attempting to delete existing ip->non existentmac causes no errors,
+        // changes no state
+        MacPortMap.deleteEntry(dir, mac, port2);
         assertEquals(port, map.get(mac));
     }
 
