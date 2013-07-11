7ca65094455ab4cad49d842589022f4007f5fcd0
Galo Navarro <galo@midokura.com>
2013-07-11 17:40:19 +0200

    Do callbacks if if flow skipped or duplicate
    If upon flow installation we see a Netlink error reporting that "file
    exists while adding flow", or if a flow installation is skipped because
    it's obsolete, we still need to perform the callbacks. This was
    generating inconsistent flow counts in the mac-learning table which in
    turn prevented the right invalidations from being fired.
    
    Issue #690
    Refs #684
    
    Change-Id: Ia4f7fb0e22db86d41da6e073b194ac090cadddce
    Signed-off-by: Galo Navarro <galo@midokura.com>
    
    Conflicts:
    	midolman/src/main/scala/org/midonet/midolman/PacketWorkflow.scala

--- midolman/src/main/scala/org/midonet/midolman/PacketWorkflow.scala
+++ midolman/src/main/scala/org/midonet/midolman/PacketWorkflow.scala
@@ -9,7 +9,7 @@ import scala.collection.{Set => ROSet}
 import scala.compat.Platform
 import akka.actor._
 import akka.dispatch.{ExecutionContext, Promise, Future}
-import akka.event.{Logging, LoggingAdapter, LoggingReceive}
+import akka.event.{Logging, LoggingAdapter}
 import akka.pattern.ask
 import akka.util.Timeout
 import java.lang.{Integer => JInteger}
@@ -41,7 +41,8 @@ import org.midonet.odp.Packet.Reason.FlowActionUserspace
 import org.midonet.util.functors.Callback0
 import org.midonet.util.throttling.ThrottlingGuard
 import org.midonet.sdn.flows.{WildcardFlow, WildcardMatch}
-import com.yammer.metrics.core.{Clock, Counter}
+import com.yammer.metrics.core.Clock
+import annotation.tailrec
 
 object PacketWorkflow {
     case class PacketIn(wMatch: WildcardMatch,
@@ -212,6 +213,7 @@ class PacketWorkflow(
                     log.info("File exists while adding flow for {}", cookieStr)
                     DeduplicationActor.getRef() !
                         ApplyFlow(flow.getActions, Some(cookie))
+                    runCallbacks(removalCallbacks.toArray)
                     promise.success(true)
                 } else {
                     // NOTE(pino) - it'd be more correct to execute the
@@ -226,7 +228,15 @@ class PacketWorkflow(
                     promise.failure(ex)
                 }
             }
+    }
+
+    @tailrec
+    private def runCallbacks(callbacks: Array[Callback0], i: Int = 0) {
+        if (callbacks != null && callbacks.length > i) {
+            callbacks(i).call()
+            runCallbacks(callbacks, i+1)
         }
+    }
 
     private def addTranslatedFlow(wildFlow: WildcardFlow,
                                   tags: ROSet[Any] = Set.empty,
