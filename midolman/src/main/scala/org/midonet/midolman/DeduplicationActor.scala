d397ff29489188c392a4f095569b714bf7246709
Galo Navarro <galo@midokura.com>
2013-07-12 18:30:57 +0200

    Make Deduplication actor actually deduplicate
    It was checking pressence in the match-to-cookie map, but never actually
    putting new entries in there. So all matches were "new" when in reality
    there could be cookies already in the system for equal matches.
    
    Issue #690
    
    Change-Id: I51ebd4a212dab3fcc25f31dba784acd6b5d489c3
    Signed-off-by: Galo Navarro <galo@midokura.com>

--- midolman/src/main/scala/org/midonet/midolman/DeduplicationActor.scala
+++ midolman/src/main/scala/org/midonet/midolman/DeduplicationActor.scala
@@ -178,11 +178,15 @@ class DeduplicationActor extends Actor with ActorLogWithoutPath with
                 log.info("Datapath hook installed")
             }
 
-        case HandlePacket(packet) =>
+        case HandlePacket(packet) => {
+            log.debug("Handling packet with match {}", packet.getMatch)
             dpMatchToCookie.get(packet.getMatch) match {
             case None =>
                 val cookie: Int = idGenerator.getAndIncrement
+                log.debug("No match, generate new cookie: {}", packet.getMatch)
 
+
+                dpMatchToCookie.put(packet.getMatch, cookie)
                 cookieToPendedPackets.addBinding(cookie, packet)
                 // If there is no match on the cookie, create an object to
                 // handle the packet.
@@ -193,19 +197,22 @@ class DeduplicationActor extends Actor with ActorLogWithoutPath with
                         metrics, tracedConditions)(this.context.dispatcher,
                         this.context.system, this.context)
 
-                log.debug("Created new {} packet handler.", "PacketWorkflow-" + cookie)
+                    log.debug("Created new {} packet handler.",
+                              "PacketWorkflow-" + cookie)
                 context.dispatcher.execute{new Runnable {
                         override def run() { packetWorkflow.start() }
                     }
                 }
 
             case Some(cookie: Int) =>
-                log.debug("A matching packet with cookie {} is already being handled ", cookie)
+                    log.debug("A matching packet with cookie {} is already " +
+                              "being handled ", cookie)
                 // Simulation in progress. Just pend the packet.
                 throttler.tokenOut()
                 cookieToPendedPackets.addBinding(cookie, packet)
                 metrics.pendedPackets.inc()
             }
+        }
 
         case ApplyFlow(actions, cookieOpt) => cookieOpt foreach { cookie =>
             cookieToPendedPackets.remove(cookie) foreach { pendedPackets =>
