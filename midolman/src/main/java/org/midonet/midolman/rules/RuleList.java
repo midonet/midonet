065ba6736fc318cbfef905d57b7ab7d0455cbe70
Dave Cahill <dcahill@midokura.com>
2013-07-16 14:59:44 +0900

    Add RuleList DTO and serialization tests
    - This lays the foundation for refactoring Rule ordering in
      order to fix several bugs
    
    Change-Id: I0ac21a0f0b447d34c3469ca44760badfc77c0484

---
+++ midolman/src/main/java/org/midonet/midolman/rules/RuleList.java
@@ -0,0 +1,54 @@
+// Copyright (c) 2013 Midokura Europe SARL, All Rights Reserved.
+
+package org.midonet.midolman.rules;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class RuleList {
+
+    public List<UUID> ruleList;
+
+    // Default constructor for the Jackson deserialization.
+    public RuleList() {
+        super();
+        this.ruleList = new ArrayList<UUID>();
+    }
+
+    public RuleList(List<UUID> ruleList_) {
+        this.ruleList = ruleList_;
+    }
+
+    /* Custom accessors for Jackson serialization with more readable IPs. */
+
+    public List<UUID> getRuleList() {
+        return this.ruleList;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other)
+            return true;
+        if (!(other instanceof RuleList))
+            return false;
+
+        RuleList rl = (RuleList) other;
+        return rl.equals(this.ruleList);
+    }
+
+    @Override
+    public int hashCode() {
+        return ruleList.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return ruleList.toString();
+    }
+
+    public RuleList fromString() {
+        return new RuleList();
+    }
+
+}
