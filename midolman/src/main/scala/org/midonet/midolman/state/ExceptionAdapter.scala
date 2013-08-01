dc1843d18d300a2ba05aa7d704f135a358346733
Hugo Benichi <hugo@midokura.com>
2013-08-01 11:20:25 +0900

    risky block of code was evaluated before try catch
    Some method which was trying to wrap a block of code in a try catch
    statement had a bug in it because the block of code was passed as a
    call-by-value argument and evaluated at the method invocation. Fixed
    by changing to call-by-name semantics. Found while doing code reading
    with Brandon.
    
    Change-Id: I16e0a76241dc43c020a5edd22d785b4904ae3999
    Signed-off-by: hugobenichi <hugo@midokura.com>

---
+++ midolman/src/main/scala/org/midonet/midolman/state/ExceptionAdapter.scala
@@ -0,0 +1,25 @@
+/******************************************************************************
+ *                                                                            *
+ *      Copyright (c) 2013 Midokura Europe SARL, All Rights Reserved.         *
+ *                                                                            *
+ ******************************************************************************/
+
+package org.midonet.midolman.state
+
+import org.apache.zookeeper.KeeperException
+
+object ZKExceptions {
+
+    /** Wrap a block of code with lazy "call by name" semantics in a
+     *  try catch block which adapts ZK exceptions to Midolman exceptions.
+     *
+     *  @param  block of code which can potentially throw KeeperException or
+     *          InterruptedException.
+     *  @return the return value of the block of code passed as argument.
+     */
+    def adapt[T](f: => T): T = try { f } catch {
+        case e: KeeperException      => throw new StateAccessException(e)
+        case e: InterruptedException => throw new StateAccessException(e)
+    }
+
+}
