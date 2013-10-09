f6559a804e4a68cc07f4f592fada1234155cc94a
Guillermo Ontanon <guillermo@midokura.jp>
2013-10-09 16:00:01 -0700

    MidolanActorsService: prepare for mock version
    Move most functionality to an abstract GuiceActorService class,
    and leave to subclasses the decision of what actors to launch only.
    
    This prepares the code for a mock actors service needed in the new
    unit tests.
    
    Change-Id: Ie00c7dc7439c81f5cdf5374a98786b34e9b5b4fe
    Signed-off-by: Guillermo Ontanon <guillermo@midokura.jp>

---
+++ midolman/src/main/scala/org/midonet/midolman/services/MidolmanActorsServiceImpl.scala
@@ -0,0 +1,58 @@
+/*
+* Copyright 2012 Midokura Europe SARL
+*/
+package org.midonet.midolman.services
+
+
+import com.google.inject.Inject
+import com.google.inject.Injector
+import org.slf4j.LoggerFactory
+
+import org.midonet.midolman.DatapathController
+import org.midonet.midolman.DeduplicationActor
+import org.midonet.midolman.FlowController
+import org.midonet.midolman.NetlinkCallbackDispatcher
+import org.midonet.midolman.config.MidolmanConfig
+import org.midonet.midolman.routingprotocols.RoutingManagerActor
+import org.midonet.midolman.topology.VirtualToPhysicalMapper
+import org.midonet.midolman.topology.VirtualTopologyActor
+import org.midonet.midolman.monitoring.MonitoringActor
+
+/**
+ * Midolman actors coordinator internal service.
+ * <p/>
+ * It can start the actor system, spawn the initial top level actors and kill
+ * them when it's time to shutdown.
+ */
+class MidolmanActorsServiceImpl extends MidolmanActorsService {
+
+    private val log = LoggerFactory.getLogger(classOf[MidolmanActorsServiceImpl])
+
+    @Inject
+    override val injector: Injector = null
+    @Inject
+    val config: MidolmanConfig = null
+
+    override protected def actorSpecs = {
+        val specs = List(
+            (propsFor(classOf[VirtualTopologyActor]),      VirtualTopologyActor.Name),
+            (propsFor(classOf[VirtualToPhysicalMapper]).
+                withDispatcher("actors.stash-dispatcher"), VirtualToPhysicalMapper.Name),
+            (propsFor(classOf[DatapathController]),        DatapathController.Name),
+            (propsFor(classOf[FlowController]),            FlowController.Name),
+            (propsFor(classOf[RoutingManagerActor]),       RoutingManagerActor.Name),
+            (propsFor(classOf[DeduplicationActor]),        DeduplicationActor.Name),
+            (propsFor(classOf[NetlinkCallbackDispatcher]), NetlinkCallbackDispatcher.Name))
+
+        if (config.getMidolmanEnableMonitoring)
+            (propsFor(classOf[MonitoringActor]), MonitoringActor.Name) :: specs
+        else
+            specs
+    }
+
+    override def initProcessing() {
+        log.debug("Sending Initialization message to datapath controller.")
+        DatapathController.getRef(system) ! DatapathController.initializeMsg
+    }
+
+}
