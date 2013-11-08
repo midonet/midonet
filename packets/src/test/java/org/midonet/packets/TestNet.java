790003e4f0948de85e1274f5623770399759dcbe
Hugo Benichi <hugo@midokura.com>
2013-11-08 16:53:15 +0900

    Removing unused conversion functions in Net.java
    Change-Id: Ia1a5790b2bb120009fe2e4c962bf877ce045d14b
    Signed-off-by: hugobenichi <hugo@midokura.com>

--- packets/src/test/java/org/midonet/packets/TestNet.java
+++ packets/src/test/java/org/midonet/packets/TestNet.java
@@ -10,15 +10,6 @@ import java.net.UnknownHostException;
 public class TestNet {
 
     @Test
-    public void testConvertInetAddressToInt() throws UnknownHostException {
-        Assert.assertEquals(0xc0a801f2, 
-                Net.convertInetAddressToInt(InetAddress.getByAddress(
-                        new byte[] { (byte)0xc0, (byte)0xa8,
-                                     (byte)0x01, (byte)0xf2 }
-        )));
-    }
-
-    @Test
     public void testConvertIntAddressToString() {
         Assert.assertEquals("192.168.1.2",
                 Net.convertIntAddressToString(-1062731518));
