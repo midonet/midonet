bab64d61d86a4ae5e9f26dc44890ebeb05292fb8
Hugo Benichi <hugo@midokura.com>
2013-11-11 18:45:33 +0900

    IPv4Addr int / string / byte[] conversions
    + unit tests
    
    Change-Id: Ie2d6174eb4587851ccb1f0b8f5f0d56a4c91bfec
    Signed-off-by: hugobenichi <hugo@midokura.com>

--- packets/src/main/scala/org/midonet/packets/IPv4Addr.scala
+++ packets/src/main/scala/org/midonet/packets/IPv4Addr.scala
@@ -21,7 +21,7 @@ class IPv4Addr(val addr: Int) extends IPAddr with Ordered[IPv4Addr] {
     override def toUrlString = toString()
 
     @JsonValue
-    override def toString = IPv4Addr.intToIpStr(addr)
+    override def toString = IPv4Addr.intToString(addr)
 
     // See "Programming in Scala" sec. 30.4
     override def equals(o: Any): Boolean = {
@@ -49,7 +49,7 @@ class IPv4Addr(val addr: Int) extends IPAddr with Ordered[IPv4Addr] {
     def compare(that: IPv4Addr): Int = this.addr.compare(that.toInt)
     def toIntIPv4 = new IntIPv4(addr)
     def toInt = addr
-    override def toBytes: Array[Byte] = IPv4.toIPv4AddressBytes(addr)
+    override def toBytes: Array[Byte] = IPv4Addr.intToBytes(addr)
 
 }
 
@@ -59,41 +59,101 @@ object IPv4Addr {
 
     def random = IPv4Addr(r.nextInt)
 
-    def apply(s: String): IPv4Addr = fromString(s)
+    def apply(s: String) = fromString(s)
 
-    def apply(i: Int): IPv4Addr = fromInt(i)
+    def apply(i: Int) = fromInt(i)
 
-    def apply(bytes: Array[Byte]): IPv4Addr = fromBytes(bytes)
+    def apply(bytes: Array[Byte]) = fromBytes(bytes)
 
     @JsonCreator
-    def fromString(s: String): IPv4Addr =
-        new IPv4Addr(Net.convertStringAddressToInt(s))
+    def fromString(s: String) = new IPv4Addr(stringToInt(s))
 
-    def fromInt(i: Int): IPv4Addr =
-        new IPv4Addr(i)
+    def fromInt(i: Int) = new IPv4Addr(i)
 
-    def fromIntIPv4(ii: IntIPv4): IPv4Addr =
-        new IPv4Addr(ii.addressAsInt)
+    def fromIntIPv4(ii: IntIPv4) = new IPv4Addr(ii.addressAsInt)
 
-    def fromBytes(addr: Array[Byte]) = IPv4Addr(bytesToInt(addr))
+    def fromBytes(addr: Array[Byte]) = new IPv4Addr(bytesToInt(addr))
 
     def fromIPv4(ipv4: IPv4Addr) = ipv4
 
-    def intToIpStr(addr: Int): String =
+    def intToString(addr: Int) =
         "%d.%d.%d.%d" format ((addr >> 24) & 0xff,
                               (addr >> 16) & 0xff,
                               (addr >> 8) & 0xff,
                               (addr >> 0) & 0xff)
 
-    def bytesToInt(addr: Array[Byte]) = {
-        if (addr.length != 4)
-            throw new IllegalArgumentException
+    def intToBytes(addr: Int) = {
+        val addrBytes = new Array[Byte](4)
+        addrBytes(0) = ((addr >> 24) & 0xff).toByte
+        addrBytes(1) = ((addr >> 16) & 0xff).toByte
+        addrBytes(2) = ((addr >> 8) & 0xff).toByte
+        addrBytes(3) = ((addr >> 0) & 0xff).toByte
+        addrBytes
+    }
 
-        ((addr(0) << 24) & 0xFF000000) |
-            ((addr(1) << 16) & 0x00FF0000) |
-            ((addr(2) <<  8) & 0x0000FF00) |
-            ((addr(3) <<  0) & 0x000000FF)
+    def illegalIPv4String(str: String) = new IllegalArgumentException(
+        "IPv4 string must be 4 numbers between 0 and 255 joined with 3 '.' " +
+            "but was " + str)
+
+    def stringToInt(s: String) = {
+        if (s == null)
+            throw illegalIPv4String(s)
+        val bytes = s split '.'
+        if (bytes.length != 4)
+            throw illegalIPv4String(s)
+        var addr = 0
+        try {
+            bytes.foreach { byteStr =>
+                val byteVal = byteStr.toInt
+                if ((byteVal | 0xff) != 0xff)
+                    throw illegalIPv4String(s)
+                addr = (addr << 8) + byteVal
+            }
+        } catch {
+            case _: NumberFormatException => throw illegalIPv4String(s)
+        }
+        addr
+    }
+
+    def stringToBytes(s: String) = {
+        if (s == null)
+            throw illegalIPv4String(s)
+        val bytes = s split '.'
+        if (bytes.length != 4)
+            throw illegalIPv4String(s)
+        val addrBytes = new Array[Byte](4)
+        var index = 0
+        try {
+            bytes.foreach { byteStr =>
+                val byteVal = byteStr.toInt
+                if ((byteVal | 0xff) != 0xff)
+                    throw illegalIPv4String(s)
+                addrBytes(index) = byteVal.toByte
+                index += 1
+            }
+        } catch {
+            case _: NumberFormatException => throw illegalIPv4String(s)
+        }
+        addrBytes
+    }
+
+    val illegalIPv4Bytes = new IllegalArgumentException(
+        "byte array representing an IPv4 String must have length 4 exactly")
+
+    def bytesToInt(addr: Array[Byte]) = {
+        if (addr == null || addr.length != 4)
+            throw illegalIPv4Bytes
+        ((addr(0) << 24) & 0xff000000) |
+            ((addr(1) << 16) & 0x00ff0000) |
+            ((addr(2) <<  8) & 0x0000ff00) |
+            ((addr(3) <<  0) & 0x000000ff)
+    }
+
+    def bytesToString(addr: Array[Byte]) = {
+        if (addr == null || addr.length != 4)
+            throw illegalIPv4Bytes
+        "%d.%d.%d.%d" format (
+            addr(0) & 0xff, addr(1) & 0xff, addr(2) & 0xff, addr(3) & 0xff)
     }
 
 }
-
