6f028fda7bd45ba0ca75d2377d01e4db0ccd2b5f
Guillermo Ontanon <guillermo@midokura.jp>
2013-11-18 14:47:36 +0100

    DDA: ability to 'pause' simulations
    The DDA accepts messages that request that a simulation be 'paused'
    until a certain Promise is completed. 'Pausing' a simulation means
    temporarily releasing its simulation throttling token, allowing others
    to take its place. A ring buffer limits the number of simultaneously
    paused simulations, when the ring buffer is found to be full, the oldest
    waiting promise is timed out.
    
    Refs MN-867
    
    Change-Id: I5ca1a48a63baee71e4a8673e6d76a866f9e91e57
    Signed-off-by: Guillermo Ontanon <guillermo@midokura.jp>

---
+++ midolman/src/test/scala/org/midonet/midolman/SuspendedPacketQueueActorTest.scala
@@ -0,0 +1,101 @@
+package org.midonet.midolman
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import akka.testkit.TestActorRef
+import org.midonet.midolman.SuspendedPacketQueue.{_CleanCompletedPromises, SuspendOnPromise}
+import akka.dispatch.Promise
+import org.midonet.util.throttling.ThrottlingGuard
+import org.slf4j.LoggerFactory
+import org.scalatest.concurrent.Eventually._
+import java.util.concurrent.TimeoutException
+
+@RunWith(classOf[JUnitRunner])
+class SuspendedPacketQueueActorTest extends SingleActorTestCase {
+
+    var spq: TestActorRef[TestableSPQA] = null
+    var throttler: ThrottlingGuard = null
+    var log = LoggerFactory.getLogger(classOf[SuspendedPacketQueueActorTest])
+    val simSlots = 6
+
+    override def beforeTest() {
+        spq = actorFor(() => new TestableSPQA())
+        throttler = spq.underlyingActor.throttler
+        while (throttler.numTokens() > 0)
+            throttler.tokenOut()
+        throttler.numTokens() should be === 0
+    }
+
+    feature("SuspendedPacketQueueActor manages tokens correctly") {
+        scenario("a promise is suspended and released when complete") {
+            val cookie = Option(9)
+            val promise = Promise[Int]()(actorSystem.dispatcher)
+
+            throttler.tokenIn()
+            throttler.numTokens() should be === 1
+            when("the message with an incomplete promise is sent")
+            spq ! SuspendOnPromise(cookie, promise)
+            eventually {
+                throttler.numTokens() should be === 0
+            }
+            and("the ring should contain the promise")
+            spq.underlyingActor.peekRing should be === Some((cookie, promise))
+            then("the promise succeeds")
+            promise.success(1)
+            and("the actor should release the token")
+            throttler.numTokens() should be === 1
+            spq ! _CleanCompletedPromises
+            and("the ring should be empty")
+            eventually {
+                spq.underlyingActor.isRingEmpty should be === true
+            }
+        }
+    }
+
+    feature("Too many suspended promises generate timeouts") {
+        scenario("lots of promises are suspended") {
+
+            throttler.numTokens() should be === 0
+
+            var promises = List[Promise[Int]]()
+            for (c <- 1 to simSlots-1) {
+                val promise = Promise[Int]()(actorSystem.dispatcher)
+                promises = promise :: promises
+                throttler.tokenIn()
+                throttler.numTokens() should be === 1
+                spq ! SuspendOnPromise(Some(c), promise)
+                eventually {
+                    throttler.numTokens() should be === 0
+                }
+            }
+            var failure: Throwable = null
+            promises.last.onFailure { case e => failure = e }
+            when ("a new promise is suspended")
+            val promise = Promise[Int]()(actorSystem.dispatcher)
+            throttler.tokenIn()
+            throttler.numTokens() should be === 1
+            spq ! SuspendOnPromise(Some(simSlots), promise)
+            eventually {
+                failure should not be null
+                failure.getClass should be === classOf[TimeoutException]
+            }
+            promises.last.isCompleted should be === true
+            and ("the failed exception will have recovered a token")
+            throttler.numTokens() should be === 1
+
+        }
+    }
+
+    class TestableSPQA extends DeduplicationActor {
+
+        override def SUSPENDED_SIM_SLOTS = simSlots
+
+        override def preStart() {
+            // cancel the scheduled cleanups
+        }
+
+        def isRingEmpty = ring.isEmpty
+        def peekRing = ring.peek
+    }
+}
+
