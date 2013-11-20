ed3967b9c5168bf7039a707f618345a179cf37f7
Hugo Benichi <hugo@midokura.com>
2013-11-20 17:54:48 +0900

    Unit test for VPM #portForget #portRemove
    covering all branches.
    
    Ref MN-493
    Signed-off-by: hugobenichi <hguo@midokura.com>
    
    Change-Id: I1f277d1fb88861c3e41382850fd12388d30bb91d

--- midolman/src/test/scala/org/midonet/midolman/actors/VirtualPortManagerTest.scala
+++ midolman/src/test/scala/org/midonet/midolman/actors/VirtualPortManagerTest.scala
@@ -51,7 +51,7 @@ class VirtualPortManagerTest extends Suite with FeatureSpec with ShouldMatchers 
     val ids = for (_ <- 1 to 3) yield UUID.randomUUID
     val itfs = List("eth0", "eth1", "eth2")
     val itfsInfo = itfs.map{ new InterfaceDescription(_) }
-    val portNo = List(1,2,3)
+    val portNo = List[java.lang.Integer](1,2,3)
     val dpPorts = itfs.zip(portNo).map {
         case (itf, no) => new NetDevPort(itf).setPortNo(no)
     }
@@ -133,4 +133,127 @@ class VirtualPortManagerTest extends Suite with FeatureSpec with ShouldMatchers 
             controller.checkBlank()
         }
     }
+
+    feature("Notifying the VPM about a successfull dpport remove request") {
+
+        val (itf, (dpport, portno), (id, _)) = bindings(0)
+
+        def addMapping(vpm: VirtualPortManager) {
+            vpm.interfaceToDpPort = vpm.interfaceToDpPort + (itf -> dpport)
+            vpm.dpPortNumToInterface = vpm.dpPortNumToInterface + (portno -> itf)
+        }
+
+        def addBinding(vpm: VirtualPortManager) {
+            vpm.interfaceToVport = vpm.interfaceToVport + (itf, id)
+        }
+
+        def addStatus(vpm: VirtualPortManager, status: Boolean) {
+            vpm.interfaceToStatus = vpm.interfaceToStatus + (itf -> status)
+        }
+
+        def checkNoMapping(vpm: VirtualPortManager) {
+            vpm.interfaceToDpPort.get(itf) should be (None)
+            vpm.dpPortNumToInterface.get(portno) should be (None)
+        }
+
+        scenario("did not have mapping") {
+            val (vpm, controller) = getVPM()
+            vpm.dpPortsInProgress = vpm.dpPortsInProgress + itf
+
+            val newVpm = vpm.datapathPortRemoved(itf)
+            newVpm.dpPortsInProgress.contains(itf) should be(false)
+
+            controller.checkBlank()
+        }
+
+        scenario("has mapping, does not have binding") {
+            val (vpm, controller) = getVPM()
+            vpm.dpPortsInProgress = vpm.dpPortsInProgress + itf
+            addMapping(vpm)
+
+            val newVpm = vpm.datapathPortRemoved(itf)
+            newVpm.dpPortsInProgress.contains(itf) should be(false)
+            checkNoMapping(newVpm)
+
+            controller.checkBlank()
+        }
+
+        scenario("has mapping, has binding, unknown status") {
+            val (vpm, controller) = getVPM()
+            vpm.dpPortsInProgress = vpm.dpPortsInProgress + itf
+            addMapping(vpm)
+            addBinding(vpm)
+
+            val newVpm = vpm.datapathPortRemoved(itf)
+            newVpm.dpPortsInProgress.contains(itf) should be(false)
+            checkNoMapping(newVpm)
+
+            controller.checkBlank()
+        }
+
+        scenario("has mapping, has binding, status was inactive") {
+            val (vpm, controller) = getVPM()
+            vpm.dpPortsInProgress = vpm.dpPortsInProgress + itf
+            addMapping(vpm)
+            addBinding(vpm)
+            addStatus(vpm, false)
+
+            val newVpm = vpm.datapathPortRemoved(itf)
+            newVpm.dpPortsInProgress.contains(itf) should be(true)
+            newVpm.dpPortsWeAdded.contains(itf) should be(true)
+            checkNoMapping(newVpm)
+
+            controller.addDp.headOption should be(Some(itf))
+            controller.vportStatus.headOption should be(None)
+        }
+
+        scenario("has mapping, has binding, status was active, own request") {
+            val (vpm, controller) = getVPM()
+            vpm.dpPortsInProgress = vpm.dpPortsInProgress + itf
+            addMapping(vpm)
+            addBinding(vpm)
+            addStatus(vpm, true)
+
+            val newVpm = vpm.datapathPortRemoved(itf)
+            newVpm.dpPortsInProgress.contains(itf) should be(true)
+            newVpm.dpPortsWeAdded.contains(itf) should be(true)
+            checkNoMapping(newVpm)
+
+            controller.addDp.headOption should be(Some(itf))
+            controller.vportStatus.headOption should be(None)
+        }
+
+        scenario("has mapping, has binding, status was active, not own request") {
+            val (vpm, controller) = getVPM()
+            addMapping(vpm)
+            addBinding(vpm)
+            addStatus(vpm, true)
+
+            val newVpm = vpm.datapathPortRemoved(itf)
+            newVpm.dpPortsInProgress.contains(itf) should be(true)
+            newVpm.dpPortsWeAdded.contains(itf) should be(true)
+            checkNoMapping(newVpm)
+
+            controller.addDp.headOption should be(Some(itf))
+            controller.vportStatus.headOption should be(Some(dpport, id, false))
+        }
+
+    }
+
+    feature("Notifying the VPM about a dpport to forget about") {
+        scenario("The request was registered as in progress") {
+            val (itf, (dpport, portno), (id, _)) = bindings(0)
+            val (vpm, controller) = getVPM()
+
+            vpm.dpPortsInProgress = vpm.dpPortsInProgress + itf
+            vpm.dpPortsWeAdded = vpm.dpPortsWeAdded + itf
+            vpm.interfaceToStatus = vpm.interfaceToStatus + (itf -> true)
+
+            val newVpm = vpm.datapathPortForget(dpport)
+            newVpm.dpPortsInProgress.contains(itf) should be(false)
+            newVpm.dpPortsWeAdded.contains(itf) should be(false)
+            newVpm.interfaceToStatus.get(itf) should be(None)
+        }
+    }
+
 }
