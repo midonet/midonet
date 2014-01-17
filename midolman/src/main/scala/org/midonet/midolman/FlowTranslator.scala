midonet/midonet

commit cb8a645bb68578a4340c60ff275e478fc4266fda

Makes FlowActions and FlowKeys immutable
FlowActions and FlowKeys are now immutable, modulo deserialization.

This patch solves a bug that prevented object pooling from working
for almost all types of FlowKeys. After being created and tested
against the object pool, most FlowKeys were mutated. This means
that a FlowKey would not match any of those in the object pool and
would be inserted, but after setting additional fields it would
become equal to all the other homologous objects in the pool.

Addresses MN-1020
Signed-off-by: Duarte Nunes <duarte@midokura.com>

Change-Id: I30527a9302e8531f926f0cb37c5f949cb4b03123

Conflicts:
	docs/code-conventions.md
	midolman/src/main/scala/org/midonet/midolman/DatapathController.scala
	midolman/src/main/scala/org/midonet/midolman/FlowTranslator.scala
	midolman/src/test/scala/org/midonet/midolman/BridgeSimulationTestCase.scala
	midolman/src/test/scala/org/midonet/midolman/DeduplicationActorTestCase.scala
	midolman/src/test/scala/org/midonet/midolman/FlowManagementForPortSetTestCase.scala
	midolman/src/test/scala/org/midonet/midolman/InstallWildcardFlowForRemotePortTestCase.scala
	midolman/src/test/scala/org/midonet/midolman/InstallWildcardFlowTestCase.scala
	midolman/src/test/scala/org/midonet/midolman/RouterFlowInvalidationTest.scala
	midolman/src/test/scala/org/midonet/midolman/layer4/NatTestCase.scala
	midolman/src/test/scala/org/midonet/midolman/simulation/AdminStateTest.scala
	odp/src/main/java/org/midonet/odp/FlowMatches.java
	odp/src/main/java/org/midonet/odp/Port.java
	odp/src/main/java/org/midonet/odp/flows/FlowAction.java
	odp/src/main/java/org/midonet/odp/flows/FlowActionSample.java
	odp/src/main/java/org/midonet/odp/flows/FlowActionSetKey.java
	odp/src/main/java/org/midonet/odp/flows/FlowActions.java
	odp/src/main/java/org/midonet/odp/flows/FlowKeyEncap.java
	odp/src/main/java/org/midonet/odp/flows/FlowKeyTunnel.java
	odp/src/main/java/org/midonet/odp/flows/FlowKeyVLAN.java

midolman/src/main/scala/org/midonet/midolman/FlowTranslator.scala
@@ -9,6 +9,7 @@ import akka.actor.{ActorContext, ActorSystem}
 import akka.dispatch.{ExecutionContext, Promise, Future}
 import akka.event.LoggingAdapter
 import akka.pattern.ask
+import akka.event.LoggingAdapter
 import akka.util.Timeout
 import java.{util => ju}
 import java.util.UUID
@@ -28,6 +29,7 @@ import org.midonet.cluster.client
 import org.midonet.cluster.client.ExteriorPort
 import org.midonet.odp.flows.{
         FlowActionUserspace, FlowKeys, FlowActions, FlowAction}
+import org.midonet.odp.flows.FlowActions.{output, setKey}
 import org.midonet.odp.protos.OvsDatapathConnection
 import org.midonet.sdn.flows.{WildcardFlow, WildcardMatch}
 import org.midonet.util.functors.Callback0
@@ -202,9 +204,9 @@ trait FlowTranslator {
                         "be dropped because we cannot make Output actions.")
 
         // Translate a flow to local ports.
-        def translateFlowToLocalPorts(): TaggedActions =
-            localPorts.map { id =>
-                (FlowActions.output(id), FlowTagger.invalidateDPPort(id))
+        def translateFlowToLocalPorts: TaggedActions =
+            localPorts.map { 
+              id => (output(id), FlowTagger.invalidateDPPort(id))
             }.unzip
 
         // Helper functions for remote port translation
@@ -213,7 +215,7 @@ trait FlowTranslator {
 
         // Prepare an ODP object which can set the flow tunnel info
         def flowTunnelKey(key: Long, route: (Int, Int)) =
-            FlowActions.setKey(FlowKeys.tunnel(key, route._1, route._2))
+            setKey(FlowKeys.tunnel(key, route._1, route._2))
 
         // Generate a list of tunneling actions and tags for given tunnel key
         def tunnelSettings(key: Long, output: FlowAction[_]): TaggedActions = {
@@ -285,7 +287,7 @@ trait FlowTranslator {
                     expandPortAction(Seq(p), p.portId, inPortUUID, dpTags,
                         wMatch)(ec, system)
                 case u: FlowActionUserspace =>
-                    u.setUplinkPid(datapathConnection.getChannel.getLocalAddress.getPid)
+                    output(datapathConnection.getChannel.getLocalAddress.getPid)
                     Promise.successful(Seq(u))(ec)
                 case a =>
                     Promise.successful(Seq[FlowAction[_]](a))(ec)

commit 17ebf15cf7b43dbbee4e31d77282362a6a6db57d

Properly emit userspace flow actions
This patches addresses a regression introduced
in commit cb8a645bb68578a4340c60ff275e478fc4266fda.

Fixes MN-1089
Signed-off-by: hugobenichi <hugo@midokura.com>

Change-Id: I72c19cef18461c69bc315f59a676c3b0f7e6bbb0

midolman/src/main/scala/org/midonet/midolman/FlowTranslator.scala
@@ -29,7 +29,7 @@ import org.midonet.cluster.client
 import org.midonet.cluster.client.ExteriorPort
 import org.midonet.odp.flows.{
         FlowActionUserspace, FlowKeys, FlowActions, FlowAction}
-import org.midonet.odp.flows.FlowActions.{output, setKey}
+import org.midonet.odp.flows.FlowActions.{output, setKey, userspace}
 import org.midonet.odp.protos.OvsDatapathConnection
 import org.midonet.sdn.flows.{WildcardFlow, WildcardMatch}
 import org.midonet.util.functors.Callback0
@@ -287,8 +287,9 @@ trait FlowTranslator {
                     expandPortAction(Seq(p), p.portId, inPortUUID, dpTags,
                         wMatch)(ec, system)
                 case u: FlowActionUserspace =>
-                    output(datapathConnection.getChannel.getLocalAddress.getPid)
-                    Promise.successful(Seq(u))(ec)
+                    val uspace = userspace(datapathConnection
+                                           .getChannel.getLocalAddress.getPid)
+                    Promise.successful(Seq(uspace))(ec)
                 case a =>
                     Promise.successful(Seq[FlowAction[_]](a))(ec)
             }