midonet/midonet

commit dd0ecaedf49120602f5f65283ea1f8abf87ea841

Makes FlowActions and FlowKeys immutable
FlowActions and FlowKeys are now immutable, modulo deserialization.

This patch solves a bug that prevented object pooling from working
for almost all types of FlowKeys. After being created and tested
against the object pool, most FlowKeys were mutated. This means
that a FlowKey would not match any of those in the object pool and
would be inserted, but after setting additional fields it would
become equal to all the other homologous objects in the pool.

Addresses MN-1020
Signed-off-by: Duarte Nunes <duarte@midokura.com>

Change-Id: I30527a9302e8531f926f0cb37c5f949cb4b03123

Conflicts:
	midolman/src/main/scala/org/midonet/midolman/DatapathController.scala
	midolman/src/main/scala/org/midonet/midolman/FlowTranslator.scala
	midolman/src/test/scala/org/midonet/midolman/BridgeSimulationTestCase.scala
	midolman/src/test/scala/org/midonet/midolman/simulation/AdminStateTest.scala

midolman/src/main/scala/org/midonet/midolman/DatapathController.scala
@@ -34,7 +34,8 @@ import org.midonet.netlink.Callback
 import org.midonet.netlink.exceptions.NetlinkException
 import org.midonet.netlink.exceptions.NetlinkException.ErrorCode
 import org.midonet.odp.{PortOptions, Port, Ports, Datapath}
-import org.midonet.odp.flows.{FlowActions, FlowAction, FlowActionOutput}
+import org.midonet.odp.flows.{FlowAction, FlowActionOutput}
+import org.midonet.odp.flows.FlowActions.output
 import org.midonet.odp.ports._
 import org.midonet.odp.protos.OvsDatapathConnection
 import org.midonet.packets.IPv4Addr
@@ -635,7 +636,7 @@ class DatapathController extends Actor with ActorLogging with FlowTranslator {
             FlowTagger.invalidateByTunnelKey(exterior.tunnelKey))
 
         val wMatch = new WildcardMatch().setTunnelID(exterior.tunnelKey)
-        val actions = List[FlowAction[_]](FlowActions.output(port.getPortNo.shortValue))
+        val actions = List[FlowAction[_]](output(port.getPortNo.shortValue))
         val tags = Set[Any](FlowTagger.invalidateDPPort(port.getPortNo.shortValue()))
         fc ! AddWildcardFlow(WildcardFlow(wcmatch = wMatch, actions = actions),
                              None, ROSet.empty, tags)

midolman/src/main/scala/org/midonet/midolman/FlowTranslator.scala
@@ -10,8 +10,8 @@ import scala.concurrent.Future
 import scala.util.Failure
 
 import akka.actor.ActorSystem
-import akka.event.LoggingAdapter
 import akka.pattern.ask
+import akka.event.LoggingAdapter
 import akka.util.Timeout
 
 import org.midonet.cluster.client.Port
@@ -21,11 +21,9 @@ import org.midonet.midolman.topology.FlowTagger
 import org.midonet.midolman.topology.VirtualToPhysicalMapper
 import org.midonet.midolman.topology.VirtualTopologyActor
 import org.midonet.midolman.topology.rcu.PortSet
-import org.midonet.odp.flows.FlowAction
-import org.midonet.odp.flows.FlowActionUserspace
-import org.midonet.odp.flows.FlowActions
-import org.midonet.odp.flows.FlowKeys
 import org.midonet.odp.protos.OvsDatapathConnection
+import org.midonet.odp.flows._
+import org.midonet.odp.flows.FlowActions.{output, setKey}
 import org.midonet.sdn.flows.VirtualActions
 import org.midonet.sdn.flows.{WildcardFlow, WildcardMatch}
 import org.midonet.util.functors.Callback0
@@ -204,13 +202,12 @@ trait FlowTranslator {
 
         // Translate a flow to local ports.
         def translateFlowToLocalPorts: TaggedActions =
-            localPorts.map { id =>
-                (FlowActions.output(id), FlowTagger.invalidateDPPort(id))
+            localPorts.map { id => (output(id), FlowTagger.invalidateDPPort(id))
             }.unzip
 
         // Prepare an ODP object which can set the flow tunnel info
         def flowTunnelKey(key: Long, route: (Int, Int)) =
-            FlowActions.setKey(FlowKeys.tunnel(key, route._1, route._2))
+            setKey(FlowKeys.tunnel(key, route._1, route._2))
 
         // Generate a list of tunneling actions and tags for given tunnel key
         def tunnelSettings(key: Long, output: FlowAction[_]): TaggedActions = {
@@ -279,7 +276,7 @@ trait FlowTranslator {
                     expandPortAction(Seq(p), p.portId, inPortUUID, dpTags,
                         wMatch)
                 case u: FlowActionUserspace =>
-                    u.setUplinkPid(datapathConnection.getChannel.getLocalAddress.getPid)
+                    output(datapathConnection.getChannel.getLocalAddress.getPid)
                     Future.successful(Seq(u))
                 case a =>
                     Future.successful(Seq[FlowAction[_]](a))

midolman/src/test/scala/org/midonet/midolman/simulation/AdminStateTest.scala
@@ -30,9 +30,10 @@ import org.midonet.midolman.topology.rcu.PortSet
 import org.midonet.midolman.topology.VirtualTopologyActor.BridgeRequest
 import org.midonet.midolman.topology.VirtualToPhysicalMapper.PortSetRequest
 import org.midonet.midolman.util.MockCache
-import org.midonet.odp.protos.OvsDatapathConnection
 import org.midonet.odp
-import org.midonet.odp.flows.{FlowActions, FlowAction}
+import org.midonet.odp.flows.FlowAction
+import org.midonet.odp.flows.FlowActions.output
+import org.midonet.odp.protos.OvsDatapathConnection
 import org.midonet.packets.util.PacketBuilder._
 import org.midonet.packets._
 import org.midonet.packets.ICMP.UNREACH_CODE
@@ -242,7 +243,7 @@ class AdminStateTest extends FeatureSpec
 
             simRes should be (toPortSet(bridge.getId))
             var tacts = translateActions(simRes)
-            tacts should contain (FlowActions.output(1).asInstanceOf[Any])
+            tacts should contain (output(1).asInstanceOf[Any])
 
             When("the port is set to down")
 
@@ -255,7 +256,7 @@ class AdminStateTest extends FeatureSpec
             simRes should be (toPortSet(bridge.getId))
 
             tacts = translateActions(simRes)
-            tacts should not (contain (FlowActions.output(1).asInstanceOf[Any]))
+            tacts should not (contain (output(1).asInstanceOf[Any]))
         }
 
         scenario("a down router sends an ICMP prohibited error") {
@@ -415,7 +416,7 @@ class AdminStateTest extends FeatureSpec
         icmpPkt should not be null
 
         icmpPkt.getType should be (ICMP.TYPE_UNREACH)
-        icmpPkt.getCode should be (UNREACH_CODE.UNREACH_FILTER_PROHIB.toChar)
+        icmpPkt.getCode should be (UNREACH_CODE.UNREACH_FILTER_PROHIB.toByte)
     }
 
     feature("Setting the administrative state of a device should " +

commit 7bd7780e29bd73d32ac8a5df626cbaf8a6bf9b07

Properly emit userspace flow actions
This patches addresses a regression introduced in dd0ecaedf49120602f5f65283ea1f8abf87ea841.

Fixes MN-1089

Change-Id: I9cb7823a65ddbf3bd505679965ba434c9d6587a3
Signed-off-by: Duarte Nunes <duarte@midokura.com>

Conflicts:
	midolman/src/main/scala/org/midonet/midolman/FlowTranslator.scala

midolman/src/main/scala/org/midonet/midolman/DatapathController.scala
@@ -91,6 +91,9 @@ trait DatapathState extends VirtualPortsResolver with UnderlayResolver {
     // controller to the flow controller.
     def version: Long
 
+    // provide the netlink pid of the current datapath connection
+    def uplinkPid: Int
+
 }
 
 object DatapathController extends Referenceable {
@@ -352,6 +355,11 @@ class DatapathController extends Actor with ActorLogging with FlowTranslator {
             case m =>
                 log.info("Not handling {} (behaving as InitializationActor)", m)
         })
+        if (datapathConnection != null)
+          dpState.uplinkPid =
+            datapathConnection.getChannel.getLocalAddress.getPid
+        else
+          log.warning("preStart(): OvsDatapathConnection not yet initialized.")
     }
 
     def receive = null
@@ -1432,4 +1440,5 @@ class DatapathStateManager(val controller: VirtualPortManager.Controller)(
     override def getDpPortName(num: JInteger): Option[String] =
         _vportMgr.dpPortNumToInterface.get(num)
 
+    var uplinkPid: Int = 0
 }

midolman/src/main/scala/org/midonet/midolman/FlowTranslator.scala
@@ -4,10 +4,11 @@
 package org.midonet.midolman
 
 import java.util.UUID
+
 import scala.collection.mutable.ListBuffer
 import scala.collection.{Set => ROSet, mutable, breakOut}
 import scala.concurrent.Future
-import scala.util.Failure
+import scala.util.{Success, Failure}
 
 import akka.actor.ActorSystem
 import akka.pattern.ask
@@ -21,12 +22,12 @@ import org.midonet.midolman.topology.FlowTagger
 import org.midonet.midolman.topology.VirtualToPhysicalMapper
 import org.midonet.midolman.topology.VirtualTopologyActor
 import org.midonet.midolman.topology.rcu.PortSet
-import org.midonet.odp.protos.OvsDatapathConnection
 import org.midonet.odp.flows._
-import org.midonet.odp.flows.FlowActions.{output, setKey}
+import org.midonet.odp.flows.FlowActions.{setKey, output, userspace}
 import org.midonet.sdn.flows.VirtualActions
 import org.midonet.sdn.flows.{WildcardFlow, WildcardMatch}
 import org.midonet.util.functors.Callback0
+import org.midonet.util.concurrent.{CallingThreadExecutionContext, toFutureOps}
 
 object FlowTranslator {
 
@@ -74,7 +75,6 @@ trait FlowTranslator {
     import VirtualTopologyActor._
     import VirtualActions._
 
-    protected val datapathConnection: OvsDatapathConnection
     protected val dpState: DatapathState
 
     implicit protected val requestReplyTimeout: Timeout
@@ -84,9 +84,9 @@ trait FlowTranslator {
     val log: LoggingAdapter
     val cookieStr: String
 
-    protected def translateVirtualWildcardFlow(
-            flow: WildcardFlow, tags: ROSet[Any] = Set.empty):
-                Future[(WildcardFlow, ROSet[Any])] = {
+    protected def translateVirtualWildcardFlow(flow: WildcardFlow,
+                                               tags: ROSet[Any] = Set.empty)
+    : Future[(WildcardFlow, ROSet[Any])] = {
 
         val flowMatch = flow.getMatch
         val inPortId = flowMatch.getInputPortUUID
@@ -104,27 +104,24 @@ trait FlowTranslator {
             case None =>
         }
 
-        val actions = Option(flow.getActions) getOrElse Seq.empty
-
-        val translatedFuture = translateActions(
-                actions, Option(inPortId), Option(dpTags), flow.getMatch)
-
-        translatedFuture map { translated =>
-            WildcardFlow(wcmatch = flow.wcmatch,
-                actions = translated.toList,
-                priority = flow.priority,
-                hardExpirationMillis = flow.hardExpirationMillis,
-                idleExpirationMillis = flow.idleExpirationMillis)
-        } recover {
-            case ex =>
+        translateActions(
+            flow.getActions, Option(inPortId), Option(dpTags), flow.getMatch
+        ).continue {
+            case Success(translated) =>
+                (WildcardFlow(wcmatch = flow.wcmatch,
+                              actions = translated.toList,
+                              priority = flow.priority,
+                              hardExpirationMillis = flow.hardExpirationMillis,
+                              idleExpirationMillis = flow.idleExpirationMillis),
+                 dpTags)
+            case Failure(ex) =>
                 log.error(ex, "Translation of {} failed, falling back " +
-                          "on drop WilcardFlow {}", actions, cookieStr)
-                WildcardFlow(
-                    wcmatch = flow.wcmatch,
-                    priority = flow.priority,
-                    hardExpirationMillis = 5000)
-        } map { (_, dpTags) }
-
+                          "on drop WilcardFlow {}", flow.getActions, cookieStr)
+                (WildcardFlow(wcmatch = flow.wcmatch,
+                              priority = flow.priority,
+                              hardExpirationMillis = 5000),
+                 dpTags)
+        }(CallingThreadExecutionContext)
     }
 
     protected def portsForLocalPorts(localVrnPorts: Seq[UUID]): Seq[Short] = {
@@ -169,12 +166,12 @@ trait FlowTranslator {
 
         // Apply the chains.
         Future.sequence(localPorts map { portToChain })
-            .map{ chainsToPortsId }
+            .map { chainsToPortsId }(CallingThreadExecutionContext)
             .andThen {
                 case Failure(ex) =>
                     log.error(ex, "Error getting chains for PortSet {} {}",
                               portSetID, cookieStr)
-            }
+            }(CallingThreadExecutionContext)
     }
 
     /**
@@ -273,11 +270,9 @@ trait FlowTranslator {
                     // expandPortSetAction
                     epsa(Seq(s), s.portSetId, inPortUUID, dpTags, wMatch)
                 case p: FlowActionOutputToVrnPort =>
-                    expandPortAction(Seq(p), p.portId, inPortUUID, dpTags,
-                        wMatch)
+                    expandPortAction(Seq(p), p.portId, inPortUUID, dpTags, wMatch)
                 case u: FlowActionUserspace =>
-                    output(datapathConnection.getChannel.getLocalAddress.getPid)
-                    Future.successful(Seq(u))
+                    Future.successful(Seq(userspace(dpState.uplinkPid)))
                 case a =>
                     Future.successful(Seq[FlowAction[_]](a))
             }

midolman/src/main/scala/org/midonet/midolman/PacketWorkflow.scala
@@ -275,15 +275,17 @@ abstract class PacketWorkflow(protected val datapathConnection: OvsDatapathConne
     }
 
     private def executePacket(actions: Seq[FlowAction[_]]): Future[Boolean] = {
+        if (actions == null || actions.isEmpty) {
+            log.debug("Dropping packet {}", cookieStr)
+            return Future.successful(true)
+        }
+
         log.debug("Executing packet {}", cookieStr)
+
         packet.setActions(actions.asJava)
         if (packet.getMatch.isUserSpaceOnly) {
             log.debug("Applying userspace actions to packet {}", cookieStr)
             applyActionsAfterUserspaceMatch(packet)
-        }
-
-        if (actions == null || actions.isEmpty) {
-            return Future.successful(true)
         }
 
         val pktPromise = promise[Boolean]()
@@ -515,12 +517,7 @@ abstract class PacketWorkflow(protected val datapathConnection: OvsDatapathConne
             case ex =>
                 log.warning("failed to translate actions: {}", ex)
                 Nil
-        } flatMap {
-            case Nil =>
-                Future.successful(true)
-            case translatedActions =>
-                executePacket(translatedActions)
-        }
+        } flatMap executePacket
     }
 
     private def runCallbacks(callbacks: Iterable[Callback0]) {

midolman/src/main/scala/org/midonet/sdn/flows/WildcardFlow.scala
@@ -17,10 +17,10 @@ import org.midonet.util.functors.Callback0
 
 object WildcardFlow {
     def apply(wcmatch: WildcardMatch,
-                     actions: List[FlowAction[_]] = Nil,
-                     hardExpirationMillis: Int = 0,
-                     idleExpirationMillis: Int = 0,
-                     priority: Short = 0) =
+              actions: List[FlowAction[_]] = Nil,
+              hardExpirationMillis: Int = 0,
+              idleExpirationMillis: Int = 0,
+              priority: Short = 0) =
         new WildcardFlowImpl(wcmatch, actions, hardExpirationMillis,
                          idleExpirationMillis, priority)
 

midonet-util/src/main/scala/org/midonet/util/concurrent/FutureOps.scala
@@ -17,7 +17,9 @@ class FutureOps[T](val f: Future[T]) extends AnyVal {
     def continueWith[S](cont: Future[T] => S)
                        (implicit executor: ExecutionContext): Future[S] = {
         val p = promise[S]()
-        f onComplete { _ => p complete Try(cont(f)) }
+        f.onComplete { _ =>
+            p complete Try(cont(f))
+        }(CallingThreadExecutionContext)
         p.future
     }
 
@@ -30,7 +32,9 @@ class FutureOps[T](val f: Future[T]) extends AnyVal {
     def continue[S](cont: Try[T] => S)
                    (implicit executor: ExecutionContext): Future[S] = {
         val p = promise[S]()
-        f onComplete { x => p complete Try(cont(x)) }
+        f.onComplete { x =>
+            p complete Try(cont(x))
+        }(CallingThreadExecutionContext)
         p.future
     }
 }

netlink/src/main/java/org/midonet/netlink/MockNetlinkChannel.java
@@ -27,4 +27,8 @@ class MockNetlinkChannel extends NetlinkChannel {
     protected void implConfigureBlocking(boolean block) throws IOException {
     }
 
+    @Override
+    public Netlink.Address getLocalAddress() {
+        return new Netlink.Address(0);
+    }
 }

midolman/src/test/scala/org/midonet/midolman/flows/FlowTranslatorTestCase.scala
@@ -1,0 +1,112 @@
+/*
+ * Copyright (c) 2014 Midokura Europe SARL, All Rights Reserved.
+ */
+package org.midonet.midolman.flows
+
+import java.util.UUID
+
+import scala.collection.{Set => ROSet}
+import scala.collection.immutable.List
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+import akka.actor.ActorSystem
+import akka.event.{NoLogging, LoggingAdapter}
+import akka.util.Timeout
+
+import org.junit.runner.RunWith
+
+import org.scalatest.{GivenWhenThen, Matchers, FeatureSpec}
+import org.scalatest.junit.JUnitRunner
+
+import org.midonet.midolman.{DatapathState, FlowTranslator}
+import org.midonet.midolman.{VirtualTopologyHelper, MockMidolmanActors}
+import org.midonet.odp.Port
+import org.midonet.odp.flows.{FlowAction, FlowActionOutput}
+import org.midonet.odp.flows.FlowActions.userspace
+import org.midonet.sdn.flows.{WildcardFlow, WildcardMatch}
+import org.midonet.midolman.topology.VirtualTopologyActor
+import org.midonet.midolman.services.MessageAccumulator
+import org.midonet.midolman.topology.rcu.Host
+import org.midonet.odp.protos.OvsDatapathConnection
+
+@RunWith(classOf[JUnitRunner])
+class FlowTranslatorTestCase extends FeatureSpec
+                             with Matchers
+                             with GivenWhenThen
+                             with MockMidolmanActors
+                             with VirtualTopologyHelper {
+
+    override def registerActors = List(
+        VirtualTopologyActor -> (() => new VirtualTopologyActor
+                                       with MessageAccumulator))
+
+    class TranslationContext(dpState: TestDatapathState) {
+        def uplinkPid = dpState.uplinkPid
+
+        def uplinkPid_=(pid: Int): this.type = {
+            dpState.uplinkPid = pid
+            this
+        }
+    }
+
+    feature("UserspaceFlowActions are translated") {
+        scenario("the uplink pid is correctly set") {
+            Given("a UserspaceFlowAction")
+            val ufa = userspace()
+
+            When("translating that flow")
+            val (wcflow, _) = translate(ufa) { ctx =>
+                ctx.uplinkPid = 1
+            }
+
+            Then("the uplink pid should have been translated")
+            wcflow.actions should be (List(userspace(1)))
+        }
+    }
+
+    private[this] def translate(actions: FlowAction[_]*)
+                               (prepareCtx: TranslationContext => Unit)
+    : (WildcardFlow, ROSet[Any]) =
+        translate(actions.toList)(prepareCtx)
+
+    private[this] def translate(actions: List[FlowAction[_]])
+                               (prepareCtx: TranslationContext => Unit)
+    : (WildcardFlow, ROSet[Any]) = {
+        val testDpState = new TestDatapathState
+        val ctx = new TranslationContext(testDpState)
+
+        prepareCtx(ctx)
+
+        val wcmatch = new WildcardMatch()
+        val f = new FlowTranslator {
+            implicit protected def system: ActorSystem = actorSystem
+            implicit protected val requestReplyTimeout: Timeout = Timeout(3 seconds)
+            protected val datapathConnection: OvsDatapathConnection = null
+            val log: LoggingAdapter = NoLogging
+            val cookieStr = ""
+            val dpState = testDpState
+
+            def translate(actions: List[FlowAction[_]]) = {
+                val wcflow = WildcardFlow(wcmatch, actions = actions)
+                translateVirtualWildcardFlow(wcflow, null)
+            }
+        }.translate(actions)
+
+        Await.result(f, 3 seconds)
+    }
+
+    class TestDatapathState extends DatapathState {
+        var version: Long = 0
+        var host: Host = null
+        var uplinkPid: Int = 0
+
+        def peerTunnelInfo(peer: UUID): Option[(Int, Int)] = None
+        def tunnelGre: Option[Port[_, _]] = None
+        def greOutputAction: Option[FlowActionOutput] = None
+        def getDpPortNumberForVport(vportId: UUID): Option[Integer] = None
+        def getDpPortForInterface(itfName: String): Option[Port[_, _]] = None
+        def getVportForDpPortNumber(portNum: Integer): Option[UUID] = None
+        def getDpPortName(num: Integer): Option[String] = None
+    }
+}

midolman/src/test/scala/org/midonet/midolman/simulation/AdminStateTest.scala
@@ -390,6 +390,7 @@ class AdminStateTest extends FeatureSpec
             def getVportForDpPortNumber(portNum: Integer): Option[UUID] = null
             def getDpPortName(num: Integer): Option[String] = null
             def version: Long = 0L
+            def uplinkPid: Int = 0
         }
 
         def receive = {