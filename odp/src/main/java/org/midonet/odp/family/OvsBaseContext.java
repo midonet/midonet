dd957a68e753e6a3ac4db4cface2c23fb475cbb0
Hugo Benichi <hugo@midokura.com>
2014-01-18 18:12:03 +0900

    Adding NetlinkRequestContext for PortFamily
    and changing the _doPorts* methods in OvsDatapathConnectionImpl
    to not create RequestBuilder. Also changing _doDatapathsGet()
    into using the DatapathFamily context objects.
    
    Change-Id: I65cd3716b9cc0da086263b38bb6d4e8e23804ca9
    Signed-off-by: hugobenichi <hugo@midokura.com>

---
+++ odp/src/main/java/org/midonet/odp/family/OvsBaseContext.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2014 Midokura Europe SARL, All Rights Reserved.
+ */
+package org.midonet.odp.family;
+
+import org.midonet.netlink.NetlinkRequestContext;
+
+abstract class OvsBaseContext implements NetlinkRequestContext {
+    final short commandFamily;
+    final byte command;
+    public OvsBaseContext(int familyId, int command) {
+        this.commandFamily = (short) familyId;
+        this.command = (byte) command;
+    }
+    public short commandFamily() { return commandFamily; }
+    public byte command() { return command; }
+    abstract public byte version();
+}
