53e96ce1ad008ae345484bd2b7dac4f1e2a8e187
Hugo Benichi <hugo@midokura.com>
2014-02-12 17:13:23 +0900

    ChainManagers correctly set names of Chain objects
    Fix MN-132
    Signed-off-by: hugobenichi <hugo@midokura.com>
    
    Change-Id: Icea41b8b3a3f903327492177b17c28da6d4e5da4

--- midolman/src/main/java/org/midonet/cluster/client/ChainBuilder.java
+++ midolman/src/main/java/org/midonet/cluster/client/ChainBuilder.java
@@ -12,4 +12,5 @@ import org.midonet.midolman.rules.Rule;
 public interface ChainBuilder {
     void setRules(List<Rule> rules);
     void setRules(List<UUID> ruleOrder, Map<UUID, Rule> rules);
+    void setName(String name);
 }
