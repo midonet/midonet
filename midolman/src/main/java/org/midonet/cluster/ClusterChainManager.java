d9c839cba5e1f2133c555c71bd08e9ee36b4c65f
Hugo Benichi <hugo@midokura.com>
2014-02-12 17:13:23 +0900

    ChainManagers correctly set names of Chain objects
    Fix MN-132
    Signed-off-by: hugobenichi <hugo@midokura.com>
    
    Change-Id: Icea41b8b3a3f903327492177b17c28da6d4e5da4

--- midolman/src/main/java/org/midonet/cluster/ClusterChainManager.java
+++ midolman/src/main/java/org/midonet/cluster/ClusterChainManager.java
@@ -6,8 +6,8 @@ package org.midonet.cluster;
 
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
@@ -15,26 +15,29 @@ import java.util.UUID;
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
 import com.google.inject.Inject;
+import org.apache.zookeeper.KeeperException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.midonet.midolman.rules.Rule;
-import org.midonet.midolman.state.zkManagers.RuleZkManager;
 import org.midonet.cluster.client.ChainBuilder;
+import org.midonet.midolman.rules.Rule;
+import org.midonet.midolman.state.DirectoryCallback;
+import org.midonet.midolman.state.zkManagers.ChainZkManager;
+import org.midonet.midolman.state.zkManagers.RuleZkManager;
 
 public class ClusterChainManager extends ClusterManager<ChainBuilder> {
-    private static final Logger log = LoggerFactory
-        .getLogger(ClusterChainManager.class);
+    private static final Logger log =
+        LoggerFactory.getLogger(ClusterChainManager.class);
 
     @Inject
     RuleZkManager ruleMgr;
 
-    private Map<UUID, Map<UUID, Rule>> chainIdToRuleMap =
-            new HashMap<UUID, Map<UUID, Rule>>();
-    private Map<UUID, List<UUID>> chainToRuleIds =
-            new HashMap<UUID, List<UUID>>();
-    private Multimap<UUID, UUID> chainToMissingRuleIds =
-            HashMultimap.create();
+    @Inject
+    ChainZkManager chainMgr;
+
+    private Map<UUID, Map<UUID, Rule>> chainIdToRuleMap = new HashMap<>();
+    private Map<UUID, List<UUID>> chainToRuleIds = new HashMap<>();
+    private Multimap<UUID,UUID> chainToMissingRuleIds = HashMultimap.create();
 
     @Override
     protected void getConfig(UUID chainId) {
@@ -42,7 +45,9 @@ public class ClusterChainManager extends ClusterManager<ChainBuilder> {
             log.error("Trying to request the same Chain {}.", chainId);
             return;
         }
-        chainIdToRuleMap.put(chainId, new HashMap<UUID, Rule>());
+        chainIdToRuleMap.put(chainId, new HashMap<UUID,Rule>());
+        ChainNameCallback nameCB = new ChainNameCallback(chainId);
+        chainMgr.getNameAsync(chainId, nameCB, nameCB);
         RuleListCallback ruleListCB = new RuleListCallback(chainId);
         ruleMgr.getRuleIdListAsync(chainId, ruleListCB, ruleListCB);
     }
@@ -50,6 +55,37 @@ public class ClusterChainManager extends ClusterManager<ChainBuilder> {
     private void requestRule(UUID ruleID) {
         RuleCallback ruleCallback = new RuleCallback(ruleID);
         ruleMgr.getAsync(ruleID, ruleCallback, ruleCallback);
+    }
+
+    private class ChainNameCallback extends CallbackWithWatcher<String> {
+        private UUID chainId;
+
+        private ChainNameCallback(UUID chainId) {
+            this.chainId = chainId;
+        }
+
+        @Override
+        protected String describe() {
+            return "ChainName:" + chainId;
+        }
+
+        @Override
+        public void onSuccess(String data) {
+            getBuilder(chainId).setName(data);
+        }
+
+        @Override
+        public void pathDataChanged(String path) {
+            chainMgr.getNameAsync(chainId, this, this);
+        }
+
+        @Override
+        protected Runnable makeRetry() {
+            return new Runnable() {
+                @Override
+                public void run() { }
+            };
+        }
     }
 
     private class RuleListCallback extends CallbackWithWatcher<List<UUID>> {
