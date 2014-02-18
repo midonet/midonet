a13c6827f38a9d6f8323af8a803b6f2772292091
Brandon Berg <bberg@midokura.jp>
2014-02-18 16:05:51 +0900

    Improvements to Chain.apply()
    -No longer static, improving readability.
    -Guaranteed to allocate only one RuleResult.
    -Uses recursion instead of heap-allocated stack for jump backtracking.
    -Lifts flow tag construction out of apply().
    -No longer strikes fear into the hearts of men.
    -Now with unit tests!
    
    Signed-off-by: Brandon Berg <bberg@midokura.jp>
    Change-Id: I1d795135bfe068f31a05e4e68acd9cc82abb21c6

---
+++ midolman/src/test/scala/org/midonet/midolman/simulation/ChainTest.scala
@@ -0,0 +1,254 @@
+/*
+ * Copyright (c) 2014 Midokura Europe SARL, All Rights Reserved.
+ */
+package org.midonet.midolman.simulation
+
+import org.midonet.midolman.rules._
+import org.midonet.midolman.rules.RuleResult.Action
+import org.midonet.packets.IPAddr
+import org.midonet.sdn.flows.WildcardMatch
+
+import java.util.UUID
+import org.junit.runner.RunWith
+import org.scalatest.{BeforeAndAfterAll, BeforeAndAfter, Matchers, Suite}
+import org.scalatest.junit.JUnitRunner
+import akka.actor.ActorSystem
+import org.midonet.midolman.layer4.{MockNatMapping, NatMapping, NatMappingFactory}
+import scala.collection.mutable
+
+import scala.collection.JavaConverters._
+
+private object ChainTest {
+    protected val rejectRule = new LiteralRule(Condition.TRUE, Action.REJECT)
+    protected val acceptRule = new LiteralRule(Condition.TRUE, Action.ACCEPT)
+    protected val continueRule = new LiteralRule(Condition.FALSE, Action.ACCEPT)
+}
+
+@RunWith(classOf[JUnitRunner])
+class ChainTest extends Suite
+                with Matchers
+                with BeforeAndAfter
+                with BeforeAndAfterAll {
+    import ChainTest._
+
+    private implicit val actorSystem = ActorSystem("ChainTest")
+
+    private var fwdInfo: ChainPacketContext = _
+    private var pktMatch: WildcardMatch = _
+    private val ownerId: UUID = UUID.randomUUID
+
+    before {
+        pktMatch = new WildcardMatch()
+        pktMatch.setEthernetDestination("01:02:03:04:05:06")
+        pktMatch.setNetworkDestination(IPAddr.fromString("1.2.3.4"))
+
+        fwdInfo = new PacketContext(None, null, 0, null, null, null, true,
+                                    None, pktMatch)
+    }
+
+    override def beforeAll() {
+        Chain.natMappingFactory = new NatMappingFactory {
+
+            private val map = mutable.Map[UUID, NatMapping]()
+
+            def get(ownerID: UUID): NatMapping = {
+                map.getOrElse(ownerID, {
+                    val mapping = new MockNatMapping
+                    map.put(ownerID, mapping)
+                    mapping
+                })
+            }
+        }
+    }
+
+    def testNullChain() {
+        val res = applyChain(null)
+        res.action should be (Action.ACCEPT)
+    }
+
+    def testEmptyChain() {
+        val c = makeChain(Nil)
+        applyChain(c).action should be (Action.ACCEPT)
+    }
+
+    def testAcceptRule() {
+        val c = makeChain(List(acceptRule))
+        applyChain(c).action should be (Action.ACCEPT)
+    }
+
+    def testRejectRule() {
+        val c = makeChain(List(rejectRule))
+        applyChain(c).action should be (Action.REJECT)
+    }
+
+    def testContinueUntilAccept() {
+        val c = makeChain(List(continueRule, continueRule, acceptRule))
+        applyChain(c).action should be (Action.ACCEPT)
+    }
+
+    def testContinueUntilReject() {
+        val c = makeChain(List(continueRule, continueRule, rejectRule))
+        applyChain(c).action should be (Action.REJECT)
+    }
+
+    def testContinueUntilEnd() {
+        val c = makeChain(List(continueRule, continueRule, continueRule))
+        applyChain(c).action should be (Action.ACCEPT)
+    }
+
+    def testAcceptThenReject() {
+        val c = makeChain(List(acceptRule, rejectRule))
+        applyChain(c).action should be (Action.ACCEPT)
+    }
+
+    def testRejectThenAccept() {
+        val c = makeChain(List(rejectRule, acceptRule))
+        applyChain(c).action should be (Action.REJECT)
+    }
+
+    def testJumpToAccept() {
+        val acceptChain = makeChain(List(acceptRule))
+        val c = makeChain(List(makeJumpRule(acceptChain)),
+                          List(acceptChain))
+        applyChain(c).action should be (Action.ACCEPT)
+    }
+
+    def testJumpToReject() {
+        val rejectChain = makeChain(List(rejectRule))
+        val c = makeChain(List(makeJumpRule(rejectChain)),
+                          List(rejectChain))
+        applyChain(c).action should be (Action.REJECT)
+    }
+
+    def testDoubleJumpToAccept() {
+        val acceptChain = makeChain(List(acceptRule))
+        val innerJumpChain = makeChain(List(makeJumpRule(acceptChain)),
+                                       List(acceptChain))
+        val outerJumpChain = makeChain(List(makeJumpRule(innerJumpChain)),
+                                       List(innerJumpChain))
+        applyChain(outerJumpChain).action should be (Action.ACCEPT)
+    }
+
+    def testDoubleJumpToReject() {
+        val rejectChain = makeChain(List(rejectRule))
+        val innerJumpChain = makeChain(List(makeJumpRule(rejectChain)),
+                                       List(rejectChain))
+        val outerJumpChain = makeChain(List(makeJumpRule(innerJumpChain)),
+                                       List(innerJumpChain))
+        applyChain(outerJumpChain).action should be (Action.REJECT)
+    }
+
+    /*
+     * outerJumpChain
+     *   innerJumpToContinueChain
+     *     Continue
+     *   innerJumpToAcceptChain
+     *     Accept <-- Stop here
+     */
+    def testBacktrackToAccept() {
+        val continueChain = makeChain(List(continueRule))
+        val innerJumpToContinueChain =
+            makeChain(List(makeJumpRule(continueChain)), List(continueChain))
+        val acceptChain = makeChain(List(acceptRule))
+        val innerJumpToAcceptChain =
+            makeChain(List(makeJumpRule(acceptChain)), List(acceptChain))
+        val outerJumpChain =
+            makeChain(List(makeJumpRule(innerJumpToContinueChain),
+                           makeJumpRule(innerJumpToAcceptChain),
+                           rejectRule), // Shouldn't make it to this rule.
+                      List(innerJumpToContinueChain, innerJumpToAcceptChain))
+        applyChain(outerJumpChain).action should be (Action.ACCEPT)
+    }
+
+    /*
+     * outerJumpChain
+     *   Juwp to innerJumpToContinueChain:
+     *     Continue
+     *   Jump to innerJumpToRejectChain:
+     *     Reject <-- Stop here
+     */
+    def testBacktrackToReject() {
+        val continueChain = makeChain(List(continueRule))
+        val innerJumpToContinueChain =
+            makeChain(List(makeJumpRule(continueChain)), List(continueChain))
+        val rejectChain = makeChain(List(rejectRule))
+        val innerJumpToRejectChain =
+            makeChain(List(makeJumpRule(rejectChain)), List(rejectChain))
+        val outerJumpChain =
+            makeChain(List(makeJumpRule(innerJumpToContinueChain),
+                           makeJumpRule(innerJumpToRejectChain),
+                           acceptRule), // Shouldn't make it to this rule.
+                      List(innerJumpToContinueChain, innerJumpToRejectChain))
+        applyChain(outerJumpChain).action should be (Action.REJECT)
+    }
+
+    /*
+     * innerAndOuterChain:
+     *   Jump to middleChain:
+     *     Jump to innerAndOuterChain <-- Loop detected.
+     *     Accept <-- Stop here
+     *   Reject <-- Never reached.
+     */
+    def testLoopThenAccept() {
+        // Can't use makeChain because we need to add a jump target.
+        val jumpTargetMap = mutable.Map[UUID, Chain]()
+        val chainId = UUID.randomUUID
+        val name = "Chain-" + chainId
+        val innerAndOuterChain = new Chain(chainId,
+                                           List[Rule](rejectRule).asJava,
+                                           jumpTargetMap, name,
+                                           actorSystem.eventStream)
+        val middleChain = makeChain(List(makeJumpRule(innerAndOuterChain),
+                                         acceptRule),
+                                    List(innerAndOuterChain))
+
+        // Add a jump from innerAndOuterChain to middleChain.
+        innerAndOuterChain.getRules.add(0, makeJumpRule(middleChain))
+        jumpTargetMap(middleChain.id) = middleChain
+
+        applyChain(innerAndOuterChain).action should be (Action.ACCEPT)
+    }
+
+    /*
+     * innerAndOuterChain:
+     *   Jump to middleChain:
+     *     Jump to innerAndOuterChain <-- Loop detected.
+     *     Reject <-- Stop here
+     *   Accept <-- Never reached.
+     */
+    def testLoopThenReject() {
+        // Can't use makeChain because we need to add a jump target.
+        val jumpTargetMap = mutable.Map[UUID, Chain]()
+        val chainId = UUID.randomUUID
+        val name = "Chain-" + chainId
+        val innerAndOuterChain = new Chain(chainId,
+                                           List[Rule](acceptRule).asJava,
+                                           jumpTargetMap, name,
+                                           actorSystem.eventStream)
+        val middleChain = makeChain(List(makeJumpRule(innerAndOuterChain),
+                                         rejectRule),
+                                    List(innerAndOuterChain))
+
+        // Add a jump from innerAndOuterChain to middleChain.
+        innerAndOuterChain.getRules.add(0, makeJumpRule(middleChain))
+        jumpTargetMap(middleChain.id) = middleChain
+
+        applyChain(innerAndOuterChain).action should be (Action.REJECT)
+    }
+
+    private def applyChain(c: Chain) =
+        Chain.apply(c, fwdInfo, pktMatch, ownerId, false)
+
+    private def makeChain(rules: List[Rule],
+                          jumpTargets: List[Chain] = Nil): Chain = {
+        val chainId = UUID.randomUUID
+        val jumpTargetMap = jumpTargets.map(c => (c.id, c)).toMap
+        val name = "Chain-" + chainId.toString
+        rules.foreach(_.chainId = chainId)
+        new Chain(chainId, rules.asJava, jumpTargetMap, name,
+                  actorSystem.eventStream)
+    }
+
+    private def makeJumpRule(target: Chain) =
+        new JumpRule(Condition.TRUE, target.id, target.name, null, 0)
+}
