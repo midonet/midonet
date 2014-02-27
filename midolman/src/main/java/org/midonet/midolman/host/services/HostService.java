bc2dd7358c717c33ca249f56848594a6575a4037
Duarte Nunes <duarte@midokura.com>
2014-02-27 18:50:19 +0100

    Adds recovery of host ID on startup
    This patch allows a host to take ownership of an alive host ID in ZK
    iif the local and remote metadata match.
    
    Fix MN-1297
    
    Signed-off-by: Duarte Nunes <duarte@midokura.com>
    Change-Id: Id329fe28d575bafc803fd4e73c1aed9d74722388

--- midolman/src/main/java/org/midonet/midolman/host/services/HostService.java
+++ midolman/src/main/java/org/midonet/midolman/host/services/HostService.java
@@ -1,25 +1,22 @@
 /*
- * Copyright 2012 Midokura PTE LTD.
- * Copyright 2013 Midokura PTE LTD.
+ * Copyright (c) 2013 Midokura Europe SARL, All Rights Reserved.
  */
+
 package org.midonet.midolman.host.services;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.locks.LockSupport;
 
-import com.google.common.util.concurrent.AbstractService;
-import com.google.inject.Inject;
-import org.midonet.midolman.serialization.SerializationException;
-import org.midonet.midolman.state.ZkManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.util.concurrent.AbstractService;
+import com.google.inject.Inject;
+
 import org.midonet.midolman.host.HostIdGenerator;
-import org.midonet.midolman.host.HostIdGenerator.HostIdAlreadyInUseException;
 import org.midonet.midolman.host.HostIdGenerator.PropertiesFileNotWritableException;
 import org.midonet.midolman.host.HostInterfaceWatcher;
 import org.midonet.midolman.host.commands.executors.HostCommandWatcher;
@@ -28,8 +25,11 @@ import org.midonet.midolman.host.interfaces.InterfaceDescription;
 import org.midonet.midolman.host.scanner.InterfaceScanner;
 import org.midonet.midolman.host.state.HostDirectory;
 import org.midonet.midolman.host.state.HostZkManager;
+import org.midonet.midolman.serialization.SerializationException;
 import org.midonet.midolman.services.HostIdProviderService;
 import org.midonet.midolman.state.StateAccessException;
+import org.midonet.midolman.state.StatePathExistsException;
+import org.midonet.midolman.state.ZkManager;
 
 /**
  * Host internal service.
@@ -65,12 +65,18 @@ public class HostService extends AbstractService
     @Inject
     private ZkManager zkManager;
 
+    public static class HostIdAlreadyInUseException extends Exception {
+        HostIdAlreadyInUseException(String message) {
+            super(message);
+        }
+    }
+
     @Override
     protected void doStart() {
 
         log.info("Starting Midolman host agent.");
         try {
-            hostId = identifyHostId();
+            identifyHostId();
             cmdWatcher.checkCommands(hostId);
             watcherThread = new Thread(interfaceWatcher);
             interfaceWatcher.setHostId(hostId);
@@ -123,16 +129,17 @@ public class HostService extends AbstractService
      *                                     be written
      * @throws InterruptedException
      */
-    private UUID identifyHostId()
+    private void identifyHostId()
             throws StateAccessException, PropertiesFileNotWritableException,
-            InterruptedException, SerializationException {
+                   InterruptedException, SerializationException,
+                   HostIdAlreadyInUseException {
 
         log.debug("Identifying host");
         // Try to get the host Id
         HostDirectory.Metadata metadata = new HostDirectory.Metadata();
 
         // Retrieve the interfaces and store the addresses in the metadata
-        List<InetAddress> listAddresses = new ArrayList<InetAddress>();
+        ArrayList<InetAddress> listAddresses = new ArrayList<>();
         for (InterfaceDescription info: scanner.scanInterfaces()) {
             listAddresses.addAll(info.getInetAddresses());
         }
@@ -140,33 +147,56 @@ public class HostService extends AbstractService
         metadata.setAddresses(
                 listAddresses.toArray(new InetAddress[listAddresses.size()]));
 
-        UUID hostId = null;
-        // If HostIdAlreadyInUseException is thrown it will loop forever
-        while (hostId == null) {
-            try {
-                hostId = HostIdGenerator.getHostId(configuration, hostZkManager);
-                if (hostId != null) {
-                    String hostName;
-                    try {
-                        hostName = InetAddress.getLocalHost().getHostName();
-                    } catch (UnknownHostException e) {
-                        hostName = "UNKNOWN";
-                    }
-                    metadata.setName(hostName);
-                    hostZkManager.makeAlive(hostId, metadata);
-                    hostZkManager.setHostVersion(hostId);
-                    break;
-                }
-            } catch (HostIdAlreadyInUseException e) {
-                // The ID is already in use, wait. It could be that the ephemeral
-                // node has not been deleted yet (if the host just crashed)
-                log.warn("Host Id already in use. Waiting for it to be released.", e);
-                // Reset the hostId to null to continue looping
-                hostId = null;
-            }
+        try {
+            metadata.setName(InetAddress.getLocalHost().getHostName());
+        } catch (UnknownHostException e) {
+            metadata.setName("UNKNOWN");
+        }
+
+        hostId = HostIdGenerator.getHostId(configuration);
+        int retries = configuration.getRetriesForUniqueHostId();
+
+        while (!create(hostId, metadata, hostZkManager) && --retries >= 0) {
+            // The ID is already in use, wait. It could be that the ephemeral
+            // node has not been deleted yet (if the host just crashed)
+            log.warn("Host ID already in use. Waiting for it to be released.");
+
             Thread.sleep(configuration.getWaitTimeForUniqueHostId());
         }
-        return hostId;
+
+        if (retries < 0) {
+            log.error("Couldn't take ownership of the in-use host ID");
+            throw new HostIdAlreadyInUseException(
+                    "Host ID " + hostId + "appears to already be taken");
+        }
+    }
+
+    private static boolean create(UUID id,
+                                  HostDirectory.Metadata metadata,
+                                  HostZkManager zkManager)
+            throws StateAccessException, SerializationException {
+
+        if (zkManager.hostExists(id)) {
+            if (!metadata.equals(zkManager.getHostMetadata(id))) {
+                if (zkManager.isAlive(id))
+                    return false;
+                zkManager.updateMetadata(id, metadata);
+            }
+        } else {
+            zkManager.createHost(id, metadata);
+        }
+
+        // We want to make sure that the ephemeral nodes didn't
+        // disappear in the meanwhile
+        try {
+            zkManager.makeAlive(id);
+        } catch (StatePathExistsException ignored) { }
+
+        try {
+            zkManager.setHostVersion(id);
+        } catch (StatePathExistsException ignored) { }
+
+        return true;
     }
 
     public UUID getHostId() {
