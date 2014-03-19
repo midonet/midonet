9832a593b4f5bc0186c9a65f64fb43687067b74f
Galo Navarro <galo@midokura.com>
2014-03-19 21:19:22 +0100

    Report AuthServiceProvider errors in logs
    This has caused lots of headaches in deployments, Tomcat hides
    exceptions and config mistakes go unnoticed.
    
    Change-Id: I1f4a24064ebf9b4571590f094da9b47156bf1edb
    Signed-off-by: Galo Navarro <galo@midokura.com>

--- midonet-api/src/main/java/org/midonet/api/auth/AuthServiceProvider.java
+++ midonet-api/src/main/java/org/midonet/api/auth/AuthServiceProvider.java
@@ -10,16 +10,21 @@ import org.midonet.api.auth.cloudstack.CloudStackAuthService;
 import org.midonet.api.auth.cloudstack.CloudStackClient;
 import org.midonet.api.auth.cloudstack.CloudStackConfig;
 import org.midonet.api.auth.cloudstack.CloudStackJsonParser;
-import org.midonet.api.auth.keystone.v2_0.KeystoneClient;
 import org.midonet.api.auth.keystone.KeystoneConfig;
+import org.midonet.api.auth.keystone.v2_0.KeystoneClient;
 import org.midonet.api.auth.keystone.v2_0.KeystoneService;
 import org.midonet.cluster.DataClient;
 import org.midonet.config.ConfigProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Auth service provider.
  */
 public class AuthServiceProvider implements Provider<AuthService> {
+
+    private static final Logger log =
+        LoggerFactory.getLogger(AuthServiceProvider.class);
 
     private final ConfigProvider provider;
     private final DataClient dataClient;
@@ -39,6 +44,8 @@ public class AuthServiceProvider implements Provider<AuthService> {
         try {
             clazz = Class.forName(config.getAuthProvider());
         } catch (ClassNotFoundException e) {
+            log.error("Auth provider doesn't exist {}",
+                      config.getAuthProvider(), e);
             throw new UnsupportedOperationException(
                     "Auth provider does not exist: "
                             + config.getAuthProvider(), e);
@@ -75,15 +82,21 @@ public class AuthServiceProvider implements Provider<AuthService> {
             return new MockAuthService(mockAuthConfig, dataClient);
         } else {
             try {
-                // Try instantiating with no parameter
+                log.warn("AuthProvider is not recognized, trying to use a" +
+                         "default one but this is likely a config error " +
+                         "(review your web.xml)");
                 return (AuthService) clazz.newInstance();
             } catch (InstantiationException e) {
                 // The class is abstract or interface
+                log.error("Auth provider is not valid: {}",
+                          config.getAuthProvider(), e);
                 throw new UnsupportedOperationException(
                         "Auth provider is not a valid class: "
                                 + config.getAuthProvider(), e);
             } catch (IllegalAccessException e) {
                 // The constructor is not public
+                log.error("Auth provider cannot be instantiated: {}",
+                          config.getAuthProvider(), e);
                 throw new UnsupportedOperationException(
                         "Not authorized to instantiate auth provider: "
                                 + config.getAuthProvider(), e);
