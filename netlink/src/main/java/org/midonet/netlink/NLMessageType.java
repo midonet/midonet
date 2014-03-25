ebc5df120cf3f44912750422eae860bbd10cc850
hugo <hugo@midokura.jp>
2014-03-25 18:24:34 +0900

    Netlink message type refactor
    Similar to NetlinkFlag refactor.
    
    Change-Id: Ie77ce52cf94d2094c5ec19f3ccd1a00ae22fe42e
    Signed-off-by: hugobenichi <hugo@midokura.com>

---
+++ netlink/src/main/java/org/midonet/netlink/NLMessageType.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2014 Midokura Europe SARL, All Rights Reserved.
+ */
+package org.midonet.netlink;
+
+/**
+ * Interface listing constant short values for netlink message types.
+ * See include/uapi/linux/netlink.h in Linux kernel sources.
+ */
+public interface NLMessageType {
+    short NOOP    = (short) 0x0001;
+    short ERROR   = (short) 0x0002;
+    short DONE    = (short) 0x0003;
+    short OVERRUN = (short) 0x0004;
+}
