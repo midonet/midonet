ef9a7653dde49e83a0dd7777b3c1d6e917756307
Midokura <midokura@bcn10.bcn.midokura.com>
2014-03-26 02:21:12 -0400

    perftests: add option to set up scenario and exit

--- misc/perf/perftests.sh
+++ misc/perf/perftests.sh
@@ -159,7 +159,7 @@ assert_dependencies() {
     test -f $UPLOAD_KEY || err_exit "upload ssh key not found at $UPLOAD_KEY"
 }
 
-run_tests() {
+setup_tests() {
     assert_dependencies
     umask 0022
     mkdir -p $TMPDIR
@@ -177,8 +177,12 @@ run_tests() {
     install_midolman
 
     create_scenario
-    setup_jmxtrans
     connectivity_check
+}
+
+run_tests() {
+    setup_tests
+    setup_jmxtrans
     warm_up
     test_throughput
     long_running_tests
@@ -671,8 +675,15 @@ destroy_scenario() {
 # Script body
 ########################################################################
 
+setup_only=no
+
+if [ $1 == '-s' ] || [ $1 == '--setup'] ; then
+    shift
+    setup_only=yes
+fi
+
 if [ -z $1 ] ; then
-    echo "Usage: perftests.sh scenario" >&2
+    echo "Usage: perftests.sh [-s|--setup] scenario" >&2
     exit 1
 fi
 
@@ -692,6 +703,11 @@ fi
 echo "Sourcing topology description from $topology"
 source $topology
 echo "Executing tests.."
-run_tests
+
+if [ $setup_only = 'yes' ] ; then
+    setup_tests
+else
+    run_tests
+fi
 
 popd
