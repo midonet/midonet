midonet/midonet

commit 5b40bd9150eca25f5ec265a8714540bde2394d6a

Several small code cleanups
* remove unused guice modules
* fix imports
* use enums to parse enum-like config keys

Change-Id: Idc9cf6731a93fd75154a7b59b5c1656f136dfccf
Signed-off-by: Guillermo Ontanon <guillermo@midokura.jp>

midolman/src/main/java/org/midonet/midolman/guice/MidolmanModule.java
@@ -6,15 +6,15 @@ package org.midonet.midolman.guice;
 import com.google.inject.Inject;
 import com.google.inject.PrivateModule;
 import com.google.inject.Provider;
-
 import com.yammer.metrics.core.MetricsRegistry;
+
+import org.midonet.cluster.Client;
 import org.midonet.config.ConfigProvider;
 import org.midonet.midolman.config.MidolmanConfig;
 import org.midonet.midolman.services.DatapathConnectionService;
 import org.midonet.midolman.services.MidolmanActorsService;
 import org.midonet.midolman.services.MidolmanService;
 import org.midonet.midolman.simulation.Chain;
-import org.midonet.cluster.Client;
 
 /**
  * Main midolman configuration module

midolman/src/main/java/org/midonet/midolman/services/MidolmanService.java
@@ -8,9 +8,10 @@ import com.google.common.util.concurrent.AbstractService;
 import com.google.inject.Inject;
 import com.yammer.metrics.core.MetricsRegistry;
 import com.yammer.metrics.reporting.JmxReporter;
-import org.midonet.midolman.host.services.HostService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import org.midonet.midolman.host.services.HostService;
 
 /**
  * Basic controller of the internal midolman services.
@@ -30,9 +31,6 @@ public class MidolmanService extends AbstractService {
     DatapathConnectionService datapathConnectionService;
 
     @Inject
-    SelectLoopService selectLoopService;
-
-    @Inject
     MetricsRegistry metrics;
 
     @Inject(optional = true)
@@ -43,7 +41,6 @@ public class MidolmanService extends AbstractService {
 
     @Override
     protected void doStart() {
-        startService(selectLoopService);
         startService(datapathConnectionService);
         startService(actorsService);
         startService(hostService);
@@ -59,7 +56,6 @@ public class MidolmanService extends AbstractService {
             stopService(hostService);
             stopService(actorsService);
             stopService(datapathConnectionService);
-            stopService(selectLoopService);
 
             notifyStopped();
         } catch (Exception e) {

midolman/conf/midolman.conf
@@ -103,7 +103,8 @@ max_flow_count = 10000
 max_wildcard_flow_count = 10000
 # Midolman uses a pool of reusable buffers to send requests to the
 # datapath. The options below tune the pool's size and that of its
-# buffers.
+# buffers. One pool is created for each output channel, the settings
+# defined here will apply to each of those pools.
 # max_size: maximum number of buffers to hold in the pool. When the
 #           pool is empty (all buffers are in use) and has reached
 #           its maximum size, temporary buffers will be allocated.
@@ -121,7 +122,34 @@ send_buffer_pool_buf_size_kb = 16
 # by reducing synchronization costs. Too high a value may hurt latency.
 msgs_per_batch = 200
 
+
+# Midolman limits the amount of packets in flight in the system at any
+# given time. This prevents its internal queues from growing infinitely.
+# Additionally, midolman ensures that its processing capacity is shared
+# fairly among ports connected to the datapath. This, for example,
+# would prevent a single VM from setting up new flows at a rate that
+# would starve other VMs in the system.
+#
+# This behaviour is achieved by routing packets that miss the datapath
+# flow table and rise to userspace through a Hierarchical Token Bucket.
+# This HTB is set up in such a way such that tunnel ports will get 50%
+# of the resources, and the remaining 50% is shared fairly among all
+# other ports (typically, VMs).
+#
+# The rate at which the buckets are refilled is automatic and dynamic.
+# However the size of the buckets at each stage of the HTB can be tuned
+# through the settings below, increasing a bucket size will increase the
+# burstiness at which traffic can be queued before new tokens become
+# available.
+
+# global_incoming_burts_capacity: size of the root bucket in the HTB.
 global_incoming_burst_capacity = 20000
+
+# tunnel_incoming_burst_capacity: bucket size for tunnel ports (GRE, VxLAN)
 tunnel_incoming_burst_capacity = 10000
+
+# vm_incoming_burst_capacity: bucket size for VM ports
 vm_incoming_burst_capacity = 8000
+
+# vtep_incoming_burst_capacity: bucket size for VTEP (VxLAN) ports.
 vtep_incoming_burst_capacity = 2000

midolman/src/main/java/org/midonet/midolman/Midolman.java
@@ -26,7 +26,6 @@ import org.midonet.cluster.services.MidostoreSetupService;
 import org.midonet.midolman.guice.cluster.ClusterClientModule;
 import org.midonet.midolman.guice.config.ConfigProviderModule;
 import org.midonet.midolman.guice.datapath.DatapathModule;
-import org.midonet.midolman.guice.reactor.ReactorModule;
 import org.midonet.midolman.guice.serialization.SerializationModule;
 import org.midonet.midolman.guice.zookeeper.ZookeeperConnectionModule;
 import org.midonet.midolman.host.guice.HostModule;
@@ -105,7 +104,6 @@ public class Midolman {
 
         injector = Guice.createInjector(
             new ZookeeperConnectionModule(),
-            new ReactorModule(),
             new VersionModule(),
             new HostModule(),
             new ConfigProviderModule(configFilePath),

midolman/src/main/java/org/midonet/midolman/ZkCluster.java
@@ -21,13 +21,10 @@ import org.midonet.cluster.data.host.Host;
 import org.midonet.config.ConfigProvider;
 import org.midonet.midolman.config.MidolmanConfig;
 import org.midonet.midolman.guice.MockCacheModule;
-import org.midonet.midolman.guice.InterfaceScannerModule;
 import org.midonet.midolman.guice.cluster.ClusterClientModule;
 import org.midonet.midolman.guice.config.ConfigProviderModule;
-import org.midonet.midolman.guice.reactor.ReactorModule;
 import org.midonet.midolman.guice.serialization.SerializationModule;
 import org.midonet.midolman.guice.zookeeper.ZookeeperConnectionModule;
-import org.midonet.midolman.host.guice.HostModule;
 import org.midonet.midolman.monitoring.store.Store;
 import org.midonet.midolman.version.guice.VersionModule;
 
@@ -64,7 +61,6 @@ public class ZkCluster {
         log.info("Injecting custom ZkCluster juice profile");
         clusterInjector = Guice.createInjector(
             new ZookeeperConnectionModule(),
-            new ReactorModule(),
             new VersionModule(),
             new ConfigProviderModule(configFilePath),
             new ClosedStoreModule(),

midolman/src/main/java/org/midonet/midolman/guice/MidolmanActorsModule.java
@@ -3,11 +3,9 @@
 */
 package org.midonet.midolman.guice;
 
+import java.io.IOException;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
-
-import org.midonet.midolman.host.config.HostConfig;
-import org.midonet.midolman.l4lb.HealthMonitor;
 import scala.concurrent.duration.Duration;
 
 import akka.actor.ActorInitializationException;
@@ -16,7 +14,6 @@ import akka.actor.OneForOneStrategy;
 import akka.actor.SupervisorStrategy;
 import akka.actor.SupervisorStrategy.Directive;
 import akka.japi.Function;
-
 import com.google.inject.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,14 +21,17 @@ import org.slf4j.LoggerFactory;
 import org.midonet.cache.Cache;
 import org.midonet.midolman.*;
 import org.midonet.midolman.config.MidolmanConfig;
-import org.midonet.midolman.guice.datapath.DatapathModule;
+import org.midonet.midolman.host.config.HostConfig;
 import org.midonet.midolman.io.DatapathConnectionPool;
 import org.midonet.midolman.io.UpcallDatapathConnectionManager;
+import org.midonet.midolman.l4lb.HealthMonitor;
 import org.midonet.midolman.monitoring.MonitoringActor;
 import org.midonet.midolman.routingprotocols.RoutingManagerActor;
 import org.midonet.midolman.services.HostIdProviderService;
 import org.midonet.midolman.services.MidolmanActorsService;
 import org.midonet.midolman.topology.*;
+import org.midonet.util.eventloop.SelectLoop;
+import org.midonet.util.eventloop.SimpleSelectLoop;
 
 import static org.midonet.midolman.guice.CacheModule.NAT_CACHE;
 
@@ -56,6 +56,9 @@ public class MidolmanActorsModule extends PrivateModule {
     public @interface RESUME_STRATEGY {}
     @BindingAnnotation @Target({FIELD, METHOD}) @Retention(RUNTIME)
     public @interface CRASH_STRATEGY {}
+
+    @BindingAnnotation @Target({FIELD, METHOD}) @Retention(RUNTIME)
+    public @interface ZEBRA_SERVER_LOOP {}
 
     private static final Logger log = LoggerFactory
             .getLogger(MidolmanActorsModule.class);
@@ -114,6 +117,15 @@ public class MidolmanActorsModule extends PrivateModule {
                 log.warn("Unknown supervisor strategy [{}], " +
                          "falling back to resume strategy", strategy);
                 return getResumeStrategy();
+        }
+    }
+
+    @Provides @Exposed @Singleton @ZEBRA_SERVER_LOOP
+    public SelectLoop provideSelectLoop() {
+        try {
+            return new SimpleSelectLoop();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 

midolman/src/main/java/org/midonet/midolman/guice/datapath/DatapathModule.java
@@ -5,10 +5,17 @@ package org.midonet.midolman.guice.datapath;
 
 import javax.inject.Singleton;
 
-import com.google.inject.*;
+import com.google.inject.Inject;
+import com.google.inject.PrivateModule;
+import com.google.inject.Provider;
 
 import org.midonet.midolman.config.MidolmanConfig;
-import org.midonet.midolman.io.*;
+import org.midonet.midolman.io.DatapathConnectionPool;
+import org.midonet.midolman.io.OneToOneConnectionPool;
+import org.midonet.midolman.io.OneToOneDpConnManager;
+import org.midonet.midolman.io.OneToManyDpConnManager;
+import org.midonet.midolman.io.UpcallDatapathConnectionManager;
+import org.midonet.midolman.io.TokenBucketPolicy;
 import org.midonet.midolman.services.DatapathConnectionService;
 
 
@@ -44,8 +51,21 @@ public class DatapathModule extends PrivateModule {
     public static class UpcallDatapathConnectionManagerProvider
             implements Provider<UpcallDatapathConnectionManager> {
 
-        public final String ONE_TO_ONE = "one_to_one";
-        public final String ONE_TO_MANY = "one_to_many";
+        public enum ThreadingMapping {
+            ONE_TO_MANY("one_to_many"),
+            ONE_TO_ONE("one_to_one");
+
+            private final String text;
+
+            private ThreadingMapping(final String text) {
+                this.text = text;
+            }
+
+            @Override
+            public String toString() {
+                return text;
+            }
+        }
 
         @Inject
         MidolmanConfig config;
@@ -55,14 +75,15 @@ public class DatapathModule extends PrivateModule {
 
         @Override
         public UpcallDatapathConnectionManager get() {
-            String threadingModel = config.getInputChannelThreading();
-            if (ONE_TO_ONE.equalsIgnoreCase(threadingModel)) {
-                return new OneToOneDpConnManager(config, tbPolicy);
-            } else if (ONE_TO_MANY.equalsIgnoreCase(threadingModel)) {
-                return new OneToManyDpConnManager(config, tbPolicy);
-            } else {
-                throw new IllegalArgumentException(
-                    "Unknown value for input_channel_threading: " + threadingModel);
+            String val = config.getInputChannelThreading();
+            switch (ThreadingMapping.valueOf(val)) {
+                case ONE_TO_MANY:
+                    return new OneToManyDpConnManager(config, tbPolicy);
+                case ONE_TO_ONE:
+                    return new OneToOneDpConnManager(config, tbPolicy);
+                default:
+                    throw new IllegalArgumentException(
+                        "Unknown value for input_channel_threading: " + val);
             }
         }
     }

midolman/src/main/java/org/midonet/midolman/guice/reactor/MockReactorModule.java
@@ -1,45 +1,0 @@
-/*
-* Copyright 2013 Midokura Europe SARL
-*/
-package org.midonet.midolman.guice.reactor;
-
-import java.io.IOException;
-
-import com.google.inject.*;
-
-import org.midonet.midolman.services.SelectLoopService;
-import org.midonet.util.eventloop.*;
-
-import static org.midonet.midolman.guice.reactor.ReactorModule.ZEBRA_SERVER_LOOP;
-
-/**
- * This is an Guice module that will expose a {@link SelectLoop} and a {@link Reactor}
- * binding to the enclosing injector.
- */
-public class MockReactorModule extends PrivateModule {
-    @Override
-    protected void configure() {
-        bind(SelectLoop.class)
-                .annotatedWith(ZEBRA_SERVER_LOOP.class)
-                .toProvider(MockSelectLoopProvider.class)
-                .in(Singleton.class);
-
-        expose(Key.get(SelectLoop.class, ZEBRA_SERVER_LOOP.class));
-
-        bind(SelectLoopService.class)
-            .in(Singleton.class);
-
-        expose(SelectLoopService.class);
-    }
-
-    public static class MockSelectLoopProvider implements Provider<SelectLoop> {
-        @Override
-        public SelectLoop get() {
-            try {
-                return new MockSelectLoop();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-}

commit 3792ddda37d70977233d033b35d9de46d3de4a89

Restore SelectLoopService
Regression introduced in 5b40bd9

Related to MN-1482

Without SelectLoopService, zebra-server-loop doesn't start and
routes advertised by BGP are not installed in virtual routers.

Change-Id: Ib0b242721d843434bef30121be89fcdd15c72cf9
Signed-off-by: Takayuki Usui <takayuki@midokura.com>

midolman/src/main/java/org/midonet/midolman/guice/MidolmanModule.java
@@ -14,6 +14,7 @@ import org.midonet.midolman.config.MidolmanConfig;
 import org.midonet.midolman.services.DatapathConnectionService;
 import org.midonet.midolman.services.MidolmanActorsService;
 import org.midonet.midolman.services.MidolmanService;
+import org.midonet.midolman.services.SelectLoopService;
 import org.midonet.midolman.simulation.Chain;
 
 /**
@@ -41,7 +42,10 @@ public class MidolmanModule extends PrivateModule {
         bind(MetricsRegistry.class).toInstance(new MetricsRegistry());
         expose(MetricsRegistry.class);
 
-        requestStaticInjection(Chain.class);
+        bind(SelectLoopService.class)
+            .asEagerSingleton();
+
+       requestStaticInjection(Chain.class);
 
     }
 

midolman/src/main/java/org/midonet/midolman/services/MidolmanService.java
@@ -31,6 +31,9 @@ public class MidolmanService extends AbstractService {
     DatapathConnectionService datapathConnectionService;
 
     @Inject
+    SelectLoopService selectLoopService;
+
+    @Inject
     MetricsRegistry metrics;
 
     @Inject(optional = true)
@@ -42,6 +45,7 @@ public class MidolmanService extends AbstractService {
     @Override
     protected void doStart() {
         startService(datapathConnectionService);
+        startService(selectLoopService);
         startService(actorsService);
         startService(hostService);
         startService(storeService);
@@ -55,6 +59,7 @@ public class MidolmanService extends AbstractService {
             stopService(storeService);
             stopService(hostService);
             stopService(actorsService);
+            stopService(selectLoopService);
             stopService(datapathConnectionService);
 
             notifyStopped();