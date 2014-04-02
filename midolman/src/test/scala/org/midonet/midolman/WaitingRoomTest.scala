midonet/midonet

commit 2c88c20d5579b1b81b90385c9fef7f9eb69f1331

Fixes memory leak when expiring packets
This patch fixes a memory leak that left pending packets around when
we expired the simulated packet. It also refines HTB integration to
account for packets that are re-simulated.

An issue this patch does not address is adding a bucket for the
waiting room, to make sure we cap the number of restarted simulations.

Ref MN-1455
Signed-off-by: Duarte Nunes <duarte@midokura.com>
Change-Id: I9b2ece9915770e946bbc802d6493dab611da04a0

midolman/src/main/scala/org/midonet/midolman/DeduplicationActor.scala
@@ -21,7 +21,6 @@ import org.midonet.midolman.logging.ActorLogWithoutPath
 import org.midonet.midolman.monitoring.metrics.PacketPipelineMetrics
 import org.midonet.midolman.rules.Condition
 import org.midonet.midolman.simulation.Coordinator
-import org.midonet.midolman.topology.VirtualTopologyActor
 import org.midonet.midolman.topology.rcu.TraceConditions
 import org.midonet.netlink.exceptions.NetlinkException
 import org.midonet.odp.flows.FlowAction
@@ -66,7 +65,7 @@ class DeduplicationActor(
             val traceMessageCache: Cache,
             val traceIndexCache: Cache,
             val metrics: PacketPipelineMetrics,
-            val packetOut: () => Unit)
+            val packetOut: Int => Unit)
             extends Actor with ActorLogWithoutPath {
 
     import DatapathController.DatapathReady
@@ -93,7 +92,7 @@ class DeduplicationActor(
     protected val simulationExpireMillis = 5000L
 
     private val waitingRoom = new WaitingRoom[PacketHandler](
-        giveUpWorkflows, (simulationExpireMillis millis).toNanos)
+                                        (simulationExpireMillis millis).toNanos)
 
     override def preStart() {
         super.preStart()
@@ -131,20 +130,18 @@ class DeduplicationActor(
 
         case ApplyFlow(actions, cookieOpt) if cookieOpt.isDefined =>
             val cookie = cookieOpt.get
-            cookieToPendedPackets.remove(cookie) foreach { pendedPackets =>
-                cookieToDpMatch.remove(cookie) foreach {
-                    dpMatch => dpMatchToCookie.remove(dpMatch)
-                }
+            val pendingPackets = removePacket(cookie)
+
+            if (pendingPackets.size > 0) {
+                packetOut(pendingPackets.size)
 
                 // Send all pended packets with the same action list (unless
                 // the action list is empty, which is equivalent to dropping)
                 if (actions.nonEmpty) {
-                    for (unpendedPacket <- pendedPackets) {
-                        executePacket(cookie, unpendedPacket, actions)
-                        metrics.pendedPackets.dec()
-                    }
+                    pendingPackets foreach (executePacket(cookie, _, actions))
+                    metrics.pendedPackets.dec(pendingPackets.size)
                 } else {
-                    metrics.packetsProcessed.mark(pendedPackets.size)
+                    metrics.packetsProcessed.mark(pendingPackets.size)
                 }
             }
 
@@ -163,17 +160,17 @@ class DeduplicationActor(
             traceConditions = newTraceConditions
     }
 
-    private def giveUpWorkflows(pws: Iterable[PacketHandler]) {
-        for (pw <- pws if pw.idle)
-            giveUpWorkflow(pw)
-    }
-
-    private def giveUpWorkflow(pw: PacketHandler) {
-        try {
-            pw.drop()
-        } catch {
-            case e: Exception =>
-                log.error(e, "Failed to drop flow for {}", pw.packet)
+    // We return collection.Set so we can return an empty immutable set
+    // and a non-empty mutable set.
+    private def removePacket(cookie: Int): collection.Set[Packet] = {
+        val pending = cookieToPendedPackets.remove(cookie)
+        if (pending.isDefined) {
+            val dpMatch = cookieToDpMatch.remove(cookie)
+            if (dpMatch.isDefined)
+                dpMatchToCookie.remove(dpMatch.get)
+            pending.get
+        } else {
+            Set.empty
         }
     }
 
@@ -207,7 +204,6 @@ class DeduplicationActor(
      */
     private def postponeOn(pw: PacketHandler, f: Future[_]) {
         log.debug("Packet with {} postponed", pw.cookieStr)
-        packetOut()
         f onComplete {
             case Success(_) =>
                 log.info("Issuing restart for simulation {}", pw.cookieStr)
@@ -216,7 +212,31 @@ class DeduplicationActor(
                 log.warning("Failure on waiting workflow's future", ex)
         }
         metrics.packetPostponed()
-        waitingRoom enter pw
+        giveUpWorkflows(waitingRoom enter pw)
+    }
+
+    private def giveUpWorkflows(pws: IndexedSeq[PacketHandler]) {
+        var i = 0
+        while (i < pws.size) {
+            giveUpWorkflow(pws(i))
+            i += 1
+        }
+    }
+
+    private def giveUpWorkflow(pw: PacketHandler) {
+        try {
+            pw.drop()
+        } catch {
+            case e: Exception =>
+                log.error(e, "Failed to drop flow for {}", pw.packet)
+        } finally {
+            var dropped = 0
+            if (pw.cookie.isDefined) {
+                dropped = removePacket(pw.cookie.get).size
+                packetOut(dropped)
+            }
+            metrics.packetsDropped.mark(dropped + 1)
+        }
     }
 
     /**
@@ -226,9 +246,6 @@ class DeduplicationActor(
         log.debug("Packet with {} processed", pw.cookieStr)
         pw.cookie match {
             case Some(c) =>
-                // TODO: use the PacketContext to know if a simulation has
-                // been restarted or not, and conditionally call packetOut
-                packetOut()
                 val latency = (Clock.defaultClock().tick() -
                     pw.packet.getStartTimeNanos).toInt
                 metrics.packetsProcessed.mark()
@@ -262,6 +279,9 @@ class DeduplicationActor(
             }
         } catch {
             case ex: Exception => handleErrorOn(pw, ex)
+        } finally {
+            if (pw.runs == 1 && pw.cookie.isDefined)
+                packetOut(1)
         }
     }
 
@@ -284,7 +304,6 @@ class DeduplicationActor(
                 log.debug("A matching packet with cookie {} is already " +
                     "being handled", cookie)
                 cookieToPendedPackets.addBinding(cookie, packet)
-                metrics.pendedPackets.inc()
         }
     }
 

midolman/src/main/scala/org/midonet/midolman/WaitingRoom.scala
@@ -3,8 +3,7 @@
  */
 package org.midonet.midolman
 
-import scala.collection.mutable
-import scala.collection.mutable.ListBuffer
+import scala.collection.{immutable, mutable}
 import java.util.concurrent.TimeUnit
 
 /**
@@ -17,12 +16,9 @@ import java.util.concurrent.TimeUnit
  * This class is not thread safe, and all instances expected to be confined to
  * a thread.
  *
- * @param leave function to invoke with each group of waiters that tired
- *              of waiting
  * @param timeout timeout, in nanoseconds
  */
-class WaitingRoom[W](val leave: (Iterable[W] => Any), // tired of waiting
-                     val timeout: Long = TimeUnit.SECONDS.toNanos(3)) {
+class WaitingRoom[W](val timeout: Long = TimeUnit.SECONDS.toNanos(3)) {
 
     private[this] val waiters = new mutable.HashSet[W]()
     private[this] val timeouts = new mutable.ListBuffer[(W, Long)]()
@@ -38,29 +34,30 @@ class WaitingRoom[W](val leave: (Iterable[W] => Any), // tired of waiting
      * If the element is already in the waiting room, it will not be added again
      * with the *old* waiting time unaltered.
      */
-    def enter(w: W) {
-        doExpirations()
-        if (waiters contains w) {
-            return
+    def enter(w: W): IndexedSeq[W] = {
+        val evictions = doExpirations()
+        if (!(waiters contains w)) {
+            waiters += w
+            timeouts += ((w, System.nanoTime() + timeout))
         }
-
-        waiters += w
-        timeouts += ((w, System.nanoTime() + timeout))
+        evictions
     }
 
-    /* Finds waiters whose futures have not completed in time and kicks them out
-     * of the waiting room.
-     */
-    private def doExpirations() {
-        val evictions = ListBuffer[W]()
-        while(timeouts.nonEmpty && timeouts.head._2 < System.nanoTime()) {
+    private def doExpirations(): IndexedSeq[W] = {
+        var evictions: mutable.ArrayBuffer[W] = null
+        val now = System.nanoTime()
+        while (timeouts.nonEmpty && (now - timeouts.head._2) > 0) {
+            if (evictions == null)
+                evictions = mutable.ArrayBuffer()
+
             val w = (timeouts remove 0)._1
             evictions += w
             waiters -= w
         }
-        if (evictions.nonEmpty)
-            leave(evictions)
+
+        if (evictions == null)
+            immutable.Vector.empty
+        else
+            evictions
     }
-
 }
-

midolman/src/test/scala/org/midonet/midolman/WaitingRoomTest.scala
@@ -18,33 +18,33 @@ class WaitingRoomTest extends FeatureSpec with Matchers {
     feature("Add a waiter") {
 
         scenario("Construction") {
-            val wr = new WaitingRoom[Int](_.hashCode, to)
+            val wr = new WaitingRoom[Int](to)
             wr.timeout shouldEqual to
         }
 
         scenario("Construction with defaults") {
-            val wr = new WaitingRoom[Int](_.hashCode)
+            val wr = new WaitingRoom[Int]()
             wr.timeout shouldEqual TimeUnit.SECONDS.toNanos(3)
         }
 
         scenario("Each waiter triggers a cleanup of timed out waiters") {
-            val cb = new ListBuffer[Int]()
-            val wr = new WaitingRoom[Int](x => cb ++= x, to)
+            val evictions = new ListBuffer[Int]()
+            val wr = new WaitingRoom[Int](to)
             val w1 = 1
             val w2 = 2
             val w3 = 2
 
             // add elements and verify size
             wr.count should be (0)
-            wr enter w1
+            evictions ++= wr enter w1
             wr.count should be (1)
-            cb should have size 0 // no callbacks
+            evictions should have size 0 // no evictions
             wr enter w2
             wr.count should be (2)
 
             // duplicate elements
-            wr enter w1
-            wr enter w2
+            evictions ++= wr enter w1
+            evictions ++= wr enter w2
             wr.count should be (2) // now remain the same
 
             // wait for expirations
@@ -52,13 +52,11 @@ class WaitingRoomTest extends FeatureSpec with Matchers {
 
             // none should have happened since no elements were added
             wr.count should be (2)
-            cb should have size 0
+            evictions should have size 0
 
-            wr enter w3 // this expires, then adds
+            evictions ++= wr enter w3 // this expires, then adds
             wr.count should be (1)
-            cb shouldEqual List(w1, w2)
+            evictions shouldEqual List(w1, w2)
         }
-
     }
-
 }

midolman/src/main/scala/org/midonet/midolman/PacketWorkflow.scala
@@ -10,7 +10,6 @@ import java.util.concurrent.TimeUnit
 import scala.collection.JavaConversions._
 import scala.collection.JavaConverters._
 import scala.collection.{Set => ROSet, mutable}
-import scala.concurrent._
 import scala.reflect.ClassTag
 
 import akka.actor._
@@ -44,6 +43,7 @@ trait PacketHandler {
     val egressPort: Option[UUID]
     val cookieStr: String
     def idle: Boolean
+    def runs: Int
 
     override def toString = s"PacketWorkflow[$cookieStr]"
 }
@@ -125,10 +125,8 @@ abstract class PacketWorkflow(val deduplicator: ActorRef,
 
     val ERROR_CONDITION_HARD_EXPIRATION = 10000
 
-    private[this] var _idle = true
-    override def idle = _idle
-
-    private var runs = 0 // how many times it has been started
+    var idle = true
+    var runs = 0 // how many times it has been started
 
     override val cookie = cookieOrEgressPort match {
         case Left(c) => Some(c)
@@ -150,7 +148,7 @@ abstract class PacketWorkflow(val deduplicator: ActorRef,
     val lastInvalidation = FlowController.lastInvalidationEvent
 
     override def start(): Urgent[PipelinePath] = {
-        _idle = false
+        idle = false
         runs += 1
         log.debug("Initiating processing of packet {}, attempt: {}",
                   cookieStr, runs)
@@ -158,13 +156,13 @@ abstract class PacketWorkflow(val deduplicator: ActorRef,
             case Some(_) => handlePacketWithCookie()
             case None => doEgressPortSimulation()
         }) match {
-            case n@NotYet(_) => _idle = true; n
+            case n@NotYet(_) => idle = true; n
             case r => r
         }
     }
 
     override def drop() {
-        _idle = false
+        idle = false
         val wildFlow = WildcardFlow(wcmatch = wcMatch,
             hardExpirationMillis = ERROR_CONDITION_HARD_EXPIRATION)
         addTranslatedFlow(wildFlow, Set.empty, Nil)

midolman/src/main/scala/org/midonet/midolman/PacketsEntryPoint.scala
@@ -101,7 +101,7 @@ class PacketsEntryPoint extends Actor with ActorLogWithoutPath {
         val props = Props(classOf[DeduplicationActor],
                             cookieGen, dpConnPool, clusterDataClient,
                             connectionCache, traceMessageCache, traceIndexCache,
-                            metrics, () => counter.incAndGet(index))
+                            metrics, counter.addAndGet(index, _: Int))
                     .withDispatcher("actors.pinned-dispatcher")
 
         context.actorOf(props, s"PacketProcessor-$index")

midolman/src/main/scala/org/midonet/midolman/monitoring/metrics/PacketPipelineMetrics.scala
@@ -38,19 +38,16 @@ class PacketPipelineMetrics(val registry: MetricsRegistry) {
         classOf[PacketPipelineMeter],
         "packetsPostponed", "packets",
         TimeUnit.SECONDS)
+
     val packetsProcessed = registry.newMeter(
         classOf[PacketPipelineMeter],
         "packetsProcessed", "packets",
         TimeUnit.SECONDS)
 
-    // FIXME(guillermo)
-    //  - make this a meter
-    val packetsDropped = registry.newGauge(
+    val packetsDropped = registry.newMeter(
         classOf[PacketPipelineCounter],
-        "packetsDropped",
-        new Gauge[Long]{
-            override def value = 0
-        })
+        "packetsDropped", "packets",
+        TimeUnit.SECONDS)
 
     val liveSimulations = registry.newGauge(
         classOf[PacketPipelineGauge],

midolman/src/test/scala/org/midonet/midolman/DeduplicationActorTestCase.scala
@@ -1,6 +1,6 @@
 /*
-* Copyright 2013 Midokura Europe SARL
-*/
+ * Copyright (c) 2013 Midokura SARL, All Rights Reserved.
+ */
 package org.midonet.midolman
 
 import java.util.UUID
@@ -9,7 +9,6 @@ import scala.collection.immutable
 import scala.collection.mutable.ListBuffer
 import scala.concurrent.Promise
 import scala.concurrent.promise
-import scala.concurrent.duration._
 
 import akka.actor.Props
 import akka.testkit.TestActorRef
@@ -44,16 +43,25 @@ class DeduplicationActorTestCase extends FeatureSpec
     var packetsSeen = List[(Packet, Either[Int, UUID])]()
     var ddaRef: TestActorRef[TestableDDA] = _
     def dda = ddaRef.underlyingActor
+    var packetsOut = 0
 
     lazy val dpConnPool = injector.getInstance(classOf[DatapathConnectionPool])
     lazy val metricsReg = injector.getInstance(classOf[MetricsRegistry])
 
     override def beforeTest() {
         datapath = mockDpConn().futures.datapathsCreate("midonet").get()
+        createDda()
+    }
+
+    def createDda(simulationExpireMillis: Long = 5000L): Unit = {
+        if (ddaRef != null)
+            actorSystem.stop(ddaRef)
 
         val ddaFactory = () => new TestableDDA(new CookieGenerator(1, 1),
             dpConnPool, clusterDataClient(), null, null, null,
-            new PacketPipelineMetrics(metricsReg))
+            new PacketPipelineMetrics(metricsReg),
+            (x: Int) => { packetsOut += x },
+            simulationExpireMillis)
 
         ddaRef = TestActorRef(Props(ddaFactory))(actorSystem)
         dda should not be null
@@ -107,11 +115,11 @@ class DeduplicationActorTestCase extends FeatureSpec
             Then("the DDA should execute exactly one workflow")
             packetsSeen.length should be (1)
 
-            Then("and exactly one packet should be pended")
+            And("exactly one packet should be pended")
             dda.pendedPackets(1) should be (Some(Set(pkts.head)))
         }
 
-        scenario("discards packets When ApplyFlow has no actions") {
+        scenario("discards packets when ApplyFlow has no actions") {
             Given("three different packets with the same match")
             val pkts = List(makePacket(1), makeUniquePacket(1), makeUniquePacket(1))
 
@@ -126,11 +134,15 @@ class DeduplicationActorTestCase extends FeatureSpec
             ddaRef ! ApplyFlow(Nil, Some(1))
 
             Then("the packets should be dropped")
-            mockDpConn().packetsSent.size should be (0)
+            mockDpConn().packetsSent should be (empty)
             dda.pendedPackets(1) should be (None)
+
+
+            And("packetsOut should be called with the correct number")
+            packetsOut should be (3)
         }
 
-        scenario("emits packets When ApplyFlow contains actions") {
+        scenario("emits packets when ApplyFlow contains actions") {
             Given("three different packets with the same match")
             val pkts = List(makePacket(1), makeUniquePacket(1), makeUniquePacket(1))
 
@@ -151,6 +163,9 @@ class DeduplicationActorTestCase extends FeatureSpec
 
             And("no pended packets should remain")
             dda.pendedPackets(1) should be (None)
+
+            And("packetsOut should be called with the correct number")
+            packetsOut should be (3)
         }
 
         scenario("simulates sequences of packets from the datapath") {
@@ -185,6 +200,31 @@ class DeduplicationActorTestCase extends FeatureSpec
             packetsSeen map {
                 case (packet, uuid) => (packet.getPacket, uuid)
             } should be (List((frame, Right(id))))
+
+            And("packetsOut should not be called")
+            packetsOut should be (0)
+        }
+
+        scenario("expires packets") {
+            Given("a pending packet in the DDA")
+            createDda(0)
+            val pkts = List(makePacket(1), makePacket(1))
+            ddaRef ! DeduplicationActor.HandlePackets(pkts.toArray)
+            dda.pendedPackets(1) should not be None
+            dda.pendedPackets(1).get should have size 1
+            dda.pendedPackets(1).get.head should be (pkts(1))
+
+            When("putting another packet handler in the waiting room")
+            val pkt2 = makePacket(2)
+            ddaRef ! DeduplicationActor.HandlePackets(Array(pkt2))
+            dda.pendedPackets(2) should not be None
+            dda.pendedPackets(2).get should be (empty)
+
+            Then("the DDA will expire the first one")
+            dda.pendedPackets(1) should be (None)
+
+            And("packetsOut should be called with the correct number")
+            packetsOut should be (3)
         }
     }
 
@@ -192,9 +232,7 @@ class DeduplicationActorTestCase extends FeatureSpec
         val futures: ListBuffer[Promise[PacketWorkflow.PipelinePath]] = new ListBuffer()
 
         def complete() {
-            futures foreach {
-                f => f.success(Simulation)
-            }
+            futures foreach (_ success Simulation)
             futures.clear()
         }
     }
@@ -212,11 +250,12 @@ class DeduplicationActorTestCase extends FeatureSpec
             case Right(port) => Some(port)
         }
 
-        private var _idle = true
-        override def idle = _idle
+        var idle = true
+        var runs = 0
 
         override def start() = {
-            _idle = false
+            idle = false
+            runs += 1
             packetsSeen = packetsSeen :+ (packet, cookieOrEgressPort)
             val p = promise[PacketWorkflow.PipelinePath]()
             MockPacketHandler.futures.append(p)
@@ -224,7 +263,7 @@ class DeduplicationActorTestCase extends FeatureSpec
         }
 
         override def drop() {
-            _idle = false
+            idle = false
         }
     }
 
@@ -234,11 +273,13 @@ class DeduplicationActorTestCase extends FeatureSpec
                       cCache: Cache,
                       tmCache: Cache,
                       tiCache: Cache,
-                      metrics: PacketPipelineMetrics) extends
-              DeduplicationActor(cookieGen, dpConnPool, clusterDataClient,
-                cCache, tmCache, tiCache, metrics, () => { }) with MessageAccumulator {
-
-        override protected val simulationExpireMillis = 100L
+                      metrics: PacketPipelineMetrics,
+                      packetOut: Int => Unit,
+                      override val simulationExpireMillis: Long)
+            extends DeduplicationActor(cookieGen, dpConnPool, clusterDataClient,
+                                       cCache, tmCache, tiCache, metrics,
+                                       packetOut)
+            with MessageAccumulator {
 
         implicit override val dispatcher = this.context.dispatcher
 

commit 1d83c3fa3f4fba68655de0f1f1064e68aff0cb8c

Completed workflows leave the waiting room
Fix a regression introduced in 2c88c20d5579b1b81b90385c9fef7f9eb69f1331
Before, the giveUpWorkflows was skipping non-idle workflow returned
from the waiting room. This patch reintroduce a check to only give up
workflows that are not active, and log a warning msg otherwise. In
addition, when a workflow complete, it is removed from the waiting
room.

Fix MN-1517
Signed-off-by: hugobenichi <hugo@midokura.com>

Change-Id: I92c1ee53044ce524cd22a1b6fb2ba33c825560f1

midolman/src/main/scala/org/midonet/midolman/DeduplicationActor.scala
@@ -246,7 +246,12 @@ class DeduplicationActor(
     private def giveUpWorkflows(pws: IndexedSeq[PacketHandler]) {
         var i = 0
         while (i < pws.size) {
-            giveUpWorkflow(pws(i))
+            val workflow = pws(i)
+            if (workflow.idle)
+                giveUpWorkflow(workflow)
+            else
+                log.warning("Pending PacketWorkflow {} was scheduled " +
+                            "for cleanup but was not idle", workflow)
             i += 1
         }
     }
@@ -272,6 +277,7 @@ class DeduplicationActor(
      */
     private def complete(path: PipelinePath, pw: PacketHandler) {
         log.debug("Packet with {} processed", pw.cookieStr)
+        waitingRoom leave pw
         pw.cookie match {
             case Some(c) =>
                 applyFlow(c, pw)

midolman/src/main/scala/org/midonet/midolman/WaitingRoom.scala
@@ -43,16 +43,21 @@ class WaitingRoom[W](val timeout: Long = TimeUnit.SECONDS.toNanos(3)) {
         evictions
     }
 
+    def leave(w: W): Unit = {
+        waiters -= w
+    }
+
     private def doExpirations(): IndexedSeq[W] = {
         var evictions: mutable.ArrayBuffer[W] = null
         val now = System.nanoTime()
         while (timeouts.nonEmpty && (now - timeouts.head._2) > 0) {
-            if (evictions == null)
-                evictions = mutable.ArrayBuffer()
-
             val w = (timeouts remove 0)._1
-            evictions += w
-            waiters -= w
+            if (waiters contains w) {
+                if (evictions == null)
+                    evictions = mutable.ArrayBuffer()
+                evictions += w
+                waiters -= w
+            }
         }
 
         if (evictions == null)

midolman/src/test/scala/org/midonet/midolman/WaitingRoomTest.scala
@@ -32,7 +32,7 @@ class WaitingRoomTest extends FeatureSpec with Matchers {
             val wr = new WaitingRoom[Int](to)
             val w1 = 1
             val w2 = 2
-            val w3 = 2
+            val w3 = 3
 
             // add elements and verify size
             wr.count should be (0)
@@ -57,6 +57,15 @@ class WaitingRoomTest extends FeatureSpec with Matchers {
             evictions ++= wr enter w3 // this expires, then adds
             wr.count should be (1)
             evictions shouldEqual List(w1, w2)
+        }
+
+        scenario("A waiter leaving before timing out is not expired") {
+            val wr = new WaitingRoom[Int](to)
+            List(1,2,3) foreach { wr enter _ }
+            wr leave 2
+
+            Thread.sleep(TimeUnit.NANOSECONDS.toMillis(to))
+            (wr enter 4) should (contain(1) and contain(3) and not contain(2))
         }
     }
 }