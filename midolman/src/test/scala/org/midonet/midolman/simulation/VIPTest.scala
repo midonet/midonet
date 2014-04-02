ea31cb64006a60887facbcf68aab8d4765ab069e
Dave Cahill <dcahill@midokura.com>
2014-04-02 16:23:42 +0900

    Enforce TCP traffic only via VIP
    - Our L4LB spec specifies that VIPs only support TCP traffic, but
      UDP traffic was being allowed through
    - Added tests to check UDP traffic dropped, and made relevant change
    
    Change-Id: Id928f5af35abb7814d9978be1ca158c75167c988

--- midolman/src/test/scala/org/midonet/midolman/simulation/VIPTest.scala
+++ midolman/src/test/scala/org/midonet/midolman/simulation/VIPTest.scala
@@ -8,11 +8,11 @@ import java.util.UUID
 import compat.Platform
 
 import org.junit.runner.RunWith
-import org.midonet.midolman.MidolmanTestCase
 import org.scalatest.junit.JUnitRunner
 
 import org.midonet.cluster.data.l4lb
-import org.midonet.packets.IPv4Addr
+import org.midonet.midolman.MidolmanTestCase
+import org.midonet.packets.{TCP, UDP, IPv4Addr}
 import org.midonet.sdn.flows.WildcardMatch
 
 @RunWith(classOf[JUnitRunner])
@@ -27,27 +27,39 @@ class VIPTest extends MidolmanTestCase {
         val addr2 = IPv4Addr.fromString("10.0.0.2")
         val port2 = 44
 
-        val ingressMatch = new WildcardMatch()
-                .setNetworkDestination(addr1).setTransportDestination(port1)
-        val context = new PacketContext(None, null,
+        val tcpIngressMatch = new WildcardMatch()
+                .setNetworkDestination(addr1)
+                .setTransportDestination(port1)
+                .setNetworkProtocol(TCP.PROTOCOL_NUMBER)
+
+        val tcpContext = new PacketContext(None, null,
                                         Platform.currentTime + 10000, null,
-                                        null, null, true, None, ingressMatch)(actors())
+                                        null, null, true,
+                                        None, tcpIngressMatch)(actors())
+
+        val udpIngressMatch = tcpIngressMatch.clone().setNetworkProtocol(UDP.PROTOCOL_NUMBER)
+        val udpContext = new PacketContext(None, null,
+            Platform.currentTime + 10000, null,
+            null, null, true, None, udpIngressMatch)(actors())
 
         // Admin state up VIP with same addr / port should match
         val vip1Up = createVip(true, addr1, port1)
-        assert(vip1Up.matches(context))
+        assert(vip1Up.matches(tcpContext))
+
+        // Admin state up VIP with same addr / port should not match UDP context
+        assert(!vip1Up.matches(udpContext))
 
         // Admin state down VIP with same addr / port should not match
         val vip1Down = createVip(false, addr1, port1)
-        assert(!vip1Down.matches(context))
+        assert(!vip1Down.matches(tcpContext))
 
         // Admin state up VIP with diff addr / port should not match
         val vip2Up = createVip(true, addr2, port2)
-        assert(!vip2Up.matches(context))
+        assert(!vip2Up.matches(tcpContext))
 
         // Admin state down VIP with diff addr / port should not match
         val vip2Down = createVip(false, addr2, port2)
-        assert(!vip2Down.matches(context))
+        assert(!vip2Down.matches(tcpContext))
     }
 }
 
