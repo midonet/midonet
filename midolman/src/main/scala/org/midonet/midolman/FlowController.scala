midonet/midonet

commit 83e50baa1a8ac3f0dcfb0005c78a905d7665ca1c

Adds the WildcardMatch to the FlowAdded message
This patch enriches the FlowAdded message with the WildcardMatch so
that the FlowController doesn't need to recreate it. Also, it
eliminates the need to create a temporary WildcardMatch for the
query to the WildcardFlow table in the packet-in path.
Signed-off-by: Duarte Nunes <duarte@midokura.com>

Change-Id: I9fda3beed9420068b3ce85a07e7c06b6d9d20d87

midolman/src/main/scala/org/midonet/midolman/FlowController.scala
@@ -105,7 +105,7 @@ object FlowController extends Referenceable {
 
     case class InvalidateFlowsByTag(tag: Any)
 
-    case class FlowAdded(flow: Flow)
+    case class FlowAdded(flow: Flow, wcMatch: WildcardMatch)
 
     case class FlowUpdateCompleted(flow: Flow) // used in test only
 
@@ -157,9 +157,9 @@ object FlowController extends Referenceable {
             }
         }
 
-    def queryWildcardFlowTable(flowMatch: FlowMatch): Option[ManagedWildcardFlow] = {
+    def queryWildcardFlowTable(wildMatch: WildcardMatch)
+    : Option[ManagedWildcardFlow] = {
         var wildFlow: ManagedWildcardFlow = null
-        val wildMatch = WildcardMatch.fromFlowMatch(flowMatch)
 
         for (entry <- wildcardTables.entrySet()) {
             val table = entry.getValue
@@ -308,8 +308,8 @@ class FlowController extends Actor with ActorLogWithoutPath {
                 }
             }
 
-        case FlowAdded(dpFlow) =>
-            handleFlowAddedForExistingWildcard(dpFlow)
+        case FlowAdded(dpFlow, wcMatch) =>
+            handleFlowAddedForExistingWildcard(dpFlow, wcMatch)
             metrics.currentDpFlows = flowManager.getNumDpFlows
 
         case InvalidateFlowsByTag(tag) =>
@@ -391,8 +391,9 @@ class FlowController extends Actor with ActorLogWithoutPath {
         }
     }
 
-    private def handleFlowAddedForExistingWildcard(dpFlow: Flow) {
-        FlowController.queryWildcardFlowTable(dpFlow.getMatch) match {
+    private def handleFlowAddedForExistingWildcard(dpFlow: Flow,
+                                                   wcMatch: WildcardMatch) {
+        FlowController.queryWildcardFlowTable(wcMatch) match {
             case Some(wildFlow) =>
                 // the query doesn't miss, we don't care whether the returned
                 // wildcard flow is the same that the client has: the dp flow

midolman/src/main/scala/org/midonet/midolman/PacketWorkflow.scala
@@ -169,7 +169,7 @@ abstract class PacketWorkflow(protected val datapathConnection: OvsDatapathConne
                 log.debug("Successfully created flow for {}", cookieStr)
                 newWildFlow match {
                     case None =>
-                        FlowController ! FlowAdded(dpFlow)
+                        FlowController ! FlowAdded(dpFlow, wcMatch)
                     case Some(wf) =>
                         FlowController !
                                 AddWildcardFlow(wf, Some(dpFlow),
@@ -332,7 +332,7 @@ abstract class PacketWorkflow(protected val datapathConnection: OvsDatapathConne
         if (packet.getReason == Packet.Reason.FlowActionUserspace) {
             simulatePacketIn() flatMap processSimulationResult
         } else {
-            FlowController.queryWildcardFlowTable(packet.getMatch) match {
+            FlowController.queryWildcardFlowTable(wcMatch) match {
                 case Some(wildflow) =>
                   handleWildcardTableMatch(wildflow)
                 case None =>

commit ec45ea1f4ec6ab471b0c5c32fefd34553c4cb27a

Protects against changing WildCartMatch fieldsSeen
This patch fixes a regression introduced in
83e50baa1a8ac3f0dcfb0005c78a905d7665ca1c where querying the
WildcardFlowTable changes the fieldsSeen of the WildcardMatch passed
as an argument.

Ref: MN-1520
Signed-off-by: Duarte Nunes <duarte@midokura.com>
Change-Id: I4466b9cb19763cc173a1dda6ee0f3fcbb4f4e2ef

midolman/src/main/scala/org/midonet/midolman/FlowController.scala
@@ -162,7 +162,7 @@ object FlowController extends Referenceable {
     def queryWildcardFlowTable(wildMatch: WildcardMatch)
     : Option[ManagedWildcardFlow] = {
         var wildFlow: ManagedWildcardFlow = null
-
+        wildMatch.doNotTrackSeenFields()
         for (entry <- wildcardTables.entrySet()) {
             val table = entry.getValue
             val pattern = entry.getKey
@@ -177,7 +177,7 @@ object FlowController extends Referenceable {
                 }
             }
         }
-
+        wildMatch.doTrackSeenFields()
         Option(wildFlow)
     }
 