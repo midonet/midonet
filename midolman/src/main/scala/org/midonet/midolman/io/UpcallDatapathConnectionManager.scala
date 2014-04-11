f20f26f511e4ab631f34fee441f6408537b7d765
hugo <hugo@midokura.jp>
2014-04-11 11:35:38 +0900

    Fix NetlinkChannel leak when port creation fails
    when using the UpcallDatapathConnectionManager.
    
    Change-Id: Icc9bfc64ad46b077695ec2a8a7d677a7e5e8b7b5
    Signed-off-by: hugobenichi <hugo@midokura.com>

--- midolman/src/main/scala/org/midonet/midolman/io/UpcallDatapathConnectionManager.scala
+++ midolman/src/main/scala/org/midonet/midolman/io/UpcallDatapathConnectionManager.scala
@@ -66,15 +66,15 @@ abstract class UpcallDatapathConnectionManager(val config: MidolmanConfig,
             val cbDispatcher = NetlinkCallbackDispatcher.makeBatchCollector()
             dpConn setCallbackDispatcher cbDispatcher
             setUpcallHandler(dpConn, workers)
-            ensurePortPid(port, datapath, dpConn) andThen {
-                case Success((createdPort, _)) =>
-                    val kv = ((datapath, createdPort.getPortNo.intValue), conn)
-                    portToChannel += kv
-                case Failure(e) =>
-                    log.error("failed to create or retrieve datapath port "
-                              + port.getName, e)
-                    conn.stop()
-            }
+            ensurePortPid(port, datapath, dpConn)
+        } andThen {
+            case Success((createdPort, _)) =>
+                val kv = ((datapath, createdPort.getPortNo.intValue), conn)
+                portToChannel += kv
+            case Failure(e) =>
+                log.error("failed to create or retrieve datapath port "
+                          + port.getName, e)
+                conn.stop()
         }
     }
 
