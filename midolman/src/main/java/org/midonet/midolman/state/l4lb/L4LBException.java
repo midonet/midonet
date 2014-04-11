7ef1f2e5088b438c507c0435c851913bdea157e1
Taku Fukushima <tfukushima@midokura.com>
2014-04-11 19:05:46 +0900

    Force Pool-HealthMonitor mapping to be disassociated before association
    The current API implementation allows users to put a health monitor ID
    even if another health monitor is associated with a pool. This behaviour
    is inconsistent with LBaaS and causes race conditions where the state
    mutations on the association (`PENDING_CREATE` -> `ACTIVE`) and the
    disassociation (`PENDING_DELETE` -> `INACTIVE`) would compete with each
    other.
    
    So this patch modifes the API to return an error response, 400 Bad
    Request, when the pool and the health monitor is already mapped and
    users are trying to put another health monitor ID in the pool's
    `healthMonitorId`. The users need to put null to disassociate the
    mapping first, then another request where the ID of another health
    monitor is populated. If `null` is set to `healthMonitorId` and the
    pool's `mappingStaus` is `PENDING_*`, it throws
    `MappingStatusException` and users get 503 Service Unavailable.
    
    This patch also enables disabled tests to validate this behaviour.
    
    This fixes MN-1566, MN-1568
    
    Change-Id: I335bfa161676adc40ca5dfdcb52e97ca007e4939
    Signed-off-by: Taku Fukushima <tfukushima@midokura.com>

---
+++ midolman/src/main/java/org/midonet/midolman/state/l4lb/L4LBException.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2014 Midokura Europe SARL, All Rights Reserved.
+ */
+
+package org.midonet.midolman.state.l4lb;
+
+/**
+ * An common exception among the L4LB related exception
+ */
+public class L4LBException extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    public L4LBException() {
+        super();
+    }
+
+    public L4LBException(String message) {
+        super(message);
+    }
+
+    public L4LBException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public L4LBException(Throwable cause) {
+        super(cause);
+    }
+}
