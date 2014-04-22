7b0fa2929a4b3ee97332693ea68d92644f63142a
Ryu Ishimoto <ryu@midokura.com>
2014-04-22 23:55:47 +0900

    Neutron DTO classes
    New Neutron data classes needed to implement Neutron API.  These
    classes are purposely kept simple in case they are replaced by
    auto-generated code in the future.
    
    Change-Id: I43721b380d29117e5e6fb937b05b6f4ee31f88de
    Signed-off-by: Ryu Ishimoto <ryu@midokura.com>

---
+++ cluster/src/main/java/org/midonet/cluster/data/neutron/Port.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright (c) 2014 Midokura Europe SARL, All Rights Reserved.
+ */
+package org.midonet.cluster.data.neutron;
+
+import com.google.common.base.Objects;
+import org.apache.commons.collections4.ListUtils;
+import org.codehaus.jackson.annotate.JsonProperty;
+import org.midonet.util.collection.ListUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class Port {
+
+    public UUID id;
+
+    public String name;
+
+    @JsonProperty("network_id")
+    public UUID networkId;
+
+    @JsonProperty("admin_state_up")
+    public boolean adminStateUp;
+
+    @JsonProperty("mac_address")
+    public String macAddress;
+
+    @JsonProperty("fixed_ips")
+    public List<IPAllocation> fixedIps = new ArrayList<>();
+
+    @JsonProperty("device_id")
+    public String deviceId;
+
+    @JsonProperty("device_owner")
+    public String deviceOwner;
+
+    @JsonProperty("tenant_id")
+    public String tenantId;
+
+    public String status;
+
+    @JsonProperty("security_groups")
+    public List<UUID> securityGroups = new ArrayList<>();
+
+    @Override
+    public boolean equals(Object obj) {
+
+        if (obj == this) return true;
+
+        if (!(obj instanceof Port)) return false;
+        final Port other = (Port) obj;
+
+        return Objects.equal(id, other.id)
+                && Objects.equal(name, other.name)
+                && Objects.equal(networkId, other.networkId)
+                && Objects.equal(adminStateUp, other.adminStateUp)
+                && Objects.equal(macAddress, other.macAddress)
+                && Objects.equal(deviceId, other.deviceId)
+                && Objects.equal(deviceOwner, other.deviceOwner)
+                && Objects.equal(tenantId, other.tenantId)
+                && Objects.equal(status, other.status)
+                && ListUtils.isEqualList(fixedIps, other.fixedIps)
+                && ListUtils.isEqualList(securityGroups, other.securityGroups);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id, name, networkId, adminStateUp, macAddress,
+                deviceId, deviceOwner, tenantId, status,
+                ListUtils.hashCodeForList(fixedIps),
+                ListUtils.hashCodeForList(securityGroups));
+    }
+
+    @Override
+    public String toString() {
+
+        return Objects.toStringHelper(this)
+                .add("id", id)
+                .add("name", name)
+                .add("networkId", networkId)
+                .add("adminStateUp", adminStateUp)
+                .add("macAddress", macAddress)
+                .add("deviceId", deviceId)
+                .add("deviceOwner", deviceOwner)
+                .add("tenantId", tenantId)
+                .add("status", status)
+                .add("fixedIps", ListUtil.toString(fixedIps))
+                .add("securityGroups", ListUtil.toString(securityGroups))
+                .toString();
+    }
+}
