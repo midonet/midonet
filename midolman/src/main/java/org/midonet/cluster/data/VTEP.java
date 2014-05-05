2778cbc80d0e6b2196cb04763582e5cf51022ba3
Galo Navarro <galo@midokura.com>
2014-05-05 12:37:18 +0200

    Add tunnel zone id to VTEP resource
    This tunnel zone will be used to decide which of the host's IP is to be
    used as VxLAN tunnel endpoint. Tunnel zone deletions will fail if there
    is a VTEP configured with the same tunnel zone.
    
    TestTunnelZone is *not* updated because we still don't have the vtep
    client mock that allows creating vteps in tests without actually
    connecting to the switch. A later patch will add that mock, and the
    updated tunnel zone test.
    
    Change-Id: I9d53a3190874ecb9d1d63d9fdcc0744948b3ab1d
    Signed-off-by: Galo Navarro <galo@midokura.com>

--- midolman/src/main/java/org/midonet/cluster/data/VTEP.java
+++ midolman/src/main/java/org/midonet/cluster/data/VTEP.java
@@ -3,6 +3,9 @@
  */
 package org.midonet.cluster.data;
 
+import java.util.Objects;
+import java.util.UUID;
+
 import org.midonet.packets.IPv4Addr;
 
 public class VTEP extends Entity.Base<IPv4Addr, VTEP.Data, VTEP> {
@@ -20,13 +23,23 @@ public class VTEP extends Entity.Base<IPv4Addr, VTEP.Data, VTEP> {
         return getData().mgmtPort;
     }
 
+    public UUID getTunnelZoneId() {
+        return getData().tunnelZoneId;
+    }
+
     public VTEP setMgmtPort(int mgmtPort) {
         getData().mgmtPort = mgmtPort;
         return self();
     }
 
+    public VTEP setTunnelZone(UUID tunnelZone) {
+        getData().tunnelZoneId = tunnelZone;
+        return self();
+    }
+
     public static class Data {
         public int mgmtPort;
+        public UUID tunnelZoneId;
 
         @Override
         public boolean equals(Object o) {
@@ -34,17 +47,18 @@ public class VTEP extends Entity.Base<IPv4Addr, VTEP.Data, VTEP> {
             if (o == null || getClass() != o.getClass()) return false;
 
             Data data = (Data) o;
-
-            return mgmtPort == data.mgmtPort;
+            return Objects.equals(mgmtPort, data.mgmtPort) &&
+                   Objects.equals(tunnelZoneId, data.tunnelZoneId);
         }
 
         @Override
         public int hashCode() {
-            return mgmtPort;
+            return Objects.hash(mgmtPort, tunnelZoneId);
         }
 
         public String toString() {
-            return "VTEP.Data{mgmtPort=" + mgmtPort +"}";
+            return "VTEP.Data{mgmtPort=" + mgmtPort +
+                   ", tunnelZoneId=" + tunnelZoneId + "}";
         }
     }
 }
