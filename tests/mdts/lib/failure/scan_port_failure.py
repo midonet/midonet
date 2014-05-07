cf6c7ab1a9859e8a5af27339dd59ac4107ef9e71
Takayuki Usui <takayuki@midokura.com>
2014-05-07 14:43:49 +0900

    Add preliminary functional tests for HTB
    This requires I7c99f2ce0b42bb7ffab5897b636728645b6768fe
    
    Change-Id: I032850a98e647d6736ea3795bddb44a019cf5084
    Signed-off-by: Takayuki Usui <takayuki@midokura.com>

---
+++ tests/mdts/lib/failure/scan_port_failure.py
@@ -0,0 +1,49 @@
+import logging
+import subprocess
+
+from mdts.lib.failure.failure_base import FailureBase
+
+LOG = logging.getLogger(__name__)
+
+class ScanPortFailure(FailureBase):
+    """Emulate port scan using mz
+
+    @netns      network namespace name
+    @interface  interface name
+    @src        src in tuple (host, port range)
+    @dst        dst in tuple (host, port range)
+                * range can be any string mz accepts,
+                  for instance, '1-65535'
+    @delay      any string mz accepts; in usec by default
+                * See -d in mz(1)
+
+    NOTE: work in progress
+    """
+    def __init__(self, netns, interface, src, dst, delay = '0', count = 0):
+        super(ScanPortFailure, self)\
+            .__init__("scan_port_failure %s %s %s %s %s %d" \
+                          % (netns, interface, src, dst, delay, count))
+        self._netns = netns
+        self._interface = interface
+        self._proto = 'udp'
+        self._src = src
+        self._dst = dst
+        self._delay = delay
+        self._count = count
+
+    def inject(self):
+        cmdline = ['ip', 'netns', 'exec',
+                   self._netns,
+                   'mz',
+                   self._interface,
+                   '-c', '%d' % self._count,
+                   '-d', self._delay,
+                   '-A', self._src[0],
+                   '-B', self._dst[0],
+                   '-t', self._proto,
+                   'sp=%s,dp=%s' % (self._src[1], self._dst[1])]
+        LOG.debug('running %r' % (cmdline,))
+        self._process = subprocess.Popen(cmdline, stdout=subprocess.PIPE)
+
+    def eject(self):
+        self._process.kill()
