c76cc307edde0d36ced3847f157c79755c79a096
Joe Mills <joe@midokura.com>
2014-05-09 14:13:25 +0900

    Add MockDirectory utils to view data
    * This commit adds a few util functions to the MockDirectory class. These help
      in whitebox testing, for example if we want to make sure that after a
      number of resource adds, the associated deletes properly clean up
      everything.
    
    Change-Id: I62df7a26c38320b6b0ea06f7bf42e762d1a0f694
    Signed-off-by: Joe Mills <joe@midokura.com>

---
+++ midolman/src/main/java/org/midonet/midolman/state/CheckpointedDirectory.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2014 Midokura PTE LTD.
+ */
+package org.midonet.midolman.state;
+
+import java.util.Map;
+
+public interface CheckpointedDirectory extends Directory {
+
+    /**
+     * Creates a new checkpoint at the root of the Directory.
+     *
+     * @return The id of the checkpoint.
+     */
+    int createCheckPoint();
+
+    /**
+     * Creates a new checkpoint at the given path of the Directory.
+     *
+     * @param path path where we want the checkpoint to apply. The checkpoint
+     *             will only store node/values that are under this path.
+     * @return The id of the checkpoint.
+     */
+    int createCheckPoint(String path);
+
+    /**
+     * Checks if the 2 provided checkpoint indexes refer to checkpoints that
+     * are equivalent. This means the paths are the same, and the data at those
+     * paths are the same.
+     *
+     * @param cpIndex1 index of the first checkpoint.
+     * @param cpIndex2 index of the second checkpoint.
+     * @return true if the checkpoints are equivalent, false otherwise.
+     */
+    boolean checkpointsAreEquivalent(int cpIndex1, int cpIndex2);
+
+    /**
+     * returns a map of all of the added paths (paths present in checkpoint 2
+     * that are not present in checkpoint 1.
+     *
+     * @param cpIndex1 index of the first checkpoint.
+     * @param cpIndex2 index of the second checkpoint.
+     * @return map of added paths to data.
+     */
+    Map<String, String> getAddedPaths(int cpIndex1, int cpIndex2);
+
+    /**
+     * returns a map of all of the removed paths (paths present in checkpoint 1
+     * that are not present in checkpoint 2.
+     *
+     * @param cpIndex1 index of the first checkpoint.
+     * @param cpIndex2 index of the second checkpoint.
+     * @return map of removed paths to data.
+     */
+    Map<String, String> getRemovedPaths(int cpIndex1, int cpIndex2);
+
+    /**
+     * returns a map of all of the modified paths (paths present in checkpoint 1
+     * and checkpoint 2 where the data is different.
+     *
+     * @param cpIndex1 index of the first checkpoint.
+     * @param cpIndex2 index of the second checkpoint.
+     * @return map of removed paths to data.
+     */
+    Map<String, String> getModifiedPaths(int cpIndex1, int cpIndex2);
+}
