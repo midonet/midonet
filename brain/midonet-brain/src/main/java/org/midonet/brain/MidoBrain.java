95028f4422352026bf2c706a7aab93a5f8742648
Galo Navarro <galo@midokura.com>
2014-05-19 18:48:52 +0200

    Add MidoBrain to run standalone controller
    A very simple class that allows running the VxGW service as a standalone
    java app. It simply instantiates a process with a VxlanGatewayService
    inside, and starts it up hooking configured vteps and mido bridges.
    
    Change-Id: Ib230d74658a1359190f2654bd804fb838c471e35
    Signed-off-by: Galo Navarro <galo@midokura.com>

---
+++ brain/midonet-brain/src/main/java/org/midonet/brain/MidoBrain.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2014 Midokura Europe SARL, All Rights Reserved.
+ */
+package org.midonet.brain;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import org.midonet.brain.services.vxgw.VxLanGatewayService;
+import org.midonet.cluster.services.MidostoreSetupService;
+import org.midonet.midolman.guice.CacheModule;
+import org.midonet.midolman.guice.cluster.ClusterClientModule;
+import org.midonet.midolman.guice.config.ConfigProviderModule;
+import org.midonet.midolman.guice.serialization.SerializationModule;
+import org.midonet.midolman.guice.zookeeper.ZookeeperConnectionModule;
+import org.midonet.midolman.version.guice.VersionModule;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This is the main application that starts a MidoBrain process running all
+ * services configured.
+ */
+public class MidoBrain {
+
+    private static final Logger log = LoggerFactory.getLogger(MidoBrain.class);
+
+    private Injector injector;
+
+    private MidoBrain() {}
+
+    private void doServicesCleanup() {}
+
+    private void run(String configFile) {
+        log.info("Adding shutdownHook");
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                doServicesCleanup();
+            }
+        });
+
+        injector = Guice.createInjector(
+            new ConfigProviderModule(configFile),
+            new MidoBrainModule(),
+            new ZookeeperConnectionModule(),
+            new VersionModule(),
+            new CacheModule(),
+            new ClusterClientModule(),
+            new SerializationModule()
+        );
+
+        injector.getInstance(MidostoreSetupService.class).startAndWait();
+        injector.getInstance(VxLanGatewayService.class).startAndWait();
+
+        log.info("Brain initialized");
+
+    }
+
+    private static class MidoBrainHolder {
+        private static final MidoBrain instance = new MidoBrain();
+    }
+    public static MidoBrain getInstance() { return MidoBrainHolder.instance; }
+    public static Injector getInjector() { return getInstance().injector; }
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            log.error("No configuration file provided");
+            System.exit(-1);
+        }
+        try {
+            MidoBrain brain = getInstance();
+            brain.run(args[0]); // TODO: use a default location
+        } catch (Exception e) {
+            log.error("Failed starting", e);
+            System.exit(-1);
+        }
+
+    }
+}
---
+++ brain/midonet-brain/src/main/java/org/midonet/brain/configuration/MidoBrainConfig.java
@@ -0,0 +1,19 @@
+package org.midonet.brain.configuration;
+/*
+ * Copyright (c) 2014 Midokura Europe SARL, All Rights Reserved.
+ */
+
+import org.midonet.config.ConfigBool;
+import org.midonet.config.ConfigGroup;
+import org.midonet.midolman.config.CassandraConfig;
+import org.midonet.midolman.config.ZookeeperConfig;
+
+@ConfigGroup(MidoBrainConfig.GROUP_NAME)
+public interface MidoBrainConfig
+       extends ZookeeperConfig, CassandraConfig {
+
+       public final static String GROUP_NAME = "midobrain";
+
+        @ConfigBool(key = "vxgw_enabled", defaultValue = false)
+        public boolean getVxGwEnabled();
+}
