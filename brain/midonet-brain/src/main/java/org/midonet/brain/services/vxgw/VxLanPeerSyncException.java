3f578bdce9ec167613b6f8cbc75a9cd661288928
Galo Navarro <galo@midokura.com>
2014-05-20 17:45:41 +0200

    Add basic pieces of a vxgw service
    These are the basic classes that define a VxlanGatewayService. It is
    composed of VxLanPeers that abstract a particular implementation (e.g.:
    a physical switch, or a Midonet cloud), offering an rxJava Observable of
    updates on the local macs, and allowing to apply updates on remote macs
    reported by other VxLanPeers.
    
    This commit also adds rxJava as a new dependency in the brain project,
    which is used for observable streams exposed by VxLanPeers.
    
    Ref. MN-1636
    Signed-off-by: Galo Navarro <galo@midokura.com>
    
    Change-Id: I6c81c5bbf809b7bccf82e856df2cc3afd0583d5e

---
+++ brain/midonet-brain/src/main/java/org/midonet/brain/services/vxgw/VxLanPeerSyncException.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2014 Midokura Europe SARL, All Rights Reserved.
+ */
+package org.midonet.brain.services.vxgw;
+
+/**
+ * An update to a VxLanPeer could not be applied.
+ */
+public class VxLanPeerSyncException extends RuntimeException {
+    private static final long serialVersionUID = -1;
+    private final MacLocation change;
+
+    public VxLanPeerSyncException(String msg, MacLocation change) {
+        super(msg);
+        this.change = change;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Failed to apply %s, %s",
+                             change.toString(), this.getMessage());
+    }
+}
