47b9e2391a3e55e2a7457130385165cfb24c2b90
Galo Navarro <galo@midokura.com>
2014-05-20 18:33:32 +0200

    Add a VxLanGatewayService
    A very simple skeleton of the service that loads the vxlan bridges,
    hooks the VxLanPeers for them and the configured VTEPs and starts
    syncing.
    
    Ref. MN-1636
    Change-Id: I4b8c62b0a5b2f43b7fc8321d8c77659d22c777f1
    Signed-off-by: Galo Navarro <galo@midokura.com>

--- brain/midonet-brain/src/main/java/org/midonet/brain/guice/BrainModule.java
+++ brain/midonet-brain/src/main/java/org/midonet/brain/guice/BrainModule.java
@@ -4,16 +4,15 @@
 package org.midonet.brain.guice;
 
 import com.google.inject.PrivateModule;
-
 import org.midonet.config.ConfigProvider;
 
 /**
- * Brain module configuration.
+ * Brain module configuration. This should declare top level services that must
+ * be started up with the controller, and their dependencies.
  */
 public class BrainModule extends PrivateModule {
     @Override
     protected void configure() {
-
         requireBinding(ConfigProvider.class);
     }
 }
