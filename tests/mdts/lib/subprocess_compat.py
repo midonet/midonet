401e8edc16549d40f5b68ae5ff785c47e977ec7a
Takayuki Usui <takayuki@midokura.com>
2014-05-26 17:49:28 +0900

    Log stdout/stderr of failing commands
    Rule of thumb: Use subprocess_compat.check_output whenever
    you need to call an external command.
    
    This patch replaces all subprocess.check_call with
    subprocess_compat.check_output, too. subprocess.check_call
    does not return any output.
    
    Since RHEL uses python 2.6.3 by default, we have to maintain
    compatibilities through 2.6 to 2.7:
    
    1. subprocess.check_output is not available in 2.6. Use our
    wrapper function, subprocess_compat.check_output, instead.
    
    2. subprocess.CalledProcessError does not have the output
    parameter in 2.6. This patch encodes stdout and stderr as part
    of the cmd parameter.
    
    Fix: https://midobugs.atlassian.net/browse/QA-178
    
    Change-Id: Iba535c70e89cc21d6febfe20254ba34dafac9382
    Signed-off-by: Takayuki Usui <takayuki@midokura.com>

--- tests/mdts/lib/subprocess_compat.py
+++ tests/mdts/lib/subprocess_compat.py
@@ -4,12 +4,14 @@ import subprocess
 def check_output(*popenargs, **kwargs):
     if 'stdout' in kwargs:
         raise ValueError('stdout argument not allowed, it will be overridden.')
+    if not 'stderr' in kwargs:
+        kwargs['stderr'] = subprocess.PIPE
     process = subprocess.Popen(stdout=subprocess.PIPE, *popenargs, **kwargs)
-    output, unused_err = process.communicate()
+    stdout, stderr = process.communicate()
     retcode = process.poll()
     if retcode:
         cmd = kwargs.get("args")
         if cmd is None:
             cmd = popenargs[0]
-        raise subprocess.CalledProcessError(retcode, cmd)
-    return output
+        raise subprocess.CalledProcessError(retcode, str((cmd, stdout, stderr)))
+    return stdout
