33b17b8b6cf83828da53ca063a4ba84f72dd5c71
Galo Navarro <galo@midokura.com>
2014-06-04 17:10:58 +0200

    Enhancements and unit tests for VtepDataClient
    This contains unit test for most methods in the VtepDataClientImpl,
    leaving out some cases that are pretty much boilerplate. It also
    refactor the Guice configuration and introduces a couple of injectors
    inside the VtepDataClientImpl to allow mocking from the unit test, and
    also follow Guice's conventions a bit better.
    
    Also refactored the VtepDataClientProvider moving the real
    implementation to the brain module so that it can be used from there
    without needing a dependency in the API.
    
    Change-Id: I3edef51b56ff54296ce012e2d68dc5cbb28c3ecd
    Fixes: MN-1814
    Signed-off-by: Galo Navarro <galo@midokura.com>

--- brain/midonet-brain/src/main/java/org/midonet/brain/guice/BrainModule.java
+++ brain/midonet-brain/src/main/java/org/midonet/brain/guice/BrainModule.java
@@ -4,6 +4,13 @@
 package org.midonet.brain.guice;
 
 import com.google.inject.PrivateModule;
+import com.google.inject.Provider;
+
+import org.opendaylight.ovsdb.plugin.ConfigurationService;
+import org.opendaylight.ovsdb.plugin.ConnectionService;
+import org.opendaylight.ovsdb.plugin.InventoryService;
+
+import org.midonet.brain.southbound.vtep.VtepDataClient;
 import org.midonet.config.ConfigProvider;
 
 /**
@@ -14,5 +21,30 @@ public class BrainModule extends PrivateModule {
     @Override
     protected void configure() {
         requireBinding(ConfigProvider.class);
+        requireBinding(ConfigurationService.class);
+        requireBinding(ConnectionService.class);
+        requireBinding(InventoryService.class);
+        requireBinding(VtepDataClient.class);
+    }
+
+    public class InventoryServiceProvider implements Provider<InventoryService> {
+        @Override
+        public InventoryService get() {
+            return new InventoryService();
+        }
+    }
+
+    public class ConnectionServiceProvider implements Provider<ConnectionService> {
+        @Override
+        public ConnectionService get() {
+            return new ConnectionService();
+        }
+    }
+
+    public class ConfigurationServiceProvider implements Provider<ConfigurationService> {
+        @Override
+        public ConfigurationService get() {
+            return new ConfigurationService();
+        }
     }
 }
