0e35cac16e22d9dc597924fa2c85381043d40ccc
Alex Bikfalvi <alex.bikfalvi@midokura.com>
2014-06-25 11:40:43 +0200

    Licenses initialized after starting the REST API
    The patch-set creates a new license service that is initialized
    after the REST API. The service reads the current list of
    licenses from ZooKeeper upon start and saves the licenses
    to ZooKeeper when stopped.
    
    This ensures the licenses are initialized after the environment
    setup that creates the directory structure in ZooKeeper.
    
    Fixes: MN-2424
    
    Change-Id: If0238a7986163aa155d0fdcd1583f53429c19657
    Signed-off-by: Alex Bikfalvi <alex.bikfalvi@midokura.com>

--- midolman/src/main/java/org/midonet/midolman/Setup.java
+++ midolman/src/main/java/org/midonet/midolman/Setup.java
@@ -98,6 +98,7 @@ public class Setup {
         paths.add(pathMgr.getVtepsPath());
         paths.add(pathMgr.getVxLanPortIdsPath());
         paths.add(pathMgr.getLocksPath());
+        paths.add(pathMgr.getLicensesPath());
 
         // Neutron paths
         paths.add(pathMgr.getNeutronPath());
--- midonet-api/src/main/java/org/midonet/api/rest_api/RestApiModule.java
+++ midonet-api/src/main/java/org/midonet/api/rest_api/RestApiModule.java
@@ -1,16 +1,18 @@
 /*
- * Copyright 2012 Midokura PTE LTD.
+ * Copyright (c) 2012 Midokura SARL, All Rights Reserved.
  */
 package org.midonet.api.rest_api;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.assistedinject.FactoryModuleBuilder;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.midonet.api.license.LicenseManager;
 import org.midonet.api.vtep.VtepDataClientProvider;
 import org.midonet.config.ConfigProvider;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * Guice module for REST API.
