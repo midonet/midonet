midonet/midonet

commit 0d51c991ee73f4c45941d993bdbe1599af7cdde6

Increased range for BGP pairs IPs
BGP pairs in RoutingHandler had previously 1 byte of possible values
with one pair taking 4 values, resulting in only 64 possible pairs.
To decrease the risk of overflow, BGP pairs can now use two bytes,
allowing 16384 possible pairs.

Issue #727
Signed-off-by: hugobenichi <hugo@midokura.com>
Change-Id: I44fa43dd6076ec4a0c4a3eaf11f9f59221d1f726

midolman/src/main/scala/org/midonet/midolman/routingprotocols/RoutingHandler.scala
@@ -34,6 +34,12 @@ import org.midonet.util.eventloop.SelectLoop
 import org.midonet.netlink.AfUnix
 
 object RoutingHandler {
+
+    /** A conventional value for Ip prefix of BGP pairs.
+     *  173 is the MS byte value and 23 the second byte value.
+     *  Last 2 LS bytes are available for assigning BGP pairs. */
+    val BGP_IP_PREFIX = 173 * (1<<24) + 23 * (1<<16)
+
     // BgpdProcess will notify via these messages
     case object BGPD_READY
     case object BGPD_DEAD
@@ -106,9 +112,9 @@ class RoutingHandler(var rport: ExteriorRouterPort, val bgpIdx: Int,
     private final val BGP_VTY_BRIDGE_NAME: String =
         "mbgp%d_br".format(bgpIdx)
     private final val BGP_VTY_LOCAL_IP: String =
-        "172.23.0.%d".format((bgpIdx * 4) + 1)
+        IPv4Addr.intToIpStr(BGP_IP_PREFIX + 1 + 4 * bgpIdx)
     private final val BGP_VTY_MIRROR_IP: String =
-        "172.23.0.%d".format((bgpIdx * 4) + 2)
+        IPv4Addr.intToIpStr(BGP_IP_PREFIX + 2 + 4 * bgpIdx)
     private final val BGP_VTY_MASK_LEN: Int = 30
     private final val BGP_NETWORK_NAMESPACE: String =
         "mbgp%d_ns".format(bgpIdx)

packets/src/main/scala/org/midonet/packets/IPv4Addr.scala
@@ -19,12 +19,9 @@ class IPv4Addr(val addr: Int) extends IPAddr with Ordered[IPv4Addr] {
     def this() = this(0)
 
     override def toUrlString = toString()
+
     @JsonValue
-    override def toString = { "%d.%d.%d.%d" format ((addr >> 24) & 0xff,
-                                                    (addr >> 16) & 0xff,
-                                                    (addr >> 8) & 0xff,
-                                                    (addr >> 0) & 0xff)
-    }
+    override def toString = IPv4Addr.intToIpStr(addr)
 
     // See "Programming in Scala" sec. 30.4
     override def equals(o: Any): Boolean = {
@@ -58,19 +55,19 @@ class IPv4Addr(val addr: Int) extends IPAddr with Ordered[IPv4Addr] {
 
 object IPv4Addr {
 
+    def apply(s: String): IPv4Addr = fromString(s)
+
+    def apply(i: Int): IPv4Addr = fromInt(i)
+
     @JsonCreator
-    def fromString(s: String): IPv4Addr = {
-        val i: Int = Net.convertStringAddressToInt(s)
-        new IPv4Addr(i)
-    }
+    def fromString(s: String): IPv4Addr =
+        new IPv4Addr(Net.convertStringAddressToInt(s))
 
-    def fromInt(i: Int): IPv4Addr = {
+    def fromInt(i: Int): IPv4Addr =
         new IPv4Addr(i)
-    }
 
-    def fromIntIPv4(ii: IntIPv4): IPv4Addr = {
+    def fromIntIPv4(ii: IntIPv4): IPv4Addr =
         new IPv4Addr(ii.addressAsInt)
-    }
 
     def fromBytes(addr: Array[Byte]) = {
         if (addr.length != 4)
@@ -82,9 +79,13 @@ object IPv4Addr {
                      ((addr(3) <<  0) & 0x000000FF))
     }
 
-    def fromIPv4(ipv4: IPv4Addr) = {
-        new IPv4Addr(ipv4.addr)
-    }
+    def fromIPv4(ipv4: IPv4Addr) = ipv4
+
+    def intToIpStr(addr: Int): String =
+        "%d.%d.%d.%d" format ((addr >> 24) & 0xff,
+                              (addr >> 16) & 0xff,
+                              (addr >> 8) & 0xff,
+                              (addr >> 0) & 0xff)
 
 }
 

commit e30dc0d88657150d32517beb4e7056bcf0876cc4

RoutingHandler: fix typo in IP address
Commit 0d51c99 introduced a typo in the IP addresses used for BGP that turned
them into public ones.

Change-Id: If7f1f2f6b9a8caf0307c0f8e002e89bdd8de2981
Fixes: MN-1917
Signed-off-by: Guillermo Ontanon <guillermo@midokura.jp>

midolman/src/main/scala/org/midonet/midolman/routingprotocols/RoutingHandler.scala
@@ -37,9 +37,9 @@ import org.midonet.midolman.routingprotocols.RoutingManagerActor.BgpStatus
 object RoutingHandler {
 
     /** A conventional value for Ip prefix of BGP pairs.
-     *  173 is the MS byte value and 23 the second byte value.
+     *  172 is the MS byte value and 23 the second byte value.
      *  Last 2 LS bytes are available for assigning BGP pairs. */
-    val BGP_IP_INT_PREFIX = 173 * (1<<24) + 23 * (1<<16)
+    val BGP_IP_INT_PREFIX = 172 * (1<<24) + 23 * (1<<16)
 
     // BgpdProcess will notify via these messages
     case object BGPD_READY