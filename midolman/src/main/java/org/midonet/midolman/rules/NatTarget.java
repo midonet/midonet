1aefabd0651c165c4bf51550ff1aec25fa64e2a1
Duarte Nunes <duarte@midokura.com>
2014-07-23 14:42:47 +0200

    Pauseless L4: Adds L4 state operations
    This patch adds L4 state operations for connection tracking and NAT.
    
    Change-Id: Ib58a22722702b5e35ea65832d5eee207894c0164
    Signed-off-by: Duarte Nunes <duarte@midokura.com>

--- midolman/src/main/java/org/midonet/midolman/rules/NatTarget.java
+++ midolman/src/main/java/org/midonet/midolman/rules/NatTarget.java
@@ -1,24 +1,19 @@
 /*
- * Copyright 2011 Midokura KK
+ * Copyright (c) 2011 Midokura Europe SARL, All Rights Reserved.
  */
 
 package org.midonet.midolman.rules;
 
-import org.midonet.packets.IPAddr;
-import org.midonet.packets.IPAddr$;
-import org.midonet.packets.IPv4;
 import org.midonet.packets.IPv4Addr;
-import org.midonet.packets.IPv4Addr$;
-import org.midonet.packets.Net;
 
 public class NatTarget {
 
-    public IPAddr nwStart;
-    public IPAddr nwEnd;
+    public IPv4Addr nwStart;
+    public IPv4Addr nwEnd;
     public int tpStart;
     public int tpEnd;
 
-    public NatTarget(IPAddr nwStart, IPAddr nwEnd, int tpStart, int tpEnd) {
+    public NatTarget(IPv4Addr nwStart, IPv4Addr nwEnd, int tpStart, int tpEnd) {
         this.nwStart = nwStart;
         this.nwEnd = nwEnd;
         this.tpStart = tpStart;
@@ -32,15 +27,15 @@ public class NatTarget {
         this.tpEnd = tpEnd;
     }
 
-    public NatTarget(IPAddr nwStart, IPAddr nwEnd) {
+    public NatTarget(IPv4Addr nwStart, IPv4Addr nwEnd) {
         this(nwStart, nwEnd, 1, 65535);
     }
 
-    public NatTarget(IPAddr ipAddr) {
+    public NatTarget(IPv4Addr ipAddr) {
         this(ipAddr, ipAddr);
     }
 
-    public NatTarget(IPAddr ipAddr, int tpStart, int tpEnd) {
+    public NatTarget(IPv4Addr ipAddr, int tpStart, int tpEnd) {
         this(ipAddr, ipAddr, tpStart, tpEnd);
     }
 
@@ -54,7 +49,7 @@ public class NatTarget {
     }
 
     public void setNwStart(String addr) {
-        this.nwStart = IPAddr$.MODULE$.fromString(addr);
+        this.nwStart = IPv4Addr.fromString(addr);
     }
 
     public String getNwEnd() {
@@ -62,7 +57,7 @@ public class NatTarget {
     }
 
     public void setNwEnd(String addr) {
-        this.nwEnd = IPAddr$.MODULE$.fromString(addr);
+        this.nwEnd = IPv4Addr.fromString(addr);
     }
 
     @Override
