0d965adb9c13bf6ac8757f833a90d2debffea08e
Galo Navarro <galo@midokura.com>
2014-07-29 11:14:54 +0200

    VxGW: make mmm test work
    This requires a few changes in the MMM setup, mostly reverting previous
    additions for this test. For now, the test assumes that Midolman 1
    inside MMM has direct L2 connectivity to the remote VM where the
    emulator runs. That is, vxlan traffic will flow through MMM's br0, and
    you must configure the host so that this traffic gets out of the VM and
    reaches the remote VM with the emulator. In the MidoCloud this required
    adding an additional network 10.0.0.0/24 and putting both the emulator
    and the MMM box. Then on the MMM box, assuming the interface plugged to
    that network is eth1:
    
    - Remove the 10.0.0.0/24 ip from the interface
    - Add eth1 to br0
    - Remove any routes sending 10.0.0.0/24 traffic to eth1 (these interfere
      with MMM)
    - ip route add 10.0.0.101 dev br0 (assuming the vtep is at 10.0.0.101)
    
    This also ammends the tests, mostly to simplify, and removes old cases
    used for troublehooting. Also incorporates the extra vtep tunnel zone
    which is required to be able to talk to configure the vxlan tunnel.
    
    Change-Id: Ice4caaab38144b197db51cf900a32aa345702c09
    Fixes: MN-2634
    Signed-off-by: Galo Navarro <galo@midokura.com>

--- tests/mdts/tests/functional_tests/test_vxlangw.py
+++ tests/mdts/tests/functional_tests/test_vxlangw.py
@@ -17,25 +17,49 @@ PTM = PhysicalTopologyManager('../topologies/mmm_physical_test_vxlangw.yaml')
 VTM = VirtualTopologyManager('../topologies/mmm_virtual_test_vxlangw.yaml')
 BM = BindingManager(PTM, VTM)
 
-binding1 = {
-    'description': 'gateway on MM #1',
+bindings1 = {
+    'description': 'on single MM',
     'bindings': [
         {'binding':
-             {'device_name': 'bridge-000-001', 'port_id': 2,
-              'host_id': 1, 'interface_id': 1}},
-        {'binding':
-             {'device_name': 'router-000-001', 'port_id': 2,
-              'host_id': 1, 'interface_id': 2}},
+             {'device_name': 'bridge-000-001', 'port_id': 1,
+              'host_id': 1, 'interface_id': 1}}
         ]
     }
 
 # TODO(tomohiko) Move those to the virtual topology data file.
-vtep_management_ip = '119.15.120.117'
-vtep_management_port = '6632'
-port_name = 'in6'  # Physical port set up on the emulator.
-vlan_id = 0
-vm_on_vtep = '10.0.2.4'
 
+vtep_management_ip = '10.0.0.101' # The emulator's MGMT ip
+vtep_management_port = '6632' # The emulator's MGMT port
+port_name = 'in6' # Preconfigured in the VTEP emulator
+vlan_id = 0 # Preconfigured in the VTEP emulator
+
+# Hosts that can talk to the VTEP should be added to this tunnel zone, the
+# membership IP determines the src ip that the host will set in the vxlan
+# tunnelled packets, as well as the IPs injected in the VTEP as flooding proxy.
+vtep_tz_name = 'vteptz'
+
+# The device connected to the emulator. Make sure that the
+# mmm_physical_test_vxlangw defines a VM plugged to the host that belongs to
+# the same range
+vm_on_vtep = '10.0.2.26'
+
+# The VTEP itself must be able to communicate directly with this IP, which is
+# Midolmans host. This can be tricky. In the MidoCloud, this involves:
+# - Both VMs holding the VTEP and the MMM box should belong to a network 10.0.0.0/24
+# - Configure the neutron network so that it only assigns IPs between .99 and .102, or
+#   some range that will not overlap with any namespace inside MMM
+# - Assuming eth1 is the interface on the MMM box with the 10.0.0.x IP:
+#   - Remove any route that sends 10.0.0.0/24 towards the
+#     physical ifc. This is so the kernel gives that traffic to MMM's br0
+#   - Remove he ip from the interface (ifconfig eth1 0)
+#   - Add eth1 to the br0 bridge (brctl addif br0 eth1)
+#   - Ask a MidoCloud operator to remove the rules from this port that DROP
+#     traffic that doesn't have the VM's src IP and MAC
+#
+# At this point, the MMM host (whose IP we're setting below) will send straight
+# to the VTEP from its own IP, as if it was a real host in the 10.0.0.0/24
+# network
+_host_ip = '10.0.0.8'
 
 def setup():
     PTM.build()
@@ -61,41 +85,22 @@ def teardown():
         vtep.delete()
         LOG.debug('Deleted a VTEP at %s' % vtep.get_management_ip())
 
+    tzs = VTM._api.get_tunnel_zones()
+    tz = [t for t in tzs if t.get_name() == vtep_tz_name]
+    tz[0].delete()
+
     time.sleep(2)
     PTM.destroy()
     VTM.destroy()
 
 
 @nottest
-@bindings(binding1)
-def test_internet_reachability():
-    '''Tests a packet can go through to Internet.
-
-    This does not test the VxLAN GW functionality. It is here as a utility
-    during the development to test Internet reachability from a host. To be
-    removed once the tests have been finalized.
-    '''
-    sender = BM.get_iface_for_port('bridge-000-001', 2)
-    sender.execute('ping 8.8.8.8 -c2', sync=True)
-
-
-@nottest
-@bindings(binding1)
+@bindings(bindings1)
 def test_ping_host_on_vtep():
     '''Tests if a VM can ping an IP address behind a VTEP.'''
-    # Ping an IP address on the physical VTEP from a VM on a virtual bridge.
-    LOG.debug('Ping to %s should not get through' % vm_on_vtep)
-    sender = BM.get_iface_for_port('bridge-000-001', 2)
+    sender = BM.get_iface_for_port('bridge-000-001', 1)
 
     pcap_filter = 'src host %s and icmp' % vm_on_vtep
-#    f1 = sender.ping_ipv4_addr(vm_on_vtep, suppress_failure=True)
-#    f2 = async_assert_that(sender,
-#                           should_NOT_receive(pcap_filter, within_sec(5)))
-#    wait_on_futures([f1, f2])
-
-    # Send an ARP request.
-    f1 = sender.send_arp_request(vm_on_vtep)
-    wait_on_futures([f1])
 
     # Ping an IP address on the physical VTEP from a VM on a virtual bridge.
     f1 = sender.ping_ipv4_addr(vm_on_vtep)
@@ -106,49 +111,55 @@ def test_ping_host_on_vtep():
 def set_up_vtep():
     '''Helper function to set up a VTEP and a binding.
 
-    Part of this setup should be declared in the virtual topology data, and be
-    taken care of by VirtualTopologyManager, but the VTEP and VTEP binding
-    wrappers for MDTS haven't been implemented yet, so they need to be set up by
-    calling Python MidoNet Client directly here.
+    Part of this setup should be declared in the virtual topology data,
+    and be taken care of by VirtualTopologyManager, but the VTEP and
+    VTEP binding wrappers for MDTS haven't been implemented yet, so they
+    need to be set up by calling Python MidoNet Client directly here.
+
+    Creating a VTEP involves:
+    - Ensuring that a tunnel zone exists for VTEPs (type vtep)
+    - Creating a new VTEP entity, using the above VTEP
 
     TODO(tomohiko) Implement MDTS wrapper for VTEP and VTEP binding.
     '''
     LOG.debug('Setting up a VxLAN GW.')
     api = VTM._api
 
-    LOG.debug('Creating a new VTEP.')
-    # Create a VTEP. Look up a tunnel zone from the host info.
     host_1 = None
     for h in PTM._hosts:
         host = h['host']
         if host.get('id') == 1: host_1 = host
-    LOG.debug('Looked up the host id 1: %s' % host_1.get('mn_host_id'))
 
-    LOG.debug('Look up a tunnel zone.')
-    tz_data = host_1.get('tunnel_zone')
-    tzs = api.get_tunnel_zones()
-    tz = [t for t in tzs if t.get_name() == tz_data['name']]
-    tunnel_zone = tz[0]
-    tunnel_zone_id = tunnel_zone.get_id()
-    LOG.debug('Tunnel zone name/IP/ID: %s/%s/%s' %
-              (tz_data['name'], tz_data['ip_addr'], tunnel_zone_id))
+    # TODO: support this via topology, see MN-2623
+    LOG.debug('Creating a new VTEP tunnel zone')
+    tz = api.add_vtep_tunnel_zone()
+    tz.name(vtep_tz_name)
+    tz.type('vtep')
+    tz = tz.create()
+
+    LOG.debug('Adding host 1 to the VTEP tunnel zone %s %s', tz, host_1)
+    tzh = tz.add_tunnel_zone_host()
+    tzh.ip_address(_host_ip)
+    tzh.host_id(host_1.get('mn_host_id'))
+    tzh = tzh.create()
 
     vtep = api.add_vtep()\
              .name('My VTEP')\
              .management_ip(vtep_management_ip)\
              .management_port(vtep_management_port)\
-             .tunnel_zone_id(tunnel_zone_id)\
+             .tunnel_zone_id(tz.get_id())\
              .create()
+
     LOG.debug('Created a VTEP at %s' % vtep_management_ip)
 
     # Add a new VTEP binding.
     # Look up a bridge with which to bind the VTEP.
     bridge = VTM.get_bridge('bridge-000-001')
     bridge_id = bridge._mn_resource.get_id()
-    LOG.debug('Bridge ID: %s' % bridge_id)
     vtep.add_binding()\
         .port_name(port_name)\
         .vlan_id(vlan_id)\
         .network_id(bridge_id)\
         .create()
-    LOG.debug('Added a binding')
+
+    LOG.debug('Added a binding to bridge %s', bridge_id)
