a8ab48f1e091a1397e11572cef9e2c4772c4331e
Galo Navarro <galo@midokura.com>
2014-08-05 11:27:29 +0200

    VXGW-ARP-SUP: enhancements to IpToMac map
    Add functionality to the Ip4ToMacReplicatedMap map that will be required
    to support ARP supression in the VxGW service, by being able to
    manipulate this map while not conflicting with entries defined from the
    API.
    
    Tests added.
    
    Ref.: MN-1636
    Signed-off-by: Ernest Artiaga <ernest@midokura.com>
    Signed-off-by: Galo Navarro <galo@midokura.com>
    
    Change-Id: Ia2270486ef6827d94fbbcf70ed8854b067bddf42

--- midolman/src/test/java/org/midonet/midolman/state/TestIp4ToMacReplicatedMap.java
+++ midolman/src/test/java/org/midonet/midolman/state/TestIp4ToMacReplicatedMap.java
@@ -1,16 +1,18 @@
-// Copyright 2011 Midokura Inc.
-
+/*
+ * Copyright (c) 2014 Midokura SARL, All Rights Reserved.
+ */
 package org.midonet.midolman.state;
+
+import org.junit.Before;
+import org.junit.Test;
 
 import org.midonet.packets.IPv4Addr;
 import org.midonet.packets.MAC;
 
-import org.junit.Before;
-import org.junit.Test;
-import scala.collection.Map;
-
-import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 public class TestIp4ToMacReplicatedMap {
 
@@ -18,6 +20,7 @@ public class TestIp4ToMacReplicatedMap {
     MockDirectory dir;
 
     final IPv4Addr ip = IPv4Addr.fromString("1.2.3.4");
+    final IPv4Addr ip2 = IPv4Addr.fromString("5.6.7.8");
     final byte[] macBytes = { 0, 1, 2, 3, 4, 5 };
     final MAC mac = new MAC(macBytes);
     final byte[] mac2Bytes = { 0, 1, 6, 7, 8, 9 };
@@ -63,4 +66,63 @@ public class TestIp4ToMacReplicatedMap {
         assertEquals(ip, map.decodeKey("1.2.3.4"));
         assertEquals(mac, map.decodeValue("00:01:02:03:04:05"));
     }
+
+    @Test
+    public void testLearnedEntriesAddHasRemove() throws Exception {
+        Ip4ToMacReplicatedMap.addLearnedEntry(dir, ip, mac);
+        assertTrue(Ip4ToMacReplicatedMap.hasLearnedEntry(dir, ip, mac));
+        assertFalse(Ip4ToMacReplicatedMap.hasPersistentEntry(dir, ip, mac));
+        assertEquals(mac, Ip4ToMacReplicatedMap.getEntry(dir, ip));
+
+        // some deletes that should not have any effect
+        Ip4ToMacReplicatedMap.deleteLearnedEntry(dir, ip, mac2);
+        Ip4ToMacReplicatedMap.deleteLearnedEntry(dir, ip2, mac);
+
+        // Reassert
+        assertTrue(Ip4ToMacReplicatedMap.hasLearnedEntry(dir, ip, mac));
+        assertFalse(Ip4ToMacReplicatedMap.hasPersistentEntry(dir, ip, mac));
+        assertEquals(mac, Ip4ToMacReplicatedMap.getEntry(dir, ip));
+
+        // Now for real
+        Ip4ToMacReplicatedMap.deleteLearnedEntry(dir, ip, mac);
+
+        assertFalse(Ip4ToMacReplicatedMap.hasLearnedEntry(dir, ip, mac));
+        assertFalse(Ip4ToMacReplicatedMap.hasPersistentEntry(dir, ip, mac));
+        assertEquals(null, Ip4ToMacReplicatedMap.getEntry(dir, ip));
+    }
+
+    @Test
+    public void testRepeatedEntryFails() throws Exception {
+        Ip4ToMacReplicatedMap.addLearnedEntry(dir, ip, mac);
+        try {
+            Ip4ToMacReplicatedMap.addLearnedEntry(dir, ip, mac);
+            fail("StateAccesException expected when overwriting entry");
+        } catch (Exception e) {
+            // ok
+        }
+        assertTrue(Ip4ToMacReplicatedMap.hasLearnedEntry(dir, ip, mac));
+    }
+
+    /* The equivalent testLearnedDoesntOverwriteNonLearned cannot be done
+     * because the map actually allows it. This is likely a bug, but will
+     * require verifying that no feature is depending on the bug.
+     */
+    @Test
+    public void testNotLearnedOverwritesLearned() throws Exception {
+        Ip4ToMacReplicatedMap.addLearnedEntry(dir, ip, mac);
+        assertTrue(Ip4ToMacReplicatedMap.hasLearnedEntry(dir, ip, mac));
+        assertFalse(Ip4ToMacReplicatedMap.hasPersistentEntry(dir, ip, mac));
+        Ip4ToMacReplicatedMap.addPersistentEntry(dir, ip, mac);
+        assertFalse(Ip4ToMacReplicatedMap.hasLearnedEntry(dir, ip, mac));
+        assertTrue(Ip4ToMacReplicatedMap.hasPersistentEntry(dir, ip, mac));
+    }
+
+    @Test // the ordinary deleteEntry should affect also learned entries
+    public void testDeleteAffectsLearnedEntries() throws Exception {
+        Ip4ToMacReplicatedMap.addLearnedEntry(dir, ip, mac);
+        assertTrue(Ip4ToMacReplicatedMap.hasLearnedEntry(dir, ip, mac));
+        Ip4ToMacReplicatedMap.deleteEntry(dir, ip, mac);
+        assertFalse(Ip4ToMacReplicatedMap.hasLearnedEntry(dir, ip, mac));
+    }
+
 }
