ab53706aa231ca013ddc603244ef40ff427537f4
Alex Bikfalvi <alex.bikfalvi@midokura.com>
2014-10-14 10:19:13 +0200

    Re-architect the agent VTA to use ZOOM storage
    This patch introduces a VirtualTopology (VT) class that replaces
    the VTA to store the virtual topology devices and to use the ZOOM
    storage interface based on observables.
    
    The VT receives updates for a device using a DeviceObservable
    instance, which receives topology updates from the storage layer
    and creates the corresponding simulation device. The device
    observable exposes the device information as a future and reactive
    observable. In addition, each device observable tracks the current
    subscription to enable future garbage collection of unused observables
    and updates a VT device cache that improves lookup performance for
    existing devices.
    
    The DeviceObseravable is an abstract class, and specific
    implementations for each device type must specify how to combine
    topology data read from storage, and create the corresponding
    simulation device. The sub-classes can also override update methods
    (onNext, onCompleted, onError), to take a specific action for a
    given update, such as flow invalidation.
    
    This patch demonstrates the implementation of a DeviceObservable for
    a port device type: PortObservable.
    
    Ref: MN-2988
    
    Change-Id: Icda6346d8d590c5615fa4453ef1281fe23266294
    Signed-off-by: Alex Bikfalvi <alex.bikfalvi@midokura.com>

---
+++ midonet-util/src/test/scala/org/midonet/util/reactivex/AwaitableObserver.scala
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2014 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.midonet.util.reactivex
+
+import java.util.concurrent.CountDownLatch
+
+import scala.collection.mutable
+import scala.concurrent.duration.Duration
+
+import rx.Observer
+
+object AwaitableObserver {
+
+    trait Notification
+    case class OnNext[T](value: T) extends Notification
+    case class OnCompleted() extends Notification
+    case class OnError(e: Throwable) extends Notification
+
+}
+
+class AwaitableObserver[T](awaitCount: Int = 1) extends Observer[T] {
+
+    import AwaitableObserver._
+
+    private val list = new mutable.MutableList[Notification]
+    @volatile private var counter = new CountDownLatch(awaitCount)
+
+    override def onNext(value: T): Unit = {
+        list += OnNext(value)
+        counter.countDown()
+    }
+
+    override def onCompleted(): Unit = {
+        list += OnCompleted()
+        counter.countDown()
+    }
+
+    override def onError(e: Throwable): Unit = {
+        list += OnError(e)
+        counter.countDown()
+    }
+
+    def await(duration: Duration): Boolean = {
+        counter.await(duration.length, duration.unit)
+    }
+
+    def await(duration: Duration, resetCount: Int): Boolean = try {
+        counter.await(duration.length, duration.unit)
+    } finally {
+        counter = new CountDownLatch(resetCount)
+    }
+
+    def reset(resetCount: Int): Unit = {
+        counter = new CountDownLatch(resetCount)
+    }
+
+    def notifications: Seq[Notification] = list
+
+}
