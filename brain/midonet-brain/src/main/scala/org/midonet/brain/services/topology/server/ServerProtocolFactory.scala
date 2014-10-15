bf460272246a73bbafcfbdb48616cfd1d2ff24c8
Ernest Artiaga <ernest@midokura.com>
2014-10-15 17:39:25 +0200

    Websockets/Netty topology access service
    Implement a service to provide read-only access
    to Midonet topology for clients using
    Netty and/or Websockets (e.g. agent and CP).
    
    Ref: MN-2985
    
    Change-Id: Ic2cdf0b8616203192ac4ac67e8bafaf53735173b
    Signed-off-by: Ernest Artiaga <ernest@midokura.com>
    Signed-off-by: Galo Navarro <galo@midokura.com>

---
+++ brain/midonet-brain/src/main/scala/org/midonet/brain/services/topology/server/ServerProtocolFactory.scala
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2014 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.brain.services.topology.server
+
+import scala.concurrent.Promise
+
+import com.google.protobuf.Message
+
+import rx.{Subscription, Observer}
+import org.midonet.brain.services.topology.server.ServerState.SessionFactory
+import org.midonet.cluster.services.topology.common.{ProtocolFactory}
+import org.midonet.cluster.util.UUIDUtil
+
+/**
+ * Protocol handling the server-side communication
+ * @param sMgr is the session inventory manager, responsible to maintain
+ *             the backend zoom subscriptions for each client.
+ */
+class ServerProtocolFactory(private val sMgr: SessionInventory)
+    extends ProtocolFactory {
+
+    /**
+     * Return the initial state and the future subscription to the client's
+     * session.
+     * @param output is the stream of messages to be sent to the client
+     */
+    override def start(output: Observer[Message]): ProtocolStart = {
+        val subs = Promise[Option[Subscription]]()
+        val sessionFactory: SessionFactory = (id, successMsg) => try {
+            val session = sMgr.claim(UUIDUtil.fromProto(id))
+            subs.success(Some(session.observable.subscribe(output)))
+            output.onNext(successMsg)
+            Some(session)
+        } catch {
+            case cause: Exception =>
+                subs.failure(cause)
+                None
+        }
+        (Ready(sessionFactory), subs.future)
+    }
+}
+
---
+++ brain/midonet-brain/src/main/scala/org/midonet/brain/services/topology/server/ServerState.scala
@@ -0,0 +1,110 @@
+package org.midonet.brain.services.topology.server
+
+import com.google.protobuf.Message
+
+import org.midonet.brain.services.topology.server.ServerState.{SessionFactory,replyAck,replyNAck}
+import org.midonet.cluster.models.Commons
+import org.midonet.cluster.rpc.Commands.Response._
+import org.midonet.cluster.rpc.Commands._
+import org.midonet.cluster.services.topology.common.{Interruption, State}
+import org.midonet.cluster.services.topology.common.TopologyMappings.klassOf
+
+/** When called, it should either provide a valid GoBetween, or get the
+  * given Message as a reply to the client. */
+object ServerState {
+    type SessionFactory =
+        (Commons.UUID, Message) => Option[Session]
+
+    /** Provides an instance of the TopologyProtocol in the 'started' state,
+      * listening for new (or recoverable) connections. */
+    def start(sf: SessionFactory): State = Ready(sf)
+
+    private val builder = Response.newBuilder()
+    private val uuidBuilder = Commons.UUID.newBuilder()
+    private val ackBuilder = Ack.newBuilder()
+    private val nackBuilder = NAck.newBuilder()
+    private def ack(reqId: Commons.UUID): Ack = {
+        ackBuilder.clear()
+                  .setReqId(uuidBuilder.clear().mergeFrom(reqId).build())
+                  .build()
+    }
+    private def nack(reqId: Commons.UUID): NAck = {
+        nackBuilder.clear()
+                   .setReqId(uuidBuilder.clear().mergeFrom(reqId).build())
+                   .build()
+    }
+    def replyAck(id: Commons.UUID) = builder.clear().setAck(ack(id)).build()
+    def replyNAck(id: Commons.UUID) = builder.clear().setNack(nack(id)).build()
+}
+
+
+/** The Connection is listening for the handshake exchange, either for a new
+  * connection or to resume an interrupted connection. */
+final case class Ready(sf: SessionFactory)
+    extends State {
+    override def process(msg: Any) = msg match {
+        case m: Request if m.hasHandshake =>
+            val hs = m.getHandshake
+            sf(hs.getCnxnId, replyAck(hs.getReqId)) match {
+                case Some(c) => Active(c)
+                case None => Closed
+            }
+        case Interruption => Closed
+        case _ => this
+    }
+}
+
+/** We have a funcional Session liaising between the client and the topology
+  * so we're able to process messages from the client. */
+final case class Active(session: Session) extends State {
+    override def process(msg: Any) = msg match {
+        case m: Request if m.hasGet && m.getGet.getSubscribe =>
+            klassOf(m.getGet.getType) match {
+                case None =>
+                    replyNAck(m.getGet.getReqId)
+                case Some(k) =>
+                    session.watch(Id.fromProto(m.getGet.getId), k,
+                                  replyNAck(m.getGet.getReqId))
+            }
+            this
+        case m: Request if m.hasGet =>
+            klassOf(m.getGet.getType) match {
+                case None =>
+                    replyNAck(m.getGet.getReqId)
+                case Some(k) =>
+                    session.get(Id.fromProto(m.getGet.getId), k,
+                                replyNAck(m.getGet.getReqId))
+            }
+            this
+        case m: Request if m.hasUnsubscribe =>
+            klassOf(m.getUnsubscribe.getType) match {
+                case None =>
+                    replyNAck(m.getUnsubscribe.getReqId)
+                case Some(k) =>
+                    session.unwatch(Id.fromProto(m.getUnsubscribe.getId), k,
+                                    replyAck(m.getUnsubscribe.getReqId),
+                                    replyNAck(m.getUnsubscribe.getReqId))
+
+            }
+            this
+        case Interruption =>
+            // We remain interested but the connection was somehow interrupted
+            Closed
+        case m: Request if m.hasBye =>
+            session.terminate(replyAck(m.getBye.getReqId))
+            Closed
+        case m =>
+            this
+    }
+    override def equals(that: Any) = that match {
+        case that: Active => this.session.equals(that.session)
+        case _ => false
+    }
+}
+
+/** The Connection is closed, it won't support any resumes and the server is
+  * free to clean up all associated resources. */
+case object Closed extends State {
+    override def process(m: Any) = this
+}
+
