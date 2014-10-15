f5b535586d59a16d081ec45a275cd06e4373ae02
Ernest Artiaga <ernest@midokura.com>
2014-10-15 17:39:25 +0200

    Websockets/Netty topology access service
    Implement a service to provide read-only access
    to Midonet topology for clients using
    Netty and/or Websockets (e.g. agent and CP).
    
    Ref: MN-2985
    
    Change-Id: Ic2cdf0b8616203192ac4ac67e8bafaf53735173b
    Signed-off-by: Ernest Artiaga <ernest@midokura.com>
    Signed-off-by: Galo Navarro <galo@midokura.com>

---
+++ cluster/src/main/scala/org/midonet/cluster/services/topology/common/TopologyMappings.scala
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2014 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.cluster.services.topology.common
+
+import com.google.protobuf.Message
+
+import org.midonet.cluster.models.Topology
+
+/**
+ * Common utilities for Topology messages
+ */
+object TopologyMappings {
+    /** Mapping Topology entity types to their container class */
+    val typeToKlass: Map[Topology.Type, Class[_ <: Message]] = Map(
+        Topology.Type.CHAIN  -> classOf[Topology.Chain],
+        Topology.Type.HOST -> classOf[Topology.Host],
+        Topology.Type.IP_ADDR_GROUP -> classOf[Topology.IpAddrGroup],
+        Topology.Type.NETWORK -> classOf[Topology.Network],
+        Topology.Type.PORT -> classOf[Topology.Port],
+        Topology.Type.PORT_GROUP -> classOf[Topology.PortGroup],
+        Topology.Type.ROUTE -> classOf[Topology.Route],
+        Topology.Type.ROUTER -> classOf[Topology.Router],
+        Topology.Type.RULE -> classOf[Topology.Rule],
+        Topology.Type.TUNNEL_ZONE -> classOf[Topology.TunnelZone],
+        Topology.Type.VTEP -> classOf[Topology.Vtep],
+        Topology.Type.VTEP_BINDING -> classOf[Topology.VtepBinding]
+    )
+    /** Retrieve the klass for the given Topology entity type */
+    def klassOf(t: Topology.Type) = typeToKlass.get(t)
+
+    /** Retrieve the type corresponding to a given class */
+    def typeOf(k: Class[_ <: Message]): Option[Topology.Type] =
+        typeToKlass.find(_._2 == k) match {
+        case Some(entry) => Some(entry._1)
+        case None => None
+    }
+}
