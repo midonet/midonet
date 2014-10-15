b446a027d12058da188c86fd7aa1591e1bc7c431
Ernest Artiaga <ernest@midokura.com>
2014-10-15 17:39:25 +0200

    Websockets/Netty topology access service
    Implement a service to provide read-only access
    to Midonet topology for clients using
    Netty and/or Websockets (e.g. agent and CP).
    
    Ref: MN-2985
    
    Change-Id: Ic2cdf0b8616203192ac4ac67e8bafaf53735173b
    Signed-off-by: Ernest Artiaga <ernest@midokura.com>
    Signed-off-by: Galo Navarro <galo@midokura.com>

---
+++ brain/midonet-brain/src/main/scala/org/midonet/brain/services/topology/TopologyApiServiceApp.scala
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2014 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.brain.services.topology
+
+import com.google.inject.{Inject, Provides, AbstractModule, Guice}
+
+import org.apache.curator.framework.CuratorFramework
+import org.slf4j.LoggerFactory
+
+import org.midonet.cluster.config.ZookeeperConfig
+import org.midonet.cluster.data.storage.{Storage, ZookeeperObjectMapper}
+import org.midonet.config.ConfigProvider
+import org.midonet.midolman.guice.zookeeper.ZookeeperConnectionModule.CuratorFrameworkProvider
+
+/**
+ * Stand-alone application to start the TopologyApiService
+ */
+object TopologyApiServiceApp extends App {
+    private val log = LoggerFactory.getLogger(this.getClass)
+
+    private val cfgFile = args(0)
+    private val cfg = ConfigProvider.fromConfigFile(cfgFile)
+    private val cfgProvider = ConfigProvider.providerForIniConfig(cfg)
+
+    private val apiCfg = cfgProvider.getConfig(classOf[TopologyApiServiceConfig])
+    private val zkCfg = cfgProvider.getConfig(classOf[ZookeeperConfig])
+
+    protected[brain] val injector = Guice.createInjector(new AbstractModule {
+        override def configure(): Unit = {
+            bind(classOf[ConfigProvider]).toInstance(cfgProvider)
+            bind(classOf[TopologyApiServiceConfig]).toInstance(apiCfg)
+
+            bind(classOf[ZookeeperConfig]).toInstance(zkCfg)
+            bind(classOf[CuratorFramework])
+                .toProvider(classOf[CuratorFrameworkProvider])
+
+            bind(classOf[Storage]).to(classOf[ZookeeperObjectMapper])
+            bind(classOf[TopologyApiService]).asEagerSingleton()
+        }
+
+        @Provides
+        def provideZookeeperObjectMapper(curator: CuratorFramework):
+            ZookeeperObjectMapper =
+            new ZookeeperObjectMapper("/zoom", curator)
+    })
+
+    sys.addShutdownHook {
+        log.info("Terminating instance of Topology API server")
+        injector.getInstance(classOf[TopologyApiService])
+                .stopAsync()
+                .awaitTerminated()
+    }
+
+    try {
+        log.info("Starting instance of Topology API server")
+        injector.getInstance(classOf[TopologyApiService])
+                .startAsync()
+                .awaitRunning()
+        log.info("Started instance of Topology API server")
+
+        try {
+            while (!Thread.currentThread().isInterrupted)
+                Thread.sleep(600000)
+        } catch {
+            case e: InterruptedException => Thread.currentThread().interrupt()
+        } finally {
+            log.info("Interrupted. Shutting down instance of Topology API server")
+        }
+
+    } catch {
+        case e: Exception =>
+            log.error("Failed to start instance of Topology API server", e)
+    }
+}
