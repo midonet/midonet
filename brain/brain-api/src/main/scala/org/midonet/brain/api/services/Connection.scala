bcca7231514c8fd50669f1a6b6a60758ab2e97d1
Ernest Artiaga <ernest@midokura.com>
2014-10-15 17:39:25 +0200

    <WIP> Websockets/Netty storage access service
    Implement a service to provide read-only access
    to Midonet storage for clients using
    Netty and/or Websockets (e.g. agent and CP).
    
    Ref: MN-2985
    
    Change-Id: Ic2cdf0b8616203192ac4ac67e8bafaf53735173b
    Signed-off-by: Ernest Artiaga <ernest@midokura.com>

---
+++ brain/brain-api/src/main/scala/org/midonet/brain/api/services/Connection.scala
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2014 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.brain.api.services
+
+import io.netty.channel.ChannelHandlerContext
+import org.slf4j.LoggerFactory
+
+import org.midonet.cluster.models.Commons
+
+/**
+ * Connection channel
+ */
+class Connection(private val ctx: ChannelHandlerContext) {
+    private val log = LoggerFactory.getLogger(classOf[Connection])
+    private var client: Client = null
+
+    def error(exc: Throwable): Unit = {
+        log.error("topology server error", exc)
+        // TODO: send error response to client
+    }
+
+    def msg(proto: Commons.UUID): Unit = {
+
+    }
+
+    /**
+     * Clean-up terminated connection
+     */
+    def clear(): Unit = {
+        if (client != null) client.disconnect(ctx)
+        client = null
+        ctx.close()
+    }
+}
