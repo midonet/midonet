0e033b33d15872c7c5bbbc9162f8f675a375297e
Brandon Berg <bberg@midokura.com>
2014-10-16 21:52:43 +0900

    Update copyright statements in midonet/odp and midonet/packets
    Change-Id: I041466875074c7661e114a4b72820a50de6a7785
    Signed-off-by: Brandon Berg <bberg@midokura.com>

--- odp/src/main/java/org/midonet/odp/util/TapWrapper.java
+++ odp/src/main/java/org/midonet/odp/util/TapWrapper.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2014 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.midonet.odp.util;
 
 import org.midonet.packets.ARP;
@@ -16,12 +32,6 @@ import java.util.Arrays;
 import static org.midonet.util.process.ProcessHelper.newProcess;
 
 
-/**
- * Copyright 2011 Midokura Europe SARL
- * User: rossella rossella@midokura.com
- * Date: 12/9/11
- * Time: 1:03 PM
- */
 public class TapWrapper {
     private final static Logger log = LoggerFactory.getLogger(TapWrapper.class);
     private boolean up;
