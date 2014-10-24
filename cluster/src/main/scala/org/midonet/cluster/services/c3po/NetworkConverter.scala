849c971d99dca000e265b6c754a1e5bcda56f6d5
Tomohiko KIMURA <tomohiko@midokura.com>
2014-10-24 15:53:45 +0900

    Defines a common base class, NeutronApiTranslator.
    Defines NeutronApiTranslator, a common abstract base class for
    concrete Neutron model translatior implementation such as
    NetworkTranslator. Defines an API method toMido, which converts an
    operation (i.e. CREATE, UPDATE, DELETE) on a high-level Neutron model
    object into a list of operations on low-level MidoNet model objects.
    
    Also defines TranslationException that wraps around low-level
    exceptions such as ObjectExistsException thrown by Storage.
    NeutronApiTranslators are expected to catch those low-level Storage
    Exceptions and rethrow them wrapped in TranslationException. Their
    clients are expected to catch this Exception class and perform some
    exception handling.
    
    Fix MN-2999
    Change-Id: Ibfb8de2d65482fc045d017efb950769982447821

---
+++ cluster/src/main/scala/org/midonet/cluster/services/c3po/NetworkConverter.scala
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2014 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.midonet.cluster.services.c3po
+
+import org.midonet.cluster.models.Neutron.NeutronNetwork
+import org.midonet.cluster.models.Topology.Network
+
+/**
+ * Defines the conversion logic.
+ */
+object NetworkConverter {
+    implicit def toMido(network: NeutronNetwork): Network = {
+       Network.newBuilder()
+              .setId(network.getId)
+              .setTenantId(network.getTenantId)
+              .setName(network.getName)
+              .setAdminStateUp(network.getAdminStateUp)
+              .build
+    }
+}
