87b0c62a704c6ff9a99ad1cf5d62a48977a27cbf
Duarte Nunes <duarte@midokura.com>
2014-10-30 14:29:56 +0100

    Flow removal callbacks are executed by Sim threads
    This patch establishes a channel between the FlowController and the
    Simulation threads, through which the former sends flow removal
    callbacks so the latter can execute them. This allows for
    synchronization to be removed from several data structures.
    
    Change-Id: Ibb46585ec1b61b1d7a041d2aff2258837cc4a16e
    Signed-off-by: Duarte Nunes <duarte@midokura.com>

--- midolman/src/main/scala/org/midonet/midolman/DeduplicationActor.scala
+++ midolman/src/main/scala/org/midonet/midolman/DeduplicationActor.scala
@@ -18,10 +18,20 @@ package org.midonet.midolman
 
 import java.util.{UUID, HashMap => JHashMap, List => JList}
 
+import scala.collection.mutable
+import scala.concurrent.duration._
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success}
+
 import akka.actor._
-import akka.event.LoggingReceive
+
 import com.codahale.metrics.Clock
+import org.slf4j.MDC
 import com.typesafe.scalalogging.Logger
+
+import org.jctools.queues.QueueFactory
+import org.jctools.queues.spec.ConcurrentQueueSpec._
+
 import org.midonet.cluster.DataClient
 import org.midonet.midolman.FlowController.InvalidateFlowsByTag
 import org.midonet.midolman.HostRequestProxy.FlowStateBatch
@@ -38,16 +48,12 @@ import org.midonet.netlink.exceptions.NetlinkException
 import org.midonet.odp.flows.FlowAction
 import org.midonet.odp.{Datapath, FlowMatch, Packet}
 import org.midonet.packets.Ethernet
+import org.midonet.sdn.CallbackExecutor
 import org.midonet.sdn.flows.WildcardMatch
 import org.midonet.sdn.state.{FlowStateTable, FlowStateTransaction}
 import org.midonet.util.collection.Reducer
-import org.midonet.util.concurrent.ExecutionContextOps
-import org.slf4j.MDC
-
-import scala.collection.mutable
-import scala.concurrent.duration._
-import scala.concurrent.{ExecutionContext, Future}
-import scala.util.{Failure, Success}
+import org.midonet.util.concurrent._
+import org.midonet.util.functors.Callback0
 
 object DeduplicationActor {
     // Messages
@@ -176,6 +182,9 @@ class DeduplicationActor(
     protected var replicator: FlowStateReplicator = _
 
     protected var workflow: PacketHandler = _
+    val cbExecutor: CallbackExecutor = new CallbackExecutor(
+        QueueFactory.newQueue[Callback0](createBoundedSpsc(1024))
+    )
 
     private var pendingFlowStateBatches = List[FlowStateBatch]()
 
@@ -193,7 +202,7 @@ class DeduplicationActor(
             }
         }
 
-    override def receive = LoggingReceive {
+    override def receive = {
 
         case DatapathReady(dp, state) if null == datapath =>
             datapath = dp
@@ -206,7 +215,8 @@ class DeduplicationActor(
                 datapath)
             pendingFlowStateBatches foreach (self ! _)
             workflow = new PacketWorkflow(dpState, datapath, clusterDataClient,
-                                          dpConnPool, actionsCache, replicator)
+                                          dpConnPool, cbExecutor, actionsCache,
+                                          replicator)
 
         case m: FlowStateBatch =>
             if (replicator ne null)
@@ -226,6 +236,8 @@ class DeduplicationActor(
                 handlePacket(packets(i))
                 i += 1
             }
+
+            cbExecutor.run()
 
         case RestartWorkflow(pktCtx) =>
             MDC.put("cookie", pktCtx.cookieStr)
