15e292e71a3049f41ec08ab5b484f5080b72b987
Duarte Nunes <duarte@midokura.com>
2014-11-04 18:30:48 +0100

    Adds backchannel support to the disruptor
    Change-Id: I36408416056e0d2621f6d603bb1a90067b9f17bb
    Signed-off-by: Duarte Nunes <duarte@midokura.com>

---
+++ midonet-util/src/test/scala/org/midonet/util/concurrent/BackchannelEventProcessorTest.scala
@@ -0,0 +1,120 @@
+/*
+ * Copyright 2014 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.util.concurrent
+
+import java.lang.Thread.State
+
+import com.lmax.disruptor._
+
+import org.junit.runner.RunWith
+import org.scalatest.concurrent.Eventually.eventually
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{OneInstancePerTest, FeatureSpec, Matchers}
+
+@RunWith(classOf[JUnitRunner])
+class BackchannelEventProcessorTest extends FeatureSpec
+                                    with Matchers
+                                    with OneInstancePerTest {
+
+    feature ("BackchannelEventProcessor receives events from the " +
+             "ring buffer and backchannel") {
+        val backchannel = new Backchannel {
+            var processed = 0
+            var hasWork = false
+            override def shouldProcess() = hasWork
+            override def process(): Unit = processed += 1
+        }
+
+        val ringBuffer = RingBuffer.createSingleProducer(new EventFactory[Object] {
+            override def newInstance(): Object = new Object
+        }, 16)
+
+        val handler = new EventPoller.Handler[Object] {
+            var handled = 0
+
+            override def onEvent(event: Object, sequence: Long,
+                                 endOfBatch: Boolean): Boolean = {
+                handled += 1
+                true
+            }
+        }
+
+        val processor = new BackchannelEventProcessor[Object](ringBuffer, handler, backchannel)
+        val t = new Thread() {
+            override def run() = processor.run()
+        }
+        t.setDaemon(true)
+        t.start()
+
+        scenario ("Processor reacts to new events") {
+            eventually {
+                backchannel.processed should be (BackchannelEventProcessor.DEFAULT_RETRIES + 1)
+                t.getState should be (State.WAITING)
+            }
+            backchannel.shouldProcess should be (false)
+            handler.handled should be (0)
+
+            ringBuffer.publish(0)
+            ringBuffer.isPublished(0) should be (true)
+            eventually {
+                handler.handled should be (1)
+                backchannel.processed should be ((BackchannelEventProcessor.DEFAULT_RETRIES + 1) * 2)
+            }
+        }
+
+        scenario ("Thread reacts to work from backchannel") {
+            eventually {
+                backchannel.processed should be (BackchannelEventProcessor.DEFAULT_RETRIES + 1)
+                t.getState should be (State.WAITING)
+            }
+            backchannel.shouldProcess should be (false)
+
+            backchannel.hasWork = true
+            eventually {
+                handler.handled should be (0)
+                backchannel.processed should be >= BackchannelEventProcessor.DEFAULT_RETRIES * 3
+            }
+        }
+
+        scenario ("Thread reacts to processor shutdown") {
+            eventually {
+                backchannel.processed should be (BackchannelEventProcessor.DEFAULT_RETRIES + 1)
+                t.getState should be (State.WAITING)
+            }
+            backchannel.shouldProcess should be (false)
+
+            processor.halt()
+            t.join()
+        }
+
+        scenario ("Backchannel is queried at batch boundary") {
+            eventually {
+                backchannel.processed should be (BackchannelEventProcessor.DEFAULT_RETRIES + 1)
+                t.getState should be (State.WAITING)
+            }
+            backchannel.shouldProcess should be (false)
+
+            ringBuffer.publish(0, 1)
+            ringBuffer.isPublished(0) should be (true)
+            ringBuffer.isPublished(1) should be (true)
+            eventually {
+                handler.handled should be (2)
+                backchannel.processed should be ((BackchannelEventProcessor.DEFAULT_RETRIES + 1) * 2)
+            }
+        }
+    }
+}
