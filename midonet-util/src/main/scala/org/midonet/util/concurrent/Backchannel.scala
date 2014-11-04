5dd33f7c192a47f64f426501895ed7279faa8f80
Duarte Nunes <duarte@midokura.com>
2014-11-04 18:30:48 +0100

    Adds backchannel support to the disruptor
    Change-Id: I36408416056e0d2621f6d603bb1a90067b9f17bb
    Signed-off-by: Duarte Nunes <duarte@midokura.com>

---
+++ midonet-util/src/main/scala/org/midonet/util/concurrent/Backchannel.scala
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2014 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.util.concurrent
+
+/**
+ * An interface to be used together with the BackchannelEventProcessor.
+ * The shouldProcess() method informs whether there is work to be processed
+ * on this backchannel and it should be thread-safe. The process() method
+ * does the actual work and can be called unguarded by a call to shouldProcess().
+ */
+trait Backchannel {
+    def shouldProcess(): Boolean
+    def process(): Unit
+}
