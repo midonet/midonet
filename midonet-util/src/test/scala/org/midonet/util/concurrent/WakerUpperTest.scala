343b15183e36242bb76a0706d15bf8e1c68a30bb
Duarte Nunes <duarte@midokura.com>
2014-11-04 16:28:55 +0100

    Adds WakerUpper and WakerUpperWaitStrategy
    This patch adds a global object, WakerUpper, with which threads can
    register when they are parked. The WakerUpper will then be responsible
    for waking up those threads based on the specified criteria.
    
    A WaitStrategy for the Disruptor based on the WakerUpper is included.
    
    Change-Id: Ic2f43f48caa22bf19fe7759beaf02855ce89719d
    Signed-off-by: Duarte Nunes <duarte@midokura.com>

---
+++ midonet-util/src/test/scala/org/midonet/util/concurrent/WakerUpperTest.scala
@@ -0,0 +1,113 @@
+/*
+ * Copyright 2014 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.util.concurrent
+
+import java.util.concurrent.ThreadLocalRandom
+
+import org.junit.runner.RunWith
+import org.midonet.util.concurrent.WakerUpper.WaitContext
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{FeatureSpec, Matchers}
+
+import scala.compat.Platform
+
+@RunWith(classOf[JUnitRunner])
+class WakerUpperTest extends FeatureSpec with Matchers {
+
+    feature ("WakerUpper wakes blocked threads") {
+
+        scenario ("WakerUpper is activated when a thread blocks") {
+            val then = Platform.currentTime
+            new WaitContext {
+                override def shouldWakeUp(): Boolean =
+                    Platform.currentTime > then + 100
+            } park()
+        }
+
+        scenario ("WakerUpper supports multiple concurrent threads blocking") {
+            val timesToWakeUp = 500
+            val threads = (0 until 4) map { _ =>
+                new Thread() with WaitContext {
+                    @volatile var timesWokenUp = 0
+
+                    override def run(): Unit =
+                            park()
+
+                    override def shouldWakeUp(): Boolean =
+                        if (this eq Thread.currentThread()) {
+                            Thread.sleep(2)
+                            timesWokenUp >= timesToWakeUp
+                        } else if (ThreadLocalRandom.current().nextInt(100) < 25) {
+                            timesWokenUp += 1
+                            true
+                        } else {
+                            false
+                        }
+                }
+            }
+
+            threads foreach (_.start())
+            threads foreach (_.join())
+            threads foreach (_.timesWokenUp should be (timesToWakeUp))
+        }
+
+        scenario ("Other sources of wake up are handled gracefully") {
+            val timesToWakeUp = 500
+            val threads = (0 until 3) map { _ =>
+                new Thread() with WaitContext {
+                    @volatile var timesWokenUp = 0
+
+                    override def run(): Unit =
+                        while (timesWokenUp < timesToWakeUp) {
+                            try {
+                                park()
+                            } catch { case _: InterruptedException =>
+                            }
+                        }
+
+                    override def shouldWakeUp(): Boolean =
+                        if (this eq Thread.currentThread()) {
+                            Thread.sleep(2)
+                            timesWokenUp >= timesToWakeUp
+                        } else if (ThreadLocalRandom.current().nextInt(100) < 25) {
+                            timesWokenUp += 1
+                            true
+                        } else {
+                            false
+                        }
+                    }
+                } toArray
+
+            @volatile var stopped = false
+            val chaosThread = new Thread() {
+                override def run(): Unit =
+                    while (!stopped) {
+                        threads(ThreadLocalRandom.current().nextInt(0, 3)).interrupt()
+                        Thread.sleep(2)
+                    }
+            }
+
+            threads foreach (_.start())
+            chaosThread.start()
+            threads foreach (_.join())
+            stopped = true
+            chaosThread.join()
+
+            threads foreach (_.timesWokenUp should be (timesToWakeUp))
+      }
+    }
+}
