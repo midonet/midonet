3677fcba286f7e04e84eeb26a1231e97d02a8028
Nicolas Schiper <nicolas@midokura.com>
2014-11-10 08:26:00 +0100

    A Tunnel Zone Mapper
    This observable class can be used by the agent to retrieve a tunnel zone
    simulation object, which contains the IP addresses of the hosts in this
    tunnel zone.
    
    Ref: MN-3066
    
    Change-Id: I0290a04dab7e116bd1b037bf44c9413551730b49
    Signed-off-by: Nicolas Schiper <nicolas@midokura.com>

--- midolman/src/test/scala/org/midonet/midolman/topology/devices/TunnelZoneConversionTest.scala
+++ midolman/src/test/scala/org/midonet/midolman/topology/devices/TunnelZoneConversionTest.scala
@@ -25,9 +25,12 @@ import org.scalatest.{FeatureSpec, Matchers}
 
 import org.midonet.cluster.data.ZoomConvert
 import org.midonet.cluster.models.Topology
+import org.midonet.cluster.models.Topology.TunnelZone
+import org.midonet.cluster.util.IPAddressUtil
 import org.midonet.cluster.util.UUIDUtil._
 import org.midonet.sdn.flows.FlowTagger
 import org.midonet.sdn.flows.FlowTagger.FlowTag
+import org.midonet.packets.IPAddr
 
 @RunWith(classOf[JUnitRunner])
 class TunnelZoneConversionTest extends FeatureSpec with Matchers {
@@ -55,7 +58,7 @@ class TunnelZoneConversionTest extends FeatureSpec with Matchers {
             setZoomObjFields(zoomObj)
             val proto = ZoomConvert.toProto(zoomObj, classOf[Topology.TunnelZone])
 
-            proto should not be None
+            proto should not be null
             proto.getType should be(Topology.TunnelZone.Type.GRE)
             assertEquals(proto, zoomObj)
         }
@@ -80,7 +83,7 @@ class TunnelZoneConversionTest extends FeatureSpec with Matchers {
             setZoomObjFields(zoomObj)
             val proto = ZoomConvert.toProto(zoomObj, classOf[Topology.TunnelZone])
 
-            proto should not be None
+            proto should not be null
             proto.getType should be(Topology.TunnelZone.Type.VXLAN)
             assertEquals(proto, zoomObj)
         }
@@ -105,7 +108,7 @@ class TunnelZoneConversionTest extends FeatureSpec with Matchers {
             setZoomObjFields(zoomObj)
             val proto = ZoomConvert.toProto(zoomObj, classOf[Topology.TunnelZone])
 
-            proto should not be None
+            proto should not be null
             proto.getType should be(Topology.TunnelZone.Type.VTEP)
             assertEquals(proto, zoomObj)
         }
@@ -115,23 +118,30 @@ class TunnelZoneConversionTest extends FeatureSpec with Matchers {
                             zoomObj: TunnelZone) = {
         protoBuf.getId.asJava should be (zoomObj.id)
         protoBuf.getName should be (zoomObj.name)
-        protoBuf.getHostIdsList.size() should be(zoomObj.hostIds.size)
-        protoBuf.getHostIdsList foreach(id => {
-            zoomObj.hostIds should contain(id.asJava)
+        protoBuf.getHostsList.size() should be(zoomObj.hosts.size)
+        protoBuf.getHostsList foreach(host => {
+            val ip = IPAddressUtil.toIPAddr(host.getIp)
+            zoomObj.hosts.get(host.getHostId.asJava) should be(Some(ip))
         })
     }
 
-   private def newProto = {
-       Topology.TunnelZone.newBuilder
-       .setId(UUID.randomUUID.asProto)
-       .setName("toto")
-       .addHostIds(UUID.randomUUID().asProto)
-       .addHostIds(UUID.randomUUID().asProto)
+    private def newProto = {
+        Topology.TunnelZone.newBuilder
+            .setId(UUID.randomUUID.asProto)
+            .setName("toto")
+            .addHosts(newHostToIp("192.168.0.1"))
     }
 
-   private def setZoomObjFields(zoomObj: TunnelZone) = {
-       zoomObj.id = UUID.randomUUID()
-       zoomObj.name = "toto"
-       zoomObj.hostIds = Set(UUID.randomUUID(), UUID.randomUUID())
-   }
+    private def newHostToIp(ip: String) = {
+        TunnelZone.HostToIp.newBuilder
+            .setHostId(UUID.randomUUID().asProto)
+            .setIp(IPAddressUtil.toProto(ip))
+            .build()
+    }
+
+    private def setZoomObjFields(zoomObj: TunnelZone) = {
+        zoomObj.id = UUID.randomUUID()
+        zoomObj.name = "toto"
+        zoomObj.hosts = Map(UUID.randomUUID() -> IPAddr.fromString("192.168.0.1"))
+    }
 }
