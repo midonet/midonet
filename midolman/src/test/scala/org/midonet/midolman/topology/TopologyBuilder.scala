eebb41869be0428495cf0b16618805bb5c3de011
Nicolas Schiper <nicolas@midokura.com>
2014-11-10 08:26:00 +0100

    A Tunnel Zone Mapper
    This observable class can be used by the agent to retrieve a tunnel zone
    simulation object, which contains the IP addresses of the hosts in this
    tunnel zone.
    
    Ref: MN-3066
    
    Change-Id: I0290a04dab7e116bd1b037bf44c9413551730b49
    Signed-off-by: Nicolas Schiper <nicolas@midokura.com>

---
+++ midolman/src/test/scala/org/midonet/midolman/topology/TopologyBuilder.scala
@@ -0,0 +1,181 @@
+/*
+ * Copyright 2014 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.midonet.midolman.topology
+
+import java.util.UUID
+
+import scala.collection.mutable
+import scala.util.Random
+
+import org.midonet.cluster.data.ZoomConvert
+import org.midonet.cluster.models.Topology._
+import org.midonet.cluster.util.MapConverter
+import org.midonet.cluster.util.IPAddressUtil._
+import org.midonet.cluster.util.IPSubnetUtil._
+import org.midonet.cluster.util.UUIDUtil._
+import org.midonet.midolman.topology.devices.{HostIpConverter, PortInterfaceConverter}
+import org.midonet.packets._
+
+trait TopologyBuilder {
+
+    import TopologyBuilder._
+
+    protected def createNetworkPort(id: UUID = UUID.randomUUID,
+                                    networkId: UUID = UUID.randomUUID,
+                                    inboundFilterId: UUID = UUID.randomUUID,
+                                    outboundFilterId: UUID = UUID.randomUUID,
+                                    tunnelKey: Long = -1L,
+                                    peerId: UUID = UUID.randomUUID,
+                                    vifId: UUID = UUID.randomUUID,
+                                    hostId: UUID = UUID.randomUUID,
+                                    interfaceName: String = "",
+                                    adminStateUp: Boolean = true,
+                                    portGroupIds: Set[UUID] = Set.empty,
+                                    vlanId: Int = 0): Port = {
+        createPortBuilder(id, networkId, inboundFilterId,
+                                        outboundFilterId, tunnelKey, peerId,
+                                        vifId, hostId, interfaceName,
+                                        adminStateUp, portGroupIds)
+            .setVlanId(vlanId)
+            .build
+    }
+
+    protected def createRouterPort(id: UUID = UUID.randomUUID,
+                                   networkId: UUID = UUID.randomUUID,
+                                   inboundFilterId: UUID = UUID.randomUUID,
+                                   outboundFilterId: UUID = UUID.randomUUID,
+                                   tunnelKey: Long = -1L,
+                                   peerId: UUID = UUID.randomUUID,
+                                   vifId: UUID = UUID.randomUUID,
+                                   hostId: UUID = UUID.randomUUID,
+                                   interfaceName: String = "",
+                                   adminStateUp: Boolean = true,
+                                   portGroupIds: Set[UUID] = Set.empty,
+                                   portSubnet: IPSubnet[_] = randomIPv4Subnet,
+                                   portAddress: IPAddr = IPv4Addr.random,
+                                   portMac: MAC = MAC.random): Port = {
+        createPortBuilder(id, networkId, inboundFilterId,
+                                        outboundFilterId, tunnelKey, peerId,
+                                        vifId, hostId, interfaceName,
+                                        adminStateUp, portGroupIds)
+            .setPortSubnet(portSubnet.asProto)
+            .setPortAddress(portAddress.asProto)
+            .setPortMac(portMac.toString)
+            .build
+    }
+
+    protected def createVxLanPort(id: UUID = UUID.randomUUID,
+                                  networkId: UUID = UUID.randomUUID,
+                                  inboundFilterId: UUID = UUID.randomUUID,
+                                  outboundFilterId: UUID = UUID.randomUUID,
+                                  tunnelKey: Long = -1L,
+                                  peerId: UUID = UUID.randomUUID,
+                                  vifId: UUID = UUID.randomUUID,
+                                  hostId: UUID = UUID.randomUUID,
+                                  interfaceName: String = "",
+                                  adminStateUp: Boolean = true,
+                                  portGroupIds: Set[UUID] = Set.empty,
+                                  vxLanMgmtIp: IPAddr = IPv4Addr.random,
+                                  vxLanMgmtPort: Int = random.nextInt(),
+                                  vxLanVni: Int = random.nextInt(),
+                                  vxLanTunnelIp: IPAddr = IPv4Addr.random,
+                                  vxLanTunnelZoneId: UUID = UUID.randomUUID): Port = {
+        createPortBuilder(id, networkId, inboundFilterId,
+                                        outboundFilterId, tunnelKey, peerId,
+                                        vifId, hostId, interfaceName,
+                                        adminStateUp, portGroupIds)
+            .setVxlanMgmtIp(vxLanMgmtIp.asProto)
+            .setVxlanMgmtPort(vxLanMgmtPort)
+            .setVxlanVni(vxLanVni)
+            .setVxlanTunnelIp(vxLanTunnelIp.asProto)
+            .setVxlanTunnelZoneId(vxLanTunnelZoneId.asProto)
+            .build
+    }
+
+    private def createPortBuilder(id: UUID,
+                                  networkId: UUID,
+                                  inboundFilterId: UUID,
+                                  outboundFilterId: UUID,
+                                  tunnelKey: Long,
+                                  peerId: UUID,
+                                  vifId: UUID,
+                                  hostId: UUID,
+                                  interfaceName: String,
+                                  adminStateUp: Boolean,
+                                  portGroupIds: Set[UUID]): Port.Builder = {
+        val builder = Port.newBuilder
+            .setId(id.asProto)
+            .setNetworkId(networkId.asProto)
+            .setInboundFilterId(inboundFilterId.asProto)
+            .setOutboundFilterId(outboundFilterId.asProto)
+            .setTunnelKey(tunnelKey)
+            .setPeerId(peerId.asProto)
+            .setVifId(vifId.asProto)
+            .setHostId(hostId.asProto)
+            .setInterfaceName(interfaceName)
+            .setAdminStateUp(adminStateUp)
+        portGroupIds foreach { id => builder.addPortGroupIds(id.asProto) }
+        builder
+    }
+
+    protected def createTunnelZoneBuilder(id: UUID, name: String,
+                                          hosts: Map[UUID, IPAddr]): TunnelZone.Builder = {
+        val builder = TunnelZone.newBuilder
+            .setId(id.asProto)
+            .setName(name)
+        val converter = getConverter(classOf[HostIpConverter])
+        converter.asInstanceOf[MapConverter[UUID, IPAddr, HostToIp]]
+            .toProto(hosts, classOf[HostToIp])
+        builder
+    }
+
+    protected def createHostBuilder(id: UUID,
+                                  name: String,
+                                  addresses: Set[String],
+                                  portInterfaceMapping: Map[UUID, String],
+                                  floodingProxyWeight: Int,
+                                  tunnelZoneIds: Set[UUID]): Host.Builder = {
+        val builder = Host.newBuilder
+            .setId(id.asProto)
+            .setName(name)
+            .setFloodingProxyWeight(floodingProxyWeight)
+        addresses foreach { ip => builder.addAddresses(IPAddr.fromString(ip).asProto) }
+
+        val converter = getConverter(classOf[PortInterfaceConverter])
+        converter.asInstanceOf[MapConverter[UUID, String, PortToInterface]]
+            .toProto(portInterfaceMapping, classOf[PortToInterface])
+
+        tunnelZoneIds foreach { tunnelId => builder.addTunnelZoneIds(tunnelId.asProto) }
+        builder
+    }
+
+    private def getConverter(clazz: Class[_ <: ZoomConvert.Converter[_,_]])
+        :ZoomConvert.Converter[_,_] = {
+
+        converters.getOrElseUpdate(clazz, clazz.newInstance()
+            .asInstanceOf[ZoomConvert.Converter[_,_]])
+    }
+}
+
+object TopologyBuilder {
+
+    private val random = new Random()
+
+    def randomIPv4Subnet = new IPv4Subnet(random.nextInt(), random.nextInt(32))
+
+    val converters = mutable.Map[Class[_ <: ZoomConvert.Converter[_,_]],
+                                 ZoomConvert.Converter[_,_]]()
+}
---
+++ midonet-util/src/main/scala/org/midonet/util/reactivex/AwaitableObserver.scala
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2014 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.midonet.util.reactivex
+
+import java.util.concurrent.CountDownLatch
+
+import scala.collection.mutable
+import scala.concurrent.duration.Duration
+
+import rx.{Observable, Observer}
+
+object AwaitableObserver {
+
+    trait Notification
+    case class OnNext[T](value: T) extends Notification
+    case class OnCompleted() extends Notification
+    case class OnError(e: Throwable) extends Notification
+
+}
+
+class AwaitableObserver[T](awaitCount: Int = 1) extends Observer[T] {
+
+    import org.midonet.util.reactivex.AwaitableObserver._
+
+    val list = new mutable.MutableList[Notification]
+    var ex: Throwable = _
+    @volatile private var counter = new CountDownLatch(awaitCount)
+
+    override def onNext(value: T): Unit = {
+        list += OnNext(value)
+        counter.countDown()
+    }
+
+    override def onCompleted(): Unit = {
+        list += OnCompleted()
+        counter.countDown()
+    }
+
+    override def onError(e: Throwable): Unit = {
+        ex = e
+        list += OnError(e)
+        counter.countDown()
+    }
+
+    def await(duration: Duration): Boolean = {
+        counter.await(duration.length, duration.unit)
+    }
+
+    def await(duration: Duration, resetCount: Int): Boolean = try {
+        counter.await(duration.length, duration.unit)
+    } finally {
+        counter = new CountDownLatch(resetCount)
+    }
+
+    def reset(resetCount: Int): Unit = {
+        counter = new CountDownLatch(resetCount)
+    }
+
+    def notifications: Seq[Notification] = list
+}
+
+
+class ClassAwaitableObserver[T](awaitCount: Int) extends Observer[Observable[T]] {
+
+    val list = new mutable.MutableList[AwaitableObserver[T]]
+    @volatile private var counter: CountDownLatch = new CountDownLatch(awaitCount)
+
+    def onCompleted {
+        throw new IllegalStateException("Class subscription should not complete.")
+    }
+
+    def onError(e: Throwable) {
+        throw new RuntimeException("Got exception from class subscription", e)
+    }
+
+    def onNext(value: Observable[T]) {
+        val obs = new AwaitableObserver[T](1)
+        value.subscribe(obs)
+        list += obs
+        counter.countDown()
+    }
+
+    def await(duration: Duration, resetCount: Int): Boolean = try {
+        counter.await(duration.length, duration.unit)
+    } finally {
+        counter = new CountDownLatch(resetCount)
+    }
+
+    def reset(newCounter: Int) {
+        counter = new CountDownLatch(newCounter)
+    }
+}
