1338b4fc6b74bb0d2ca4f40da3ef1026c3135381
Duarte Nunes <duarte@midokura.com>
2014-11-11 10:47:42 +0100

    Trade Options for nulls in ObjectPool
    Change-Id: Ic848efef1897de501569c97eac8b3bfb391d8eb9
    Signed-off-by: Duarte Nunes <duarte@midokura.com>

--- midolman/src/main/scala/org/midonet/midolman/DatapathController.scala
+++ midolman/src/main/scala/org/midonet/midolman/DatapathController.scala
@@ -482,7 +482,7 @@ class DatapathController extends Actor
 
         val wMatch = new WildcardMatch().setTunnelKey(tunnelKey)
         val actions = List[FlowAction](port.toOutputAction)
-        val tags = Set(FlowTagger.tagForDpPort(port.getPortNo.shortValue))
+        val tags = Set(FlowTagger.tagForDpPort(port.getPortNo))
         fc ! AddWildcardFlow(WildcardFlow(wcmatch = wMatch, actions = actions),
                              null, new ArrayList[Callback0](), tags)
 
@@ -503,8 +503,7 @@ class DatapathController extends Actor
         //   - The case for invalidating on deactivation is obvious.
         //   - On activation we invalidate flows for this dp port number in case
         //     it has been reused by the dp: we want to start with a clean state
-        FlowController ! InvalidateFlowsByTag(
-            FlowTagger.tagForDpPort(dpPort.getPortNo.shortValue()))
+        FlowController ! InvalidateFlowsByTag(FlowTagger.tagForDpPort(dpPort.getPortNo))
 
         if (active)
             installTunnelKeyFlow(dpPort, tunnelKey)
