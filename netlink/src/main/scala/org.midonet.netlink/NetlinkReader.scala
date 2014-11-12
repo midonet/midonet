20820d4f0f7380df09a72def4451435457a99b66
Duarte Nunes <duarte@midokura.com>
2014-11-12 17:39:15 +0100

    Makes Netlink layer more compositional
    Change-Id: Ie1419c330eef58ae7d7be368eba6a78df376fae6
    Signed-off-by: Duarte Nunes <duarte@midokura.com>

---
+++ netlink/src/main/scala/org.midonet.netlink/NetlinkReader.scala
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2014 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.netlink
+
+import java.io.IOException
+import java.nio.ByteBuffer
+
+import org.midonet.netlink.clib.cLibrary
+import org.midonet.netlink.exceptions.NetlinkException
+
+object NetlinkReader {
+    object MessageTruncated extends NetlinkException(NetlinkException.GENERIC_IO_ERROR,
+                                                     "Message truncated") {
+        override def fillInStackTrace() = this
+    }
+}
+
+/**
+ * Utility class to read Netlink messages off a channel.
+ */
+class NetlinkReader(val channel: NetlinkChannel) {
+
+    import NetlinkReader._
+
+    @throws(classOf[IOException])
+    def close(): Unit =
+        channel.close()
+
+    /**
+     * Reads from the channel into the destination buffer. Netlink errors are
+     * communicated by throwing a NetlinkException. This method will not modify
+     * the buffer's position. The amount of bytes read is returned and it
+     * matches src.remaining().
+     */
+    @throws(classOf[IOException])
+    @throws(classOf[NetlinkException])
+    def read(dst: ByteBuffer): Int = {
+        val start = dst.position()
+        val nbytes = try {
+            channel.read(dst)
+        } finally {
+            dst.position(start)
+        }
+
+        dst.limit(nbytes)
+
+        if (nbytes >= NetlinkMessage.HEADER_SIZE) {
+            val msgType = dst.getShort(start + NetlinkMessage.NLMSG_TYPE_OFFSET)
+
+            if (msgType == NLMessageType.ERROR) {
+                val error = dst.getInt(start + NetlinkMessage.NLMSG_ERROR_OFFSET)
+                if (error != 0) {
+                    val errorMessage = cLibrary.lib.strerror(-error)
+                    throw new NetlinkException(-error, errorMessage)
+                }
+            } else if (NetlinkMessage.isTruncated(dst) ||
+                       msgType == NLMessageType.OVERRUN) {
+                throw MessageTruncated
+            }
+        }
+        nbytes
+    }
+}
---
+++ netlink/src/main/scala/org.midonet.netlink/NetlinkWriter.scala
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2014 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.netlink
+
+import java.io.IOException
+import java.nio.ByteBuffer
+import java.nio.channels.Selector
+
+import scala.concurrent.duration._
+
+/**
+ * Utility class to write Netlink messages into a channel.
+ */
+class NetlinkWriter(val channel: NetlinkChannel) {
+
+    @throws(classOf[IOException])
+    def close(): Unit =
+        channel.close()
+
+    /**
+     * Writes from the source buffer into the the channel. This method will not
+     * modify the buffer's position and limit, returning only the amount of
+     * bytes written. We assume the buffers contains one or more correctly
+     * formatted Netlink messages.
+     */
+    @throws(classOf[IOException])
+    def write(src: ByteBuffer): Int = {
+        val oldLimit = src.limit()
+        val start = src.position()
+        try {
+            channel.write(src)
+        } finally {
+            src.position(start)
+            src.limit(oldLimit)
+        }
+    }
+}
+
+class NetlinkBlockingWriter(channel: NetlinkChannel) extends NetlinkWriter(channel) {
+
+    private val timeout = (100 millis).toMillis
+    private val selector = channel.selector()
+
+    /**
+     * Writes into the underlying channel, blocking regardless of the channel
+     * mode while the write doesn't succeed.
+     */
+    @throws(classOf[IOException])
+    override def write(src: ByteBuffer): Int = {
+        var nbytes = 0
+        while ({nbytes = super.write(src); nbytes } == 0) {
+            if (!channel.isOpen)
+                return 0
+            selector.select(timeout)
+        }
+        nbytes
+    }
+}
