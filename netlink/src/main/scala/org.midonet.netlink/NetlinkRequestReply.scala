559dd7e27bbd803ca170d821e7028db2dfa567e9
Duarte Nunes <duarte@midokura.com>
2014-11-12 17:39:15 +0100

    Makes Netlink layer more compositional
    Change-Id: Ie1419c330eef58ae7d7be368eba6a78df376fae6
    Signed-off-by: Duarte Nunes <duarte@midokura.com>

---
+++ netlink/src/main/scala/org.midonet.netlink/NetlinkRequestReply.scala
@@ -0,0 +1,251 @@
+/*
+ * Copyright 2014 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.netlink
+
+import java.io.IOException
+import java.nio.ByteBuffer
+
+import scala.concurrent.duration._
+
+import org.midonet.Util
+import rx.Observer
+
+import org.midonet.netlink.exceptions.NetlinkException
+import org.midonet.util.concurrent.NanoClock
+
+object NetlinkRequestReply {
+    private val FREE = -1
+    private val unsafe = Util.getUnsafe
+
+    private sealed class NetlinkContext {
+
+        /**
+         * The time when the underlying request expires.
+         */
+        var expiration: Long = _
+
+        /**
+         * onNext is called with the Netlink reply payload (which can be part
+         * of a multi); onError is called when the Netlink reply is an error;
+         * onComplete is called on an ACK or at the end of all the answers to
+         * the request.
+         */
+        var observer: Observer[ByteBuffer] = _
+
+        /**
+         * The sequence number currently active for this context. Because we
+         * re-use NetlinkContext instances, we need this field to validate
+         * whether we're getting a reply for a valid request or if it is the
+         * delayed reply for a timed out request
+         */
+        var sequence: Int = _
+
+        private val sequenceAddress = unsafe.objectFieldOffset(
+                        classOf[NetlinkContext].getDeclaredField("sequence"))
+
+        /**
+         * Called by the thread handling replies. Protects this context
+         * against being incorrectly expired by setting expiration to
+         * Long.MaxValue.
+         */
+        def clear(): Unit = {
+            expiration = Long.MaxValue
+            observer = null
+            unsafe.putOrderedInt(this, sequenceAddress, FREE)
+        }
+
+        /**
+         * Prepare this context for a request.
+         */
+        def prepare(expiration: Long, observer: Observer[ByteBuffer],
+                    sequence: Int): Unit = {
+            this.expiration = expiration
+            this.observer = observer
+            unsafe.putOrderedInt(this, sequenceAddress, sequence)
+        }
+    }
+
+    private val NOOP = new Observer[ByteBuffer] {
+        override def onCompleted(): Unit = { }
+        override def onError(e: Throwable): Unit = { }
+        override def onNext(t: ByteBuffer): Unit = { }
+    }
+
+    private val timeoutException = new NetlinkException(
+        NetlinkException.ErrorCode.ETIMEOUT,
+        "Timeout while waiting for Nelink replies") {
+
+        override def fillInStackTrace() = this
+    }
+}
+
+/**
+ * Class through which to make Netlink requests that require a reply.
+ * Thread-safe for concurrent callers of request and processReplies,
+ * although the individual methods are not thread-safe.
+ */
+class NetlinkRequestReply(reader: NetlinkReader,
+                          writer: NetlinkBlockingWriter,
+                          maxPendingRequests: Int,
+                          readBuf: ByteBuffer,
+                          clock: NanoClock,
+                          timeout: Duration = 1 second) {
+    import org.midonet.netlink.NetlinkRequestReply._
+
+    val capacity = Util.findNextPositivePowerOfTwo(maxPendingRequests)
+    private val mask = capacity - 1
+    private var sequenceNumber = 0
+    private val timeoutNanos = timeout.toNanos
+    private val requests = new Array[NetlinkContext](capacity)
+
+    {
+        var i = 0
+        while (i < capacity) {
+            requests(i) = new NetlinkContext
+            requests(i).clear()
+            i += 1
+        }
+    }
+
+    /**
+     * Makes a request, consisting of a serialized Netlink message, and
+     * registers an Observer through which the reply will be streamed.
+     * Returns the number of bytes written.
+     */
+    @throws(classOf[IOException])
+    def request(src: ByteBuffer, observer: Observer[ByteBuffer]): Int = {
+        val ctx = acquireContext(clock.tick + timeoutNanos, observer)
+        try {
+            if (ctx ne null) {
+                src.putInt(src.position() + NetlinkMessage.NLMSG_SEQ_OFFSET,
+                           ctx.sequence)
+                writer.write(src)
+            } else {
+                0
+            }
+        } catch { case t: Throwable =>
+            if (ctx ne null) {
+                ctx.clear()
+            }
+            observer.onError(t)
+            0
+        }
+    }
+
+    /**
+     * Processes a reply - a stream of ByteBuffers - if one is available.
+     * Any reply that doesn't match a valid sequence number is passed on to
+     * the optional unhandled Observer. Returns the number of bytes read.
+     */
+    @throws(classOf[IOException])
+    def processReply(unhandled: Observer[ByteBuffer] = NOOP): Int =
+        try {
+            val nbytes = reader.read(readBuf)
+            var start = 0
+            while (readBuf.remaining() >= NetlinkMessage.HEADER_SIZE) {
+                val size = readBuf.getInt(start + NetlinkMessage.NLMSG_LEN_OFFSET)
+                handleReply(readBuf, unhandled, start, size)
+                start += size
+                readBuf.position(start)
+            }
+            nbytes
+        } catch { case e: NetlinkException =>
+            val seq = readBuf.getInt(NetlinkMessage.NLMSG_SEQ_OFFSET)
+            val ctx = getContext(seq)
+            getObserver(seq, ctx, unhandled).onError(e)
+            ctx.clear()
+            0
+        } finally {
+            readBuf.clear()
+            doTimeoutExpiration()
+        }
+
+    private def handleReply(reply: ByteBuffer, unhandled: Observer[ByteBuffer],
+                            start: Int, size: Int): Unit = {
+        val seq = readBuf.getInt(start + NetlinkMessage.NLMSG_SEQ_OFFSET)
+        val ctx = getContext(seq)
+        val observer = getObserver(seq, ctx, unhandled)
+
+        val `type` = readBuf.getShort(start + NetlinkMessage.NLMSG_TYPE_OFFSET)
+        if (`type` >= NLMessageType.NLMSG_MIN_TYPE &&
+            size >= NetlinkMessage.GENL_HEADER_SIZE) {
+
+            val flags = readBuf.getShort(start + NetlinkMessage.NLMSG_FLAGS_OFFSET)
+
+            val oldLimit = readBuf.limit()
+            readBuf.limit(start + size)
+            readBuf.position(start + NetlinkMessage.GENL_HEADER_SIZE)
+            observer.onNext(readBuf)
+            readBuf.limit(oldLimit)
+
+            if (NLFlag.isMultiFlagSet(flags)) {
+                return
+            }
+        }
+
+        ctx.clear()
+        observer.onCompleted()
+    }
+
+    private def getContext(seq: Int) = requests(seq & mask)
+
+    private def getObserver(seq: Int, ctx: NetlinkContext,
+                            orElse: Observer[ByteBuffer]) =
+        if (seq != 0 && ctx.sequence == seq) {
+            ctx.observer
+        } else {
+            orElse
+        }
+
+    private def nextSequenceNumber(): Int = {
+        var seq = sequenceNumber + 1
+        if (seq == 0) {
+            seq += 1
+        }
+        sequenceNumber = seq
+        seq
+    }
+
+    private def acquireContext(expiration: Long,
+                               observer: Observer[ByteBuffer]): NetlinkContext = {
+        var i = 0
+        do {
+            val seq = nextSequenceNumber()
+            val ctx = requests(seq & mask)
+            if (ctx.sequence == FREE) {
+                ctx.prepare(expiration, observer, seq)
+                return ctx
+            }
+            i += 1
+        } while (i < capacity)
+        null
+    }
+
+    private def doTimeoutExpiration(): Unit = {
+        val currentTime = clock.tick
+        var i = 0
+        while (i < capacity) {
+            val ctx = requests(i)
+            if (ctx.sequence != FREE && currentTime > ctx.expiration) {
+                val obs = ctx.observer
+                ctx.clear()
+                obs.onError(timeoutException)
+            }
+            i += 1
+        }
+    }
+}
---
+++ netlink/src/test/scala/org/midonet/netlink/NetlinkRequestReplyTest.scala
@@ -0,0 +1,253 @@
+package org.midonet.netlink
+
+import java.nio.ByteBuffer
+
+import scala.concurrent.duration._
+
+import org.junit.runner.RunWith
+import org.scalatest._
+import org.scalatest.junit.JUnitRunner
+
+import rx.Observer
+
+import org.midonet.util.concurrent.MockClock
+import org.midonet.netlink.Netlink.Address
+
+@RunWith(classOf[JUnitRunner])
+class NetlinkRequestReplyTest extends FeatureSpec
+                              with ShouldMatchers
+                              with OneInstancePerTest {
+
+    val maxRequests = 8
+    val channel = new MockNetlinkChannel(Netlink.selectorProvider,
+                                         NetlinkProtocol.NETLINK_GENERIC)
+
+    {
+        channel.connect(new Address(0))
+    }
+
+    val reader = new NetlinkReader(channel)
+    val writer = new MockNetlinkWriter
+    val clock = new MockClock
+    val requestBuf = ByteBuffer.allocate(512)
+    val replyBuf = ByteBuffer.allocate(1024)
+    val requestReply = new NetlinkRequestReply(reader, writer, maxRequests, replyBuf,
+                                               clock, timeout = 1 milli)
+
+    class CountingObserver extends Observer[ByteBuffer] {
+        var onNextCalls = 0
+        var onErrorCalls = 0
+        var onCompleteCalls = 0
+
+        override def onCompleted(): Unit = onCompleteCalls += 1
+        override def onError(e: Throwable): Unit = onErrorCalls += 1
+        override def onNext(t: ByteBuffer): Unit = onNextCalls += 1
+    }
+
+    feature ("Can make requests to a NetlinkRequestReply") {
+        scenario ("A sequence number is added to a netlink request") {
+            NetlinkMessage.writeHeader(requestBuf, 256, 1, 2, 3 /* seq */, 4, 5, 6)
+
+            requestReply.request(requestBuf, null) should be (512)
+            requestBuf.getInt(NetlinkMessage.NLMSG_SEQ_OFFSET) should be (1)
+        }
+
+        scenario ("Errors are communicated through the Observer") {
+            writer.shouldThrow = true
+            val obs = new CountingObserver
+            requestReply.request(requestBuf, obs) should be (0)
+            obs.onErrorCalls should be (1)
+        }
+
+        scenario ("Number of in-flight requests is bounded") {
+            (1 to maxRequests) foreach { i =>
+                requestReply.request(requestBuf, null) should be (512)
+                requestBuf.getInt(NetlinkMessage.NLMSG_SEQ_OFFSET) should be (i)
+            }
+            requestReply.request(requestBuf, null) should be (0)
+        }
+    }
+
+    feature ("Can get replies from a NetlinkRequestReply") {
+        scenario ("An ACK calls into onComplete") {
+            val obs = new CountingObserver
+
+            requestReply.request(requestBuf, obs)
+            val seq = requestBuf.getInt(NetlinkMessage.NLMSG_SEQ_OFFSET)
+
+            val size = NetlinkMessage.NLMSG_ERROR_SIZE + NetlinkMessage.NLMSG_ERROR_HEADER_SIZE
+            NetlinkMessage.writeHeader(replyBuf, size, NLMessageType.ERROR, 0, seq, 0, 0, 0)
+
+            replyBuf.putInt(NetlinkMessage.NLMSG_ERROR_OFFSET, 0)
+            replyBuf.limit(size)
+            requestReply.processReply()
+            obs.onCompleteCalls should be (1)
+            obs.onNextCalls should be (0)
+        }
+
+        scenario ("A no-op calls into onComplete") {
+            val obs = new CountingObserver
+
+            requestReply.request(requestBuf, obs)
+            val seq = requestBuf.getInt(NetlinkMessage.NLMSG_SEQ_OFFSET)
+
+            val size = NetlinkMessage.HEADER_SIZE
+            NetlinkMessage.writeHeader(replyBuf, size , 0, 0, seq, 0, 0, 0)
+
+            replyBuf.limit(size)
+            requestReply.processReply()
+            obs.onCompleteCalls should be (1)
+            obs.onNextCalls should be (0)
+        }
+
+        scenario ("An error calls into onError") {
+            val obs = new CountingObserver
+
+            requestReply.request(requestBuf, obs)
+            val seq = requestBuf.getInt(NetlinkMessage.NLMSG_SEQ_OFFSET)
+
+            val size = NetlinkMessage.NLMSG_ERROR_SIZE + NetlinkMessage.NLMSG_ERROR_HEADER_SIZE
+            NetlinkMessage.writeHeader(replyBuf, size, NLMessageType.ERROR, 0, seq, 0, 0, 0)
+
+            replyBuf.putInt(NetlinkMessage.NLMSG_ERROR_OFFSET, -24)
+            replyBuf.limit(size)
+            requestReply.processReply()
+            obs.onErrorCalls should be (1)
+            obs.onNextCalls should be (0)
+            obs.onCompleteCalls should be (0)
+        }
+
+        scenario ("An answer calls into onNext and onComplete") {
+            val obs = new CountingObserver {
+                override def onNext(buf: ByteBuffer) = {
+                    buf.remaining() should be (16)
+                    super.onNext(buf)
+                }
+            }
+
+            requestReply.request(requestBuf, obs)
+            val seq = requestBuf.getInt(NetlinkMessage.NLMSG_SEQ_OFFSET)
+
+            val size = NetlinkMessage.GENL_HEADER_SIZE + 16
+            NetlinkMessage.writeHeader(replyBuf, size, 160, 0, seq, 0, 0, 0)
+            replyBuf.limit(size)
+
+            requestReply.processReply()
+            obs.onNextCalls should be (1)
+            obs.onCompleteCalls should be (1)
+        }
+
+        scenario ("Can have multiple answers per buffer") {
+            val obs = new CountingObserver {
+                override def onNext(buf: ByteBuffer) = {
+                    buf.remaining() should be (16)
+                    super.onNext(buf)
+                }
+            }
+
+            requestReply.request(requestBuf, obs)
+            val seq1 = requestBuf.getInt(NetlinkMessage.NLMSG_SEQ_OFFSET)
+            requestReply.request(requestBuf, obs)
+            val seq2 = requestBuf.getInt(NetlinkMessage.NLMSG_SEQ_OFFSET)
+
+            val size = NetlinkMessage.GENL_HEADER_SIZE + 16
+            NetlinkMessage.writeHeader(replyBuf, size, 16, 0, seq1, 0, 0, 0)
+            replyBuf.position(size)
+            NetlinkMessage.writeHeader(replyBuf, size, 16, 0, seq2, 0, 0, 0)
+            replyBuf.position(0)
+            replyBuf.limit(size*2)
+
+            requestReply.processReply()
+            obs.onNextCalls should be (2)
+            obs.onCompleteCalls should be (2)
+        }
+
+        scenario ("A multipart answer calls multiple times into onNext") {
+            val obs = new CountingObserver {
+                override def onNext(buf: ByteBuffer) = {
+                    buf.remaining() should be (16)
+                    super.onNext(buf)
+                }
+            }
+
+            requestReply.request(requestBuf, obs)
+            val seq = requestBuf.getInt(NetlinkMessage.NLMSG_SEQ_OFFSET)
+
+            val size = NetlinkMessage.GENL_HEADER_SIZE + 16
+            NetlinkMessage.writeHeader(replyBuf, size, 16, 0, seq, 0, 0, 0)
+            replyBuf.putShort(NetlinkMessage.NLMSG_FLAGS_OFFSET, NLFlag.MULTI)
+            replyBuf.limit(size)
+
+            requestReply.processReply()
+            replyBuf.limit(size)
+            requestReply.processReply()
+
+            NetlinkMessage.writeHeader(replyBuf, NetlinkMessage.GENL_HEADER_SIZE,
+                                       NLMessageType.DONE, 0, seq, 0, 0, 0)
+            replyBuf.limit(NetlinkMessage.GENL_HEADER_SIZE)
+            requestReply.processReply()
+
+            obs.onNextCalls should be (2)
+            obs.onCompleteCalls should be (1)
+        }
+
+        scenario ("An upcall packet is passed into a catch-all Observer") {
+            val obs = new CountingObserver {
+                override def onNext(buf: ByteBuffer) = {
+                    buf.remaining() should be (16)
+                    super.onNext(buf)
+                }
+            }
+            val size = NetlinkMessage.GENL_HEADER_SIZE + 16
+            NetlinkMessage.writeHeader(replyBuf, size, 16, 0, 0, 0, 0, 0)
+            replyBuf.limit(size)
+
+            requestReply.processReply(obs)
+
+            obs.onNextCalls should be (1)
+            obs.onCompleteCalls should be (1)
+        }
+
+        scenario ("A truncated packet is detected") {
+            val obs = new CountingObserver {
+                override def onError(t: Throwable) = {
+                    t should be (NetlinkReader.MessageTruncated)
+                    super.onError(t)
+                }
+            }
+
+            requestReply.request(requestBuf, obs)
+            val seq = requestBuf.getInt(NetlinkMessage.NLMSG_SEQ_OFFSET)
+
+            NetlinkMessage.writeHeader(replyBuf, 1024, 0, 0, seq, 0, 0, 0)
+
+            replyBuf.limit(512)
+            requestReply.processReply()
+            obs.onErrorCalls should be (1)
+        }
+
+    }
+
+    feature ("Requests expire") {
+        scenario ("Expirations are processed after a reply") {
+            val obs = new CountingObserver
+
+            clock.time = 0
+            requestReply.request(requestBuf, obs)
+
+            clock.time = (1 milli).toNanos
+            requestReply.request(requestBuf, obs)
+            requestReply.request(requestBuf, obs)
+
+            clock.time = (1 milli).toNanos + 1
+            replyBuf.limit(0)
+            requestReply.processReply()
+            obs.onErrorCalls should be (1)
+
+            clock.time *= 2
+            replyBuf.limit(0)
+            requestReply.processReply()
+            obs.onErrorCalls should be (3)
+        }
+    }
+}
