43b6610cabedc03deb401c37bfbb9cfdfbf78e52
Galo Navarro <galo@midokura.com>
2014-11-12 10:12:23 +0100

    Replace subscribe with exposed observable in ZOOM
    Rather than the intermediary subject trick, which follows the standard
    pattern in rx libraries and enables easier composition.
    
    This patch also removes the thread that cleaned ZK connections from
    observables that have no subscribers. This is first to eliminate the
    complexity required to thread-safe the management of the various data
    structures involved in keeping caches and gc candidates. Second, because
    the goal of the code was to cancel a given observable's connections GC
    if a subscriber arrived after we decided to GC it, but before we
    actually got to do it. In practise once we expose the observable there is
    no guarantee that we're able to do this, because a potential subscriber
    may retrieve the observable while it's still active, and hold it until
    well after the connection has been GC'd, in which case we have no way of
    notifying it.
    
    The approach proposed here changes this strategy. Whenever we detect
    that the last subscriber is gone, we immediately close the connection.
    The underlying ObservableNodeCache or ObservablePathChildrenCache will
    signal this by emitting an onError on the Observable. In the situation
    described above, any late subscriber will receive a notification
    indicating that it needs to go back to ZOOM and request a new
    Observable. Which can be done easily with the onError operators.
    
    Change-Id: I7380be154294f45599d944437d1afb4589c0355a
    Signed-off-by: Galo Navarro <galo@midokura.com>

--- cluster/src/test/scala/org/midonet/cluster/util/ObservableNodeCacheTest.scala
+++ cluster/src/test/scala/org/midonet/cluster/util/ObservableNodeCacheTest.scala
@@ -15,39 +15,119 @@
  */
 package org.midonet.cluster.util
 
+import java.util.concurrent.{CountDownLatch, TimeUnit}
+
 import org.apache.curator.framework.recipes.cache.ChildData
-
-import rx.observers.TestObserver
-
+import org.junit.Assert.{assertEquals, assertTrue}
 import org.junit.runner.RunWith
 import org.scalatest._
 import org.scalatest.junit.JUnitRunner
+import rx.Subscriber
+import rx.observers.{TestSubscriber, TestObserver}
 
 @RunWith(classOf[JUnitRunner])
 class ObservableNodeCacheTest extends Suite
                               with CuratorTestFramework
                               with Matchers {
+
+    private def makePath(s: String): String = {
+        val path = ZK_ROOT + "/" + s
+        curator.create().forPath(path, s.getBytes)
+        curator.getData.forPath(path)
+        path
+    }
+
     /** Create and delete a node, verify that the observable emits the right
       * contents */
     def testCreateDelete() {
-        val path = ZK_ROOT + "/1"
-        curator.create().forPath(path, "1".getBytes)
+        val path = makePath("1")
+
+        val onc = new ObservableNodeCache(curator, path)
+
+        // Will subscribe before connect
+        val sub1 = new TestObserver[ChildData]()
+        // Will subscribe after connect
+        val sub2 = new TestObserver[ChildData]()
+
+        onc.observable.subscribe(sub1)
+        onc.connect()
+
         Thread.sleep(500)
 
-        val onc = new ObservableNodeCache(curator)
-        onc connect path
-        val cd = new TestObserver[ChildData]()
-        onc.observable.subscribe(cd)
+        sub1.getOnNextEvents should have size 1
+        sub2.getOnNextEvents should have size 0
+
+        onc.observable.subscribe(sub2)
+
         Thread sleep 500
 
-        cd.getOnNextEvents should have size 1
-        cd.getOnNextEvents.get(0).getData should have size 1
+        sub1.getOnNextEvents should have size 1
+        sub2.getOnNextEvents should have size 1
+
+        sub1.getOnNextEvents.get(0).getData should have size 1
+        sub2.getOnNextEvents.get(0).getData should have size 1
         onc.current.getData should be ("1".getBytes)
 
         curator delete() forPath path
         Thread sleep 500
 
-        cd.getOnErrorEvents should be (empty)
-        cd.getOnCompletedEvents should have size 1
+        List(sub1, sub2) foreach { s =>
+            s.getOnErrorEvents should be (empty)
+            s.getOnCompletedEvents should have size 1
+        }
+    }
+
+    def testNonExistentPathOnErrors(): Unit = {
+        val c = new CountDownLatch(1)
+        val onc = new ObservableNodeCache(curator, "/nonExistent")
+        onc.observable.subscribe(new Subscriber[ChildData]() {
+            override def onCompleted(): Unit = fail("unexpected onComplete")
+            override def onError(e: Throwable): Unit = {
+                c.countDown()
+                e match {
+                    case _: NodeCacheOrphaned => // ok
+                    case _ => fail("Unexpected error " + e)
+                }
+            }
+            override def onNext(t: ChildData): Unit = fail("unexpected onNext")
+        })
+        onc.connect()
+        assertTrue(c.await(500, TimeUnit.MILLISECONDS))
+    }
+
+    def testTwoConnectsAreOk(): Unit = {
+        val n = new CountDownLatch(1)
+        val path = makePath("3")
+        val onc = new ObservableNodeCache(curator, path)
+        onc.observable.subscribe(new Subscriber[ChildData]() {
+            override def onError(e: Throwable): Unit = fail("Unexpected " + e)
+            override def onCompleted(): Unit = fail()
+            override def onNext(t: ChildData): Unit = n.countDown()
+        })
+        onc.connect()
+        onc.connect()
+        assertTrue(n.await(500, TimeUnit.MILLISECONDS))
+    }
+
+    def testClosedCacheNotifiesOnError(): Unit = {
+        val n = new CountDownLatch(1)
+        val e = new CountDownLatch(1)
+        val s = new TestSubscriber[ChildData](new Subscriber[ChildData]() {
+            override def onError(t: Throwable): Unit = e.countDown()
+            override def onCompleted(): Unit = fail("Unexpected onComplete")
+            override def onNext(t: ChildData): Unit = n.countDown()
+        })
+        val path = makePath("3")
+        val onc = new ObservableNodeCache(curator, path)
+        onc.observable.subscribe(s)
+
+        onc.connect()
+        assertTrue(n.await(500, TimeUnit.MILLISECONDS))
+        onc.close()
+        assertTrue(e.await(500, TimeUnit.MILLISECONDS))
+
+        s.getOnCompletedEvents shouldBe empty
+        s.getOnNextEvents should have size 1
+        s.getOnErrorEvents should have size 1
     }
 }
