cb106637c490858e35f925198e4e017d85b7b219
Alex Bikfalvi <alex.bikfalvi@midokura.com>
2014-11-13 11:46:05 +0100

    A bridge mapper for the virtual topology
    Adds an implementation of the VirtualDeviceMapper for a
    bridge simulation device.
    
    Ref: MN-3062
    
    Change-Id: I0499d86e58cf2fc89a539abeb8730783812f453c
    Signed-off-by: Alex Bikfalvi <alex.bikfalvi@midokura.com>

--- midolman/src/main/java/org/midonet/cluster/VlanPortMapImpl.java
+++ midolman/src/main/java/org/midonet/cluster/VlanPortMapImpl.java
@@ -23,31 +23,31 @@ import org.midonet.cluster.client.VlanPortMap;
 
 public class VlanPortMapImpl implements VlanPortMap {
 
-        private Map<Short, UUID> dir = new HashMap<Short, UUID>();
-        private Map<UUID, Short> rev = new HashMap<UUID, Short>();
+    private Map<Short, UUID> dir = new HashMap<>();
+    private Map<UUID, Short> rev = new HashMap<>();
 
-        public void add(Short vlanId, UUID portId) {
-            dir.put(vlanId, portId);
-            rev.put(portId, vlanId);
-        }
+    public void add(Short vlanId, UUID portId) {
+        dir.put(vlanId, portId);
+        rev.put(portId, vlanId);
+    }
 
-        @Override
-        public Short getVlan(UUID portId) {
-            return (portId == null) ? null : rev.get(portId);
-        }
+    @Override
+    public Short getVlan(UUID portId) {
+        return (portId == null) ? null : rev.get(portId);
+    }
 
-        @Override
-        public UUID getPort(Short vlanId) {
-            return (vlanId == null) ? null : dir.get(vlanId);
-        }
+    @Override
+    public UUID getPort(Short vlanId) {
+        return (vlanId == null) ? null : dir.get(vlanId);
+    }
 
-        @Override
-        public String toString() {
-            return "Direct vlan->port map: {" + dir.toString() + "}";
-        }
+    @Override
+    public String toString() {
+        return "Direct vlan->port map: {" + dir.toString() + "}";
+    }
 
-        @Override
-        public boolean isEmpty() {
-            return dir.isEmpty();
-        }
+    @Override
+    public boolean isEmpty() {
+        return dir.isEmpty();
+    }
 }
