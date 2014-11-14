e524ed378e93c0d29bf74ddf01f45975948249cc
Tomohiko KIMURA <tomohiko@midokura.com>
2014-11-14 12:21:54 +0900

    Implements ApiTranslator for Neutron Network.
    - NeutronApiTranslator is a common base class for ApiTranslator
      implementations for Neutron model.
    - NetworkTranslator translates operations on a Neutron Network into
      corresponding operations on MidoNet Network.
    
    Fix MN-2999
    
    Change-Id: I4d587887150f5ebff3d8defcca0330545dee700c

---
+++ cluster/src/main/scala/org/midonet/cluster/services/neutron/NetworkTranslator.scala
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2014 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.midonet.cluster.services.neutron
+
+import org.slf4j.LoggerFactory
+
+import org.midonet.cluster.services.c3po.C3POOp
+import org.midonet.cluster.services.c3po.{MidoCreate, MidoDelete, MidoUpdate}
+import org.midonet.cluster.services.c3po.{ModelCreate, ModelDelete, ModelUpdate}
+import org.midonet.cluster.services.c3po.NetworkConverter
+import org.midonet.cluster.services.c3po.TranslationException
+import org.midonet.cluster.data.storage.ReadOnlyStorage
+import org.midonet.cluster.models.Neutron.NeutronNetwork
+import org.midonet.cluster.models.Topology.Network
+
+/**
+ * Provides a Neutron model translator for Network.
+ */
+class NetworkTranslator(override val storage: ReadOnlyStorage)
+        extends NeutronApiTranslator[NeutronNetwork](
+                classOf[NeutronNetwork], classOf[Network], storage) {
+    val log = LoggerFactory.getLogger(classOf[NetworkTranslator])
+
+    @throws[TranslationException]
+    override def toMido(op: C3POOp[NeutronNetwork]) = {
+        try {
+            op match {
+                case c: ModelCreate[_] =>
+                    List(MidoCreate(NetworkConverter.toMido(c.model)))
+                case u: ModelUpdate[_] =>
+                    List(MidoUpdate(NetworkConverter.toMido(u.model)))
+                case d: ModelDelete[_] =>
+                    List(MidoDelete(classOf[Network], d.id))
+            }
+        } catch {
+            case e: Exception =>
+                processExceptions(e, op.opType)
+        }
+    }
+}
