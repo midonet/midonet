afd788b439c910495dc2092b792955138090b84f
Duarte Nunes <duarte@midokura.com>
2014-11-18 13:16:12 +0100

    Adds OvsNetlinkFamilies utility class
    Change-Id: Idafd2fdc2b529caee09635e99dc96015efac7940
    Signed-off-by: Duarte Nunes <duarte@midokura.com>

--- odp/src/test/java/org/midonet/odp/protos/OvsFlowsCreateSetMatchTest.java
+++ odp/src/test/java/org/midonet/odp/protos/OvsFlowsCreateSetMatchTest.java
@@ -44,8 +44,6 @@ public abstract class OvsFlowsCreateSetMatchTest
 
     public void doTest() throws Exception {
 
-        initializeConnection(connection.futures.initialize(), 6);
-
         Future<Datapath> dpFuture = connection.futures.datapathsGet("bibi");
         exchangeMessage();
         Datapath datapath = dpFuture.get();
