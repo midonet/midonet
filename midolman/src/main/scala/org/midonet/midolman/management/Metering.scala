57ace997c22e31c4cc0ac9eb9ccf3afc3db30597
Guillermo Ontanon <guillermo@midokura.jp>
2014-11-28 13:34:46 +0100

    Add metering infrastructure
     * Defines a MeterTag marker trait for FlowTags.
     * Makes the FlowController & FlowManager track those tags as meters.
       that get stats from the flows that have them.
     * Adds a JMX interface to these meters.
     * Adds a command line tool to query them ala vmstat.
    
    Change-Id: I105b896de7a674d7cd54252fd1c49343ea246bed
    Signed-off-by: Guillermo Ontanon <guillermo@midokura.jp>

---
+++ midolman/src/main/scala/org/midonet/midolman/management/Metering.scala
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2014 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.midonet.midolman.management
+
+import java.lang.management._
+import java.util.Collections
+import javax.management._
+
+import com.typesafe.scalalogging.Logger
+import org.slf4j.LoggerFactory
+
+import org.midonet.midolman.monitoring.MeterRegistry
+import org.midonet.odp.flows.FlowStats
+
+object Metering extends MeteringMXBean {
+    val log = Logger(LoggerFactory.getLogger("org.midonet.midolman.management"))
+
+    private val ZERO = new FlowStats
+    private var registry: MeterRegistry = null
+
+    override def listMeters = {
+        val keys = Collections.list(registry.meters.keys)
+        keys.toArray(new Array[String](keys.size()))
+    }
+
+    override def getMeter(name: String) = {
+        val meter = registry.meters.get(name)
+        if (meter ne null)
+            meter
+        else
+            ZERO
+    }
+
+    /* this flag prevents multiple registrations on the same jvm. this would
+     * happen on unit tests */
+    private var registered = false
+
+    def registerAsMXBean(meters: MeterRegistry) = this.synchronized {
+        try {
+            registry = meters
+            if ((meters ne null) && !registered) {
+                ManagementFactory.getPlatformMBeanServer.registerMBean(this,
+                    new ObjectName(MeteringMXBean.NAME))
+                registered = true
+            }
+        } catch {
+            case e: Exception =>
+                log.error("Failed to register metering JMX bean", e)
+        }
+    }
+}
---
+++ midolman/src/test/scala/org/midonet/midolman/monitoring/MeterRegistryTest.scala
@@ -0,0 +1,140 @@
+/*
+ * Copyright 2014 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.midonet.midolman.monitoring
+
+import java.util.UUID
+import scala.collection.JavaConverters._
+
+import org.midonet.odp.flows.FlowStats
+import org.midonet.odp.{FlowMatches, Packet, FlowMatch}
+import org.midonet.packets.{IPv4Addr, MAC, Ethernet}
+import org.midonet.sdn.flows.FlowTagger
+import org.midonet.sdn.flows.FlowTagger.{MeterTag, FlowTag}
+import org.scalatest.{Matchers, FeatureSpec}
+import org.scalatest.junit.JUnitRunner
+import org.junit.runner.RunWith
+
+import org.midonet.packets.util.PacketBuilder._
+
+@RunWith(classOf[JUnitRunner])
+class MeterRegistryTest extends FeatureSpec with Matchers {
+
+    val deviceA: MeterTag = FlowTagger.tagForDevice(UUID.randomUUID()).asInstanceOf[MeterTag]
+    val deviceB: MeterTag = FlowTagger.tagForDevice(UUID.randomUUID()).asInstanceOf[MeterTag]
+    val commonDevice: MeterTag = FlowTagger.tagForDevice(UUID.randomUUID()).asInstanceOf[MeterTag]
+
+    def nonMeterRandomTag = FlowTagger.tagForBroadcast(UUID.randomUUID())
+
+    val packetA: Ethernet = { eth addr MAC.random() -> MAC.random() } <<
+                            { ip4 addr IPv4Addr.random --> IPv4Addr.random } <<
+                            { udp ports 4500 ---> 500 } <<
+                            { payload("foo") }
+    val packetB: Ethernet = { eth addr MAC.random() -> MAC.random() } <<
+                            { ip4 addr IPv4Addr.random --> IPv4Addr.random } <<
+                            { icmp.echo.request id 23 seq 32}
+
+
+    val matchA = FlowMatches.fromEthernetPacket(packetA)
+    val matchB = FlowMatches.fromEthernetPacket(packetB)
+
+    val metersA: List[MeterTag] = List(deviceA, commonDevice)
+    val metersB: List[MeterTag] = List(deviceB, commonDevice)
+
+    val tagsA: Array[FlowTag] = (nonMeterRandomTag :: metersA).toArray
+    val tagsB: Array[FlowTag] = (nonMeterRandomTag :: metersB).toArray
+
+    feature("Meter registry") {
+        scenario("registers new meters") {
+            val registry = new MeterRegistry(10)
+
+            registry.trackFlow(matchA, tagsA)
+            registry.meters should have size 2
+            for (meter <- metersA) {
+                registry.meters.keySet should contain (meter.meterName)
+            }
+
+            registry.trackFlow(matchB, tagsB)
+            registry.meters should have size 3
+            for (meter <- metersB) {
+                registry.meters.keySet should contain (meter.meterName)
+            }
+
+            for (stats <- registry.meters.values.asScala) {
+                stats.packets should === (0)
+                stats.bytes should === (0)
+            }
+        }
+
+        scenario("tracks stats for a single flow, N meters") {
+            val registry = new MeterRegistry(10)
+            registry.trackFlow(matchA, tagsA)
+
+            val stats = new FlowStats()
+            for (i <- 1 to 10) {
+                stats.packets = i
+                stats.bytes = i * 100
+                registry.updateFlow(matchA, stats)
+
+                for (meter <- registry.meters.values.asScala) {
+                    meter.packets should === (i)
+                    meter.bytes should === (i * 100)
+                }
+            }
+        }
+
+        scenario("forgets flows") {
+            val registry = new MeterRegistry(10)
+            registry.trackFlow(matchA, tagsA)
+
+            val fixedPackets = 5
+            val fixedBytes = 55
+
+            val stats = new FlowStats(fixedPackets, fixedBytes)
+            registry.updateFlow(matchA, stats)
+            registry.forgetFlow(matchA)
+
+            stats.packets = 245
+            stats.bytes = 1235
+            registry.updateFlow(matchA, stats)
+            for (meter <- registry.meters.values.asScala) {
+                meter.packets should === (fixedPackets)
+                meter.bytes should === (fixedBytes)
+            }
+        }
+
+        scenario("tracks stats for two flows, overlapping meters") {
+            val registry = new MeterRegistry(10)
+            registry.trackFlow(matchA, tagsA)
+            registry.trackFlow(matchB, tagsB)
+
+            val stats = new FlowStats()
+            for (i <- 1 to 10) {
+                stats.packets = i
+                stats.bytes = i * 100
+                registry.updateFlow(matchA, stats)
+                registry.updateFlow(matchB, stats)
+
+                registry.meters.get(commonDevice.meterName).packets should === (i * 2)
+                registry.meters.get(commonDevice.meterName).bytes should === (i * 200)
+                registry.meters.get(deviceA.meterName).packets should === (i)
+                registry.meters.get(deviceA.meterName).bytes should === (i * 100)
+                registry.meters.get(deviceB.meterName).packets should === (i)
+                registry.meters.get(deviceB.meterName).bytes should === (i * 100)
+
+            }
+        }
+    }
+}
