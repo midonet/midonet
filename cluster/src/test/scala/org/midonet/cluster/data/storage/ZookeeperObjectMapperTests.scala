8358dddbc600175592efaaf03a94191d8eba88d8
Tomohiko KIMURA <tomohiko.kimura@gmail.com>
2014-12-02 19:49:21 +0100

    Removes a print debug statement left behind.
    Change-Id: I696145d8ce75f9911b77eff68c718d2c6b208f97

--- cluster/src/test/scala/org/midonet/cluster/data/storage/ZookeeperObjectMapperTests.scala
+++ cluster/src/test/scala/org/midonet/cluster/data/storage/ZookeeperObjectMapperTests.scala
@@ -357,7 +357,6 @@ class ZookeeperObjectMapperTests extends Suite
     }
 
     def testFlushResetsWatcher() {
-        print("ZOMT: testFlushResetsWatcher.\n")
         zom.flush()
         val zom2 = new ZookeeperObjectMapper(ZK_ROOT, curator)
         initAndBuildZoom(zom2)
