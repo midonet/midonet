8c5d4956b4d77435e757e28b8238ad88ad83ea6e
Brandon Berg <bberg@midokura.com>
2014-12-04 19:29:40 +0100

    Preliminary work for Neutron translators.
    Factor out OneToOneNeutronApiTranslator, and add framework for adding
    translators to NeutronPollingMinion's C3PODataManager instance.
    
    Signed-off-by: Brandon Berg <bberg@midokura.com>
    Change-Id: Idebdfcb0d1af6bd02738d571b412ba0f455274dd

--- cluster/src/main/scala/org/midonet/cluster/services/neutron/NetworkTranslator.scala
+++ cluster/src/main/scala/org/midonet/cluster/services/neutron/NetworkTranslator.scala
@@ -15,35 +15,15 @@
  */
 package org.midonet.cluster.services.neutron
 
-import org.slf4j.LoggerFactory
-
 import org.midonet.cluster.data.storage.ReadOnlyStorage
 import org.midonet.cluster.models.Neutron.NeutronNetwork
 import org.midonet.cluster.models.Topology.Network
-import org.midonet.cluster.services.c3po.{C3POCreate, C3PODelete, C3POOp, C3POUpdate, MidoCreate, MidoDelete, MidoUpdate, NetworkConverter, TranslationException}
+import org.midonet.cluster.services.c3po.NetworkConverter
 
 /**
  * Provides a Neutron model translator for Network.
  */
 class NetworkTranslator(override val storage: ReadOnlyStorage)
-        extends NeutronApiTranslator[NeutronNetwork](
-                classOf[NeutronNetwork], classOf[Network], storage) {
-    val log = LoggerFactory.getLogger(classOf[NetworkTranslator])
-
-    @throws[TranslationException]
-    override def toMido(op: C3POOp[NeutronNetwork]) = {
-        try {
-            op match {
-                case c: C3POCreate[_] =>
-                    List(MidoCreate(NetworkConverter.toMido(c.model)))
-                case u: C3POUpdate[_] =>
-                    List(MidoUpdate(NetworkConverter.toMido(u.model)))
-                case d: C3PODelete[_] =>
-                    List(MidoDelete(classOf[Network], d.id))
-            }
-        } catch {
-            case e: Exception =>
-                processExceptions(e, op.opType)
-        }
-    }
-}
+    extends OneToOneNeutronApiTranslator[NeutronNetwork, Network](
+        classOf[NeutronNetwork], classOf[Network],
+        NetworkConverter.toMido, storage)
