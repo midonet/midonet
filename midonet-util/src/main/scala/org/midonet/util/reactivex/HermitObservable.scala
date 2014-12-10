a652122357bcb03330ed9bfe04cb591c7716e203
Galo Navarro <galo@midokura.com>
2014-12-10 11:59:43 +0100

    Introduce HermitObservable
    A transformation on an Observable that turns it into a hermit that will
    only allow a single subscriber at any given point in time. The resulting
    HermitObservable will emit an onError on any Subscriber that tries to
    connect while another subscriber is still active.
    
    Change-Id: Ic6d78d3c3f385ccb6124f31342ef726eb200121f
    Signed-off-by: Galo Navarro <galo@midokura.com>

---
+++ midonet-util/src/main/scala/org/midonet/util/reactivex/HermitObservable.scala
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2014 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.util.reactivex
+
+import java.util.concurrent.atomic.AtomicBoolean
+
+import rx.Observable.OnSubscribe
+import rx.{Observable, Subscriber, Subscription}
+
+import org.midonet.util.functors.makeAction0
+
+/** Turns an Observable into a hermit, only accepting a single subscription at
+  * any given point in time. */
+object HermitObservable {
+
+    /** Rejects a subscription, for it has one already. */
+    class HermitOversubscribedException
+        extends RuntimeException("Observable already has a subscription")
+
+    implicit def hermitize[T](o: Observable[T]): Observable[T]
+        = Observable.create(new OnSubscribeHermit(o))
+
+    /** Turns a given Observable into a hermit that will only accept a single
+      * subscriber at any point in time.
+      *
+      * @param o the Observable to turn into a hermit
+      * @throws HermitOversubscribedException if a subscriber tries to subscribe
+      *                                       while another subscriber is
+      *                                       already subscribed
+      */
+    class OnSubscribeHermit[T](private val o: Observable[T])
+        extends OnSubscribe[T] {
+
+        private var current: Subscription = _
+        private val owned = new AtomicBoolean(false)
+
+        override def call(subscriber: Subscriber[_ >: T]): Unit = {
+            if (!owned.compareAndSet(false, true)) {
+                throw new HermitOversubscribedException
+            }
+            // we're the sole owners of this observable now, we can subscribe
+            current = o.doOnUnsubscribe ( makeAction0 {
+                                              current.unsubscribe()
+                                              current = null
+                                              owned set false
+                                          } )
+                       .unsafeSubscribe(subscriber)
+            subscriber.add(current)
+        }
+    }
+}
