dad9cbcfca980df5bd7012ef4dc3dbda7e1bf428
Galo Navarro <galo@midokura.com>
2014-12-11 18:48:03 +0100

    ObservablePathNodeCache closes child observables
    When the cache is closed (typically when the ClassSubscriptionCache
    decides to), the main observable was closed but it did not close the
    children observables that it had emitted. This patch corrects this, so
    that any subscribers that could have raced with the close() are always
    aware that they need to request a new Observable.
    
    This patch also adds some minor fixes pending from a previous review,
    plus some buf fixes in connection management (and tests).
    
    Change-Id: Ice43813a678cb12f2de34de98ea93fe3988d7f3e
    Signed-off-by: Galo Navarro <galo@midokura.com>

--- cluster/src/main/scala/org/midonet/cluster/util/ObservableNodeCache.scala
+++ cluster/src/main/scala/org/midonet/cluster/util/ObservableNodeCache.scala
@@ -19,36 +19,48 @@ import java.io.IOException
 import java.util.concurrent.atomic.AtomicBoolean
 
 import org.apache.curator.framework.CuratorFramework
+import org.apache.curator.framework.api.{CuratorEvent, CuratorListener}
 import org.apache.curator.framework.recipes.cache.{ChildData, NodeCache, NodeCacheListener}
+import org.apache.curator.framework.state.{ConnectionState, ConnectionStateListener}
+import org.apache.zookeeper.KeeperException.NoNodeException
 import org.slf4j.LoggerFactory
 import rx.Observable
 import rx.subjects.BehaviorSubject
 
-/** A wrapper around Curator's NodeCache that exposes updates as observables,
-  * guaranteeing that every subscriber will receive the latest known state of
-  * the node, as well as further updates. The underlying NodeCache will manage
-  * the connection to ZK.
+/** A wrapper around Curator's NodeCache that exposes updates as
+  * observables, guaranteeing that every subscriber will receive the
+  * latest known state of the node, as well as further updates until
+  * the node is deleted, when it'll complete the Observable. If the node
+  * doen't exist at the moment when the Observable is created, it will
+  * immediately emit an onError with a NoNodeException.
   *
-  * A single call to connect() is required before the observable starts emitting
-  * elements. Further calls to connect() will be ignored.
+  * The underlying NodeCache will manage the connection to ZK according
+  * to the configuration set in the CuratorFramework instance provided.
+  * Whenever the retry mechanism gives up and the connection is
+  * permanently lost, the Observable will emit an onError with a
+  * NodeCacheDisconnected. Any new subscribers will get the same onError
+  * immediately after subscribing. This implies that the Observable is
+  * unusable, users wanting to keep monitoring the same node should
+  * create a new instance.
   *
-  * A call to close() will forcefully disconnect the underlying NodeCache. The
-  * existing subscribers will be notified through a NodeCacheDisconnected
-  * exception on the Observable.
+  * A single call to connect() is required before the observable starts
+  * emitting elements. Further calls to connect() will be ignored.
+  *
+  * A call to close() will forcefully disconnect the underlying
+  * NodeCache. The existing subscribers will be notified through a
+  * NodeCacheDisconnected exception on the Observable.
   */
 class ObservableNodeCache(zk: CuratorFramework, path: String) {
 
-    private val log = LoggerFactory.getLogger(classOf[ObservableNodeCache])
-
     private val connected = new AtomicBoolean(false)
-    private val completed = new AtomicBoolean(false)
+    private val terminated = new AtomicBoolean(false)
     private val stream = BehaviorSubject.create[ChildData]()
 
     /* Stream of updates to the node */
     private val nodeCache = new NodeCache(zk, path)
-    nodeCache.getListenable.addListener(new NodeCacheListener() {
+    nodeCache.getListenable.addListener(new NodeCacheListener {
         override def nodeChanged() {
-            if (!completed.get()) {
+            if (!terminated.get()) {
                 nodeCache.getCurrentData match {
                     case cd: ChildData => stream.onNext(cd)
                     case _ => stream.onCompleted()
@@ -57,19 +69,35 @@ class ObservableNodeCache(zk: CuratorFramework, path: String) {
         }
     })
 
-    /** Return the current state of the watched node, or null if not tracked */
+    zk.getConnectionStateListenable.addListener(new ConnectionStateListener {
+        override def stateChanged(client: CuratorFramework,
+                                  newState: ConnectionState): Unit = {
+            newState match {
+                case ConnectionState.LOST =>
+                    terminated.set(true)
+                    try {
+                        nodeCache.close()
+                    } finally {
+                        stream.onError(new NodeCacheDisconnected(path))
+                    }
+                case _ =>
+            }
+        }
+    })
+
+    /** Return the current state of the watched node, or null if the node is
+      * not tracked, or has been deleted. */
     def current = nodeCache.getCurrentData
 
     /** Instruct the observable to start emitting node states. This is a
       * BLOCKING operation. */
     def connect(): Unit = {
         if (!connected.compareAndSet(false, true)) {
-            log.warn(s"connect() called more than once on $path, ignored")
             return
         }
         nodeCache.start(true)
         if (current == null) {
-            stream.onError(new NodeCacheOrphaned(s"Absent path: $path"))
+            stream.onError(new NoNodeException(s"Absent path: $path"))
         } else {
             stream.onNext(current)
         }
@@ -87,7 +115,7 @@ class ObservableNodeCache(zk: CuratorFramework, path: String) {
       */
     @throws[IOException]
     def close(): Unit = {
-        if (!completed.compareAndSet(false, true)) {
+        if (!terminated.compareAndSet(false, true)) {
             return
         }
         stream.onError(new NodeCacheDisconnected(path))
@@ -103,7 +131,4 @@ class ObservableNodeCache(zk: CuratorFramework, path: String) {
 }
 
 /** Signals that the underlying NodeCache has lost the connection to ZK. */
-class NodeCacheDisconnected(s: String) extends RuntimeException
-
-/** Signals that the underlying NodeCache relies on a non existent path */
-class NodeCacheOrphaned(s: String) extends RuntimeException(s)
+class NodeCacheDisconnected(s: String) extends RuntimeException(s)
