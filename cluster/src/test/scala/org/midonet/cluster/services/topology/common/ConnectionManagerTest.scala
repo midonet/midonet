4ac5b8375add6df976367958c86d6361f86da559
Ernest Artiaga <ernest@midokura.com>
2014-12-11 17:49:53 +0100

    Topology client/server libraries
    Helper libraries for transporting protobuf-based
    topology data over netty connections.
    
    Ref: MN-2985
    
    Change-Id: Iaa31cb80fb5ddd27157ef7ee8800b2b10aba607a
    Signed-off-by: Ernest Artiaga <ernest@midokura.com>

---
+++ cluster/src/test/scala/org/midonet/cluster/services/topology/common/ConnectionManagerTest.scala
@@ -0,0 +1,114 @@
+/*
+ * Copyright 2014 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.cluster.services.topology.common
+
+import scala.concurrent.Promise
+
+import com.google.protobuf.Message
+
+import org.junit.runner.RunWith
+import org.mockito.Mockito
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{FeatureSpec, Matchers}
+
+import rx.{Observer, Subscription}
+
+import io.netty.channel.ChannelHandlerContext
+
+
+@RunWith(classOf[JUnitRunner])
+class ConnectionManagerTest extends FeatureSpec with Matchers {
+
+    feature("Get the connection associated to a low level channel")
+    {
+        scenario("register a new connection") {
+            val ctx = Mockito.mock(classOf[ChannelHandlerContext])
+            val protocol = Mockito.mock(classOf[ProtocolFactory])
+            val start = (Mockito.mock(classOf[State]),
+                Promise[Option[Subscription]]().success(None).future)
+            Mockito.stub(
+                protocol.start(org.mockito.Matchers.anyObject[Observer[Message]])
+            ).toReturn(start)
+
+            val cMgr = new ConnectionManager(protocol)
+            val conn = cMgr.get(ctx)
+
+            conn should not be null
+        }
+
+        scenario("retrieve different connections") {
+            val ctx1 = Mockito.mock(classOf[ChannelHandlerContext])
+            val ctx2 = Mockito.mock(classOf[ChannelHandlerContext])
+            val protocol = Mockito.mock(classOf[ProtocolFactory])
+            val start = (Mockito.mock(classOf[State]),
+                Promise[Option[Subscription]]().success(None).future)
+            Mockito.stub(
+                protocol.start(org.mockito.Matchers.anyObject[Observer[Message]])
+            ).toReturn(start)
+
+            val cMgr = new ConnectionManager(protocol)
+            val conn1 = cMgr.get(ctx1)
+            val conn2 = cMgr.get(ctx2)
+
+            conn1 should not be null
+            conn2 should not be null
+            conn2 should not be conn1
+        }
+
+        scenario("retrieve the same connection several times") {
+            val ctx1 = Mockito.mock(classOf[ChannelHandlerContext])
+            val protocol = Mockito.mock(classOf[ProtocolFactory])
+            val start = (Mockito.mock(classOf[State]),
+                Promise[Option[Subscription]]().success(None).future)
+            Mockito.stub(
+                protocol.start(org.mockito.Matchers.anyObject[Observer[Message]])
+            ).toReturn(start)
+
+            val cMgr = new ConnectionManager(protocol)
+            val conn1 = cMgr.get(ctx1)
+            val conn2 = cMgr.get(ctx1)
+
+            conn1 should not be null
+            conn2 should not be null
+            conn2 should be (conn1)
+        }
+
+        scenario("unregister and register a connection") {
+            val ctx1 = Mockito.mock(classOf[ChannelHandlerContext])
+            val protocol = Mockito.mock(classOf[ProtocolFactory])
+            val start = (Mockito.mock(classOf[State]),
+                Promise[Option[Subscription]]().success(None).future)
+            Mockito.stub(
+                protocol.start(org.mockito.Matchers.anyObject[Observer[Message]])
+            ).toReturn(start)
+
+            val cMgr = new ConnectionManager(protocol)
+            val conn1 = cMgr.get(ctx1)
+
+            conn1 should not be null
+
+            cMgr.unregister(ctx1)
+            val conn2 = cMgr.get(ctx1)
+
+            conn1 should not be null
+            conn2 should not be null
+            conn2 should not be conn1
+        }
+    }
+}
+
+
