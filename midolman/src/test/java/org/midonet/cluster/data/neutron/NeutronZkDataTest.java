4c1dcdca01f756e9f0722de99ccb9ef8f13b82cb
Ryu Ishimoto <ryu@midokura.com>
2014-12-12 21:08:33 +0000

    Clean up FIP after external net deletion
    Delete floating IP addresses that belong to the external network
    when the external network is deleted.
    
    Change-Id: I8f4f93801d460470615d8b63cf439d7cafcea18a
    Fixes: MNA-26

--- midolman/src/test/java/org/midonet/cluster/data/neutron/NeutronZkDataTest.java
+++ midolman/src/test/java/org/midonet/cluster/data/neutron/NeutronZkDataTest.java
@@ -22,8 +22,10 @@ import org.apache.curator.test.TestingServer;
 import org.junit.Before;
 import org.junit.Test;
 
+import org.midonet.midolman.serialization.SerializationException;
 import org.midonet.midolman.state.DirectoryVerifier;
 import org.midonet.midolman.state.PathBuilder;
+import org.midonet.midolman.state.StateAccessException;
 
 public final class NeutronZkDataTest extends NeutronPluginTest {
 
@@ -39,7 +41,24 @@ public final class NeutronZkDataTest extends NeutronPluginTest {
         dirVerifier = new DirectoryVerifier(getDirectory());
     }
 
-    private void verifyFloatingIp() {
+    private void verifyFipDnatRule(int expectedMatchCnt) {
+
+        String rulesPath = pathBuilder.getRulesPath();
+
+        String floatingIpAddr = floatingIp.floatingIpAddress;
+        String fixedIpAddr = floatingIp.fixedIpAddress;
+
+        Map<String, Object> matches = new HashMap<>();
+        matches.put("type", "ForwardNat");
+        matches.put("condition.nwDstIp.address", floatingIpAddr);
+        matches.put("natTargets[0].nwStart", fixedIpAddr);
+        matches.put("natTargets[0].nwEnd", fixedIpAddr);
+
+        dirVerifier.assertChildrenFieldsMatch(rulesPath, matches,
+                                              expectedMatchCnt);
+    }
+
+    private void verifyFipSnatRule(int expectedMatchCnt) {
 
         String rulesPath = pathBuilder.getRulesPath();
 
@@ -52,21 +71,31 @@ public final class NeutronZkDataTest extends NeutronPluginTest {
         matches.put("natTargets[0].nwStart", floatingIpAddr);
         matches.put("natTargets[0].nwEnd", floatingIpAddr);
 
-        dirVerifier.assertChildrenFieldsMatch(rulesPath, matches, 1);
+        dirVerifier.assertChildrenFieldsMatch(rulesPath, matches,
+                                              expectedMatchCnt);
+    }
 
-        matches = new HashMap<>();
-        matches.put("type", "ForwardNat");
-        matches.put("condition.nwDstIp.address", floatingIpAddr);
-        matches.put("natTargets[0].nwStart", fixedIpAddr);
-        matches.put("natTargets[0].nwEnd", fixedIpAddr);
+    private void verifyFloatingIpRules() {
 
-        dirVerifier.assertChildrenFieldsMatch(rulesPath, matches, 1);
+        verifyFipSnatRule(1);
+        verifyFipDnatRule(1);
+
+    }
+
+    public void verifyNoFloatingIpRules() {
+
+        verifyFipSnatRule(0);
+        verifyFipDnatRule(0);
     }
 
     @Test
-    public void testBasicScenario() {
+    public void testBasicScenario()
+        throws SerializationException, StateAccessException {
 
-        verifyFloatingIp();
+        verifyFloatingIpRules();
 
+        plugin.deleteNetwork(extNetwork.id);
+
+        verifyNoFloatingIpRules();
     }
 }
