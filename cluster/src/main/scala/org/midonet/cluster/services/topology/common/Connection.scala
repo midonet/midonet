34d3b18068da9d70928d63b7ace33d9788c8c7c6
Ernest Artiaga <ernest@midokura.com>
2014-12-17 12:12:01 +0100

    Topology API connection fixes
    Assorted changes to the Topology API
    Connection class to simplify the code and
    protect it against miss-behaved protocol
    implementations.
    
    Ref: MN-2985
    
    Change-Id: Ib348a03d382f500b2782360fb76564187ebec177
    Signed-off-by: Ernest Artiaga <ernest@midokura.com>

--- cluster/src/main/scala/org/midonet/cluster/services/topology/common/Connection.scala
+++ cluster/src/main/scala/org/midonet/cluster/services/topology/common/Connection.scala
@@ -16,6 +16,8 @@
 
 package org.midonet.cluster.services.topology.common
 
+import java.util.concurrent.atomic.AtomicBoolean
+
 import scala.util.Success
 
 import com.google.protobuf.Message
@@ -42,26 +44,20 @@ class Connection(private val ctx: ChannelHandlerContext,
                  private val protocol: ProtocolFactory)
                 (implicit val mgr: ConnectionManager)
     extends Subscriber[Message] {
-    private val log = LoggerFactory.getLogger(classOf[Connection])
 
-    // Stream of messages to be sent back through the communication channel;
-    // this object 'subscribes' to this stream to actually send out the
-    // outgoing messages
-    private val outgoing: Subject[Message, Message] =
-        PublishSubject.create()
-    outgoing.subscribe(this)
+    // Connection has already been disconnected
+    private val done = new AtomicBoolean(false)
 
     // Send a message through the low level channel
     // TODO: some backpressure mechanism is probably needed here
-    private def send(rsp: Message) = ctx.writeAndFlush(rsp)
+    private def send(rsp: Message) = if (!done.get()) ctx.writeAndFlush(rsp)
 
     // Terminate this connection
-    private def terminate() = {
+    private def terminate() = if (done.compareAndSet(false, true)) {
         backendSubscription.value match {
             case Some(Success(Some(subs))) => subs.unsubscribe()
             case _ =>
         }
-        this.unsubscribe()
         ctx.close()
         mgr.unregister(ctx)
     }
@@ -73,19 +69,36 @@ class Connection(private val ctx: ChannelHandlerContext,
 
     // State engine
     // TODO: This is not thread-safe, which is currently fine
-    // as each channel is currently handled by a single thread at most.
-    private var (state, backendSubscription) = protocol.start(outgoing)
+    // as each channel is currently handled by a single thread.
+    private var (state, backendSubscription) = protocol.start(this)
+
+    /**
+     * Dismiss the connection state (called upon netty disconnection)
+     */
     def disconnect() = {
         state = state.process(Interruption)
         // in case protocol does not honor the disconnect request:
-        outgoing.onCompleted()
+        terminate()
     }
+
+    /**
+     * Process a protobuf message received from netty
+     * @param req the protobuf message enconding either a request or a
+     *            response of a given protocol. Note that it is not safe
+     *            to keep this protobuf for future use without increasing
+     *            its reference counter via 'retain' (and releasing it
+     *            when no longer needed via 'ReferenceCountUtils.release'
+     */
     def msg(req: Message) = {
         state = state.process(req)
-        // note: the req message is being released by the caller;
-        // its refcount should be increased (via 'retain') by the
-        // protocol, if needed)
     }
+
+    /**
+     * Process exceptions originated in the netty pipeline (normally they
+     * are not recuperable, but the high level protocol may want to take
+     * some action).
+     * @param e the captured exception
+     */
     def error(e: Throwable) = {
         state = state.process(e)
     }
