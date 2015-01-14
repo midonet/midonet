989fda49260c6b6ecc169dbda87eb5e2ebef93d9
Ernest Artiaga <ernest@midokura.com>
2015-01-14 11:14:44 +0100

    Port VTEP to new OVSDB client
    Change the VTEP-related classes to make
    use of the new (and mainstream) ovsdb
    client library, so we do not depend on
    our own modified ovsdb sources.
    
    Ref: MN-2543
    
    Change-Id: Ic2118e6afcfcfb8c880c4a61474c8892100fa87d
    Signed-off-by: Ernest Artiaga <ernest@midokura.com>

--- cluster/src/main/java/org/midonet/cluster/data/vtep/VtepException.java
+++ cluster/src/main/java/org/midonet/cluster/data/vtep/VtepException.java
@@ -17,8 +17,6 @@ package org.midonet.cluster.data.vtep;
 
 import org.midonet.cluster.data.vtep.model.VtepEndPoint;
 
-import org.midonet.cluster.data.vtep.model.VtepEndPoint;
-
 /**
  * A checked exception for a VTEP data vtep.
  */
