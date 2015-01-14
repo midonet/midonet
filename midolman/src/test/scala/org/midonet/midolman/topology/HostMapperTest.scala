bb49ff72f0ee885cb9dfaaeed9548b0c62b8739d
Alex Bikfalvi <alex.bikfalvi@midokura.com>
2015-01-14 10:31:07 +0100

    Rearch: Detect the port state in the PortMapper
    Modifies the PortMapper of the virtual topology to detect
    changes in the port state, as notified by the ownership-
    based storage.
    
    Ref: MN-3128
    
    Change-Id: If4cef40af6c70b2ab5a7d8c4997274c25a2c8f21
    Signed-off-by: Alex Bikfalvi <alex.bikfalvi@midokura.com>

--- midolman/src/test/scala/org/midonet/midolman/topology/HostMapperTest.scala
+++ midolman/src/test/scala/org/midonet/midolman/topology/HostMapperTest.scala
@@ -69,7 +69,7 @@ class HostMapperTest extends MidolmanSpec
             val tuple = createZoomObjs
             val protoHost = tuple._1
             val protoTunnelZone = tuple._2
-            val hostMapper = new HostMapper(protoHost.getId.asJava, vt, dataClient)
+            val hostMapper = new HostMapper(protoHost.getId.asJava, vt)
 
             When("We subscribe to the host")
             val hostObs = new AwaitableObserver[SimHost](1)
@@ -89,7 +89,7 @@ class HostMapperTest extends MidolmanSpec
             var tuple = createZoomObjs
             val protoHost = tuple._1
             val protoTunnelZone = tuple._2
-            val hostMapper = new HostMapper(protoHost.getId.asJava, vt, dataClient)
+            val hostMapper = new HostMapper(protoHost.getId.asJava, vt)
 
             When("We subscribe to the host")
             val hostObs = new AwaitableObserver[SimHost](2)
@@ -116,7 +116,7 @@ class HostMapperTest extends MidolmanSpec
             val tuple = createZoomObjs
             val protoHost = tuple._1
             val protoTunnelZone = tuple._2
-            val hostMapper = new HostMapper(protoHost.getId.asJava, vt, dataClient)
+            val hostMapper = new HostMapper(protoHost.getId.asJava, vt)
 
             When("We subscribe to the host")
             val hostObs = new AwaitableObserver[SimHost](2)
@@ -141,7 +141,7 @@ class HostMapperTest extends MidolmanSpec
             val tuple = createZoomObjs
             val protoHost = tuple._1
             val protoTunnelZone = tuple._2
-            val hostMapper = new HostMapper(protoHost.getId.asJava, vt, dataClient)
+            val hostMapper = new HostMapper(protoHost.getId.asJava, vt)
 
             When("We subscribe to the host")
             val hostObs = new AwaitableObserver[SimHost](2)
@@ -165,7 +165,7 @@ class HostMapperTest extends MidolmanSpec
             val tuple = createZoomObjs
             val protoHost = tuple._1
             setHostAliveStatus(protoHost.getId, true /* alive? */)
-            val hostMapper = new HostMapper(protoHost.getId.asJava, vt, dataClient)
+            val hostMapper = new HostMapper(protoHost.getId.asJava, vt)
             When("We subscribe to the host")
             val hostObs = new AwaitableObserver[SimHost](1)
             hostMapper.observable.subscribe(hostObs)
@@ -234,13 +234,13 @@ class HostMapperTest extends MidolmanSpec
 
         val newProtoTunnelZone = newTunnelZone(protoHost.getId.asJava)
         store.create(newProtoTunnelZone)
-        val host2 = Await.result(store.get(classOf[Host], protoHost.getId), 1.second)
+        Await.result(store.get(classOf[Host], protoHost.getId), 1.second)
         val oldTZId = protoHost.getTunnelZoneIds(0).asJava
         val newTZId = newProtoTunnelZone.getId.asJava
         val updatedHost = newHost(protoHost.getId,
                                   Set(oldTZId, newTZId))
         store.update(updatedHost)
-        val host3 = Await.result(store.get(classOf[Host], protoHost.getId), 1.second)
+        Await.result(store.get(classOf[Host], protoHost.getId), 1.second)
 
         (updatedHost, newProtoTunnelZone)
     }
@@ -253,14 +253,12 @@ class HostMapperTest extends MidolmanSpec
     }
 
     private def newTunnelZone(hostId: UUID): TunnelZone = {
-        val tunnelId = UUID.randomUUID()
-        createTunnelZoneBuilder(tunnelId, "foo",
-                                Map(hostId -> IPAddr.fromString("192.168.0.1")))
-                                .build()
+        createTunnelZone(UUID.randomUUID, "foo",
+                         Map(hostId -> IPAddr.fromString("192.168.0.1")))
     }
 
     private def newHost(hostId: UUID, tunnelIds: Set[UUID]): Host = {
-        createHostBuilder(hostId, Map(UUID.randomUUID() -> "eth0"), tunnelIds).build()
+        createHost(hostId, Map(UUID.randomUUID() -> "eth0"), tunnelIds)
     }
 
     private def createZoomObjs: (Host, TunnelZone) = {
