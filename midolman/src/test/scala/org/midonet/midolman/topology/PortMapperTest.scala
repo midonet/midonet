b7b7fea32b95fec81bbbf7d0c2e869214df22ddc
Alex Bikfalvi <alex.bikfalvi@midokura.com>
2015-01-14 10:31:07 +0100

    Rearch: Detect the port state in the PortMapper
    Modifies the PortMapper of the virtual topology to detect
    changes in the port state, as notified by the ownership-
    based storage.
    
    Ref: MN-3128
    
    Change-Id: If4cef40af6c70b2ab5a7d8c4997274c25a2c8f21
    Signed-off-by: Alex Bikfalvi <alex.bikfalvi@midokura.com>

---
+++ midolman/src/test/scala/org/midonet/midolman/topology/PortMapperTest.scala
@@ -0,0 +1,287 @@
+/*
+ * Copyright 2014-2015 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.midonet.midolman.topology
+
+import java.util.UUID
+
+import scala.concurrent.Await._
+import scala.concurrent.duration._
+
+import org.apache.commons.configuration.HierarchicalConfiguration
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+import rx.Observable
+
+import org.midonet.cluster.data.storage.{NotFoundException, StorageWithOwnership}
+import org.midonet.cluster.models.Topology.{Port => TopologyPort}
+import org.midonet.midolman.topology.devices.{Port => SimPort, VxLanPort, RouterPort, BridgePort}
+import org.midonet.midolman.util.MidolmanSpec
+import org.midonet.util.reactivex.AwaitableObserver
+
+@RunWith(classOf[JUnitRunner])
+class PortMapperTest extends MidolmanSpec with TopologyBuilder
+                     with TopologyMatchers {
+
+    private var vt: VirtualTopology = _
+    private implicit var store: StorageWithOwnership = _
+    private var mapper: PortMapper = _
+
+    protected override def beforeTest(): Unit = {
+        vt = injector.getInstance(classOf[VirtualTopology])
+        store = injector.getInstance(classOf[StorageWithOwnership])
+    }
+
+    protected override def fillConfig(config: HierarchicalConfiguration)
+    : HierarchicalConfiguration = {
+        config.setProperty("zookeeper.cluster_storage_enabled", true)
+        config
+    }
+
+    feature("The port mapper emits port devices") {
+        scenario("The mapper emits error for non-existing port") {
+            Given("A port identifier")
+            val id = UUID.randomUUID
+
+            And("A port mapper")
+            val mapper = new PortMapper(id, vt)
+
+            And("An observer to the port mapper")
+            val obs = new AwaitableObserver[SimPort]
+
+            When("The observer subscribes to an observable on the mapper")
+            Observable.create(mapper).subscribe(obs)
+
+            Then("The observer should see a NotFoundException")
+            obs.await(1 second)
+            val e = obs.getOnErrorEvents.get(0).asInstanceOf[NotFoundException]
+            e.clazz shouldBe classOf[TopologyPort]
+            e.id shouldBe id
+        }
+
+        scenario("The mapper emits existing bridge port") {
+            Given("A port mapper")
+            val id = UUID.randomUUID
+            val mapper = new PortMapper(id, vt)
+
+            And("A bridge port")
+            val port = createBridgePort(id = id)
+
+            And("An observer to the port mapper")
+            val obs = new AwaitableObserver[SimPort]
+
+            When("The port is created")
+            store.create(port)
+            ready(store.get(classOf[TopologyPort], id), 1 second)
+
+            And("The observer subscribes to an observable on the mapper")
+            Observable.create(mapper).subscribe(obs)
+
+            Then("The observer should receive a bridge port")
+            obs.await(1 second)
+            val device = obs.getOnNextEvents.get(0).asInstanceOf[BridgePort]
+            device shouldBeDeviceOf port
+            device.active shouldBe false
+        }
+
+        scenario("The mapper emits existing router port") {
+            Given("A port mapper")
+            val id = UUID.randomUUID
+            val mapper = new PortMapper(id, vt)
+
+            And("A router port")
+            val port = createRouterPort(id = id)
+
+            And("An observer to the port mapper")
+            val obs = new AwaitableObserver[SimPort]
+
+            When("The port is created")
+            store.create(port)
+            ready(store.get(classOf[TopologyPort], id), 1 second)
+
+            And("The observer subscribes to an observable on the mapper")
+            Observable.create(mapper).subscribe(obs)
+
+            Then("The observer should receive a router port")
+            obs.await(1 second)
+            val device = obs.getOnNextEvents.get(0).asInstanceOf[RouterPort]
+            device shouldBeDeviceOf port
+            device.active shouldBe false
+        }
+
+        scenario("The mapper emits existing VXLAN port") {
+            Given("A port mapper")
+            val id = UUID.randomUUID
+            val mapper = new PortMapper(id, vt)
+
+            And("A VXLAN port")
+            val port = createVxLanPort(id = id)
+
+            And("An observer to the port mapper")
+            val obs = new AwaitableObserver[SimPort]
+
+            When("The port is created")
+            store.create(port)
+            ready(store.get(classOf[TopologyPort], id), 1 second)
+
+            And("The observer subscribes to an observable on the mapper")
+            Observable.create(mapper).subscribe(obs)
+
+            Then("The observer should receive a router port")
+            obs.await(1 second)
+            val device = obs.getOnNextEvents.get(0).asInstanceOf[VxLanPort]
+            device shouldBeDeviceOf port
+            device.active shouldBe false
+        }
+
+        scenario("The mapper emits new device on port update") {
+            Given("A port mapper")
+            val id = UUID.randomUUID
+            val mapper = new PortMapper(id, vt)
+
+            And("A port")
+            val port1 = createBridgePort(id = id, adminStateUp = false)
+            val port2 = createBridgePort(id = id, adminStateUp = true)
+
+            And("An observer to the port mapper")
+            val obs = new AwaitableObserver[SimPort]
+
+            When("The port is created")
+            store.create(port1)
+            ready(store.get(classOf[TopologyPort], id), 1 second)
+
+            And("The observer subscribes to an observable on the mapper")
+            Observable.create(mapper).subscribe(obs)
+
+            Then("The observer should receive the port")
+            obs.await(1 second, 1)
+            val device1 = obs.getOnNextEvents.get(0).asInstanceOf[BridgePort]
+            device1 shouldBeDeviceOf port1
+            device1.adminStateUp shouldBe false
+            device1.active shouldBe false
+
+            When("The port is updated")
+            store.update(port2)
+            ready(store.get(classOf[TopologyPort], id), 1 second)
+
+            Then("The observer should receive the update")
+            obs.await(1 second)
+            val device2 = obs.getOnNextEvents.get(1).asInstanceOf[BridgePort]
+            device2 shouldBeDeviceOf port2
+            device2.adminStateUp shouldBe true
+            device2.active shouldBe false
+        }
+
+        scenario("The mapper emits new device on port owner update") {
+            Given("A port mapper")
+            val id = UUID.randomUUID
+            val mapper = new PortMapper(id, vt)
+
+            And("A port")
+            val port = createBridgePort(id = id)
+
+            And("An observer to the port mapper")
+            val obs = new AwaitableObserver[SimPort]
+
+            When("The port is created")
+            store.create(port)
+            ready(store.get(classOf[TopologyPort], id), 1 second)
+
+            And("The observer subscribes to an observable on the mapper")
+            Observable.create(mapper).subscribe(obs)
+
+            Then("The observer should receive the port")
+            obs.await(1 second, 1)
+            obs.getOnNextEvents.size shouldBe 1
+            val device1 = obs.getOnNextEvents.get(0).asInstanceOf[BridgePort]
+            device1 shouldBeDeviceOf port
+            device1.active shouldBe false
+
+            When("Adding a first owner to the port")
+            val owner1 = UUID.randomUUID
+            store.updateOwner(classOf[TopologyPort], id, owner1, true)
+            result(store.getOwners(classOf[TopologyPort], id),
+                  1 second) shouldBe Set(owner1.toString)
+
+            Then("The observer should receive the update")
+            obs.await(1 second, 1)
+            obs.getOnNextEvents.size shouldBe 2
+            val device2 = obs.getOnNextEvents.get(1).asInstanceOf[BridgePort]
+            device2 shouldBeDeviceOf port
+            device2.active shouldBe true
+
+            When("Adding a second owner to the port")
+            val owner2 = UUID.randomUUID
+            store.updateOwner(classOf[TopologyPort], id, owner2, true)
+            result(store.getOwners(classOf[TopologyPort], id),
+                1 second) shouldBe Set(owner1.toString, owner2.toString)
+
+            Then("The observer should not receive a new update")
+            obs.getOnNextEvents.size shouldBe 2
+
+            When("Removing the first owner from the port")
+            store.deleteOwner(classOf[TopologyPort], id, owner1)
+            result(store.getOwners(classOf[TopologyPort], id),
+                1 second) shouldBe Set(owner2.toString)
+
+            Then("The observer should not receive a new update")
+            obs.getOnNextEvents.size shouldBe 2
+
+            When("Removing the second owner from the port")
+            store.deleteOwner(classOf[TopologyPort], id, owner2)
+            result(store.getOwners(classOf[TopologyPort], id),
+                1 second) shouldBe Set.empty
+
+            Then("The observer should receive a new update")
+            obs.getOnNextEvents.size shouldBe 3
+            val device3 = obs.getOnNextEvents.get(2).asInstanceOf[BridgePort]
+            device3 shouldBeDeviceOf port
+            device3.active shouldBe false
+        }
+
+        scenario("The mapper completes on port delete") {
+            Given("A port mapper")
+            val id = UUID.randomUUID
+            val mapper = new PortMapper(id, vt)
+
+            And("A port")
+            val port = createBridgePort(id = id)
+
+            And("An observer to the port mapper")
+            val obs = new AwaitableObserver[SimPort]
+
+            When("The port is created")
+            store.create(port)
+            ready(store.get(classOf[TopologyPort], id), 1 second)
+
+            And("The observer subscribes to an observable on the mapper")
+            Observable.create(mapper).subscribe(obs)
+
+            Then("The observer should receive the port")
+            obs.await(1 second, 1)
+            val device1 = obs.getOnNextEvents.get(0).asInstanceOf[BridgePort]
+            device1 shouldBeDeviceOf port
+
+            When("The port is deleted")
+            store.delete(classOf[TopologyPort], id)
+            ready(store.get(classOf[TopologyPort], id), 1 second)
+
+            Then("The observer should receive a completed notification")
+            obs.await(1 second)
+            obs.getOnCompletedEvents should not be empty
+        }
+    }
+}
--- midolman/src/test/scala/org/midonet/midolman/topology/VirtualTopologyTest.scala
+++ midolman/src/test/scala/org/midonet/midolman/topology/VirtualTopologyTest.scala
@@ -66,7 +66,7 @@ class VirtualTopologyTest extends MidolmanSpec with TopologyBuilder {
 
             When("Waiting for the future to complete")
             val e2 = intercept[NotFoundException] {
-                result(future, 1.seconds)
+                result(future, 1 seconds)
             }
 
             Then("The future fails with a NotFoundException")
@@ -75,11 +75,11 @@ class VirtualTopologyTest extends MidolmanSpec with TopologyBuilder {
         }
 
         scenario("The port exists in the topology and is not cached") {
-            Given("A network port")
+            Given("A bridge port")
             val id = UUID.randomUUID
             val port = createBridgePort(id = id)
             store.create(port)
-            ready(store.get(classOf[TopologyPort], id), 1.second)
+            ready(store.get(classOf[TopologyPort], id), 1 second)
 
             When("Requesting the port")
             // The port is not cached, the VT throws a not yet exception
@@ -89,7 +89,7 @@ class VirtualTopologyTest extends MidolmanSpec with TopologyBuilder {
 
             Then("The request throws a NotYetException with a future")
             val future = e.waitFor.asInstanceOf[Future[SimulationPort]]
-            ready(future, 1.second)
+            ready(future, 1 second)
 
             And("The future completes successfully with the given port")
             future.isCompleted shouldBe true
@@ -98,17 +98,17 @@ class VirtualTopologyTest extends MidolmanSpec with TopologyBuilder {
         }
 
         scenario("The port exists in the topology and is cached") {
-            Given("A network port")
+            Given("A bridge port")
             val id = UUID.randomUUID
             val port = createBridgePort(id = id)
             store.create(port)
-            ready(store.get(classOf[TopologyPort], id), 1.second)
+            ready(store.get(classOf[TopologyPort], id), 1 second)
 
             And("Requesting the port to update the VT cache")
             // Try get the port, and wait for the port to be cached.
             ready(intercept[NotYetException] {
                 VirtualTopology.tryGet[SimulationPort](id)
-            }.waitFor, 1.second)
+            }.waitFor, 1 second)
 
             When("Requesting the port a second time")
             // Get the port again, this time from cache.
@@ -120,18 +120,18 @@ class VirtualTopologyTest extends MidolmanSpec with TopologyBuilder {
         }
 
         scenario("The VT returns the latest port version") {
-            Given("A network port")
+            Given("A bridge port")
             val id = UUID.randomUUID
             val port1 = createBridgePort(id = id, tunnelKey = 1)
             store.create(port1)
-            ready(store.get(classOf[TopologyPort], id), 1.second)
+            ready(store.get(classOf[TopologyPort], id), 1 second)
 
             When("Requesting the port")
             // Try get the port, and wait for the port to be cached.
             val future = intercept[NotYetException] {
                 VirtualTopology.tryGet[SimulationPort](id)
             }.waitFor.asInstanceOf[Future[SimulationPort]]
-            ready(future, 1.second)
+            ready(future, 1 second)
 
             Then("The thrown future completes successfully with the port")
             future.isCompleted shouldBe true
@@ -142,7 +142,7 @@ class VirtualTopologyTest extends MidolmanSpec with TopologyBuilder {
             When("The port is updated")
             val port2 = createBridgePort(id = id, tunnelKey = 2)
             store.update(port2)
-            ready(store.get(classOf[TopologyPort], id), 1.second)
+            ready(store.get(classOf[TopologyPort], id), 1 second)
 
             And("Requesting the port")
             val device = VirtualTopology.tryGet[SimulationPort](id)
@@ -154,16 +154,16 @@ class VirtualTopologyTest extends MidolmanSpec with TopologyBuilder {
         }
 
         scenario("The VT removes the port from cache on port deletion") {
-            Given("A network port")
+            Given("A bridge port")
             val id = UUID.randomUUID
             val port = createBridgePort(id = id)
             store.create(port)
-            ready(store.get(classOf[TopologyPort], id), 1.second)
+            ready(store.get(classOf[TopologyPort], id), 1 second)
 
             And("Requesting the port to update the VT cache")
             ready(intercept[NotYetException] {
                 VirtualTopology.tryGet[SimulationPort](id)
-            }.waitFor, 1.second)
+            }.waitFor, 1 second)
 
             When("Deleting the port")
             store.delete(classOf[TopologyPort], id)
@@ -178,7 +178,7 @@ class VirtualTopologyTest extends MidolmanSpec with TopologyBuilder {
 
             When("Waiting for the future to complete")
             val e2 = intercept[NotFoundException] {
-                result(future, 1.seconds)
+                result(future, 1 seconds)
             }
 
             Then("The future fails with a NotFoundException")
@@ -199,7 +199,7 @@ class VirtualTopologyTest extends MidolmanSpec with TopologyBuilder {
             VirtualTopology.observable[SimulationPort](id).subscribe(observer)
 
             Then("The observer should receive an error")
-            observer.await(1.second)
+            observer.await(1 second)
             observer.getOnErrorEvents.size shouldBe 1
 
             And("The exception shouldBe a NotFoundException")
@@ -210,11 +210,11 @@ class VirtualTopologyTest extends MidolmanSpec with TopologyBuilder {
         }
 
         scenario("The port exists in the topology and is not cached") {
-            Given("A network port")
+            Given("A bridge port")
             val id = UUID.randomUUID
             val port = createBridgePort(id = id)
             store.create(port)
-            ready(store.get(classOf[TopologyPort], id), 1.second)
+            ready(store.get(classOf[TopologyPort], id), 1 second)
 
             And("An awaitable observer")
             val observer = new AwaitableObserver[SimulationPort]()
@@ -223,7 +223,7 @@ class VirtualTopologyTest extends MidolmanSpec with TopologyBuilder {
             VirtualTopology.observable[SimulationPort](id).subscribe(observer)
 
             Then("The observer should received one onNext notification")
-            observer.await(1.second)
+            observer.await(1 second)
             observer.getOnNextEvents should contain only VirtualTopology
                 .get(id).value.get.get
 
@@ -232,11 +232,11 @@ class VirtualTopologyTest extends MidolmanSpec with TopologyBuilder {
         }
 
         scenario("The port exists in the topology and is cached") {
-            Given("A network port")
+            Given("A bridge port")
             val id = UUID.randomUUID
             val port = createBridgePort(id = id)
             store.create(port)
-            ready(store.get(classOf[TopologyPort], id), 1.second)
+            ready(store.get(classOf[TopologyPort], id), 1 second)
 
             And("An awaitable observer")
             val observer = new AwaitableObserver[SimulationPort]()
@@ -245,7 +245,7 @@ class VirtualTopologyTest extends MidolmanSpec with TopologyBuilder {
             val future = intercept[NotYetException] {
                 VirtualTopology.tryGet[SimulationPort](id)
             }.waitFor.asInstanceOf[Future[SimulationPort]]
-            ready(future, 1.second)
+            ready(future, 1 second)
 
             Then("The request should return the device")
             val device = future.value.get.get
@@ -255,16 +255,16 @@ class VirtualTopologyTest extends MidolmanSpec with TopologyBuilder {
             VirtualTopology.observable[SimulationPort](id).subscribe(observer)
 
             Then("The observer should received one onNext notification")
-            observer.await(1.second)
+            observer.await(1 second)
             observer.getOnNextEvents should contain only device
         }
 
         scenario("The observer receives update notifications") {
-            Given("A network port")
+            Given("A bridge port")
             val id = UUID.randomUUID
             val port1 = createBridgePort(id = id, tunnelKey = 1)
             store.create(port1)
-            ready(store.get(classOf[TopologyPort], id), 1.second)
+            ready(store.get(classOf[TopologyPort], id), 1 second)
 
             And("An awaitable observer")
             val observer = new AwaitableObserver[SimulationPort]()
@@ -273,27 +273,27 @@ class VirtualTopologyTest extends MidolmanSpec with TopologyBuilder {
             VirtualTopology.observable[SimulationPort](id).subscribe(observer)
 
             Then("The observer should receive one onNext notification")
-            observer.await(1.second, 1)
+            observer.await(1 second, 1)
             val device1 = VirtualTopology.get[SimulationPort](id).value.get.get
             observer.getOnNextEvents should contain only device1
 
             When("The port is updated")
             val port2 = createBridgePort(id = id, tunnelKey = 2)
             store.update(port2)
-            ready(store.get(classOf[TopologyPort], id), 1.second)
+            ready(store.get(classOf[TopologyPort], id), 1 second)
 
             Then("The observer should receive a second onNext notification")
-            observer.await(1.second)
+            observer.await(1 second)
             val device2 = VirtualTopology.get[SimulationPort](id).value.get.get
             observer.getOnNextEvents should contain inOrder (device1, device2)
         }
 
         scenario("The observer should not receive updated after unsubscribe") {
-            Given("A network port")
+            Given("A bridge port")
             val id = UUID.randomUUID
             val port1 = createBridgePort(id = id, tunnelKey = 1)
             store.create(port1)
-            ready(store.get(classOf[TopologyPort], id), 1.second)
+            ready(store.get(classOf[TopologyPort], id), 1 second)
 
             And("An awaitable observer")
             val observer = new AwaitableObserver[SimulationPort]()
@@ -304,7 +304,7 @@ class VirtualTopologyTest extends MidolmanSpec with TopologyBuilder {
                 .subscribe(observer)
 
             Then("The observer should receive one onNext notification")
-            observer.await(1.second)
+            observer.await(1 second)
             val device1 = VirtualTopology.get[SimulationPort](id).value.get.get
             observer.getOnNextEvents should contain only device1
 
@@ -314,18 +314,18 @@ class VirtualTopologyTest extends MidolmanSpec with TopologyBuilder {
             And("The port is updated")
             val port2 = createBridgePort(id = id, tunnelKey = 2)
             store.update(port2)
-            ready(store.get(classOf[TopologyPort], id), 1.second)
+            ready(store.get(classOf[TopologyPort], id), 1 second)
 
             Then("The observer should not receive a second onNext notification")
             observer.getOnNextEvents should contain only device1
         }
 
         scenario("The observer unsubscribing should not affect other observers") {
-            Given("A network port")
+            Given("A bridge port")
             val id = UUID.randomUUID
             val port1 = createBridgePort(id = id, tunnelKey = 1)
             store.create(port1)
-            ready(store.get(classOf[TopologyPort], id), 1.second)
+            ready(store.get(classOf[TopologyPort], id), 1 second)
 
             And("Two awaitable observers")
             val observer1 = new AwaitableObserver[SimulationPort]()
@@ -337,7 +337,7 @@ class VirtualTopologyTest extends MidolmanSpec with TopologyBuilder {
                 .subscribe(observer1)
 
             Then("The observer 1 should receive one onNext notification")
-            observer1.await(1.second)
+            observer1.await(1 second)
             val device1 = VirtualTopology.get[SimulationPort](id).value.get.get
             observer1.getOnNextEvents should contain only device1
 
@@ -347,7 +347,7 @@ class VirtualTopologyTest extends MidolmanSpec with TopologyBuilder {
                 .subscribe(observer2)
 
             Then("The observer 2 should receive one onNext notification")
-            observer2.await(1.second, 1)
+            observer2.await(1 second, 1)
             observer2.getOnNextEvents should contain only device1
 
             When("Observer 1 is unsubscribing")
@@ -356,23 +356,23 @@ class VirtualTopologyTest extends MidolmanSpec with TopologyBuilder {
             And("The port is updated")
             val port2 = createBridgePort(id = id, tunnelKey = 2)
             store.update(port2)
-            ready(store.get(classOf[TopologyPort], id), 1.second)
+            ready(store.get(classOf[TopologyPort], id), 1 second)
 
             Then("The observer 1 should not receive a second onNext notification")
             observer1.getOnNextEvents should contain only device1
 
             Then("The observer 2 should receive a second onNext notification")
-            observer2.await(1.second)
+            observer2.await(1 second)
             val device2 = VirtualTopology.get[SimulationPort](id).value.get.get
             observer2.getOnNextEvents should contain inOrder(device1, device2)
         }
 
         scenario("The observer should receive a deletion notification") {
-            Given("A network port")
+            Given("A bridge port")
             val id = UUID.randomUUID
             val port1 = createBridgePort(id = id, tunnelKey = 1)
             store.create(port1)
-            ready(store.get(classOf[TopologyPort], id), 1.second)
+            ready(store.get(classOf[TopologyPort], id), 1 second)
 
             And("An awaitable observer")
             val observer = new AwaitableObserver[SimulationPort]()
@@ -383,7 +383,7 @@ class VirtualTopologyTest extends MidolmanSpec with TopologyBuilder {
                 .subscribe(observer)
 
             Then("The observer should receive one onNext notification")
-            observer.await(1.second)
+            observer.await(1 second)
             val device1 = VirtualTopology.get[SimulationPort](id).value.get.get
             observer.getOnNextEvents should contain only device1
 
