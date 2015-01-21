1239dfdd8fdf90892f37b4c95642d14799b2ac4f
Ryu Ishimoto <ryu@midokura.com>
2015-01-21 18:58:54 +0000

    development tool for midonet
    Scripts to launch services and install packages that midonet
    depends on, as well as starting midonet processes in screen
    sessions for development.
    
    Change-Id: Id821b2b105c2aedbfc4cde60f3b89cc66c9e11fa

---
+++ tools/devmido/undevmido.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+# Copyright 2015 Midokura SARL
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Keep track of the devmido(current) directory
+DEVMIDO_DIR=$(cd $(dirname $0) && pwd)
+
+# Import common functions
+source $DEVMIDO_DIR/functions
+
+# Check if run as root
+if [[ $EUID -eq 0 ]]; then
+    die $LINENO "You cannot run this script as root."
+fi
+
+MIDORC=$DEVMIDO_DIR/midonetrc
+if [[ ! -r $MIDORC ]]; then
+    die $LINENO "Missing $MIDORC"
+fi
+source $MIDORC
+
+set -o xtrace
+
+# Clean up the remainder of the screen processes
+SCREEN=$(which screen)
+if [[ -n "$SCREEN" ]]; then
+    SESSION=$(screen -ls | awk -v pat="[0-9].$SCREEN_NAME" '$0 ~ pat { print $1 }')
+    if [[ -n "$SESSION" ]]; then
+        screen -X -S $SESSION quit
+    fi
+fi
