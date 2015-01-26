12510fa382796f86bf76a45a3d3389296a83e6d7
Ivan Kelly <ivan@midokura.com>
2015-01-26 17:43:28 +0100

    Migrate BridgeSimulationTestCase
    Conversion is straight forward. Checks the result of simulation as the
    action conversion doesn't take place in the virtual topology actor.
    
    Change-Id: I08ef6ebee780d2f4d37e1017f8b2abaf3be7aa4b

---
+++ midolman/src/test/scala/org/midonet/midolman/BridgeSimulationTest.scala
@@ -0,0 +1,359 @@
+/*
+ * Copyright 2014-2015 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.midonet.midolman
+
+import java.lang.{Short => JShort}
+import java.util.UUID
+import org.junit.experimental.categories.Category
+import org.junit.runner.RunWith
+
+import org.midonet.cluster.data.{Bridge => ClusterBridge, TunnelZone}
+import org.midonet.cluster.data.ports.BridgePort
+import org.midonet.midolman.FlowController._
+import org.midonet.midolman.PacketWorkflow.{Drop, SimulationResult}
+import org.midonet.midolman.rules.{Condition, RuleResult}
+import org.midonet.midolman.simulation.Bridge
+import org.midonet.midolman.simulation.Coordinator.{FloodBridgeAction, ToPortAction}
+import org.midonet.midolman.topology.VirtualTopologyActor
+import org.midonet.midolman.util.MidolmanSpec
+import org.midonet.odp.flows.FlowActions.output
+import org.midonet.packets._
+import org.midonet.packets.util.PacketBuilder._
+
+import org.scalatest.junit.JUnitRunner
+import org.slf4j.LoggerFactory
+import scala.collection.JavaConversions._
+import scala.collection.immutable
+
+@Category(Array(classOf[SimulationTests]))
+@RunWith(classOf[JUnitRunner])
+class BridgeSimulationTest extends MidolmanSpec {
+
+    registerActors(VirtualTopologyActor -> (() => new VirtualTopologyActor))
+
+    val log = LoggerFactory.getLogger(classOf[BridgeSimulationTest])
+
+    private var port1OnHost1: BridgePort = null
+    private var port2OnHost1: BridgePort = null
+    private var port3OnHost1: BridgePort = null
+    private var portOnHost2: BridgePort = null
+    private var portOnHost3: BridgePort = null
+
+    private var bridge: ClusterBridge = null
+
+    val host1Ip = IPv4Addr("192.168.100.1")
+    val host2Ip = IPv4Addr("192.168.125.1")
+    val host3Ip = IPv4Addr("192.168.150.1")
+
+    val port2OnHost1Mac = MAC.fromString("0a:fe:88:70:33:ab")
+
+    override def beforeTest(): Unit ={
+        val tunnelZone = greTunnelZone("default")
+
+        val host1 = newHost("host1", hostId)
+        val host2 = newHost("host2")
+        val host3 = newHost("host3")
+
+        bridge = newBridge("bridge")
+
+        port1OnHost1 = newBridgePort(bridge)
+        port2OnHost1 = newBridgePort(bridge)
+        port3OnHost1 = newBridgePort(bridge)
+        portOnHost2 = newBridgePort(bridge)
+        portOnHost3 = newBridgePort(bridge)
+
+        materializePort(port1OnHost1, host1.getId, "port1")
+        materializePort(portOnHost2, host2.getId, "port2")
+        materializePort(portOnHost3, host3.getId, "port3")
+        materializePort(port2OnHost1, host1.getId, "port4")
+        materializePort(port3OnHost1, host1.getId, "port5")
+
+        List(host1, host2, host3).zip(List(host1Ip, host2Ip, host3Ip)).foreach{
+            case (host, ip) =>
+                clusterDataClient.tunnelZonesAddMembership(tunnelZone.getId,
+                    new TunnelZone.HostConfig(host.getId).setIp(ip))
+        }
+
+        fetchTopology(bridge, port1OnHost1, portOnHost2, portOnHost3,
+                      port2OnHost1, port3OnHost1)
+    }
+
+    /**
+      * All frames generated from this test will get the vlan tags set in this
+      * list. The bridge (in this case it's a VUB) should simply let them pass
+      * and apply mac-learning based on the default vlan id (0).
+      *
+      * Here the list is simply empty, but just extending this test and
+      * overriding the method with a different list you get all the test cases
+      * but with vlan-tagged traffic.
+      *
+      * At the bottom there are a couple of classes that add vlan ids to the
+      * same test cases.
+      *
+      * See MN-200
+      *
+      */
+    def networkVlans: List[JShort] = List()
+
+    scenario("Malformed L3 packet") {
+        val malformed = eth mac "02:11:22:33:44:10" -> "02:11:22:33:44:20"
+        malformed << payload("00:00")
+        malformed ether_type IPv4.ETHERTYPE vlans networkVlans
+
+        val bridgeDevice: Bridge = fetchDevice(bridge)
+        val (pktCtx, action) = simulateDevice(bridgeDevice,
+                                              malformed, port1OnHost1.getId)
+        verifyFloodAction(action)
+    }
+
+    scenario("bridge simulation for normal packet") {
+        val srcMac = MAC.fromString("02:11:22:33:44:10")
+        val ethPkt = Packets.udp(
+                srcMac,
+                MAC.fromString("02:11:22:33:44:11"),
+                IPv4Addr.fromString("10.0.1.10"),
+                IPv4Addr.fromString("10.0.1.11"),
+                10, 11, "My UDP packet".getBytes)
+        ethPkt.setVlanIDs(networkVlans)
+
+        val bridgeDevice: Bridge = fetchDevice(bridge)
+        val (pktCtx, action) = simulateDevice(bridgeDevice,
+                                              ethPkt, port1OnHost1.getId)
+        verifyFloodAction(action)
+
+        verifyMacLearned(srcMac, port1OnHost1.getId)
+    }
+
+    scenario("inbound chains not applied to vlan traffic") {
+        // setup chains that drop all UDP traffic both at pre and post
+        val udpCond = new Condition()
+        udpCond.nwProto = Byte.box(UDP.PROTOCOL_NUMBER)
+
+        val preChain = newInboundChainOnBridge("brFilter-in", bridge)
+        newLiteralRuleOnChain(preChain, 1,udpCond, RuleResult.Action.DROP)
+
+        checkTrafficWithDropChains()
+    }
+
+    scenario("outbound chains not applied to vlan traffic") {
+        // setup chains that drop all UDP traffic both at pre and post
+        val udpCond = new Condition()
+        udpCond.nwProto = Byte.box(UDP.PROTOCOL_NUMBER)
+
+        val postChain = newOutboundChainOnBridge("brFilter-out", bridge)
+        newLiteralRuleOnChain(postChain, 1, udpCond, RuleResult.Action.DROP)
+
+        checkTrafficWithDropChains()
+    }
+
+    /**
+     * Use after setting chains that drop UDP traffic on a bridge. The method
+     * will send traffic through the bridge and expect it dropped (by matching
+     * on the chains) if it is vlan-tagged, but not dropped otherwise.
+     */
+    private def checkTrafficWithDropChains() {
+
+        val ethPkt = Packets.udp(
+            MAC.fromString("0a:fe:88:70:44:55"),
+            MAC.fromString("ff:ff:ff:ff:ff:ff"),
+            IPv4Addr.fromString("10.10.10.10"),
+            IPv4Addr.fromString("10.11.11.11"),
+            10, 12, "Test UDP packet".getBytes)
+        ethPkt.setVlanIDs(networkVlans)
+
+        log.info("Testing traffic with vlans: {}", networkVlans)
+
+        val bridgeDevice: Bridge = fetchDevice(bridge)
+        val (pktCtx, action) = simulateDevice(bridgeDevice,
+                                              ethPkt, port1OnHost1.getId)
+
+        if (networkVlans.isEmpty) {
+            action should be (Drop)
+        } else {
+            verifyFloodAction(action)
+        }
+    }
+
+    scenario("ethernet broadcast on bridge") {
+        val srcMac = MAC.fromString("0a:fe:88:70:44:55")
+        val ethPkt = Packets.udp(
+                srcMac,
+                MAC.fromString("ff:ff:ff:ff:ff:ff"),
+                IPv4Addr.fromString("10.10.10.10"),
+                IPv4Addr.fromString("10.11.11.11"),
+                10, 12, "Test UDP packet".getBytes)
+        ethPkt.setVlanIDs(networkVlans)
+
+        val bridgeDevice: Bridge = fetchDevice(bridge)
+        val (pktCtx, action) = simulateDevice(bridgeDevice,
+                                              ethPkt, port1OnHost1.getId)
+        verifyFloodAction(action)
+
+        //Our source MAC should also be learned
+        verifyMacLearned(srcMac, port1OnHost1.getId)
+    }
+
+    scenario("broadcast arp on bridge") {
+        val srcMac = MAC.fromString("0a:fe:88:90:22:33")
+        val ethPkt = Packets.arpRequest(
+                srcMac,
+                IPv4Addr.fromString("10.10.10.11"),
+                IPv4Addr.fromString("10.11.11.10"))
+        ethPkt.setVlanIDs(networkVlans)
+
+        val bridgeDevice: Bridge = fetchDevice(bridge)
+        val (pktCtx, action) = simulateDevice(bridgeDevice,
+                                              ethPkt, port1OnHost1.getId)
+        verifyFloodAction(action)
+
+        //Our source MAC should also be learned
+        verifyMacLearned(srcMac, port1OnHost1.getId)
+    }
+
+    scenario("multicast destination ethernet on bridge") {
+        val srcMac = MAC.fromString("0a:fe:88:90:22:33")
+        val ethPkt = Packets.udp(
+                srcMac,
+                MAC.fromString("01:00:cc:cc:dd:dd"),
+                IPv4Addr.fromString("10.10.10.11"),
+                IPv4Addr.fromString("10.11.11.10"), 10, 12,
+                "Test UDP Packet".getBytes)
+        ethPkt.setVlanIDs(networkVlans)
+
+        val bridgeDevice: Bridge = fetchDevice(bridge)
+        val (pktCtx, action) = simulateDevice(bridgeDevice,
+                                              ethPkt, port1OnHost1.getId)
+        verifyFloodAction(action)
+
+        //Our source MAC should also be learned
+        verifyMacLearned(srcMac, port1OnHost1.getId)
+    }
+
+    scenario("multicast src ethernet on bridge") {
+            val ethPkt = Packets.udp(
+                    MAC.fromString("ff:54:ce:50:44:ce"),
+                MAC.fromString("0a:de:57:16:a3:06"),
+                IPv4Addr.fromString("10.10.10.12"),
+                IPv4Addr.fromString("10.11.11.12"),
+                10, 12, "Test UDP packet".getBytes)
+        ethPkt.setVlanIDs(networkVlans)
+
+        val bridgeDevice: Bridge = fetchDevice(bridge)
+        val (pktCtx, action) = simulateDevice(bridgeDevice,
+                                              ethPkt, port1OnHost1.getId)
+        action should be (Drop)
+    }
+
+    scenario("mac migration on bridge") {
+        val srcMac = MAC.fromString("02:13:66:77:88:99")
+        var ethPkt = Packets.udp(
+                srcMac,
+                MAC.fromString("02:11:22:33:44:55"),
+                IPv4Addr.fromString("10.0.1.10"),
+                IPv4Addr.fromString("10.0.1.11"),
+                10, 11, "My UDP packet".getBytes)
+        ethPkt.setVlanIDs(networkVlans)
+
+        val bridgeDevice: Bridge = fetchDevice(bridge)
+        val (pktCtx, action) = simulateDevice(bridgeDevice,
+                                              ethPkt, port1OnHost1.getId)
+        verifyFloodAction(action)
+
+        verifyMacLearned(srcMac, port1OnHost1.getId)
+
+        /*
+         * MAC moved from port1OnHost1 to port3OnHost1
+         * frame is going toward port2OnHost1 (the learned MAC from
+         * verifyMacLearned)
+         */
+        ethPkt = Packets.udp(
+                     srcMac,
+                     port2OnHost1Mac,
+                     IPv4Addr.fromString("10.0.1.10"),
+                     IPv4Addr.fromString("10.0.1.11"),
+                     10, 11, "My UDP packet".getBytes)
+        ethPkt.setVlanIDs(networkVlans)
+
+        val (pktCtx2, action2) = simulateDevice(bridgeDevice,
+                                                ethPkt, port3OnHost1.getId)
+        action2 match {
+            case ToPortAction(outPortUUID) =>
+                outPortUUID should equal (port2OnHost1.getId)
+            case _ => fail("Not ToPortAction, instead: " + action.toString)
+        }
+
+        verifyMacLearned(srcMac, port3OnHost1.getId)
+    }
+
+    /*
+     * In this test, always assume input port is port2OnHost1 (keep src mac
+     * consistent), dst mac is variable
+     */
+    private def verifyMacLearned(learnedMac: MAC, expectedPort: UUID) {
+        val ethPkt = Packets.udp(
+            port2OnHost1Mac,
+            learnedMac,
+            IPv4Addr.fromString("10.10.10.10"),
+            IPv4Addr.fromString("10.11.11.11"),
+            10, 12, "Test UDP packet".getBytes)
+        ethPkt.setVlanIDs(networkVlans)
+
+        val bridgeDevice: Bridge = fetchDevice(bridge)
+        val (pktCtx, action) = simulateDevice(bridgeDevice,
+                                              ethPkt, port2OnHost1.getId)
+        action match {
+            case ToPortAction(outPortUUID) =>
+                outPortUUID should equal (expectedPort)
+            case _ => fail("Not ToPortAction, instead: " + action.toString)
+        }
+    }
+
+    private def verifyFloodAction(action: SimulationResult)
+            : Unit = action match {
+        case FloodBridgeAction(brId, ports) =>
+            assert(brId === bridge.getId)
+            assert(ports.contains(port1OnHost1.getId))
+            assert(ports.contains(port2OnHost1.getId))
+            assert(ports.contains(port3OnHost1.getId))
+            assert(ports.contains(portOnHost2.getId))
+            assert(ports.contains(portOnHost3.getId))
+        case _ => fail("Not FloodBridgeAction, instead: " + action.toString)
+    }
+}
+
+/**
+  * The same tests as the parent
+  * [[org.midonet.midolman.BridgeSimulationTestCase]], but transmitting frames
+  * that have one vlan id.
+  *
+  * The tests are expected to work in exactly the same way, since here we're
+  * adding a Vlan Unaware Bridge (all its interior ports are not vlan-tagged)
+  * and therefore it should behave with vlan traffic in the same way as if it
+  * was not tagged.
+  */
+class BridgeSimulationTestWithOneVlan extends BridgeSimulationTest {
+    override def networkVlans: List[JShort] = List(2.toShort)
+}
+
+/**
+  * The same tests [[org.midonet.midolman.BridgeSimulationTestCase]], but
+  * transmitting frames that have one vlan id.
+  */
+class BridgeSimulationTestWithManyVlans extends BridgeSimulationTest {
+    override def networkVlans: List[JShort] = List(3,4,5,6) map {
+        x => short2Short(x.toShort)
+    }
+}
