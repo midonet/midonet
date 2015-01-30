86a8e73e9cced24421bd31b518bf07c1be988722
Ryu Ishimoto <ryu@midokura.com>
2015-01-30 17:43:43 +0000

    Add midostack.sh
    Create a file that can be sourced from devstack that is a wrapper
    of mido.sh.  This file lets you manage the environemnt variables
    that get passed down from devstack to devmido.
    
    Change-Id: I7771e4c004ab559066c04814bf17ad232b6e433b

---
+++ tools/devmido/midostack.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# Copyright 2015 Midokura SARL
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This script is meant to be sourced from devstack.  It is a wrapper of
+# mido.sh that allows proper exporting of environment variables to mido.sh.
+
+# Keep track of the current directory
+MIDOSTACK_DIR=$(cd $(dirname $0) && pwd)
+
+# Share the same logging locations
+export TIMESTAMP_FORMAT
+export LOGFILE
+export SCREEN_LOGDIR
+
+# Keep the midonet API URL consistent
+export SERVICE_HOST=$MIDONET_SERVICE_HOST
+export API_PORT=$MIDONET_API_PORT
+
+$MIDOSTACK_DIR/mido.sh
