803880612303bb2c7386f60c06f2f421750c7de6
Duarte Nunes <duarte@midokura.com>
2015-02-02 16:09:15 +0100

    Ensure NatTarget ports are always within range
    This patch fixes a bug (and prevents future ones) whereby a class was
    not properly trimming the port fields in NatTarget. More specifically,
    when creating a rule with NatTargets, the Neutron plugin or the user
    can write -1 to mean the maximum allowed port value. This is stored in
    ZK and read from it as an integer instead of a short, breaking code
    that expects the port value to be within the valid range.
    
    Change-Id: Id98a79f50d038967f20fecc3b1e84f987c54301e
    Signed-off-by: Duarte Nunes <duarte@midokura.com>

--- midolman/src/main/java/org/midonet/midolman/rules/NatTarget.java
+++ midolman/src/main/java/org/midonet/midolman/rules/NatTarget.java
@@ -18,29 +18,28 @@ package org.midonet.midolman.rules;
 
 import org.midonet.packets.IPv4Addr;
 
+import org.codehaus.jackson.annotate.JsonCreator;
+
 public class NatTarget {
+    public final IPv4Addr nwStart;
+    public final IPv4Addr nwEnd;
+    public final int tpStart;
+    public final int tpEnd;
 
-    public IPv4Addr nwStart;
-    public IPv4Addr nwEnd;
-    public int tpStart;
-    public int tpEnd;
-
+    @JsonCreator
     public NatTarget(IPv4Addr nwStart, IPv4Addr nwEnd, int tpStart, int tpEnd) {
         this.nwStart = nwStart;
         this.nwEnd = nwEnd;
-        this.tpStart = tpStart;
-        this.tpEnd = tpEnd;
+        this.tpStart = tpStart & 0xffff;
+        this.tpEnd = tpEnd & 0xffff;
     }
 
     public NatTarget(int nwStart, int nwEnd, int tpStart, int tpEnd) {
-        this.nwStart = new IPv4Addr(nwStart);
-        this.nwEnd = new IPv4Addr(nwEnd);
-        this.tpStart = tpStart;
-        this.tpEnd = tpEnd;
+        this(new IPv4Addr(nwStart), new IPv4Addr(nwEnd), tpStart, tpEnd);
     }
 
     public NatTarget(IPv4Addr nwStart, IPv4Addr nwEnd) {
-        this(nwStart, nwEnd, 1, 65535);
+        this(nwStart, nwEnd, 1, 0xffff);
     }
 
     public NatTarget(IPv4Addr ipAddr) {
@@ -51,27 +50,6 @@ public class NatTarget {
         this(ipAddr, ipAddr, tpStart, tpEnd);
     }
 
-    // Default constructor for the Jackson deserialization.
-    public NatTarget() { super(); }
-
-    /* Custom accessors for Jackson serialization with more readable IPs. */
-
-    public String getNwStart() {
-        return this.nwStart.toString();
-    }
-
-    public void setNwStart(String addr) {
-        this.nwStart = IPv4Addr.fromString(addr);
-    }
-
-    public String getNwEnd() {
-        return this.nwEnd.toString();
-    }
-
-    public void setNwEnd(String addr) {
-        this.nwEnd = IPv4Addr.fromString(addr);
-    }
-
     @Override
     public boolean equals(Object other) {
         if (this == other)
@@ -80,15 +58,8 @@ public class NatTarget {
             return false;
 
         NatTarget nt = (NatTarget) other;
-        if (nt.nwStart == null && nwStart != null)
-            return false;
-        if (nt.nwEnd == null && nwEnd != null)
-            return false;
-        if (!nt.nwStart.equals(nwStart))
-            return false;
-        if (!nt.nwEnd.equals(nwEnd))
-            return false;
-        return tpStart == nt.tpStart && tpEnd == nt.tpEnd;
+        return nwStart.equals(nt.nwStart) && nwEnd.equals(nt.nwEnd) &&
+               tpStart == nt.tpStart && tpEnd == nt.tpEnd;
     }
 
     @Override
@@ -101,12 +72,8 @@ public class NatTarget {
 
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder("NatTarget [");
-        sb.append("nwStart=").append(nwStart.toString());
-        sb.append(", nwEnd=").append(nwEnd.toString());
-        sb.append(", tpStart=").append(tpStart & 0xffff);
-        sb.append(", tpEnd=").append(tpEnd & 0xffff);
-        sb.append("]");
-        return sb.toString();
+        return "NatTarget [" + "nwStart=" + nwStart.toString() + ", nwEnd="
+               + nwEnd.toString() + ", tpStart=" + tpStart + ", tpEnd=" + tpEnd
+               + "]";
     }
 }
