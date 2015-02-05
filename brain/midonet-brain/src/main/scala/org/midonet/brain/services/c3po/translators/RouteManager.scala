c293b4822828e54e8c8cef19506e57603bcd0f1c
Brandon Berg <bberg@midokura.com>
2015-02-05 19:57:26 +0900

    Rearch: Router translations with gateway and SNAT.
    Signed-off-by: Brandon Berg <bberg@midokura.com>
    Change-Id: Ibd4e86009c8b5bad2eaa972794f29da8017332cb

--- brain/midonet-brain/src/main/scala/org/midonet/brain/services/c3po/translators/RouteManager.scala
+++ brain/midonet-brain/src/main/scala/org/midonet/brain/services/c3po/translators/RouteManager.scala
@@ -16,49 +16,25 @@
 
 package org.midonet.brain.services.c3po.translators
 
+import org.midonet.brain.services.c3po.midonet.Create
 import org.midonet.cluster.data.neutron.MetaDataService
-import org.midonet.cluster.data.storage.ReadOnlyStorage
 import org.midonet.cluster.models.Commons.{IPAddress, IPSubnet, UUID}
 import org.midonet.cluster.models.Topology.Dhcp.Opt121RouteOrBuilder
-import org.midonet.cluster.models.Topology.Route
-import org.midonet.cluster.models.Topology.RouteOrBuilder
 import org.midonet.cluster.models.Topology.Route.NextHop
+import org.midonet.cluster.models.Topology.{PortOrBuilder, Route, RouteOrBuilder}
+import org.midonet.cluster.util.IPSubnetUtil.univSubnet4
+import org.midonet.cluster.util.UUIDUtil.asRichProtoUuid
 import org.midonet.cluster.util.{IPSubnetUtil, UUIDUtil}
 
+
 trait RouteManager {
-    val storage: ReadOnlyStorage
-    val META_DATA_SRVC = IPSubnetUtil.toProto(MetaDataService.IPv4_ADDRESS)
-    val DEFAULT_WEIGHT = 100
-
-    def createLocalRoute(portId: UUID, portAddr: IPSubnet, routerId: UUID)
-    : Route = {
-        Route.newBuilder()
-             .setDstSubnet(portAddr)
-             .setNextHop(NextHop.LOCAL)
-             .setNextHopPortId(portId)
-             .setRouterId(routerId).build()
-    }
-
-    def createMetaDataServiceRoute(srcSubnet: IPSubnet, nextHopPortId: UUID,
-                                   nextHopGw: IPAddress, routerId: UUID)
-    : Route = {
-        Route.newBuilder
-             .setId(UUIDUtil.randomUuidProto)
-             .setSrcSubnet(srcSubnet)
-             .setDstSubnet(META_DATA_SRVC)
-             .setNextHop(NextHop.PORT)
-             .setNextHopPortId(nextHopPortId)
-             .setNextHopGateway(nextHopGw)
-             .setWeight(DEFAULT_WEIGHT)
-             .setRouterId(routerId)
-             .build
-    }
-
+    import RouteManager._
     /**
      * Tests if the route is to Meta Data Server.
      * @param nextHopGw A next hop gateway to Meta Data Server.
      */
-    def isMetaDataSvrRoute(route: RouteOrBuilder, nextHopGw: IPAddress) =
+    protected def isMetaDataSvrRoute(route: RouteOrBuilder,
+                                     nextHopGw: IPAddress): Boolean =
         route.getDstSubnet == META_DATA_SRVC &&
         route.getNextHopGateway == nextHopGw
 
@@ -66,7 +42,80 @@ trait RouteManager {
      * Tests if the route is an Opt 121 route to Meta Data Server.
      * @param nextHopGw A next hop gateway to Meta Data Server.
      */
-    def isMetaDataSvrOpt121Route(route: Opt121RouteOrBuilder,
-                                 nextHopGw: IPAddress) =
+    protected def isMetaDataSvrOpt121Route(route: Opt121RouteOrBuilder,
+                                           nextHopGw: IPAddress): Boolean =
         route.getDstSubnet == META_DATA_SRVC && route.getGateway == nextHopGw
+
+    protected def newRouteBuilder(routerId: UUID): Route.Builder =
+        Route.newBuilder()
+            .setId(UUIDUtil.randomUuidProto)
+            .setRouterId(routerId)
+
+    protected def newLocalRoute(portId: UUID, portAddr: IPAddress)
+    : Route = {
+        Route.newBuilder
+             .setId(RouteManager.localRouteId(portId))
+             .setSrcSubnet(IPSubnetUtil.univSubnet4)
+             .setDstSubnet(IPSubnetUtil.toProto(portAddr))
+             .setNextHop(NextHop.LOCAL)
+             .setWeight(DEFAULT_WEIGHT)
+             .setNextHopPortId(portId).build()
+    }
+
+    def newMetaDataServiceRoute(srcSubnet: IPSubnet, nextHopPortId: UUID,
+                                   nextHopGw: IPAddress)
+    : Route = {
+        newNextHopPortRoute(nextHopPortId,
+                            id = metadataServiceRouteId(nextHopPortId),
+                            nextHopGwIpAddr = nextHopGw,
+                            srcSubnet = srcSubnet,
+                            dstSubnet = META_DATA_SRVC)
+    }
+
+    protected def newNextHopPortRoute(nextHopPortId: UUID,
+                                      id: UUID = null,
+                                      nextHopGwIpAddr: IPAddress = null,
+                                      srcSubnet: IPSubnet = univSubnet4,
+                                      dstSubnet: IPSubnet = univSubnet4,
+                                      weight: Int = DEFAULT_WEIGHT): Route = {
+        val bldr = Route.newBuilder
+        bldr.setId(if (id != null) id else UUIDUtil.randomUuidProto)
+        bldr.setNextHop(NextHop.PORT)
+        bldr.setNextHopPortId(nextHopPortId)
+        bldr.setSrcSubnet(srcSubnet)
+        bldr.setDstSubnet(dstSubnet)
+        bldr.setWeight(weight)
+        if (nextHopGwIpAddr != null) bldr.setNextHopGateway(nextHopGwIpAddr)
+        bldr.build()
+    }
+
+    protected def addLocalRouteToRouter(rPort: PortOrBuilder): MidoOpList = {
+        List(Create(newLocalRoute(rPort.getId,
+                                  rPort.getPortAddress)))
+    }
+}
+
+object RouteManager {
+    val META_DATA_SRVC = IPSubnetUtil.toProto(MetaDataService.IPv4_ADDRESS)
+    val DEFAULT_WEIGHT = 100
+
+    /**
+     * Tests whether route is to a port, i.e., next hop is PORT or LOCAL
+     * rather than REJECT or BLACKHOLE
+     */
+    protected def isToPort(route: RouteOrBuilder): Boolean =
+        route.getNextHop == NextHop.LOCAL || route.getNextHop == NextHop.PORT
+
+    /** ID of local route to port, based on port's ID. */
+    def localRouteId(portId: UUID): UUID =
+        portId.xorWith(0x13bd079b6c0e43fbL, 0x80fe647e6e718b72L)
+
+    /** ID of next-hop route from tenant router to provider router via
+      * gateway port, or vice-versa.
+      */
+    def gatewayRouteId(gwPortId: UUID): UUID =
+        gwPortId.xorWith(0x6ba5df84b8a44ab4L, 0x90adb3f665e7850dL)
+
+    def metadataServiceRouteId(dhcpPortId: UUID): UUID =
+        dhcpPortId.xorWith(0xa0132e5a1583461cL, 0xa752d8609a517a6cL)
 }
