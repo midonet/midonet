fe875cb3ac25f6122fc9f608ef5c6c2fd4571252
Tomoe Sugihara <tomoe@midokura.com>
2015-02-06 14:11:14 +0900

    Quick workaround for new zookeeper package
    Since new ZK package has gone upstart,
    /etc/init.d/zookeeper doesn't work inside netns.
    
    Change-Id: I30889a55eb3e69a478b696402ab9e02e509df49b

--- tests/mmm/scripts/zookeeper/start-nth.sh
+++ tests/mmm/scripts/zookeeper/start-nth.sh
@@ -16,4 +16,5 @@
 
 find /var/log/zookeeper -type f -exec rm -f '{}' ';'
 
-/etc/init.d/zookeeper start
+/usr/bin/java -cp /etc/zookeeper/conf:/usr/share/java/jline.jar:/usr/share/java/log4j-1.2.jar:/usr/share/java/xercesImpl.jar:/usr/share/java/xmlParserAPIs.jar:/usr/share/java/netty.jar:/usr/share/java/slf4j-api.jar:/usr/share/java/slf4j-log4j12.jar:/usr/share/java/zookeeper.jar -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.local.only=false -Dzookeeper.log.dir=/var/log/zookeeper -Dzookeeper.root.logger=INFO,ROLLINGFILE org.apache.zookeeper.server.quorum.QuorumPeerMain /etc/zookeeper/conf/zoo.cfg &
+
