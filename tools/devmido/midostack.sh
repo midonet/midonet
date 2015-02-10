414157f1eaad8c2d00ae4e02531ef996dac7749b
Ryu Ishimoto <ryu@midokura.com>
2015-02-10 14:57:07 +0000

    midostack.sh to glue devstack and devmido
    Create a script that exports appropriate environment
    variables from devstack to devmido so that they can be
    fully integrated.
    
    Change-Id: I14908c5d9524efe8e98b2e3e290c659faebcf317

--- tools/devmido/midostack.sh
+++ tools/devmido/midostack.sh
@@ -15,7 +15,20 @@
 # limitations under the License.
 
 # This script is meant to be sourced from devstack.  It is a wrapper of
-# mido.sh that allows proper exporting of environment variables to mido.sh.
+# devmido scripts that allows proper exporting of environment variables.
+#
+# Example:
+#    source ./midostack.sh mido                 // Runs mido.sh
+#    source ./midostack.sh unmido               // Runs unmido.sh
+#    source ./midostack.sh create_fake_uplink   // Runs create_fake_uplink.sh
+#    source ./midostack.sh delete_fake_uplink   // Runs delete_fake_uplink.sh
+
+if [[ -n "$1" ]]; then
+    CMD=$1
+else
+    echo "Usage: source midostack.sh <COMMAND>"
+    exit -1
+fi
 
 # Back up the current options of devstack so that we can change them in here
 # as we wish
@@ -27,15 +40,31 @@ set -o errexit
 
 # Sanity checks - these must be set
 MIDONET_DIR=${MIDONET_DIR:?Error \$MIDONET_DIR is not set}
-export SERVICE_HOST=${MIDONET_SERVICE_HOST:?Error \$MIDONET_SERVICE_HOST is not set}
-export API_PORT=${MIDONET_API_PORT:?Error \$MIDONET_API_PORT is not set}
 
-# Share the same logging locations
-export TIMESTAMP_FORMAT
-export LOGFILE
-export SCREEN_LOGDIR
+if [[ $CMD == "mido" ]]; then
 
-$MIDONET_DIR/tools/devmido/mido.sh
+    export SERVICE_HOST=${MIDONET_SERVICE_HOST:?Error \$MIDONET_SERVICE_HOST is not set}
+    export API_PORT=${MIDONET_API_PORT:?Error \$MIDONET_API_PORT is not set}
+
+    export TIMESTAMP_FORMAT
+    export LOGFILE
+    export SCREEN_LOGDIR
+
+elif [[ $CMD == "unmido" ]]; then
+
+    :  # Nothing to export
+
+elif [[ $CMD == "create_fake_uplink" ]] || [[ $CMD == "delete_fake_uplink" ]]; then
+
+    export CIDR=${FLOATING_RANGE:?Error \$FLOATING_RANGE is not set}
+
+else
+    echo "Error - Unrecognized command: $CMD"
+    exit -1
+fi
+
+# Run the command
+$MIDONET_DIR/tools/devmido/$CMD.sh
 
 # Restore the options
 eval "$OLD_OPTS" 2> /dev/null
