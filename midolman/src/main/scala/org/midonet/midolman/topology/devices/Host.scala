adead884811d2601427fbb6dc62af79b131772ce
Alex Bikfalvi <alex.bikfalvi@midokura.com>
2015-02-16 00:02:43 +0100

    Host mapper fails when host is not a tunnel-zone member
    This patch fixes the following issues:
    - the host mapper fails if the host is configured with a tunnel
    zone, but the host is not included in the zone membership
    - the mapper may miss notifcations of tunnel zone observables for
    hosts with more than one tunnel zone, and if the notification of
    the host is synchronous with the subscription
    - all notifications are emitted on a VT thread, such that different
    mappes may subscribe to the VT
    
    Fixes: MNA-106
    
    Change-Id: I8c3db6c73db7ae2922026143782e6ecdb63bde81
    Signed-off-by: Alex Bikfalvi <alex.bikfalvi@midokura.com>

--- midolman/src/main/scala/org/midonet/midolman/topology/devices/Host.scala
+++ midolman/src/main/scala/org/midonet/midolman/topology/devices/Host.scala
@@ -24,77 +24,76 @@ import org.midonet.cluster.models.Topology.Host.PortToInterface
 import org.midonet.cluster.util.MapConverter
 import org.midonet.cluster.util.UUIDUtil.{Converter => UUIDConverter, _}
 import org.midonet.midolman.topology.VirtualTopology.Device
+import org.midonet.midolman.topology.devices.Host.PortInterfaceConverter
 import org.midonet.packets.IPAddr
 
-/**
- * This class implements the MapConverter trait to do the conversion between
- * tuples of type (UUID, String) and PortToInterface protos.
- */
-class PortInterfaceConverter extends MapConverter[UUID, String, PortToInterface] {
+object Host {
 
-    override def toKey(proto: PortToInterface): UUID = {
-        proto.getPortId.asJava
+    /**
+     * This class implements the [[MapConverter]] trait to do the conversion
+     * between tuples of type (UUID, String) and PortToInterface messages.
+     */
+    class PortInterfaceConverter
+        extends MapConverter[UUID, String, PortToInterface] {
+
+        override def toKey(proto: PortToInterface): UUID = {
+            proto.getPortId.asJava
+        }
+
+        override def toValue(proto: PortToInterface): String = {
+            proto.getInterfaceName
+        }
+
+        override def toProto(key: UUID, value: String): PortToInterface = {
+            PortToInterface.newBuilder
+                .setPortId(key.asProto)
+                .setInterfaceName(value)
+                .build()
+        }
     }
 
-    def toValue(proto: PortToInterface): String = {
-        proto.getInterfaceName
-    }
-
-    def toProto(key: UUID, value: String): PortToInterface = {
-        PortToInterface.newBuilder
-            .setPortId(key.asProto)
-            .setInterfaceName(value)
-            .build()
-    }
 }
 
 @ZoomClass(clazz = classOf[Topology.Host])
 class Host extends ZoomObject with Device {
 
-    def this(host: Host) = {
+    def this(hostId: UUID, isHostAlive: Boolean, ports: Map[UUID, String],
+             zones: Map[UUID, IPAddr]) = {
         this()
-        id = host.id
-        portToInterface = host.portToInterface
-        tunnelZoneIds = host.tunnelZoneIds
-        tunnelZones = host.tunnelZones
-        alive = host.alive
+        id = hostId
+        portBindings = ports
+        tunnelZoneIds = zones.keySet
+        tunnelZones = zones
+        alive = isHostAlive
     }
 
     @ZoomField(name = "id", converter = classOf[UUIDConverter])
     var id: UUID = _
     @ZoomField(name = "port_interface_mapping",
                converter = classOf[PortInterfaceConverter])
-    var portToInterface = Map.empty[UUID, String]
+    var portBindings = Map.empty[UUID, String]
     @ZoomField(name = "tunnel_zone_ids", converter = classOf[UUIDConverter])
     var tunnelZoneIds = Set.empty[UUID]
 
     // To be filled by the HostMapper.
     // The IP address of the host in each one of the tunnel zones.
     var tunnelZones = Map.empty[UUID, IPAddr]
-
     // The alive status of the host is stored outside of the host proto.
-    var alive: Boolean = _
+    var alive: Boolean = false
 
-    def this(hostId: UUID, isHostAlive: Boolean, ports: Map[UUID, String],
-             zones: Map[UUID, IPAddr]) = {
-        this()
-        id = hostId
-        portToInterface = ports
-        tunnelZoneIds = zones.keySet
-        tunnelZones = zones
-        alive = isHostAlive
-    }
+    override def toString =
+        s"Host [id=$id portBindings=$portBindings tunnelZones=$tunnelZoneIds]"
 
     override def equals(o: Any): Boolean = o match {
         case host: Host =>
             host.alive == alive &&
             host.id == id &&
-            host.portToInterface == portToInterface &&
+            host.portBindings == portBindings &&
             host.tunnelZoneIds == tunnelZoneIds &&
             host.tunnelZones == tunnelZones
         case _ => false
     }
 
     override def hashCode: Int =
-        Objects.hashCode(alive, id, portToInterface, tunnelZoneIds, tunnelZones)
+        Objects.hashCode(alive, id, portBindings, tunnelZoneIds, tunnelZones)
 }
