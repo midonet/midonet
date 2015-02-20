369837e52cf7c6b3a6a0add05c59d87ba9b909fc
Duarte Nunes <duarte@midokura.com>
2015-02-20 11:33:11 +0100

    Flow state: really invalidate flows on new state
    Change-Id: I37a26ead84ad94f3db15d8a2ed0313e307c73c84
    Signed-off-by: Duarte Nunes <duarte@midokura.com>

--- midolman/src/main/scala/org/midonet/midolman/DeduplicationActor.scala
+++ midolman/src/main/scala/org/midonet/midolman/DeduplicationActor.scala
@@ -206,11 +206,11 @@ class DeduplicationActor(
             datapath = dp
             dpState = state
             replicator = new FlowStateReplicator(connTrackStateTable,
-                natStateTable,
-                storage,
-                dpState,
-                FlowController ! _,
-                datapath)
+                                                 natStateTable,
+                                                 storage,
+                                                 dpState,
+                                                 FlowController ! InvalidateFlowsByTag(_),
+                                                 datapath)
             pendingFlowStateBatches foreach (self ! _)
             workflow = new PacketWorkflow(dpState, datapath, clusterDataClient,
                                           dpConnPool, cbExecutor, actionsCache,
