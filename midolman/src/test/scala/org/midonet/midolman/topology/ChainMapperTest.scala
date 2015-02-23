19190583e4625e3765ab39c67f8553cdfc65d3d6
Alex Bikfalvi <alex.bikfalvi@midokura.com>
2015-02-23 14:03:26 +0100

    A router mapper for the virtual topology
    Adds an implementation of the VirtualDeviceMapper for a
    router simulation device.
    
    Ref: MNA-124
    
    Change-Id: I56d11259b502b4c5266e10ce313fc666d01eb7bf
    Signed-off-by: Alex Bikfalvi <alex.bikfalvi@midokura.com>

--- midolman/src/test/scala/org/midonet/midolman/topology/ChainMapperTest.scala
+++ midolman/src/test/scala/org/midonet/midolman/topology/ChainMapperTest.scala
@@ -61,7 +61,7 @@ class ChainMapperTest extends TestKit(ActorSystem("ChainMapperTest"))
     }
 
     private def assertThread(): Unit = {
-        assert(vt.threadId == Thread.currentThread.getId)
+        assert(vt.vtThreadId == Thread.currentThread.getId)
     }
 
     private def subscribeToChain(count: Int, chainId: UUID) = {
