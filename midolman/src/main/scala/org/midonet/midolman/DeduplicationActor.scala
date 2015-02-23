7974f38c8cb0681e71da8384f5d38a67c6cb872e
Ivan Kelly <ivan@midokura.com>
2015-02-23 12:57:05 +0100

    Add request id to trace rules
    Allow a unique identifier to be attached to a trace rule when it is
    being created. This can then be used to query all trace logs for that
    particular trace session.
    
    Ref MN-3196
    
    Change-Id: Ie8df0a9a5e97f2db8cea85263ac98ff26d2179f8
    Signed-off-by: Ivan Kelly <ivan@midokura.com>

--- midolman/src/main/scala/org/midonet/midolman/DeduplicationActor.scala
+++ midolman/src/main/scala/org/midonet/midolman/DeduplicationActor.scala
@@ -82,6 +82,7 @@ class DeduplicationActor(
     import DeduplicationActor._
     import PacketWorkflow._
 
+    private val TraceLoggingContextKey = "traceId"
     override def logSource = "org.midonet.packet-worker"
 
     var dpState: DatapathState = null
@@ -312,6 +313,7 @@ class DeduplicationActor(
             if (context.ingressed)
                 packetOut(1)
             MDC.remove("cookie")
+            MDC.remove(TraceLoggingContextKey)
         }
 
     protected def runWorkflow(pktCtx: PacketContext): Unit =
@@ -323,8 +325,8 @@ class DeduplicationActor(
             }
         } catch {
             case TraceRequiredException =>
-                pktCtx.setTracingEnabled()
                 pktCtx.log.debug(s"Enabling trace for $pktCtx, and rerunning simulation")
+                MDC.put(TraceLoggingContextKey, pktCtx.tracingContext)
                 runWorkflow(pktCtx)
             case NotYetException(f, msg) =>
                 pktCtx.log.debug(s"Postponing simulation because: $msg")
