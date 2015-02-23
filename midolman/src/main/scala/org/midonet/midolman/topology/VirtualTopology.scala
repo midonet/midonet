f3dba1f89645329e8e5647502dd59f5376e9e55c
Alex Bikfalvi <alex.bikfalvi@midokura.com>
2015-02-23 14:03:26 +0100

    A router mapper for the virtual topology
    Adds an implementation of the VirtualDeviceMapper for a
    router simulation device.
    
    Ref: MNA-124
    
    Change-Id: I56d11259b502b4c5266e10ce313fc666d01eb7bf
    Signed-off-by: Alex Bikfalvi <alex.bikfalvi@midokura.com>

--- midolman/src/main/scala/org/midonet/midolman/topology/VirtualTopology.scala
+++ midolman/src/main/scala/org/midonet/midolman/topology/VirtualTopology.scala
@@ -16,24 +16,31 @@
 package org.midonet.midolman.topology
 
 import java.util.UUID
+import java.util.concurrent.atomic.AtomicInteger
 import java.util.concurrent.{ConcurrentHashMap, Executors, ThreadFactory}
 
 import scala.concurrent.{Future, Promise}
 import scala.reflect._
 
 import com.google.inject.Inject
+
 import rx.Observable
 import rx.schedulers.Schedulers
 
 import org.midonet.cluster.DataClient
 import org.midonet.cluster.services.MidonetBackend
+import org.midonet.cluster.state.StateStorage
 import org.midonet.midolman.FlowController.InvalidateFlowsByTag
+import org.midonet.midolman.config.MidolmanConfig
 import org.midonet.midolman.logging.MidolmanLogging
 import org.midonet.midolman.services.MidolmanActorsService
+import org.midonet.midolman.simulation.Router
 import org.midonet.midolman.state.ZkConnectionAwareWatcher
 import org.midonet.midolman.topology.devices._
 import org.midonet.midolman.{FlowController, NotYetException}
 import org.midonet.sdn.flows.FlowTagger.FlowTag
+import org.midonet.util.executors.SameThreadExecutor
+import org.midonet.util.functors.makeRunnable
 import org.midonet.util.reactivex._
 
 /**
@@ -152,7 +159,9 @@ object VirtualTopology extends MidolmanLogging {
  * | Port/Network/RouterMapper extends DeviceMapper | (1 per device)
  * +------------------------------------------------+
  */
-class VirtualTopology @Inject() (val backend: MidonetBackend,
+class VirtualTopology @Inject() (val config: MidolmanConfig,
+                                 val backend: MidonetBackend,
+                                 val state: StateStorage,
                                  val dataClient: DataClient,
                                  val connectionWatcher: ZkConnectionAwareWatcher,
                                  val actorsService: MidolmanActorsService)
@@ -162,16 +171,27 @@ class VirtualTopology @Inject() (val backend: MidonetBackend,
 
     override def logSource = "org.midonet.devices.devices-service"
 
-    @volatile private[topology] var threadId: Long = _
-    private[topology] val executor = Executors.newSingleThreadExecutor(
+    @volatile private[topology] var vtThreadId: Long = _
+    private val vtExecutor = Executors.newSingleThreadExecutor(
         new ThreadFactory {
             override def newThread(r: Runnable): Thread = {
                 val thread = new Thread(r, "devices-service")
-                threadId = thread.getId
+                vtThreadId = thread.getId
                 thread
             }
         })
-    private[topology] val scheduler = Schedulers.from(executor)
+    private[topology] val vtScheduler = Schedulers.from(vtExecutor)
+
+    private val ioThreadIndex = new AtomicInteger()
+    private val ioExecutorFactory = new ThreadFactory {
+        override def newThread(r: Runnable): Thread = {
+            new Thread(r, s"devices-io-${ioThreadIndex.getAndIncrement}")
+        }
+    }
+    private val ioExecutor =
+        if (config.getTopologyAsyncIo)
+            Executors.newCachedThreadPool(ioExecutorFactory)
+        else SameThreadExecutor
 
     private[topology] val devices =
         new ConcurrentHashMap[UUID, Device]()
@@ -184,7 +204,8 @@ class VirtualTopology @Inject() (val backend: MidonetBackend,
         classTag[BridgePort] -> (new PortMapper(_, this)),
         classTag[VxLanPort] -> (new PortMapper(_, this)),
         classTag[TunnelZone] -> (new TunnelZoneMapper(_, this)),
-        classTag[Host] -> (new HostMapper(_, this))
+        classTag[Host] -> (new HostMapper(_, this)),
+        classTag[Router] -> (new RouterMapper(_, this)(actorsService.system))
     )
 
     register(this)
@@ -213,5 +234,29 @@ class VirtualTopology @Inject() (val backend: MidonetBackend,
 
     private[topology] def invalidate(tag: FlowTag): Unit = {
         FlowController.getRef()(actorsService.system) ! InvalidateFlowsByTag(tag)
+    }
+
+    /** Safely executes a task on the virtual topology thread. */
+    private[topology] def executeVt(task: => Unit) = {
+        vtExecutor.execute(makeRunnable {
+            try {
+                task
+            } catch {
+                case e: Throwable =>
+                    log.error("Uncaught exception on topology thread.", e)
+            }
+        })
+    }
+
+    /** Safely executes a task on the IO thread(s). */
+    private[topology] def executeIo(task: => Unit) = {
+        ioExecutor.execute(makeRunnable {
+            try {
+                task
+            } catch {
+                case e: Throwable =>
+                    log.error("Uncaught exception on topology IO thread.", e)
+            }
+        })
     }
 }
