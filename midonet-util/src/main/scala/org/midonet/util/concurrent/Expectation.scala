2da9c8eaa80f7aafa92e0392177ee060c0d2ef7b
Ernest Artiaga <ernest@midokura.com>
2015-03-17 16:46:38 +0100

    A wrapper around scala promises and futures
    This is a wrapper class to facilitate the use
    of Scala promises and futures from Java code.
    This overcomes issues due to some complex type
    translations from Java to Scala and vice versa.
    (this helps, in particular, the integration of
    Scala code with the new OVSDB library...)
    
    Change-Id: Ic3798c3fb7fe49b67c7d12fad3e94173b4817d0f
    Signed-off-by: Ernest Artiaga <ernest@midokura.com>

---
+++ midonet-util/src/main/scala/org/midonet/util/concurrent/Expectation.scala
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2015 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.util.concurrent
+
+import java.util.concurrent.TimeoutException
+
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, ExecutionContext, Future, Promise}
+import scala.util.{Failure, Success}
+
+/**
+ * A class to wrap scala promises and futures for using them from Java
+ * @tparam T is the type of the promise value
+ */
+class Expectation[T](final val promise: Promise[T]) {
+    def this() = this(Promise[T]())
+
+    import Expectation._
+
+    /** Set the success value */
+    def success(value: T): Unit = promise.success(value)
+    /** Set the failure exception */
+    def failure(exception: Throwable): Unit = promise.failure(exception)
+
+
+    /** Callbacks for promise completion */
+    def onSuccess(cb: OnSuccess[T], ec: ExecutionContext): Unit =
+        promise.future.onSuccess({case v => cb.call(v)})(ec)
+    def onFailure(cb: OnFailure, ec: ExecutionContext): Unit =
+        promise.future.onFailure({case e => cb.call(e)})(ec)
+    def onComplete(cb: OnComplete[T], ec: ExecutionContext): Unit =
+        promise.future.onComplete({
+            case Success(v) => cb.onSuccess(v)
+            case Failure(e) => cb.onFailure(e)
+        })(ec)
+
+    /** Extract the future */
+    def future: Future[T] = promise.future
+
+    /** Implicit conversion to future */
+    implicit def expectationToFuture(e: Expectation[T]): Future[T] = e.future
+
+    /** Wait until the promise has been fulfilled */
+    @throws[TimeoutException]
+    @throws[InterruptedException]
+    def await(timeOut: Duration): this.type = {
+        Await.ready(promise.future, timeOut)
+        this
+    }
+
+    /* This is equivalent to using a default parameter, but usable from Java */
+    @throws[TimeoutException]
+    @throws[InterruptedException]
+    def await(): this.type = {Await.ready(promise.future, Duration.Inf); this}
+
+    /** Wait until the promise is fulfilled, and return the value or throw
+      * the exception */
+    @throws[Exception]
+    def result(timeOut: Duration): T = Await.result(promise.future, timeOut)
+
+    /* This is equivalent to using a default parameter, but usable from Java */
+    @throws[Exception]
+    def result(): T = Await.result(promise.future, Duration.Inf)
+}
+
+object Expectation {
+    trait OnSuccess[T] {
+        def call(value: T): Unit
+    }
+    trait OnFailure {
+        def call(exception: Throwable): Unit
+    }
+    trait OnComplete[T] {
+        def onSuccess(value: T): Unit
+        def onFailure(exception: Throwable): Unit
+    }
+}
