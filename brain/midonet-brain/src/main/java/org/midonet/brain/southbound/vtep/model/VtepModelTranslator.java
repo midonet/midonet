6543c3b87855f0bd42007ecee567e9a28092af03
Ernest Artiaga <ernest@midokura.com>
2015-03-20 12:33:54 +0100

    Migrate to new Vtep Data models
    Remove the old ovsdb-dependent vtep-related
    data model and make midonet-brain and midonet-api
    depend on the ovsdb-agnostic data models in the
    cluster module.
    
    This is part of the preparation for the
    introduction of a new vtep support implementation
    based on the standard ovsdb library, instead of
    our own forked ovsdb branch.
    
    Ref: MN-2543
    
    Change-Id: I698e1b047c672d29b1b3f189de2fa49be629f809
    Signed-off-by: Ernest Artiaga <ernest@midokura.com>

--- brain/midonet-brain/src/main/java/org/midonet/brain/southbound/vtep/model/VtepModelTranslator.java
+++ brain/midonet-brain/src/main/java/org/midonet/brain/southbound/vtep/model/VtepModelTranslator.java
@@ -16,6 +16,7 @@
 package org.midonet.brain.southbound.vtep.model;
 
 import java.math.BigInteger;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
@@ -30,106 +31,131 @@ import org.opendaylight.ovsdb.lib.table.vtep.Physical_Switch;
 import org.opendaylight.ovsdb.lib.table.vtep.Ucast_Macs_Local;
 import org.opendaylight.ovsdb.lib.table.vtep.Ucast_Macs_Remote;
 
+import org.midonet.cluster.data.vtep.model.LogicalSwitch;
+import org.midonet.cluster.data.vtep.model.McastMac;
+import org.midonet.cluster.data.vtep.model.PhysicalPort;
+import org.midonet.cluster.data.vtep.model.PhysicalSwitch;
+import org.midonet.cluster.data.vtep.model.UcastMac;
+import org.midonet.packets.IPv4Addr;
+
 /**
  * Collection of translators between VTEP entities between the OVSDB schema and
  * the Midonet schema.
  */
 public class VtepModelTranslator {
 
-    public static LogicalSwitch toMido(Logical_Switch ovsdbLs, UUID uuid) {
+    public static java.util.UUID toMido(UUID uuid) {
+        return java.util.UUID.fromString(uuid.toString());
+    }
+
+    // TODO: ovsdb-specific UUIDs should be eventually erradicated
+    public static UUID fromMido(java.util.UUID uuid) {
+        return new UUID(uuid.toString());
+    }
+
+    public static LogicalSwitch toMido(Logical_Switch ovsdbLs,
+                                       java.util.UUID uuid) {
         return new LogicalSwitch(
             uuid,
-            ovsdbLs.getDescription(),
             ovsdbLs.getName(),
             // NOTE: this complicated thing is necessary because for some
             // reason the tunnel_key OvsdbSet<Integer> contains an array of
-            // BigInteger which breaks when trying to use an interator.
+            // BigInteger which breaks when trying to use an iterator.
             ovsdbLs.getTunnel_key().isEmpty()
                 ? null
                 :  ((BigInteger)ovsdbLs.getTunnel_key()
-                                       .toArray()[0]).intValue()
+                                       .toArray()[0]).intValue(),
+            ovsdbLs.getDescription()
         );
     }
 
-    public static PhysicalSwitch toMido(Physical_Switch ovsdbPs, UUID uuid) {
+    public static PhysicalSwitch toMido(Physical_Switch ovsdbPs,
+                                        java.util.UUID uuid) {
 
         Set<String> ports = new HashSet<>();
         for (UUID port : ovsdbPs.getPorts()) {
             ports.add(port.toString());
         }
+        Set<IPv4Addr> mgmtIps = new HashSet<>();
+        for (String ip: ovsdbPs.getManagement_ips().delegate()) {
+            mgmtIps.add(IPv4Addr.fromString(ip));
+        }
+        Set<IPv4Addr> tunnelIps = new HashSet<>();
+        for (String ip: ovsdbPs.getTunnel_ips().delegate()) {
+            tunnelIps.add(IPv4Addr.fromString(ip));
+        }
 
         return new PhysicalSwitch(
             uuid,
-            ovsdbPs.getDescription(),
             ovsdbPs.getName(),
+            ovsdbPs.getDescription(),
             ports,
-            ovsdbPs.getManagement_ips().delegate(),
-            ovsdbPs.getTunnel_ips().delegate()
+            mgmtIps,
+            tunnelIps
         );
     }
 
     public static PhysicalPort toMido(Physical_Port ovsdbPort) {
-        return new PhysicalPort(
-            ovsdbPort.getDescription(),
+        return new PhysicalPort(null,
             ovsdbPort.getName(),
-            bigIntToShort(ovsdbPort.getVlan_bindings()),
-            bigIntToShort(ovsdbPort.getVlan_stats()),
+            ovsdbPort.getDescription(),
+            toMido(ovsdbPort.getVlan_bindings()),
+            toMido(ovsdbPort.getVlan_stats()),
             ovsdbPort.getPort_fault_status()
         );
     }
 
     public static UcastMac toMido(Ucast_Macs_Remote ovsdbMac) {
-        return new UcastMac(
-            ovsdbMac.getMac(),
+        return UcastMac.apply(null,
             ovsdbMac.getLogical_switch().isEmpty()
-                ? null : ovsdbMac.getLogical_switch().iterator().next(),
+            ? null : toMido(ovsdbMac.getLogical_switch().iterator().next()),
+            ovsdbMac.getMac(),
+            ovsdbMac.getIpaddr(),
             ovsdbMac.getLocator().isEmpty()
-                ? null : ovsdbMac.getLocator().iterator().next(),
-            ovsdbMac.getIpaddr()
+            ? null : toMido(ovsdbMac.getLocator().iterator().next())
         );
     }
 
     public static UcastMac toMido(Ucast_Macs_Local ovsdbMac) {
-        return new UcastMac(
-            ovsdbMac.getMac(),
+        return UcastMac.apply(null,
             ovsdbMac.getLogical_switch().isEmpty()
-                ? null : ovsdbMac.getLogical_switch().iterator().next(),
+                ? null: toMido(ovsdbMac.getLogical_switch().iterator().next()),
+            ovsdbMac.getMac(),
+            ovsdbMac.getIpaddr(),
             ovsdbMac.getLocator().isEmpty()
-                ? null : ovsdbMac.getLocator().iterator().next(),
-            ovsdbMac.getIpaddr()
+                ? null : toMido(ovsdbMac.getLocator().iterator().next())
         );
     }
 
     public static McastMac toMido(Mcast_Macs_Local ovsdbMac) {
-        return new McastMac(
-            ovsdbMac.getMac(),
+        return McastMac.apply(null,
             ovsdbMac.getLogical_switch().isEmpty()
-                ? null : ovsdbMac.getLogical_switch().iterator().next(),
+                ? null : toMido(ovsdbMac.getLogical_switch().iterator().next()),
+            ovsdbMac.getMac(),
+            ovsdbMac.getIpaddr(),
             ovsdbMac.getLocator_set().isEmpty()
-                ? null : ovsdbMac.getLocator_set().iterator().next(),
-            ovsdbMac.getIpaddr()
+                ? null : toMido(ovsdbMac.getLocator_set().iterator().next())
         );
     }
 
     public static McastMac toMido(Mcast_Macs_Remote ovsdbMac) {
-        return new McastMac(
-            ovsdbMac.getMac(),
+        return McastMac.apply(null,
             ovsdbMac.getLogical_switch().isEmpty()
-                ? null : ovsdbMac.getLogical_switch().iterator().next(),
+                ? null : toMido(ovsdbMac.getLogical_switch().iterator().next()),
+            ovsdbMac.getMac(),
+            ovsdbMac.getIpaddr(),
             ovsdbMac.getLocator_set().isEmpty()
-                ? null : ovsdbMac.getLocator_set().iterator().next(),
-            ovsdbMac.getIpaddr()
+                ? null : toMido(ovsdbMac.getLocator_set().iterator().next())
         );
     }
 
-    private static <T> OvsDBMap<Short, T>
-        bigIntToShort(OvsDBMap<BigInteger, T> map) {
-        OvsDBMap<Short, T> shortMap = new OvsDBMap<>();
-        for(Map.Entry<BigInteger, T> e : map.entrySet()) {
-            shortMap.put(e.getKey().shortValue(), e.getValue());
+    private static Map<Integer, java.util.UUID>
+        toMido(OvsDBMap<BigInteger, UUID> map) {
+        Map<Integer, java.util.UUID> intMap = new HashMap<>();
+        for(Map.Entry<BigInteger, UUID> e: map.entrySet()) {
+            intMap.put(e.getKey().intValue(), toMido(e.getValue()));
         }
-        return shortMap;
+        return intMap;
     }
-
 }
 
