e5b1e7a5166aecb8ba7a08752dc11c1e27b3ceac
Ernest Artiaga <ernest@midokura.com>
2015-03-20 11:51:36 +0100

    Models for VTEP Data
    This patch introduces the basic data models
    used to handle a VTEP, and makes them
    independent from low-level implementations
    (namely ovsdb-specific data types). This is
    a preparation patch to make midonet-brain
    and midonet-api to depend on the cluster
    module instead of depending directly on
    the ovsdb library.
    
    Ref: MN-2543
    
    Change-Id: Iecfa57fe757a2de4cb79cda3809786b48caa0427
    Signed-off-by: Ernest Artiaga <ernest@midokura.com>

---
+++ cluster/src/main/scala/org/midonet/cluster/data/vtep/model/PhysicalLocatorSet.scala
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2015 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.cluster.data.vtep.model
+
+import java.util.{Objects, UUID}
+
+/**
+ * Represents a VTEP's physical locator set. The set of locators may be empty,
+ * but not null.
+ * @param id is UUID of this locator set in OVSDB
+ * @param locators is the (immutable) set of the ids of locators in this set
+ */
+final class PhysicalLocatorSet(id: UUID, locators: Set[UUID]) {
+    val uuid = if (id == null) UUID.randomUUID() else id
+    val locatorIds: Set[UUID] = if (locators == null) Set() else locators
+
+    override def toString: String = "PhysicalLocatorSet{" +
+        "uuid=" + uuid + ", " +
+        "locatorIds='" + locatorIds + "'}"
+
+    override def equals(o: Any): Boolean = o match {
+        case null => false
+        case that: PhysicalLocatorSet =>
+            Objects.equals(uuid, that.uuid) &&
+            Objects.equals(locatorIds, that.locatorIds)
+        case other => false
+    }
+
+    override def hashCode: Int = uuid.hashCode()
+}
+
+object PhysicalLocatorSet {
+    def apply(id: UUID, locators: Set[UUID]): PhysicalLocatorSet =
+        new PhysicalLocatorSet(id, locators)
+    def apply(locators: Set[UUID]): PhysicalLocatorSet =
+        new PhysicalLocatorSet(null, locators)
+}
+
+
+
