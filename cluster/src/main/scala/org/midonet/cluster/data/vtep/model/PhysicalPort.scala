c2cf00824015d1056f708ee9ff5b4f31b88887b1
Ernest Artiaga <ernest@midokura.com>
2015-03-20 11:51:36 +0100

    Models for VTEP Data
    This patch introduces the basic data models
    used to handle a VTEP, and makes them
    independent from low-level implementations
    (namely ovsdb-specific data types). This is
    a preparation patch to make midonet-brain
    and midonet-api to depend on the cluster
    module instead of depending directly on
    the ovsdb library.
    
    Ref: MN-2543
    
    Change-Id: Iecfa57fe757a2de4cb79cda3809786b48caa0427
    Signed-off-by: Ernest Artiaga <ernest@midokura.com>

---
+++ cluster/src/main/scala/org/midonet/cluster/data/vtep/model/PhysicalPort.scala
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2015 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.cluster.data.vtep.model
+
+import java.util
+import java.util.{Objects, UUID}
+
+import scala.collection.JavaConversions.{asScalaSet, mapAsScalaMap}
+
+/**
+ * Represents a physical port in a physical switch. Note that the maps and sets
+ * may be empty, but not null.
+ * Due to some ovsdb specifications being unclear on using null values or
+ * empty strings to indicate unset string values, this class unifies both
+ * representations. Note that 'name', as an identifier, is not allowed to
+ * be null.
+ * @param id is UUID of this locator in OVSDB
+ * @param ppName physical port name
+ * @param desc physical port description
+ * @param bindings a map with a pair (Vlan key -> LogicalSwitch id) for each
+ *                 binding
+ * @param stats a map with a pair (Vlan key -> stats info id) for each
+ *              binding, if supported by switch
+ * @param faultStatus (undocumented feature from ovsdb)
+ */
+final class PhysicalPort(id: UUID, ppName: String, desc: String,
+                         bindings: Map[Integer, UUID],
+                         stats: Map[Integer, UUID],
+                         faultStatus: Set[String]) {
+    val uuid = if (id == null) UUID.randomUUID() else id
+    val name: String = if (ppName == null) "" else ppName
+    val description: String = if (desc == null || desc.isEmpty) null else desc
+
+    val vlanBindings: Map[Integer, UUID] =
+        if (bindings == null) Map() else bindings
+    val vlanStats: Map[Integer, UUID] =
+        if (stats == null) Map() else stats
+    val portFaultStatus: Set[String] =
+        if (faultStatus == null) Set() else faultStatus
+
+    private val str: String = "PhysicalPort{" +
+        "uuid=" + uuid + ", " +
+        "name='" + name + "', " +
+        "description='" + description + "'}"
+
+    override def toString = str
+
+    override def equals(o: Any): Boolean = o match {
+        case null => false
+        case that: PhysicalPort => Objects.equals(name, that.name)
+        case other => false
+    }
+
+    override def hashCode: Int = uuid.hashCode()
+}
+
+object PhysicalPort {
+    def apply(id: UUID, name: String, desc: String,
+              bindings: Map[Integer, UUID], stats: Map[Integer, UUID],
+              faultStatus: Set[String]): PhysicalPort =
+        new PhysicalPort(id, name, desc, bindings, stats, faultStatus)
+
+    // Java compatibility stuff
+    def apply(id: UUID, name: String, desc: String,
+              bindings: util.Map[Integer, UUID], stats: util.Map[Integer, UUID],
+              faultStatus: util.Set[String]): PhysicalPort =
+        new PhysicalPort(
+            id, name, desc,
+            if (bindings == null) null else mapAsScalaMap(bindings).toMap,
+            if (stats == null) null else mapAsScalaMap(stats).toMap,
+            if (faultStatus == null) null else asScalaSet(faultStatus).toSet)
+
+    // For testing purposes
+    def apply(id: UUID, name: String, desc: String): PhysicalPort =
+        new PhysicalPort(id, name, desc, null, null, null)
+
+}
+
+
