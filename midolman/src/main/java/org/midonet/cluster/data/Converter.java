033ef4a33bb19faa73058cc5aca95ce412a13333
Ivan Kelly <ivan@midokura.com>
2015-03-23 12:39:03 +0100

    Add request id to trace rules
    Allow a unique identifier to be attached to a trace rule when it is
    being created. This can then be used to query all trace logs for that
    particular trace session.
    
    Ref MN-3196
    
    Change-Id: Ie8df0a9a5e97f2db8cea85263ac98ff26d2179f8
    Signed-off-by: Ivan Kelly <ivan@midokura.com>

--- midolman/src/main/java/org/midonet/cluster/data/Converter.java
+++ midolman/src/main/java/org/midonet/cluster/data/Converter.java
@@ -510,7 +510,7 @@ public class Converter {
         if (rule instanceof TraceRule) {
             TraceRule typedRule = (TraceRule) rule;
             ruleConfig = new org.midonet.midolman.rules.TraceRule(
-                    typedRule.getCondition());
+                    typedRule.getRequestId(), typedRule.getCondition());
         }
 
         if (rule instanceof JumpRule) {
@@ -567,7 +567,10 @@ public class Converter {
         }
 
         if (ruleConfig instanceof org.midonet.midolman.rules.TraceRule) {
-            rule = new TraceRule(ruleConfig.getCondition());
+            org.midonet.midolman.rules.TraceRule casted
+                = (org.midonet.midolman.rules.TraceRule)ruleConfig;
+            rule = new TraceRule(casted.getRequestId(),
+                    ruleConfig.getCondition());
         }
 
         if (ruleConfig instanceof org.midonet.midolman.rules.JumpRule) {
