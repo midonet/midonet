cfb26f490b03587862e51f812b2ede7e071adcfa
Galo Navarro <galo@midokura.com>
2015-03-23 10:58:44 +0100

    Debian packaging for Midonet Cluster
    Change-Id: I2635093c16de295c6cc4ba4eb516699ff21a163f
    Signed-off-by: Galo Navarro <galo@midokura.com>

---
+++ brain/midonet-brain/src/deb/init/midolman-env.sh
@@ -0,0 +1,7 @@
+MIDO_HOME=/usr/share/midonet-brain
+MIDO_ETC=/etc/midonet-brain
+MIDO_CFG=/etc/midonet-brain
+MIDO_LOG_DIR=/var/log/midonet-brain/
+MIDO_DEBUG_PORT=8001
+MIDO_CFG_FILE=midonet-brain.conf
+MIDO_LOG_BACK=logback.xml
