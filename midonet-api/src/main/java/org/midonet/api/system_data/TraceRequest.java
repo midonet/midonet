3014e8a3174ebd05a6ad07ac76ebecdf160ead9f
Ivan Kelly <ivan@midokura.com>
2015-03-23 15:17:23 +0100

    Add human readable names to trace requests
    Allow human readable names to be attached to trace requests to make it
    easy for users to see which trace request is which.
    
    Ref MN-3333
    
    Change-Id: Id9291d63a94c37cc91f1623c9c85fc2a6c5440ef
    Signed-off-by: Ivan Kelly <ivan@midokura.com>

--- midonet-api/src/main/java/org/midonet/api/system_data/TraceRequest.java
+++ midonet-api/src/main/java/org/midonet/api/system_data/TraceRequest.java
@@ -35,6 +35,9 @@ public class TraceRequest extends UriResource {
     private UUID id;
 
     @NotNull
+    private String name;
+
+    @NotNull
     private DeviceType deviceType;
 
     @NotNull
@@ -50,24 +53,26 @@ public class TraceRequest extends UriResource {
         super();
     }
 
-    public TraceRequest(UUID id, DeviceType deviceType,
+    public TraceRequest(UUID id, String name, DeviceType deviceType,
                         UUID deviceId, Condition condition, boolean enabled) {
         super();
         this.id = id;
+        this.name = name;
         this.deviceType = deviceType;
         this.deviceId = deviceId;
         this.condition = condition;
         this.enabled = enabled;
     }
 
-    public TraceRequest(UUID id, DeviceType deviceType,
+    public TraceRequest(UUID id, String name, DeviceType deviceType,
                         UUID deviceId, Condition condition) {
-        this(id, deviceType, deviceId, condition, false);
+        this(id, name, deviceType, deviceId, condition, false);
     }
 
     public TraceRequest(org.midonet.cluster.data.TraceRequest traceRequest) {
         super();
         this.id = traceRequest.getId();
+        this.name = traceRequest.getName();
         this.deviceType = traceRequest.getDeviceType();
         this.deviceId = traceRequest.getDeviceId();
         this.condition = new Condition();
@@ -81,6 +86,14 @@ public class TraceRequest extends UriResource {
 
     public UUID getId() {
         return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public void setDeviceType(DeviceType deviceType) {
@@ -138,6 +151,7 @@ public class TraceRequest extends UriResource {
         org.midonet.midolman.rules.Condition c = condition.makeCondition();
         return new org.midonet.cluster.data.TraceRequest()
             .setId(id)
+            .setName(name)
             .setDeviceType(deviceType)
             .setDeviceId(deviceId)
             .setCondition(c);
