583d6c68c500b64efc3f8915f61a1e06a41a7053
Galo Navarro <galo@midokura.com>
2015-03-31 15:46:22 +0200

    Compat API, first iteration with basic CRUD
    Also, support for hosts (which require a custom render given that the
    alive field comes from a State node, not part of the proto.)
    
    Change-Id: I88f5d5ebad7404734298e9ee3136b3653d44742c
    Signed-off-by: Galo Navarro <galo@midokura.com>
    Signed-off-by: Alex Bikfalvi <alex.bikfalvi@midokura.com>

---
+++ brain/midonet-brain/src/main/java/org/midonet/brain/services/rest_api/models/ExteriorBridgePort.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2015 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.midonet.brain.services.rest_api.models;
+
+import java.util.UUID;
+
+public class ExteriorBridgePort extends BridgePort implements ExteriorPort {
+
+    public ExteriorBridgePort() {
+        super();
+    }
+
+    @Override
+    public String getType() {
+        return PortType.EXTERIOR_BRIDGE;
+    }
+
+    @Override
+    public UUID getVifId() {
+        return this.vifId;
+    }
+
+    @Override
+    public void setVifId(UUID vifId) {
+        this.vifId = vifId;
+    }
+
+}
