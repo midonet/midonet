e8ce3235da074d4b8cb8b7b3dbaaee428d68bd59
Galo Navarro <galo@midokura.com>
2015-03-31 15:46:22 +0200

    Compat API, first iteration with basic CRUD
    Also, support for hosts (which require a custom render given that the
    alive field comes from a State node, not part of the proto.)
    
    Change-Id: I88f5d5ebad7404734298e9ee3136b3653d44742c
    Signed-off-by: Galo Navarro <galo@midokura.com>
    Signed-off-by: Alex Bikfalvi <alex.bikfalvi@midokura.com>

---
+++ brain/midonet-brain/src/main/scala/org/midonet/brain/services/rest_api/SystemStateResource.scala
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2015 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.brain.services.rest_api
+
+import javax.ws.rs.core.MediaType
+import javax.ws.rs.{GET, Path, Produces}
+
+import com.google.inject.servlet.RequestScoped
+
+import org.midonet.brain.services.rest_api.models.SystemState
+
+@Path("system_state") // TODO: why?!?? figure out how to use the ResourceUris one
+@RequestScoped
+class SystemStateResource {
+
+    // TODO: we're cheating here, it should use a real system state, but for
+    //       MDTS we don't really care
+    @GET
+    @Produces(Array(MidonetMediaTypes.APPLICATION_SYSTEM_STATE_JSON,
+                    MidonetMediaTypes.APPLICATION_SYSTEM_STATE_JSON_V2,
+                    MediaType.APPLICATION_JSON))
+    def get(): SystemState = {
+        val st = new SystemState()
+        st.state = "ACTIVE"
+        st.availability = "READWRITE"
+        st
+    }
+
+}
