e649a2e89af175690b25792420086420c7aeac70
Guillermo Ontanon <guillermo@midokura.jp>
2015-04-15 11:36:58 +0200

    Implement a new ARP table
    Substitute the pre-existing ARP table with a new implementation that
    follows these principles:
    
      * Single threaded. Each simulation thread whould have its own instance
        and drive the ARP table in the PacketWorkFlow process() method.
      * No coordination for ARP requests. Thus, agents don't write null entries
        to the ZK replicated map anymore. Expiry is still handled by the
        agent that wrote the entry.
      * ARP requests intervals are randomized to prevent multiple ARP requests,
        due to lack of coordination, to be sent at the same time.
    
    Change-Id: Ib4b7f35ecb7843715b1233b878f3da2c3a5596e6
    Signed-off-by: Guillermo Ontanon <guillermo@midokura.jp>
    (cherry picked from commit 70489019764b9f0bb6cb118b05485f05de5a853c)

--- midolman/src/main/scala/org/midonet/midolman/PacketWorkflow.scala
+++ midolman/src/main/scala/org/midonet/midolman/PacketWorkflow.scala
@@ -180,6 +180,8 @@ class PacketWorkflow(
     protected val traceStateTx = new FlowStateTransaction(traceStateTable)
     protected var replicator: FlowStateReplicator = _
 
+    protected val arpBroker = new ArpRequestBroker(genPacketEmitter, config, flowInvalidator )
+
     private val invalidateExpiredConnTrackKeys =
         new Reducer[ConnTrackKey, ConnTrackValue, Unit]() {
             override def apply(u: Unit, k: ConnTrackKey, v: ConnTrackValue) {
@@ -244,7 +246,8 @@ class PacketWorkflow(
 
     override def shouldProcess(): Boolean =
         super.shouldProcess() ||
-        genPacketEmitter.pendingPackets > 0
+        genPacketEmitter.pendingPackets > 0 ||
+        arpBroker.shouldProcess()
 
     override def process(): Unit = {
         super.process()
@@ -253,6 +256,7 @@ class PacketWorkflow(
         natStateTable.expireIdleEntries((), invalidateExpiredNatKeys)
         natLeaser.obliterateUnusedBlocks()
         traceStateTable.expireIdleEntries()
+        arpBroker.process()
     }
 
     protected def packetContext(packet: Packet): PacketContext =
@@ -268,7 +272,7 @@ class PacketWorkflow(
         val cookie = cookieGen.next
         log.debug(s"Creating new PacketContext for cookie $cookie")
         val context = new PacketContext(cookie, packet, fmatch, egressPort)
-        context.reset(genPacketEmitter)
+        context.reset(genPacketEmitter, arpBroker)
         context.initialize(connTrackTx, natTx, natLeaser, traceStateTx)
         context.log = PacketTracing.loggerFor(fmatch)
         context
