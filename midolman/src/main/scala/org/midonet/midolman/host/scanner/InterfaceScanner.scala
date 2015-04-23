6e205301568bf2c4531efaab992354c4a1108aa3
Taku Fukushima <tfukushima@midokura.com>
2015-04-23 16:27:23 +0900

    Replace polling InterfaceScanner with rtnetlink
    This patch replaces the existing InterfaceScanner which scans the
    interfaces on the host polling in every few seconds with the one
    that speaks rtnetlink and emits the event stream with RxJava.
    
    Change-Id: I267498aaefae433e16eebc9eac2595708895e827
    Signed-off-by: Taku Fukushima <tfukushima@midokura.com>

---
+++ midolman/src/main/scala/org/midonet/midolman/host/scanner/InterfaceScanner.scala
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2015 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.midolman.host.scanner
+
+import rx.{Observer, Subscription}
+
+import org.midonet.midolman.host.interfaces.InterfaceDescription
+import org.midonet.netlink.rtnetlink.AbstractRtnetlinkConnection
+
+/**
+ * Interface data scanning API. It's job is scan and find out the
+ * current list of interface data from the local system and notify
+ * observers whenever there are changes.
+ */
+trait InterfaceScanner extends AbstractRtnetlinkConnection {
+    /**
+     * Let an Observer to subscribe notifications from InterfaceScanner.
+     *
+     * @param obs an Observer to subscribe a published set of all L2 Ethernet
+     *            interfaces on the host notified when a interface is
+     *            added/remove to/from the host.
+     * @return Subscription object through which users can unsubscribe events
+     *         from InterfaceScanner.
+     */
+    def subscribe(obs: Observer[Set[InterfaceDescription]]): Subscription
+    /**
+     * Start scanning and notifying the interfaces on the host.
+     */
+    def start(): Unit
+
+    /**
+     * Stop scanning and notifying the interfaces on the host.
+     */
+    def stop(): Unit
+}
--- midolman/src/test/scala/org/midonet/midolman/util/mock/MockFlowProcessor.scala
+++ midolman/src/test/scala/org/midonet/midolman/util/mock/MockFlowProcessor.scala
@@ -20,16 +20,17 @@ import java.nio.ByteBuffer
 import java.util.{Map => JMap}
 
 import com.typesafe.scalalogging.Logger
-import org.midonet.midolman.DatapathStateDriver
-import org.midonet.odp.OpenVSwitch.Flow.Attr
-import org.midonet.odp.family.{PacketFamily, FlowFamily, PortFamily, DatapathFamily}
-import org.midonet.odp.flows.FlowKeys
 import org.slf4j.LoggerFactory
 import rx.Observer
 
+import org.midonet.midolman.DatapathStateDriver
 import org.midonet.midolman.datapath.FlowProcessor
-import org.midonet.netlink.{NetlinkMessage, MockNetlinkChannelFactory}
-import org.midonet.odp.{Datapath, OvsNetlinkFamilies, Flow, FlowMatch}
+import org.midonet.midolman.util.MockNetlinkChannelFactory
+import org.midonet.netlink.NetlinkMessage
+import org.midonet.odp.OpenVSwitch.Flow.Attr
+import org.midonet.odp.family.{DatapathFamily, FlowFamily, PacketFamily, PortFamily}
+import org.midonet.odp.flows.FlowKeys
+import org.midonet.odp.{Datapath, Flow, FlowMatch, OvsNetlinkFamilies}
 import org.midonet.util.concurrent.MockClock
 
 class MockFlowProcessor(val flowsTable: JMap[FlowMatch, Flow] = null)
