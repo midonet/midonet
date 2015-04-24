6e3433abd4898660bbbe3bf227445c1e11bf8507
Ryu Ishimoto <ryu@midokura.com>
2015-04-24 01:08:40 +0000

    mm-ctl access ZK directly
    Instead of going through the Neutron DB, access the ZK (DataClient
    or ZOOM) directly from mm-ctl so that it avoids dependency to the
    Neutron DB.
    
    Change-Id: I98aae5a9a982c6031ad9edbd4afa72855839b1c8
    Implements: MNA-262

---
+++ midolman/src/main/scala/org/midonet/midolman/tools/MmCtl.scala
@@ -0,0 +1,273 @@
+/*
+ * Copyright 2015 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.midolman.tools
+
+import java.util.UUID
+
+import com.google.inject.{Guice, Injector}
+import com.sun.security.auth.module.UnixSystem
+import org.apache.commons.cli._
+import org.midonet.cluster.DataClient
+import org.midonet.cluster.data.storage.Storage
+import org.midonet.cluster.models.Topology
+import org.midonet.cluster.services.MidonetBackend
+import org.midonet.cluster.storage.MidonetBackendModule
+import org.midonet.cluster.util.UUIDUtil
+import org.midonet.conf.{HostIdGenerator, MidoNodeConfigurator}
+import org.midonet.midolman.cluster.LegacyClusterModule
+import org.midonet.midolman.cluster.serialization.SerializationModule
+import org.midonet.midolman.cluster.zookeeper.ZookeeperConnectionModule
+import org.midonet.midolman.config.MidolmanConfig
+import org.midonet.midolman.guice.config.MidolmanConfigModule
+import org.midonet.midolman.state.{StateAccessException, ZookeeperConnectionWatcher}
+import org.midonet.util.concurrent.toFutureOps
+
+
+sealed class MmCtlResult(exitCode: Int, message: String) {
+    def getExitCode: Int = this.exitCode
+    def isSuccess = this.exitCode == 0
+    def getMessage = this.message
+}
+
+object MmCtlResult {
+
+    sealed abstract class MmCtlRetCode(code: Int, msg: String)
+        extends Ordered[MmCtlRetCode] {
+
+        def compare(that: MmCtlRetCode) = this.code - that.getCode
+        def getResult = new MmCtlResult(this.code, this.msg)
+        def getCode = this.code
+    }
+
+    case object UNKNOWN_ERROR extends MmCtlRetCode(-1, "Command failed")
+    case object SUCCESS extends MmCtlRetCode(0, "Command succeeded")
+    case object BAD_COMMAND extends MmCtlRetCode(1, "Invalid command")
+    case object HOST_ID_NOT_FOUND extends MmCtlRetCode(2,
+                                                       "Failed to get host ID")
+    case object STATE_ERROR extends MmCtlRetCode(3,
+                                                 "State configuration error")
+    case object PERMISSION_DENIED extends MmCtlRetCode(13, "Permission denied")
+
+}
+
+trait PortBinder {
+
+    import MmCtlResult._
+
+    def bindPort(portId: UUID, hostId: UUID, deviceName: String): MmCtlResult
+    def unbindPort(portId: UUID, hostId: UUID): MmCtlResult
+
+    def bindPort(portId: UUID, deviceName: String): MmCtlResult = {
+        try {
+            bindPort(portId, HostIdGenerator.getHostId, deviceName)
+        } catch {
+            case e: HostIdGenerator.PropertiesFileNotWritableException =>
+                HOST_ID_NOT_FOUND.getResult
+        }
+    }
+
+    def unbindPort(portId: UUID): MmCtlResult = {
+        try {
+            unbindPort(portId, HostIdGenerator.getHostId)
+        } catch {
+            case e: HostIdGenerator.PropertiesFileNotWritableException =>
+                HOST_ID_NOT_FOUND.getResult
+        }
+    }
+}
+
+class DataClientPortBinder(dataClient: DataClient) extends PortBinder {
+
+    import MmCtlResult._
+
+    override def bindPort(portId: UUID, hostId: UUID,
+                          deviceName: String): MmCtlResult = {
+        try {
+            dataClient.hostsAddVrnPortMapping(hostId, portId, deviceName)
+        } catch {
+            case e: StateAccessException =>
+                return STATE_ERROR.getResult
+            case e: Exception =>
+                return UNKNOWN_ERROR.getResult
+        }
+        SUCCESS.getResult
+    }
+
+    override def unbindPort(portId: UUID, hostId: UUID): MmCtlResult = {
+        try {
+            dataClient.hostsDelVrnPortMapping(hostId, portId)
+        } catch {
+            case e: StateAccessException =>
+                return STATE_ERROR.getResult
+            case e: Exception =>
+                return UNKNOWN_ERROR.getResult
+        }
+        SUCCESS.getResult
+    }
+}
+
+class ZoomPortBinder(storage: Storage) extends PortBinder {
+
+    import MmCtlResult._
+
+    override def bindPort(portId: UUID, hostId: UUID,
+                          deviceName: String): MmCtlResult = {
+        val builder = storage.get(classOf[Topology.Port],
+                               portId).await().toBuilder
+        val port = builder.setHostId(
+            UUIDUtil.toProto(hostId)).setInterfaceName(deviceName).build()
+        storage.update(port)
+        SUCCESS.getResult
+    }
+
+    override def unbindPort(portId: UUID, hostId: UUID): MmCtlResult = {
+        val builder = storage.get(classOf[Topology.Port],
+                                  portId).await().toBuilder
+        val port = builder.clearHostId().clearInterfaceName().build()
+        storage.update(port)
+        SUCCESS.getResult
+    }
+}
+
+class MmCtl(injector: Injector) {
+
+    val portBinder = getPortBinder(injector)
+
+    def getPortBinder(injector: Injector): PortBinder = {
+        val dataClient = injector.getInstance(classOf[DataClient])
+        val backend = injector.getInstance(classOf[MidonetBackend])
+        val config = injector.getInstance(classOf[MidolmanConfig])
+
+        // TODO: Change 'neutron' to 'cluster'
+        if (config.neutron.enabled)
+            new ZoomPortBinder(backend.store)
+        else
+            new DataClientPortBinder(dataClient)
+    }
+
+    def bindPort(portId: UUID, deviceName: String): MmCtlResult = {
+        portBinder.bindPort(portId, deviceName)
+    }
+
+    def unbindPort(portId: UUID): MmCtlResult = {
+        portBinder.unbindPort(portId)
+    }
+}
+
+
+object MmCtl {
+
+    import MmCtlResult._
+
+    def getInjector: Injector = {
+        val configurator: MidoNodeConfigurator = MidoNodeConfigurator.apply()
+        val config: MidolmanConfig = MidolmanConfigModule
+            .createConfig(configurator)
+        Guice.createInjector(new MidolmanConfigModule(config),
+                             new MidonetBackendModule(config.zookeeper),
+                             new ZookeeperConnectionModule(
+                                 classOf[ZookeeperConnectionWatcher]),
+                             new SerializationModule,
+                             new LegacyClusterModule)
+    }
+
+    def getPortUuid(v: String): UUID = {
+        try {
+            UUID.fromString(v)
+        }
+        catch {
+            case e: IllegalArgumentException =>
+                throw new ParseException("Invalid port ID encountered")
+        }
+    }
+
+    def getMutuallyExclusiveOptionGroup: OptionGroup = {
+
+        val mutuallyExclusiveOptions = new OptionGroup
+
+        OptionBuilder.hasArgs(2)
+        OptionBuilder.isRequired
+        OptionBuilder.withLongOpt("bind-port")
+        OptionBuilder.withDescription("Bind a port to an interface")
+        mutuallyExclusiveOptions.addOption(OptionBuilder.create)
+
+        OptionBuilder.hasArg
+        OptionBuilder.isRequired
+        OptionBuilder.withLongOpt("unbind-port")
+        OptionBuilder.withDescription("Unbind a port from an interface")
+        mutuallyExclusiveOptions.addOption(OptionBuilder.create)
+
+        mutuallyExclusiveOptions.setRequired (true)
+        mutuallyExclusiveOptions
+
+        // TODO: add a debug mode
+    }
+
+    def checkUserAccess() = {
+        if (new UnixSystem().getUid != 0) {
+            System.err.println("This command should be executed by root.")
+            System.exit(PERMISSION_DENIED.getCode)
+        }
+    }
+
+    def main(args: Array[String]): Unit = {
+
+        checkUserAccess()
+
+        // Configure the CLI options
+        val options = new Options
+        options.addOptionGroup(getMutuallyExclusiveOptionGroup)
+
+        val parser = new PosixParser
+        try {
+            val cl = parser.parse(options, args)
+            val mmctl = new MmCtl(getInjector)
+            var res: MmCtlResult = null
+
+            if (cl.hasOption("bind-port")) {
+                val opts = cl.getOptionValues("bind-port")
+                if (opts == null || opts.length < 2) {
+                    throw new ParseException("bind-port requires two " +
+                                             "arguments: port ID and device " +
+                                             "name")
+                }
+                res = mmctl.bindPort(getPortUuid(opts(0)), opts(1))
+            }
+            else if (cl.hasOption("unbind-port")) {
+                val opt = cl.getOptionValue("unbind-port")
+                res = mmctl.unbindPort(getPortUuid(opt))
+            }
+            else {
+                throw new RuntimeException("Unknown option encountered.")
+            }
+
+            if (res.isSuccess)
+                System.out.println(res.getMessage)
+            else
+                System.err.println(res.getMessage)
+
+            System.exit(res.getExitCode)
+        }
+        catch {
+            case e: ParseException =>
+                System.err.println("Error with the options: " + e.getMessage)
+                val formatter = new HelpFormatter
+                formatter.printHelp("mm-ctl", options)
+                System.exit(BAD_COMMAND.getCode)
+        }
+    }
+}
