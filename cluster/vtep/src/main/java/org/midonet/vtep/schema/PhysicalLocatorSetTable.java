0dd6c0895dd9f632bacaf58ebf898d00818c0ed0
Ernest Artiaga <ernest@midokura.com>
2015-05-03 11:13:37 +0200

    OVSDB Vtep schema translations
    This patch is part of the port of Ovsdb support
    to the new Ovsdb client libraries. It consolidates
    the code to translate from internal OVSDB-based
    vtep data schemas to implementation-agnostic
    vtep data models.
    
    These schemas provide low-level support and are
    based on the specs in
    http://openvswitch.org/docs/vtep.5.pdf
    and the reference implementation of the ovsdb
    library: https://github.com/opendaylight/ovsdb
    
    Ref: MN-2543
    
    Change-Id: I4f17f55a48fa981a25b04e5e3fb33caa1ad88aba
    Signed-off-by: Ernest Artiaga <ernest@midokura.com>

--- cluster/vtep/src/main/java/org/midonet/vtep/schema/PhysicalLocatorSetTable.java
+++ cluster/vtep/src/main/java/org/midonet/vtep/schema/PhysicalLocatorSetTable.java
@@ -28,8 +28,7 @@ import org.opendaylight.ovsdb.lib.schema.DatabaseSchema;
 import org.opendaylight.ovsdb.lib.schema.GenericTableSchema;
 
 import org.midonet.cluster.data.vtep.model.PhysicalLocatorSet;
-
-import static scala.collection.JavaConversions.setAsJavaSet;
+import org.midonet.cluster.data.vtep.model.VtepEntry;
 
 import static org.midonet.vtep.OvsdbTranslator.fromOvsdb;
 import static org.midonet.vtep.OvsdbTranslator.toOvsdb;
@@ -38,16 +37,22 @@ import static org.midonet.vtep.OvsdbTranslator.toOvsdb;
  * Schema for the Ovsdb physical switch table
  */
 public final class PhysicalLocatorSetTable extends Table {
-    static private final String TB_NAME = "Physical_Locator_Set";
+    static public final String TB_NAME = "Physical_Locator_Set";
     static private final String COL_LOCATORS = "locators";
 
     public PhysicalLocatorSetTable(DatabaseSchema databaseSchema) {
-        super(databaseSchema, TB_NAME);
+        super(databaseSchema, TB_NAME, PhysicalLocatorSet.class);
+    }
+
+    public String getName() {
+        return TB_NAME;
     }
 
     /** Get the schema of the columns of this table */
+    @Override
     public List<ColumnSchema<GenericTableSchema, ?>> getColumnSchemas() {
-        List<ColumnSchema<GenericTableSchema, ?>> cols = super.getColumnSchemas();
+        List<ColumnSchema<GenericTableSchema, ?>> cols =
+            super.partialColumnSchemas();
         cols.add(getLocatorsSchema());
         return cols;
     }
@@ -72,16 +77,34 @@ public final class PhysicalLocatorSetTable extends Table {
     /**
      * Extract the locator set object
      */
-    public PhysicalLocatorSet parseLocatorSet(Row<GenericTableSchema> row) {
-        return PhysicalLocatorSet.apply(parseUuid(row), parseLocators(row));
+    @Override
+    @SuppressWarnings(value = "unckecked")
+    public <E extends VtepEntry>
+    E parseEntry(Row<GenericTableSchema> row, Class<E> clazz)
+        throws IllegalArgumentException {
+        ensureOutputClass(clazz);
+        return (row == null)? null:
+               (E)PhysicalLocatorSet.apply(parseUuid(row), parseLocators(row));
     }
 
     /**
      * Generate an insert command
      */
-    public Insert<GenericTableSchema> insert(PhysicalLocatorSet set) {
-        Insert<GenericTableSchema> op = super.insert(set.uuid());
-        op.value(getLocatorsSchema(), toOvsdb(setAsJavaSet(set.locatorIds())));
-        return op;
+    @Override
+    public <E extends VtepEntry> OvsdbInsert insert(E row)
+        throws IllegalArgumentException {
+        Insert<GenericTableSchema> op = newInsert(row);
+        PhysicalLocatorSet set = (PhysicalLocatorSet)row;
+        op.value(getLocatorsSchema(), toOvsdb(set.locatorIds()));
+        return new OvsdbInsert(op);
+    }
+
+    @Override
+    public <E extends VtepEntry> Row<GenericTableSchema> generateRow(
+        E entry) throws IllegalArgumentException {
+        Row<GenericTableSchema> row = super.generateRow(entry);
+        PhysicalLocatorSet data = (PhysicalLocatorSet)entry;
+        addToRow(row, getLocatorsSchema(), toOvsdb(data.locatorIds()));
+        return row;
     }
 }
