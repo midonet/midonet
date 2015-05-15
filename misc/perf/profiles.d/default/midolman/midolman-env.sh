c551f31f93ec13fc7a910df847a1c45e0a3c9206
Guillermo Ontanon <guillermo@midokura.jp>
2015-05-15 16:16:57 +0200

    agent: allow configuration of watchdog behavior
    Add two options to midolman-env.sh to let operators
    disable the watchdog or tune it's timeout.
    
    Change-Id: Ie94aeaff9653eb21df5b2cbcea9a5c145deab49a
    Signed-off-by: Guillermo Ontanon <guillermo@midokura.jp>

--- misc/perf/profiles.d/default/midolman/midolman-env.sh
+++ misc/perf/profiles.d/default/midolman/midolman-env.sh
@@ -14,65 +14,25 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-check_for_java7() {
-    [ "x" = "x$1" ] && return 1
-    [ -x "$1" ] || return 1
-    $1 -version 2>&1 | grep '^java version' | sed -e 's/^[^"]*"\(.*\)"$/\1/' \
-        | grep '^1.7.' >/dev/null 2>&1
-}
+MIDO_HOME=/usr/share/midolman
+MIDO_CFG=/etc/midolman
+MIDO_LOG_DIR=/var/log/midolman/
+MIDO_DEBUG_PORT=8001
+JMX_PORT="7200"
+MIDO_CFG_FILE=midolman.conf
+QUAGGA_DIR=/var/run/quagga
+# setting this option will make the agent not run under the watchdog
+WATCHDOG_DISABLE=yes
+WATCHDOG_TIMEOUT=10
 
 # The first existing directory is used for JAVA_HOME if needed.
 JVM_SEARCH_DIRS="/usr/lib/jvm/java-7-openjdk-amd64 /usr/lib/jvm/java-7-openjdk"
 
-if [ -n "`which java`" ]; then
-        java=`which java`
-        # Dereference symlink(s)
-        while true; do
-            if [ -h "$java" ]; then
-                java=`readlink "$java"`
-                continue
-            fi
-            break
-        done
-        JVM_SEARCH_DIRS="`dirname $java`/../ $JVM_SEARCH_DIRS"
-fi
-if [ ! -z "$JAVA_HOME" ]; then
-    JVM_SEARCH_DIRS="$JAVA_HOME $JVM_SEARCH_DIRS"
-fi
-
-oldopts=$-
-set +e
-JAVA_HOME=
-for jdir in $JVM_SEARCH_DIRS; do
-    check_for_java7 "$jdir/bin/java"
-    if [ $? -eq 0 ]; then
-        JAVA_HOME="$jdir"
-        break
-    fi
-done
-echo $oldopts | grep 'e' 2>&1 >/dev/null && set -e
-
-if [ -z "$JAVA_HOME" ] ; then
-    echo "No suitable JVM found (at least v1.7 required)"
-    exit 1
-fi
-JAVA="$JAVA_HOME/bin/java"
-
-# Override these to set the amount of memory to allocate to the JVM at
-# start-up. For production use you almost certainly want to adjust
-# this for your environment. MAX_HEAP_SIZE is the total amount of
-# memory dedicated to the Java heap; HEAP_NEWSIZE refers to the size
-# of the young generation. Both MAX_HEAP_SIZE and HEAP_NEWSIZE should
-# be either set or not (if you set one, set the other).
-#
-# The main trade-off for the young generation is that the larger it
-# is, the longer GC pause times will be. The shorter it is, the more
-# expensive GC will be (usually).
-#
-# The example HEAP_NEWSIZE assumes a modern 8-core+ machine for decent pause
-# times. If in doubt, and if you do not particularly want to tweak, go with
-# 100 MB per physical CPU core.
+# Amount of memory to allocate for the JVM heap.
 MAX_HEAP_SIZE="300M"
+
+# Amount of memory, out of the total size defined by MAX_HEAP_SIZE, to 
+# allocate to the eden generation withih
 HEAP_NEWSIZE="200M"
 
 if [ "x$MAX_HEAP_SIZE" = "x" ] ||  [ "x$HEAP_NEWSIZE" = "x" ]; then
@@ -80,17 +40,12 @@ if [ "x$MAX_HEAP_SIZE" = "x" ] ||  [ "x$HEAP_NEWSIZE" = "x" ]; then
     exit 1
 fi
 
-# Specifies the default port over which Midolman will be available for
-# JMX connections.
-JMX_PORT="7200"
-
 # Here we create the arguments that will get passed to the jvm when
 # starting midolman.
 
 # enable assertions.  disabling this in production will give a modest
 # performance benefit (around 5%).
 # JVM_OPTS="$JVM_OPTS -ea"
-
 JVM_OPTS="$JVM_OPTS -XX:+AggressiveOpts"
 
 # enable thread priorities, primarily so we can give periodic tasks
@@ -107,16 +62,18 @@ JVM_OPTS="$JVM_OPTS -XX:ThreadPriorityPolicy=42"
 JVM_OPTS="$JVM_OPTS -Xms${MAX_HEAP_SIZE}"
 JVM_OPTS="$JVM_OPTS -Xmx${MAX_HEAP_SIZE}"
 JVM_OPTS="$JVM_OPTS -Xmn${HEAP_NEWSIZE}"
-JVM_OPTS="$JVM_OPTS -XX:+HeapDumpOnOutOfMemoryError" 
+JVM_OPTS="$JVM_OPTS -XX:HeapDumpPath=/var/log/midolman/"
+JVM_OPTS="$JVM_OPTS -XX:+HeapDumpOnOutOfMemoryError"
+JVM_OPTS="$JVM_OPTS -XX:OnOutOfMemoryError=\"kill;-3;%p\""
 
 # Do not use biased locking
 JVM_OPTS="$JVM_OPTS -XX:-UseBiasedLocking"
 
 # GC tuning options
-JVM_OPTS="$JVM_OPTS -XX:+UseParNewGC" 
-JVM_OPTS="$JVM_OPTS -XX:+UseConcMarkSweepGC" 
-JVM_OPTS="$JVM_OPTS -XX:+CMSParallelRemarkEnabled" 
-JVM_OPTS="$JVM_OPTS -XX:SurvivorRatio=8" 
+JVM_OPTS="$JVM_OPTS -XX:+UseParNewGC"
+JVM_OPTS="$JVM_OPTS -XX:+UseConcMarkSweepGC"
+JVM_OPTS="$JVM_OPTS -XX:+CMSParallelRemarkEnabled"
+JVM_OPTS="$JVM_OPTS -XX:SurvivorRatio=8"
 JVM_OPTS="$JVM_OPTS -XX:MaxTenuringThreshold=6"
 JVM_OPTS="$JVM_OPTS -XX:CMSInitiatingOccupancyFraction=75"
 JVM_OPTS="$JVM_OPTS -XX:+UseCMSInitiatingOccupancyOnly"
@@ -131,12 +88,12 @@ JVM_OPTS="$JVM_OPTS -XX:PretenureSizeThreshold=2m"
 # JVM_OPTS="$JVM_OPTS -XX:+PrintClassHistogram"
 # JVM_OPTS="$JVM_OPTS -XX:+PrintTenuringDistribution"
 # JVM_OPTS="$JVM_OPTS -XX:+PrintGCApplicationStoppedTime"
-# JVM_OPTS="$JVM_OPTS -Xloggc:/var/log/midolman/gc-`date +%s`.log"
+# JVM_OPTS="$JVM_OPTS -Xloggc:/var/log/midolman/gc-`date +%Y%m%d_%H%M%S`.log"
 
 # uncomment to have Midolman JVM listen for remote debuggers/profilers on port 1414
 # JVM_OPTS="$JVM_OPTS -Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=1414"
 
-# Prefer binding to IPv4 network intefaces (when net.ipv6.bindv6only=1). See 
+# Prefer binding to IPv4 network intefaces (when net.ipv6.bindv6only=1). See
 # http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=6342561 (short version:
 # comment out this entry to enable IPv6 support).
 # JVM_OPTS="$JVM_OPTS -Djava.net.preferIPv4Stack=true"
@@ -145,11 +102,11 @@ JVM_OPTS="$JVM_OPTS -XX:PretenureSizeThreshold=2m"
 # JMXDISABLE=true
 
 # jmx: metrics and administration interface
-# 
+#
 # add this if you're having trouble connecting:
 # JVM_OPTS="$JVM_OPTS -Djava.rmi.server.hostname=<public name>"
-# 
-# see 
+#
+# see
 # http://blogs.sun.com/jmxetc/entry/troubleshooting_connection_problems_in_jconsole
 # for more on configuring JMX through firewalls, etc. (Short version:
 # get it working with no firewall first.)
@@ -159,4 +116,12 @@ if [ "x$JMXDISABLE" = "x" ] ; then
     JVM_OPTS="$JVM_OPTS -Dcom.sun.management.jmxremote.port=$JMX_PORT"
     JVM_OPTS="$JVM_OPTS -Dcom.sun.management.jmxremote.ssl=false"
     JVM_OPTS="$JVM_OPTS -Dcom.sun.management.jmxremote.authenticate=false"
+    HOSTNAME=`hostname`
+    JVM_OPTS="$JVM_OPTS -Djava.rmi.server.hostname=$HOSTNAME"
+fi
+
+if [ "$MIDOLMAN_HPROF" = "1" ] ; then
+    DATE=$(date +'%H%M%S')
+    HPROF_FILENAME=${HPROF_FILENAME:-/tmp/midolman-$DATE.hprof}
+    JVM_OPTS="$JVM_OPTS -agentlib:hprof=cpu=samples,depth=100,interval=1,lineno=y,thread=y,file=$HPROF_FILENAME"
 fi
