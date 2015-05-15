4b55e14a002d62b2f902e7a07852d91eb3ee3ee2
alvico <albert.vico@midokura.com>
2015-05-15 19:25:02 +0200

    Added UDP capability to our ServerFrontEnd
    Allows ServerFrontEnd to work with UDP, also
    changed the derived classes and added a unit test.
    
    Change-Id: I6c600d334d66a5c7df895c33eb4e91917e0a0576
    Signed-off-by: alvico <albert.vico@midokura.com>

--- midonet-util/src/main/java/org/midonet/util/netty/ServerFrontEnd.java
+++ midonet-util/src/main/java/org/midonet/util/netty/ServerFrontEnd.java
@@ -18,15 +18,18 @@ package org.midonet.util.netty;
 
 import com.google.common.util.concurrent.AbstractService;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.netty.bootstrap.Bootstrap;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioDatagramChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * Plain netty-based server
@@ -35,29 +38,54 @@ public class ServerFrontEnd extends AbstractService {
     private static final Logger log =
         LoggerFactory.getLogger(ServerFrontEnd.class);
 
+    private final boolean datagram;
     private final int port;
-    private final EventLoopGroup boss = new NioEventLoopGroup();
+    private final EventLoopGroup boss;
     private final EventLoopGroup wrkr = new NioEventLoopGroup();
     private ChannelFuture sock;
 
     private final ChannelInboundHandlerAdapter adapter;
 
-    public ServerFrontEnd(ChannelInboundHandlerAdapter adapter, int port) {
+    public static ServerFrontEnd udp(ChannelInboundHandlerAdapter adapter, int port) {
+        return new ServerFrontEnd(adapter, port, true);
+    }
+
+    public static ServerFrontEnd tcp(ChannelInboundHandlerAdapter adapter, int port) {
+        return new ServerFrontEnd(adapter, port, false);
+    }
+
+    private ServerFrontEnd(ChannelInboundHandlerAdapter adapter, int port, boolean datagram) {
         this.adapter = adapter;
         this.port = port;
+        this.datagram = datagram;
+        if(datagram) {
+            boss = null;
+        } else {
+            boss = new NioEventLoopGroup();
+        }
     }
 
     @Override
     protected void doStart() {
-        log.info("Starting Netty server on port {}", port);
+
         try {
-            ServerBootstrap boot = new ServerBootstrap();
-            boot.group(boss, wrkr)
-                .channel(NioServerSocketChannel.class)
-                .childHandler(adapter)
-                .option(ChannelOption.SO_BACKLOG, 128)
-                .option(ChannelOption.SO_KEEPALIVE, true);
-            sock = boot.bind(port).sync();
+            if(datagram) {
+                log.info("Starting Netty UDP server on port {}", port);
+                Bootstrap boot = new Bootstrap();
+                boot.group(wrkr)
+                    .channel(NioDatagramChannel.class)
+                    .handler(adapter);
+                sock = boot.bind(port).sync();
+            } else {
+                log.info("Starting Netty TCP server on port {}", port);
+                ServerBootstrap boot = new ServerBootstrap();
+                boot.group(boss, wrkr)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(adapter)
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .option(ChannelOption.SO_KEEPALIVE, true);
+                sock = boot.bind(port).sync();
+            }
             log.info("Netty server started");
             notifyStarted();
         } catch (InterruptedException e) {
@@ -70,8 +98,10 @@ public class ServerFrontEnd extends AbstractService {
     @Override
     protected void doStop() {
         log.info("Stopping Netty server");
-        boss.shutdownGracefully();
         wrkr.shutdownGracefully();
+        if(!datagram) {
+            boss.shutdownGracefully();
+        }
         try {
             sock.channel().closeFuture().sync();
         } catch (InterruptedException e) {
