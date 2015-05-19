d58e05f745074532ea06eff3e28179db52d58a82
Galo Navarro <galo@midokura.com>
2015-05-19 19:34:39 +0200

    Unify rule DTOs (including trace reqs)
    Also a lot of cleanup, rearranging validations, conversions etc. into
    more logical places as before this was done inside the DTOs themselves
    which caused very deep coupling, bad separation of concerns, mixed
    responsibilities, etc.
    
    The trace request relies on Condition, so it forced me to refactor that
    one as well. Given that the tests were not following the exact same
    model as the other api tests, I found some additional changes required
    like adding media types, tweaking the client side calls from the test,
    etc.
    
    Ref.: MNA-342
    Signed-off-by: Galo Navarro <galo@midokura.com>
    
    Change-Id: I34ae9c3dc43d74127fd376c89eba842885ded6ea
    
    Trace Request
    
    Change-Id: If406bfb7f6c60a3caca1cbf917e738633930adf1
    Signed-off-by: Galo Navarro <galo@midokura.com>

---
+++ cluster/midonet-cluster/src/main/java/org/midonet/cluster/rest_api/models/TraceRequest.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2015 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.midonet.cluster.rest_api.models;
+
+import java.net.URI;
+import java.util.UUID;
+
+import javax.validation.constraints.NotNull;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.midonet.cluster.data.TraceRequest.DeviceType;
+
+/* Class representing trace info */
+@XmlRootElement
+public class TraceRequest extends UriResource {
+
+    @NotNull
+    public UUID id;
+
+    @NotNull
+    public String name;
+
+    @NotNull
+    public DeviceType deviceType;
+
+    @NotNull
+    public UUID deviceId;
+
+    @NotNull
+    public Condition condition;
+
+    public long creationTimestampMs = System.currentTimeMillis();
+
+    public long limit = Long.MAX_VALUE;
+
+    @NotNull
+    public boolean enabled;
+
+    public TraceRequest() {
+        super();
+    }
+
+    public TraceRequest(UUID id, String name, DeviceType deviceType,
+                        UUID deviceId, Condition condition,
+                        long creationTimestampMs,
+                        long limit, boolean enabled) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.deviceType = deviceType;
+        this.deviceId = deviceId;
+        this.condition = condition;
+        this.creationTimestampMs = creationTimestampMs;
+        this.limit = limit;
+        this.enabled = enabled;
+    }
+
+    @Override
+    public String toString() {
+        return "TraceRequest{name=" + name
+            + ", deviceType=" + deviceType
+            + ", deviceId=" + deviceId
+            + ", condition=" + condition
+            + ", creationTimestampMs=" + creationTimestampMs
+            + ", limit=" + limit
+            + ", enabled=" + enabled + "}";
+    }
+
+    @Override
+    public URI getUri() {
+        return absoluteUri(ResourceUris.TRACE_REQUESTS, id);
+    }
+}
