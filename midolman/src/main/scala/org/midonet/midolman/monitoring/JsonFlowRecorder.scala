bfdd779f182014c0d7d8ebfdc03044890eabeaa6
Ivan Kelly <ivan@midokura.com>
2015-05-20 15:33:12 +0200

    Json flow recorder
    Json implementation of flow recorder. Generates flow summaries from the
    packet context and outputs it to a udp port.
    
    Ref MN-3402
    
    Change-Id: I40cf2acb7733277e356d685b392c547c65dcdcfe
    Signed-off-by: Ivan Kelly <ivan@midokura.com>

---
+++ midolman/src/main/scala/org/midonet/midolman/monitoring/JsonFlowRecorder.scala
@@ -0,0 +1,177 @@
+/*
+ * Copyright 2015 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.midolman.monitoring
+
+import scala.collection.JavaConverters._
+
+import java.nio.ByteBuffer
+import java.util.{ArrayList, HashSet, List, Set, UUID}
+
+import org.midonet.cluster.flowhistory._
+import org.midonet.midolman.PacketWorkflow
+import org.midonet.midolman.PacketWorkflow.{SimulationResult => MMSimRes}
+import org.midonet.midolman.config.FlowHistoryConfig
+import org.midonet.midolman.rules.{RuleResult => MMRuleResult}
+import org.midonet.midolman.simulation.PacketContext
+import org.midonet.odp.FlowMatch
+import org.midonet.odp.flows._
+import org.midonet.packets.MAC
+import org.midonet.sdn.flows.FlowTagger.{DeviceTag, FlowTag}
+
+class JsonFlowRecorder(hostId: UUID, config: FlowHistoryConfig)
+        extends AbstractFlowRecorder(config) {
+
+    val serializer = new JsonSerialization
+
+    override def encodeRecord(pktContext: PacketContext,
+                              simRes: MMSimRes): ByteBuffer = {
+        ByteBuffer.wrap(
+            serializer.flowRecordToBuffer(buildRecord(pktContext, simRes)))
+    }
+
+    private def buildRecord(pktContext: PacketContext,
+                            simRes: MMSimRes): FlowRecord = {
+        FlowRecord(hostId, pktContext.inPortId,
+                   buildFlowRecordMatch(pktContext.origMatch),
+                   pktContext.cookie, buildDevices(pktContext.flowTags),
+                   buildRules(pktContext), buildSimResult(simRes),
+                   pktContext.outPorts, buildActions(pktContext.flowActions))
+    }
+
+    private def buildFlowRecordMatch(fmatch: FlowMatch): FlowRecordMatch = {
+        FlowRecordMatch(fmatch.getInputPortNumber, fmatch.getTunnelKey,
+                        fmatch.getTunnelSrc, fmatch.getTunnelDst,
+                        fmatch.getEthSrc.getAddress, fmatch.getEthDst.getAddress,
+                        fmatch.getEtherType, fmatch.getNetworkSrcIP.toBytes,
+                        fmatch.getNetworkDstIP.toBytes, fmatch.getNetworkProto,
+                        fmatch.getNetworkTTL, fmatch.getNetworkTOS,
+                        fmatch.getIpFragmentType.value,
+                        fmatch.getSrcPort, fmatch.getDstPort,
+                        fmatch.getIcmpIdentifier, fmatch.getIcmpData,
+                        fmatch.getVlanIds)
+    }
+
+    private def buildActions(actions: List[FlowAction]): Set[Actions.FlowAction] = {
+        val recActions = new HashSet[Actions.FlowAction]
+
+        def setKeyAction(action: FlowActionSetKey): Unit = {
+            action.getFlowKey match {
+                case a: FlowKeyARP =>
+                    recActions.add(Actions.Arp(a.arp_sip, a.arp_tip, a.arp_op,
+                                               a.arp_sha, a.arp_tha))
+                case a: FlowKeyEthernet =>
+                    recActions.add(Actions.Ethernet(a.eth_src, a.eth_dst))
+                case a: FlowKeyEtherType =>
+                    recActions.add(Actions.EtherType(a.etherType))
+                case a: FlowKeyICMPEcho =>
+                    recActions.add(Actions.IcmpEcho(a.icmp_type,
+                                                    a.icmp_code, a.icmp_id))
+                case a: FlowKeyICMPError =>
+                    recActions.add(Actions.IcmpError(a.icmp_type,
+                                                     a.icmp_code,
+                                                     a.icmp_data))
+                case a: FlowKeyICMP =>
+                    recActions.add(Actions.Icmp(a.icmp_type, a.icmp_code))
+                case a: FlowKeyIPv4 =>
+                    recActions.add(Actions.IPv4(a.ipv4_src, a.ipv4_dst,
+                                                a.ipv4_proto, a.ipv4_tos,
+                                                a.ipv4_ttl, a.ipv4_frag))
+                case a: FlowKeyTCP =>
+                    recActions.add(Actions.TCP(a.tcp_src.toShort,
+                                               a.tcp_dst.toShort))
+                case a: FlowKeyTunnel =>
+                    recActions.add(Actions.Tunnel(a.tun_id.toInt, a.ipv4_src,
+                                                  a.ipv4_dst, a.tun_flags,
+                                                  a.ipv4_tos, a.ipv4_ttl))
+                case a: FlowKeyUDP =>
+                    recActions.add(Actions.UDP(a.udp_src.toShort,
+                                               a.udp_dst.toShort))
+                case a: FlowKeyVLAN =>
+                    recActions.add(Actions.VLan(a.vlan))
+                case _ =>
+            }
+        }
+
+        var i = 0
+        while (i < actions.size) {
+            actions.get(i) match {
+                case a: FlowActionOutput =>
+                    recActions.add(Actions.Output(a.getPortNumber))
+                case a: FlowActionPopVLAN =>
+                    recActions.add(Actions.PopVlan())
+                case a: FlowActionPushVLAN =>
+                    recActions.add(Actions.PushVlan(a.getTagProtocolIdentifier,
+                                            a.getTagControlIdentifier))
+                case a: FlowActionSetKey =>
+                    setKeyAction(a)
+                case a: FlowActionUserspace =>
+                    recActions.add(Actions.Userspace(a.uplinkPid, a.userData))
+                case _ =>
+            }
+            i += 1
+        }
+        recActions
+    }
+
+    def buildDevices(tags: List[FlowTag]): List[UUID] = {
+        tags.asScala.collect({ case d: DeviceTag => d.device }).asJava
+    }
+
+    def buildRules(pktContext: PacketContext): List[TraversedRule] = {
+        var i = 0
+        val rules = new ArrayList[TraversedRule]
+
+        def convertResult(result: MMRuleResult): RuleResult.RuleResult =
+            result.action match {
+                case MMRuleResult.Action.ACCEPT => RuleResult.ACCEPT
+                case MMRuleResult.Action.CONTINUE => RuleResult.CONTINUE
+                case MMRuleResult.Action.DROP => RuleResult.DROP
+                case MMRuleResult.Action.JUMP => RuleResult.JUMP
+                case MMRuleResult.Action.REJECT => RuleResult.REJECT
+                case MMRuleResult.Action.RETURN => RuleResult.RETURN
+            }
+        while (i < pktContext.traversedRules.size) {
+            rules.add(TraversedRule(
+                          pktContext.traversedRules.get(i),
+                          convertResult(pktContext.traversedRuleResults.get(i))))
+            i += 1
+        }
+        rules
+    }
+
+    def buildSimResult(simRes: MMSimRes): SimulationResult.SimulationResult = {
+        simRes match {
+            case PacketWorkflow.NoOp => SimulationResult.NOOP
+            case PacketWorkflow.Drop => SimulationResult.DROP
+            case PacketWorkflow.ErrorDrop => SimulationResult.TEMP_DROP
+            case PacketWorkflow.ShortDrop => SimulationResult.TEMP_DROP
+            case PacketWorkflow.AddVirtualWildcardFlow =>
+                SimulationResult.ADD_VIRTUAL_WILDCARD_FLOW
+            case PacketWorkflow.StateMessage => SimulationResult.STATE_MESSAGE
+            case PacketWorkflow.UserspaceFlow =>
+                SimulationResult.USERSPACE_FLOW
+            case PacketWorkflow.FlowCreated => SimulationResult.FLOW_CREATED
+            case PacketWorkflow.DuplicatedFlow => SimulationResult.DUPE_FLOW
+            case PacketWorkflow.GeneratedPacket =>
+                SimulationResult.GENERATED_PACKET
+            case PacketWorkflow.SendPacket =>
+                SimulationResult.GENERATED_PACKET
+        }
+    }
+
+}
+
---
+++ nsdb/src/main/scala/org/midonet/cluster/flowhistory/FlowRecordEncoding.scala
@@ -0,0 +1,242 @@
+/*
+ * Copyright 2015 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.midonet.cluster.flowhistory
+
+import java.util.{ArrayList, Arrays, Collections, HashSet}
+import java.util.{List, Objects, Random, Set, UUID}
+
+object RuleResult extends Enumeration {
+    type RuleResult = Value
+    val ACCEPT, CONTINUE, DROP, JUMP, REJECT, RETURN = Value
+}
+
+object SimulationResult extends Enumeration {
+    type SimulationResult = Value
+    val NOOP, DROP, TEMP_DROP, SEND_PACKET, ADD_VIRTUAL_WILDCARD_FLOW,
+        STATE_MESSAGE, USERSPACE_FLOW, FLOW_CREATED, DUPE_FLOW,
+        GENERATED_PACKET = Value
+}
+
+object Actions {
+    sealed trait FlowAction
+    case class Output(portNo: Int) extends FlowAction
+    case class PopVlan() extends FlowAction
+    case class PushVlan(tpid: Short, tci: Short) extends FlowAction
+    case class Userspace(uplinkId: Int, userData: Long) extends FlowAction
+    case class Arp(sip: Int, tip: Int, op: Short,
+                   sha: Array[Byte], tha: Array[Byte]) extends FlowAction {
+        override def hashCode(): Int =
+            ((((((sip * 31) + tip) * 31) + op) * 31) +
+                 Arrays.hashCode(sha)) * 31 + Arrays.hashCode(tha)
+
+        override def equals(obj: Any): Boolean = obj match {
+            case other: Arp => Objects.equals(sip, other.sip) &&
+                Objects.equals(tip, other.tip) &&
+                Objects.equals(op, other.op) &&
+                Arrays.equals(sha, other.sha) &&
+                Arrays.equals(tha, other.tha)
+            case _ => false
+        }
+    }
+    case class Ethernet(src: Array[Byte], dst: Array[Byte]) extends FlowAction {
+        override def hashCode(): Int =
+            Arrays.hashCode(src) * 31 + Arrays.hashCode(dst)
+        override def equals(obj: Any): Boolean = obj match {
+            case other: Ethernet => Arrays.equals(src, other.src) &&
+                Arrays.equals(dst, other.dst)
+            case _ => false
+        }
+    }
+    case class EtherType(`type`: Short) extends FlowAction
+    case class IcmpEcho(`type`: Byte, code: Byte, id: Short) extends FlowAction
+    case class IcmpError(`type`: Byte, code: Byte,
+                         data: Array[Byte]) extends FlowAction {
+        override def hashCode(): Int =
+            (((`type` * 31) + code) * 31) + Arrays.hashCode(data)
+
+        override def equals(obj: Any): Boolean = obj match {
+            case other: IcmpError => Objects.equals(`type`, other.`type`) &&
+                Objects.equals(code, other.code) &&
+                Arrays.equals(data, other.data)
+            case _ => false
+        }
+    }
+    case class Icmp(`type`: Byte, code: Byte) extends FlowAction
+    case class IPv4(src: Int, dst: Int, proto: Byte,
+                    tos: Byte, ttl: Byte, frag: Byte) extends FlowAction
+    case class TCP(src: Short, dst: Short) extends FlowAction
+    case class Tunnel(id: Int, ipv4_src: Int, ipv4_dst: Int,
+                      flags: Short, ipv4_tos: Byte,
+                      ipv4_ttl: Byte) extends FlowAction
+    case class UDP(src: Short, dst: Short) extends FlowAction
+    case class VLan(id: Short) extends FlowAction
+}
+
+case class TraversedRule(rule: UUID, result: RuleResult.RuleResult)
+
+case class FlowRecordMatch(inputPortNo: Int, tunnelKey: Long,
+                           tunnelSrc: Int, tunnelDst: Int,
+                           ethSrc: Array[Byte], ethDst: Array[Byte],
+                           etherType: Short, networkSrc: Array[Byte],
+                           networkDst: Array[Byte], networkProto: Byte,
+                           networkTTL: Byte, networkTOS: Byte,
+                           ipFragType: Byte, srcPort: Int,
+                           dstPort: Int, icmpId: Short,
+                           icmpData: Array[Byte],
+                           vlanIds: List[java.lang.Short]) {
+    override def equals(obj: Any): Boolean = obj match {
+        case other: FlowRecordMatch =>
+            Objects.equals(inputPortNo, other.inputPortNo) &&
+            Objects.equals(tunnelKey, other.tunnelKey) &&
+            Objects.equals(tunnelSrc, other.tunnelSrc) &&
+            Objects.equals(tunnelDst, other.tunnelDst) &&
+            Arrays.equals(ethSrc, other.ethSrc) &&
+            Arrays.equals(ethDst, other.ethDst) &&
+            Objects.equals(etherType, other.etherType) &&
+            Arrays.equals(networkSrc, other.networkSrc) &&
+            Arrays.equals(networkDst, other.networkDst) &&
+            Objects.equals(networkProto, other.networkProto) &&
+            Objects.equals(networkTTL, other.networkTTL) &&
+            Objects.equals(networkTOS, other.networkTOS) &&
+            Objects.equals(ipFragType, other.ipFragType) &&
+            Objects.equals(srcPort, other.srcPort) &&
+            Objects.equals(dstPort, other.dstPort) &&
+            Objects.equals(icmpId, other.icmpId) &&
+            Arrays.equals(icmpData, other.icmpData) &&
+            Objects.deepEquals(vlanIds, other.vlanIds)
+        case _ => false
+    }
+
+}
+
+case class FlowRecord(host: UUID, inPort: UUID,
+                      flowMatch: FlowRecordMatch,
+                      cookie: Int,
+                      devices: List[UUID],
+                      rules: List[TraversedRule],
+                      simResult: SimulationResult.SimulationResult,
+                      outPorts: List[UUID],
+                      actions: Set[Actions.FlowAction]) {
+}
+
+
+object FlowRecord {
+    import Actions._
+
+    def random(): FlowRecord = {
+        implicit val r = new Random
+
+        import SimulationResult._
+        val simRes = Arrays.asList(NOOP, DROP, TEMP_DROP, SEND_PACKET,
+                                   ADD_VIRTUAL_WILDCARD_FLOW, STATE_MESSAGE,
+                                   USERSPACE_FLOW, FLOW_CREATED, DUPE_FLOW,
+                                   GENERATED_PACKET)
+        Collections.shuffle(simRes)
+        FlowRecord(UUID.randomUUID, UUID.randomUUID, randomMatch(r),
+                   r.nextInt(), randomUUIDS(r),
+                   randomRules(r), simRes.get(0),
+                   randomUUIDS(r), randomActions(r))
+    }
+
+    private def randomMatch(r: Random): FlowRecordMatch = {
+        val ipBytes = new Array[Byte](4)
+        val macBytes = new Array[Byte](6)
+        r.nextBytes(ipBytes)
+        r.nextBytes(macBytes)
+        val vlans = new ArrayList[java.lang.Short]
+        for (i <- 0 until r.nextInt(5)) {
+            vlans.add(r.nextInt().toShort)
+        }
+
+        FlowRecordMatch(r.nextInt(), r.nextLong(), r.nextInt(), r.nextInt(),
+                        if (r.nextBoolean()) { macBytes } else { null },
+                        if (r.nextBoolean()) { macBytes } else { null },
+                        r.nextInt().toShort,
+                        if (r.nextBoolean()) { ipBytes } else { null },
+                        if (r.nextBoolean()) { ipBytes } else { null },
+                        r.nextInt().toByte, r.nextInt().toByte,
+                        r.nextInt().toByte, r.nextInt().toByte,
+                        r.nextInt(), r.nextInt(),
+                        r.nextInt().toShort,
+                        if (r.nextBoolean()) { macBytes } else { null },
+                        vlans)
+    }
+
+    private def randomActions(r: Random): Set[FlowAction] = {
+        val ipBytes = new Array[Byte](4)
+        val macBytes = new Array[Byte](6)
+        r.nextBytes(ipBytes)
+        r.nextBytes(macBytes)
+        val randActions = Arrays.asList(
+            Output(r.nextInt()), PopVlan(),
+            PushVlan(r.nextInt().toShort, r.nextInt().toShort),
+            Userspace(r.nextInt(), r.nextInt()),
+            Arp(r.nextInt(), r.nextInt(), r.nextInt().toShort,
+                ipBytes, ipBytes),
+            Ethernet(macBytes, macBytes),
+            EtherType(r.nextInt().toShort),
+            IcmpEcho(r.nextInt().toByte, r.nextInt().toByte,
+                     r.nextInt().toShort),
+            IcmpError(r.nextInt().toByte, r.nextInt().toByte,
+                      macBytes),
+            Icmp(r.nextInt().toByte, r.nextInt().toByte),
+            IPv4(r.nextInt(), r.nextInt(), r.nextInt().toByte,
+                 r.nextInt().toByte, r.nextInt().toByte,
+                 r.nextInt().toByte),
+            TCP(r.nextInt().toShort, r.nextInt().toShort),
+            Tunnel(r.nextInt(), r.nextInt(), r.nextInt(),
+                   r.nextInt().toShort, r.nextInt().toByte,
+                   r.nextInt().toByte),
+            UDP(r.nextInt().toShort, r.nextInt().toShort),
+            VLan(r.nextInt().toShort))
+        Collections.shuffle(randActions)
+        val count = r.nextInt(randActions.size)
+        val actions = new HashSet[FlowAction]
+        for (i <- 0 until count) {
+            actions.add(randActions.get(i))
+        }
+        actions
+    }
+
+    private def randomRules(r: Random): List[TraversedRule] = {
+        import RuleResult._
+        val results = Arrays.asList(ACCEPT, CONTINUE, DROP, JUMP, REJECT, RETURN)
+        val count = r.nextInt() % 20
+        if (count < 0) {
+            null
+        } else {
+            val rules = new ArrayList[TraversedRule]
+            for (i <- 0 until count) {
+                rules.add(TraversedRule(UUID.randomUUID,
+                                        results.get(r.nextInt(results.size))))
+            }
+            rules
+        }
+    }
+
+    private def randomUUIDS(r: Random): List[UUID] = {
+        val count = r.nextInt() % 20
+        if (count < 0) {
+            null
+        } else {
+            val devices = new ArrayList[UUID]
+            for (i <- 0 until count) {
+                devices.add(UUID.randomUUID)
+            }
+            devices
+        }
+    }
+}
