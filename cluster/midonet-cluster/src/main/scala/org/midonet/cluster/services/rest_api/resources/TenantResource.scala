64d4d466c7591128aa7608d262abf6da57c6a4c9
Galo Navarro <galo@midokura.com>
2015-05-21 12:27:00 +0200

    Compat API: support tenant resources
    Change-Id: I2e87aca958d972335be414889ae50c08e09e5b20
    Signed-off-by: Galo Navarro <galo@midokura.com>

---
+++ cluster/midonet-cluster/src/main/scala/org/midonet/cluster/services/rest_api/resources/TenantResource.scala
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2015 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.cluster.services.rest_api.resources
+
+import java.util.UUID
+import javax.ws.rs.{Produces, Path, GET}
+import javax.ws.rs.core.UriInfo
+
+import com.google.inject.Inject
+import com.google.inject.servlet.RequestScoped
+
+import org.midonet.cluster.auth.{AuthService, Tenant}
+import org.midonet.cluster.services.MidonetBackend
+import org.midonet.cluster.services.rest_api.MidonetMediaTypes
+
+@RequestScoped
+class TenantResource @Inject()(backend: MidonetBackend, uriInfo: UriInfo,
+                               authService: AuthService)
+    extends MidonetResource(backend, uriInfo) {
+
+    @GET
+    @Path("{id}")
+    @Produces(Array(MidonetMediaTypes.APPLICATION_TENANT_JSON))
+    def get(id: UUID): Tenant = {
+        authService.getTenant(id.toString)
+    }
+
+    @GET
+    @Produces(Array(MidonetMediaTypes.APPLICATION_TENANT_COLLECTION_JSON))
+    def list(): java.util.List[Tenant] = {
+        authService.getTenants(null) // the req is not necessary
+    }
+
+}
