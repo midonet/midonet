f07e80371a02784b8290063f4cc7fbb5b9a7cf0a
Tomohiko KIMURA <tomohiko.kimura@gmail.com>
2015-06-01 11:56:28 +0900

    Translate Load Balancer Pool Delete.
    Translate a Delete operation on Neutron Load Balancer Pool into a
    corresponding Delete operation on MidoNet Pool. Keep the associated
    Load Balancer object even if the Pool to delete is the last Pool
    associated with the Load Balancer, in order to keep the implementation
    simple.
    
    Fix MNA-350
    
    Change-Id: I40076d16e3fc306ec9c90aef5d9e5eba835cf44b

--- cluster/midonet-cluster/src/main/scala/org/midonet/cluster/services/c3po/translators/LoadBalancerPoolTranslator.scala
+++ cluster/midonet-cluster/src/main/scala/org/midonet/cluster/services/c3po/translators/LoadBalancerPoolTranslator.scala
@@ -23,7 +23,7 @@ import org.midonet.cluster.models.Commons.UUID
 import org.midonet.cluster.models.Neutron.NeutronLoadBalancerPool
 import org.midonet.cluster.models.Topology.Pool.PoolHealthMonitorMappingStatus._
 import org.midonet.cluster.models.Topology.{LoadBalancer, Pool}
-import org.midonet.cluster.services.c3po.midonet.{Create, Update}
+import org.midonet.cluster.services.c3po.midonet.{Create, Delete, Update}
 import org.midonet.util.concurrent.toFutureOps
 
 /** Provides a Neutron model translator for NeutronLoadBalancerPool. */
@@ -53,8 +53,10 @@ class LoadBalancerPoolTranslator(protected val storage: ReadOnlyStorage)
         midoOps.toList
     }
 
+    /* The translator will keep around the Load Balancer when the last Pool
+     * is deleted in order to keep the implementation simple. */
     override protected def translateDelete(id: UUID)
-    : MidoOpList = List()
+    : MidoOpList = List(Delete(classOf[Pool], id))
 
     override protected def translateUpdate(nPool: NeutronLoadBalancerPool)
     : MidoOpList = {
