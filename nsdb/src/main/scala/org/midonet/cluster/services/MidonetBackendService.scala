abaf645c3366a7123cd896530e43c039cc86808a
Alex Bikfalvi <alex.bikfalvi@midokura.com>
2015-06-02 18:58:27 +0200

    Rearch: BGP mapper integration
    This patch integrates the BGP mapper with the routing handler.
    
    Ref: MNA-202
    
    Change-Id: I648149083d6e253d17ede87cab0bbd6a4cc3e09a
    Signed-off-by: Alex Bikfalvi <alex.bikfalvi@midokura.com>

--- nsdb/src/main/scala/org/midonet/cluster/services/MidonetBackendService.scala
+++ nsdb/src/main/scala/org/midonet/cluster/services/MidonetBackendService.scala
@@ -34,8 +34,9 @@ import org.midonet.cluster.storage.MidonetBackendConfig
 object MidonetBackend {
 
     final val AliveKey = "alive"
-    final val HostsKey = "hosts"
+    final val BgpKey = "bgp"
     final val HostKey = "host"
+    final val HostsKey = "hosts"
     final val RoutesKey = "routes"
 
 }
@@ -151,6 +152,7 @@ abstract class MidonetBackend extends AbstractService {
                              classOf[FloatingIp], "port_id", CLEAR)
 
         stateStore.registerKey(classOf[Host], AliveKey, SingleFirstWriteWins)
+        stateStore.registerKey(classOf[BgpPeer], BgpKey, SingleLastWriteWins)
         stateStore.registerKey(classOf[Host], HostKey, SingleFirstWriteWins)
         stateStore.registerKey(classOf[Port], HostsKey, Multiple)
         stateStore.registerKey(classOf[Port], RoutesKey, Multiple)
