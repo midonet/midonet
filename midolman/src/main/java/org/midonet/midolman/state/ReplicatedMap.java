dc55b0e3b629491d13ab750a78a664fee4013c1a
Galo Navarro <galo@midokura.com>
2015-06-02 18:54:31 +0200

    VxGW: remove macs when vxlan port removed
    This patch will ensure that whenever a VxLAN port is removed from a
    bridge (this happens when the last VTEP binding is removed) all the
    MAC-Port entries associated to the VxLAN port (that is, all MACs in the
    VTEP) are removed from the MacPort map, as well as the MAC-IP entries
    from the ArpTable.
    
    This adds minor related fixes to the tests, and also a fix handling
    mac-port replacements, as the replicated maps were leaving entries
    behind even when doing a replacement.
    
    It also solves two races:
    - On VxLAN port removal, correctly cleaning up state and notifying the
      VTEP controller.
    - When a mac was removed and added from a MidoNet port in rapid
      succession, a race might result in the put being reordered before the
      removal and seeing a correct value removed.  There is a more correct
      fix that involves adding the endpoint id to the MacLocation also on
      removals, but this is a larger change that will be left to a later
      patch.
    
    Ref.: MN-3556
    Signed-off-by: Galo Navarro <galo@midokura.com>
    
    Change-Id: I7e8c3f189a5d48684c9f1b52c7a775532d6af979

--- midolman/src/main/java/org/midonet/midolman/state/ReplicatedMap.java
+++ midolman/src/main/java/org/midonet/midolman/state/ReplicatedMap.java
@@ -280,10 +280,6 @@ public abstract class ReplicatedMap<K, V> {
 
     /**
      * Provides <pre>Map</pre> with a copy of the local map.
-     *
-     * Synchronized.
-     *
-     * @return
      */
     public Map<K, V> getMap() {
         Map<K, MapValue> snapshot = localMap;
@@ -399,7 +395,6 @@ public abstract class ReplicatedMap<K, V> {
      *
      * Synchronized.
      *
-     * @param key
      * @return true if the key's <pre>MapValue</pre> version is owned here.
      */
     public synchronized boolean isKeyOwner(K key) {
@@ -455,8 +450,6 @@ public abstract class ReplicatedMap<K, V> {
      * The get and verifications are performed synchronously blocking on the
      * map itself. Notifications and deletion from the ZK directory are not.
      *
-     * @param key
-     * @return
      * @throws KeeperException
      * @throws InterruptedException
      */
@@ -479,6 +472,11 @@ public abstract class ReplicatedMap<K, V> {
         MapValue(V value, int version) {
             this.value = value;
             this.version = version;
+        }
+
+        @Override
+        public String toString() {
+            return "{value: " + value + ", version: " + version + "}";
         }
     }
 
