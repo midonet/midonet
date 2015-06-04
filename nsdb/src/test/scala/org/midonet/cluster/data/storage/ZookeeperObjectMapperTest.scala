557f1cc6433e5601d09eba6e66b5228b34eb495c
Alex Bikfalvi <alex.bikfalvi@midokura.com>
2015-06-05 01:38:20 +0200

    ZOOM: Unify storage unit tests
    The patch unifies the tests from the classes:
    - ZookeeperObjectMapperTest
    - ZookeeperObjectMapperTests
    - InMemoryStorageTest
    in a single base trait called StorageTest.
    
    Specific implementation of the Storage trait (e.g. ZOOM and the
    in-memory storage) will extend this trait and setup the actual
    storage instance.
    
    Ref: MNA-397
    
    Change-Id: Ic10674bdc8130a4050cd4ff9b71724ba45d6c031
    Signed-off-by: Alex Bikfalvi <alex.bikfalvi@midokura.com>

---
+++ nsdb/src/test/scala/org/midonet/cluster/data/storage/ZookeeperObjectMapperTest.scala
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2014 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.midonet.cluster.data.storage
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+import rx.observers.TestObserver
+
+import org.midonet.cluster.data.storage.StorageTestClasses._
+import org.midonet.cluster.util.{ClassAwaitableObserver, CuratorTestFramework, PathCacheDisconnectedException}
+
+@RunWith(classOf[JUnitRunner])
+class ZookeeperObjectMapperTest extends StorageTest with CuratorTestFramework {
+
+    import StorageTest._
+
+    protected override def setup(): Unit = {
+        storage = new ZookeeperObjectMapper(ZK_ROOT, curator)
+        assert = () => {}
+        initAndBuildStorage(storage)
+    }
+
+    protected override def createStorage = {
+        new ZookeeperObjectMapper(ZK_ROOT, curator)
+    }
+
+    private case class OwnerSnapshot(id: String, version: Int) {
+        override def equals(obj: Any) = obj match {
+            case os: OwnerSnapshot => id.equals(os.id)
+            case _ => false
+        }
+        override def hashCode: Int = id.hashCode
+    }
+
+    feature("Test subscribe") {
+        scenario("Test subscribe with GC") {
+            val bridge = createPojoBridge()
+            val obs = new TestObserver[PojoBridge]
+            val sub = storage.observable(classOf[PojoBridge], bridge.id)
+                .subscribe(obs)
+
+            val zoom = storage.asInstanceOf[ZookeeperObjectMapper]
+            zoom.subscriptionCount(classOf[PojoBridge], bridge.id) shouldBe Option(1)
+            sub.unsubscribe()
+            zoom.subscriptionCount(classOf[PojoBridge], bridge.id) shouldBe None
+        }
+
+        scenario("Test subscribe all with GC") {
+            val obs = new ClassAwaitableObserver[PojoBridge](0)
+            val sub = storage.observable(classOf[PojoBridge]).subscribe(obs)
+
+            val zoom = storage.asInstanceOf[ZookeeperObjectMapper]
+            zoom.subscriptionCount(classOf[PojoBridge]) should equal (Option(1))
+            sub.unsubscribe()
+            zoom.subscriptionCount(classOf[PojoBridge]) should equal (None)
+
+            obs.getOnCompletedEvents should have size 0
+            obs.getOnErrorEvents should have size 1
+            assert(obs.getOnErrorEvents.get(0)
+                       .isInstanceOf[PathCacheDisconnectedException])
+        }
+    }
+
+    feature("Test Zookeeper") {
+        scenario("Test get path") {
+            val zoom = storage.asInstanceOf[ZookeeperObjectMapper]
+            zoom.getClassPath(classOf[PojoBridge]) shouldBe s"$ZK_ROOT/1/PojoBridge"
+        }
+    }
+
+}
