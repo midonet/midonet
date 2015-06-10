5e198bb1c23b756c6161784b8873ab1b7cb7089c
Duarte Nunes <duarte@midokura.com>
2015-06-10 19:39:42 +0200

    Agent: Fixes to dealing with an existing DP port
    When creating a DP port, we handle the case where that port already
    exists. One reason it may exist can be because we timed out a previous
    creation attempt, due to, for example, a long GC; netlink timeouts
    don't really exist, so the operation still completed.
    
    This patch adds an exception - address already in use - that tells us the port
    exists in case of a Vxlan tunnel port.
    
    It also changes the recovery to delete and re-create the port, since
    simply setting the upcall pid fails with operation not supported for
    Vxlan tunnel ports.
    
    Change-Id: If834f0bc4df235a8b726c14f41b3670621967d4a
    Signed-off-by: Duarte Nunes <duarte@midokura.com>

--- midolman/src/main/scala/org/midonet/midolman/datapath/FlowProcessor.scala
+++ midolman/src/main/scala/org/midonet/midolman/datapath/FlowProcessor.scala
@@ -28,7 +28,7 @@ import org.midonet.midolman.datapath.DisruptorDatapathChannel._
 import org.midonet.netlink._
 import org.midonet.netlink.exceptions.NetlinkException
 import org.midonet.odp.{FlowMatch, OvsNetlinkFamilies, OvsProtocol}
-import org.midonet.Util
+import org.midonet.{ErrorCode, Util}
 import org.midonet.util.concurrent.{NanoClock, Backchannel}
 
 object FlowProcessor {
@@ -142,10 +142,10 @@ class FlowProcessor(families: OvsNetlinkFamilies,
             log.warn("Unexpected reply; probably the late answer of a request that timed out")
 
         override def onError(e: Throwable): Unit = e match {
-            case ne: NetlinkException if ne.getErrorCodeEnum == NetlinkException.ErrorCode.EEXIST =>
+            case ne: NetlinkException if ne.getErrorCodeEnum == ErrorCode.EEXIST =>
                 log.debug("Tried to add duplicate DP flow")
             case ne: NetlinkException =>
-                log.warn(s"Unexpected error with code ${ne.getErrorCodeEnum}; " +
+                log.warn(s"Unexpected error ${ne.getErrorCodeEnum}; " +
                          "probably the late answer of a request that timed out")
             }
 
--- netlink/src/main/java/org/midonet/netlink/NetlinkRequest.java
+++ netlink/src/main/java/org/midonet/netlink/NetlinkRequest.java
@@ -24,6 +24,7 @@ import java.util.concurrent.TimeUnit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import org.midonet.ErrorCode;
 import org.midonet.netlink.exceptions.NetlinkException;
 
 /** Class used by AbstractNetlinkRequest to manage reply handlers and user given
@@ -121,9 +122,8 @@ public abstract class NetlinkRequest implements Runnable {
     }
 
     public Runnable expired() {
-        NetlinkException.ErrorCode er = NetlinkException.ErrorCode.ETIMEOUT;
         String msg = "request #" + seq + " timeout";
-        return failed(new NetlinkException(er, msg));
+        return failed(new NetlinkException(ErrorCode.ETIMEOUT, msg));
     }
 
     protected void changeState(State nextState, Object data) {
