e148b765ecdfda5f896dd35e7fcd5386c48fe79f
Galo Navarro <galo@midokura.com>
2015-06-19 19:05:31 +0200

    VxGW: apply backpressure on network list
    This patch fixes a bug whereby the service would crash when initializing
    if the virtual topology contained more than 128 networks.  The
    underlying cause was a buffer size in RxJava's observeOn that is used to
    deliver work units (networks) to the thread examining them and deciding
    whether the network participates in a VxLAN Gateway.  This buffer has a
    size of 128, and triggers backpressure on the parent observable when
    full.  This would happen mainly during initialization, when all existing
    networks are notified at once.  Unfortunately, the type of Observable we
    were using (a PublishSubject) does not support backpressure, so this
    process resulted in a failure.
    
    This patch fixes this by applying onBackpressureBuffer on the network
    observable.  This buffer is sized to a large value that can be
    controlled via mn-conf, and is sufficient for our current scalability
    targets.  Upon overflow, the service will alert the operator by
    displaying an error message in logs.
    
    The patch also includes two counters exposed through the cluster's JMX
    bean that allow monitoring the total number of networks watched and
    those that are part of a VxLAN Gateway.
    
    Fixes:  MNA-554
    Change-Id: Ib796e69d444adcf67a91d4ef1dc2e2769cb8bcfd
    Signed-off-by: Galo Navarro <galo@midokura.com>

---
+++ cluster/midonet-cluster/src/test/scala/org/midonet/cluster/services/vxgw/VxlanGatewayServiceTest.scala
@@ -0,0 +1,141 @@
+/*
+ * Copyright 2015 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.cluster.services.vxgw
+
+import java.util.concurrent.{CountDownLatch, TimeUnit}
+import java.util.{Random, UUID}
+
+import com.codahale.metrics
+import com.codahale.metrics.MetricRegistry
+import com.google.common.util.concurrent.Service.State
+import com.google.inject.{Guice, Injector}
+import com.typesafe.config.ConfigFactory
+import org.junit.Assert
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
+import org.slf4j.LoggerFactory
+
+import org.midonet.cluster.data.Bridge
+import org.midonet.cluster.util.CuratorTestFramework
+import org.midonet.cluster.{ClusterTestUtils, ClusterConfig, ClusterNode, DataClient}
+import org.midonet.conf.MidoTestConfigurator
+import org.midonet.midolman.host.state.HostZkManager
+import org.midonet.midolman.state.{Directory, ZookeeperConnectionWatcher}
+import org.midonet.util.MidonetEventually
+
+@RunWith(classOf[JUnitRunner])
+class VxlanGatewayServiceTest extends FlatSpec with Matchers
+                                               with BeforeAndAfter
+                                               with VxlanGatewayTest
+                                               with MidonetEventually
+                                               with CuratorTestFramework {
+
+    val log = LoggerFactory.getLogger(classOf[VxlanGatewayManagerTest])
+
+    override var dataClient: DataClient = _
+    override var hostManager: HostZkManager = _
+
+    var injector: Injector = _
+    var zkConnWatcher: ZookeeperConnectionWatcher = _
+    var mgrClosedLatch: CountDownLatch = _
+    var tzState: TunnelZoneStatePublisher = _
+    var hostState: HostStatePublisher = _
+    var nodeId: UUID = _
+    var metrics: MetricRegistry = _
+
+    protected override def setup(): Unit = {
+        injector = Guice.createInjector(ClusterTestUtils.modules())
+        val directory = injector.getInstance(classOf[Directory])
+        ClusterTestUtils.setupZkTestDirectory(directory)
+
+        dataClient = injector.getInstance(classOf[DataClient])
+        Assert.assertNotNull(dataClient)
+
+        hostState = new HostStatePublisher(dataClient, zkConnWatcher)
+        tzState = new TunnelZoneStatePublisher(dataClient, zkConnWatcher,
+                                               hostState, new Random)
+
+        hostManager = injector.getInstance(classOf[HostZkManager])
+        Assert.assertNotNull(hostManager)
+
+        mgrClosedLatch = new CountDownLatch(1)
+        nodeId = UUID.randomUUID
+
+        metrics = new MetricRegistry
+
+        curator.create().creatingParentsIfNeeded().forPath("/midonet/vxgw")
+    }
+
+    protected override def teardown(): Unit = {
+        curator.delete().deletingChildrenIfNeeded().forPath("/midonet/vxgw")
+    }
+
+    private def makeBridges(n: Int): Unit = 0 until n map { i =>
+        dataClient.bridgesCreate(new Bridge())
+    }
+
+    private def vxgwService(conf: ClusterConfig): VxlanGatewayService = {
+        new VxlanGatewayService(new ClusterNode.Context(nodeId), dataClient,
+                                zkConnWatcher, null, curator, metrics, conf)
+    }
+
+    // Test below verifids that initialization with a large number of bridges
+    // doesn't saturate the rx streams dealing with initialization.
+
+    "The VxGW service" should "initialize correctly even with many bridges" in {
+        val conf = new ClusterConfig(MidoTestConfigurator.forClusters())
+        val vx = vxgwService(conf)
+
+        makeBridges(conf.vxgw.networkBufferSize )
+
+        vx.startAsync()
+        vx.awaitRunning(100, TimeUnit.SECONDS)
+
+        eventually { vx.numNetworks shouldBe conf.vxgw.networkBufferSize }
+
+        makeBridges(1)
+
+        eventually { vx.numNetworks shouldBe conf.vxgw.networkBufferSize + 1 }
+
+        vx.stopAsync()
+        vx.awaitTerminated(100, TimeUnit.SECONDS)
+    }
+
+    // Test below verifids that initialization with a large number of bridges
+    // over the configured buffer DOES saturate the service and alerts the
+    // operator.
+
+    "The VxGW service" should "break if the buffer is not big enough" in {
+        val conf = new ClusterConfig(MidoTestConfigurator.forClusters())
+        val vx = vxgwService(conf)
+
+        makeBridges(conf.vxgw.networkBufferSize * 2) // too many bridges
+
+        vx.startAsync()
+
+        eventually {
+            vx.state() shouldBe State.FAILED
+            (vx.numNetworks <= conf.vxgw.networkBufferSize) shouldBe true
+        }
+    }
+
+    "The VxGW service" should "default to 10k virtual networks" in {
+        val conf = MidoTestConfigurator.forClusters()
+        conf.getInt("cluster.vxgw.net_buffer_size") shouldBe 10000
+    }
+}
