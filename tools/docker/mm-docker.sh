f6fed6b15a38a4042380530124c831bb11d53f52
Taku Fukushima <tfukushima@midokura.com>
2015-06-25 03:44:44 +0900

    Fix mm-docker.sh
    This patch fixes bugs of argument processings in mm-docker.sh injected
    when midonet datapath argument became optional. This patch also fixes
    an issue of the command availability check and adds the capability to
    set the default route for containers.
    
    Change-Id: I68daa46f5d63b4f1f94ddb9a95e34beeeb25b5c2
    Signed-off-by: Taku Fukushima <tfukushima@midokura.com>

--- tools/docker/mm-docker.sh
+++ tools/docker/mm-docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright 2014 Midokura SARL
+# Copyright 2015 Midokura SARL
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,8 +15,8 @@
 # limitations under the License.
 
 check_command_availability() {
-    if !(("$1" "${2:-\"--version\"}") > /dev/null 2>&1); then
-        echo >&2 "$COMMAND: $1 is required to run this script"
+    if ! $1 ${2:---version} > /dev/null 2>&1; then
+        echo >&2 "${COMMAND}: $1 is required to run this script"
         exit 1
     fi
 }
@@ -29,30 +29,30 @@ mm_dpctl () {
 #   https://docs.docker.com/articles/networking/#how-docker-networks-a-container
 create_netns_symblink() {
     mkdir -p /var/run/netns
-    if [ ! -e /var/run/netns/"$PID" ]; then
-        ln -s /proc/"$PID"/ns/net /var/run/netns/"$PID"
+    if [[ ! -e /var/run/netns/"${PID}" ]]; then
+        ln -s /proc/"${PID}"/ns/net /var/run/netns/"${PID}"
         trap 'delete_netns_symlink' 0
         for signal in 1 2 3 13 14 15; do
-            trap "delete_netns_symlink; trap - $signal; kill -$signal $$" $signal
+            trap "delete_netns_symlink; trap - ${signal}; kill -${signal} $$" $signal
         done
     fi
 }
 
 delete_netns_symlink() {
-    rm -f /var/run/netns/"$PID"
+    rm -f /var/run/netns/"${PID}"
 }
 
 add_if() {
-    if [ "$#" -lt 2 ]; then
+    if [[ "$#" < "2" ]]; then
         echo >&2 "Too few arguments for add-if."
         help
         exit 1
     fi
 
-    if [ "$#" -eq 3 ]; then
+    if [[ "$#" = "2" || "$#" = "3" ]]; then
         DATAPATH="midonet"
     fi
-    if [ "$#" -eq 4 ]; then
+    if [[ "$#" > "3" ]]; then
         DATAPATH="$1"
         shift
     fi
@@ -60,29 +60,31 @@ add_if() {
     INTERFACE="$1"
     CONTAINER="$2"
     ADDRESS="$3"
+    DEFAULT_GATEWAY="$4"
 
-    if !((mm_dpctl --list-dps | grep "$DATAPATH" > /dev/nul 2>&1) ||
-            (mm_dpctl --add-dp "$DATAPATH")); then
-        echo >&2 "Failed to create datapathe $DATAPATHE"
-        exit 1
+    if [[ "$(mm_dpctl --list-dps | grep -- "${DATAPATH}")" = "" ]]; then
+        if ! mm_dpctl --add-dp "${DATAPATH}" ; then
+            echo "Failed to create datapath: ${DATAPATH}"
+            exit 1
+        fi
     fi
 
-    PID=`docker inspect -f '{{.State.Pid}}' "$CONTAINER"`
-    if [ "$PID" = "" ]; then
-        echo >&2 "Failed to get the PID of the container $CONTAINER"
+    PID="$(docker inspect -f '{{.State.Pid}}' "${CONTAINER}")"
+    if [[ "${PID}" == "" ]]; then
+        echo >&2 "Failed to get the PID of the container ${CONTAINER}"
         exit 1
     fi
 
     create_netns_symblink
 
     # Create a veth pair.
-    ID=`uuidgen | sed 's/-//g'`
-    IFNAME="${CONTAINER:0:8}${INTERFACE:0:5}"
-    sudo ip link add "${IFNAME}" type veth peer name "${IFNAME}_c"
+    ID="$(uuidgen | sed 's/-//g')"
+    IFNAME="${CONTAINER:0:8}-${INTERFACE:0:5}"
+    ip link add "${IFNAME}" type veth peer name "${IFNAME}_c"
 
     # Add one end of veth to the datapath.
-    if !((mm_dpctl --add-if "${IFNAME}" "$DATAPATH") > /dev/null 2>&1); then
-        echo >&2 "Failed to add \"${IFNAME}\" interface to the datapath $DATAPATH"
+    if ! mm_dpctl --add-if "${IFNAME}" "${DATAPATH}" 2>&1 > /dev/null; then
+        echo >&2 "Failed to add interface ${IFNAME} to the datapath ${DATAPATH}"
         ip link delete "${IFNAME}"
         delete_netns_symlink
         exit 1
@@ -91,27 +93,32 @@ add_if() {
     ip link set "${IFNAME}" up
 
     # Move "{IFNAME}_c" inside the container and changes its name.
-    ip link set "${IFNAME}_c" netns "$PID"
-    ip netns exec "$PID" ip link set dev "${IFNAME}_c" name "${INTERFACE}"
-    ip netns exec "$PID" ip link set "$INTERFACE" up
+    ip link set "${IFNAME}_c" netns "${PID}"
+    ip netns exec "${PID}" ip link set dev "${IFNAME}_c" name "${INTERFACE}"
+    ip netns exec "${PID}" ip link set "${INTERFACE}" up
 
-    if [ -n "$ADDRESS" ]; then
-        ip netns exec $PID ip addr add "$ADDRESS" dev "${INTERFACE}"
+    if [[ "${ADDRESS}" != "" ]]; then
+        ip netns exec "${PID}" ip addr add "${ADDRESS}" dev "${INTERFACE}"
+    fi
+
+    if [[ "${DEFAULT_GATEWAY}" != "" ]]; then
+        ip netns exec "${PID}" \
+            ip route add default via "${DEFAULT_GATEWAY}" dev "${INTERFACE}"
     fi
 
     delete_netns_symlink
 }
 
 del_if() {
-    if [ "$#" -lt 2 ]; then
+    if [[ "$#" < "2" ]]; then
         help
         exit 1
     fi
 
-    if [ "$#" -eq 3]; then
+    if [[ "$#" = "2" ]]; then
         DATAPATH="midonet"
     fi
-    if [ "$#" -eq 4 ]; then
+    if [[ "$#" = "3" ]]; then
         DATAPATH="$1"
         shift
     fi
@@ -119,45 +126,45 @@ del_if() {
     INTERFACE="$1"
     CONTAINER="$2"
 
-    IFNAME="${CONTAINER:0:8}${INTERFACE:0:5}"
-    PORT=`mm_dpctl --show-dp midonet \
-            | grep -P "Port \#\d+ \"$IFNAME\"" \
-            | awk -F '"' '{print $2}'`
-    if [ "$PORT" = "" ]; then
-        echo >&2 "Failed to find any attached port in $DATAPATH" \
-            "for CONTAINER=$CONTAINER and INTERFACE=$INTERFACE"
+    IFNAME="${CONTAINER:0:8}-${INTERFACE:0:5}"
+    PORT="$(mm_dpctl --show-dp midonet \
+            | grep -P "Port \#\d+ \"${IFNAME}\"" \
+            | awk -F '"' '{print $2}')"
+    if [[ "${PORT}" = "" ]]; then
+        echo >&2 "Failed to find any attached port in ${DATAPATH}" \
+            "for CONTAINER=${CONTAINER} and INTERFACE=${INTERFACE}"
         exit 1
     fi
 
-    mm_dpctl --delete-if "$PORT" "$DATAPATH"
+    mm_dpctl --delete-if "${PORT}" "${DATAPATH}"
 
-    ip link delete "$PORT"
+    ip link delete "${PORT}"
 }
 
 del_ifs() {
-    if [ "$#" -lt 1 ]; then
+    if [[ "$#" < "1" ]]; then
         help
         exit 1
     fi
 
-    if [ "$#" -eq 3 ]; then
+    if [[ "$#" = "1" ]]; then
         DATAPATH="midonet"
     fi
-    if [ "$#" -eq 2 ]; then
+    if [[ "$#" = "2" ]]; then
         DATAPATH="$1"
         shift
     fi
 
     CONTAINER="$1"
 
-    PORTS=`mm_dpctl --show-dp midonet | grep -P "Port \#\d+" | awk -F '"' '{print $2}'`
-    if [ -z "$PORTS" ]; then
+    PORTS="$(mm_dpctl --show-dp midonet | grep -P "Port \#\d+" | awk -F '"' '{print $2}')"
+    if [[ "${PORTS}" = "" ]]; then
         exit 0
     fi
 
     for PORT in $PORTS; do
-        mm_dpctl --delete-if "$PORT" "$DATAPATH"
-        ip link delete "$PORT"
+        mm_dpctl --delete-if "${PORT}" "${DATAPATH}"
+        ip link delete "${PORT}"
     done
 }
 
@@ -167,16 +174,19 @@ ${COMMAND}: Integrates the network interfaces into MidoNet.
 usage: ${COMMAND} COMMAND
 
 Commands:
-  add-if [DATAPATH] INTERFACE CONTAINER [IPv4_ADDRESS/NET_MASK]
+  add-if [DATAPATH] INTERFACE CONTAINER [IPv4_ADDRESS/NET_MASK] [DEFAULT_GATEWAY]
           Adds INTERFACE inside CONTAINER and connects it as a port in the given
           DATAPATH or "midonet" datapath if it's not provided. It also adds the
           IPv4 address to the interface if it's given with "ip addr add"
-          command.
+          command. If DEFAULT_GATEWAY is given, it sets the default gateway with
+          the given IP address and [DATAPATH] MUST BE given along with it in the
+          first argument for this subcommand.
 
           Examples:
             ${COMMAND} add-if dp0 veth8a72ecc1-5a 5506de2b643b 192.168.100.1
             ${COMMAND} add-if veth8a72ecc1-5a 5506de2b643b 192.168.100.1/32
             ${COMMAND} add-if veth8a72ecc1-5a 5506de2b643b
+            ${COMMAND} add-if dp0 veth8a72ecc1-5a 5506de2b643b 192.168.1.42/24 192.168.1.1
 
   del-if [DATAPATH] INTERFACE CONTAINER
           Deletes INTERFACE inside CONTAINER and removes its connection to the
@@ -206,7 +216,7 @@ check_command_availability ip netns
 check_command_availability docker
 check_command_availability uuidgen
 
-if [ "$#" -eq 0 ]; then
+if [[ "$#" == "0" ]]; then
     help
     exit 0
 fi
