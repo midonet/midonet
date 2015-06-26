96f169cd613fb0d77499671c96d716423346f3fa
Duarte Nunes <duarte@midokura.com>
2015-06-26 12:48:58 +0200

    MDTS: Use 2 simulation threads by default
    Some bugs are hidden by the fact we're only using 1 simulation thread,
    so we enable 2 threads in an attempt to catch more of these bugs.
    
    Change-Id: If07684455236c4c75786a4e2072ea7df06376da5
    Signed-off-by: Duarte Nunes <duarte@midokura.com>

--- tests/mmm/scripts/mn-conf/mn-conf.sh
+++ tests/mmm/scripts/mn-conf/mn-conf.sh
@@ -3,11 +3,7 @@
 # starting the zookeeper cluster and is supposed to contain
 # mn-conf commands to provide global configuration to the
 # midonet services (cluster, agent, etc.)
-# e.g.:
-# mn-conf set -t default <<EOF
-# zookeeper.zookeeper_hosts="10.0.0.2:2181,10.0.0.3:2181,10.0.0.4:2181"
-# cassandra.replication_factor=3
-# cassandra.cluster=midonet
-# agent.logger.root=DEBUG
-# ...
-# EOF
+
+mn-conf set -t default <<EOF
+agent.midolman.simulation_threads=2
+EOF
