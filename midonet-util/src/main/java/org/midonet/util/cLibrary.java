midonet/midonet

commit bc3ef1d19310e8bcba7e830b148017a56bebefb2

Upgrade JNA to 4.1.0
This patch upgrades JNA from 3.3.0 to 4.1.0 because 3.3.0 is too old
and some dependency hell occurred because of JNA's version.

https://discuss.gradle.org/t/build-failure-when-4-1-0-of-net-java-dev-jna-jna-is-selected-in-place-of-3-3-0-due-to-change-in-artifacts-with-classifiers/9377

Change-Id: I8ed2059ecb2fb02472eaf0a9563705a376d2ac0b
Signed-off-by: Taku Fukushima <tfukushima@midokura.com>

midonet-util/src/main/java/org/midonet/util/cLibrary.java
@@ -16,6 +16,8 @@
 package org.midonet.util;
 
 import java.nio.ByteBuffer;
+import java.util.Arrays;
+import java.util.List;
 
 import com.sun.jna.LastErrorException;
 import com.sun.jna.Library;
@@ -36,10 +38,20 @@ public interface cLibrary extends Library {
         public short nl_pad;
         public int nl_pid;
         public int nl_groups;
+
+        @Override
+        protected List getFieldOrder() {
+            return Arrays.asList("nl_family", "nl_pad", "nl_pid", "nl_groups");
+        }
     }
 
     public static class UnixPath extends Structure {
         public byte[] chars = new byte[108];
+
+        @Override
+        protected List getFieldOrder() {
+            return Arrays.asList("chars");
+        }
     }
 
     public static class UnixPathByVal extends UnixPath implements ByValue {}
@@ -48,6 +60,11 @@ public interface cLibrary extends Library {
     public static class UnixDomainSockAddress extends Structure {
         public short sun_family;
         public UnixPathByVal sun_path;
+
+        @Override
+        protected List getFieldOrder() {
+            return Arrays.asList("sun_family", "sun_path");
+        }
     }
 
     public static final int AF_UNIX = 1;

build.gradle
@@ -58,7 +58,7 @@ project.ext {
         findbugs: "com.google.code.findbugs:jsr305:2.0.0",
         logback: "ch.qos.logback:logback-classic:1.1.2",
         logback_logstash: "net.logstash.logback:logstash-logback-encoder:3.5",
-        jna: "net.java.dev.jna:jna:3.3.0",
+        jna: ["net.java.dev.jna:jna:4.1.0", "net.java.dev.jna:jna-platform:4.1.0"],
         jsch: 'com.jcraft:jsch:0.1.45',
         collections4: 'org.apache.commons:commons-collections4:4.0',
         commons_codec: "commons-codec:commons-codec:1.2",
@@ -142,7 +142,9 @@ project.ext {
             "com.sun.jersey.jersey-test-framework:jersey-test-framework-core:${versions.jersey}") {
                 exclude group: 'junit', module: 'junit'},
 
-        mysql_jdbc: "org.mariadb.jdbc:mariadb-java-client:1.1.8",
+        mysql_jdbc: dependencies.create("org.mariadb.jdbc:mariadb-java-client:1.1.8") {
+            exclude group: 'net.java.dev.jna'
+        },
         sqlite: "org.xerial:sqlite-jdbc:3.8.7",
 
         vmware_midoapi: [ "com.vmware:vijava:5.1", "dom4j:dom4j:1.6.1" ],

odp/src/main/java/org/midonet/odp/util/Tap.java
@@ -16,14 +16,14 @@
 package org.midonet.odp.util;
 
 import java.util.Arrays;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.List;
 
 import com.sun.jna.Library;
 import com.sun.jna.Native;
 import com.sun.jna.Structure;
 import com.sun.jna.Union;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Tap {
 
@@ -496,6 +496,11 @@ public class Tap {
         public InterfaceReqName ifr_ifrn;
         public InterfaceReqParam ifr_ifru;
 
+        @Override
+        protected List getFieldOrder() {
+            return Arrays.asList("ifr_ifrn", "ifr_ifru");
+        }
+
         public static class InterfaceReqByRef extends InterfaceReq implements Structure.ByReference {
         }
 
@@ -525,6 +530,11 @@ public class Tap {
         public static class SockAddr extends Structure {
             public short sa_family_t;
             public byte[] sa_data = new byte[14];
+
+            @Override
+            protected List getFieldOrder() {
+                return Arrays.asList("sa_family_t", "sa_data");
+            }
         }
     }
 

commit e79412a10219382aab2525ccf7703bfe16a65656

Agent: Fix regression introduced by JNA upgrade
This patch fixes a regression introduced in
bc3ef1d19310e8bcba7e830b148017a56bebefb2 due to
https://java.net/jira/browse/JNA-185.

We now need to correctly set ByteBuffer position when making JNA
calls.

Change-Id: Id3700b471d1e09c83b76e88a2809c1dcde4c1866
Signed-off-by: Duarte Nunes <duarte@midokura.com>

midonet-util/src/main/java/org/midonet/util/cLibrary.java
@@ -130,6 +130,7 @@ public interface cLibrary extends Library {
     int getsockname(int fd, UnixDomainSockAddress addrSockAddress, IntByReference size);
 
     int setsockopt(int fd, int level, int optname, ByteBuffer buf, int buflen);
+    int getsockopt(int fd, int level, int optname, ByteBuffer buf, ByteBuffer buflen);
 
     int send(int fd, ByteBuffer buf, int len, int flags);
 

netlink/src/main/java/org/midonet/netlink/NetlinkChannel.java
@@ -93,17 +93,30 @@ public class NetlinkChannel extends UnixChannel<Netlink.Address> {
         fd = IOUtil.newFD(socket);
         fdVal = IOUtil.fdVal(fd);
 
-        ByteBuffer sobuf = BytesUtil.instance.allocate(4);
+        ByteBuffer len = BytesUtil.instance.allocateDirect(4);
+        len.putInt(4);
+
+        ByteBuffer sobuf = BytesUtil.instance.allocateDirect(4);
         sobuf.putInt(RCVBUF_SIZE);
+        sobuf.flip();
 
         int sockoptret = cLibrary.lib.setsockopt(
             fdVal, cLibrary.SOL_SOCKET, cLibrary.SO_RCVBUFFORCE, sobuf, 4);
         if (sockoptret != 0) {
             log.error("SETSOCKOPT failed: {}",
-                cLibrary.lib.strerror(Native.getLastError()));
+                      cLibrary.lib.strerror(Native.getLastError()));
         } else {
-            log.debug("Successfully set netlink channel RCVBUFF size to {}",
-                      RCVBUF_SIZE);
+            sobuf.clear();
+            len.clear();
+            sockoptret = cLibrary.lib.getsockopt(
+                fdVal, cLibrary.SOL_SOCKET, cLibrary.SO_RCVBUF, sobuf, len);
+            if (sockoptret != 0) {
+                log.debug("GETSOCKOPT NETLINK_BROADCAST_ERROR failed: {}",
+                          cLibrary.lib.strerror(Native.getLastError()));
+            }
+            sobuf.clear();
+            log.debug("Successfully set netlink channel RCVBUFF size to {} (got {})",
+                      RCVBUF_SIZE, sobuf.getInt());
         }
 
         /* Set NETLINK_BROADCAST_ERROR to 1.
@@ -115,15 +128,26 @@ public class NetlinkChannel extends UnixChannel<Netlink.Address> {
          * netlink_broadcast() to send to userspace, so this will make sure it
          * gets the errors (and ignore them at will).
          */
-        ByteBuffer sobuf2 = BytesUtil.instance.allocate(4);
-        sobuf2.putInt(1);
+        sobuf.clear();
+        sobuf.putInt(1);
+        sobuf.flip();
         sockoptret = cLibrary.lib.setsockopt(
-            fdVal, cLibrary.SOL_NETLINK, cLibrary.NETLINK_BROADCAST_ERROR, sobuf2, 4);
+            fdVal, cLibrary.SOL_NETLINK, cLibrary.NETLINK_BROADCAST_ERROR, sobuf, 4);
         if (sockoptret != 0) {
             log.error("SETSOCKOPT NETLINK_BROADCAST_ERROR failed: {}",
                     cLibrary.lib.strerror(Native.getLastError()));
         } else {
-            log.debug("SETSOCKOPT success: NETLINK_BROADCAST_ERROR");
+            sobuf.clear();
+            len.clear();
+            sockoptret = cLibrary.lib.getsockopt(
+                fdVal, cLibrary.SOL_NETLINK, cLibrary.NETLINK_BROADCAST_ERROR, sobuf, len);
+            if (sockoptret != 0) {
+                log.debug("GETSOCKOPT NETLINK_BROADCAST_ERROR failed: {}",
+                          cLibrary.lib.strerror(Native.getLastError()));
+            }
+            sobuf.clear();
+            log.debug("SETSOCKOPT success: NETLINK_BROADCAST_ERROR (got {})",
+                      sobuf.get());
         }
 
         /* Set NETLINK_NO_ENOBUFS to 1.
@@ -140,13 +164,26 @@ public class NetlinkChannel extends UnixChannel<Netlink.Address> {
          * will wait for userspace to deplete the read buffer before writing
          * again to it, causing spikes in throughput.
          */
+        sobuf.clear();
+        sobuf.putInt(1);
+        sobuf.flip();
         sockoptret = cLibrary.lib.setsockopt(
-                fdVal, cLibrary.SOL_NETLINK, cLibrary.NETLINK_NO_ENOBUFS, sobuf2, 4);
+                fdVal, cLibrary.SOL_NETLINK, cLibrary.NETLINK_NO_ENOBUFS, sobuf, 4);
         if (sockoptret != 0) {
             log.error("SETSOCKOPT NETLINK_NO_ENOBUFS failed: {}",
                     cLibrary.lib.strerror(Native.getLastError()));
         } else {
-            log.debug("SETSOCKOPT success: NETLINK_NO_ENOBUFS");
+            sobuf.clear();
+            len.clear();
+            sockoptret = cLibrary.lib.getsockopt(
+                fdVal, cLibrary.SOL_NETLINK, cLibrary.NETLINK_NO_ENOBUFS, sobuf, len);
+            if (sockoptret != 0) {
+                log.debug("GETSOCKOPT NETLINK_NO_ENOBUFS failed: {}",
+                          cLibrary.lib.strerror(Native.getLastError()));
+            }
+            sobuf.clear();
+            log.debug("SETSOCKOPT success: NETLINK_NO_ENOBUFS (got {})",
+                      sobuf.getInt());
         }
     }
 