52e1634e2bbc5e47b9635eb5925ae1fb053a0d79
Xavi León <xavi.leon@midokura.com>
2015-07-07 20:25:50 +0200

    A service discovery framework
    This patch introduces a very basic framework for service discovery. It
    allows a service to register its availability to offer a given
    service, and a client to obtain the list of registered services. It's
    up to the client to decide which one to pick at any given time.
    More sophisticated policies are left for following patches.
    
    Ref: ME-431 MI-653
    
    Change-Id: I024cc9a9f6f102d77a0b20beabb6f33aee5381da
    Signed-off-by: Ernest Artiaga <ernest@midokura.com>
    Signed-off-by: Xavi León <xavi.leon@midokura.com>

---
+++ nsdb/src/main/scala/org/midonet/cluster/services/discovery/MidonetDiscovery.scala
@@ -0,0 +1,276 @@
+/*
+ * Copyright 2016 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.cluster.services.discovery
+
+import java.net.URI
+import java.util.concurrent.ExecutorService
+
+import scala.collection.JavaConverters._
+import scala.reflect.runtime.universe._
+import scala.util.control.NonFatal
+
+import com.google.common.annotations.VisibleForTesting
+import com.google.inject.Inject
+import com.typesafe.scalalogging.Logger
+
+import org.apache.curator.framework.CuratorFramework
+import org.apache.curator.framework.state.ConnectionState
+import org.apache.curator.x.discovery._
+import org.apache.curator.x.discovery.details.ServiceCacheListener
+import org.slf4j.LoggerFactory
+
+import rx.subjects.BehaviorSubject
+
+import org.midonet.cluster.services.discovery.MidonetDiscovery._
+import org.midonet.cluster.storage.MidonetBackendConfig
+import org.midonet.util.functors.makeRunnable
+
+/**
+  * A basic service discovery mechanism
+  *
+  * In order to use the service discovery mechanism, both for servers and
+  * clients, a MidonetDiscovery should be instantiated.
+  *
+  * This instance generates both service and client handles. The service
+  * is identified by its connection information, which is usually a
+  * server and a port or a URI.
+  *
+  * The executor is necessary to perform background operations to update the
+  * list of candidate services every time there's a new update (a service
+  * is registered or unregistered).
+  *
+  * Service registration example for a service on URI "udp://10.0.0.1:2674"
+  * ...
+  * val discoveryService = new MidonetDiscovery(curator, executor, config)
+  * val service = discoveryService.registerServiceInstance(
+  *     "service_name", new URI("udp://10.0.0.1:2674"))
+  * ...
+  * // service no longer available
+  * service.unregister()
+  * // upon shutdown
+  * discoveryService.stop()
+  *
+  * Service client example:
+  * ...
+  * val discoveryService = new MidonetDiscovery(curator, executor, config)
+  * val client = discovery.getClient[MidonetServiceURI]("service_name")
+  * val instances: MidonetServiceURI = client.instances
+  * val address: String = instances.head.uri.getHost
+  * val port: Int = instances.head.uri.getPort()
+  * val protocol: String = instances.head.uri.getScheme()
+  * ...
+  *
+  * Service observable client example:
+  * ...
+  * val serviceObserver: Observer[List[MidonetServiceURI]] = ...
+  * client.observable.subscribe(serviceObserver)
+  * ...
+  *
+  */
+object MidonetDiscovery {
+    // Path inside zookeeper
+    final val discoveryPath = "/service-discovery"
+
+    final val serviceDiscoveryLog = "org.midonet.cluster.services.discovery"
+
+}
+
+class MidonetDiscovery @Inject()(curator: CuratorFramework,
+                                 executor: ExecutorService,
+                                 config: MidonetBackendConfig) {
+
+    private val log = Logger(LoggerFactory.getLogger(serviceDiscoveryLog))
+
+    log.debug("Midonet Discovery root path: " +
+              config.rootKey + discoveryPath)
+
+    private val discoveryService = ServiceDiscoveryBuilder
+        .builder[Void](classOf[Void])
+        .client(curator)
+        .basePath(config.rootKey + discoveryPath)
+        .build
+
+    discoveryService.start()
+
+    /** Stops the discovery service so all resources are closed. It does not
+      * unregisters currently registered service instances.
+      */
+    def stop(): Unit = discoveryService.close()
+
+    /** Returns a [[MidonetDiscoveryClient]] handle for the service discovery.
+      * This client has an observable field that publishes changes in the set
+      * of a given service provider name as well as methods to retrieve current
+      * service instance candidates. On subscription to the observable, it
+      * provides the current set of providers.
+      *
+      * @param serviceName is the name of the desired service
+      */
+    def getClient[S](serviceName: String)(implicit tag: TypeTag[S])
+    : MidonetDiscoveryClient[S] = {
+        new MidonetDiscoveryClient[S](serviceName, discoveryService, executor)
+    }
+
+    /** Returns a [[MidonetServiceHandler]] handle necessary to register the
+      * service instance in the discovery service. The returned service instance
+      * must be explicitly unregistered. This method accepts an address and port
+      * specifying the discovery information. If the registered node crashes,
+      * it will be unregistered upon session timeout.
+      *
+      * @param serviceName name of the service to register
+      * @param address String with the ip address
+      * @param port Int port where the service is bound to
+      * @return
+      */
+    def registerServiceInstance(serviceName: String, address: String, port: Int)
+    : MidonetServiceHandler = {
+        new MidonetServiceHandler(serviceName, Option(address), Option(port),
+                                   Option.empty, discoveryService)
+    }
+
+    /** Returns a [[MidonetServiceHandler]] handle necessary to register the
+      * service instance in the discovery service. The returned service instance
+      * must be explicitly unregistered. This method accept an uri specifying
+      * discovery information. If the registered node crashes, it will be
+      * unregistered upon session timeout.
+      *
+      * @param serviceName name of the service to register
+      * @param uri URI object specifying the discovery information
+      * @return
+      */
+    def registerServiceInstance(serviceName: String, uri: URI)
+    : MidonetServiceHandler = {
+        new MidonetServiceHandler(serviceName, Option.empty, Option.empty,
+                                  Option(uri), discoveryService)
+    }
+}
+
+/**
+  * Case classes supported by the MidonetDiscoveryService. When obtaining a
+  * client, one of these cases classes should be suplied and the registered
+  * information will be presented according to this interface.
+  */
+trait MidonetServiceInstance
+case class MidonetServiceHostAndPort(val address: String, val port: Int)
+    extends MidonetServiceInstance
+case class MidonetServiceURI(val uri: URI) extends MidonetServiceInstance
+
+/**
+  * A class implementing a service discovery client providing an observable
+  * with the service provider changes.
+  * The client instance should be explicitly 'stopped' if no one is going
+  * to use it any more.
+  */
+private[discovery] final class MidonetDiscoveryClient[S](
+        serviceName: String, serviceDiscovery: ServiceDiscovery[Void],
+        executor: ExecutorService)(implicit val tag: TypeTag[S]) {
+
+    private val log = Logger(LoggerFactory.getLogger(serviceDiscoveryLog))
+
+    private val updates = BehaviorSubject.create[Seq[S]]
+
+    private val cache =
+        serviceDiscovery.serviceCacheBuilder.name(serviceName).build
+
+    private def updateServiceInstances(): Unit = {
+        updates.onNext(instances)
+    }
+
+    cache.addListener(new ServiceCacheListener {
+        override def cacheChanged(): Unit = updateServiceInstances()
+        override def stateChanged(client: CuratorFramework,
+                                  newState: ConnectionState): Unit =
+            log.info(s"Changed state for $serviceName service: $newState")
+    }, executor)
+
+    cache.start()
+
+    executor.submit(makeRunnable { updateServiceInstances() })
+
+    /** Provide an observable for service provider changes */
+    val observable = updates.asObservable.distinctUntilChanged
+
+    /** Stop receiving service provider information. It closes resources
+      * used by cache and completes the observable. */
+    def stop(): Unit = {
+        try {
+            // The discovery service may have been closed previously
+            cache.close()
+        } catch {
+            case NonFatal(e) => log.info("Service discovery already closed.")
+        } finally {
+            updates.onCompleted()
+        }
+    }
+
+    /** Get the current set of registered [[ServiceInstance]]. It's updated
+      * upon notification from the storage layer (ZooKeeper) so it could be
+      * outdated for a limited amount of time. Instances registered not
+      * complying with the client type S are filtered.*/
+    def instances: Seq[S] = {
+        cache.getInstances.asScala.filter(_.isDefined).map(_.get)
+    }
+
+    private implicit def asMidonetService(instance: ServiceInstance[Void]): Option[S] = {
+        (tag, instance.getAddress, instance.getPort, instance.getUriSpec) match {
+            case (_, address: String, port: Integer, _)
+                if typeOf[S] =:= typeOf[MidonetServiceHostAndPort] =>
+                Option(MidonetServiceHostAndPort(address, port).asInstanceOf[S])
+            case (_, _, _, uri: UriSpec)
+                if typeOf[S] =:= typeOf[MidonetServiceURI] =>
+                Option(MidonetServiceURI(new URI(uri.build)).asInstanceOf[S])
+            case _ =>
+                // We filter those instances that do not comply with the
+                // type of the client doing the request
+                Option.empty[S]
+        }
+    }
+
+}
+
+/**
+  * Handler for a registered midonet service. Once a service is registered,
+  * it needs to be registered through its handler explicitly. If the service
+  * crashes, the service will be automatically registered upon session
+  * expiration.
+  */
+private[discovery] final class MidonetServiceHandler(name: String,
+       address: Option[String], port: Option[Int], uri: Option[URI],
+       discovery: ServiceDiscovery[Void]) {
+
+    /** Register the service with the specified discovery information into the
+      * discovery service. It must be registered explicitly before being
+      * available for discovery. */
+
+    @VisibleForTesting
+    private[discovery] val instanceBldr = ServiceInstance.builder[Void]()
+        .serviceType(ServiceType.DYNAMIC)
+        .name(name)
+    if (address.isDefined) instanceBldr.address(address.get)
+    if (port.isDefined) instanceBldr.port(port.get)
+    if (uri.isDefined) instanceBldr.uriSpec(new UriSpec(uri.get.toString))
+
+    @VisibleForTesting
+    private[discovery] val instance = instanceBldr.build
+
+    discovery.registerService(instance)
+
+    /** Indicate that the service is no longer provided by this instance.
+      * This method must be explicitly called to unregister a service. Shutting
+      * down the discovery service does not modify the registration state to
+      * prevent service unregistration upon crash. */
+    def unregister(): Unit = discovery.unregisterService(instance)
+}
---
+++ nsdb/src/test/scala/org/midonet/cluster/services/discovery/MidonetDiscoveryTest.scala
@@ -0,0 +1,292 @@
+/*
+ * Copyright 2016 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.cluster.services.discovery
+
+import java.net.URI
+import java.util.concurrent.TimeUnit
+
+import scala.collection.JavaConversions._
+import scala.concurrent.duration.Duration
+
+import com.typesafe.config.ConfigFactory
+import com.typesafe.scalalogging.Logger
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{GivenWhenThen, FeatureSpec, Matchers}
+import org.slf4j.LoggerFactory
+
+import org.midonet.cluster.storage.MidonetBackendConfig
+import org.midonet.cluster.util.CuratorTestFramework
+import org.midonet.util.MidonetEventually
+import org.midonet.util.concurrent.SameThreadButAfterExecutorService
+import org.midonet.util.reactivex.TestAwaitableObserver
+
+/*
+ * Note: For these tests we use service descriptions implemented as Java
+ * and Scala classes because there are some differences in how json
+ * serializer treats them, so we make sure it works for both languages.
+ * (It also serves as example: note that 'TestJavaServiceDetails' definition
+ * requires an annotation...)
+ */
+@RunWith(classOf[JUnitRunner])
+class MidonetDiscoveryTest extends FeatureSpec with Matchers
+                                   with CuratorTestFramework
+                                   with MidonetEventually with GivenWhenThen {
+
+    private val timeout = Duration(5, TimeUnit.SECONDS)
+    private val executor = new SameThreadButAfterExecutorService
+
+    private val config = new MidonetBackendConfig(ConfigFactory.empty())
+
+    feature("discovery basics") {
+        scenario("instantiation") {
+            val discovery = new MidonetDiscovery(curator, executor, config)
+            discovery shouldNot be (null)
+        }
+    }
+
+    feature("server") {
+        scenario("server registration - server and port") {
+            Given("A discovery service")
+            val discovery = new MidonetDiscovery(curator, executor, config)
+            When("Registering the service")
+            val handler = discovery.registerServiceInstance("testService", "10.0.0.1", 1234)
+            Then("It's handler should contain a non-null instance")
+            handler.instance shouldNot be (null)
+            handler.unregister()
+            discovery.stop()
+        }
+
+        scenario("server registration - uri") {
+            Given("A discovery service")
+            val discovery = new MidonetDiscovery(curator, executor, config)
+            When("Registering the service")
+            val handler = discovery.registerServiceInstance("testService", new URI("udp://10.0.0.1:1234"))
+            Then("It's handler should contain a non-null instance")
+            handler.instance shouldNot be (null)
+            handler.unregister()
+            discovery.stop()
+        }
+
+        scenario("client handle - server and port") {
+            Given("A discovery service")
+            val discovery = new MidonetDiscovery(curator, executor, config)
+
+            And("A service registered and a client of its type")
+            val handler = discovery.registerServiceInstance("testService", "10.0.0.1", 1234)
+            val client = discovery.getClient[MidonetServiceHostAndPort]("testService")
+
+            Then("the service is of the given type and matches the data")
+            eventually {
+                val service = client.instances.head
+                service.address shouldBe "10.0.0.1"
+                service.port shouldBe 1234
+                client.instances should have size 1
+            }
+
+            handler.unregister()
+            client.stop()
+            discovery.stop()
+        }
+
+        scenario("client handle - uri") {
+            Given("A discovery service")
+            val discovery = new MidonetDiscovery(curator, executor, config)
+
+            And("A service registered and a client of its type")
+            val handler = discovery.registerServiceInstance(
+                "testService", new URI("udp://10.0.0.1:1234/path/to/endpoint"))
+            val client = discovery.getClient[MidonetServiceURI]("testService")
+
+            Then("the service is of the given type and matches the data")
+            eventually {
+                val service = client.instances.head
+                service.uri.getHost shouldBe "10.0.0.1"
+                service.uri.getPort shouldBe 1234
+                service.uri.getPath shouldBe "/path/to/endpoint"
+                service.uri.getScheme shouldBe "udp"
+                client.instances.toSet should have size 1
+            }
+
+            handler.unregister()
+            discovery.stop()
+            client.stop()
+        }
+
+        scenario("service and client from different types") {
+            Given("A discovery service")
+            val discovery = new MidonetDiscovery(curator, executor, config)
+
+            And("two services of different types under the same name")
+            val handler1 = discovery.registerServiceInstance(
+                "testService", new URI("udp://10.0.0.1:1234"))
+            val handler2 = discovery.registerServiceInstance(
+                "testService", "10.0.0.2", 1234)
+
+            And("Two clients of differents types for the same service name")
+            val client1 = discovery.getClient[MidonetServiceHostAndPort]("testService")
+            val client2 = discovery.getClient[MidonetServiceURI]("testService")
+
+            Then("Querying each client just returns its type instances")
+            eventually {
+                client1.instances should have size 1
+                client1.instances.head.address shouldBe "10.0.0.2"
+                client1.instances.head.port shouldBe 1234
+                client2.instances should have size 1
+                client2.instances.head.uri.getHost shouldBe "10.0.0.1"
+                client2.instances.head.uri.getPort shouldBe 1234
+                client2.instances.head.uri.getScheme shouldBe "udp"
+            }
+            handler1.unregister()
+            handler2.unregister()
+            client1.stop()
+            client2.stop()
+            discovery.stop()
+        }
+
+        scenario("server disconnection - server and port") {
+            Given("A discovery service")
+            val discovery = new MidonetDiscovery(curator, executor, config)
+
+            And("Two registered services and a client")
+            val address1 = "10.0.0.1"
+            val address2 = "20.0.0.2"
+            val handler1 = discovery.registerServiceInstance("testService", address1, 1234)
+            val handler2 = discovery.registerServiceInstance("testService", address2, 1234)
+            val client = discovery.getClient[MidonetServiceHostAndPort]("testService")
+            val log = Logger(LoggerFactory.getLogger("TEST"))
+            var address: String = null
+
+            Then("The two services are registered")
+            eventually {
+                client.instances should have length 2
+                val service = client.instances.head
+                address = service.address
+            }
+
+            if (address == address1) {
+                When("Unregistering one of them")
+                handler1.unregister()
+
+                Then("Is not present on the instances returned by the client")
+                eventually {
+                    client.instances should have length 1
+                    val service = client.instances.head
+                    service.address shouldBe address2
+                }
+
+                When("Unregistering the other service")
+                handler2.unregister()
+            } else {
+                handler2.unregister()
+                eventually {
+                    client.instances should have length 1
+                    val service = client.instances.head
+                    service.address shouldBe address1
+                }
+                handler1.unregister()
+            }
+
+            Then("No instance is returned by the client")
+            eventually {
+                client.instances should have length 0
+                client.instances.isEmpty shouldBe true
+            }
+
+            discovery.stop()
+            client.stop()
+        }
+    }
+
+    feature("notifications") {
+        scenario("service addition") {
+            Given("A discovery service")
+            val discovery = new MidonetDiscovery(curator, executor, config)
+
+            And("A client and a observable")
+            val monitor = new TestAwaitableObserver[Seq[MidonetServiceHostAndPort]]
+            val client = discovery.getClient[MidonetServiceHostAndPort]("testService")
+            client.observable.subscribe(monitor)
+
+            Then("The first event is an empty list")
+            monitor.awaitOnNext(1, timeout) shouldBe true
+            monitor.getOnNextEvents.last should have length 0
+
+            When("Registering one instance")
+            val address1 = "10.0.0.1"
+            val handler1 = discovery.registerServiceInstance("testService", address1, 1234)
+
+            Then("There's a new event with the registered service")
+            monitor.awaitOnNext(2, timeout) shouldBe true
+            monitor.getOnNextEvents.last should have length 1
+            monitor.getOnNextEvents.last.get(0).address shouldBe address1
+
+            When("Adding a second service")
+            val address2 = "20.0.0.2"
+            val handler2 = discovery.registerServiceInstance("testService", address2, 1234)
+
+            Then("A new event is emitted with both services")
+            monitor.awaitOnNext(3, timeout) shouldBe true
+            monitor.getOnNextEvents.last should have length 2
+            monitor.getOnNextEvents.last.map(s => s.address)
+                .toSet shouldBe Set(address1, address2)
+
+            handler1.unregister()
+            handler2.unregister()
+            discovery.stop()
+            client.stop()
+        }
+
+        scenario("service removal") {
+            Given("A service discovery")
+            val discovery = new MidonetDiscovery(curator, executor, config)
+
+            And("Two services registered")
+            val address1 = "10.0.0.1"
+            val address2 = "20.0.0.2"
+            val handler1 = discovery.registerServiceInstance("testService", address1, 1234)
+            val handler2 = discovery.registerServiceInstance("testService", address2, 1234)
+
+            And("An observable and a client")
+            val monitor = new TestAwaitableObserver[Seq[MidonetServiceHostAndPort]]
+            val client = discovery.getClient[MidonetServiceHostAndPort]("testService")
+            client.observable.subscribe(monitor)
+
+            Then("The first event has two elements")
+            monitor.awaitOnNext(1, timeout) shouldBe true
+            monitor.getOnNextEvents.last.map(s => s.address)
+                .toSet shouldBe Set(address1, address2)
+
+            When("Unregistering one instances")
+            handler1.unregister()
+            Then("A new event without this instance is emitted")
+            monitor.awaitOnNext(2, timeout) shouldBe true
+            monitor.getOnNextEvents.last.map(s => s.address)
+                .toSet shouldBe Set(address2)
+
+            When("Unregistering the other instance")
+            handler2.unregister()
+            Then("A new event is empty with an empty list")
+            monitor.awaitOnNext(3, timeout) shouldBe true
+            monitor.getOnNextEvents.last.toSet shouldBe Set.empty
+
+            discovery.stop()
+            client.stop()
+        }
+    }
+}
