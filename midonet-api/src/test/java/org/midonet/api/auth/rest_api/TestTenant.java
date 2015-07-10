4629aef89d09f221d11213bc9acb1a34747271e0
Galo Navarro <galo@midokura.com>
2015-07-10 15:20:28 +0200

    API: tenant refactor, client dto removal
    This patch:
    - Removes the DtoTenant
    - Moves the Tenant DTO to the cluster
    - Cleans code and removes redundant javadocs
    - Tweaks the TestTenant to adapt to the lack of DtoTenant.
      Specifically, the main point of the Dto classes is that they are able
      to deserialize the URI into fields and add them to the equals.
      Unfortunately this means we also need to support duplicate models
      at client and server side.  This patch solves this by having the test
      fill the base URI by itself, and recovers the lost coverage by adding
      a new test that ensure that with a given base URI, the Tenant object
      serializes a correct URI.
    - Adds support to add tenants to the MockAuthService, as the v2 stack
      does not store tenants in ZK, but the tests require adding tenants.
      This allows the Cluster API to pass relevant tests in TestRouter,
      TestBridge and TestTenant where having the tenant available via the
      API is necessary.
    
    This patch is a preparation to increase backwards compatibility on the
    Cluster API.
    
    Ref.: MNA-557
    Signed-off-by: Galo Navarro <galo@midokura.com>
    
    Change-Id: Ib9e72ed851aa715e32037081effcb50123cd4b54

--- midonet-api/src/test/java/org/midonet/api/auth/rest_api/TestTenant.java
+++ midonet-api/src/test/java/org/midonet/api/auth/rest_api/TestTenant.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Midokura SARL
+ * Copyright 2015 Midokura SARL
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,7 +15,10 @@
  */
 package org.midonet.api.auth.rest_api;
 
-import com.fasterxml.jackson.databind.JavaType;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.test.framework.JerseyTest;
 
@@ -23,20 +26,22 @@ import org.junit.Before;
 import org.junit.Test;
 
 import org.midonet.api.rest_api.DtoWebResource;
-import org.midonet.api.rest_api.FuncTest;
 import org.midonet.api.rest_api.Topology;
 import org.midonet.client.dto.DtoApplication;
 import org.midonet.client.dto.DtoBridge;
-import org.midonet.client.dto.DtoTenant;
+import org.midonet.cluster.auth.AuthService;
+import org.midonet.cluster.auth.MockAuthService;
+import org.midonet.cluster.rest_api.ResourceUris;
 import org.midonet.cluster.rest_api.VendorMediaType;
+import org.midonet.cluster.rest_api.models.Tenant;
 
-import javax.ws.rs.core.UriBuilder;
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
+import static org.midonet.api.rest_api.FuncTest._injector;
+import static org.midonet.api.rest_api.FuncTest.appDesc;
+import static org.midonet.api.rest_api.FuncTest.isCompatApiEnabled;
 
 /**
  * Tests tenant API operations.  Currently assumes MockAuth is being used.
@@ -47,49 +52,43 @@ public class TestTenant extends JerseyTest {
     private DtoWebResource dtoResource;
 
     public TestTenant() {
-        super(FuncTest.appDesc);
+        super(appDesc);
     }
 
     private void createActualTenants(int count) {
         Topology.Builder builder = new Topology.Builder(dtoResource);
 
         for (int i = 0 ; i < count ; i++) {
-            DtoBridge bridge = new DtoBridge();
             String tenantId = Integer.toString(i);
-            bridge.setName(tenantId);
-            bridge.setTenantId(tenantId);
-            builder.create(tenantId, bridge);
+            if (isCompatApiEnabled()) {
+                // In the new storage stack we don't store tenants in MidoNet
+                // and instead fetch them directly from the AuthService, so
+                // let's add them there.
+                AuthService as = _injector.getInstance(AuthService.class);
+                ((MockAuthService)as).addTenant(tenantId, tenantId);
+            } else {
+                // TODO: remove when old stack is removed
+                DtoBridge bridge = new DtoBridge();
+                bridge.setName(tenantId);
+                bridge.setTenantId(tenantId);
+                builder.create(tenantId, bridge);
+            }
         }
 
         topology = builder.build();
     }
 
-    private static DtoTenant getExpectedTenant(URI baseUri, URI tenantsUri,
-                                               String id) {
-        String uri = tenantsUri.toString() + "/" + id;
-        DtoTenant t = new DtoTenant(id, id);
-        t.setUri(UriBuilder.fromUri(uri).build());
-        t.setRouters(UriBuilder.fromUri(baseUri + "routers?tenant_id=" + id)
-                .build());
-        t.setBridges(UriBuilder.fromUri(baseUri + "bridges?tenant_id=" + id)
-                .build());
-        t.setChains(UriBuilder.fromUri(baseUri + "chains?tenant_id=" + id)
-                .build());
-        t.setPortGroups(UriBuilder.fromUri(baseUri + "port_groups?tenant_id="
-                + id).build());
-        return t;
+    private static Tenant getExpectedTenant(URI baseUri, String id) {
+        return new Tenant(baseUri, id, id);
     }
 
-    private static List<DtoTenant> getExpectedTenants(URI baseUri,
-                                                      URI tenantsUri,
-                                                      int startTenantId,
-                                                      int endTenantId) {
-        List<DtoTenant> tenants = new ArrayList<>();
+    private static List<Tenant> getExpectedTenants(URI baseUri,
+                                                   int startTenantId,
+                                                   int endTenantId) {
+        List<Tenant> tenants = new ArrayList<>();
 
         for (int i = startTenantId; i <= endTenantId; i++) {
-            DtoTenant t = getExpectedTenant(baseUri, tenantsUri,
-                    Integer.toString(i));
-            tenants.add(t);
+            tenants.add(getExpectedTenant(baseUri, Integer.toString(i)));
         }
 
         return tenants;
@@ -99,46 +98,47 @@ public class TestTenant extends JerseyTest {
     public void setUp() {
         WebResource resource = resource();
         dtoResource = new DtoWebResource(resource);
-
         createActualTenants(10);
+    }
+
+    @Test
+    public void testDtoGeneratesUris() throws Exception {
+        String id = "sometenant";
+        Tenant t = new Tenant(topology.getApplication().getUri(),
+                              id, id + "-name");
+        assertEquals(t.getUri().toString(),
+                     topology.getApplication().getUri() +
+                     ResourceUris.TENANTS + "/" + id);
     }
 
     @Test
     public void testListTenants() throws Exception {
 
-        // Get the expected list of DtoTenant objects
+        // Get the expected list of Tenant objects
         DtoApplication app = topology.getApplication();
-        List<DtoTenant> expected = getExpectedTenants(app.getUri(),
-                app.getTenants(), 0, 9);
+        List<Tenant> expected = getExpectedTenants(app.getUri(), 0, 9);
 
-        // Get the actual DtoTenant objects
-        String actualRaw = dtoResource.getAndVerifyOk(app.getTenants(),
+        // Get the actual Tenant objects
+        Tenant[] actualRaw = dtoResource.getAndVerifyOk(app.getTenants(),
                 VendorMediaType.APPLICATION_TENANT_COLLECTION_JSON,
-                String.class);
-        JavaType type = FuncTest.objectMapper.getTypeFactory()
-                .constructParametrizedType(List.class, List.class,
-                                           DtoTenant.class);
-        List<DtoTenant> actual = FuncTest.objectMapper.readValue(
-                actualRaw, type);
-
+                Tenant[].class);
+        // Fill in because the URI won't get deserialized
+        for (Tenant t : actualRaw) {
+            t.setBaseUri(topology.getApplication().getUri());
+        }
         // Compare the actual and expected
-        assertThat(actual, containsInAnyOrder(expected.toArray()));
-        assertThat(expected, containsInAnyOrder(actual.toArray()));
+        assertThat(expected, containsInAnyOrder(actualRaw));
 
         // Test that the URI for 'tenant' is correct in each item
-        for (DtoTenant t : actual) {
+        for (Tenant t : actualRaw) {
 
             // Construct the expected object
-            DtoTenant expectedTenant = getExpectedTenant(app.getUri(),
-                    app.getTenants(),
-                    t.getId());
+            Tenant expectedTenant = getExpectedTenant(app.getUri(), t.id);
 
             // Get the actual object
-            actualRaw = dtoResource.getAndVerifyOk(t.getUri(),
+            Tenant actualTenant = dtoResource.getAndVerifyOk(t.getUri(),
                     VendorMediaType.APPLICATION_TENANT_JSON,
-                    String.class);
-            DtoTenant actualTenant = FuncTest.objectMapper.readValue(
-                    actualRaw, DtoTenant.class);
+                    Tenant.class);
 
             // Compare
             assertThat(actualTenant, equalTo(expectedTenant));
--- midonet-client/src/main/java/org/midonet/client/resource/Application.java
+++ midonet-client/src/main/java/org/midonet/client/resource/Application.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Midokura SARL
+ * Copyright 2015 Midokura SARL
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,11 +18,53 @@ package org.midonet.client.resource;
 
 import java.net.URI;
 import java.util.UUID;
+
 import javax.ws.rs.core.MultivaluedMap;
 
 import org.midonet.client.WebResource;
-import org.midonet.client.dto.*;
-import org.midonet.cluster.rest_api.VendorMediaType;
+import org.midonet.client.dto.DtoAdRoute;
+import org.midonet.client.dto.DtoApplication;
+import org.midonet.client.dto.DtoBgp;
+import org.midonet.client.dto.DtoBridge;
+import org.midonet.client.dto.DtoBridgePort;
+import org.midonet.client.dto.DtoHost;
+import org.midonet.client.dto.DtoHostVersion;
+import org.midonet.client.dto.DtoPort;
+import org.midonet.client.dto.DtoPortGroup;
+import org.midonet.client.dto.DtoRoute;
+import org.midonet.client.dto.DtoRouter;
+import org.midonet.client.dto.DtoRouterPort;
+import org.midonet.client.dto.DtoRule;
+import org.midonet.client.dto.DtoRuleChain;
+import org.midonet.client.dto.DtoSystemState;
+import org.midonet.client.dto.DtoTunnelZone;
+import org.midonet.client.dto.DtoWriteVersion;
+
+import static org.midonet.cluster.rest_api.VendorMediaType.APPLICATION_AD_ROUTE_JSON;
+import static org.midonet.cluster.rest_api.VendorMediaType.APPLICATION_BGP_JSON;
+import static org.midonet.cluster.rest_api.VendorMediaType.APPLICATION_BRIDGE_COLLECTION_JSON;
+import static org.midonet.cluster.rest_api.VendorMediaType.APPLICATION_BRIDGE_JSON;
+import static org.midonet.cluster.rest_api.VendorMediaType.APPLICATION_CHAIN_COLLECTION_JSON;
+import static org.midonet.cluster.rest_api.VendorMediaType.APPLICATION_CHAIN_JSON;
+import static org.midonet.cluster.rest_api.VendorMediaType.APPLICATION_HOST_COLLECTION_JSON_V3;
+import static org.midonet.cluster.rest_api.VendorMediaType.APPLICATION_HOST_JSON_V3;
+import static org.midonet.cluster.rest_api.VendorMediaType.APPLICATION_HOST_VERSION_JSON;
+import static org.midonet.cluster.rest_api.VendorMediaType.APPLICATION_JSON_V5;
+import static org.midonet.cluster.rest_api.VendorMediaType.APPLICATION_PORTGROUP_COLLECTION_JSON;
+import static org.midonet.cluster.rest_api.VendorMediaType.APPLICATION_PORTGROUP_JSON;
+import static org.midonet.cluster.rest_api.VendorMediaType.APPLICATION_PORT_V2_JSON;
+import static org.midonet.cluster.rest_api.VendorMediaType.APPLICATION_ROUTER_COLLECTION_JSON;
+import static org.midonet.cluster.rest_api.VendorMediaType.APPLICATION_ROUTER_JSON;
+import static org.midonet.cluster.rest_api.VendorMediaType.APPLICATION_ROUTE_JSON;
+import static org.midonet.cluster.rest_api.VendorMediaType.APPLICATION_RULE_JSON_V2;
+import static org.midonet.cluster.rest_api.VendorMediaType.APPLICATION_SYSTEM_STATE_JSON_V2;
+import static org.midonet.cluster.rest_api.VendorMediaType.APPLICATION_TENANT_COLLECTION_JSON;
+import static org.midonet.cluster.rest_api.VendorMediaType.APPLICATION_TENANT_JSON;
+import static org.midonet.cluster.rest_api.VendorMediaType.APPLICATION_TUNNEL_ZONE_COLLECTION_JSON;
+import static org.midonet.cluster.rest_api.VendorMediaType.APPLICATION_TUNNEL_ZONE_HOST_COLLECTION_JSON;
+import static org.midonet.cluster.rest_api.VendorMediaType.APPLICATION_TUNNEL_ZONE_HOST_JSON;
+import static org.midonet.cluster.rest_api.VendorMediaType.APPLICATION_TUNNEL_ZONE_JSON;
+import static org.midonet.cluster.rest_api.VendorMediaType.APPLICATION_WRITE_VERSION_JSON;
 
 public class Application extends ResourceBase<Application, DtoApplication> {
 
@@ -31,7 +73,7 @@ public class Application extends ResourceBase<Application, DtoApplication> {
     private static String ID_TOKEN = "{id}";
 
     public Application(WebResource resource, DtoApplication app) {
-        this(resource, app, VendorMediaType.APPLICATION_JSON_V5);
+        this(resource, app, APPLICATION_JSON_V5);
     }
 
     public Application(WebResource resource, DtoApplication app,
@@ -41,264 +83,136 @@ public class Application extends ResourceBase<Application, DtoApplication> {
         this.resource = resource;
     }
 
-    /**
-     * Returns URI of the REST API for this resource
-     *
-     * @return uri of the resource
-     */
     @Override
     public URI getUri() {
         return app.getUri();
     }
 
-    /**
-     * Returns version of the application
-     *
-     * @return version
-     */
     public String getVersion() {
         get();
         return app.getVersion();
     }
 
-    /**
-     * Gets tenants.
-     *
-     * @return Collection of tenants
-     */
     public ResourceCollection<Tenant> getTenants(
             MultivaluedMap<String,String> queryParams) {
         return getChildResources(principalDto.getTenants(),
                 queryParams,
-                VendorMediaType
-                        .APPLICATION_TENANT_COLLECTION_JSON,
-                Tenant.class, DtoTenant.class);
+                APPLICATION_TENANT_COLLECTION_JSON,
+                Tenant.class, Tenant.class);
     }
 
-    /**
-     * Gets bridges.
-     *
-     * @return Collection of bridges
-     */
     public ResourceCollection<Bridge> getBridges(
             MultivaluedMap<String,String> queryParams) {
         return getChildResources(principalDto.getBridges(),
                                  queryParams,
-                                 VendorMediaType
-                                     .APPLICATION_BRIDGE_COLLECTION_JSON,
+                                 APPLICATION_BRIDGE_COLLECTION_JSON,
                                  Bridge.class, DtoBridge.class);
     }
 
-    /**
-     * Gets routers.
-     *
-     * @return collection of routers
-     */
     public ResourceCollection<Router> getRouters(
             MultivaluedMap<String,String> queryParams) {
         return getChildResources(principalDto.getRouters(),
                                  queryParams,
-                                 VendorMediaType
-                                     .APPLICATION_ROUTER_COLLECTION_JSON,
+                                 APPLICATION_ROUTER_COLLECTION_JSON,
                                  Router.class, DtoRouter.class);
     }
 
-    /**
-     * Gets chains
-     *
-     * @return collection of chains
-     */
     public ResourceCollection<RuleChain> getChains(
             MultivaluedMap<String,String> queryParams) {
         return getChildResources(principalDto.getChains(),
                                  queryParams,
-                                 VendorMediaType
-                                     .APPLICATION_CHAIN_COLLECTION_JSON,
+                                 APPLICATION_CHAIN_COLLECTION_JSON,
                                  RuleChain.class, DtoRuleChain.class);
     }
 
-    /**
-     * Gets port groups.
-     *
-     * @return collection of port groups
-     */
     public ResourceCollection<PortGroup> getPortGroups(
             MultivaluedMap<String,String> queryParams) {
         return getChildResources(principalDto.getPortGroups(),
                 queryParams,
-                VendorMediaType.APPLICATION_PORTGROUP_COLLECTION_JSON,
+                APPLICATION_PORTGROUP_COLLECTION_JSON,
                 PortGroup.class, DtoPortGroup.class);
     }
 
-    /**
-     * Gets IP address groups.
-     *
-     * @return collection of IP address groups
-     */
-    public ResourceCollection<IpAddrGroup> getIpAddrGroups(
-            MultivaluedMap<String, String> queryParams) {
-        return getChildResources(principalDto.getIpAddrGroups(), queryParams,
-                VendorMediaType.APPLICATION_IP_ADDR_GROUP_COLLECTION_JSON,
-                IpAddrGroup.class, DtoIpAddrGroup.class);
-    }
-
-    /**
-     * Gets hosts.
-     *
-     * @return collection host
-     */
     public ResourceCollection<Host> getHosts(
             MultivaluedMap<String,String> queryParams) {
         return getChildResources(principalDto.getHosts(),
                                  queryParams,
-                                 VendorMediaType
-                                     .APPLICATION_HOST_COLLECTION_JSON_V3,
+                                 APPLICATION_HOST_COLLECTION_JSON_V3,
                                  Host.class, DtoHost.class);
     }
 
-    /**
-     * Gets Tunnel Zones.
-     *
-     * @return collection of tunnel zones
-     */
     public ResourceCollection<TunnelZone>
             getTunnelZones(MultivaluedMap<String,String> queryParams) {
         return getChildResources(principalDto.getTunnelZones(), queryParams,
-            VendorMediaType.APPLICATION_TUNNEL_ZONE_COLLECTION_JSON,
+            APPLICATION_TUNNEL_ZONE_COLLECTION_JSON,
             TunnelZone.class, DtoTunnelZone.class);
     }
 
-    /**
-     * Adds a bridge.
-     *
-     * @return new Bridge resource
-     */
     public Bridge addBridge() {
         return new Bridge(resource, principalDto.getBridges(),
                           new DtoBridge());
     }
 
-    /**
-     * Adds a router.
-     *
-     * @return new Router() resource
-     */
     public Router addRouter() {
         return new Router(resource, principalDto.getRouters(),
                           new DtoRouter());
     }
 
-    /**
-     * Adds a chain.
-     *
-     * @return new Chain() resource
-     */
     public RuleChain addChain() {
         return new RuleChain(resource, principalDto.getChains(),
                              new DtoRuleChain());
     }
 
-    /**
-     * Adds a port group.
-     *
-     * @return new PortGroup() resource.
-     */
     public PortGroup addPortGroup() {
         return new PortGroup(resource, principalDto.getPortGroups(),
                 new DtoPortGroup());
     }
 
-    /**
-     * Adds an IP address group.
-     *
-     * @return new PortGroup() resource.
-     */
-    public IpAddrGroup addIpAddrGroup() {
-        return new IpAddrGroup(resource,
-                principalDto.getIpAddrGroups(), new DtoIpAddrGroup());
-    }
-
-    /**
-     * Adds a tunnel zone
-     *
-     * @return new gre tunnel zone.
-     */
     public TunnelZone addGreTunnelZone() {
         return new TunnelZone(resource,
                 principalDto.getTunnelZones(),
                 new DtoTunnelZone(),
-                VendorMediaType.APPLICATION_TUNNEL_ZONE_HOST_JSON,
-                VendorMediaType
-                        .APPLICATION_TUNNEL_ZONE_HOST_COLLECTION_JSON);
+                APPLICATION_TUNNEL_ZONE_HOST_JSON,
+                APPLICATION_TUNNEL_ZONE_HOST_COLLECTION_JSON);
     }
 
-    /**
-     * Returns an ad route
-     *
-     * @param id ID of ad route
-     * @return AdRoute
-     */
     public AdRoute getAdRoute(UUID id) {
         URI uri = createUriFromTemplate(
                 app.getAdRouteTemplate(), ID_TOKEN, id);
         DtoAdRoute adRoute = resource.get(uri, null, DtoAdRoute.class,
-                VendorMediaType.APPLICATION_AD_ROUTE_JSON);
+                APPLICATION_AD_ROUTE_JSON);
         return new AdRoute(resource, null, adRoute);
     }
 
-    /**
-     * Returns BGP object
-     *
-     * @param id ID of BGP
-     * @return BGP
-     */
     public Bgp getBgp(UUID id) {
         URI uri = createUriFromTemplate(
                 principalDto.getBgpTemplate(), ID_TOKEN, id);
         DtoBgp bgp = resource.get(uri, null, DtoBgp.class,
-                VendorMediaType.APPLICATION_BGP_JSON);
+                APPLICATION_BGP_JSON);
         return new Bgp(resource, null, bgp);
     }
 
-    /**
-     * Returns Bridge object
-     *
-     * @param id ID of bridge
-     * @return Bridge
-     */
     public Bridge getBridge(UUID id) {
         URI uri = createUriFromTemplate(
                 principalDto.getBridgeTemplate(), ID_TOKEN, id);
         DtoBridge bridge = resource.get(uri, null, DtoBridge.class,
-                VendorMediaType.APPLICATION_BRIDGE_JSON);
+                APPLICATION_BRIDGE_JSON);
         return new Bridge(resource, null, bridge);
     }
 
-    /**
-     * Returns Host object
-     *
-     * @param id ID of host
-     * @return Host
-     */
     public Host getHost(UUID id) {
         URI uri = createUriFromTemplate(
                 principalDto.getHostTemplate(), ID_TOKEN, id);
         DtoHost host = resource.get(uri, null, DtoHost.class,
-                VendorMediaType.APPLICATION_HOST_JSON_V3);
+                APPLICATION_HOST_JSON_V3);
         return new Host(resource, null, host);
     }
 
-    /**
-     * Returns Port object
-     *
-     * @param id ID of port
-     * @return Port
-     */
     public Port<?,?> getPort(UUID id) {
         URI uri = createUriFromTemplate(
                 principalDto.getPortTemplate(), ID_TOKEN, id);
         DtoPort port = resource.get(uri, null, DtoPort.class,
-                VendorMediaType.APPLICATION_PORT_V2_JSON);
+                APPLICATION_PORT_V2_JSON);
         if (port instanceof DtoBridgePort) {
             return new BridgePort(resource, null, (DtoBridgePort) port);
         } else if (port instanceof  DtoRouterPort) {
@@ -309,156 +223,87 @@ public class Application extends ResourceBase<Application, DtoApplication> {
         }
     }
 
-    /**
-     * Returns PortGroup object
-     *
-     * @param id ID of port group
-     * @return PortGroup
-     * */
     public PortGroup getPortGroup(UUID id) {
         URI uri = createUriFromTemplate(
                 principalDto.getPortGroupTemplate(), ID_TOKEN, id);
         DtoPortGroup portGroup = resource.get(uri, null, DtoPortGroup.class,
-                VendorMediaType.APPLICATION_PORTGROUP_JSON);
+                APPLICATION_PORTGROUP_JSON);
         return new PortGroup(resource, null, portGroup);
     }
 
-    /**
-     * Returns IpAddrGroup object
-     *
-     * @param id ID of IP address group
-     * @return IPAddrgroup
-     */
-    public IpAddrGroup getIpAddrGroup(UUID id) {
-        URI uri = createUriFromTemplate(
-                principalDto.getIpAddrGroupTemplate(), ID_TOKEN, id);
-        DtoIpAddrGroup IpAddrGroup = resource.get(
-                uri, null, DtoIpAddrGroup.class,
-                VendorMediaType.APPLICATION_IP_ADDR_GROUP_JSON);
-        return new IpAddrGroup(resource, null, IpAddrGroup);
-    }
-
-    /**
-     * Returns Route object
-     *
-     * @param id ID of route
-     * @return Route
-     * */
     public Route getRoute(UUID id) {
         URI uri = createUriFromTemplate(
                 principalDto.getRouteTemplate(), ID_TOKEN, id);
         DtoRoute route = resource.get(uri, null, DtoRoute.class,
-                VendorMediaType.APPLICATION_ROUTE_JSON);
+                APPLICATION_ROUTE_JSON);
         return new Route(resource, null, route);
     }
 
-    /**
-     * Returns Router object
-     *
-     * @param id ID of router
-     * @return Router
-     * */
     public Router getRouter(UUID id) {
         URI uri = createUriFromTemplate(
                 principalDto.getRouterTemplate(), ID_TOKEN, id);
         DtoRouter router = resource.get(uri, null, DtoRouter.class,
-                VendorMediaType.APPLICATION_ROUTER_JSON);
+                APPLICATION_ROUTER_JSON);
         return new Router(resource, null, router);
     }
 
-    /**
-     * Returns Rule object
-     *
-     * @param id ID of rule
-     * @return Rule
-     */
     public Rule getRule(UUID id) {
         URI uri = createUriFromTemplate(
                 principalDto.getRuleTemplate(), ID_TOKEN, id);
         DtoRule rule = resource.get(uri, null, DtoRule.class,
-                VendorMediaType.APPLICATION_RULE_JSON_V2);
+                APPLICATION_RULE_JSON_V2);
         return new Rule(resource, null, rule);
     }
 
-    /**
-     * Returns RuleChain object
-     *
-     * @param id ID of chain
-     * @return RuleChain
-     */
     public RuleChain getRuleChain(UUID id) {
         URI uri = createUriFromTemplate(
                 principalDto.getChainTemplate(), ID_TOKEN, id);
         DtoRuleChain chain = resource.get(uri, null, DtoRuleChain.class,
-                VendorMediaType.APPLICATION_CHAIN_JSON);
+                APPLICATION_CHAIN_JSON);
         return new RuleChain(resource, null, chain);
     }
 
-    /**
-     * Returns Tenant object
-     *
-     * @param id ID of tenant
-     * @return Tenant
-     */
     public Tenant getTenant(String id) {
         URI uri = createUriFromTemplate(
                 principalDto.getTenantTemplate(), ID_TOKEN, id);
-        DtoTenant tenant = resource.get(uri, null, DtoTenant.class,
-                VendorMediaType.APPLICATION_TENANT_JSON);
-        return new Tenant(resource, null, tenant);
+        return new Tenant(
+            resource, null,
+            resource.get(uri, null,
+                         org.midonet.cluster.rest_api.models.Tenant.class,
+                         APPLICATION_TENANT_JSON)
+        );
     }
 
-    /**
-     * Gets the WriteVersion object.
-     *
-     * @return WriteVersion
-     */
     public WriteVersion getWriteVersion() {
         URI uri = principalDto.getWriteVersion();
         DtoWriteVersion writeVersion = resource.get(uri, null,
                 DtoWriteVersion.class,
-                VendorMediaType.APPLICATION_WRITE_VERSION_JSON);
+                APPLICATION_WRITE_VERSION_JSON);
         return new WriteVersion(resource, null, writeVersion);
     }
 
-    /**
-     * Gets the SystemState object.
-     *
-     * @return SystemState
-     */
     public SystemState getSystemState() {
         URI uri = principalDto.getSystemState();
         DtoSystemState systemState = resource.get(uri, null,
                 DtoSystemState.class,
-                VendorMediaType.APPLICATION_SYSTEM_STATE_JSON_V2);
+                APPLICATION_SYSTEM_STATE_JSON_V2);
         return new SystemState(resource, null, systemState);
     }
 
-    /**
-     * Gets the HostVersion object.
-     *
-     * @return SystemState
-     */
     public ResourceCollection<HostVersion> getHostVersions() {
         return getChildResources(principalDto.getHostVersions(), null,
-                                 VendorMediaType.APPLICATION_HOST_VERSION_JSON,
+                                 APPLICATION_HOST_VERSION_JSON,
                                  HostVersion.class, DtoHostVersion.class);
     }
 
-    /**
-     * Returns TunnelZone object
-     *
-     * @param id ID of tunnel zone
-     * @return TunnelZone
-     */
     public TunnelZone getTunnelZone(UUID id) {
         URI uri = createUriFromTemplate(
                 principalDto.getTunnelZoneTemplate(), ID_TOKEN,
                 id);
         DtoTunnelZone tunnelZone = resource.get(uri, null, DtoTunnelZone.class,
-                VendorMediaType.APPLICATION_TUNNEL_ZONE_JSON);
+                APPLICATION_TUNNEL_ZONE_JSON);
         return new TunnelZone(resource, null, tunnelZone,
-                VendorMediaType.APPLICATION_TUNNEL_ZONE_HOST_JSON,
-                VendorMediaType.APPLICATION_TUNNEL_ZONE_HOST_COLLECTION_JSON);
+                APPLICATION_TUNNEL_ZONE_HOST_JSON,
+                APPLICATION_TUNNEL_ZONE_HOST_COLLECTION_JSON);
     }
 }
