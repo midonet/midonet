cde2ad2b0912de20debcb363ca75ed29a7cc23c9
YAMAMOTO Takashi <yamamoto@midokura.com>
2015-07-13 18:09:42 +0900

    Metadata proxy server embedded in midolman
    This commit introduces MidoNet Instance Metadata Proxy for OpenStack
    Integration.  It lives inside the midolman process and proxies metadata
    requests from VMs to Nova Metadata Api.  It basically replaces metadata
    proxy provided by Neutron and thus allows deployments without Neutron
    DHCP agent.
    
    This introduces the following configuration knobs you can tweak with
    mn-conf.  You need to restart midolman to reload these configurations.
    
        mn-conf key  (default value)
    
        agent.openstack.metadata.enabled  (false)
        agent.openstack.metadata.nova_metadata_url  (http://localhost:8775)
        agent.openstack.metadata.shared_secret  (empty string)
    
    If enabled, this uses 169.254/64 link-local addresses on the hypervisor.
    Also, it listens on 169.254.169.254:9697.  Please make sure that
    these addresses are not used for other purposes on the hypervisor.
    
    To migrate from Neutron metadata proxy to MidoNet metadata proxy,
    - Set agent.openstack.metadata.enabled to true
    - Set agent.openstack.metadata.nova_metadata_url to
      "http://${nova_metadata_ip}:${nova_metadata_port}" where
      nova_metadata_ip and nova_metadata_port are the corresponding
      neutron-metadata-agent configuration.
    - Set agent.openstack.metadata.shared_secret to the same as
      "metadata_proxy_shared_secret" neutron-metadata-agent configuration.
    - Restart midolman on each hypervisors.  After the restart, VMs on
      the hypervisor will be served by MidoNet metadata proxy.
    After migrating all hypervisors, you might want to stop the relevant
    Neutron agents, namely neutron-dhcp-agent and neutron-metadata-agent,
    as they are not necessary anymore.  Also, you might want to clean up
    dhcp ports and namespaces used by neutron-dhcp-agent.  However, note
    that the existance of dhcp ports still has effects on metadata routes
    distributed by MidoNet dhcp.
    
    Ref MNA-474
    
    Signed-off-by: YAMAMOTO Takashi <yamamoto@midokura.com>
    Change-Id: Id24031d01a80f0b10e53ff72963a9447160977c4

---
+++ midolman/src/main/scala/org/midonet/midolman/openstack/metadata/DatapathInterface.scala
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2015 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.midolman.openstack.metadata
+
+import akka.actor.ActorSystem
+import scala.Option.option2Iterable
+import scala.collection.JavaConversions.asJavaIterable
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+import scala.concurrent.ExecutionContext
+import scala.sys.process.Process
+
+import rx.Observable
+import rx.subjects.ReplaySubject
+import org.slf4j.{Logger, LoggerFactory}
+
+import org.midonet.midolman.DatapathState
+import org.midonet.midolman.host.interfaces.InterfaceDescription
+import org.midonet.midolman.host.scanner.InterfaceScanner
+import org.midonet.midolman.io.UpcallDatapathConnectionManager
+import org.midonet.midolman.io.VirtualMachine
+import org.midonet.odp.ports.InternalPort
+import org.midonet.packets.MAC
+import org.midonet.util.functors.makeFunc1
+import org.midonet.util.concurrent.CallingThreadExecutionContext
+
+class DatapathInterface(private val scanner: InterfaceScanner,
+                        private val dpState: DatapathState,
+                        private val dpConnManager:
+                            UpcallDatapathConnectionManager) {
+    private val log: Logger = MetadataService.getLogger
+
+    private def run(command: String) = {
+        if (Process(command).! != 0) {
+            throw new RuntimeException(s"command failed: ${command}")
+        }
+    }
+
+    def init(implicit as: ActorSystem) = {
+        /*
+         * 1. Subscribe InterfaceScanner
+         * 2. Create our port
+         * 3. Wait for InterfaceDescription for the port
+         */
+        implicit val ec = CallingThreadExecutionContext
+        val ifName = "metadata"
+        val obs = ReplaySubject.create[Set[InterfaceDescription]]
+        val subscription = scanner.subscribe(obs)
+        val create = dpConnManager.createAndHookDpPort(dpState.datapath,
+                                                       new InternalPort(ifName),
+                                                       VirtualMachine)
+        val (dpPort, _) = Await.result(create, Duration.Inf)
+        val portObs = obs flatMap makeFunc1 { data =>
+            Observable.from(asJavaIterable(data.collectFirst {
+                case i if i.getName == ifName => i
+            }))
+        }
+        val port = portObs.toBlocking.first
+        subscription.unsubscribe
+        val mdInfo = ProxyInfo(dpPort.getPortNo, MetadataApi.address,
+                               MAC.bytesToString(port.getMac))
+
+        /*
+         * Assign the IP address which our metadata proxy will listen on.
+         * Make the port up.
+         *
+         * REVISIT(yamamoto): better to use rtnetlink
+         */
+        run(s"ip addr add ${MetadataApi.address}/16 dev ${ifName}")
+        run(s"ip link set ${ifName} up")
+
+        log debug s"mdInfo ${mdInfo}"
+        mdInfo
+    }
+}
