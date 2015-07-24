6f1de859c34f47c5579ca2169941c238b5cd3831
Galo Navarro <galo@midokura.com>
2015-07-24 17:12:09 +0200

    Cluster API: support DHCPv6
    Adds the message to the proto, and adds support to the cluster API.
    
    The following response codes are changed in the API:
    - create a host in a dhcpv6 with a mac already used: 500 -> 409
      (conflict)
    - update a host in a dhcpv6: 200 -> 204 (no content)
    
    This is in order to respect consistency with other resources (the first
    one is also more adequate).
    
    Ref.: MNA-577
    Signed-off-by: Galo Navarro <galo@midokura.com>
    
    Change-Id: Ie507b7c845ac3923bbde9a6380bb0421f3f0d51f

---
+++ cluster/midonet-cluster/src/main/scala/org/midonet/cluster/services/rest_api/resources/DhcpV6HostResource.scala
@@ -0,0 +1,136 @@
+/*
+ * Copyright 2015 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.cluster.services.rest_api.resources
+
+import java.util.{List => JList, UUID}
+import javax.ws.rs._
+import javax.ws.rs.core.MediaType.APPLICATION_JSON
+import javax.ws.rs.core.Response
+import javax.ws.rs.core.Response.Status
+
+import scala.collection.JavaConverters._
+import scala.concurrent.Future
+
+import com.google.inject.Inject
+import com.google.inject.servlet.RequestScoped
+
+import org.midonet.cluster.rest_api.models.{Bridge, DhcpSubnet6, DhcpV6Host}
+import org.midonet.cluster.services.rest_api.MidonetMediaTypes._
+import org.midonet.cluster.services.rest_api.resources.MidonetResource.ResourceContext
+import org.midonet.packets.IPv6Subnet
+
+@RequestScoped
+class DhcpV6HostResource @Inject()(bridgeId: UUID, subnetAddress: IPv6Subnet,
+                                  resContext: ResourceContext)
+    extends MidonetResource[DhcpV6Host](resContext) {
+
+    @GET
+    @Path("/{client_id}")
+    @Produces(Array(APPLICATION_DHCPV6_HOST_JSON,
+                    APPLICATION_JSON))
+    override def get(@PathParam("client_id") clientId: String,
+                     @HeaderParam("Accept") accept: String): DhcpV6Host = {
+        val reqClientId = DhcpV6Host.clientIdFromUri(clientId)
+        getSubnet(subnetAddress).map(_.flatMap(subnet => {
+            val host = subnet.dhcpHosts.asScala.find (_.clientId == reqClientId)
+            host.foreach(_.setBaseUri(subnet.getUri))
+            host
+        }))
+            .getOrThrow
+            .getOrElse(throw new WebApplicationException(Status.NOT_FOUND))
+    }
+
+    @GET
+    @Produces(Array(APPLICATION_DHCPV6_HOST_COLLECTION_JSON))
+    override def list(@HeaderParam("Accept") accept: String)
+    : JList[DhcpV6Host] = {
+        getSubnet(subnetAddress).map(_.map(subnet => {
+            val hosts = subnet.dhcpHosts.asScala
+            hosts.foreach(_.setBaseUri(subnet.getUri))
+            hosts.asJava
+        }))
+            .getOrThrow
+            .getOrElse(throw new WebApplicationException(Status.NOT_FOUND))
+    }
+
+    @POST
+    @Consumes(Array(APPLICATION_DHCPV6_HOST_JSON,
+                    APPLICATION_JSON))
+    override def create(host: DhcpV6Host,
+                        @HeaderParam("Content-Type") contentType: String)
+    : Response = {
+        getSubnet(subnetAddress).map(_.map(subnet => {
+            if (subnet.dhcpHosts.asScala.exists(_.clientId == host.clientId)) {
+                Response.status(Status.CONFLICT).build()
+            } else {
+                host.setBaseUri(subnet.getUri)
+                subnet.dhcpHosts.add(host)
+                updateResource(subnet, Response.created(host.getUri).build())
+            }
+        }))
+            .getOrThrow
+            .getOrElse(Response.status(Status.NOT_FOUND).build())
+    }
+
+    @PUT
+    @Path("/{client_id}")
+    @Consumes(Array(APPLICATION_DHCPV6_HOST_JSON,
+                    APPLICATION_JSON))
+    override def update(@PathParam("client_id") clientId: String,
+                        host: DhcpV6Host,
+                        @HeaderParam("Content-Type") contentType: String)
+    : Response = {
+        val reqClientId = DhcpV6Host.clientIdFromUri(clientId)
+        getSubnet(subnetAddress).map(_.flatMap(subnet => {
+            subnet.dhcpHosts.asScala.find( _.clientId == reqClientId )
+                .map(h => {
+                    val index = subnet.dhcpHosts.indexOf(h)
+                    host.setBaseUri(subnet.getUri)
+                    subnet.dhcpHosts.remove(index)
+                    subnet.dhcpHosts.add(index, host)
+                    updateResource(subnet)
+                })
+        }))
+            .getOrThrow
+            .getOrElse(Response.status(Status.NOT_FOUND).build())
+    }
+
+    @DELETE
+    @Path("/{client_id}")
+    override def delete(@PathParam("client_id") clientId: String): Response = {
+        val reqClientId = DhcpV6Host.clientIdFromUri(clientId)
+        getSubnet(subnetAddress).map(_.flatMap(subnet => {
+            subnet.dhcpHosts.asScala.find( _.clientId == reqClientId)
+                                    .map(h => {
+                                        subnet.dhcpHosts.remove(h)
+                                        updateResource(subnet)
+                                    })
+
+        }))
+            .getOrThrow
+            .getOrElse(Response.status(Status.NOT_FOUND).build())
+    }
+
+    private def getSubnet(subnetAddress: IPv6Subnet)
+    : Future[Option[DhcpSubnet6]] = {
+        getResource(classOf[Bridge], bridgeId)
+            .flatMap(bridge => listResources(classOf[DhcpSubnet6],
+                                             bridge.dhcpv6Ids.asScala))
+            .map(_.find(_.subnetAddress == subnetAddress))
+    }
+
+}
--- midonet-client/src/main/java/org/midonet/client/resource/DhcpSubnet.java
+++ midonet-client/src/main/java/org/midonet/client/resource/DhcpSubnet.java
@@ -32,172 +32,79 @@ public class DhcpSubnet extends ResourceBase<DhcpSubnet, DtoDhcpSubnet> {
             .APPLICATION_DHCP_SUBNET_JSON);
     }
 
-    /**
-     * Gets URI for this DhcpSubnet
-     *
-     * @return
-     */
     @Override
     public URI getUri() {
         return principalDto.getUri();
     }
 
-    /**
-     * Gets prefix for this subnet.
-     *
-     * @return
-     */
     public String getSubnetPrefix() {
         return principalDto.getSubnetPrefix();
     }
 
-    /**
-     * Gets default gateway for this subnet.
-     *
-     * @return
-     */
     public String getDefaultGateway() {
         return principalDto.getDefaultGateway();
     }
 
-    /**
-     * Gets opt121Routes.
-     *
-     * @return
-     */
     public List<DtoDhcpOption121> getOpt121Routes() {
         return principalDto.getOpt121Routes();
     }
 
-    /**
-     * Gets DHCP server IP.
-     *
-     * @return
-     */
     public String getServerAddr() {
         return principalDto.getServerAddr();
     }
 
-    /**
-     * Gets DNS server IP.
-     *
-     * @return
-     */
     public List<String> getDnsServerAddrs() {
         return principalDto.getDnsServerAddrs();
     }
 
-    /**
-     * Gets interface MTU.
-     *
-     * @return
-     */
     public int getInterfaceMTU() {
         return principalDto.getInterfaceMTU();
     }
 
-    /**
-     * Gets length of the subnet address.
-     *
-     * @return
-     */
     public int getSubnetLength() {
         return principalDto.getSubnetLength();
     }
 
-    /**
-     * Sets default gateway.
-     *
-     * @param defaultGateway
-     * @return this
-     */
     public DhcpSubnet defaultGateway(String defaultGateway) {
         principalDto.setDefaultGateway(defaultGateway);
         return this;
     }
 
-    /**
-     * Sets host.
-     *
-     * @param hosts
-     * @return this
-     */
     public DhcpSubnet hosts(URI hosts) {
         principalDto.setHosts(hosts);
         return this;
     }
 
-    /**
-     * Sets opt121Routes.
-     *
-     * @param opt121Routes
-     * @return this
-     */
     public DhcpSubnet opt121Routes(List<DtoDhcpOption121> opt121Routes) {
         principalDto.setOpt121Routes(opt121Routes);
         return this;
     }
 
-    /**
-     * Sets SubnetLength.
-     *
-     * @param subnetLength
-     * @return this
-     */
     public DhcpSubnet subnetLength(int subnetLength) {
         principalDto.setSubnetLength(subnetLength);
         return this;
     }
 
-    /**
-     * Sets prefix of the subnet.
-     *
-     * @param subnetPrefix
-     * @return this.
-     */
     public DhcpSubnet subnetPrefix(String subnetPrefix) {
         principalDto.setSubnetPrefix(subnetPrefix);
         return this;
     }
 
-    /**
-     * Sets DHCP Server IP.
-     *
-     * @param serverAddr
-     * @return this
-     */
     public DhcpSubnet serverAddr(String serverAddr) {
         principalDto.setServerAddr(serverAddr);
         return this;
     }
 
-    /**
-     * Sets DNS Server IP.
-     *
-     * @param dnsServerAddr
-     * @return this
-     */
     public DhcpSubnet dnsServerAddrs(List<String> dnsServerAddr) {
         principalDto.setDnsServerAddrs(dnsServerAddr);
         return this;
     }
 
-    /**
-     * Sets Interface MTU.
-     *
-     * @param interfaceMTU
-     * @return this
-     */
     public DhcpSubnet interfaceMTU(int interfaceMTU) {
         principalDto.setInterfaceMTU(interfaceMTU);
         return this;
     }
 
-    /**
-     * Gets host resources under this subnet.
-     *
-     * @return
-     */
     public ResourceCollection<DhcpHost> getDhcpHosts() {
         return getChildResources(
             principalDto.getHosts(),
@@ -207,11 +114,6 @@ public class DhcpSubnet extends ResourceBase<DhcpSubnet, DtoDhcpSubnet> {
             DtoDhcpHost.class);
     }
 
-    /**
-     * Adds subnet host resource under this subnet.
-     *
-     * @return new DhcpHost()
-     */
     public DhcpHost addDhcpHost() {
         return new DhcpHost(resource, principalDto.getHosts(),
                               new DtoDhcpHost());
