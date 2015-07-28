b44353a4804bcbee2a30172fcadefaaddb4b4411
Guillermo Ontanon <guillermo@midokura.jp>
2015-07-28 19:30:50 +0200

    Simulation: move coordinator logic into devices
    This lets port types have their own behaviour.
    
    Change-Id: Ia0efab7f46237082f71a359f0bdb4daa26f45794
    Signed-off-by: Guillermo Ontanon <guillermo@midokura.jp>

--- midolman/src/main/scala/org/midonet/midolman/PacketWorkflow.scala
+++ midolman/src/main/scala/org/midonet/midolman/PacketWorkflow.scala
@@ -437,9 +437,6 @@ class PacketWorkflow(
             handlePacketEgress(context)
     }
 
-    def runSimulation(context: PacketContext): SimulationResult =
-        new Coordinator(context).simulate()
-
     protected def addTranslatedFlow(context: PacketContext,
                                     expiration: Expiration): SimulationResult =
         if (context.packet.getReason == Packet.Reason.FlowActionUserspace) {
@@ -492,7 +489,7 @@ class PacketWorkflow(
 
     private def handlePacketEgress(context: PacketContext) = {
         context.log.debug("Handling generated packet")
-        processSimulationResult(context, runSimulation(context))
+        processSimulationResult(context, Simulator.simulate(context))
     }
 
     def processSimulationResult(context: PacketContext,
@@ -550,7 +547,7 @@ class PacketWorkflow(
         if (handleDHCP(context)) {
             NoOp
         } else {
-            runSimulation(context)
+            Simulator.simulate(context)
         }
 
     protected def handleStateMessage(context: PacketContext): Unit = {
