d76129d082ba32775ee4204bdf48ad1c51d9043a
Galo Navarro <galo@midokura.com>
2015-07-28 15:02:14 +0200

    Flooding Proxy calculator
    This patch implements a Flooding Proxy calculator that remains agnostic
    of the means to trigger the calculation.  This is part of the incoming
    work to implement VxgW on V2.
    
    Ref.: MNA-127
    Signed-off-by: Galo Navarro <galo@midokura.com>
    
    Change-Id: I1f6d1987728aa51ee4848793430f1da91602db19

---
+++ cluster/midonet-cluster/src/main/scala/org/midonet/cluster/services/vxgw/FloodingProxyCalculator.scala
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2015 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.cluster.services.vxgw
+
+import java.util.UUID
+
+import org.midonet.cluster.models.Topology.Host
+import org.midonet.cluster.util.UUIDUtil
+
+/** This class encapsulates the algorithm to generate a Flooding Proxy for a
+  * given tunnel zone.
+  */
+object FloodingProxyCalculator {
+
+    private val random = new java.util.Random
+
+    def calculate(candidates: Seq[Host]): Option[UUID] = {
+        val eligible = candidates.filterNot(h => h.getFloodingProxyWeight < 0)
+        if (eligible.isEmpty) {
+            return None
+        }
+        // Sort the candidate proxies: provides a deterministic
+        // behavior for unit testing (not necessary in practice).
+        val sorted = eligible.sortWith { (a, b) =>
+            a.getFloodingProxyWeight > b.getFloodingProxyWeight
+        }
+        val sumWeight = sorted.foldLeft(0)((seed, h) =>
+                                               seed + h.getFloodingProxyWeight)
+        val randomWeight = random.nextInt(sumWeight)
+        var index = 0
+        var sum = 0
+        var candidate: Host = null
+        while (index < sorted.length && sum < randomWeight) {
+            candidate = sorted(index)
+            sum += candidate.getFloodingProxyWeight
+            index = index +1
+        }
+        Option(UUIDUtil.fromProto(candidate.getId))
+    }
+
+}
