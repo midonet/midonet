ecd43f8219ffedff2046031977671c7a0d8d0126
Galo Navarro <galo@midokura.com>
2015-07-29 09:56:33 +0200

    VxGW v2: Flooding Proxy management
    This patch provides a reimplementation of the flooding proxy management
    code, adapted to the new storage stack.  The fp is now published as a
    Tunnel Zone key using the state storage.
    
    The patch introduces a FloodingProxyHerald that can be used outside of
    the vxgw service to retrieve the flooding proxies for different tunnel
    zones.
    
    Change-Id: Ia4fd7df88f9a9223a1cfbe5f03309f1833030ab1
    Signed-off-by: Galo Navarro <galo@midokura.com>

---
+++ cluster/midonet-cluster/src/main/scala/org/midonet/cluster/services/vxgw/WritableFloodingProxyHerald.scala
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2015 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.cluster.services.vxgw
+
+import java.util.UUID
+import java.util.concurrent.Executor
+
+import rx.Observable
+
+import org.midonet.cluster.data.storage.StateResult
+import org.midonet.cluster.models.Topology.TunnelZone
+import org.midonet.cluster.services.MidonetBackend
+import org.midonet.cluster.services.MidonetBackend._
+import org.midonet.cluster.services.vxgw.FloodingProxyHerald.FloodingProxy
+import org.midonet.util.reactivex
+
+/**
+ * This class is responsible for keeping a register of the current flooding
+ * proxy for each tunnel zone, and announcing it via the NSDB.
+ */
+class WritableFloodingProxyHerald(backend: MidonetBackend, executor: Executor)
+    extends FloodingProxyHerald(backend, executor) {
+
+    /** Asynchronously publish the given hostId as Flooding Proxy for the given
+      * tunnel zone.
+      */
+    private[vxgw] def announce(tzId: UUID, fp: FloodingProxy,
+                               onError: Throwable => Unit): Unit = (
+        if (fp == null) removal(tzId)
+        else update(tzId, fp)
+    ).subscribe(reactivex.retryObserver[StateResult](onError))
+
+    /** Asynchronously publishes a removal of the flooding proxy, emitting the
+      * result on the observable when the value is safely stored in the NSDB.
+      * If the write fails, the Observable will emit an onError.
+      */
+    private def removal(tzId: UUID)
+    : Observable[StateResult] = {
+        backend.stateStore.removeValue(classOf[TunnelZone], tzId,
+                                       TunnelZoneKey, null /* ignored */)
+    }
+
+    /** Asynchronously publishes a new flooding proxy, emitting the result on
+      * the observable when the value is safely stored in the NSDB.  If the
+      * write fails, the Observable will emit an onError.
+      */
+    private def update(tzId: UUID, fp: FloodingProxy)
+    : Observable[StateResult] = {
+        backend.stateStore
+               .addValue(classOf[TunnelZone], tzId, TunnelZoneKey, fp.toString)
+    }
+}
