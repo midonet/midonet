a4380c90ce7ba9f634cab634c2cd70577045a2f3
Duarte Nunes <duarte@midokura.com>
2015-07-29 17:05:00 +0100

    MidoNet: Update to JDK 1.8
    Change-Id: Ic8de2ad1d865be00fdce887b84416a96ec1909be
    Signed-off-by: Duarte Nunes <duarte@midokura.com>

--- netlink/src/main/java/org/midonet/netlink/UnixChannel.java
+++ netlink/src/main/java/org/midonet/netlink/UnixChannel.java
@@ -24,12 +24,17 @@ import java.nio.channels.spi.SelectorProvider;
 import javax.annotation.Nullable;
 
 import com.sun.jna.Native;
+import sun.nio.ch.IOStatus;
+import sun.nio.ch.NativeThread;
+import sun.nio.ch.SelectionKeyImpl;
+import sun.nio.ch.Net;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import sun.nio.ch.SelectionKeyImpl;
 
+import org.midonet.netlink.hacks.IOUtil;
+import org.midonet.netlink.hacks.NativeDispatcher;
 import org.midonet.util.cLibrary;
-import org.midonet.netlink.hacks.*;
 
 /**
  * Abstracts a netlink channel. The implementation will make a native netlink
@@ -302,20 +307,19 @@ public abstract class UnixChannel<Address> extends AbstractSelectableChannel
         int newOps = 0;
 
         if ((ops & SelectionKey.OP_ACCEPT) != 0)
-            newOps |= PollArrayWrapper.POLLIN;
+            newOps |= Net.POLLIN;
         if ((ops & SelectionKey.OP_READ) != 0)
-            newOps |= PollArrayWrapper.POLLIN;
+            newOps |= Net.POLLIN;
         if ((ops & SelectionKey.OP_WRITE) != 0)
-            newOps |= PollArrayWrapper.POLLOUT;
+            newOps |= Net.POLLOUT;
         if ((ops & SelectionKey.OP_CONNECT) != 0)
-            newOps |= PollArrayWrapper.POLLIN;
+            newOps |= Net.POLLIN;
 
-        SelectorCaller.putEventOps(sk.selector(), sk, newOps);
+        sk.selector.putEventOps(sk, newOps);
     }
 
-
     public boolean translateAndUpdateReadyOps(int ops, SelectionKeyImpl sk) {
-        int initialOps = SelectionKeyImplCaller.nioReadyOps(sk);
+        int initialOps = sk.nioReadyOps();
         return translateReadyOps(ops, initialOps, sk);
     }
 
@@ -328,41 +332,40 @@ public abstract class UnixChannel<Address> extends AbstractSelectableChannel
      */
     private boolean translateReadyOps(int ops, int initialOps,
                                      SelectionKeyImpl sk) {
-        int intOps = SelectionKeyImplCaller.nioInterestOps(sk);
-        int oldOps = SelectionKeyImplCaller.nioReadyOps(sk);
+        int intOps = sk.nioInterestOps();
+        int oldOps = sk.nioReadyOps();
         int newOps = initialOps;
 
-        if ((ops & PollArrayWrapper.POLLNVAL) != 0) {
+        if ((ops & Net.POLLNVAL) != 0) {
             // This should only happen if this channel is pre-closed while a
             // selection operation is in progress
             // ## Throw an error if this channel has not been pre-closed
             return false;
         }
 
-        if ((ops & (PollArrayWrapper.POLLERR
-            | PollArrayWrapper.POLLHUP)) != 0) {
+        if ((ops & (Net.POLLERR | Net.POLLHUP)) != 0) {
             newOps = intOps;
-            SelectionKeyImplCaller.nioReadyOps(sk, newOps);
+            sk.nioReadyOps(newOps);
             return (newOps & ~oldOps) != 0;
         }
 
-        if (((ops & PollArrayWrapper.POLLIN) != 0) &&
+        if (((ops & Net.POLLIN) != 0) &&
             ((intOps & SelectionKey.OP_READ) != 0))
             newOps |= SelectionKey.OP_READ;
 
-        if (((ops & PollArrayWrapper.POLLIN) != 0) &&
+        if (((ops & Net.POLLIN) != 0) &&
                 ((intOps & SelectionKey.OP_ACCEPT) != 0))
             newOps |= SelectionKey.OP_ACCEPT;
 
-        if (((ops & PollArrayWrapper.POLLOUT) != 0) &&
+        if (((ops & Net.POLLOUT) != 0) &&
             ((intOps & SelectionKey.OP_WRITE) != 0))
             newOps |= SelectionKey.OP_WRITE;
 
-        if (((ops & PollArrayWrapper.POLLHUP) != 0) &&
+        if (((ops & Net.POLLHUP) != 0) &&
                 ((intOps & SelectionKey.OP_WRITE) != 0))
             newOps |= SelectionKey.OP_WRITE;
 
-        SelectionKeyImplCaller.nioReadyOps(sk, newOps);
+        sk.nioReadyOps(newOps);
         return (newOps & ~oldOps) != 0;
     }
 
