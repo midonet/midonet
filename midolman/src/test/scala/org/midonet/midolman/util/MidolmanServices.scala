62d789f188ac4a4d9f897b0b35db39578238abd1
Duarte Nunes <duarte@midokura.com>
2015-08-04 18:19:19 +0100

    Agent: Propagate flow state to the FloodingProxies
    This patch ensure we propagate flow state to any existing
    FloodingProxy when we're outputting state from a Vtep connected port.
    This is required because return traffic comes from the FloodingProxy
    host, which may not have been participating in the simulation of the
    forward packet.
    
    Ref MN-3309
    
    Change-Id: I8869354c19e73b199ff38048333e6e383f3e2e18
    Signed-off-by: Duarte Nunes <duarte@midokura.com>

--- midolman/src/test/scala/org/midonet/midolman/util/MidolmanServices.scala
+++ midolman/src/test/scala/org/midonet/midolman/util/MidolmanServices.scala
@@ -18,12 +18,15 @@ package org.midonet.midolman.util
 
 import java.util.UUID
 
-import scala.concurrent.ExecutionContext
-import scala.reflect.ClassTag
-
 import akka.actor.ActorSystem
+import org.midonet.cluster.state.LegacyStorage
+
+import scala.concurrent.ExecutionContext
+
 import com.codahale.metrics.{MetricFilter, MetricRegistry}
 import com.google.inject.Injector
+import org.midonet.midolman.state.PeerResolver
+
 import com.typesafe.scalalogging.Logger
 import org.slf4j.helpers.NOPLogger
 
@@ -51,6 +54,9 @@ trait MidolmanServices {
 
     def config =
         injector.getInstance(classOf[MidolmanConfig])
+
+    def peerResolver =
+        injector.getInstance(classOf[PeerResolver])
 
     def virtConfBuilderImpl =
         injector.getInstance(classOf[VirtualConfigurationBuilders])
