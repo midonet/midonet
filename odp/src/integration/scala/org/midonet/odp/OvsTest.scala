a4de5d913454f2a43ad1bd58ce11c7e4c5b5a37e
Duarte Nunes <duarte@midokura.com>
2015-08-10 22:20:45 +0100

    Agent: Fix OvsTest
    This patch fixes a race in OvsTest causing a test to fail.
    
    Ref MNA-724
    
    Change-Id: I30b2a8980f3b6f4af68ebdd2bf900298b43069b8
    Signed-off-by: Duarte Nunes <duarte@midokura.com>

--- odp/src/integration/scala/org/midonet/odp/OvsTest.scala
+++ odp/src/integration/scala/org/midonet/odp/OvsTest.scala
@@ -15,9 +15,8 @@
 package org.midonet.odp
 
 import scala.concurrent.duration._
-import scala.concurrent.{Future, Await}
+import scala.concurrent.Await
 import scala.sys.process._
-import scala.util.Try
 
 import org.junit.runner.RunWith
 import org.scalatest._
@@ -27,17 +26,12 @@ import org.midonet.odp.protos.OvsDatapathConnection
 import org.midonet.odp.test.{DatapathTest => DpTest, PortTest, MegaFlowTest, FlowTest}
 import org.midonet.util.IntegrationTests
 
-
 @RunWith(classOf[JUnitRunner])
 class OvsTest extends FeatureSpec with BeforeAndAfterAll with ShouldMatchers with
         DpTest with FlowTest with MegaFlowTest with PortTest {
 
-    import org.midonet.util.IntegrationTests._
-
     var baseConnection: OvsDatapathConnection = _
     var con: OvsConnectionOps = _
-
-    var datapaths: List[String] = Nil
 
     def beforeTests() {
         "./src/main/resources/ovs_integration_create.sh".!
@@ -46,45 +40,24 @@ class OvsTest extends FeatureSpec with BeforeAndAfterAll with ShouldMatchers wit
     }
 
     override def afterAll() {
-        for (dpName <- datapaths) {
-            Try(Await.result(con delDp dpName, 1 second))
-        }
-
         baseConnection.getChannel.close()
         "./src/main/resources/ovs_integration_del.sh".!
     }
 
-    private def cleanup(dps: Seq[(String, Future[Datapath])]): Unit = {
-        for ((desc, dp) <- dps) {
-            Await.result(dp, 2 seconds)
-        }
-    }
-
-    def checkResults(dpName: String, test: (Future[Datapath]) => IntegrationTests.TestSuite): Unit = {
-        val dpF = con createDp dpName
-        datapaths ::= dpName
-        for ((desc, result) <- test(dpF)) {
-            scenario(desc) {
-                Await.result(result, 2 seconds)
-            }
-        }
-    }
-
-    def checkResults(ts: IntegrationTests.TestSuite): Unit = {
+    def checkResults(ts: IntegrationTests.TestSuite): Unit =
         for ((desc, result) <- ts) {
             scenario(desc) {
                 Await.result(result, 2 seconds)
             }
         }
-    }
 
     feature("OVS integration") {
         beforeTests()
-        checkResults("portTests", dpPortTests)
-        checkResults("flowTests", flowTests)
-        checkResults("megaflowTests", wflowTests)
-        val (_, cleanupTests, dpTests) = datapathTests()
+        val (dpF, cleanupTests, dpTests) = datapathTests()
         checkResults(dpTests)
+        checkResults(dpPortTests(dpF))
+        checkResults(flowTests(dpF))
+        checkResults(wflowTests(dpF))
         checkResults(dpCleanup(cleanupTests))
     }
 }
