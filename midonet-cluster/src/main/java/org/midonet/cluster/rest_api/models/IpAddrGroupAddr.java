a6d2f29d7729e98360bd57e4a4188efed695e6a7
Brandon Berg <bberg@midokura.com>
2015-08-18 19:26:37 +0900

    Midonet Topology migrator (1.9 -> 2.0)
    Migrator for low-level Midonet topology (ignores Neutron) from 1.9 to
    2.0. This is a standalone tool that only requires access to Zookeeper;
    the Midonet API and Agent don't need to be running.
    
    Migration of BGP- and VTEP-related topology objects is NYI, and
    migration of L4LB topology objects is untested. Testing in general is
    fairly light so far, so there may be some issues in other areas as
    well.
    
    Signed-off-by: Brandon Berg <bberg@midokura.com>
    Fixes: MNA-199
    Change-Id: I46b082130673d6c7135942400071fc919da67956

--- midonet-cluster/src/main/java/org/midonet/cluster/rest_api/models/IpAddrGroupAddr.java
+++ midonet-cluster/src/main/java/org/midonet/cluster/rest_api/models/IpAddrGroupAddr.java
@@ -27,6 +27,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.google.common.base.MoreObjects;
 
 import org.midonet.cluster.rest_api.ResourceUris;
 import org.midonet.packets.IPv4;
@@ -78,5 +79,14 @@ public abstract class IpAddrGroupAddr extends UriResource {
 
     public URI getIpAddrGroup() {
         return absoluteUri(ResourceUris.IP_ADDR_GROUPS, ipAddrGroupId);
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+            .omitNullValues()
+            .add("addr", addr)
+            .add("ipAddrGroupId", ipAddrGroupId)
+            .toString();
     }
 }
