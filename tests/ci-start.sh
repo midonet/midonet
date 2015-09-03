ea3aec36ff3c227ebfa6abe68045d742e999b4ae
Guillermo Ontanon <guillermo@midokura.jp>
2015-09-03 15:49:49 +0200

    Packaging: introduce midonet-tools
    Several changes come with it:
    
      * Jar dpeendencies are bundled in a fat jar for mm and -tools. Each package
        brings his own jar.
      * Jar lose the -version suffix.
      * mn-conf, mm-meter, mm-stat moved over to midonet-tools. On which midolman
        depends
      * Cleaned up and removed redundant dependencies all over the place.
    
    Change-Id: Idb4f68ca22a9dae5439ff0566ec1df2db3946bf8
    Signed-off-by: Guillermo Ontanon <guillermo@midokura.jp>

--- tests/ci-start.sh
+++ tests/ci-start.sh
@@ -6,7 +6,9 @@ sudo pip install -r tests/mdts.dependencies
 # We assume all gates/nightlies put the necessary packages in $WORKSPACE
 # so we know where to find them.
 cp midolman*.deb tests/sandbox/override_v2/midolman
+cp midonet-tools*.deb tests/sandbox/override_v2/midolman
 cp midonet-cluster*.deb tests/sandbox/override_v2/cluster
+cp midonet-tools*.deb tests/sandbox/override_v2/cluster
 cp python-midonetclient*.deb tests/sandbox/override_v2/cluster
 
 # Necessary software in the host, we assume packages are already present on
