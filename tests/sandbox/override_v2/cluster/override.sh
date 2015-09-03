bce45533948bca75e8238b4bb970940fa6821e29
Guillermo Ontanon <guillermo@midokura.jp>
2015-09-03 15:49:49 +0200

    Packaging: introduce midonet-tools
    Several changes come with it:
    
      * Jar dpeendencies are bundled in a fat jar for mm and -tools. Each package
        brings his own jar.
      * Jar lose the -version suffix.
      * mn-conf, mm-meter, mm-stat moved over to midonet-tools. On which midolman
        depends
      * Cleaned up and removed redundant dependencies all over the place.
    
    Change-Id: Idb4f68ca22a9dae5439ff0566ec1df2db3946bf8
    Signed-off-by: Guillermo Ontanon <guillermo@midokura.jp>

--- tests/sandbox/override_v2/cluster/override.sh
+++ tests/sandbox/override_v2/cluster/override.sh
@@ -3,7 +3,11 @@
 # Installs newest package (lexicographycally) in override
 LATEST_CLIENT=$(ls override/python-midonetclient*deb | tail -n1)
 LATEST_CLUSTER=$(ls override/midonet-cluster*deb | tail -n1)
-dpkg -i --force-confnew $LATEST_CLIENT $LATEST_CLUSTER
+LATEST_TOOLS=$(ls override/midonet-tools*deb | tail -n1)
+dpkg -r midonet-cluster
+dpkg -r midonet-tools
+dpkg -r python-midonetclient
+dpkg -i --force-confnew $LATEST_CLIENT $LATEST_CLUSTER $LATEST_TOOLS
 
 # TODO: remove this once the midonet-cluster points to port 8080 by default
 # Update midonet-cli to point to cluster
