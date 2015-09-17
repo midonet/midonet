ab5c65bff77885bb41ecb8ad7497bdefb97c0162
Duarte Nunes <duarte@midokura.com>
2015-09-17 19:07:00 +0200

    Agent: Don't write to Zookeeper using the old stack
    This patch ensures that the agent doesn't create the legacy storage
    tree in Zookeeper. It also does some additional cleanup.
    
    Ref MNA-859
    
    Change-Id: I4a46322c41ca1ae9d448bb7c77b5bd2574df3929
    Signed-off-by: Duarte Nunes <duarte@midokura.com>

--- midolman/src/main/java/org/midonet/midolman/SystemDataProvider.java
+++ midolman/src/main/java/org/midonet/midolman/SystemDataProvider.java
@@ -17,40 +17,9 @@ package org.midonet.midolman;
 
 import org.midonet.midolman.state.StateAccessException;
 
-import java.util.List;
-
 /**
  * This interface defines methods to provide system data information.
  */
 public interface SystemDataProvider {
-
-    public String getWriteVersion()
-            throws StateAccessException;
-
-    public boolean writeVersionExists()
-            throws StateAccessException;
-
-    public void setWriteVersion(String version)
-            throws StateAccessException;
-
-    public boolean systemUpgradeStateExists()
-            throws StateAccessException;
-
-    public void setOperationState(String state)
-            throws StateAccessException;
-
-    public void setConfigState(String state)
-            throws StateAccessException;
-
-    public boolean configReadOnly()
-            throws StateAccessException;
-
-    public boolean isBeforeWriteVersion(String version)
-            throws StateAccessException;
-
-    public List<String> getVersionsInDeployment()
-            throws StateAccessException;
-
-    public List<String> getHostsWithVersion(String version)
-            throws StateAccessException;
+    String getWriteVersion() throws StateAccessException;
 }
