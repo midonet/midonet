b1713d18f2c645a15a3bdbd04502f1792f1a794f
Duarte Nunes <duarte@midokura.com>
2015-09-17 19:07:00 +0200

    Agent: Don't write to Zookeeper using the old stack
    This patch ensures that the agent doesn't create the legacy storage
    tree in Zookeeper. It also does some additional cleanup.
    
    Ref MNA-859
    
    Change-Id: I4a46322c41ca1ae9d448bb7c77b5bd2574df3929
    Signed-off-by: Duarte Nunes <duarte@midokura.com>

--- midolman/src/main/java/org/midonet/midolman/state/ZkSystemDataProvider.java
+++ midolman/src/main/java/org/midonet/midolman/state/ZkSystemDataProvider.java
@@ -15,19 +15,14 @@
  */
 package org.midonet.midolman.state;
 
-import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.List;
-import java.util.Set;
 import java.util.concurrent.atomic.AtomicReference;
 
 import com.google.inject.Inject;
-import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.KeeperException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.midonet.cluster.data.SystemState;
 import org.midonet.midolman.SystemDataProvider;
 import org.midonet.midolman.cluster.serialization.VerCheck;
 
@@ -55,70 +50,6 @@ public class ZkSystemDataProvider implements SystemDataProvider {
         this.comparator = comparator;
     }
 
-    @Override
-    public boolean systemUpgradeStateExists() throws StateAccessException {
-        String systemStateUpgradePath = paths.getSystemStateUpgradePath();
-        return zk.exists(systemStateUpgradePath);
-    }
-
-    @Override
-    public boolean writeVersionExists() throws StateAccessException {
-        String writeVersionPath = paths.getWriteVersionPath();
-        if (zk.exists(writeVersionPath)) {
-            return (zk.get(writeVersionPath) != null);
-        } else {
-            return false;
-        }
-    }
-
-    @Override
-    public void setWriteVersion(String version) throws StateAccessException {
-        String writeVersionPath = paths.getWriteVersionPath();
-        zk.update(writeVersionPath, version.getBytes());
-        cachedWriteVersion.set(version);
-    }
-
-    @Override
-    public void setOperationState(String state) throws StateAccessException {
-        if (state.equals(SystemState.State.UPGRADE.toString())) {
-            try {
-                zk.add(paths.getSystemStateUpgradePath(), null, CreateMode.PERSISTENT);
-            } catch (StatePathExistsException e) {
-                // Do nothing. We won't treat this as an error.
-            }
-        } else if (state.equals(SystemState.State.ACTIVE.toString())) {
-            try {
-                zk.delete(paths.getSystemStateUpgradePath());
-            } catch (NoStatePathException e) {
-                // Do nothing. We won't treat this as an error.
-            }
-        }
-    }
-
-    @Override
-    public void setConfigState(String state) throws StateAccessException {
-        if (state.equals(SystemState.Availability.READONLY.toString())) {
-            try {
-                zk.add(paths.getConfigReadOnlyPath(), null, CreateMode.PERSISTENT);
-            } catch (StatePathExistsException e) {
-                // Do nothing. We won't treat this as an error.
-            }
-        } else if (state.equals((SystemState.Availability.READWRITE.toString()))) {
-            try {
-                zk.delete(paths.getConfigReadOnlyPath());
-            } catch (NoStatePathException e) {
-                // Do nothing. We won't treat this as an error.
-            }
-        }
-    }
-
-    @Override
-    public boolean configReadOnly()
-        throws StateAccessException {
-        String configReadOnlyPath = paths.getConfigReadOnlyPath();
-        return zk.exists(configReadOnlyPath);
-    }
-
     /**
      * Get the current write version in a plain string.
      *
@@ -140,39 +71,6 @@ public class ZkSystemDataProvider implements SystemDataProvider {
         log.trace("Exiting ZkSystemDataProvider.getWriteVersion. " +
                 "Version={}", version);
         return version;
-    }
-
-    /**
-     * Checks if the given version is before the write version.
-     *
-     * @param version Version to check
-     * @return True if the version is before the write version
-     * @throws StateAccessException
-     */
-    @Override
-    public boolean isBeforeWriteVersion(String version)
-            throws StateAccessException {
-        return (comparator.compare(version, this.getWriteVersion()) < 0);
-    }
-
-    public List<String> getVersionsInDeployment()
-            throws StateAccessException {
-        List<String> versionList = new ArrayList<String>();
-        Set<String> versionSet = zk.getChildren(paths.getVersionsPath());
-        for (String version : versionSet) {
-            versionList.add(version);
-        }
-        return versionList;
-    }
-
-    public List<String> getHostsWithVersion(String version)
-            throws StateAccessException {
-        List<String> hosts = new ArrayList<String>();
-        Set<String> hostSet = zk.getChildren(paths.getVersionPath(version));
-        for (String host : hostSet) {
-            hosts.add(host);
-        }
-        return hosts;
     }
 
     class WriteVersionWatcher extends Directory.DefaultTypedWatcher
@@ -204,9 +102,7 @@ public class ZkSystemDataProvider implements SystemDataProvider {
         }
 
         public void run() {
-            int retries = DEFAULT_RETRIES;
             readWriteVersion();
         }
     }
-
 }
