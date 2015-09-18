87479dce0a536a862d1e183a4608e8135ab3dc75
Duarte Nunes <duarte@midokura.com>
2015-09-19 00:10:04 +0200

    Cluster: Rewrite ZkNatBlockAllocator using Curator
    This patch rewrites the ZkNatBlockAllocator using Curator to eliminate
    dependencies on the old stack.
    
    Ref: MNA-873
    Change-Id: I2c814cb18aa94aee81e0e937f6bf47ea518c4520
    Signed-off-by: Duarte Nunes <duarte@midokura.com>

---
+++ midolman/src/main/scala/org/midonet/midolman/config/MidolmanModule.scala
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2015 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.midolman.config
+
+import com.google.inject.PrivateModule
+import org.midonet.cluster.storage.MidonetBackendConfig
+
+/** This Guice module is dedicated to declare general-purpose dependencies that
+  * are exposed to MidoNet components that need to access the various storage
+  * backends that exist within a deployment.  It should not include any
+  * dependencies linked to any specific service or component. */
+class MidolmanModule(val conf: MidonetBackendConfig)
+    extends PrivateModule {
+
+    override def configure(): Unit = {
+
+    }
+}
