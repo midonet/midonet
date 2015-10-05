fdfc5dfd7478aaac408c02e05896a0820f1f8f9d
Alex Bikfalvi <alex.bikfalvi@midokura.com>
2015-10-05 19:54:02 +0300

    VXGW: MAC updater handles all MAC updates
    This ensures the VTEP MAC updater requests a new update from the
    producer observable after processing an update. It also includes
    some logging cleanup.
    
    Fixes: MNA-924
    
    Change-Id: Ie5b933eed71726b4461654f7c0ede2bdc42ec87f
    Signed-off-by: Alex Bikfalvi <alex.bikfalvi@midokura.com>

--- midolman/src/test/scala/org/midonet/midolman/l4lb/HaproxyTest.scala
+++ midolman/src/test/scala/org/midonet/midolman/l4lb/HaproxyTest.scala
@@ -123,17 +123,11 @@ class HaproxyTest extends TestKit(ActorSystem("HealthMonitorConfigWatcherTest"))
         conf = injector.getInstance(classOf[MidolmanConfig])
         HMSystem.ipCommand = mock(classOf[IP])
 
-        val host = Host.newBuilder
-            .setId(UUIDUtil.toProto(hostId))
-            .build()
-        backend.store.create(host)
-
         hmSystem = TestActorRef(Props(new TestableHealthMonitor()))(actorSystem)
     }
 
     override def afterTest(): Unit = {
         actorSystem.stop(hmSystem)
-        backend.store.delete(classOf[Host], UUIDUtil.toProto(hostId))
     }
 
     feature("Single health monitor on a router") {
