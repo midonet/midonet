bd74bc5b042d5ce9817ace9ccad4ed5e8d384433
Xavi León <xavi.leon@midokura.com>
2015-10-09 15:58:21 +0000

    Gradle: add mdts and sandbox tasks
    Simply add convenient targets to execute sandbox and mdts from the
    root path of the source tree including copying the latest midonet
    packages to the corresponding override.
    
    Change-Id: I352849eae36c255c99bd41c0744f6377840fa6c8
    Signed-off-by: Xavi León <xavi.leon@midokura.com>

---
+++ tests/copy_to_override.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Copyright 2015 Midokura SARL
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Copy the latest packages present on the tree to the override
+# specified by command line.
+# e.g: $ tests/copy_to_override.sh override_v2
+
+if [ $# -ne 1 ]; then
+    echo "Usage: tests/copy_to_override.sh override_v2"
+    exit 1
+fi
+
+MIDOLMAN_PATH=midolman/build/packages
+CLUSTER_PATH=midonet-cluster/build/packages
+TOOLS_PATH=midonet-tools/build/packages
+CLIENT_PATH=python-midonetclient
+
+cp $(ls $MIDOLMAN_PATH/*deb | tail -n1) tests/sandbox/$1/midolman
+cp $(ls $TOOLS_PATH/*deb | tail -n1) tests/sandbox/$1/midolman
+cp $(ls $CLUSTER_PATH/*deb | tail -n1) tests/sandbox/$1/cluster
+cp $(ls $TOOLS_PATH/*deb | tail -n1) tests/sandbox/$1/cluster
+cp $(ls $CLIENT_PATH/*deb | tail -n1) tests/sandbox/$1/cluster
