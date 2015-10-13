d9e9551ca55be9cb1920373252448d27c6afd269
Alex Bikfalvi <alex.bikfalvi@midokura.com>
2015-10-13 18:53:31 +0200

    VXGW: Functional tests for the VXLAN gateway service
    These tests verify only traffic from MidoNet to one or more
    VTEP ports. They include the following test cases:
    - single VTEP, single bridge, single MM
    - single VTEP, single bridge, multi MM (disabled due to MNA-1006)
    - single VTEP, multi bridge, single MM
    - single VTEP, multi bridge, multi MM
    - multi VTEP, single tunnel-zone, single MM
    - multi VTEP, single tunnel-zone, multi MM
    - multi VTEP, multi tunnel-zone, single MM
    - multi VTEP, multi tunne-zone, multi MM
    
    Todo: Additional tests should verify inbound traffic from the VTEP
    both unicast and multicast (testing the functionality of the
    flooding proxy).
    
    Node: Some checks are disabled due to known issues: MNA-1003
    
    Ref: MNA-136, MNA-990, MNA-991
    
    Change-Id: I73d06d6fc4305652de646ecd5324034a5413648c
    Signed-off-by: Alex Bikfalvi <alex.bikfalvi@midokura.com>

--- tests/sandbox/provisioning/vtep-provisioning.sh
+++ tests/sandbox/provisioning/vtep-provisioning.sh
@@ -6,4 +6,13 @@ sudo docker exec mnsandboxmdts_vtep1_1 /etc/init.d/openvswitch-vtep start
 sudo docker exec mnsandboxmdts_vtep2_1 /etc/init.d/openvswitch-vtep start
 
 sudo docker exec mnsandboxmdts_vtep1_1 /configure-vtep.sh
+#TODO: Move the following to the configuration script.
+sudo docker exec mnsandboxmdts_vtep1_1 pkill ovs-vtep
+sudo docker exec mnsandboxmdts_vtep1_1 sleep 3
+sudo docker exec mnsandboxmdts_vtep1_1 /usr/share/openvswitch/scripts/ovs-vtep --log-file=/var/log/openvswitch/ovs-vtep.log --pidfile=/var/run/openvswitch/ovs-vtep.pid --detach vtep0
+
 sudo docker exec mnsandboxmdts_vtep2_1 /configure-vtep.sh
+#TODO: Move the following to the configuration script.
+sudo docker exec mnsandboxmdts_vtep2_1 pkill ovs-vtep
+sudo docker exec mnsandboxmdts_vtep2_1 sleep 3
+sudo docker exec mnsandboxmdts_vtep2_1 /usr/share/openvswitch/scripts/ovs-vtep --log-file=/var/log/openvswitch/ovs-vtep.log --pidfile=/var/run/openvswitch/ovs-vtep.pid --detach vtep0
