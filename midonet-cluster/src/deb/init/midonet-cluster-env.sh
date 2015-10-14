c964eaa3ce29ad87a7d6df91e370e1bebefb0fdc
Nicolas Schiper <nicolas@midokura.com>
2015-10-14 18:54:46 +0200

    Removing midonet-cluster.conf
    This patch removes configuration file
    /midonet-cluster/conf/midonet-cluster.conf. It also modifies the
    ClusterNode and TopologyApiService classes such that when no
    configuration file is passed as a program argument, configuration is
    picked up from the default locations.
    
    Ref: mna-966
    
    Change-Id: Iff30e08c7d983d3fea92414ae1084efe379e303f
    Signed-off-by: Nicolas Schiper <nicolas@midokura.com>

--- midonet-cluster/src/deb/init/midonet-cluster-env.sh
+++ midonet-cluster/src/deb/init/midonet-cluster-env.sh
@@ -1,6 +1,6 @@
 MIDO_HOME=/usr/share/midonet-cluster
 MIDO_ETC=/etc/midonet-cluster
-MIDO_CFG=/etc/midonet-cluster
+MIDO_CFG=/etc/midonet
 MIDO_LOG_DIR=/var/log/midonet-cluster/
 MIDO_DEBUG_PORT=8001
-MIDO_CFG_FILE=midonet-cluster.conf
+MIDO_CFG_FILE=midonet.conf
