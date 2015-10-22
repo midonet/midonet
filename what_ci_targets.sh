31eb0754829b29fec8bfaaf90d109c8e4179d80c
Galo Navarro <galo@midokura.com>
2015-10-22 17:52:00 +0200

    Add script to query CI targets
    This patch provides a script that allows a developer (or the CI system)
    get the right gradle tasks and flags that should be used to reliably
    verify HEAD.  Refer to ./what_ci_targets.sh for further documentation.
    
    Change-Id: Ib1c258a14512352e6673417c7d334bcf99cf2f91
    Signed-off-by: Galo Navarro <galo@midokura.com>

---
+++ what_ci_targets.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+#
+# Recommends targets for CI based on the last commit's contents.
+#
+# Usage:
+#
+# ./what_ci_targets.sh (debian|rpm)
+#
+# Where:
+# - DIST is the target distribution when packaging, defaults to debian
+#
+# Outputs at most two lines explaning the right targets for unit and
+# MDTS tests.
+#
+# A line starting with ":gradle" will *always* be output and provides a
+# parameter list to pass to a gradle command.  E.g.:
+#
+#   :gradle clean test
+
+# A line starting with :type will explain the type of change detected by
+# the script.
+#
+# A second line starting with ":mdts" *may* be output, identifying MDTS
+# targets that can be passed straight to run_tests.sh.  E.g.:
+#
+#   :mdts -t test_bridge
+#
+# When the :mdts line is ommitted, we don't consider MDTS tests
+# necessary for HEAD.
+#
+# Users may execute the script, drop the :gradle or :mdts tag, and pass
+# the remainder of each line to the ./gradlew or
+# ./tests/mdts/functional_tests/run_tests.sh executables.
+
+DIST=$1
+if [ "x$DIST" == "x" ]
+then
+    DIST=debian
+fi
+
+if [ "$DIST" != "debian" ] && [ "$DIST" != "rpm" ]
+then
+    echo "$DIST is not a valid distribution (use debian or rpm)"
+    exit -1
+fi
+
+DESC_VER_BUMP=":type Version bump"
+DESC_NORMAL=":type Normal changeset"
+DESC_NO_CHANGES=":type Changeset without changes relevant for testing"
+FAST_BUILD=":gradle clean assemble testClasses midonet-util:test $DIST"
+FULL_BUILD=":gradle clean build $DIST"
+FULL_MDTS=":mdts"
+FAST_MDTS=":mdts -t test_bridge:test_icmp"
+
+# Various regexes matching files that don't require unit/integration tests
+REGEX_FOR_UNIT='^(python-midonetclient|docs|misc|tools/devmido|specs|LICENSE|DEVELOPMENT.md|README.md|what_ci_targets.sh)'
+REGEX_FOR_MDTS='^(docs|misc|tools/devmido|specs|LICENSE|DEVELOPMENT.md|README.md|what_ci_targets.sh)'
+
+# How many unit-test relevant files are changed in this patch
+FOR_UNIT=`git diff-tree --no-commit-id --name-only -r HEAD | grep -v -E '$REGEX_FOR_UNIT' | wc -l | xargs`
+FOR_MDTS=`git diff-tree --no-commit-id --name-only -r HEAD | grep -v -E '$REGEX_FOR_MDTS' | wc -l | xargs`
+
+let TOTAL="$FOR_UNIT + $FOR_MDTS"
+if [ "$TOTAL" == 0 ]
+then
+    echo "$DESC_NO_CHANGES"
+    echo "$FAST_BUILD"
+    exit 0
+fi
+
+if [ "$TOTAL" == "1" ] && [ $(git diff-tree --no-commit-id --name-only -r HEAD build.gradle) ]
+then
+  # Only 1 relevant file has changes, but it's build.gradle, let's ensure that
+  # we're just doing a version bump
+  LINES_CHANGED=`git diff HEAD~1 build.gradle | grep -E "^\+ " | wc -l | xargs`
+  if [ "$LINES_CHANGED" == "1" ] && [ -z $(git diff HEAD~1 build.gradle | grep -E "^\+ " | grep -v midonetVersion) ]
+  then
+    echo "$DESC_VER_BUMP"
+    echo "$FAST_BUILD"
+    echo "$FAST_MDTS"
+    exit 0
+  fi
+fi
+
+echo "$DESC_NORMAL with $FOR_UNIT changes relevant for unit tests and $FOR_MDTS for mdts"
+test "$FOR_UNIT" -gt 0 && echo "$FULL_BUILD"
+test "$FOR_UNIT" -gt 0 && echo "$FULL_MDTS"
