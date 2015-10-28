1ff8f311ee377e7e81be1f8bcf173a2e44a3b7bb
Nicolas Schiper <nicolas@midokura.com>
2015-10-28 15:42:52 +0100

    Using a ZK lock in the HealthMonitor and in the api
    This patch makes the api and the health monitor use a ZooKeeper lock
    to perform updates on the topology.
    
    This is a temporary solution to write contention between the health
    monitor and the api.
    
    Ref: mna-1002
    
    Change-Id: Iab1b41ee78242d7b8705058ef90581245811b07f
    Signed-off-by: Nicolas Schiper <nicolas@midokura.com>

--- tests/mdts/tests/functional_tests/test_load_balancer.py
+++ tests/mdts/tests/functional_tests/test_load_balancer.py
@@ -193,6 +193,8 @@ def disable_and_assert_traffic_fails(sender, action_fun, **kwargs):
     # Do action
     action_fun("disable", **kwargs)
 
+    time.sleep(5)
+
     # Make one request to the non sticky loadbalancer IP, should fail
     assert_request_fails_to(sender, kwargs['vips']['non_sticky_vip'])
 
@@ -203,6 +205,8 @@ def disable_and_assert_traffic_fails(sender, action_fun, **kwargs):
 def enable_and_assert_traffic_succeeds(sender, action_fun, **kwargs):
     # Do action
     action_fun("enable", **kwargs)
+
+    time.sleep(5)
 
     # Make one request to the non sticky loadbalancer IP, should succeed
     assert_request_succeeds_to(sender, kwargs['vips']['non_sticky_vip'])
@@ -294,6 +298,8 @@ def start_servers():
     for backend_num in range(1, NUM_BACKENDS + 1):
         start_server(backend_num)
     set_filters('router-000-001', 'rev_snat', 'snat')
+    lb_pools = VTM.get_load_balancer('lb-000-001').get_pools()
+    get_current_leader(lb_pools)
 
 def stop_servers():
     global SERVERS
@@ -538,7 +544,6 @@ def test_disabling_topology_loadbalancing():
     disable_and_assert_traffic_fails(sender, action_loadbalancer, vips=vips)
     enable_and_assert_traffic_succeeds(sender, action_loadbalancer, vips=vips)
 
-@nottest
 @bindings(binding_multihost)
 @with_setup(start_servers, stop_servers)
 def test_haproxy_failback():
@@ -593,7 +598,6 @@ def test_haproxy_failback():
                 'L4LB: not all agents were elected as leaders %s' %
                 leaders_elected)
 
-@nottest
 @bindings(binding_multihost)
 @with_setup(start_servers, stop_servers)
 def test_health_monitoring_backend_failback():
