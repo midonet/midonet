0b9905d4e7d69b2ee86f1e47d3975bacf9793f40
Nicolas Schiper <nicolas@midokura.com>
2015-10-28 15:42:52 +0100

    Retry write operations in the HealthMonitor
    Retry write operations issued to Zoom when a
    ConcurrentModificationException is thrown. Retries follow an
    exponential back-off approach where the initial sleep is randomly
    distributed between 0 and a configurable time (in HealthMonitor
    config).
    
    Change-Id: Ia43beb3ee70fe86debef2d24e13c0cfd6f519444
    Signed-off-by: Nicolas Schiper <nicolas@midokura.com>

--- midolman/src/main/scala/org/midonet/midolman/config/MidolmanConfig.scala
+++ midolman/src/main/scala/org/midonet/midolman/config/MidolmanConfig.scala
@@ -142,6 +142,10 @@ class HealthMonitorConfig(val conf: Config, val schema: Config) extends TypeFail
     def enable = getBoolean(s"$PREFIX.health_monitor_enable")
     def namespaceCleanup = getBoolean(s"$PREFIX.namespace_cleanup")
     def haproxyFileLoc = getString(s"$PREFIX.haproxy_file_loc")
+    def retriesOnConcurrentModification =
+        Try(getInt(s"$PREFIX.retries.on.concurrent.modification")).getOrElse(10)
+    def seedTimeAfterConcurrentModification: Long =
+        Try(getDuration(s"$PREFIX.seed.time.after.concurrent.modification", TimeUnit.MILLISECONDS)).getOrElse(1000L)
 }
 
 
