849d0e56b4f195c53260b34643db6a41b79130ef
Nicolas Schiper <nicolas@midokura.com>
2015-10-28 15:42:52 +0100

    Using a ZK lock in the HealthMonitor and in the api
    This patch makes the api and the health monitor use a ZooKeeper lock
    to perform updates on the topology.
    
    This is a temporary solution to write contention between the health
    monitor and the api.
    
    Ref: mna-1002
    
    Change-Id: Ia43beb3ee70fe86debef2d24e13c0cfd6f519444
    Signed-off-by: Nicolas Schiper <nicolas@midokura.com>

--- midonet-cluster/src/main/scala/org/midonet/cluster/services/rest_api/resources/PoolMemberResource.scala
+++ midonet-cluster/src/main/scala/org/midonet/cluster/services/rest_api/resources/PoolMemberResource.scala
@@ -18,7 +18,7 @@ package org.midonet.cluster.services.rest_api.resources
 
 import java.util.UUID
 import javax.ws.rs.core.Response
-import javax.ws.rs.{PathParam, Path, DELETE}
+import javax.ws.rs._
 import javax.ws.rs.core.MediaType.APPLICATION_JSON
 
 import scala.collection.JavaConverters._
@@ -28,10 +28,33 @@ import com.google.inject.servlet.RequestScoped
 
 import org.midonet.cluster.rest_api.NotFoundHttpException
 import org.midonet.cluster.rest_api.annotation._
-import org.midonet.cluster.rest_api.models.{Pool, PoolMember}
+import org.midonet.cluster.rest_api.models.{RouterPort, Pool, PoolMember}
 import org.midonet.cluster.services.rest_api.MidonetMediaTypes._
-import org.midonet.cluster.services.rest_api.resources.MidonetResource.{Ids, NoOps, Ops, ResourceContext}
+import org.midonet.cluster.services.rest_api.resources.MidonetResource._
 
+/**
+  * All operations that need to be performed atomically are done by
+  * acquiring a ZooKeeper lock. This is to prevent races with
+  * midolman, more specifically the health monitor, which operates on:
+  * - Route, with side effects on Router and RouterPort due to ZOOM bindings.
+  * - Port, with side effects on Router and Host due to ZOOM bindings.
+  * - Pool
+  * - PoolMember
+  *
+  * Any api or midolman sections that may be doing something like:
+  *
+  *   val port = store.get(classOf[Port], id)
+  *   val portModified = port.toBuilder() -alter fields- .build()
+  *   store.update(portModified)
+  *
+  * Might overwrite changes made by this class on the port (directly, or
+  * implicitly as a result of referential constraints).  To protect
+  * against this, sections of the code similar to the one above should
+  * be protected using the following lock:
+  *
+  *   new ZkOpLock(lockFactory, lockOpNumber.getAndIncrement,
+  *                ZookeeperLockFactory.ZOOM_TOPOLOGY)
+  */
 @ApiResource(version = 1, name = "poolMembers", template = "poolMemberTemplate")
 @Path("pool_members")
 @RequestScoped
@@ -52,15 +75,29 @@ class PoolMemberResource @Inject()(resContext: ResourceContext)
         NoOps
     }
 
+    @PUT
+    @Path("{id}")
+    @Consumes(Array(APPLICATION_POOL_MEMBER_JSON,
+                    APPLICATION_JSON))
+    override def update(@PathParam("id") id: String, member: PoolMember,
+                        @HeaderParam("Content-Type") contentType: String)
+    : Response = {
+        zkLock {
+             super.update(id, member, contentType)
+        }
+    }
+
     @DELETE
     @Path("{id}")
     override def delete(@PathParam("id") id: String): Response = {
-        try {
-            deleteResource(classOf[PoolMember], id)
-        } catch {
-            case t: NotFoundHttpException => // ok, idempotent
+        zkLock {
+            try {
+                deleteResource(classOf[PoolMember], id)
+            } catch {
+                case t: NotFoundHttpException => // ok, idempotent
+            }
+            MidonetResource.OkNoContentResponse
         }
-        MidonetResource.OkNoContentResponse
     }
 
 }
--- midonet-cluster/src/main/scala/org/midonet/cluster/services/rest_api/resources/RouterResource.scala
+++ midonet-cluster/src/main/scala/org/midonet/cluster/services/rest_api/resources/RouterResource.scala
@@ -17,21 +17,43 @@
 package org.midonet.cluster.services.rest_api.resources
 
 import java.util.UUID
-
 import javax.ws.rs._
 import javax.ws.rs.core.MediaType.APPLICATION_JSON
-
-import scala.concurrent.Future
+import javax.ws.rs.core.Response
 
 import com.google.inject.Inject
 import com.google.inject.servlet.RequestScoped
-
 import org.midonet.cluster.rest_api.annotation._
 import org.midonet.cluster.rest_api.models.Router
 import org.midonet.cluster.services.rest_api.MidonetMediaTypes._
 import org.midonet.cluster.services.rest_api.resources.MidonetResource._
 import org.midonet.midolman.state.PathBuilder
 
+import scala.concurrent.Future
+
+/**
+  * All operations that need to be performed atomically are done by
+  * acquiring a ZooKeeper lock. This is to prevent races with
+  * midolman, more specifically the health monitor, which operates on:
+  * - Route, with side effects on Router and RouterPort due to ZOOM bindings.
+  * - Port, with side effects on Router and Host due to ZOOM bindings.
+  * - Pool
+  * - PoolMember
+  *
+  * Any api or midolman sections that may be doing something like:
+  *
+  *   val port = store.get(classOf[Port], id)
+  *   val portModified = port.toBuilder() -alter fields- .build()
+  *   store.update(portModified)
+  *
+  * Might overwrite changes made by this class on the port (directly, or
+  * implicitly as a result of referential constraints).  To protect
+  * against this, sections of the code similar to the one above should
+  * be protected using the following lock:
+  *
+  *   new ZkOpLock(lockFactory, lockOpNumber.getAndIncrement,
+  *                ZookeeperLockFactory.ZOOM_TOPOLOGY)
+  */
 @ApiResource(version = 1, name = "routers", template = "routerTemplate")
 @Path("routers")
 @RequestScoped
@@ -71,6 +93,27 @@ class RouterResource @Inject()(resContext: ResourceContext,
     @Path("{id}/bgp_peers")
     def bgpPeers(@PathParam("id") id: UUID): RouterBgpPeerResource = {
         new RouterBgpPeerResource(id, resContext)
+    }
+
+    @PUT
+    @Path("{id}")
+    @Consumes(Array(APPLICATION_ROUTER_JSON_V3,
+                    APPLICATION_JSON))
+    override def update(@PathParam("id") id: String, router: Router,
+                        @HeaderParam("Content-Type") contentType: String)
+    : Response = {
+        zkLock {
+            getResource(classOf[Router], id).map(current => {
+                router.update(current)
+                updateResource(router, OkNoContentResponse)
+            }).getOrThrow
+        }
+    }
+
+    @DELETE
+    @Path("{id}")
+    override def delete(@PathParam("id") id: String): Response = {
+        zkLock { deleteResource(classOf[Router], id) }
     }
 
     protected override def listFilter(routers: Seq[Router]): Future[Seq[Router]] = {
