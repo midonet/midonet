10d5b681293cc04a7b2cbef0606fd519d56da021
Nicolas Schiper <nicolas@midokura.com>
2015-10-28 15:42:52 +0100

    Using a ZK lock in the HealthMonitor and in the api
    This patch makes the api and the health monitor use a ZooKeeper lock
    to perform updates on the topology.
    
    This is a temporary solution to write contention between the health
    monitor and the api.
    
    Ref: mna-1002
    
    Change-Id: Ia43beb3ee70fe86debef2d24e13c0cfd6f519444
    Signed-off-by: Nicolas Schiper <nicolas@midokura.com>

---
+++ midolman/src/test/scala/org/midonet/midolman/l4lb/BackendUtilTest.scala
@@ -0,0 +1,113 @@
+/*
+ * Copyright 2015 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.midolman.l4lb
+
+import java.util.UUID
+
+import scala.concurrent.Future
+
+import org.apache.curator.framework.recipes.locks.InterProcessSemaphoreMutex
+import org.mockito.{Matchers, Mockito}
+import org.scalatest.{BeforeAndAfter, FlatSpec}
+
+import org.midonet.cluster.ZookeeperLockFactory
+import org.midonet.cluster.data.storage.Storage
+import org.midonet.cluster.models.Topology.Pool
+import org.midonet.cluster.models.Topology.Pool.PoolHealthMonitorMappingStatus
+import org.midonet.cluster.util.UUIDUtil
+
+class BackendUtilTest extends FlatSpec with BeforeAndAfter {
+
+    var backend: Storage = _
+    var lockFactory: ZookeeperLockFactory = _
+    var backendUtil: BackendUtil = _
+
+    before {
+        backend = Mockito.mock(classOf[Storage])
+        lockFactory = Mockito.mock(classOf[ZookeeperLockFactory])
+        backendUtil = new BackendUtil(backend, lockFactory)
+    }
+
+    private def getMutexMockAndSetReturnValues(lockFactory: ZookeeperLockFactory,
+                                               acquireSuccessful: Boolean)
+    : InterProcessSemaphoreMutex = {
+        val mutex = Mockito.mock(classOf[InterProcessSemaphoreMutex])
+        Mockito.when(lockFactory.createShared(ZookeeperLockFactory.ZOOM_TOPOLOGY))
+               .thenReturn(mutex)
+        Mockito.when(mutex.acquire(Matchers.anyLong(), Matchers.anyObject()))
+               .thenReturn(acquireSuccessful)
+        mutex
+    }
+
+    "writeWithLock" should "acquire and release the lock" in {
+        var called = false
+        def someFunction(): Unit = {
+            called = true
+        }
+        val mutex = getMutexMockAndSetReturnValues(lockFactory,
+                                                   acquireSuccessful = true)
+
+        backendUtil.writeWithLock(someFunction)
+
+        /* Specify the order in which methods on the mocks should be called. */
+        val order = Mockito.inOrder(lockFactory, mutex, mutex)
+
+        order.verify(lockFactory, Mockito.times(1))
+             .createShared(Matchers.anyString())
+        order.verify(mutex, Mockito.times(1))
+             .acquire(Matchers.anyLong(), Matchers.anyObject())
+        assert(called)
+        order.verify(mutex, Mockito.times(1)).release()
+    }
+
+    "When the lock cannot be acquired then the method" should "not be called" in {
+        var called = false
+        def someFunction(): Unit = {
+            called = true
+        }
+        val mutex = getMutexMockAndSetReturnValues(lockFactory,
+                                                   acquireSuccessful = false)
+        backendUtil.writeWithLock(someFunction)
+
+        /* Specify the order in which methods on the mocks should be called. */
+        val order = Mockito.inOrder(lockFactory, mutex, mutex)
+
+        order.verify(lockFactory, Mockito.times(1))
+             .createShared(Matchers.anyString())
+        order.verify(mutex, Mockito.times(1))
+            .acquire(Matchers.anyLong(), Matchers.anyObject())
+        assert(!called)
+        order.verify(mutex, Mockito.times(0)).release()
+    }
+
+    "setPoolMappingStatus" should "update the pool in storage" in {
+        val poolId = UUID.randomUUID()
+        val pool = Pool.newBuilder().setId(UUIDUtil.toProto(poolId)).build()
+        val status = PoolHealthMonitorMappingStatus.ACTIVE
+        getMutexMockAndSetReturnValues(lockFactory, acquireSuccessful = true)
+        Mockito.when(backend.get(classOf[Pool], poolId))
+            .thenReturn(Future.successful(pool))
+
+        backendUtil.setPoolMappingStatus(poolId, status)
+
+        Mockito.verify(backend, Mockito.times(1))
+               .get(classOf[Pool], poolId)
+        val updatedPool = pool.toBuilder.setMappingStatus(status).build()
+        Mockito.verify(backend, Mockito.times(1))
+               .update(updatedPool)
+    }
+}
