3d8885af3476ace99214af6200a4df98e26099c1
Duarte Nunes <duarte@midokura.com>
2015-10-30 11:12:30 +0100

    Agent: Update OVS benchmark
    Results:
    
    Benchmark                      Mode  Cnt   Score   Error  Units
    ConcurrentFlowCreate2          avgt   25  26.039 ± 2.106  us/op
    FlowCreate                     avgt   25   5.775 ± 0.706  us/op
    FlowCreateAndDelete            avgt   25  12.427 ± 1.602  us/op
    FlowCreateAndDeleteWithBroker  avgt   25  12.800 ± 1.709  us/op
    PacketExecute                  avgt   25   2.820 ± 0.287  us/op
    
    Machine:
    
    CPU:                Intel(R) Xeon(R) CPU E5620  @ 2.40GHz
    Number of CPUs:     16
    Threads per core:   2
    Cores per socket:   4
    Sockets:            2
    NUMA node(s):       2
    L1 cache:           128K
    L2 cache:           1MB
    L3 cache:           12MB
    System memory:      24GB
    
    Change-Id: Ibad9c7595526d38c70b25df8217bb07b79945b98
    Signed-off-by: Duarte Nunes <duarte@midokura.com>

--- netlink/src/perf/scala/org/midonet/odp/OvsBenchmark.scala
+++ netlink/src/perf/scala/org/midonet/odp/OvsBenchmark.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Midokura SARL
+ * Copyright 2015 Midokura SARL
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,70 +16,151 @@
 
 package org.midonet.odp
 
+import java.nio.ByteBuffer
 import java.{util => ju}
-import java.util.concurrent.{ThreadLocalRandom, TimeUnit}
+import java.util.concurrent.TimeUnit
 
 import scala.collection.JavaConversions._
-import scala.concurrent.duration._
-import scala.concurrent.{Await, ExecutionContext}
+import scala.util.control.NonFatal
 
 import org.openjdk.jmh.annotations._
+import rx.Observer
 
-import org.midonet.netlink.{NetlinkChannel, NetlinkWriter, NetlinkChannelFactory, BytesUtil}
+import org.midonet.ErrorCode
+import org.midonet.netlink._
+import org.midonet.netlink.exceptions.NetlinkException
 import org.midonet.odp.flows._
-import org.midonet.odp.OvsBenchmark.{FlowHolder, ChannelHolder}
+import org.midonet.odp.OpenVSwitch.Flow.{Attr => FlowAttr}
+import org.midonet.odp.OpenVSwitch.FlowKey.{Attr => KeyAttr}
 import org.midonet.odp.ports.NetDevPort
 import org.midonet.odp.util.TapWrapper
 import org.midonet.packets.util.PacketBuilder._
-import org.midonet.packets.{TCP, IPv4Addr, MAC}
-import org.midonet.util.concurrent._
+import org.midonet.packets.{IPv4Addr, MAC}
+import org.midonet.util.concurrent.NanoClock
 
 object OvsBenchmark {
-    val families = {
-        val channel = new NetlinkChannelFactory().create(blocking = true)
-        val families = OvsNetlinkFamilies.discover(channel)
-        channel.close()
-        families
+    val dpName = "bench"
+    val tapName = "bench-test-tap"
+    val portNumber = 10
+
+    val channel = new NetlinkChannelFactory().create(blocking = true)
+    val families = OvsNetlinkFamilies.discover(channel)
+    val protocol = new OvsProtocol(channel.getLocalAddress.getPid,
+                                   OvsBenchmark.families)
+    val writer = new NetlinkWriter(channel)
+    val reader = new NetlinkReader(channel)
+
+    def createDp(buf: ByteBuffer): Datapath = {
+        protocol.prepareDatapathCreate(dpName, buf)
+        NetlinkUtil.rpc(buf, writer, reader, Datapath.buildFrom)
+    }
+
+    def deleteDp(buf: ByteBuffer): Datapath = {
+        protocol.prepareDatapathDel(0, dpName, buf)
+        NetlinkUtil.rpc(buf, writer, reader, Datapath.buildFrom)
     }
 
     @State(Scope.Benchmark)
-    class ChannelHolder() {
-        var channel: NetlinkChannel = _
-        var protocol: OvsProtocol = _
-        var writer: NetlinkWriter = _
-        
+    class DatapathState {
+        private val buf = BytesUtil.instance.allocateDirect(512)
+        private var tapWrapper: TapWrapper = _
+        var datapath: Datapath = _
+
         @Setup
-        def createNewCon(): Unit = {
-            channel = new NetlinkChannelFactory().create(blocking = true)
-            protocol = new OvsProtocol(channel.getLocalAddress.getPid,
-                                              OvsBenchmark.families)
-            writer = new NetlinkWriter(channel)
+        def createDatapath(): Unit = {
+            datapath = try {
+                createDp(buf)
+            } catch {
+                case e: NetlinkException if e.getErrorCodeEnum == ErrorCode.EEXIST =>
+                    deleteDp(buf)
+                    createDp(buf)
+            }
+
+            tapWrapper = try {
+                new TapWrapper(tapName)
+            } catch { case NonFatal(e) =>
+                new TapWrapper(tapName, false)
+            }
+            val tapDpPort = new NetDevPort(tapWrapper.getName, portNumber)
+            protocol.prepareDpPortCreate(datapath.getIndex, tapDpPort, buf)
+            NetlinkUtil.rpc(buf, writer, reader, DpPort.buildFrom)
         }
 
         @TearDown
-        def destroyCon(): Unit =
-            channel.close()
+        def teardownDatapath(): Unit = {
+            val stats = deleteDp(buf).getStats
+            println(s"Flows: ${stats.getFlows}")
+            tapWrapper.remove()
+        }
+    }
+
+    object FlowHolder {
+        def findEthSrc(buf: ByteBuffer): Int = {
+            val limit = buf.limit()
+            var index = 0
+            buf.position(NetlinkMessage.GENL_HEADER_SIZE)
+            buf.getInt // DP id
+            NetlinkMessage.scanAttributes(buf, new AttributeHandler {
+                override def use(buffer: ByteBuffer, id: Short): Unit =
+                    if (NetlinkMessage.unnest(id) == FlowAttr.Key) {
+                        NetlinkMessage.scanAttributes(buffer, new AttributeHandler() {
+                            override def use(buffer: ByteBuffer, id: Short): Unit =
+                                if (id == KeyAttr.Ethernet)
+                                    index = buffer.position() + 4
+                        })
+                    }
+            })
+            buf.limit(limit)
+            buf.position(limit)
+            index
+        }
     }
 
     @State(Scope.Thread)
-    class FlowHolder extends ChannelHolder {
-        val rand = ThreadLocalRandom.current()
-        val ethKey = new FlowKeyEthernet(new Array[Byte](6), new Array[Byte](6))
-        val keys: ju.List[FlowKey] = List(
-                new FlowKeyInPort(0),
-                ethKey,
-                new FlowKeyIPv4(rand.nextInt(), rand.nextInt(), TCP.PROTOCOL_NUMBER,
-                                0, -1, 0),
-                new FlowKeyTCP(rand.nextInt() & 0xffff, rand.nextInt() & 0xffff))
-        val actions: ju.List[FlowAction] = List(FlowActions.output(1))
-        val flowBuf = BytesUtil.instance.allocateDirect(2 * 1024)
+    class FlowHolder {
+        private var curEth = 0
+        private var ethSrcIdx = 0
+        val flowBuf = BytesUtil.instance.allocateDirect(512)
+
+        val payload = (
+                { eth src MAC.fromAddress(new Array[Byte](6)) dst MAC.random } <<
+                { ip4 src IPv4Addr.random dst IPv4Addr.random} <<
+                { tcp src 80 dst 1001 }).packet
+        val wcmatch = FlowMatches.fromEthernetPacket(payload)
+        val actions: ju.List[FlowAction] = List(FlowActions.output(portNumber))
+
+        @Setup(Level.Trial)
+        def setupFlow(dp: DatapathState): Unit = {
+            protocol.prepareFlowCreate(dp.datapath.getIndex, wcmatch.getKeys,
+                                       actions, null, flowBuf)
+            ethSrcIdx = FlowHolder.findEthSrc(flowBuf)
+        }
 
         @Setup(Level.Invocation)
-        def setup(): Unit = {
-            rand.nextBytes(ethKey.eth_src)
-            rand.nextBytes(ethKey.eth_dst)
-            flowBuf.clear()
-            protocol.prepareFlowCreate(0, keys, actions, null, flowBuf)
+        def setupUniqueness(): Unit = {
+            curEth += 1
+            flowBuf.flip()
+            flowBuf.putInt(ethSrcIdx, curEth)
+        }
+    }
+
+    @State(Scope.Thread)
+    class FlowDeletionHolder extends FlowHolder {
+        private var curEth = 0
+        private var ethSrcIdx = 0
+        val flowDelBuf = BytesUtil.instance.allocateDirect(512)
+
+        override def setupFlow(dp: DatapathState): Unit = {
+            super.setupFlow(dp)
+            protocol.prepareFlowDelete(dp.datapath.getIndex, wcmatch.getKeys, flowDelBuf)
+            ethSrcIdx = FlowHolder.findEthSrc(flowDelBuf)
+        }
+
+        @Setup(Level.Invocation)
+        def setupDelUniqueness(): Unit = {
+            curEth += 1
+            flowDelBuf.flip()
+            flowDelBuf.putInt(ethSrcIdx, curEth)
         }
     }
 }
@@ -90,62 +171,102 @@ object OvsBenchmark {
 @Measurement(iterations = 5)
 @Fork(5)
 @State(Scope.Benchmark)
-abstract class OvsBenchmark {
-    implicit val ec = ExecutionContext.callingThread
-
-    var ops: OvsConnectionOps = _
-    var datapath: Datapath = _
-    var tapWrapper: TapWrapper = _
-    var port: DpPort = _
-
-    @Setup
-    def createDatapath(): Unit = {
-        ops = new OvsConnectionOps(DatapathClient.createConnection())
-        datapath = Await.result(ops createDp "midonet", Duration.Inf)
-        tapWrapper = new TapWrapper("odp-test-tap")
-        val tapDpPort = new NetDevPort(tapWrapper.getName)
-        port = Await.result(ops createPort(tapDpPort, datapath), 1 second)
-    }
-
-    @TearDown
-    def teardownDatapath(): Unit = {
-        Await.result(ops delPort(port, datapath), Duration.Inf)
-        Await.result(ops delDp "midonet", Duration.Inf)
-        tapWrapper.remove()
-    }
-}
-
-class PacketExecute extends OvsBenchmark {
+class PacketExecute {
+    import OvsBenchmark._
     var pktExec = BytesUtil.instance.allocateDirect(4 * 1024)
 
     @Setup
-    def createPacket(channel: ChannelHolder): Unit = {
+    def createPacket(dp: DatapathState): Unit = {
         val payload = ({ eth src MAC.random dst MAC.random } <<
                        { ip4 src IPv4Addr.random dst IPv4Addr.random} <<
                        { tcp src 80 dst 1001 }).packet
         val wcmatch = FlowMatches.fromEthernetPacket(payload)
         val pkt = new Packet(payload, wcmatch)
-        val actions = List[FlowAction](FlowActions.output(port.getPortNo))
-        channel.protocol.preparePacketExecute(datapath.getIndex, pkt, actions, pktExec)
+        val actions = List[FlowAction](FlowActions.output(portNumber))
+        protocol.preparePacketExecute(dp.datapath.getIndex, pkt, actions, pktExec)
+        pktExec.position(pktExec.limit())
     }
 
+    @Setup(Level.Invocation)
+    def prepareBuffer(): Unit =
+        pktExec.flip()
+
     @Benchmark
-    def packetExecute(channel: ChannelHolder): Int =
-        channel.writer.write(pktExec)
+    def packetExecute(): Int =
+        writer.write(pktExec)
 }
 
-class FlowCreate extends OvsBenchmark {
+@BenchmarkMode(Array(Mode.AverageTime))
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@Warmup(iterations = 5)
+@Measurement(iterations = 5)
+@Fork(5)
+@State(Scope.Benchmark)
+class FlowCreate {
+    import OvsBenchmark._
 
     @Benchmark
     def createFlow(holder: FlowHolder): Int =
-        holder.writer.write(holder.flowBuf)
+        writer.write(holder.flowBuf)
+}
+
+@BenchmarkMode(Array(Mode.AverageTime))
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@Warmup(iterations = 5)
+@Measurement(iterations = 5)
+@Fork(5)
+@State(Scope.Benchmark)
+class FlowCreateAndDelete {
+    import OvsBenchmark._
+
+    var buf = BytesUtil.instance.allocateDirect(4 * 1024)
+
+    @Setup(Level.Invocation)
+    def clearBuffer(): Unit =
+        buf.clear()
+
+    @Benchmark
+    def createFlow(holder: FlowDeletionHolder): Int = {
+        writer.write(holder.flowBuf)
+        writer.write(holder.flowDelBuf)
+        reader.read(buf)
+    }
+}
+
+@BenchmarkMode(Array(Mode.AverageTime))
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@Warmup(iterations = 5)
+@Measurement(iterations = 5)
+@Fork(5)
+@State(Scope.Benchmark)
+class FlowCreateAndDeleteWithBroker {
+    import OvsBenchmark._
+
+    val broker = new NetlinkRequestBroker(
+        new NetlinkBlockingWriter(channel),
+        reader,
+        maxPendingRequests = 1,
+        maxRequestSize = 1024,
+        readBuf = BytesUtil.instance.allocateDirect(4 * 1024),
+        clock = NanoClock.DEFAULT)
+    val obs = new Observer[ByteBuffer]() {
+        override def onCompleted(): Unit = { }
+        override def onError(e: Throwable): Unit = { }
+        override def onNext(t: ByteBuffer): Unit = { }
+    }
+
+    @Benchmark
+    def createFlow(holder: FlowDeletionHolder): Int = {
+        writer.write(holder.flowBuf)
+        val seq = broker.nextSequence()
+        val buf = broker.get(seq)
+        buf.put(holder.flowDelBuf)
+        buf.flip()
+        broker.publishRequest(seq, obs)
+        broker.writePublishedRequests()
+        broker.readReply()
+    }
 }
 
 @Threads(2)
 class ConcurrentFlowCreate2 extends FlowCreate
-
-@Threads(4)
-class ConcurrentFlowCreate4 extends FlowCreate
-
-@Threads(8)
-class ConcurrentFlowCreate8 extends FlowCreate
