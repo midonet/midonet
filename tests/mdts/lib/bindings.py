082478dca52caef83e6b710ecf8407e80f65f4ee
Xavi León <xavi.leon@midokura.com>
2015-11-01 01:29:26 +0100

    MDTS: First Neutron based test (PoC)
    This patch adds an initial test generating the topology with the Neutron
    API. Using the neutron API, we create a net, a subnet and two ports.
    This two ports are bound to two VMs located on different computes. Send
    a ping using the same framework used previsouly for testing through the
    internal midonet api.
    
    TODO:
    - move the test to a neutron directory for better organization.
    - move run_tests.sh (and associated files) to a common parent.
    - document (example test is self-explained though)
    
    Change-Id: I240cb8a0cc7a4d37d4912fc4f25dd1834cbb7ecb
    Signed-off-by: Xavi León <xavi.leon@midokura.com>

---
+++ tests/mdts/lib/bindings.py
@@ -0,0 +1,80 @@
+# Copyright 2014 Midokura SARL
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import fixtures
+from fixtures import callmany
+
+from mdts.tests.utils.utils import await_port_active
+
+import logging
+
+LOG = logging.getLogger(__name__)
+
+class BindingManager(fixtures.Fixture):
+
+    def __init__(self, ptm, vtm):
+
+        self._ptm = ptm
+        self._vtm = vtm
+        self._cleanups = callmany.CallMany()
+        self._mappings = {}
+
+    def set_binding_data(self, data):
+        self._data = data
+
+    def get_binding_data(self):
+        return self._data
+
+    def bind(self, filename=None):
+        self._ptm.build()
+        self._vtm.build()
+
+        for binding in self._data['bindings']:
+            vport = self._vtm.get_resource(binding['vport'])
+            iface = self._ptm.get_resource(binding['interface'])
+
+            vport_id = self._get_port_id(vport)
+
+            # Do the actual binding
+            binding = iface.compute_host.bind_port(iface, vport_id)
+            self.addCleanup(iface.compute_host.unbind_port, iface)
+            self._mappings[vport_id] = iface
+            await_port_active(vport_id)
+
+        # Schedule deletion of virtual and physical topologies
+        self.addCleanup(self._vtm.destroy)
+        self.addCleanup(self._ptm.destroy)
+
+    def unbind(self):
+        self.cleanUp()
+        self._cleanups = callmany.CallMany()
+        self._mappings = {}
+
+    def get_interface_on_vport(self, vport_name):
+        """
+        Given a virtual port (Neutron or Midonet), it returs the interface
+        associated to this port.
+        :param vport: A neutron or midonet virtual port object
+        :rtype: mdts.services.interface.Interface
+        """
+        vport = self._vtm.get_resource(vport_name)
+        return self._mappings[self._get_port_id(vport)]
+
+    def _get_port_id(self, vport):
+        """
+        Helper method to get the virtual port id depending on the type of object
+        :param vport: A neutron or midonet virtual port object
+        :rtype: uuid
+        """
+        return vport['port']['id'] if type(vport) is dict else vport.get_id()
