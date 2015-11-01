30a70864bd6f36cf08cd24b3da17812ae93bb2b3
Xavi León <xavi.leon@midokura.com>
2015-11-01 01:29:26 +0100

    MDTS: First Neutron based test
    This patch adds an initial test generating the topology with the Neutron
    API. Using the neutron API, we create a net, a subnet and two ports.
    This two ports are bound to two VMs located on different computes. Send
    a ping using the same framework used previsouly for testing through the
    internal midonet api.
    
    TODO:
    - move the test to a neutron directory for better organization.
    - move run_tests.sh (and associated files) to a common parent.
    
    Change-Id: I240cb8a0cc7a4d37d4912fc4f25dd1834cbb7ecb
    Signed-off-by: Xavi León <xavi.leon@midokura.com>

---
+++ tests/mdts/lib/vtm_neutron.py
@@ -0,0 +1,57 @@
+#
+# Copyright 2015 Midokura SARL
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+from mdts.lib.topology_manager import TopologyManager
+from mdts.tests.utils.utils import get_neutron_api
+
+
+class NeutronTopologyManager(TopologyManager):
+    """
+    This is the topology manager for Neutron based APIs. It contains helper
+    methods to manage the lifecycle of a topology (setup and teardown of
+    resources).
+    """
+
+    '''
+    Name mapping between C methods and D methods
+    '''
+    method_mappings = {
+        'create': 'delete',
+        'add': 'remove',
+        'associate': 'disassociate',
+        'connect': 'disconnect',
+    }
+
+    def __init__(self):
+        super(NeutronTopologyManager, self).__init__()
+        self.resources = {}
+        self.api = get_neutron_api()
+
+    def create_resource(self, resource):
+        # Get the type of the resource just created.
+        rtype = resource.keys()[0]
+
+        # Keep the reference to the resource created identified by the name.
+        # A dictionary mapping the json response.
+        name = resource[rtype]['name']
+
+        self.set_resource(name, resource)
+
+        delete_method_name = "%s_%s" % (
+            self.method_mappings['create'],
+            rtype)
+        delete = getattr(self.api, delete_method_name)
+        self.addCleanup(delete, resource[rtype]['id'])
+        return resource
---
+++ tests/mdts/services/neutron.py
@@ -0,0 +1,53 @@
+#
+# Copyright 2015 Midokura SARL
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from mdts.services import service
+from mdts.services.service import Service
+
+import neutronclient.neutron.client as neutron
+
+
+class NeutronHost(Service):
+
+    def __init__(self, container_id):
+        super(NeutronHost, self).__init__(container_id)
+
+    def get_service_status(self):
+        try:
+            api = self.get_neutron_api()
+            api.list_networks()
+            return 'up'
+        except:
+            return 'down'
+
+    def get_service_name(self):
+        return 'neutron'
+
+    def get_service_logs(self):
+        return ['/var/log/neutron/neutron-server.log']
+
+    def get_neutron_api(self):
+        """
+        :rtype: neutronclient.neutron.client.Client
+        """
+        keystone_host = service.get_container_by_hostname('keystone')
+        return neutron.Client(
+            '2.0',
+            auth_url='http://%s:35357/v2.0' % keystone_host.get_ip_address(),
+            endpoint_url='http://%s:9696' % self.get_ip_address(),
+            tenant_name='admin',
+            username='admin',
+            password='admin')
