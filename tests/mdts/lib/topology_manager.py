fd648fbb6820d154b2d56c76c004e77547e50277
Xavi León <xavi.leon@midokura.com>
2015-11-01 01:29:26 +0100

    MDTS: First Neutron based test
    This patch adds an initial test generating the topology with the Neutron
    API. Using the neutron API, we create a net, a subnet and two ports.
    This two ports are bound to two VMs located on different computes. Send
    a ping using the same framework used previsouly for testing through the
    internal midonet api.
    
    TODO:
    - move the test to a neutron directory for better organization.
    - move run_tests.sh (and associated files) to a common parent.
    
    Change-Id: I240cb8a0cc7a4d37d4912fc4f25dd1834cbb7ecb
    Signed-off-by: Xavi León <xavi.leon@midokura.com>

--- tests/mdts/lib/topology_manager.py
+++ tests/mdts/lib/topology_manager.py
@@ -1,4 +1,4 @@
-# Copyright 2014 Midokura SARL
+# Copyright 2015 Midokura SARL
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,30 +14,110 @@
 
 """ Base class for physical / virtual topology resource manager. """
 
+import fixtures
+from fixtures import callmany
 from midonetclient.api import MidonetApi
-
+import uuid
 import yaml
+
 from mdts.services import service
-from mdts.tests.utils.utils import get_midonet_api
+from mdts.tests.utils import utils
 
 
-class TopologyManager(object):
+class TopologyManager(fixtures.Fixture):
 
     def __init__(self, filename=None, data=None, midonet_api=None):
+        super(TopologyManager, self).__init__()
+        # deprecate
         self._data = self._get_data(filename, data)
         self._midonet_api_host = service.get_container_by_hostname('cluster1')
 
+        # New model
+        self._midonet_api = utils.get_midonet_api()
+        self._cleanups = callmany.CallMany()
+        self._resources = {}
+
+    #Deprecate
     def _deserialize(self, filename):
         with open(filename) as f:
             raw_data = f.read()
             return yaml.load(raw_data)
 
+    #Deprecate
     def _get_data(self, filename, data):
-        if not filename and not data:
-            raise AssertionError(
-                "One of the filename or data should be provided")
-
         if filename:
             return self._deserialize(filename)
         else:
             return data
+
+    def set_resource(self, name, resource):
+        """
+        Store the mapping between a resource and its name.
+        :param name: string
+        :param resource: object
+        :rtype: None
+        """
+        if name in self._resources:
+            RuntimeError("Resource %s previously registered on the "
+                         "topology manager. Should be unique." % name)
+        self._resources[name] = resource
+
+    def get_resource(self, name):
+        """
+        :param name: string
+        :rtype: object
+        """
+        return self._resources[name]
+
+    def build(self):
+        """
+        Builds the topology specified in this method, either physical or
+        virtual. If the topology elements should be removed during teardown,
+        you MUST schedule the delete using self.addCleanup(delete_method, args).
+        :rtype: None
+        """
+        # Do nothing by default
+        pass
+
+    def destroy(self):
+        """
+        Clean up of the topology scheduled for deletion during the test
+        lifecycle (build method or inside the test). The topology will be
+        cleaned up if an exception is raised during a test.
+        :return:
+        """
+        self.cleanUp()
+        self._cleanups = callmany.CallMany()
+        self._resources = {}
+
+    def add_host_to_tunnel_zone(self, hostname, tz_name, add_clean_up=True):
+        """
+
+        :param hostname: string
+        :param tz_name: string
+        :param add_clean_up: boolean
+        :rtype: None
+        """
+        def get_or_create_tz(tz_name):
+            tzones = self._midonet_api.get_tunnel_zones()
+            for tz in tzones:
+                if tz_name in tz.get_name():
+                    return tz
+            tz = self._midonet_api.add_vxlan_tunnel_zone()
+            tz.name('%s-%s' % (tz_name, str(uuid.uuid4())[:4]))
+            tz.create()
+            self.addCleanup(tz.delete)
+            return tz
+
+        tz = get_or_create_tz(tz_name)
+        host = service.get_container_by_hostname(hostname)
+        hostid = host.get_midonet_host_id()
+        tz_hostids = [tz_host.get_host_id() for tz_host in tz.get_hosts()]
+        # Make sure not to add an existing host in the tz
+        if hostid not in tz_hostids:
+            tz_host = tz.add_tunnel_zone_host()
+            tz_host.ip_address(host.get_ip_address())
+            tz_host.host_id(host.get_midonet_host_id())
+            tz_host.create()
+            if add_clean_up:
+                self.addCleanup(tz_host.delete)
--- tests/sandbox/provisioning/keystone-provisioning.sh
+++ tests/sandbox/provisioning/keystone-provisioning.sh
@@ -1,29 +1,45 @@
 #!/bin/bash -x
 
 INDOCKER="docker exec mnsandboxmdts_keystone_1"
-IP_ADDRESS=$($INDOCKER hostname --ip-address)
+KEYSTONE_IP=$(docker exec mnsandboxmdts_keystone_1 hostname --ip-address)
+NEUTRON_IP=$(docker exec mnsandboxmdts_neutron_1 hostname --ip-address)
 
 OS_SERVICE_TOKEN="ADMIN"
-OS_SERVICE_ENDPOINT="http://$IP_ADDRESS:35357/v2.0"
+OS_SERVICE_ENDPOINT="http://$KEYSTONE_IP:35357/v2.0"
 
 KEYSTONE="$INDOCKER keystone --os-endpoint=$OS_SERVICE_ENDPOINT --os-token=$OS_SERVICE_TOKEN"
 $INDOCKER keystone-manage db_sync
 
 
-$KEYSTONE tenant-create --name admin --description "Admin tenant"
-$KEYSTONE user-create --name admin --pass admin
 $KEYSTONE role-create --name admin
 $KEYSTONE role-create --name __member__
+
+$KEYSTONE tenant-create --name admin --description "Admin tenant"
+$KEYSTONE user-create --name admin --pass admin
 $KEYSTONE user-role-add --user admin --tenant admin --role admin
 $KEYSTONE user-role-add --user admin --tenant admin --role __member__
-$KEYSTONE tenant-create --name service --description "Service tenant"
-$KEYSTONE service-create --name keystone --type identity --description "OSt identity"
 
-SERVICE_ID=$($KEYSTONE service-list | awk  '/ identity / {print $2}' | xargs | cut -d' ' -f1)
+$KEYSTONE tenant-create --name service --description "Service tenant"
+$KEYSTONE user-create --name neutron --pass neutron
+$KEYSTONE user-role-add --user neutron --tenant service --role admin
+$KEYSTONE user-role-add --user neutron --tenant service --role __member__
+
+$KEYSTONE service-create --name keystone --type identity --description "OSt identity"
+$KEYSTONE service-create --name neutron --type network --description "OSt network"
+
+KEYSTONE_SERVICE_ID=$($KEYSTONE service-list | awk  '/ identity / {print $2}' | xargs | cut -d' ' -f1)
+NEUTRON_SERVICE_ID=$($KEYSTONE service-list | awk  '/ network / {print $2}' | xargs | cut -d' ' -f1)
 
 $KEYSTONE endpoint-create \
-  --service-id $SERVICE_ID \
-  --publicurl http://$IP_ADDRESS:5000/v2.0 \
-  --internalurl http://$IP_ADDRESS:5000/v2.0 \
-  --adminurl http://$IP_ADDRESS:35357/v2.0 \
+  --service-id $KEYSTONE_SERVICE_ID \
+  --publicurl http://$KEYSTONE_IP:5000/v2.0 \
+  --internalurl http://$KEYSTONE_IP:5000/v2.0 \
+  --adminurl http://$KEYSTONE_IP:35357/v2.0 \
+  --region regionOne
+
+$KEYSTONE endpoint-create \
+  --service-id $NEUTRON_SERVICE_ID \
+  --publicurl http://$NEUTRON_IP:9696 \
+  --internalurl http://$NEUTRON_IP:9696 \
+  --adminurl http://$NEUTRON_IP:9696 \
   --region regionOne
