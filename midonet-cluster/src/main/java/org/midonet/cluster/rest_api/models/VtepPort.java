5e83f2b80285adcf612223743a3746aac24e2992
Alex Bikfalvi <alex.bikfalvi@midokura.com>
2015-11-08 21:16:15 +0100

    API: Adds VTEP ports resource
    Ref: MNA-1077
    
    Change-Id: I0b0a393e89f64056512ee65951b70f51b99b0534
    Signed-off-by: Alex Bikfalvi <alex.bikfalvi@midokura.com>

---
+++ midonet-cluster/src/main/java/org/midonet/cluster/rest_api/models/VtepPort.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2014 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.cluster.rest_api.models;
+
+import java.net.URI;
+import java.util.UUID;
+
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.google.common.base.MoreObjects;
+
+import org.midonet.cluster.rest_api.ResourceUris;
+
+public class VtepPort extends UriResource {
+
+    @NotNull
+    public String name;
+
+    public String description;
+
+    @NotNull
+    @JsonIgnore
+    public UUID vtepId;
+
+    public VtepPort() {}
+
+    public VtepPort(String name, String description, UUID vtepId) {
+        this.name = name;
+        this.description = description;
+        this.vtepId = vtepId;
+    }
+
+    @Override
+    public URI getUri() {
+        return absoluteUri(ResourceUris.VTEPS, vtepId, ResourceUris.PORTS, name);
+    }
+
+    public URI getVtep() {
+        return absoluteUri(ResourceUris.VTEPS, vtepId);
+    }
+
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+            .omitNullValues()
+            .add("name", name)
+            .add("description", description)
+            .toString();
+    }
+}
---
+++ midonet-cluster/src/main/scala/org/midonet/cluster/services/rest_api/resources/VtepPortResource.scala
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2015 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.cluster.services.rest_api.resources
+
+import java.util.{List => JList, UUID}
+
+import javax.ws.rs.{GET, Produces}
+
+import scala.collection.JavaConverters._
+
+import com.google.inject.servlet.RequestScoped
+
+import org.midonet.cluster.rest_api.models.{Vtep, VtepPort}
+import org.midonet.cluster.services.rest_api.MidonetMediaTypes._
+import org.midonet.cluster.services.rest_api.resources.MidonetResource.ResourceContext
+import org.midonet.packets.IPv4Addr
+import org.midonet.southbound.vtep.{OvsdbVtepDataClient, OvsdbVtepConnectionProvider}
+
+@RequestScoped
+class VtepPortResource (vtepId: UUID, resContext: ResourceContext,
+                        connectionProvider: OvsdbVtepConnectionProvider)
+    extends MidonetResource[VtepPort](resContext) {
+
+    @GET
+    @Produces(Array(APPLICATION_VTEP_PORT_COLLECTION_JSON))
+    def list(): JList[VtepPort] = {
+        val vtep = getResource(classOf[Vtep], vtepId).getOrThrow
+        val connection = connectionProvider.get(IPv4Addr(vtep.managementIp),
+                                                vtep.managementPort)
+        val client = OvsdbVtepDataClient(connection)
+
+        val result = client.connect() flatMap { _ =>
+            client.physicalPorts
+        } map { ports =>
+            ports map { p => new VtepPort(p.name, p.description, vtepId) } asJava
+        } recover { case t: Throwable =>
+            client.close()
+            throw t
+        } getOrThrow
+
+        client.close()
+        result
+    }
+}
