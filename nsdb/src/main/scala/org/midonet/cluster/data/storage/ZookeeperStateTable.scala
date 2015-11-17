5e0f721eeb47c7ce175134a4fbcdfc88df40c756
Alex Bikfalvi <alex.bikfalvi@midokura.com>
2015-11-17 13:28:09 +0100

    Replicated tables: Introduce ZK state table storage
    With this change, new replicated tables can be created from the
    MidonetBackend. The current implementation allows state tables using
    ZooKeeper as backend, usually leveraging the existing implementation
    of the ReplicatedMap. Toward this, you must register a new state
    table as:
    
      store.registerTable(clazz, key, value, name, provider)
    
    Where:
    
      clazz:    Is the object class for which the table is registered
                in storage (e.g. classOf[Router])
      key:      Is the table key class (e.g. classOf[IPv4Addr])
      value:    Is the table value class (e.g. classOf[MAC])
      provider: Is the class that provides the implementation for the
                state table.
      name:     Is the name of the state table. The name must be
                unique for a given object class.
    
    The state table provider class can be any class with a public
    constructor that receives the ZkDirectory and the
    ZkConnectionAwareWatcher as arguments.
    
    To retrieve an instance of a state table use:
    
      store.getTable[K,V](clazz, id, name): StateTable[K,V]
    
    Where:
    
      K:     Is the table key class (e.g. IPv4Addr).
      V:     Is the table value class (e.g. MAC).
      clazz: Is the object class (e.g. classOf[Router])
      id:    Is the object identifier for which the table is created.
      name:  Is the state table name.
    
    Change-Id: I04f56d05400f3ec2f0ffa871d9082a622136f4b3
    Signed-off-by: Alex Bikfalvi <alex.bikfalvi@midokura.com>

---
+++ nsdb/src/main/scala/org/midonet/cluster/data/storage/ZookeeperStateTable.scala
@@ -0,0 +1,250 @@
+/*
+ * Copyright 2015 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.cluster.data.storage
+
+import java.util.UUID
+import java.util.concurrent.atomic.AtomicLong
+import java.util.concurrent.{ConcurrentHashMap, ExecutorService}
+
+import scala.collection.JavaConverters._
+import scala.collection.concurrent.TrieMap
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
+import scala.reflect.ClassTag
+import scala.util.control.NonFatal
+
+import com.google.inject.{AbstractModule, Guice}
+
+import org.apache.commons.lang3.StringUtils
+import org.apache.curator.framework.CuratorFramework
+import org.apache.curator.utils.ZKPaths
+import org.apache.zookeeper.KeeperException
+import org.apache.zookeeper.KeeperException.NoNodeException
+import org.apache.zookeeper.ZooDefs.Ids
+
+import org.midonet.cluster.backend.Directory
+import org.midonet.cluster.backend.zookeeper.{ZkConnection, ZkConnectionAwareWatcher, ZkDirectory}
+import org.midonet.cluster.data._
+import org.midonet.cluster.data.storage.TransactionManager._
+import org.midonet.packets.{IPv4Addr, MAC}
+import org.midonet.util.eventloop.Reactor
+import org.midonet.util.functors.makeRunnable
+
+trait ZookeeperStateTable extends StateTableStorage with Storage {
+
+    protected[storage] trait StateTableTransactionManager {
+
+        protected val pathsToDelete = new ConcurrentHashMap[String, Void]
+
+        protected def executorService: ExecutorService
+
+        protected def version: Long
+
+        protected def ops: mutable.LinkedHashMap[Key, TxOp]
+
+        /**
+          * Returns the operations needed to create the state tables paths for
+          * all new objects that have at least one table. These are added to
+          * the same transaction that creates a new object.
+          */
+        protected def createStateTableOps: Seq[(Key, TxOp)] = {
+            val list = new ListBuffer[(Key, TxOp)]
+            for ((Key(clazz, id), txOp) <- ops) txOp match {
+                case TxCreate(_) if tableInfo(clazz).tables.nonEmpty =>
+                    list += Key(null, tablesObjectPath(clazz, id, version)) ->
+                            TxCreateNode()
+                    for ((name, provider) <- tableInfo(clazz).tables) {
+                        list += Key(null, tableRootPath(clazz, id, name, version)) ->
+                                TxCreateNode()
+                    }
+                case _ =>
+            }
+            list
+        }
+
+        /**
+          * Deletes asynchronously the state table paths for all deleted objects.
+          * If the deletion of a path fails, it is added to the `pathsToDelete`
+          * set, such that it can be deleted later.
+          */
+        protected def deleteStateTables(): Unit = {
+            executorService.submit(makeRunnable {
+                for ((Key(clazz, id), txOp) <- ops) txOp match {
+                    case TxDelete(_) =>
+                        val path = tablesObjectPath(clazz, id, version)
+                        try {
+                            ZKPaths.deleteChildren(
+                                curator.getZookeeperClient.getZooKeeper, path,
+                                true)
+                        } catch {
+                            case e: NoNodeException => // Path already deleted
+                            case e: KeeperException =>
+                                pathsToDelete.putIfAbsent(path, null)
+                            case NonFatal(e) => // Ignore any other exception
+                        }
+                    case _ =>
+                }
+            })
+        }
+
+    }
+
+    private val providers = new TrieMap[Key, Class[_ <: StateTable[_,_]]]
+
+    protected def rootPath: String
+
+    protected def curator: CuratorFramework
+
+    protected def version: AtomicLong
+
+    protected def reactor: Reactor
+
+    protected def connection: ZkConnection
+
+    protected def connectionWatcher: ZkConnectionAwareWatcher
+
+    @Deprecated
+    override def bridgeArpTable(bridgeId: UUID): StateTable[IPv4Addr, MAC] = ???
+
+    /**
+      * Registers a new state table for the given class. The state table is
+      * identified by the given key class, value class and name, and it
+      * associated with the specified provider class. The method throws an
+      * [[IllegalStateException]] if the storage was already built, and an
+      * [[IllegalArgumentException]] if the specified object class was not
+      * previously registered, or if a state table with same parameters was
+      * already registered.
+      */
+    @throws[IllegalStateException]
+    @throws[IllegalArgumentException]
+    override def registerTable[K, V](clazz: Class[_], key: Class[K],
+                                     value: Class[V], name: String,
+                                     provider: Class[_ <: StateTable[K,V]])
+    : Unit = {
+        if (isBuilt) {
+            throw new IllegalStateException(
+                "Cannot register a state table after building the storage")
+        }
+        if (!isRegistered(clazz)) {
+            throw new IllegalArgumentException(
+                s"Class ${clazz.getSimpleName} is not registered")
+        }
+        val newProvider = TableProvider(key, value, provider)
+        val oldProvider = tableInfo(clazz).tables.getOrElse(name, {
+            tableInfo(clazz).tables.putIfAbsent(name, newProvider)
+                                   .getOrElse(newProvider)
+        })
+        if (newProvider != oldProvider) {
+            throw new IllegalArgumentException(
+                s"Table for class ${clazz.getSimpleName} key ${key.getSimpleName} " +
+                s"value ${value.getSimpleName} name $name is already " +
+                "registered to a different provider")
+        }
+    }
+
+    /**
+      * Returns a [[StateTable]] instance for the specified object class,
+      * table name, object identifier and optional table arguments.
+      */
+    @throws[ServiceUnavailableException]
+    @throws[IllegalArgumentException]
+    @throws[NoSuchMethodException]
+    @throws[SecurityException]
+    def getTable[K, V](clazz: Class[_], id: ObjId, name: String, args: Any*)
+                      (implicit key: ClassTag[K], value: ClassTag[V])
+    : StateTable[K, V] = {
+        assertBuilt()
+        val path = if (args.nonEmpty) {
+            val p = tablePath(clazz, id, name, version.longValue(), args: _*)
+            ZKPaths.mkdirs(curator.getZookeeperClient.getZooKeeper, p, true)
+            p
+        } else {
+            tableRootPath(clazz, id, name)
+        }
+
+        // Get the provider for the state table name.
+        val provider = getProvider(clazz, key.runtimeClass, value.runtimeClass,
+                                   name)
+        // Create a new directory for the state table path.
+        val directory = new ZkDirectory(connection, path, Ids.OPEN_ACL_UNSAFE,
+                                        reactor)
+
+        // Get the constructor for the provider class and create a new instance.
+        val constructor =
+            provider.clazz.getConstructor(classOf[Directory],
+                                          classOf[ZkConnectionAwareWatcher])
+        constructor.newInstance(directory, connectionWatcher)
+                   .asInstanceOf[StateTable[K, V]]
+    }
+
+    @inline
+    private[storage] def tablesPath(version: Long = version.longValue())
+    : String = {
+        s"$rootPath/$version/tables"
+    }
+
+    @inline
+    private[storage] def tablesClassPath(clazz: Class[_],
+                                         version: Long = version.longValue())
+    : String = {
+        tablesPath(version) + "/" + clazz.getSimpleName
+    }
+
+    @inline
+    private[storage] def tablesObjectPath(clazz: Class[_], id: ObjId,
+                                          version: Long = version.longValue())
+    : String = {
+        tablesClassPath(clazz, version) + "/" + getIdString(clazz, id)
+    }
+
+    @inline
+    private[storage] def tableRootPath(clazz: Class[_], id: ObjId, name: String,
+                                       version: Long = version.longValue())
+    : String = {
+        tablesObjectPath(clazz, id, version) + "/" + name
+    }
+
+    @inline
+    private[storage] def tablePath(clazz: Class[_], id: ObjId, name: String,
+                                   version: Long, args: Any*): String = {
+        tableRootPath(clazz, id, name, version) + "/" +
+        StringUtils.join(args.asJava, '/')
+    }
+
+    /** Gets the table provider for the given object, key and value classes. */
+    @throws[IllegalArgumentException]
+    private def getProvider(clazz: Class[_], key: Class[_], value: Class[_],
+                            name: String): TableProvider = {
+        val provider = tableInfo.getOrElse(clazz, throw new IllegalArgumentException(
+            s"Class ${clazz.getSimpleName} is not registered")).tables
+                .getOrElse(name, throw new IllegalArgumentException(
+            s"Table $name is not registered for class ${clazz.getSimpleName}"))
+
+        if (provider.key != key) {
+            throw new IllegalArgumentException(
+                s"Table $name for class ${clazz.getSimpleName} has different " +
+                s"key class ${provider.key.getSimpleName}")
+        }
+        if (provider.value != value) {
+            throw new IllegalArgumentException(
+                s"Table $name for class ${clazz.getSimpleName} has different " +
+                s"value class ${provider.value.getSimpleName}")
+        }
+        provider
+    }
+
+}
---
+++ nsdb/src/test/scala/org/midonet/cluster/data/storage/ZookeeperStateTableTest.scala
@@ -0,0 +1,299 @@
+/*
+ * Copyright 2015 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.cluster.data.storage
+
+import java.util.UUID
+
+import scala.concurrent.duration._
+
+import org.apache.zookeeper.CreateMode
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{GivenWhenThen, Matchers, FeatureSpec}
+
+import rx.Observable
+
+import org.midonet.cluster.backend.Directory
+import org.midonet.cluster.backend.zookeeper.ZkConnectionAwareWatcher
+import org.midonet.cluster.data.storage.StateTable.Update
+import org.midonet.cluster.data.storage.StorageTestClasses.{PojoRouter, PojoBridge}
+import org.midonet.cluster.util.MidonetBackendTest
+import org.midonet.util.MidonetEventually
+
+object ZookeeperStateTableTest {
+    private class ScoreStateTable(val directory: Directory,
+                                  val connectionWatcher: ZkConnectionAwareWatcher)
+        extends StateTable[Int, String] {
+
+        override def start(): Unit = ???
+        override def stop(): Unit = ???
+        override def add(key: Int, value: String): Unit = ???
+        override def addPersistent(key: Int, value: String): Unit = ???
+        override def remove(key: Int): String = ???
+        override def remove(key: Int, value: String): String = ???
+        override def removePersistent(key: Int, value: String): String = ???
+        override def contains(key: Int): Boolean = ???
+        override def contains(key: Int, value: String): Boolean = ???
+        override def containsPersistent(key: Int, value: String): Boolean = ???
+        override def get(key: Int): String = ???
+        override def getByValue(value: String): Set[Int] = ???
+        override def snapshot: Map[Int, String] = ???
+        override def observable: Observable[Update[Int, String]] = ???
+    }
+}
+
+@RunWith(classOf[JUnitRunner])
+class ZookeeperStateTableTest extends FeatureSpec with MidonetBackendTest
+                              with Matchers with GivenWhenThen
+                              with MidonetEventually {
+
+    import ZookeeperStateTableTest.ScoreStateTable
+
+    private abstract class OtherStateTable extends StateTable[Int, String]
+    private abstract class OtherKeyStateTable extends StateTable[String, String]
+    private abstract class OtherValueStateTable extends StateTable[Int, Int]
+
+    private val namespaceId = UUID.randomUUID.toString
+    private final val timeout = 5 seconds
+
+    private def newStorage(): ZookeeperObjectMapper = {
+        new ZookeeperObjectMapper(zkRoot, namespaceId, curator, reactor,
+                                  connection, connectionWatcher)
+    }
+
+    private def setupStorage(): ZookeeperObjectMapper = {
+        val storage = newStorage()
+        storage.registerClass(classOf[PojoBridge])
+        storage.registerTable(classOf[PojoBridge], classOf[Int], classOf[String],
+                              "score-table", classOf[ScoreStateTable])
+        storage.build()
+        storage
+    }
+
+    feature("Input validation") {
+        scenario("Cannot register a state table after the storage was built") {
+            val storage = setupStorage()
+            intercept[IllegalStateException] {
+                storage.registerTable(classOf[PojoBridge], classOf[Int],
+                                      classOf[String], "another-table",
+                                      classOf[ScoreStateTable])
+            }
+        }
+
+        scenario("Cannot register a state table for a non-registered class") {
+            val storage = newStorage()
+            intercept[IllegalArgumentException] {
+                storage.registerTable(classOf[PojoRouter], classOf[Int],
+                                      classOf[String], "another-table",
+                                      classOf[ScoreStateTable])
+            }
+        }
+
+        scenario("Cannot register a state table for a different key") {
+            val storage = newStorage()
+            storage.registerClass(classOf[PojoBridge])
+            storage.registerTable(classOf[PojoBridge], classOf[Int],
+                                  classOf[String], "same-table",
+                                  classOf[ScoreStateTable])
+            intercept[IllegalArgumentException] {
+                storage.registerTable(classOf[PojoBridge], classOf[String],
+                                      classOf[String], "same-table",
+                                      classOf[OtherKeyStateTable])
+            }
+        }
+
+        scenario("Cannot register a state table for a different value") {
+            val storage = newStorage()
+            storage.registerClass(classOf[PojoBridge])
+            storage.registerTable(classOf[PojoBridge], classOf[Int],
+                                  classOf[String], "same-table",
+                                  classOf[ScoreStateTable])
+            intercept[IllegalArgumentException] {
+                storage.registerTable(classOf[PojoBridge], classOf[Int],
+                                      classOf[Int], "same-table",
+                                      classOf[OtherValueStateTable])
+            }
+        }
+
+        scenario("Cannot register a state table for a different provider") {
+            val storage = newStorage()
+            storage.registerClass(classOf[PojoBridge])
+            storage.registerTable(classOf[PojoBridge], classOf[Int],
+                                  classOf[String], "same-table",
+                                  classOf[ScoreStateTable])
+            intercept[IllegalArgumentException] {
+                storage.registerTable(classOf[PojoBridge], classOf[Int],
+                                      classOf[String], "same-table",
+                                      classOf[OtherStateTable])
+            }
+        }
+
+        scenario("Cannot get a table if the storage was not built") {
+            val storage = newStorage()
+            intercept[ServiceUnavailableException] {
+                storage.getTable[Int, String](classOf[PojoBridge], UUID.randomUUID(),
+                                              "score-table")
+            }
+        }
+    }
+
+    feature("Test state table paths") {
+        scenario("Class path exists") {
+            Given("The path for a registered class")
+            val storage = setupStorage()
+            val path = storage.tablesClassPath(classOf[PojoBridge])
+            Then("The path should exist")
+            curator.checkExists().forPath(path) should not be null
+        }
+
+        scenario("Table path created with object") {
+            Given("An object")
+            val storage = setupStorage()
+            val obj = new PojoBridge(UUID.randomUUID(), null, null, null)
+
+            And("The path for the table of this object")
+            val path = storage.tableRootPath(classOf[PojoBridge], obj.id,
+                                             "score-table")
+
+            When("The object is created")
+            storage.create(obj)
+
+            Then("The path should exist")
+            curator.checkExists().forPath(path) should not be null
+        }
+
+        scenario("Table path deleted with object") {
+            Given("An object in storage")
+            val storage = setupStorage()
+            val obj = new PojoBridge(UUID.randomUUID(), null, null, null)
+            storage.create(obj)
+
+            And("The path for the table of this object")
+            val path = storage.tableRootPath(classOf[PojoBridge], obj.id,
+                                             "score-table")
+
+            When("The object is deleted")
+            storage.delete(classOf[PojoBridge], obj.id)
+
+            Then("The path should not exist")
+            eventually {
+                curator.checkExists().forPath(path) shouldBe null
+            }
+        }
+    }
+
+    feature("Test state table") {
+        scenario("Fail to create a state table for non-existing class") {
+            val storage = setupStorage()
+            intercept[IllegalArgumentException] {
+                storage.getTable[Int, String](classOf[PojoRouter],
+                                              UUID.randomUUID(), "some-table")
+            }
+        }
+
+        scenario("Fail to create a state table for non-existing object") {
+            val storage = setupStorage()
+            intercept[IllegalArgumentException] {
+                storage.getTable[Int, String](classOf[PojoBridge],
+                                              UUID.randomUUID(), "some-table")
+            }
+        }
+
+        scenario("Fail to create a state table for non-existing name") {
+            Given("An object in storage")
+            val storage = setupStorage()
+            val obj = new PojoBridge(UUID.randomUUID(), null, null, null)
+            storage.create(obj)
+
+            Then("Retrieving the table with non-existing name should fail")
+            intercept[IllegalArgumentException] {
+                storage.getTable[Int, String](classOf[PojoBridge], obj.id,
+                                              "some-table")
+            }
+        }
+
+        scenario("Fail to create a state table for wrong key type") {
+            Given("An object in storage")
+            val storage = setupStorage()
+            val obj = new PojoBridge(UUID.randomUUID(), null, null, null)
+            storage.create(obj)
+
+            Then("Retrieving the table for wrong key type should fail")
+            intercept[IllegalArgumentException] {
+                storage.getTable[String, String](classOf[PojoBridge], obj.id,
+                                                 "score-table")
+            }
+        }
+
+        scenario("Fail to create a state table for wrong value type") {
+            Given("An object in storage")
+            val storage = setupStorage()
+            val obj = new PojoBridge(UUID.randomUUID(), null, null, null)
+            storage.create(obj)
+
+            Then("Retrieving the table for wrong value type should fail")
+            intercept[IllegalArgumentException] {
+                storage.getTable[Int, Int](classOf[PojoBridge], obj.id,
+                                           "score-table")
+            }
+        }
+
+        scenario("Storage creates a state table instance") {
+            Given("An object in storage")
+            val storage = setupStorage()
+            val obj = new PojoBridge(UUID.randomUUID(), null, null, null)
+            storage.create(obj)
+
+            When("Retrieving the table")
+            val table = storage.getTable[Int, String](classOf[PojoBridge],
+                                                      obj.id, "score-table")
+
+            Then("The table should not be null")
+            table should not be null
+
+            And("The table connection watcher should not be null")
+            val scoreTable = table.asInstanceOf[ScoreStateTable]
+            scoreTable.connectionWatcher shouldBe connectionWatcher
+
+            And("The table directory path should be the table path")
+            val path = storage.tableRootPath(classOf[PojoBridge], obj.id,
+                                             "score-table")
+            scoreTable.directory.getPath shouldBe path
+        }
+
+        scenario("State table directory writes to storage") {
+            Given("An object in storage")
+            val storage = setupStorage()
+            val obj = new PojoBridge(UUID.randomUUID(), null, null, null)
+            storage.create(obj)
+
+            When("Retrieving the table")
+            val table = storage.getTable[Int, String](classOf[PojoBridge],
+                                                      obj.id, "score-table")
+            val scoreTable = table.asInstanceOf[ScoreStateTable]
+
+            And("The table directory adds a node to storage")
+            scoreTable.directory.add("/test_node", null, CreateMode.PERSISTENT)
+
+            Then("The node should exist on the state table path")
+            val path = storage.tableRootPath(classOf[PojoBridge], obj.id,
+                                             "score-table")
+            curator.checkExists().forPath(path + "/test_node") should not be null
+        }
+    }
+
+}
