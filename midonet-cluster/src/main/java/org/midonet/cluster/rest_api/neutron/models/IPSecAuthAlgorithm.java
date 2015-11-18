b36712a68aec3322f1ed6851c28120a704e7aebd
Galo Navarro <galo@midokura.com>
2015-11-18 22:16:18 +0100

    VPNaaS: neutron models (proto + dto)
    These are to be used also by MidoNet, but defined as part of the Neutron
    model itself.
    
    Change-Id: Ie814b31aed2a609d3883f4007c157dbece9113ee
    Signed-off-by: Galo Navarro <galo@midokura.com>

---
+++ midonet-cluster/src/main/java/org/midonet/cluster/rest_api/neutron/models/IPSecAuthAlgorithm.java
@@ -0,0 +1,12 @@
+package org.midonet.cluster.rest_api.neutron.models;
+
+import org.midonet.cluster.data.ZoomEnum;
+import org.midonet.cluster.data.ZoomEnumValue;
+import org.midonet.cluster.models.Neutron;
+
+// TODO: this field and the enum is pointless, leave out and add whenever
+// needed?
+@ZoomEnum(clazz = Neutron.IPSecAuthAlgorithm.class)
+public enum IPSecAuthAlgorithm {
+    @ZoomEnumValue("SHA1") SHA1
+}
