5fc5286fdab31ddbcda67a761791ecd5e494fb47
Galo Navarro <galo@midokura.com>
2015-11-18 22:16:18 +0100

    VPNaaS: neutron models (proto + dto)
    These are to be used also by MidoNet, but defined as part of the Neutron
    model itself.
    
    Change-Id: Ie814b31aed2a609d3883f4007c157dbece9113ee
    Signed-off-by: Galo Navarro <galo@midokura.com>

---
+++ midonet-cluster/src/main/java/org/midonet/cluster/rest_api/neutron/models/IPSecEncryptionAlgorithm.java
@@ -0,0 +1,13 @@
+package org.midonet.cluster.rest_api.neutron.models;
+
+import org.midonet.cluster.data.ZoomEnum;
+import org.midonet.cluster.data.ZoomEnumValue;
+import org.midonet.cluster.models.Neutron;
+
+@ZoomEnum(clazz = Neutron.IPSecEncryptionAlgorithm.class)
+public enum IPSecEncryptionAlgorithm {
+    @ZoomEnumValue("DES_3") DES_3,
+    @ZoomEnumValue("AES_128") AES_128,
+    @ZoomEnumValue("AES_192") AES_192,
+    @ZoomEnumValue("AES_256") AES_256;
+}
