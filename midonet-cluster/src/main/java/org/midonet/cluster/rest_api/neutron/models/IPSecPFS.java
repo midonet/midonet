5c7a9272a56ec9d77b1f0e8ddd52939ebff523f2
Galo Navarro <galo@midokura.com>
2015-11-18 22:16:18 +0100

    VPNaaS: neutron models (proto + dto)
    These are to be used also by MidoNet, but defined as part of the Neutron
    model itself.
    
    Change-Id: Ie814b31aed2a609d3883f4007c157dbece9113ee
    Signed-off-by: Galo Navarro <galo@midokura.com>

---
+++ midonet-cluster/src/main/java/org/midonet/cluster/rest_api/neutron/models/IPSecPFS.java
@@ -0,0 +1,12 @@
+package org.midonet.cluster.rest_api.neutron.models;
+
+import org.midonet.cluster.data.ZoomEnum;
+import org.midonet.cluster.data.ZoomEnumValue;
+import org.midonet.cluster.models.Neutron;
+
+@ZoomEnum(clazz = Neutron.IPSecPFS.class)
+public enum IPSecPFS {
+    @ZoomEnumValue("GROUP_2") GROUP_2,
+    @ZoomEnumValue("GROUP_5") GROUP_5,
+    @ZoomEnumValue("GROUP_14") GROUP_14
+}
---
+++ midonet-cluster/src/main/java/org/midonet/cluster/rest_api/neutron/models/VPNService.java
@@ -0,0 +1,104 @@
+package org.midonet.cluster.rest_api.neutron.models;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
+import javax.ws.rs.core.UriBuilder;
+
+import org.midonet.cluster.data.ZoomClass;
+import org.midonet.cluster.data.ZoomField;
+import org.midonet.cluster.models.Neutron;
+import org.midonet.cluster.rest_api.models.UriResource;
+import org.midonet.cluster.util.IPAddressUtil;
+import org.midonet.cluster.util.UUIDUtil;
+import org.midonet.packets.IPAddr;
+
+@ZoomClass(clazz = Neutron.VPNService.class)
+public class VPNService extends UriResource {
+    @ZoomField(name = "id", converter = UUIDUtil.Converter.class)
+    public UUID id;
+
+    @ZoomField(name = "name")
+    public String name;
+
+    @ZoomField(name = "description")
+    public String description;
+
+    @ZoomField(name = "admin_state_up")
+    public Boolean adminStateUp;
+
+    @ZoomField(name = "tenant_id")
+    public String tenantId;
+
+    @ZoomField(name = "router_id", converter = UUIDUtil.Converter.class)
+    public UUID routerId;
+
+    @ZoomField(name = "subnet_id", converter = UUIDUtil.Converter.class)
+    public UUID subnetId;
+
+    @ZoomField(name = "status")
+    public String status;
+
+    @ZoomField(name = "external_ips", converter = IPAddressUtil.Converter.class)
+    public List<IPAddr> externalIps;
+
+    @Override
+    public URI getUri() {
+        if (getBaseUri() == null) {
+            return null;
+        } else {
+            // TODO: make relative based on Neutron.getUri()
+            return UriBuilder.fromUri(getBaseUri())
+                             .path("neutron")
+                             .path("vpnservices")
+                             .path(id.toString()).build();
+        }
+    }
+
+    @Override
+    public void create() {
+        if (id == null) {
+            id = UUID.randomUUID();
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        VPNService that = (VPNService) o;
+
+        // Treat empty list as equivalent to null
+        if (externalIps == null && that.externalIps != null
+            && !that.externalIps.isEmpty()) {
+            return false;
+        }
+        if (externalIps != null && !externalIps.isEmpty()
+            && that.externalIps == null) {
+            return false;
+        }
+
+        return Objects.equals(id, that.id) &&
+               Objects.equals(name, that.name) &&
+               Objects.equals(description, that.description) &&
+               Objects.equals(adminStateUp, that.adminStateUp) &&
+               Objects.equals(tenantId, that.tenantId) &&
+               Objects.equals(routerId, that.routerId) &&
+               Objects.equals(subnetId, that.subnetId) &&
+               Objects.equals(status, that.status);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description, adminStateUp, tenantId,
+                            routerId, subnetId, status, externalIps);
+    }
+}
+
