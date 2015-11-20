7871671ee03894979eee8432292475d1af45d625
Alex Bikfalvi <alex.bikfalvi@midokura.com>
2015-11-20 14:58:11 +0100

    VPNaaS: Introduce container delegates and handlers
    A container delegate specifies the cluster custom operations to be
    executed when a new container is created, set up, teared down or
    deleted. This accommodates containers of different types having to
    perform certain operations when a container state changes (e.g.
    modify objects in the topology, etc.).
    
    A container handler implements at the agent the operations to
    setup and teardown a container namespace, local veth interface,
    the configuration and execution of the programs associate with the
    container.
    
    This patch introduces a new annotation and interfaces for service
    delegates and handlers. At startup the container managament at both
    the cluster and agent will load all service delegate and handler
    implementation, respectively, from the current class path and apply
    their actions when the containers configuration and status has
    changed.
    
    Change-Id: I88985dc048e7c2b5b606207a24e14a966bfb594c
    Signed-off-by: Alex Bikfalvi <alex.bikfalvi@midokura.com>

---
+++ midolman/src/main/scala/org/midonet/midolman/topology/containers/ContainerHandler.scala
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2015 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.midolman.topology.containers
+
+import scala.concurrent.Future
+
+import rx.Observable
+
+import org.midonet.cluster.models.State.ContainerStatus
+
+/**
+  * A container handler provides a specific implementation for each container
+  * type at the agent side. A container handler should, among other things,
+  * create and delete the container namespace and VETH interface pair for a
+  * specified exterior port. In addition, the container handler will ensure
+  * that the appropriate services are started and configured for the given
+  * service type and configuration, will update the container status, and may
+  * provide container health-monitoring.
+  */
+trait ContainerHandler {
+
+    /**
+      * Creates a container for the specified exterior port and service
+      * container. The port contains the interface name that the container
+      * handler should create, and the method returns a future that completes
+      * with the namespace name when the container has been created.
+      */
+    def create(port: ContainerPort): Future[String]
+
+    /**
+      * Deletes the container for the specified exterior port and namespace
+      * information. The method returns a future that completes when the
+      * container has been deleted.
+      */
+    def delete(): Future[Void]
+
+    /**
+      * An observable that reports the health status of the container, which
+      * includes both the container namespace/interface as well as the
+      * service application executing within the container.
+      */
+    def health: Observable[ContainerStatus.Code]
+
+}
---
+++ midonet-cluster/src/main/scala/org/midonet/cluster/services/containers/ContainerDelegate.scala
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2015 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.cluster.services.containers
+
+import java.util.UUID
+
+import org.midonet.cluster.models.State.ContainerStatus
+import org.midonet.cluster.models.Topology.{ServiceContainerGroup, ServiceContainer}
+
+/**
+  * Allows the implementation of custom handlers for the service containers.
+  * Classes extending this trait may provide custom operations for container
+  * state transitions (e.g. a container is up, a container's location has
+  * changed, etc.).
+  *
+  * The cluster Container Management service will load from the current class
+  * path all service containers that are annotated with a
+  * [[org.midonet.cluster.services.Container]] annotation, indicating
+  * the container type name and version.
+  *
+  * The lifetime of a service container is the following:
+  *
+  *                (1) onCreate
+  *                The container is created via the
+  *                API and is being scheduled on
+  *   +---------+  a specific agent.    +---------+
+  *   | DELETED |---------------------->| CREATED |
+  *   |         |                       |         |
+  *   +---------+                       +---------+
+  *        | (4) onDelete                    | (2) onUp
+  *        | The container is deleted via    | The container has been connected
+  *        | API.                            | on the scheduled agent, and the
+  *        |                                 | agent reported the container up.
+  *   +---------+  (2) onUp             +---------+
+  *   |  DOWN   |---------------------->|   UP    |
+  *   |         |<----------------------|         |
+  *   +---------+  (3) onDown           +---------+
+  *                The container has been disconnected
+  *                from the scheduled agent, because the
+  *                container is being deleted, agent
+  *                reported status down and it is being
+  *                rescheduled on a different agent.
+  */
+trait ContainerDelegate {
+
+    /**
+      * Method called when the container is created in the backend. At this
+      * point the container has been scheduled on the specified agent. The
+      * implementation of this method must create a port bound to an interface
+      * name.
+      */
+    def onCreate(container: ServiceContainer, group: ServiceContainerGroup,
+                 hostId: UUID): Unit
+
+    /**
+      * Method called when the container namespace and veth port pair are
+      * created and connected on the scheduled agent, and the agent has reported
+      * the container status as UP. The method arguments will indicate the
+      * [[ContainerStatus]] with the host identifier, namespace and interface
+      * name where the container is connected and the container health status.
+      */
+    def onUp(container: ServiceContainer, group: ServiceContainerGroup,
+             status: ContainerStatus): Unit
+
+    /**
+      * Method called when the container is no longer available on the scheduled
+      * agent. This happens when (1) the agent goes offline (the host alive
+      * status is false), (2) the agent encountered a problem with creating or
+      * maintaining the container and reported status DOWN, or (3) the container
+      * has been deleted.
+      */
+    def onDown(container: ServiceContainer, group: ServiceContainerGroup,
+               status: ContainerStatus): Unit
+
+    /**
+      * Method called when the container has been deleted. The implementation
+      * of this method must delete the port created for this container.
+      */
+    def onDelete(container: ServiceContainer, group: ServiceContainerGroup,
+                 hostId: UUID): Unit
+
+}
