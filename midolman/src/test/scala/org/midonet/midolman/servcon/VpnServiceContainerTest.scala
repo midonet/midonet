6a4f47ba3c8d74bb244e8b0f32bd7dc4f3e9e7fa
Joe Mills <joe@midokura.com>
2015-11-25 16:19:49 +0900

    VPNaaS: Add wrappers for vpn-helper script
    * This patch adds the wrapper for the vpn-helper script. This
      means that a caller just has to call "start" and "stop" to start
      and stop a VPN service container on the host.
    * Added UT for the functions.
    
    Change-Id: I37723ee8aeca96c7222bfc6f325c5b0d9161bf5b

---
+++ midolman/src/test/scala/org/midonet/midolman/servcon/VpnServiceContainerTest.scala
@@ -0,0 +1,187 @@
+/*
+ * Copyright 2015 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.midonet.midolman.servcon
+
+import org.midonet.servcon._
+
+import org.junit.runner.RunWith
+import org.junit.Assert._
+import org.scalatest._
+import org.scalatest.concurrent.Eventually
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.time.SpanSugar
+
+
+@RunWith(classOf[JUnitRunner])
+class VpnServiceContainerTest extends FeatureSpecLike
+        with ShouldMatchers
+        with SpanSugar
+        with Eventually {
+
+    val vpnService = new VPNServiceDef("name", "/opt/stack/stuff",
+                                       "100.100.100.2", 24, "1.1.1.1",
+                                       "09:08:07:06:05:04")
+    val vpnConn = new VPNConnectionDef("test_conn", "200.200.200.2", "start",
+                                       "10.1.0.0/24", "10.2.0.0/24", 1500,
+                                       "hold", 30, 120,
+                                       "never", "pfs",
+                                       "esp", "aes128", "modp1536",
+                                       3600, "tunnel",
+                                       "3600", "secret")
+
+    feature("VpnServiceContainer writes contents of config files") {
+        scenario("VpnServiceContainer creates config for single conn") {
+            val expectedSecrets =
+                s"""100.100.100.2 200.200.200.2 : PSK "secret"
+                   |""".stripMargin
+            val expectedConf =
+                s"""config setup
+                   |    nat_traversal=yes
+                   |conn %default
+                   |    ikelifetime=480m
+                   |    keylife=60m
+                   |    keyingtries=%forever
+                   |conn test_conn
+                   |    leftnexthop=%defaultroute
+                   |    rightnexthop=%defaultroute
+                   |    left=100.100.100.2
+                   |    leftid=100.100.100.2
+                   |    auto=start
+                   |    leftsubnets={ 10.1.0.0/24 }
+                   |    leftupdown="ipsec_updown --route yes"
+                   |    right=200.200.200.2
+                   |    rightid=200.200.200.2
+                   |    rightsubnets={ 10.2.0.0/24 }
+                   |    mtu=1500
+                   |    dpdaction=hold
+                   |    dpddelay=30
+                   |    dpdtimeout=120
+                   |    authby=secret
+                   |    ikev2=never
+                   |    ike=aes128-sha1;modp1536
+                   |    ikelifetime=3600s
+                   |    auth=esp
+                   |    phase2alg=aes128-sha1;modp1536
+                   |    type=tunnel
+                   |    lifetime=3600s
+                   |""".stripMargin
+            val conf = new VpnServiceConfig("vpn-helper", vpnService,
+                                            List(vpnConn))
+            val actualConf = conf.getConfigFileContents
+            val actualSecrets = conf.getSecretsFileContents
+
+            assertEquals(expectedConf, actualConf)
+            assertEquals(expectedSecrets, actualSecrets)
+        }
+        scenario("VpnServiceContainer creates config for multiple conns") {
+            val expectedSecrets =
+                s"""100.100.100.2 200.200.200.2 : PSK "secret"
+                   |100.100.100.2 230.230.230.230 : PSK "secret"
+                   |""".stripMargin
+            val expectedConf =
+                s"""config setup
+                   |    nat_traversal=yes
+                   |conn %default
+                   |    ikelifetime=480m
+                   |    keylife=60m
+                   |    keyingtries=%forever
+                   |conn test_conn
+                   |    leftnexthop=%defaultroute
+                   |    rightnexthop=%defaultroute
+                   |    left=100.100.100.2
+                   |    leftid=100.100.100.2
+                   |    auto=start
+                   |    leftsubnets={ 10.1.0.0/24 }
+                   |    leftupdown="ipsec_updown --route yes"
+                   |    right=200.200.200.2
+                   |    rightid=200.200.200.2
+                   |    rightsubnets={ 10.2.0.0/24 }
+                   |    mtu=1500
+                   |    dpdaction=hold
+                   |    dpddelay=30
+                   |    dpdtimeout=120
+                   |    authby=secret
+                   |    ikev2=never
+                   |    ike=aes128-sha1;modp1536
+                   |    ikelifetime=3600s
+                   |    auth=esp
+                   |    phase2alg=aes128-sha1;modp1536
+                   |    type=tunnel
+                   |    lifetime=3600s
+                   |conn test_conn2
+                   |    leftnexthop=%defaultroute
+                   |    rightnexthop=%defaultroute
+                   |    left=100.100.100.2
+                   |    leftid=100.100.100.2
+                   |    auto=start
+                   |    leftsubnets={ 10.1.0.0/24 }
+                   |    leftupdown="ipsec_updown --route yes"
+                   |    right=230.230.230.230
+                   |    rightid=230.230.230.230
+                   |    rightsubnets={ 10.2.0.0/24 }
+                   |    mtu=1500
+                   |    dpdaction=hold
+                   |    dpddelay=30
+                   |    dpdtimeout=120
+                   |    authby=secret
+                   |    ikev2=never
+                   |    ike=aes128-sha1;modp1536
+                   |    ikelifetime=3600s
+                   |    auth=esp
+                   |    phase2alg=aes128-sha1;modp1536
+                   |    type=tunnel
+                   |    lifetime=3600s
+                   |""".stripMargin
+            val secondConn = vpnConn.copy
+            secondConn.name = "test_conn2"
+            secondConn.remotePeerIp = "230.230.230.230"
+            var conf = new VpnServiceConfig("vpn-helper", vpnService,
+                                            List(vpnConn, secondConn))
+            var actualConf = conf.getConfigFileContents
+            assertEquals(expectedConf, actualConf)
+        }
+    }
+
+    feature("Vpn script starts and stops") {
+        scenario("commands are correctly executred") {
+            var conf = new VpnServiceConfig("vpn-helper", vpnService, List(vpnConn))
+            TestVpnServiceContainter.start(conf)
+            assertEquals(TestVpnServiceContainter.cmdList(0),
+                "vpn-helper makens -n name -g 1.1.1.1 -l 100.100.100.2 -i 100.100.100.2 -m 09:08:07:06:05:04")
+            assertEquals(TestVpnServiceContainter.cmdList(1),
+                "vpn-helper start_service -n name -p /opt/stack/stuff")
+            assertEquals(TestVpnServiceContainter.cmdList(2),
+                "vpn-helper init_conns -n name -p /opt/stack/stuff -c test_conn")
+
+            TestVpnServiceContainter.stop(conf)
+
+            assertEquals(TestVpnServiceContainter.cmdList(3),
+                "vpn-helper stop_service -n name -p /opt/stack/stuff")
+            assertEquals(TestVpnServiceContainter.cmdList(4),
+                "vpn-helper cleanns -n name")
+        }
+    }
+
+    object TestVpnServiceContainter extends VpnServiceContainerFunctions {
+        var cmdList = List[String]()
+        override def execCmd(cmd: String) = {
+            cmdList ++= List(cmd)
+        }
+        override def writeFile(contents: String, location: String): Boolean = {
+            true
+        }
+    }
+}
