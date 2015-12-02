12cf72ba9cb9874f81dae70a1dc7633fe91e83c5
Brandon Berg <bberg@midokura.com>
2015-12-02 19:19:13 +0900

    WIP: VPNService translation
    Untested and will probably set your computer on fire. Do not merge.
    
    Change-Id: I7026a30fb6a849115ba1164bb56cd33270332493

--- midonet-cluster/src/main/java/org/midonet/cluster/rest_api/neutron/models/IPSecPFS.java
+++ midonet-cluster/src/main/java/org/midonet/cluster/rest_api/neutron/models/IPSecPFS.java
@@ -1,12 +1,20 @@
 package org.midonet.cluster.rest_api.neutron.models;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 import org.midonet.cluster.data.ZoomEnum;
 import org.midonet.cluster.data.ZoomEnumValue;
 import org.midonet.cluster.models.Neutron;
 
 @ZoomEnum(clazz = Neutron.IPSecPFS.class)
 public enum IPSecPFS {
-    @ZoomEnumValue("GROUP_2") GROUP_2,
-    @ZoomEnumValue("GROUP_5") GROUP_5,
-    @ZoomEnumValue("GROUP_14") GROUP_14
+    @ZoomEnumValue("GROUP2") GROUP2,
+    @ZoomEnumValue("GROUP5") GROUP5,
+    @ZoomEnumValue("GROUP14") GROUP14;
+
+    @JsonCreator
+    @SuppressWarnings("unused")
+    public static IPSecPFS forValue(String v) {
+        return valueOf(IPSecSiteConnection.convertFromIpsecString(v));
+    }
 }
