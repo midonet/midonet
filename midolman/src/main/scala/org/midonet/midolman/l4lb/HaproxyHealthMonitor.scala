d3d3e4914c3281cc833fdead0a20c28254a11ac6
Nicolas Schiper <nicolas@midokura.com>
2015-12-10 18:29:42 +0100

    Improving test coverage of HA health monitor proxy
    This patch re-enables unit tests for the HA proxy health monitor and
    also complements these tests.
    
    Change-Id: I80d4231ff81f46c1fc5bf617d3ce11e5d95c522d
    Signed-off-by: Nicolas Schiper <nicolas@midokura.com>

--- midolman/src/main/scala/org/midonet/midolman/l4lb/HaproxyHealthMonitor.scala
+++ midolman/src/main/scala/org/midonet/midolman/l4lb/HaproxyHealthMonitor.scala
@@ -74,7 +74,7 @@ object HaproxyHealthMonitor {
     // Tells this actor that the config for the health monitor has changed.
     case class ConfigUpdate(conf: PoolConfig) extends HHMMessage
     // Tells this actor to poll haproxy once for health info.
-    private[HaproxyHealthMonitor] case object CheckHealth extends HHMMessage
+    case object CheckHealth extends HHMMessage
     // Tells this actor that its router has been removed
     case object RouterRemoved
     // Tells this actor that it now has a router
