b52ad808637ec142ebac7c3f723e3fa5d871eb84
Xavi León <xavi.leon@midokura.com>
2015-12-11 16:42:08 +0100

    VPNaaS: container service and scheduler integration
    This patch starts and stops the scheduling of containers upon starting
    and stopping the container service minion. When a new event is emitted
    by the scheduler, the minion interprets it and forwards the necessary
    information to the specific container delegate implementation.
    
    Change-Id: I31ea190a79d4e3744528e50d05acdc04e5287ae1
    Signed-off-by: Xavi León <xavi.leon@midokura.com>
    Signed-off-by: Alex Bikfalvi <alex.bikfalvi@midokura.com>

--- midonet-cluster/src/main/scala/org/midonet/cluster/services/containers/ContainerService.scala
+++ midonet-cluster/src/main/scala/org/midonet/cluster/services/containers/ContainerService.scala
@@ -16,15 +16,32 @@
 
 package org.midonet.cluster.services.containers
 
-import com.codahale.metrics.MetricRegistry
+import java.util.concurrent.Executors
+
+import scala.collection.concurrent.TrieMap
+import scala.util.control.NonFatal
+
 import com.google.inject.Inject
 import com.typesafe.scalalogging.Logger
 
 import org.slf4j.LoggerFactory
 
+import rx.Subscriber
+import rx.schedulers.Schedulers
+
 import org.midonet.cluster.ClusterNode.Context
+import org.midonet.cluster.models.Topology.ServiceContainer
+import org.midonet.cluster.services.containers.schedulers._
+import org.midonet.cluster.services.{ClusterService, MidonetBackend, Minion}
 import org.midonet.cluster.{ClusterConfig, containersLog}
-import org.midonet.cluster.services.{ClusterService, Minion}
+import org.midonet.util.concurrent.NamedThreadFactory
+import org.midonet.util.functors.makeAction0
+
+object ContainerService {
+
+    private val SchedulingBufferSize = 0x4000
+
+}
 
 /**
   * This is the cluster service for container management across the MidoNet
@@ -34,20 +51,100 @@ import org.midonet.cluster.services.{ClusterService, Minion}
   */
 @ClusterService(name = "containers")
 class ContainerService @Inject()(nodeContext: Context,
-                                 config: ClusterConfig,
-                                 metrics: MetricRegistry)
+                                 backend: MidonetBackend,
+                                 config: ClusterConfig)
     extends Minion(nodeContext) {
+
+    import ContainerService._
 
     private val log = Logger(LoggerFactory.getLogger(containersLog))
 
     override def isEnabled = config.containers.isEnabled
 
-    override def doStart(): Unit = {
-        log info "Starting Container Management service"
+    private val eventExecutor = Executors.newSingleThreadExecutor(
+        new NamedThreadFactory("container-event", isDaemon = true))
+    private val eventScheduler = Schedulers.from(eventExecutor)
+
+    private val delegateExecutor = Executors.newSingleThreadExecutor(
+        new NamedThreadFactory("container-delegate", isDaemon = true))
+    private val delegateScheduler = Schedulers.from(delegateExecutor)
+
+    private val delegateProvider = new ContainerDelegateProvider(backend,
+                                                                 config, log)
+    private val delegates = new TrieMap[String, ContainerDelegate]
+
+    private val context = schedulers.Context(backend.store, backend.stateStore,
+                                             eventExecutor, eventScheduler, log)
+
+    private val scheduler = newScheduler()
+    private val schedulerSubscriber = new Subscriber[SchedulerEvent] {
+        override def onNext(event: SchedulerEvent): Unit = {
+            log debug s"Container scheduling event: $event"
+            try {
+                event match {
+                    case ScheduleEvent(container, hostId) =>
+                        delegateOf(container).onScheduled(container, hostId)
+                    case UpEvent(container, status) =>
+                        delegateOf(container).onUp(container, status)
+                    case DownEvent(container, status) =>
+                        delegateOf(container).onDown(container, status)
+                    case UnscheduleEvent(container, hostId) =>
+                        delegateOf(container).onUnscheduled(container, hostId)
+                }
+            } catch {
+                case NonFatal(e) =>
+                    log.warn("Container delegate failed during scheduling " +
+                             s"event: $event", e)
+            }
+        }
+        override def onCompleted(): Unit = {
+            log info "Containers notification stream completed"
+        }
+        override def onError(e: Throwable): Unit = {
+            log.error("Unexpected error on the container notification stream", e)
+        }
+    }
+
+    /** Indicates whether the service is unsubscribed from container scheduling
+      * notifications.
+      */
+    def isUnsubscribed = schedulerSubscriber.isUnsubscribed
+
+    protected override def doStart(): Unit = {
+        log info "Starting container management service"
+
+        scheduler.observable
+                 .onBackpressureBuffer(SchedulingBufferSize, makeAction0 {
+                     log error "Scheduling buffer overflow"
+                 })
+                 .observeOn(delegateScheduler)
+                 .subscribe(schedulerSubscriber)
         notifyStarted()
     }
 
-    override def doStop(): Unit = {
+    protected override def doStop(): Unit = {
+        log info "Stopping container management service"
+        schedulerSubscriber.unsubscribe()
+        scheduler.complete()
         notifyStopped()
+    }
+
+    protected def newScheduler(): ServiceScheduler = {
+        new ServiceScheduler(context, config.containers)
+    }
+
+    @inline
+    protected def getDelegate(container: ServiceContainer)
+    : Option[ContainerDelegate] = {
+        delegates get container.getServiceType
+    }
+
+    @inline
+    protected def delegateOf(container: ServiceContainer): ContainerDelegate = {
+        delegates.getOrElse(container.getServiceType, {
+            val delegate = delegateProvider.getInstance(container.getServiceType)
+            delegates.putIfAbsent(container.getServiceType, delegate)
+                     .getOrElse(delegate)
+        })
     }
 }
