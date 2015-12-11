fe4abd15fb40e628728e7ff1fb876e015a045fe8
Nicolas Schiper <nicolas@midokura.com>
2015-12-11 16:54:34 +0100

    Always initialize the packet context
    This patch initializes the packet context in the simulation router, a
    previously missing operation.
    
    Change-Id: Ifdbe7658067488d4f0cbc44be60f86e5d17600d5
    Signed-off-by: Nicolas Schiper <nicolas@midokura.com>

---
+++ midolman/src/main/scala/org/midonet/midolman/state/NoOpNatLeaser.scala
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2015 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.midolman.state
+
+import java.util.UUID
+
+import com.typesafe.scalalogging.Logger
+
+import org.midonet.midolman.rules.NatTarget
+import org.midonet.midolman.state.NatState.NatBinding
+import org.midonet.packets.IPv4Addr
+import org.midonet.util.concurrent.NanoClock
+
+object NoOpNatLeaser extends NatLeaser {
+
+    override val allocator: NatBlockAllocator = null
+    override val log: Logger = null
+    override val clock: NanoClock = null
+
+    private val binding = NatBinding(IPv4Addr.fromString("0.0.0.0"), 0)
+
+    override def allocateNatBinding(deviceId: UUID,
+                                    destinationIp: IPv4Addr,
+                                    destinationPort: Int,
+                                    natTargets: Array[NatTarget]): NatBinding =
+        binding
+
+    override def freeNatBinding(deviceId: UUID,
+                                destinationIp: IPv4Addr,
+                                destinationPort: Int,
+                                binding: NatBinding): Unit = {}
+
+    override def obliterateUnusedBlocks(): Unit = {}
+}
