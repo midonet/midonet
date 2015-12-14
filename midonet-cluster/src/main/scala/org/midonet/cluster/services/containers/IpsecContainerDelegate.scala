e1ccd7129841b975e75d2a9693cfc9b82241db4d
Xavi León <xavi.leon@midokura.com>
2015-12-14 10:40:57 +0100

    VPNaaS: IPsec container delegate initial implementation
    This initial implementation provides the means to create and remove a
    binding when containers are allocated and deallocated to hosts. This
    patch also introduces a factory class to instantiate delegates
    depending on the type of container (which will be superseeded when
    rebased on top of another patch that provides guice injection for
    container delegates).
    
    Change-Id: I312a1134c3aa500905968483f5659aae4b56b628
    Signed-off-by: Xavi León <xavi.leon@midokura.com>

---
+++ midonet-cluster/src/main/scala/org/midonet/cluster/services/containers/IpsecContainerDelegate.scala
@@ -0,0 +1,117 @@
+
+/*
+ * Copyright 2015 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.cluster.services.containers
+
+import java.util.UUID
+
+import com.google.inject.Inject
+import com.typesafe.scalalogging.Logger
+
+import org.apache.commons.lang.NotImplementedException
+import org.slf4j.LoggerFactory
+
+import org.midonet.cluster.containersLog
+import org.midonet.cluster.data.storage.{NotFoundException, Storage}
+import org.midonet.cluster.models.State.ContainerStatus
+import org.midonet.cluster.models.Topology.{Port, ServiceContainer}
+import org.midonet.cluster.util.UUIDUtil._
+import org.midonet.cluster.util.logging.ProtoTextPrettifier._
+import org.midonet.containers.Container
+
+@Container(name = "IPSEC", version = 1)
+class IpsecContainerDelegate @Inject()(store: Storage) extends ContainerDelegate {
+
+    private val log = Logger(LoggerFactory.getLogger(containersLog))
+
+    /**
+      * Method called when the container is created in the backend. At this
+      * point the container has been scheduled on the specified agent. The
+      * implementation of this method must create a port bound to an interface
+      * name.
+      */
+    override def onCreate(container: ServiceContainer, hostId: UUID): Unit = {
+        log.debug(s"Allocation event: container ${container.getId.asJava}" +
+                  s"about to be scheduled on host $hostId (create port binding).")
+        // TODO FIXME: probably need to check that (like HostInterfacePortResource):
+        // 1) the host is in a tunnel zone
+        // 2) the port is not already bound
+        // 3) the interface is not already bound to another port
+        val tx = store.transaction()
+        val port = tx.get(classOf[Port], container.getPortId)
+        val newport = port.toBuilder
+            .setId(container.getPortId)
+            .setHostId(hostId.asProto)
+            .setInterfaceName(s"vpn-${port.getId.asJava.toString.substring(0,8)}_dp")
+            .build()
+
+        try {
+            tx.update(newport)
+            tx.commit()
+        } catch {
+            case nfe: NotFoundException =>
+                log.info(s"Port ${makeReadable(port.getId)} not found.")
+        }
+    }
+
+    /**
+      * Method called when the container namespace and veth port pair are
+      * created and connected on the scheduled agent, and the agent has reported
+      * the container status as UP. The method arguments will indicate the
+      * [[ContainerStatus]] with the host identifier, namespace and interface
+      * name where the container is connected and the container health status.
+      */
+    override def onUp(container: ServiceContainer,
+                      status: ContainerStatus): Unit = {
+        throw new NotImplementedException()
+    }
+
+    /**
+      * Method called when the container has been deleted. The implementation
+      * of this method must delete the port created for this container.
+      */
+    override def onDelete(container: ServiceContainer, hostId: UUID): Unit = {
+        log.debug(s"Deallocation event: container ${container.getId.asJava}" +
+                  s"about to be removed from host $hostId (delete port binding).")
+        val tx = store.transaction()
+        val port = tx.get(classOf[Port], container.getPortId)
+        val newport = port.toBuilder
+            .clearHostId()
+            .clearInterfaceName()
+            .build()
+
+        try {
+            tx.update(newport)
+            tx.commit()
+        } catch {
+            case nfe: NotFoundException =>
+                log.info(s"Port ${makeReadable(port.getId)} not found.")
+        }
+    }
+
+    /**
+      * Method called when the container is no longer available on the scheduled
+      * agent. This happens when (1) the agent goes offline (the host alive
+      * status is false), (2) the agent encountered a problem with creating or
+      * maintaining the container and reported status DOWN, or (3) the container
+      * has been deleted.
+      */
+    override def onDown(container: ServiceContainer,
+                        status: ContainerStatus): Unit = {
+        throw new NotImplementedException()
+    }
+}
