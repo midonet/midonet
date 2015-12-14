6c5d4a9dc5d21c39fcac251abdb4467ff23bb194
Xavi León <xavi.leon@midokura.com>
2015-12-14 10:40:57 +0100

    VPNaaS: IPSec container delegate
    This initial implementation provides the means to create and remove a
    binding when containers are allocated and deallocated to hosts. This
    patch anotates the class with the Container annotation so the container
    service can load it when a container of type "IPSEC" needs to do some
    actions dictated by the scheduler. Failover actions (onUp and onDown)
    will be added on a later patch.
    
    Change-Id: I312a1134c3aa500905968483f5659aae4b56b628
    Signed-off-by: Xavi León <xavi.leon@midokura.com>

---
+++ midonet-cluster/src/main/scala/org/midonet/cluster/services/containers/IpsecContainerDelegate.scala
@@ -0,0 +1,126 @@
+
+/*
+ * Copyright 2015 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.cluster.services.containers
+
+import java.util.{ConcurrentModificationException, UUID}
+
+import javax.annotation.Nullable
+
+import com.google.inject.Inject
+import com.typesafe.scalalogging.Logger
+
+import org.slf4j.LoggerFactory
+
+import org.midonet.cluster.containersLog
+import org.midonet.cluster.data.storage.{NotFoundException, Storage, Transaction}
+import org.midonet.cluster.models.State.ContainerStatus
+import org.midonet.cluster.models.Topology.{Host, Port, ServiceContainer}
+import org.midonet.cluster.util.UUIDUtil._
+import org.midonet.containers.{Container, Containers}
+
+@Container(name = Containers.IPSEC_CONTAINER, version = 1)
+class IpsecContainerDelegate @Inject()(store: Storage)
+    extends ContainerDelegate {
+
+    private val log = Logger(LoggerFactory.getLogger(s"$containersLog.ipsec"))
+    private final val storageAttempts = 10
+
+    /** This method is called when the container is scheduled at a specified
+      * host. It binds the container port to the host
+      */
+    @throws[Exception]
+    override def onScheduled(container: ServiceContainer, hostId: UUID): Unit = {
+        val containerId = container.getId.asJava
+        if (!container.hasPortId) {
+            throw new IllegalArgumentException(
+                s"Container $containerId is not connected to a port")
+        }
+        val portId = container.getPortId.asJava
+        val interfaceName = s"vpn-${portId.toString.substring(0, 8)}"
+
+        log info s"Container ${container.getId.asJava} scheduled at host" +
+                 s"$hostId: binding port $portId to interface $interfaceName"
+        tryTx { tx =>
+            tx update tx.get(classOf[Port], portId).toBuilder
+                .setHostId(hostId.asProto)
+                .setInterfaceName(interfaceName)
+                .build()
+        }
+    }
+
+    /** This method is called when the container is reported UP at the host
+      * where it was scheduled.
+      */
+    override def onUp(container: ServiceContainer, status: ContainerStatus): Unit = {
+        log debug s"Container up at host ${status.getHostId.asJava} namespace " +
+                  s"${status.getNamespaceName} interface ${status.getInterfaceName}"
+    }
+
+    /** This method is called when the container is reported DOWN at the host
+      * where it was scheduled.
+      */
+    override def onDown(container: ServiceContainer,
+                        @Nullable status: ContainerStatus): Unit = {
+        val hostId = if (status ne null) status.getHostId.asJava else null
+        log debug s"Container down at host $hostId"
+    }
+
+    /** Method called when the container has been unscheduled from the
+      * specified host.
+      */
+    @throws[Exception]
+    override def onUnscheduled(container: ServiceContainer, hostId: UUID): Unit = {
+        val containerId = container.getId.asJava
+        if (!container.hasPortId) {
+            throw new IllegalArgumentException(
+                s"Container $containerId is not connected to a port")
+        }
+
+        log info s"Container $containerId unscheduled from host $hostId: " +
+                 "unbinding port"
+        tryTx { tx =>
+            val port = tx.get(classOf[Port], container.getPortId)
+            if (!port.hasHostId || port.getHostId.asJava != hostId) {
+                throw new NotFoundException(classOf[Host], hostId.asProto)
+            }
+            tx update port.toBuilder
+                          .clearHostId()
+                          .clearInterfaceName()
+                          .build()
+        }
+    }
+
+    private def tryTx(f: (Transaction) => Unit): Unit = {
+        var attempt = 1
+        while (true) {
+            try {
+                val tx = store.transaction()
+                f(tx)
+                tx.commit()
+                return
+            } catch {
+                case e: ConcurrentModificationException
+                    if attempt < storageAttempts =>
+                    log warn s"Write $attempt of $storageAttempts failed due " +
+                             s"to a concurrent modification (${e.getMessage})"
+                    attempt += 1
+            }
+        }
+    }
+
+}
