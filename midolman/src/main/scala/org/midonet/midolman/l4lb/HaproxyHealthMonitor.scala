3f2d6726e3700f95d6bd5a4b4be68c4b5ab45d6b
Nicolas Schiper <nicolas@midokura.com>
2015-12-22 15:22:13 +0100

    HA proxy Pool status bug
    This patch fixes a bug in the HA proxy health monitor, where the
    status of a pool was previously set to INACTIVE instead of ACTIVE
    when receiving a RouterAdded message.
    
    Change-Id: I3ecd8dccf05606a683ef568b680e209da2920c05
    Signed-off-by: Nicolas Schiper <nicolas@midokura.com>

--- midolman/src/main/scala/org/midonet/midolman/l4lb/HaproxyHealthMonitor.scala
+++ midolman/src/main/scala/org/midonet/midolman/l4lb/HaproxyHealthMonitor.scala
@@ -242,7 +242,7 @@ class HaproxyHealthMonitor(var config: PoolConfig,
             routerId = newRouterId
             try {
                 hookNamespaceToRouter()
-                setPoolMappingStatus(config.id, INACTIVE,
+                setPoolMappingStatus(config.id, ACTIVE,
                                      rethrowException = true)
             } catch {
                 case NonFatal(e) =>
