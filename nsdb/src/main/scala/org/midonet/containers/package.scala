817ce4f7fdd1a565f83c0ead6b85e34f2a189958
Xavi León <xavi.leon@midokura.com>
2016-01-11 11:24:49 +0100

    VPNaaS: make sure container port/ip on router is unused
    Right now, we use a fixed subnet, and ips for the port connected to the
    vpn container. This patch makes sure the subnet/ip used for the
    container port is not used by looking for a free subnet in a subnet
    range reserved for containers. This way, other containers can take
    advantage of this allocation mechanism and prevents clashing of
    unrelated containers types (e.g. vpn container and haproxy containers
    once they are moved to the container framework).
    
    Change-Id: I3e58f87cc707b283a61726b379ad0c55d4973e5b
    Signed-off-by: Xavi León <xavi.leon@midokura.com>

--- nsdb/src/main/scala/org/midonet/containers/package.scala
+++ nsdb/src/main/scala/org/midonet/containers/package.scala
@@ -17,12 +17,17 @@
 package org.midonet
 
 import java.util.concurrent.ExecutorService
+import java.util.{Comparator, NoSuchElementException, TreeMap}
 
 import com.typesafe.scalalogging.Logger
 
 import rx.Scheduler
 
 import org.midonet.cluster.data.storage.{StateStorage, Storage}
+import org.midonet.cluster.models.Commons.{IPAddress, IPSubnet, IPVersion}
+import org.midonet.cluster.models.Topology.Port
+import org.midonet.cluster.util.{IPAddressUtil, IPSubnetUtil}
+import org.midonet.packets.{IPv4Addr, IPv4Subnet}
 
 package object containers {
 
@@ -35,5 +40,76 @@ package object containers {
                        executor: ExecutorService,
                        scheduler: Scheduler,
                        log: Logger)
+
+    private val LocalSubnetBeginAddress = 0xA9FE0000 //169.254.0.0
+    private val LocalSubnetEndAddress = 0xA9FEFFFC // 169.254.255.252
+    private val LocalSubnetPrefix = 30
+    private val LocalSubnetMask = 0xFFFFFFFF << (32 - LocalSubnetPrefix)
+    private val LocalSubnetSize = 0xFFFFFFFF >>> LocalSubnetPrefix
+    private val RouterAddressOffset = 1
+    private val ContainerAddressOffset = 2
+
+    @throws[NoSuchElementException]
+    def findLocalSubnet(ports: Seq[Port]): IPSubnet = {
+        // Finds an available 169.254.x.y/30 (0xA9FE.x.y/30) subnet within the
+        // 169.254.0.0/16 range excluding the subnets of the router ports that
+        // already have been allocated within the same range. The method works
+        // by storing the allocated subnets in an ordered map, where the key is
+        // the beginning of an allocated range, and the value is end, and then
+        // iterating through the allocated ranges to find an available /30
+        // range with a 0xA9FE prefix.
+        val subnets = new TreeMap[Int, Int](
+            new Comparator[Int]() {
+                @Override
+                def compare(a: Int, b: Int): Int = {
+                    return Integer.compareUnsigned(a, b)
+                }
+            })
+        for (port <- ports if port.getPortSubnet.getVersion == IPVersion.V4) {
+            val portSubnet = new IPv4Subnet(port.getPortSubnet.getAddress,
+                                            port.getPortSubnet.getPrefixLength)
+            val subnetBegin = portSubnet.toNetworkAddress.toInt
+            val subnetEnd = portSubnet.toBroadcastAddress.toInt
+            subnets.put(subnetBegin, subnetEnd)
+        }
+        // We begin the iteration from the first eligible address: 169.254.0.0
+        // and the last eligible subnet address is: 169.254.255.252.
+        var selection = LocalSubnetBeginAddress
+        while (Integer.compareUnsigned(selection, LocalSubnetEndAddress) < 0) {
+            // Get the first allocated range at an address equal to or greater
+            // than the selected subnet.
+            val subnet = subnets.ceilingEntry(selection)
+            // If such an entry is not allocated or its beginning address is
+            // greater than the end of the /30 we selected, return the current
+            // selection.
+            if ((subnet eq null) ||
+                Integer.compareUnsigned(subnet.getKey, selection + LocalSubnetSize) > 0)
+                return IPSubnetUtil.toProto(new IPv4Subnet(selection,
+                                                           LocalSubnetPrefix))
+            // Else, select the beginning of next /30 greater than the end of
+            // the current entry range.
+            selection = (subnet.getValue & LocalSubnetMask) + LocalSubnetSize + 1
+        }
+        throw new NoSuchElementException("There is no free /30 local subnet in " +
+                                         "the router for the 169.254.0.0/16 range.")
+    }
+
+    /**
+      * Get the associated ip address of the router port for a given container
+      * subnet.
+      */
+    def routerPortAddress(subnet: IPSubnet): IPAddress = {
+        val s = new IPv4Subnet(subnet.getAddress, LocalSubnetPrefix)
+        IPAddressUtil.toProto(IPv4Addr(s.getIntAddress + RouterAddressOffset))
+    }
+
+    /**
+      * Get the associated up address of the container port for a given
+      * container subnet. This is the ip inside the container namespace.
+      */
+    def containerPortAddress(subnet: IPSubnet): IPAddress = {
+        val s = new IPv4Subnet(subnet.getAddress, LocalSubnetPrefix)
+        IPAddressUtil.toProto(IPv4Addr(s.getIntAddress + ContainerAddressOffset))
+    }
 
 }
