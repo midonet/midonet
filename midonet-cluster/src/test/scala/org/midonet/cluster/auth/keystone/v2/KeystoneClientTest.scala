4c6b998ce941d50d5ddd34cff3adf237f8e87886
Alex Bikfalvi <alex.bikfalvi@midokura.com>
2016-01-18 19:18:05 +0100

    Keystone: Mock Keystone server and unit tests for V2 client
    This patch adds a server that implements a subset of the Keystone
    v2 API, and unit tests for the Keystone client. The mock server
    is a Jersey HTTP server that simulates the user authentication.
    
    Change-Id: I06a4477b93ba379b239325a298d3c1b426878507
    Signed-off-by: Alex Bikfalvi <alex.bikfalvi@midokura.com>

--- midonet-cluster/src/test/scala/org/midonet/cluster/auth/keystone/v2/KeystoneClientTest.scala
+++ midonet-cluster/src/test/scala/org/midonet/cluster/auth/keystone/v2/KeystoneClientTest.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Midokura SARL
+ * Copyright 2016 Midokura SARL
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,31 +16,20 @@
 
 package org.midonet.cluster.auth.keystone.v2
 
+import java.text.ParsePosition
+
+import com.fasterxml.jackson.databind.util.ISO8601Utils
 import com.typesafe.config.ConfigFactory
 
-import org.scalatest.{GivenWhenThen, FlatSpec, Matchers}
-import org.slf4j.bridge.SLF4JBridgeHandler
+import org.junit.runner.RunWith
+import org.scalatest.{GivenWhenThen, Matchers}
+import org.scalatest.junit.JUnitRunner
 
+import org.midonet.cluster.auth.keystone.v2.KeystoneTest.DateFormat
 import org.midonet.conf.MidoTestConfigurator
 
-/**
- * Provides integration tests for the Keystone v2 client. The tests require
- * a Keystone server with the configuration and at least one administrative
- * tenant as specified below.
- */
-class KeystoneClientTest extends FlatSpec with Matchers with GivenWhenThen {
-
-    // Install the SLF4J handler for the legacy loggers used in the API.
-    SLF4JBridgeHandler.removeHandlersForRootLogger()
-    SLF4JBridgeHandler.install()
-
-    private val keystoneProtocol = "http"
-    private val keystoneHost = "127.0.0.1"
-    private val keystonePort = 35357
-    private val keystoneTenant = "admin"
-    private val keystoneUser = "admin"
-    private val keystonePassword = "midonet"
-    private val keystoneToken = "somelongtesttoken"
+@RunWith(classOf[JUnitRunner])
+class KeystoneClientTest extends KeystoneTest with Matchers with GivenWhenThen {
 
     private def clientWithoutAdmin: KeystoneClient = {
         val configStr =
@@ -97,7 +86,62 @@ class KeystoneClientTest extends FlatSpec with Matchers with GivenWhenThen {
                                                  keystonePassword)
         keystoneAccess.access.token should not be null
         keystoneAccess.access.user.userName shouldBe keystoneUser
-        keystoneAccess.access.user.userName shouldBe keystoneUser
+    }
+
+    "Client" should "parse ISO8601 date format UTC" in {
+        val date = KeystoneClient.parseTimestamp("2016-01-01T00:00:00Z")
+        date shouldBe 1451606400000L
+    }
+
+    "Client" should "parse ISO8601 date format UTC with microseconds" in {
+        val date = KeystoneClient.parseTimestamp("2016-01-01T00:00:00.123000Z")
+        date shouldBe 1451606400123L
+    }
+
+    "Client" should "parse ISO8601 date format no time zone" in {
+        val date = KeystoneClient.parseTimestamp("2016-01-01T00:00:00")
+        date shouldBe 1451606400000L
+    }
+
+    "Client" should "parse ISO8601 date format no time zone with microseconds" in {
+        val date = KeystoneClient.parseTimestamp("2016-01-01T00:00:00.123000")
+        date shouldBe 1451606400123L
+    }
+
+    "Client" should "parse ISO8601 date format time zone" in {
+        val date = KeystoneClient.parseTimestamp("2016-01-01T10:00:00+10:00")
+        date shouldBe 1451606400000L
+    }
+
+    "Client" should "parse ISO8601 date format time zone with microseconds" in {
+        val date = KeystoneClient.parseTimestamp("2016-01-01T10:00:00.123000+10:00")
+        date shouldBe 1451606400123L
+    }
+
+    "Client" should "handle ISO8601 date format" in {
+        dateFormat = DateFormat.Iso8601
+        currentTime = ISO8601Utils.parse("2016-01-01T00:00:00Z",
+                                         new ParsePosition(0)).getTime
+        val client = clientWithoutAdmin
+        val keystoneAccess = client.authenticate(keystoneTenant,
+                                                 keystoneUser,
+                                                 keystonePassword)
+        keystoneAccess.access.token should not be null
+        keystoneAccess.access.token.issuedAt shouldBe "2016-01-01T00:00:00Z"
+        keystoneAccess.access.token.expires shouldBe "2016-01-01T00:30:00Z"
+    }
+
+    "Client" should "handle ISO8601 date format for Fernet tokens" in {
+        dateFormat = DateFormat.Iso8601Micro
+        currentTime = ISO8601Utils.parse("2016-01-01T00:00:00Z",
+                                         new ParsePosition(0)).getTime
+        val client = clientWithoutAdmin
+        val keystoneAccess = client.authenticate(keystoneTenant,
+                                                 keystoneUser,
+                                                 keystonePassword)
+        keystoneAccess.access.token should not be null
+        keystoneAccess.access.token.issuedAt shouldBe "2016-01-01T00:00:00.000000Z"
+        keystoneAccess.access.token.expires shouldBe "2016-01-01T00:30:00.000000Z"
     }
 
     "Client" should "handle authentication with token" in {
@@ -230,6 +274,46 @@ class KeystoneClientTest extends FlatSpec with Matchers with GivenWhenThen {
         }
     }
 
+    "Client" should "fail validate a non-existing token with admin token" in {
+        Given("A valid token")
+        val client = clientWithToken
+
+        Then("Validating the token succeeds")
+        intercept[KeystoneException] {
+            client.validate("invalid-token", tenantScope = false)
+        }
+    }
+
+    "Client" should "fail validate a non-existing token with admin password" in {
+        Given("A client")
+        val client = clientWithPassword
+
+        Then("Validating the token succeeds")
+        intercept[KeystoneException] {
+            client.validate("invalid-token", tenantScope = false)
+        }
+    }
+
+    "Client" should "fail validate an expired token obtained with admin token" in {
+        Given("A valid token")
+        val client = clientWithToken
+        var keystoneAccess = client.authenticate(keystoneTenant,
+                                                 keystoneUser,
+                                                 keystonePassword)
+
+        Then("Validating the token succeeds")
+        keystoneAccess = client.validate(keystoneAccess.access.token.id,
+                                         tenantScope = true)
+
+        When("The token expires")
+        currentTime = currentTime + tokenLifetime
+
+        Then("Validating the token fails")
+        intercept[KeystoneException] {
+            client.validate(keystoneAccess.access.token.id, tenantScope = false)
+        }
+    }
+
     "Client" should "list users" in {
         Given("A valid token")
         val client = clientWithoutAdmin
@@ -241,6 +325,7 @@ class KeystoneClientTest extends FlatSpec with Matchers with GivenWhenThen {
         val keystoneUsers = client.listUsers(keystoneAccess.access.token.id)
         keystoneUsers.users should not be empty
     }
+
 
     "Client" should "get user by name and by identifier" in {
         Given("A valid token")
@@ -258,6 +343,32 @@ class KeystoneClientTest extends FlatSpec with Matchers with GivenWhenThen {
         val userById = client.getUserById(userByName.user.id,
                                           keystoneAccess.access.token.id)
         userById.user.name shouldBe keystoneUser
+    }
+
+    "Client" should "fail get non-existing user name" in {
+        Given("A valid token")
+        val client = clientWithoutAdmin
+        val keystoneAccess = client.authenticate(keystoneTenant,
+                                                 keystoneUser,
+                                                 keystonePassword)
+
+        Then("Get the user by name fails")
+        intercept[KeystoneException] {
+            client.getUserByName("some-name", keystoneAccess.access.token.id)
+        }
+    }
+
+    "Client" should "fail get non-existing user identifier" in {
+        Given("A valid token")
+        val client = clientWithoutAdmin
+        val keystoneAccess = client.authenticate(keystoneTenant,
+                                                 keystoneUser,
+                                                 keystonePassword)
+
+        Then("Get the user by identifier fails")
+        intercept[KeystoneException] {
+            client.getUserById("some-id", keystoneAccess.access.token.id)
+        }
     }
 
     "Client" should "list tenants" in {
@@ -288,6 +399,33 @@ class KeystoneClientTest extends FlatSpec with Matchers with GivenWhenThen {
         val tenantById = client.getTenantById(tenantByName.tenant.id,
                                               keystoneAccess.access.token.id)
         tenantById.tenant.name shouldBe keystoneTenant
+    }
+
+    "Client" should "fail get non-existing tenant name" in {
+        Given("A valid token")
+        val client = clientWithoutAdmin
+        val keystoneAccess = client.authenticate(keystoneTenant,
+                                                 keystoneUser,
+                                                 keystonePassword)
+
+        Then("Get the tenant by name fails")
+        intercept[KeystoneException] {
+            client.getTenantByName("some-name", keystoneAccess.access.token.id)
+        }
+    }
+
+    "Client" should "fail get non-existing tenant identifier" in {
+        Given("A valid token")
+        val client = clientWithoutAdmin
+        val keystoneAccess = client.authenticate(keystoneTenant,
+                                                 keystoneUser,
+                                                 keystonePassword)
+
+        Then("Get the tenant by identifier fails")
+        intercept[KeystoneException] {
+            client.getTenantById("some-id", keystoneAccess.access.token.id)
+        }
+
     }
 
     "Client" should "get tenant user roles" in {
