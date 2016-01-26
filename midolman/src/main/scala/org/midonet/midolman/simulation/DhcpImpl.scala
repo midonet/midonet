e37c377f29bd2e7ea6e0e79008e2207fab8d2eca
Joe Mills <joe@midokura.com>
2016-01-26 14:33:45 +0900

    Add default route to option 121 if routerAddr set
    * When "routerAddr" is set, our DHCP implementation will send the
    "Router" option (3). However, according to RFC 3442, the router
    option should be ignored if option 121 is also sent. To let clients
    know the default route, we send it with option 121 as well.
    
    Fixes MN-193
    
    Change-Id: I72256e3712d665ec3ed5cdef1d4dae4838f7437f

--- midolman/src/main/scala/org/midonet/midolman/simulation/DhcpImpl.scala
+++ midolman/src/main/scala/org/midonet/midolman/simulation/DhcpImpl.scala
@@ -512,7 +512,7 @@ class DhcpImpl(val dhcpConfig: DhcpConfig,
     private var routerAddr: IPv4Addr = null
     private var yiaddr: IPv4Addr = null
     private var yiAddrMaskLen: Int = 0
-    private var opt121Routes: mutable.Seq[Opt121] = null
+    private var opt121Routes: mutable.Buffer[Opt121] = null
     private var dnsServerAddrsBytes: List[Array[Byte]] = Nil
 
     private var interfaceMTU : Short = 0
@@ -593,6 +593,23 @@ class DhcpImpl(val dhcpConfig: DhcpConfig,
                 log.debug("No static DHCP assignment for MAC {}", sourceMac)
                 None
         }
+    }
+
+    private def opt121ToByteArray(opt121: Opt121): mutable.ListBuffer[Byte] = {
+        val bytes = mutable.ListBuffer[Byte]()
+        // First append the destination subnet's maskLength
+        val maskLen = opt121.getRtDstSubnet.getPrefixLen.toByte
+        bytes.append(maskLen)
+        // Now append the significant octets of the subnet.
+        val dstBytes = opt121.getRtDstSubnet.getAddress.toBytes
+        if (maskLen > 0) bytes.append(dstBytes(0))
+        if (maskLen > 8) bytes.append(dstBytes(1))
+        if (maskLen > 16) bytes.append(dstBytes(2))
+        if (maskLen > 24) bytes.append(dstBytes(3))
+        // Now append the 4 octets of the gateway.
+        val gwBytes = opt121.getGateway.toBytes
+        bytes.appendAll(gwBytes.toList)
+        bytes
     }
 
     private def makeDhcpReply(port: BridgePort,
@@ -768,20 +785,24 @@ class DhcpImpl(val dhcpConfig: DhcpConfig,
         // If there are classless static routes, add the option.
         if (null != opt121Routes && opt121Routes.length > 0) {
             val bytes = mutable.ListBuffer[Byte]()
+            if (routerAddr != null) {
+                // According to RFC 3442, if classless routes (option 121) are
+                // being provided, then the router option should be ignored.
+                // In this case we want to provide the default route with
+                // option 121 in addition to option 3.
+                val opt121DefaultRoute = new Opt121()
+                val univSubnet = IPv4Subnet.fromCidr("0.0.0.0/0")
+
+                opt121DefaultRoute.setGateway(routerAddr)
+                opt121DefaultRoute.setRtDstSubnet(univSubnet)
+
+                log.debug("Found router address. Adding default route to "
+                          + opt121DefaultRoute)
+                opt121Routes += opt121DefaultRoute
+            }
             opt121Routes foreach { rt =>
                 log.debug("Found classless route {}", rt)
-                // First append the destination subnet's maskLength
-                val maskLen = rt.getRtDstSubnet.getPrefixLen.toByte
-                bytes.append(maskLen)
-                // Now append the significant octets of the subnet.
-                val dstBytes = rt.getRtDstSubnet.getAddress.toBytes
-                if (maskLen > 0) bytes.append(dstBytes(0))
-                if (maskLen > 8) bytes.append(dstBytes(1))
-                if (maskLen > 16) bytes.append(dstBytes(2))
-                if (maskLen > 24) bytes.append(dstBytes(3))
-                // Now append the 4 octets of the gateway.
-                val gwBytes = rt.getGateway.toBytes
-                bytes.appendAll(gwBytes.toList)
+                bytes.appendAll(opt121ToByteArray(rt))
             }
             log.debug("Adding Option 121 (classless static routes) with " +
                       s"${opt121Routes.length} routes")
