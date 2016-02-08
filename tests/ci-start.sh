da289456168cd32c1de38e6824ee7011dc50d2ae
Xavi León <xavi.leon@midokura.com>
2016-02-08 18:45:48 +0100

    MDTS: change to local repo in overrides
    Until now, overrides installed packages with dpkg -i which did not
    resolve package dependencies. Midonet-sandbox now provides a way
    to install packages from a local repo which allows us to automatically
    update the image in the CI every time a dependency is changes in one of
    our packages. Additionally, we pull the current downstream neutron kilo
    package to test vpnaas in the CI (will be done in the jobs in the
    future).
    
    Change-Id: I581080076da49e3276f5693678708fba15b9e075
    Signed-off-by: Xavi León <xavi.leon@midokura.com>

--- tests/ci-start.sh
+++ tests/ci-start.sh
@@ -10,11 +10,11 @@ sudo pip install -r tests/mdts.dependencies
 
 # We assume all gates/nightlies put the necessary packages in $WORKSPACE
 # so we know where to find them.
-cp midolman*.deb tests/sandbox/override_v2/midolman
-cp midonet-tools*.deb tests/sandbox/override_v2/midolman
-cp midonet-cluster*.deb tests/sandbox/override_v2/cluster
-cp midonet-tools*.deb tests/sandbox/override_v2/cluster
-cp python-midonetclient*.deb tests/sandbox/override_v2/cluster
+mkdir -p tests/sandbox/override_v2/packages
+cp midolman*.deb tests/sandbox/override_v2/packages
+cp midonet-tools*.deb tests/sandbox/override_v2/packages
+cp midonet-cluster*.deb tests/sandbox/override_v2/packages
+cp python-midonetclient*.deb tests/sandbox/override_v2/packages
 
 # Necessary software in the host, we assume packages are already present on
 # the corresponding override, in this case the v2 override.
@@ -29,9 +29,10 @@ popd
 pushd tests/
 echo "docker_registry=artifactory.bcn.midokura.com" >> sandbox.conf
 echo "docker_insecure_registry=True" >> sandbox.conf
-sudo sandbox-manage -c sandbox.conf pull-all default_v2
+SANDBOX_FLAVOUR="default_v2_neutron+kilo"
+sudo sandbox-manage -c sandbox.conf pull-all $SANDBOX_FLAVOUR
 sudo sandbox-manage -c sandbox.conf \
-                    run default_v2_neutron+kilo \
+                    run $SANDBOX_FLAVOUR \
                     --name=mdts \
                     --override=sandbox/override_v2 \
                     --provision=sandbox/provisioning/all-provisioning.sh
