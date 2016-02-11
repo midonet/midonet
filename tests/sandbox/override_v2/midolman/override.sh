d26185e4cf7775aa3c6a6c6646cf42651d8b11ab
Xavi León <xavi.leon@midokura.com>
2016-02-11 11:38:48 +0100

    MDTS: remove non-necessary apt-get
    This was necessary to pass the gates on the libreswan dependency before
    the images in the CI were updated to reflect that a new package was
    downloaded. The images are already updated so remove it to reduce the
    likelyness of connectivity failures. Also failfast in case we cannot
    update from the local repositories to avoid false positives (running the
    tests with an already installed package) and increase the timeout to
    wait for a service to be up (external repositories may be slow to
    download packages).
    
    Change-Id: I0da181bbdea81cc9a01d66123d0a578c06fb920d
    Signed-off-by: Xavi León <xavi.leon@midokura.com>

--- tests/sandbox/override_v2/midolman/override.sh
+++ tests/sandbox/override_v2/midolman/override.sh
@@ -4,9 +4,10 @@
 LOCAL_REPO_FILE=/etc/apt/sources.list.d/midonet-local.list
 echo "deb file:/packages /" > $LOCAL_REPO_FILE
 apt-get update -o Dir::Etc::sourcelist=$LOCAL_REPO_FILE
-apt-get update
+
+# Failfast if we cannot update the packages locally
 apt-get install -qy --force-yes midolman/local \
-                                midonet-tools/local
+                                midonet-tools/local || exit 1
 
 # Make sure we can access the remote management interface from outside the container
 HOST_NAME=`hostname`
