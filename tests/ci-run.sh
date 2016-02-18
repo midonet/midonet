c8610bcba86f7fef944619c6b08365caf6cd65d3
Alex Bikfalvi <alex.bikfalvi@midokura.com>
2016-02-18 13:03:47 +0100

    VPNaaS: Add containers MDTS tests
    This adds the following container tests:
    1. Container migration between hosts by changing the host
    container weight.
    2. Containers are restored after an agent restart.
    3. Containers are maintained through a cluster restart.
    
    Ref: MI-529
    
    Change-Id: Ia0767894c62533543c9b0dc7bbdce8c0935e9c04
    Signed-off-by: Alex Bikfalvi <alex.bikfalvi@midokura.com>

--- tests/ci-run.sh
+++ tests/ci-run.sh
@@ -6,6 +6,9 @@ pushd tests/mdts/tests/functional_tests
     -t test_vpnaas.py:test_non_vpn_subnet \
     -t test_vpnaas.py:test_security_groups \
     -t test_vpnaas.py:test_ipsec_site_connection_deletion \
+    -t test_vpnaas.py:test_container_migration \
+    -t test_vpnaas.py:test_container_restored_on_agent_restart \
+    -t test_vpnaas.py:test_container_maintained_on_cluster_restart \
     -t test_bgp.py:test_multisession_icmp_with_redundancy \
     -t test_bgp.py:test_multisession_icmp_failback \
     -t test_bgp.py:test_snat \
