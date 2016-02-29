e719b3f2d08f2b6619c9ca1d2d800bce4b945f21
Alex Bikfalvi <alex.bikfalvi@midokura.com>
2016-02-29 10:49:57 +0100

    VPNaaS: Container logger uses container identifier
    This modifies the container logger as follows. Instead of using
    an empty file, where the file name is <name>.<type>, the logger
    now uses a file name <id> where the container name is written to
    the file as a Protocol Buffers message.
    
    This helps identify the log file as corresponding to a particular
    container.
    
    Ref: MI-665
    
    Change-Id: If17db94252ce7c752c9876469954db121100571b
    Signed-off-by: Alex Bikfalvi <alex.bikfalvi@midokura.com>

--- midolman/src/main/scala/org/midonet/midolman/containers/ContainerLogger.scala
+++ midolman/src/main/scala/org/midonet/midolman/containers/ContainerLogger.scala
@@ -17,27 +17,34 @@
 package org.midonet.midolman.containers
 
 import java.io._
+import java.nio.ByteBuffer
+import java.nio.charset.StandardCharsets
 import java.nio.file._
 import java.util
-import java.util.Collections
+import java.util.{Collections, UUID}
 
 import scala.util.control.NonFatal
 
 import com.google.common.base.MoreObjects
 import com.typesafe.scalalogging.Logger
 
+import org.midonet.containers.models.Containers
 import org.midonet.midolman.config.ContainerConfig
 import org.midonet.midolman.containers.ContainerLogger._
 
 object ContainerLogger {
 
-    case class ContainerKey(name: String, `type`: String) {
+    case class ContainerKey(`type`: String, id: UUID, name: String) {
         override def toString = MoreObjects.toStringHelper(this).omitNullValues()
-            .add("name", name)
             .add("type", `type`)
+            .add("id", id)
+            .add("name", name)
             .toString
 
     }
+
+    val CharsetEncoder = StandardCharsets.UTF_8.newEncoder()
+    val CharsetDecoder = StandardCharsets.UTF_8.newDecoder()
 
 }
 
@@ -88,9 +95,9 @@ class ContainerLogger(config: ContainerConfig, log: Logger) {
     @throws[UnsupportedOperationException]
     @throws[IOException]
     @throws[SecurityException]
-    def log(`type`: String, operation: ContainerOp): Unit = {
+    def log(`type`: String, id: UUID, operation: ContainerOp): Unit = {
         val filePath = FileSystems.getDefault.getPath(
-            s"$logDirectory/${config.logDirectory}/${operation.name}.${`type`}")
+            s"$logDirectory/${config.logDirectory}/$id")
 
         if (!Files.exists(directoryPath)) {
             try {
@@ -102,9 +109,7 @@ class ContainerLogger(config: ContainerConfig, log: Logger) {
 
         operation.flag match {
             case ContainerFlag.Created =>
-                Files.newByteChannel(filePath,
-                                     StandardOpenOption.CREATE,
-                                     StandardOpenOption.WRITE).close()
+                writeFile(filePath, `type`, id, operation.name)
             case ContainerFlag.Deleted =>
                 Files.deleteIfExists(filePath)
         }
@@ -127,13 +132,9 @@ class ContainerLogger(config: ContainerConfig, log: Logger) {
                 val iterator = stream.iterator()
                 val containers = new util.ArrayList[ContainerKey]()
                 while (iterator.hasNext) {
-                    val fileName = iterator.next().getFileName.toString
-                    val tokens = fileName.split('.')
-                    if (tokens.length == 2) {
-                        containers add ContainerKey(name = tokens(0),
-                                                    `type` = tokens(1))
-                    } else {
-                        log warn s"Invalid container log file name: '$fileName'"
+                    val key = readContainer(iterator.next().getFileName)
+                    if (key ne null) {
+                        containers add key
                     }
                 }
                 containers
@@ -144,6 +145,59 @@ class ContainerLogger(config: ContainerConfig, log: Logger) {
             case NonFatal(e) =>
                 log.warn(s"Reading the log directory $directoryPath failed", e)
                 Collections.emptyList()
+        }
+    }
+
+    /**
+      * Writes the container name to a log file.
+      */
+    @throws[IOException]
+    private def writeFile(filePath: Path, `type`: String, id: UUID,
+                          name: String): Unit = {
+        val channel = Files.newByteChannel(filePath,
+                                           StandardOpenOption.CREATE,
+                                           StandardOpenOption.WRITE)
+        try {
+            val log = Containers.Log.newBuilder()
+                .setType(`type`)
+                .setId(id.toString)
+                .setName(name)
+                .build()
+            channel.write(ByteBuffer.wrap(log.toByteArray))
+        } finally {
+            channel.close()
+        }
+    }
+
+    /**
+      * Reads the container key from the specified file path. If the operation
+      * fails, the method returns `null`.
+      */
+    private def readContainer(filePath: Path): ContainerKey = {
+        val fileName = filePath.toString
+        try {
+            readFile(filePath)
+        } catch {
+            case NonFatal(e) =>
+                log.warn(s"Failed to read container log file '$fileName", e)
+                null
+        }
+    }
+
+    /**
+      * Reads the container name from a container log file.
+      */
+    @throws[Exception]
+    private def readFile(filePath: Path): ContainerKey = {
+        val path = directoryPath.resolve(filePath)
+        val inputStream = new FileInputStream(path.toFile)
+        try {
+            val log = Containers.Log.newBuilder().mergeFrom(inputStream).build()
+            ContainerKey(`type` = log.getType,
+                         id = UUID.fromString(log.getId),
+                         name = log.getName)
+        } finally {
+            inputStream.close()
         }
     }
 
