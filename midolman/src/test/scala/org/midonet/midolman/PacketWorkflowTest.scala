186836af8e812965fb849d2dd995ac16869498d4
Xavi León <xavi.leon@midokura.com>
2016-03-09 17:36:52 +0100

    FlowState: move encoder to nsdb
    Flow state messages are ment to be also handled by the cluster. This
    patch moves the flow state encoder into nsdb so both midolman and
    cluster nodes can make use of it.
    
    Change-Id: I8cf806783a9cc6dcee9c999186cf9264331da79d
    Signed-off-by: Xavi León <xavi.leon@midokura.com>

--- midolman/src/test/scala/org/midonet/midolman/PacketWorkflowTest.scala
+++ midolman/src/test/scala/org/midonet/midolman/PacketWorkflowTest.scala
@@ -37,14 +37,14 @@ import org.midonet.midolman.simulation.PacketContext
 import org.midonet.midolman.state.ConnTrackState.{ConnTrackKey, ConnTrackValue}
 import org.midonet.midolman.state.NatState.{NatBinding, NatKey}
 import org.midonet.midolman.state.TraceState.{TraceContext, TraceKey}
-import org.midonet.midolman.state.{FlowStatePackets, HappyGoLuckyLeaser, MockFlowStateTable, MockStateStorage}
+import org.midonet.midolman.state._
 import org.midonet.midolman.util.MidolmanSpec
 import org.midonet.midolman.util.mock.MessageAccumulator
 import org.midonet.odp.flows.FlowActions.output
 import org.midonet.odp.flows.FlowKeys.tunnel
 import org.midonet.odp.flows.{FlowActions, FlowAction}
 import org.midonet.odp.{Datapath, FlowMatches, Packet}
-import org.midonet.packets.Ethernet
+import org.midonet.packets.{Ethernet, FlowStateSbeEncoder}
 import org.midonet.packets.util.EthBuilder
 import org.midonet.packets.util.PacketBuilder.{udp, _}
 import org.midonet.sdn.flows.FlowTagger
@@ -104,9 +104,9 @@ class PacketWorkflowTest extends MidolmanSpec {
     def makePacket(variation: Short): Packet = makeFrame(variation)
 
     def makeStatePacket(): Packet = {
-        val eth = FlowStatePackets.makeUdpShell(Array())
+        val eth = FlowStateSbeEncoder.makeUdpShell(Array())
         val fmatch = FlowMatches.fromEthernetPacket(eth)
-        fmatch.addKey(tunnel(FlowStatePackets.TUNNEL_KEY, 1, 2, 0))
+        fmatch.addKey(tunnel(FlowStateSbeEncoder.TUNNEL_KEY, 1, 2, 0))
         new Packet(eth, fmatch)
     }
 
