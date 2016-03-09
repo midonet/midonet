f462522d15c0ae4b91b8893b042ecb9348fe5fda
Joe Mills <joe@midokura.com>
2016-03-09 16:05:15 +0900

    Fix IP addr group management
    * This patch adds some tests, and fixes, to make sure that the
      ip/port_id of the ip addr group matches the ips and security
      groups of the port.
    
    Fixes MI-686
    
    Change-Id: Icda7c3958246059d27a7adec8b1eda45282dd757

--- midonet-cluster/src/test/scala/org/midonet/cluster/C3POMinionTest.scala
+++ midonet-cluster/src/test/scala/org/midonet/cluster/C3POMinionTest.scala
@@ -902,6 +902,19 @@ class C3POMinionTestBase extends FlatSpec with BeforeAndAfter
         subnetId
     }
 
+    protected def createVifPort(taskId: Int, networkId: UUID,
+                                portId: UUID = UUID.randomUUID(),
+                                fixedIps: Seq[IPAlloc] = Seq(),
+                                sg: Seq[UUID] = Seq(),
+                                portSecurityEnabled: Boolean = true): UUID = {
+        val json = portJson(portId, networkId,
+                            deviceOwner = DeviceOwner.COMPUTE,
+                            fixedIps = fixedIps, securityGroups = sg,
+                            portSecurityEnabled = portSecurityEnabled)
+        insertCreateTask(taskId, PortType, json, portId)
+        portId
+    }
+
     protected def createDhcpPort(taskId: Int, networkId: UUID, subnetId: UUID,
                                  ipAddr: String,
                                  portId: UUID = UUID.randomUUID()): UUID = {
