780aab0398c3412dfe5927c32d3adf1492d7f200
Ivan Kelly <ivan@midokura.com>
2016-03-16 10:21:39 +0100

    Make it possible to specify override, provision and flavor in ci-start.sh
    Defaults to the same values are before.
    Signed-off-by: Ivan Kelly <ivan@midokura.com>
    
    Change-Id: Ibc4cedbb159124fa5b7381a2deb2a38e3e60f54c

--- tests/ci-start.sh
+++ tests/ci-start.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+SANDBOX_FLAVOUR="default_v2_neutron+kilo"
+OVERRIDE="sandbox/override_v2"
+PROVISIONING="sandbox/provisioning/all-provisioning.sh"
+
+while getopts ":f:o:p:h" opt; do
+    case $opt in
+    f)
+        SANDBOX_FLAVOUR=$OPTARG
+        ;;
+    o)
+        OVERRIDE=$OPTARG
+        ;;
+    p)
+        PROVISIONING=$OPTARG
+        ;;
+    h)
+        echo "$0 [-f SANDBOX_FLAVOUR] [-o OVERRIDE_DIRECTORY]" \
+             " [-p PROVISIONING_SCRIPT]"
+        exit 1
+        ;;
+    esac
+done
+
 # WARNING: this script is meant to be used on the CI, as it pulls images from
 # the CI infrastructure (artifactory)
 
@@ -10,11 +33,11 @@ sudo pip install -r tests/mdts.dependencies
 
 # We assume all gates/nightlies put the necessary packages in $WORKSPACE
 # so we know where to find them.
-mkdir -p tests/sandbox/override_v2/packages
-cp midolman*.deb tests/sandbox/override_v2/packages
-cp midonet-tools*.deb tests/sandbox/override_v2/packages
-cp midonet-cluster*.deb tests/sandbox/override_v2/packages
-cp python-midonetclient*.deb tests/sandbox/override_v2/packages
+mkdir -p tests/$OVERRIDE/packages
+cp midolman*.deb tests/$OVERRIDE/packages
+cp midonet-tools*.deb tests/$OVERRIDE/packages
+cp midonet-cluster*.deb tests/$OVERRIDE/packages
+cp python-midonetclient*.deb tests/$OVERRIDE/packages
 
 # Necessary software in the host, we assume packages are already present on
 # the corresponding override, in this case the v2 override.
@@ -29,12 +52,11 @@ popd
 pushd tests/
 echo "docker_registry=artifactory.bcn.midokura.com" >> sandbox.conf
 echo "docker_insecure_registry=True" >> sandbox.conf
-SANDBOX_FLAVOUR=${SANDBOX_FLAVOUR:-"default_v2_neutron+kilo"}
 sudo sandbox-manage -c sandbox.conf pull-all $SANDBOX_FLAVOUR
 sudo sandbox-manage -c sandbox.conf \
                     run $SANDBOX_FLAVOUR \
                     --name=mdts \
-                    --override=sandbox/override_v2 \
-                    --provision=sandbox/provisioning/all-provisioning.sh
+                    --override=$OVERRIDE \
+                    --provision=$PROVISIONING
 
 popd
