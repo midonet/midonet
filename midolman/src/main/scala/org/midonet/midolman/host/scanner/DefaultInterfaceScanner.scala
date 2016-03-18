3f812b5dcb4b62c574cde6a95f92646eebdf174e
Nicolas Schiper <nicolas@midokura.com>
2016-03-18 10:00:17 +0100

    InterfaceScanner: Handling null MAC addresses
    This patch handles interfaces that have L2 addresses that are not
    6-bytes long. In this case the MAC contained in the Link object
    is null (rightfully so). We thus set the MAC address in the
    InterfaceDescription to null as well. As a result, the
    correspondign interface will be filtered out, which is ok in
    this case.
    
    Ref: MI-732
    
    Change-Id: Ib818195960008efff24e7cc564a670d8ba4892d0
    Signed-off-by: Nicolas Schiper <nicolas@midokura.com>

--- midolman/src/main/scala/org/midonet/midolman/host/scanner/DefaultInterfaceScanner.scala
+++ midolman/src/main/scala/org/midonet/midolman/host/scanner/DefaultInterfaceScanner.scala
@@ -32,6 +32,7 @@ import rx.{Observable, Observer, Subscription}
 
 import org.midonet.Util
 import org.midonet.midolman.host.interfaces.InterfaceDescription
+import org.midonet.midolman.host.interfaces.InterfaceDescription.Type.VIRT
 import org.midonet.netlink._
 import org.midonet.netlink.rtnetlink._
 import org.midonet.util.concurrent.NanoClock
@@ -379,7 +380,17 @@ class DefaultInterfaceScanner(channelFactory: NetlinkChannelFactory,
         clone.setEndpoint(ifdesc.getEndpoint)
         ifdesc.getInetAddresses foreach clone.setInetAddress
         clone.setHasLink(ifdesc.hasLink)
-        clone.setMac(MAC.fromAddress(ifdesc.getMac.getAddress))
+
+        // When an interface has an L2 address that is not 6-bytes long, the MAC
+        // address of the interface description is null.
+        // Setting the mac address to null here means that the corresponding
+        // interface will not be notified to the outside world, which is fine in
+        // this case.
+        if (ifdesc.getMac == null) {
+            clone.setMac(null.asInstanceOf[MAC])
+        } else {
+            clone.setMac(MAC.fromAddress(ifdesc.getMac.getAddress))
+        }
         clone.setMtu(ifdesc.getMtu)
         clone.setPortType(ifdesc.getPortType)
         clone.setType(ifdesc.getType)
