45a448fb33416cb09066cc2204fe672e5872bb3a
Ryu Ishimoto <ryu@midokura.com>
2016-03-24 14:03:04 +0000

    Non-existing port deletion should not fail
    There may be situations where a non-existent port might be requested to
    be deleted.  In that case, the API should not fail.
    
    Conflicts:
    	midonet-cluster/src/main/scala/org/midonet/cluster/services/c3po/translators/PortTranslator.scala
    
    Change-Id: I0fa6439fc73f8f1dcbf9f9bb92f8e27501bda8d3
    Fixes: MI-755
    (cherry picked from commit 17fb6be7701b092fe9d4971a846e95b50117d2e8)

--- midonet-cluster/src/main/scala/org/midonet/cluster/services/c3po/translators/PortTranslator.scala
+++ midonet-cluster/src/main/scala/org/midonet/cluster/services/c3po/translators/PortTranslator.scala
@@ -111,7 +111,11 @@ class PortTranslator(protected val storage: ReadOnlyStorage,
     }
 
     override protected def translateDelete(id: UUID): MidoOpList = {
-        val nPort = storage.get(classOf[NeutronPort], id).await()
+        val nPort = try {
+            storage.get(classOf[NeutronPort], id).await()
+        } catch {
+            case nfe: NotFoundException => return List()
+        }
 
         // Nothing to do for floating IPs or VIPs, since we didn't create
         // anything.
