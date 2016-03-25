1fce5c43c3b3eecf4bb0b0dd5c8c6673b44a5e58
Brandon Berg <bberg@midokura.com>
2016-03-25 20:39:23 +0900

    Add exists check to Translator.translateDelete()
    Neutron sometimes gets out of sync with Midonet and tries to delete things that
    have already been deleted. This change adds a call to storage.get() in
    Translator.translateDelete() to get the Neutron object. If it fails with a
    NotFoundException, translation is skipped and the operation returns. If the get
    succeeds, the Neutron object is passed to the specific implementation, e.g.
    NetworkTranslator.translateDelete().
    
    Change-Id: I5c9c4cb7636d97cf914f65e611e6867cdb0966c8
    Fixes: MI-759
    Signed-off-by: Brandon Berg <bberg@midokura.com>

--- midonet-cluster/src/main/scala/org/midonet/cluster/services/c3po/translators/SecurityGroupRuleTranslator.scala
+++ midonet-cluster/src/main/scala/org/midonet/cluster/services/c3po/translators/SecurityGroupRuleTranslator.scala
@@ -24,7 +24,7 @@ import org.midonet.cluster.models.Topology.Rule
 import org.midonet.cluster.services.c3po.C3POStorageManager.{Create, Update, Delete}
 import org.midonet.util.concurrent.toFutureOps
 
-class SecurityGroupRuleTranslator(storage: ReadOnlyStorage)
+class SecurityGroupRuleTranslator(protected val storage: ReadOnlyStorage)
     extends Translator[SecurityGroupRule] with ChainManager {
 
     /*
@@ -46,7 +46,15 @@ class SecurityGroupRuleTranslator(storage: ReadOnlyStorage)
             "SecurityGroupRule update not supported.")
     }
 
-    protected override def translateDelete(sgrId: UUID) : OperationList = {
+    // Intentionally overriding the UUID overload instead of the
+    // SecurityGroupRule overload, to bypass the exists check in super
+    // implementation. Prior to 5.2 (change bfb99f08), when translating the
+    // creation of a Neutron SecurityGroup with embedded rules, we didn't create
+    // a corresponding top-level Neutron SecurityGroupRule in ZK. This means
+    // that sometimes we'll get a request to delete a SecurityGroupRule that
+    // doesn't exist as a top-level SGR in ZK, but we still need to delete the
+    // corresponding Midonet rules.
+    protected override def translateDelete(sgrId: UUID): OperationList = {
         val ops = new OperationListBuffer
         if (storage.exists(classOf[SecurityGroupRule], sgrId).await()) {
             val sgr = storage.get(classOf[SecurityGroupRule], sgrId).await()
