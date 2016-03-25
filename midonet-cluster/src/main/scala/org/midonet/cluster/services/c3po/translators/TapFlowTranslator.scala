426f714d9a2b6c0a57510845b17fd6120c672dbf
YAMAMOTO Takashi <yamamoto@midokura.com>
2016-03-25 13:09:07 +0900

    Tap as a service: Neutron translation
    Ref: MI-702
    Signed-off-by: YAMAMOTO Takashi <yamamoto@midokura.com>
    Change-Id: If549559451bd06ae4b8c07ab7fa443608d7f2a03

---
+++ midonet-cluster/src/main/scala/org/midonet/cluster/services/c3po/translators/TapFlowTranslator.scala
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2016 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.cluster.services.c3po.translators
+
+import scala.collection.JavaConversions._
+
+import org.midonet.cluster.data.storage.{NotFoundException, ReadOnlyStorage,
+                                         StateTableStorage}
+import org.midonet.cluster.models.Commons.UUID
+import org.midonet.cluster.models.Neutron.TapFlow
+import org.midonet.cluster.models.Topology.{Port, Mirror}
+import org.midonet.cluster.services.c3po.C3POStorageManager.Update
+import org.midonet.cluster.util.UUIDUtil.asRichProtoUuid
+import org.midonet.midolman.state.PathBuilder
+import org.midonet.util.concurrent.toFutureOps
+
+
+class TapFlowTranslator(protected val storage: ReadOnlyStorage,
+                        protected val stateTableStorage: StateTableStorage,
+                        protected val pathBldr: PathBuilder)
+    extends Translator[TapFlow] with StateTableManager {
+
+    override protected def translateCreate(tf: TapFlow): OperationList = {
+        val port = storage.get(classOf[Port], tf.getSourcePort).await().toBuilder
+        val mirrorId = tf.getTapServiceId
+
+        // Note: Neutron and our In/Out are from the opposite POVs.
+        if (tapIn(tf)) {
+            port.addPreOutboundMirrorIds(mirrorId)
+        }
+        if (tapOut(tf)) {
+            port.addPostInboundMirrorIds(mirrorId)
+        }
+        List(Update(port.build))
+    }
+
+    override protected def translateDelete(id: UUID): OperationList = {
+        val tf = try storage.get(classOf[TapFlow], id).await() catch {
+            case ex: NotFoundException => return List() // Idempotent.
+        }
+        val port = storage.get(classOf[Port], tf.getSourcePort).await().toBuilder
+        val mirrorId = tf.getTapServiceId
+
+        // Note: Neutron and our In/Out are from the opposite POVs.
+        if (tapIn(tf)) {
+            val idx = port.getPreOutboundMirrorIdsList.indexOf(mirrorId)
+            port.removePreOutboundMirrorIds(idx)
+        }
+        if (tapOut(tf)) {
+            val idx = port.getPostInboundMirrorIdsList.indexOf(mirrorId)
+            port.removePostInboundMirrorIds(idx)
+        }
+        List(Update(port.build))
+    }
+
+    override protected def translateUpdate(tf: TapFlow): OperationList =
+        List()
+
+    private def tapIn(tf: TapFlow): Boolean =
+        tf.getDirection == TapFlow.TapFlowDirection.BOTH ||
+        tf.getDirection == TapFlow.TapFlowDirection.IN
+
+    private def tapOut(tf: TapFlow): Boolean =
+        tf.getDirection == TapFlow.TapFlowDirection.BOTH ||
+        tf.getDirection == TapFlow.TapFlowDirection.OUT
+}
---
+++ midonet-cluster/src/main/scala/org/midonet/cluster/services/c3po/translators/TapServiceTranslator.scala
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2016 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.cluster.services.c3po.translators
+
+import scala.collection.JavaConversions._
+
+import org.midonet.cluster.data.storage.{NotFoundException, ReadOnlyStorage,
+                                         StateTableStorage}
+import org.midonet.cluster.models.Commons.Condition.FragmentPolicy
+import org.midonet.cluster.models.Commons.Condition
+import org.midonet.cluster.models.Commons.UUID
+import org.midonet.cluster.models.Neutron.TapService
+import org.midonet.cluster.models.Topology.Mirror
+import org.midonet.cluster.services.c3po.C3POStorageManager.{Create, Delete}
+import org.midonet.cluster.util.UUIDUtil.asRichProtoUuid
+import org.midonet.midolman.state.PathBuilder
+import org.midonet.util.concurrent.toFutureOps
+
+
+class TapServiceTranslator(protected val storage: ReadOnlyStorage,
+                           protected val stateTableStorage: StateTableStorage,
+                           protected val pathBldr: PathBuilder)
+    extends Translator[TapService] with StateTableManager {
+
+    override protected def translateCreate(ts: TapService): OperationList = {
+        val condition = Condition.newBuilder
+            .setFragmentPolicy(FragmentPolicy.ANY)
+        val mirror = Mirror.newBuilder
+            .setId(ts.getId)
+            .setToPortId(ts.getPortId)
+            .addConditions(condition)
+            .build
+
+        List(Create(mirror))
+    }
+
+    override protected def translateDelete(id: UUID): OperationList = {
+        List(Delete(classOf[Mirror], id))
+    }
+
+    override protected def translateUpdate(ts: TapService): OperationList =
+        List()
+}
