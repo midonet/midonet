8bad046358be93e6d250844afdbbbaafe5dd6b81
Xavi León <xavi.leon@midokura.com>
2016-03-30 13:49:00 +0200

    MDTS: add backwards compatibiliy test
    This tests spins up a sandbox with a basic setup (3 ZK, 2 midolmans, 1
    cassandra, 1 cluster, 1 neutron) and creates a basic topology with
    neutron that creates flow state to ensure that rolling upgrades are
    possible. The tests consists in:
    - inject traffic that creates flow state.
    - ensure forward flows and return flows are handled correctly.
    - update one of the agent to a master package and restart.
    - previous flow is recognised and gets through.
    - new flows can be stablished between different agent versions.
    
    Ref.: MI-603
    Change-Id: I2963e68572b0de476710170faaf2a0ea65352f13
    Signed-off-by: Xavi León <xavi.leon@midokura.com>
    (cherry picked from commit a41442a5c02c053f3c1c02d01a2b309af292e571)

---
+++ tests/sandbox/override_compat/midolman/override.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Install the latest packages from the local repository
+LOCAL_REPO_FILE=/etc/apt/sources.list.d/midonet-local.list
+echo "deb file:/packages /" > $LOCAL_REPO_FILE
+
+apt-get update
+
+# Make sure we can access the remote management interface from outside the container
+HOST_NAME=`hostname`
+RESOLVED_HOST_NAME=`getent hosts $HOST_NAME`
+IPADDRESS=${RESOLVED_HOST_NAME%% *}
+sed -i "\$a JVM_OPTS=\"\$JVM_OPTS -Djava.rmi.server.hostname=$IPADDRESS\"" /etc/midolman/midolman-env.sh
+
+exec /run-midolman.sh
