5e51b106dfe60cee231d0954c81e9f9be75c3ae1
Xavi León <xavi.leon@midokura.com>
2016-03-30 13:49:00 +0200

    MDTS: add backwards compatibiliy test
    This tests spins up a sandbox with a basic setup (3 ZK, 2 midolmans, 1
    cassandra, 1 cluster, 1 neutron) and creates a basic topology with
    neutron that creates flow state to ensure that rolling upgrades are
    possible. The tests consists in:
    - inject traffic that creates flow state.
    - ensure forward flows and return flows are handled correctly.
    - update one of the agent to a master package and restart.
    - previous flow is recognised and gets through.
    - new flows can be stablished between different agent versions.
    
    Ref.: MI-603
    Change-Id: I2963e68572b0de476710170faaf2a0ea65352f13
    Signed-off-by: Xavi León <xavi.leon@midokura.com>

---
+++ tests/mdts/lib/sandbox.py
@@ -0,0 +1,50 @@
+# Copyright 2016 Midokura SARL
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import logging
+import shlex
+import subprocess
+
+"""
+Helper module to execute commands on the host environment
+through docker or sandbox.
+"""
+
+LOG = logging.getLogger(__name__)
+
+
+def remove_container(container):
+    p = subprocess.Popen(
+            ["docker", "stop", container.get_container_id()],
+            stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    stdout, _ = p.communicate()
+    LOG.debug(stdout)
+    p = subprocess.Popen(
+            ["docker", "rm", container.get_container_id()],
+            stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    stdout, _ = p.communicate()
+    LOG.debug(stdout)
+
+
+def restart_sandbox(flavour, override, no_recreate=True):
+    cmd = "sandbox-manage -c sandbox.conf run default_v2_neutron+kilo+compat " \
+          "--name=mdts --override=sandbox/override_v2_compat " \
+          "--force"
+    if no_recreate:
+        cmd += " --no-recreate"
+    p = subprocess.Popen(
+            shlex.split(cmd), cwd='../../../',
+            stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    stdout, _ = p.communicate()
+    LOG.debug(stdout)
