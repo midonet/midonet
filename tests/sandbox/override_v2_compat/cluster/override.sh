f40755ca4d59956112f602606d28ed626224b4b4
Xavi León <xavi.leon@midokura.com>
2016-03-30 13:49:00 +0200

    MDTS: add backwards compatibiliy test
    This tests spins up a sandbox with a basic setup (3 ZK, 2 midolmans, 1
    cassandra, 1 cluster, 1 neutron) and creates a basic topology with
    neutron that creates flow state to ensure that rolling upgrades are
    possible. The tests consists in:
    - inject traffic that creates flow state.
    - ensure forward flows and return flows are handled correctly.
    - update one of the agent to a master package and restart.
    - previous flow is recognised and gets through.
    - new flows can be stablished between different agent versions.
    
    Change-Id: I2963e68572b0de476710170faaf2a0ea65352f13
    Signed-off-by: Xavi León <xavi.leon@midokura.com>

---
+++ tests/sandbox/override_v2_compat/cluster/override.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Install the latest pagackes from the local repository
+LOCAL_REPO_FILE=/etc/apt/sources.list.d/midonet-local.list
+echo "deb file:/packages /" > $LOCAL_REPO_FILE
+
+MIDONET_REPO_FILE=/etc/apt/sources.list.d/midonet.list
+echo "deb http://192.168.30.4/artifactory/midonet-5-deb stable main" > $MIDONET_REPO_FILE
+
+apt-get update
+
+# Failfast if we cannot update the packages locally
+apt-get install -qy --force-yes midonet-cluster=2:5.0.1 \
+                                midonet-tools=2:5.0.1 \
+                                python-midonetclient=2:5.0.1 || exit 1
+
+# Make sure we can access the remote management interface from outside the container
+HOST_NAME=`hostname`
+RESOLVED_HOST_NAME=`getent hosts $HOST_NAME`
+IPADDRESS=${RESOLVED_HOST_NAME%% *}
+sed -i "\$a JVM_OPTS=\"\$JVM_OPTS -Djava.rmi.server.hostname=$IPADDRESS\"" /etc/midonet-cluster/midonet-cluster-env.sh
+
+# Run cluster
+exec /run-midonetcluster.sh
