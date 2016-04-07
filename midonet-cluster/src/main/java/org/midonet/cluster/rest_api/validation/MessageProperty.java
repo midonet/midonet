b2d1ee7120e6a20d6f37597113cec0f781f39f24
Ryu Ishimoto <ryu@midokura.com>
2016-04-07 11:48:25 +0000

    Add gw count validation check in l2gw translation
    L2GatewayConnection should only contain one gateway device.
    Add a validation for it.
    
    Change-Id: Ic34ae3abc4e8926032b11658b19cc84d4cd05719

--- midonet-cluster/src/main/java/org/midonet/cluster/rest_api/validation/MessageProperty.java
+++ midonet-cluster/src/main/java/org/midonet/cluster/rest_api/validation/MessageProperty.java
@@ -116,6 +116,8 @@ public class MessageProperty {
             "{midokura.javarx.TunnelZoneNotVtep.message}";
     public static final String UNIQUE_TUNNEL_ZONE_NAME_TYPE =
             "{midokura.javarx.TunnelZoneNameExists.message}";
+    public static final String ONLY_ONE_GW_DEV_SUPPORTED =
+            "{midokura.javarx.OnlyOneGatewayDeviceSupported.message}";
     public static final String ONLY_ROUTER_VTEP_SUPPROTED =
             "{midokura.javarx.OnlyRouterVtepSupported.message}";
     public static final String TUNNEL_ZONE_MEMBER_EXISTS =
