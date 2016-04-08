1ddb557d7edf711bb1aee291bc02de33fa8ba213
Michael Micucci <micucci@midokura.com>
2016-04-08 02:26:32 +0000

    Migration tool: Helper package/ZK utilities
    Migration Tool for 1.9 -> 5.0:  These utilities will help dev
      users backup ZK, rollback ZK, upgrade packages, rollback
      packages, set up midonet-cluter and ZK to import tasks (from
      the migration process) and execute them.
    
    These tools are mostly meant for internal use (reflected in the
      readme), and can be polished up later for users if that would
      be helpful.
    
    Change-Id: I84793e3dfe9d4d94ec7935cf454ec6e80212ea9d
    Signed-off-by: Michael Micucci <micucci@midokura.com>

---
+++ tools/data_migration/package_update.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# Copyright (C) 2016 Midokura SARL
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from data_migration.migration_funcs import install_packages
+from data_migration.migration_funcs import remove_packages
+from data_migration.migration_funcs import restart_service
+from data_migration.migration_funcs import stop_service
+from data_migration.migration_funcs import update_api_endpoints
+from data_migration.migration_funcs import update_repos
+import logging
+import subprocess
+
+log = logging.getLogger(name='data_migration/1.9.8')
+""":type: logging.Logger"""
+
+update_repos('mem-1.9', 'midonet-5')
+update_api_endpoints(to_port=8181)
+remove_packages(['midonet-api'])
+install_packages(['midolman', 'python-midonetclient'])
+stop_service('midonet-cluster')
+
+subprocess.call(
+    'mn-conf set -t default "cluster.neutron_importer.enabled=false"',
+    stderr=subprocess.STDOUT,
+    shell=True)
+
+restart_service('neutron-server')
+restart_service('midonet-cluster')
+stop_service('tomcat7')
+
+subprocess.call('mn-conf set -t default "cluster.loggers.root=DEBUG"',
+                stderr=subprocess.STDOUT,
+                shell=True)
+subprocess.call('mn-conf set -t default "agent.loggers.root=DEBUG"',
+                stderr=subprocess.STDOUT,
+                shell=True)
+subprocess.call('sudo rm /usr/bin/wdog',
+                stderr=subprocess.STDOUT,
+                shell=True)
