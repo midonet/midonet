fb8d2f256f73719d19d342a411c2361e592f8cd9
Ryu Ishimoto <ryu@midokura.com>
2016-04-19 00:43:47 +0000

    Add ability to add Host via API
    For upgrades, and to take advantage of the pre-binding mechanism where
    the midonet agents bind the ports on their start-ups, allow hosts to be
    created via REST API so that the upgrade orchestrator can pre-create
    these hosts and its bindings and tunnel zone memberships before
    upgrading the agents.  By doing so, we can create a state right before
    the full agent upgrades where we get to see the final view of the
    cluster data in zookeeper for verification.
    
    Change-Id: I9cdca0181bdf4e3fdabbb5b5620289cd0797e061
    Implements: MI-846

--- midonet-cluster/src/main/scala/org/midonet/cluster/services/rest_api/resources/HostResource.scala
+++ midonet-cluster/src/main/scala/org/midonet/cluster/services/rest_api/resources/HostResource.scala
@@ -23,17 +23,15 @@ import javax.ws.rs.core.Response
 import javax.ws.rs.core.Response.Status
 
 import scala.collection.JavaConverters._
-
 import com.google.inject.Inject
 import com.google.inject.servlet.RequestScoped
 import com.google.protobuf.TextFormat
-
 import org.midonet.cluster.data.ZoomConvert
 import org.midonet.cluster.data.storage.SingleValueKey
 import org.midonet.cluster.models.State
 import org.midonet.cluster.rest_api.ResponseUtils._
-import org.midonet.cluster.rest_api.annotation.{ApiResource, AllowGet, AllowList}
-import org.midonet.cluster.rest_api.models.{TunnelZone, Host, HostState, Interface}
+import org.midonet.cluster.rest_api.annotation.{AllowCreate, AllowGet, AllowList, ApiResource}
+import org.midonet.cluster.rest_api.models.{Host, HostState, Interface, TunnelZone}
 import org.midonet.cluster.rest_api.validation.MessageProperty._
 import org.midonet.cluster.services.MidonetBackend
 import org.midonet.cluster.services.rest_api.MidonetMediaTypes._
@@ -42,6 +40,7 @@ import org.midonet.cluster.services.rest_api.resources.MidonetResource._
 @ApiResource(version = 1, name = "hosts", template = "hostTemplate")
 @Path("hosts")
 @RequestScoped
+@AllowCreate(Array(APPLICATION_HOST_JSON_V3))
 @AllowGet(Array(APPLICATION_HOST_JSON_V3,
                 APPLICATION_JSON))
 @AllowList(Array(APPLICATION_HOST_COLLECTION_JSON_V3,
