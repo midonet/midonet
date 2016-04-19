892d2e0f051055084342df2a9b92360033548405
Joe Mills <joe@midokura.com>
2016-04-19 18:12:24 +0900

    BGP-IP: Bgp Translators
    This patch adds the translators for Bgp Speakers and Peers
    
    Change-Id: Ia2c98d23678f3d762deaa467f141083444afcfa2

--- midonet-cluster/src/main/scala/org/midonet/cluster/services/rest_api/neutron/resources/BgpSpeakerResource.scala
+++ midonet-cluster/src/main/scala/org/midonet/cluster/services/rest_api/neutron/resources/BgpSpeakerResource.scala
@@ -16,8 +16,6 @@
 
 package org.midonet.cluster.services.rest_api.neutron.resources
 
-import java.net.URI
-import java.util
 import java.util.UUID
 
 import javax.ws.rs._
@@ -25,41 +23,13 @@ import javax.ws.rs.core.{Response, UriInfo}
 
 import com.google.inject.Inject
 
-import org.midonet.cluster.rest_api.{BadRequestHttpException, NotFoundHttpException}
-import org.midonet.cluster.rest_api.neutron.NeutronUriBuilder
+import org.midonet.cluster.rest_api.BadRequestHttpException
 import org.midonet.cluster.rest_api.neutron.models.BgpSpeaker
-import org.midonet.cluster.rest_api.validation.MessageProperty.{RESOURCE_NOT_FOUND, getMessage}
 import org.midonet.cluster.services.rest_api.MidonetMediaTypes
 import org.midonet.cluster.services.rest_api.neutron.plugin.BgpApi
 
 class BgpSpeakerResource @Inject()(uriInfo: UriInfo,
                                    private val api: BgpApi) {
-
-    private val baseUri: URI = uriInfo.getBaseUri
-
-    @GET
-    @Path("{id}")
-    @Produces(Array(MidonetMediaTypes.NEUTRON_BGP_SPEAKER_JSON_V1))
-    def get(@PathParam("id") id: UUID): BgpSpeaker = {
-        val bgpSpeaker = api.getBgpSpeaker(id)
-        if (bgpSpeaker == null)
-            throw new NotFoundHttpException(getMessage(RESOURCE_NOT_FOUND, id))
-        bgpSpeaker
-    }
-
-    @GET
-    @Produces(Array(MidonetMediaTypes.NEUTRON_BGP_SPEAKERS_JSON_V1))
-    def list: util.List[BgpSpeaker] = api.getBgpSpeakers
-
-    @POST
-    @Consumes(Array(MidonetMediaTypes.NEUTRON_BGP_SPEAKER_JSON_V1))
-    @Produces(Array(MidonetMediaTypes.NEUTRON_BGP_SPEAKER_JSON_V1))
-    def create(bgpSpeaker: BgpSpeaker): Response = {
-        api.createBgpSpeaker(bgpSpeaker)
-        Response.created(
-            NeutronUriBuilder.getBgpSpeaker(baseUri, bgpSpeaker.id))
-            .entity(bgpSpeaker).build()
-    }
 
     @PUT
     @Path("{id}")
@@ -69,13 +39,6 @@ class BgpSpeakerResource @Inject()(uriInfo: UriInfo,
             throw new BadRequestHttpException("Path ID does not match object ID")
         }
         api.updateBgpSpeaker(bgpSpeaker)
-        Response.noContent().build()
-    }
-
-    @DELETE
-    @Path("{id}")
-    def delete(@PathParam("id") id: UUID): Response = {
-        api.deleteBgpSpeaker(id)
         Response.noContent().build()
     }
 }
