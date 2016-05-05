2cb72af67c53c77fe2a7ccbae95c51bcf9385c79
Alex Bikfalvi <alex.bikfalvi@midokura.com>
2016-05-05 11:34:54 +0200

    Enable Java Flight Recorder
    Renames the `hotspot` flavor as `jfr`, sets the the Flight Recorder
    switches for the MidoNet Agent, starts and stops the JFR with the
    performance test, with the JFR recordings created at /tmp/jfr and
    then modifies the ci-perf.sh to pack the JFR recordings to the
    current workspace.
    
    Ref: MI-635
    
    Change-Id: I8e8c8415285dc130c3682dccbf5883acc2c7d667
    Signed-off-by: Alex Bikfalvi <alex.bikfalvi@midokura.com>

--- tests/ci-perf.sh
+++ tests/ci-perf.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
+
 source venv/bin/activate
 pushd tests/mdts/tests/performance_tests/
-
 ./run_tests.sh -r $WORKSPACE/tests -l logs
-
 popd
 
+pushd /tmp/jfr
+tar -cvzf $WORKSPACE/perf_jfr.tar.gz *
+popd
