4c3ce48c8adf222c46656867654f9a6fd6da0097
Alex Bikfalvi <alex.bikfalvi@midokura.com>
2016-05-05 11:34:54 +0200

    Enable Java Flight Recorder
    Renames the `hotspot` flavor as `jfr`, and sets the the Flight
    Recorder switches for the MidoNet Agent.
    
    Ref: MI-635
    
    Change-Id: I8e8c8415285dc130c3682dccbf5883acc2c7d667
    Signed-off-by: Alex Bikfalvi <alex.bikfalvi@midokura.com>

--- tests/ci-perf.sh
+++ tests/ci-perf.sh
@@ -1,7 +1,29 @@
+#!/bin/bash
+
 source venv/bin/activate
 pushd tests/mdts/tests/performance_tests/
 
+SANDBOX_FLAVOUR="default_v2_neutron+kilo+perf"
+
+while getopts ":f:h" opt; do
+    case $opt in
+    f)
+        SANDBOX_FLAVOUR=$OPTARG
+        ;;
+    h)
+        echo "$0 [-f SANDBOX_FLAVOUR] [-o OVERRIDE_DIRECTORY]" \
+             " [-p PROVISIONING_SCRIPT]"
+        exit 1
+        ;;
+    esac
+done
+
 ./run_tests.sh -r $WORKSPACE/tests -l logs
+
+if [ "$SANDBOX_FLAVOUR" = "default_v2_neutron+kilo+perf" ] ; then
+    docker cp mnsandboxmdts_midolman1_1:/midolman.jfr $WORKSPACE/midolman1_1.jfr
+    docker cp mnsandboxmdts_midolman2_1:/midolman.jfr $WORKSPACE/midolman2_1.jfr
+fi
 
 popd
 
