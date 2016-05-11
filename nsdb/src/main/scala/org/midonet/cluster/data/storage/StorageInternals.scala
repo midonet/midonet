eec0359e1804c499945aa32b9ca41b419444c364
Alex Bikfalvi <alex.bikfalvi@midokura.com>
2016-05-11 18:24:29 +0200

    ZOOM: Use a single object observable cache
    Currently, the ZOOM topology storage and state storage implement
    their own distinct caches of object observable. The topology
    storage uses this cache to service requests for these observables,
    the state storage levegares these observble to detect when an
    object was deleted.
    
    This change adds a common StorageInternals interface, that allows
    multiple classes to rely on the same interal implementation for
    object observable.
    
    In particular, the state storage will use the same object observable
    cache provided by the topology storage, without the need of
    implementing its own.
    
    Ref: MI-860
    
    Change-Id: I8d00f04103d1ddfc51876214093e55e51b1f4efb
    Signed-off-by: Alex Bikfalvi <alex.bikfalvi@midokura.com>

---
+++ nsdb/src/main/scala/org/midonet/cluster/data/storage/StorageInternals.scala
@@ -0,0 +1,16 @@
+package org.midonet.cluster.data.storage
+
+import rx.Observable
+
+import org.midonet.cluster.data._
+
+trait StorageInternals {
+
+    /**
+      * Provides an internal observable to the object with the specified class,
+      * identifier and storage version.
+      */
+    protected def internalObservable[T](clazz: Class[T], id: ObjId,
+                                        version: Long): Observable[T]
+
+}
