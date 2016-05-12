005f2e60536edd2020c1d3591f278ef087ca7fe0
Mateo Hermida <mateo@midokura.com>
2016-05-12 16:45:57 +0200

    Allow multiple sandbox to run on the same machine
    Midonet sandbox allows sandboxes to have different names, but mdts tests
    assume the sandbox will be named "mdts". This does not allow us to run
    multiple sandbox environments on a same machine. With this change the
    caller of the run_tests.sh scripts will be allowed to change a value in
    mdts.conf with the name of the sandbox in which the tests will be
    performed. The provisioning scripts' pipework and bridge setups now also
    take into account the sandbox name so as to avoid using the same
    interfaces in different sandboxes.
    
    Note: For legacy support in existing scripts, the default name for
    a sandbox is set as "mdts" in the mdts.conf file.
    
    Change-Id: I4485eadc2f61153d413d6863ca922f397486e1ea
    Signed-off-by: Mateo Hermida <mateo@midokura.com>

--- tests/sandbox/provisioning/bgp-provisioning.sh
+++ tests/sandbox/provisioning/bgp-provisioning.sh
@@ -3,31 +3,31 @@
 echo "Provisioning bgp interfaces..."
 
 # Cleaning previous bridge if exists
-sudo ip link set dev brbgp0 down
-sudo ip link set dev brbgp1 down
-sudo ip link set dev brbgp2 down
-sudo brctl delbr brbgp0
-sudo brctl delbr brbgp1
-sudo brctl delbr brbgp2
+sudo ip link set dev brbgp0_${SANDBOX_NAME} down
+sudo ip link set dev brbgp1_${SANDBOX_NAME} down
+sudo ip link set dev brbgp2_${SANDBOX_NAME} down
+sudo ip link del brbgp0_${SANDBOX_NAME}
+sudo ip link del brbgp1_${SANDBOX_NAME}
+sudo ip link del brbgp2_${SANDBOX_NAME}
 
 # One quagga (master) advertising "inet" networks connected to two
 # quaggas (peers) which will do the peering with midolman
-sudo pipework/pipework brbgp0 -i bgp0 mnsandboxmdts_quagga0_1 10.10.0.1/24
-sudo pipework/pipework brbgp0 -i bgp0 mnsandboxmdts_quagga1_1 10.10.0.2/24
-sudo pipework/pipework brbgp0 -i bgp0 mnsandboxmdts_quagga2_1 10.10.0.3/24
+sudo pipework/pipework brbgp0_${SANDBOX_NAME} -i bgp0 mnsandbox${SANDBOX_NAME}_quagga0_1 10.10.0.1/24
+sudo pipework/pipework brbgp0_${SANDBOX_NAME} -i bgp0 mnsandbox${SANDBOX_NAME}_quagga1_1 10.10.0.2/24
+sudo pipework/pipework brbgp0_${SANDBOX_NAME} -i bgp0 mnsandbox${SANDBOX_NAME}_quagga2_1 10.10.0.3/24
 
 # Two interfaces per quagga peer (for single and multiple sessions)
 # BGP peer1
-sudo pipework/pipework brbgp1 -i bgp1 mnsandboxmdts_quagga1_1 10.1.0.240/24
-sudo pipework/pipework brbgp1 -i bgp2 mnsandboxmdts_quagga2_1 10.1.0.241/24
+sudo pipework/pipework brbgp1_${SANDBOX_NAME} -i bgp1 mnsandbox${SANDBOX_NAME}_quagga1_1 10.1.0.240/24
+sudo pipework/pipework brbgp1_${SANDBOX_NAME} -i bgp2 mnsandbox${SANDBOX_NAME}_quagga2_1 10.1.0.241/24
 
 # BGP peer2
-sudo pipework/pipework brbgp2 -i bgp1 mnsandboxmdts_quagga2_1 10.2.0.240/24
-sudo pipework/pipework brbgp2 -i bgp2 mnsandboxmdts_quagga1_1 10.2.0.241/24
+sudo pipework/pipework brbgp2_${SANDBOX_NAME} -i bgp1 mnsandbox${SANDBOX_NAME}_quagga2_1 10.2.0.240/24
+sudo pipework/pipework brbgp2_${SANDBOX_NAME} -i bgp2 mnsandbox${SANDBOX_NAME}_quagga1_1 10.2.0.241/24
 
 # An interface per midolman peer (for single and multiple sessions)
 # MM peer1
-sudo pipework/pipework brbgp1 -i bgp0 mnsandboxmdts_midolman1_1 10.1.0.1/24
+sudo pipework/pipework brbgp1_${SANDBOX_NAME} -i bgp0 mnsandbox${SANDBOX_NAME}_midolman1_1 10.1.0.1/24
 
 # MM peer2
-sudo pipework/pipework brbgp2 -i bgp0 mnsandboxmdts_midolman2_1 10.2.0.1/24
+sudo pipework/pipework brbgp2_${SANDBOX_NAME} -i bgp0 mnsandbox${SANDBOX_NAME}_midolman2_1 10.2.0.1/24
