dd95c00a7f153b0d5d12c5955ffcacef74fb194e
Miguel Ángel Herranz Trillo <miguel@midokura.com>
2016-05-18 14:42:56 +0000

    SSH connection to MDTS targets
    Currently MDTS use the a python Docker API to access the
    containers. This change allow MDTS to use a SSH connection instead,
    this way MDTS can be used against other machines providing the
    services to be tested.
    
    See mdts.conf and example-ssh-containers.yaml for details
    
    WARNING: not all test suites has been tested using this method, so
    some of them may fail in this mode. This can be due to wrong escape
    incompatibilities between the python Docker API and the python SSH
    API (paramiko). Only the following tests has been run in this mode
    against a sandbox:
    
       functional_tests  test_fip_icmp
                         test_brige:test_mac_learning
                         test_brige:test_dhcp
       performance_tests test_perf_flowstate
    
    Ref: MI-896
    
    Change-Id: Iaf46715a5e3ec5a02d2a5563d8b23d5fab7d6fa5
    Signed-off-by: Miguel Ángel Herranz Trillo <miguel@midokura.com>

---
+++ tests/mdts/lib/mdtsdocker.py
@@ -0,0 +1,37 @@
+#
+# Copyright 2016 Midokura SARL
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from docker import Client
+
+class DockerClient(Client):
+    def __init__(self, base_url, timeout, sandbox_prefix, sandbox_name):
+        super(DockerClient, self).__init__(base_url=base_url,
+                                           timeout=timeout)
+        self.sandbox_prefix = sandbox_prefix
+        self.sandbox_name = sandbox_name
+
+    def containers(self, all=None):
+        only_sandbox = lambda c: self._is_sandbox_container(c)
+        containers = super(DockerClient, self).containers(all=all)
+        return filter(only_sandbox, containers)
+
+    def _is_sandbox_container(self, container):
+        prefix_len = len(self.sandbox_prefix)
+        convert_name = lambda n: n.replace('/', '')[prefix_len:].split('_')[0]
+        container_sandbox_names = map(convert_name, container['Names'])
+        return self.sandbox_name in container_sandbox_names
+
+
