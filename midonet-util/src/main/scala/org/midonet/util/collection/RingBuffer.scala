2b5b61f4f17affe19e0e4284efc1a77ad4472ef6
Xavi León <xavi.leon@midokura.com>
2016-06-09 20:57:18 +0200

    FlowState: introduce byte buffer block writer and reader
    This patch introduces an input and output streams that use a ring
    buffer of byte buffers (blocks) as the underlying storage mechanism.
    Blocks are added on-demand to the ring buffer. Each block contains
    a header specifying at least the length of the data written.
    This patch also introduces an expiration mechanism for the
    blocks allocated to a given ring buffer. After a specified amount of time,
    blocks are removed from the tail of the ring buffer and the header
    cleared. This patch slightly extends the ring buffer abstraction,
    including documentation and unit testing for the newly added
    functionality. This components will be used in subsequent patches to
    implement a memory mapped ring buffer to store flow state.
    
    Ref.: MI-985
    Signed-off-by: Xavi León <xavi.leon@midokura.com>
    
    Change-Id: If405203c72742c03e73de6f816570b8156218108

--- midonet-util/src/main/scala/org/midonet/util/collection/RingBuffer.scala
+++ midonet-util/src/main/scala/org/midonet/util/collection/RingBuffer.scala
@@ -16,15 +16,33 @@
 
 package org.midonet.util.collection
 
-class RingBuffer[T: Manifest](val capacity: Int, emptyValue: T) {
+/**
+  * Writes(put) on the head and reads(take/peek) from the tail of the queu.
+  */
+class RingBuffer[T: Manifest](val capacity: Int, val emptyValue: T) {
 
     private val ring = new Array[T](capacity)
     private var readIndex = 0
     private var writeIndex = 0
 
-    def isEmpty = readIndex == writeIndex
-    def isFull =  ((writeIndex + 1) % capacity) == readIndex
+    /**
+      * Constructor allowing to create the ring buffer with a given position
+      * for reading and writting.
+      */
+    def this(capacity: Int, emptyValue: T, head: Int, tail: Int) = {
+        this(capacity, emptyValue)
+        readIndex = head
+        writeIndex = tail
+    }
 
+    def isEmpty: Boolean = readIndex == writeIndex
+    def nonEmpty: Boolean = readIndex != writeIndex
+    def isFull: Boolean =  ((writeIndex + 1) % capacity) == readIndex
+    def length: Int = (writeIndex - readIndex + capacity) % capacity
+
+    /**
+      * Writes an element to the head of the queue.
+      */
     def put(value: T) {
         if (isFull) {
             throw new IllegalArgumentException("buffer is full")
@@ -34,8 +52,35 @@ class RingBuffer[T: Manifest](val capacity: Int, emptyValue: T) {
         }
     }
 
-    def peek: Option[T] = if (isEmpty) None else Option(ring(readIndex))
+    /**
+      * Writes the element created by the factory method to the head
+      * of the queue.
+      */
+    def put(factory: (Int) => T): T = {
+        if (isFull) {
+            throw new IllegalArgumentException("buffer is full")
+        } else {
+            val value = factory(writeIndex)
+            put(value)
+            value
+        }
+    }
 
+    /**
+      * Reads (without removing) the element at the tail of the queue.
+      */
+    def peek: Option[T] =
+        if (isEmpty) None else Option(ring(readIndex))
+
+    /**
+      * Reads (without removing) the element at the head of the queue.
+      */
+    def head: Option[T] =
+        if (isEmpty) None else Option(ring((writeIndex - 1 + capacity) % capacity))
+
+    /**
+      * Reads and removes the element at the tail of the queue.
+      */
     def take(): Option[T] = {
         if (isEmpty) {
             None
@@ -45,5 +90,28 @@ class RingBuffer[T: Manifest](val capacity: Int, emptyValue: T) {
             readIndex = (readIndex + 1) % capacity
             ret
         }
+    }
+
+    /** This iterator is not thread sage. The behaviour is undefined
+      * the underlying ring buffer is modified. */
+    def iterator: Iterator[T] = new Iterator[T] {
+        private var tailIndex = readIndex
+        private val headIndex = writeIndex
+
+        override def hasNext = headIndex != tailIndex
+
+        override def next = {
+            if (hasNext) {
+                val ret = ring(tailIndex)
+                tailIndex = (tailIndex + 1) % capacity
+                ret
+            } else {
+                throw new IndexOutOfBoundsException(
+                    "Trying to access a value outside the view")
+            }
+        }
+
+        override def length = (headIndex - tailIndex + capacity) % capacity
+
     }
 }
