9cef4872fcce3eec25f594ec2476754aecd52ce9
Xavi León <xavi.leon@midokura.com>
2016-06-09 20:57:18 +0200

    FlowState: introduce byte buffer block streaming
    This patch introduces an input and output streams that use a queue
    of byte buffers (blocks) as the underlying storage mechanism. Blocks are
    added on-demand to the queue from a pool of finite byte buffers. Each
    block contains a header specifying at least the length of the data
    written. This patch also introduces an expiration mechanism for the
    blocks allocated to a given stream. After a specified amount of time,
    blocks are removed from the head of the queue and returned to the pool
    of byte buffers to be later reused. This components will be used in
    subsequent patches to implement a memory mapped ring buffer to store
    flow state.
    
    Ref.: MI-985
    Signed-off-by: Xavi León <xavi.leon@midokura.com>
    
    Change-Id: If405203c72742c03e73de6f816570b8156218108

---
+++ midonet-util/src/main/scala/org/midonet/util/io/stream/ByteBufferExpirationStream.scala
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2016 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.util.io.stream
+
+import org.midonet.util.concurrent.NanoClock.{DEFAULT => clock}
+
+trait ExpirationBlockHeader extends BlockHeader {
+    def firstEntryTime: Long
+    def lastEntryTime: Long
+}
+
+/**
+  * This trait allows a [[ByteBufferBlockStream]] to handle block
+  * expirations according to a specified expiration time. The expired block
+  * is returned to the block pool so it can be reused later on if needed.
+  * The expiration time is a soft deadline meaning that blocks can be
+  * released after the deadline is due as we need to explicitly call
+  * the invalidateBlocks method.
+  */
+trait ExpiringBlockStream
+    extends ByteBufferBlockStream[ExpirationBlockHeader] {
+
+    /** Expiration time in nanoseconds, same precision as used by [[clock]]. */
+    val expirationTime: Long
+
+    /**
+      * This method triggers and invalidates the entries in the stream that
+      * are older than a configured amount of time.
+      */
+    def invalidateBlocks(): Unit = {
+        val tick = clock.tick
+        val numBlocks = buffers.length
+        while (buffers.nonEmpty
+               && isBlockStale(blockBuilder(buffers.front), tick)) {
+            val bb = buffers.dequeue()
+            bufferPool.offer(bb)
+        }
+
+        if (numBlocks - buffers.length > 0) {
+            log.debug(s"Released ${numBlocks - buffers.length} out of " +
+                      s"$numBlocks blocks with stale entries.")
+        }
+    }
+
+    @inline
+    private def isBlockStale(blockHeader: ExpirationBlockHeader, tick: Long): Boolean = {
+        val bool = tick - blockHeader.lastEntryTime > expirationTime
+        val diff = tick - blockHeader.lastEntryTime
+        log.debug(s"$bool $tick ${blockHeader.lastEntryTime} $diff ${expirationTime}")
+        bool
+    }
+
+}
