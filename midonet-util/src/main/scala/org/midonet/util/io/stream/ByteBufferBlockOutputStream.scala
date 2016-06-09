d2e5ddba060792cdaf11268b1dd34cbcdb371432
Xavi León <xavi.leon@midokura.com>
2016-06-09 20:57:18 +0200

    FlowState: introduce byte buffer block streaming
    This patch introduces an input and output streams that use a queue
    of byte buffers (blocks) as the underlying storage mechanism. Blocks are
    added on-demand to the queue from a pool of finite byte buffers. Each
    block contains a header specifying at least the length of the data
    written. This patch also introduces an expiration mechanism for the
    blocks allocated to a given stream. After a specified amount of time,
    blocks are removed from the head of the queue and returned to the pool
    of byte buffers to be later reused. This components will be used in
    subsequent patches to implement a memory mapped ring buffer to store
    flow state.
    
    Ref.: MI-985
    Signed-off-by: Xavi León <xavi.leon@midokura.com>
    
    Change-Id: If405203c72742c03e73de6f816570b8156218108

---
+++ midonet-util/src/main/scala/org/midonet/util/io/stream/ByteBufferBlockOutputStream.scala
@@ -0,0 +1,133 @@
+/*
+ * Copyright 2016 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.util.io.stream
+
+import java.io.{IOException, OutputStream}
+import java.nio.ByteBuffer
+
+import scala.collection.mutable
+
+import org.midonet.util.collection.ObjectPool
+
+/**
+  * This class implements an output stream for writing arbitrary data on an
+  * array of byte buffers (blocks). It supports arbitrary header formats as
+  * exposed in [[ByteBufferBlockStream]]. This block writer takes care of
+  * allocating a new block from the pool when necessary.
+  *
+  * @param blockBuilder The builder/reader for the block header.
+  * @param buffers The in-order queue of buffers used by the writer.
+  * @param bufferPool The pool where the buffer blocks were taken.
+  * @param allowSplit if true, an array won't be split in two different
+  *                   blocks but allocate a new one and write the whole
+  *                   array there. False by default.
+  */
+
+class ByteBufferBlockOutputStream[H <: BlockHeader]
+                        (override val blockBuilder: BlockHeaderBuilder[H],
+                         override val buffers: mutable.Queue[ByteBuffer],
+                         val bufferPool: ObjectPool[ByteBuffer],
+                         val allowSplit: Boolean = false)
+    extends OutputStream with ByteBufferBlockStream[H] {
+
+    /**
+      * Writes the specified byte to this output stream. It allocates a new
+      * byte buffer if there's no space left on the current block.
+      *
+      * @param b
+      */
+    override def write(b: Int): Unit = {
+        write(Array[Byte]((b & 0xff).toByte), 0, 1)
+    }
+
+    /**
+      * Writes <code>len</code> bytes from the specified byte array
+      * starting at offset <code>off</code> to this output stream. It takes
+      * care of allocating a new block if needed until the requested amount
+      * of bytes is written. If member allowSplit is true, a chunk can be
+      * split between two or more blocks for a more space friendly storage.
+      */
+    @throws[IOException]
+    override def write(buff: Array[Byte], offset: Int, length: Int): Unit = {
+        if (allowSplit) {
+            var written = 0
+
+            @inline def remainingLength = length - written
+
+            while (remainingLength > 0) {
+                val remainingBuffer = checkRemainingBuffer()
+                val toWrite = if (remainingBuffer < remainingLength) {
+                    remainingBuffer
+                } else {
+                    remainingLength
+                }
+                log.debug(s"Writing $toWrite bytes to block ${buffers.length}")
+                current.put(buff, offset + written, toWrite)
+                written += toWrite
+                blockBuilder.update(current)
+            }
+        } else {
+            val remainingBuffer = checkRemainingBuffer()
+            if (length > current.capacity - blockBuilder.headerSize) {
+                log.error(s"Buffer size ($length bytes) bigger than block size " +
+                          s"${current.capacity} but splitting it is not allowed. " +
+                          s"This is likely a configuration issue. Try increasing " +
+                          s"the block size.")
+                throw new IOException()
+            }
+            if (length > remainingBuffer) {
+                allocateNewBlock()
+            }
+            current.put(buff, offset, length)
+            blockBuilder.update(current)
+        }
+    }
+
+    /**
+      * Closes this stream without freeing any resource used and leaves
+      * it ready for reading from it. This method flushes any buffered data.
+      */
+    override def close(): Unit = {
+        for (bb <- buffers) bb.flip()
+    }
+
+    @inline private def current = buffers.last
+
+    /** Allocates a new block from buffer pool and updates the output stream. */
+    @inline private def allocateNewBlock() = {
+        val buffer = bufferPool.take
+        blockBuilder.init(buffer)
+        buffers += buffer
+    }
+
+    /**
+      * This method checks the size of the current buffer and returns the amount
+      * of remaining bytes that can be written. If there are no buffers, a new
+      * block is allocated for immediate use.
+      *
+      * @return The amount of bytes that can be written to this buffer
+      */
+    private def checkRemainingBuffer(): Int = {
+        if (buffers.isEmpty || buffers.last.remaining() == 0) {
+            log.debug("Adding a new block because there was none or the buffer " +
+                      "filled up.")
+            allocateNewBlock()
+        }
+        buffers.last.remaining()
+    }
+
+}
---
+++ midonet-util/src/main/scala/org/midonet/util/io/stream/ByteBufferBlockStream.scala
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2016 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.util.io.stream
+
+import java.nio.ByteBuffer
+
+import scala.collection.mutable
+
+import com.typesafe.scalalogging.Logger
+
+import org.slf4j.LoggerFactory
+
+/**
+  * Trait that specify the information that should be present in the
+  * headers for the Block[Output/Input]Stream format. The actual implementation
+  * and schema of the header is up to the underlying implementation of the
+  * block stream.
+  */
+trait BlockHeader {
+
+    /** Length in bytes of the data stream in the block. Does not account
+      * for the header.
+      */
+    def blockLength: Int
+}
+
+
+/**
+  * These traits specify the information and methods that a header
+  * builder / reader must implement. This is also specific to the
+  * streaming protocol.
+  */
+trait BlockHeaderBuilder[T <: BlockHeader] {
+
+    val headerSize: Int
+
+    /**
+      * Initializes the meta data of the block and moves the position
+      * of this buffer to the header size, so subsequent writes to the
+      * stream does not override the header information.
+      *
+      * @param buffer
+      */
+    def init(buffer: ByteBuffer): Unit
+
+    /**
+      * Updates the metadata of the block. Shouldn't modify the buffer position.
+      *
+      * @param buffer
+      */
+    def update(buffer: ByteBuffer, params: AnyVal*): Unit
+
+    /**
+      * Actual builder method that reads the header from the corresponding
+      * offsets from the header without touching the buffer position.
+      *
+      * @param buffer
+      * @return
+      */
+    def apply(buffer: ByteBuffer): T
+}
+
+/**
+  * Trait exposing the main information necessary by the underlying in/out
+  * block streams.
+  */
+trait ByteBufferBlockStream[H <: BlockHeader] {
+
+    val log = Logger(
+        LoggerFactory.getLogger("org.midonet.services.bytebuffer-blockstream"))
+
+    /** Buffers in use by this stream are stored in this queue */
+    val buffers: mutable.Queue[ByteBuffer]
+
+    /** Generic block builder for CRUD ops on the header */
+    val blockBuilder: BlockHeaderBuilder[H]
+
+}
+
+
