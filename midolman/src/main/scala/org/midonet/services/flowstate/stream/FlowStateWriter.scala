c09c1193ecce58e3f33de3c6c0d533c1c68ac4d0
Xavi León <xavi.leon@midokura.com>
2016-06-14 06:56:46 +0200

    [WIP] FlowState: add flow state message streams
    This patch introduces the flow state input and output streams. They use
    the underlying snappy streams and byte buffer streams to write flow
    state data compressed and stored in blocks. These streams make sure to
    write the size of the message and the message itself in a single chunk
    to avoid being split into separate blocks. For the flow state
    encoding/decondig, we make use of the SbeEncoder, a wrapper class that
    contains methods to encode/decode information into flow state SBE
    messages and provides access the underlying byte array holding the data.
    
    This patch also provides the interface for creating raw ByteBufferBlock
    streams associated to a given portId.
    
    TODO: add tests for the memory mapped files
    
    Ref.: MI-985
    Change-Id: I136a677c17f22d533fbc1065f967d8d7d5ea3f9e
    Signed-off-by: Xavi León <xavi.leon@midokura.com>

---
+++ midolman/src/main/scala/org/midonet/services/flowstate/stream/FlowStateWriter.scala
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2016 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.services.flowstate.stream
+
+import java.io._
+import java.nio.ByteBuffer
+import java.util.UUID
+
+import com.google.common.annotations.VisibleForTesting
+
+import org.midonet.midolman.config.FlowStateConfig
+import org.midonet.packets.{FlowStateEthernet, SbeEncoder}
+import org.midonet.services.flowstate.stream.snappy.SnappyBlockWriter
+import org.midonet.util.Clearable
+import org.midonet.util.collection.RingBufferWithFactory
+import org.midonet.util.io.stream._
+
+object FlowStateWriter {
+
+    @VisibleForTesting
+    private[flowstate] def apply(config: FlowStateConfig,
+                                 buffers: RingBufferWithFactory[ByteBuffer])
+    : FlowStateWriter = {
+        val blockWriter = new ByteBufferBlockWriter(FlowStateBlock,
+                                                    buffers,
+                                                    config.expirationTime toNanos)
+        val snappyWriter = new SnappyBlockWriter(blockWriter, config.blockSize)
+        new FlowStateWriter(config, snappyWriter)
+    }
+
+    def apply(config: FlowStateConfig, portId: UUID): FlowStateWriter = {
+        val blockWriter = ByteBufferBlockWriter(config, portId)
+        val snappyWriter = new SnappyBlockWriter(blockWriter, config.blockSize)
+        new FlowStateWriter(config, snappyWriter)
+    }
+}
+
+/**
+  * Output stream where flow state is written to already encoded. Each port
+  * should have its own output stream. Use the [[FlowStateWriter#apply]]
+  * constructors to create an output stream for a given port. This class is
+  * not thread safe.
+  *
+  * @param config
+  */
+protected class FlowStateWriter(val config: FlowStateConfig,
+                                val out: SnappyBlockWriter)
+    extends Closeable with Flushable with Clearable {
+
+    private[flowstate] val buff =
+        ByteBuffer.allocate(LengthSize +
+                            FlowStateEthernet.FLOW_STATE_MAX_PAYLOAD_LENGTH)
+
+    /**
+      * Write the flow state message encoded by the 'encoder' into the
+      * data stream.
+      */
+    def write(encoder: SbeEncoder): Unit = {
+        val msgSize = encoder.encodedLength()
+        buff.clear()
+        buff.putInt(msgSize)
+        buff.put(encoder.flowStateBuffer.array(), 0, msgSize)
+        out.write(buff.array(), 0, LengthSize + msgSize)
+    }
+
+    /**
+      * Refer to [[Flushable#flush]]
+      */
+    override def flush(): Unit = out.flush()
+
+
+    /**
+      * Closes this stream without freeing any resource used and leaves
+      * it ready for reading from it. This method flushes any buffered data.
+      */
+    @throws[IllegalStateException]
+    override def close(): Unit = {
+        log.debug("Closing flow state compressed (snappy) output stream.")
+        out.close()
+    }
+}
+
+
