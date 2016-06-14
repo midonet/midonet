a23f39c6750b00039a566bafd834d3c2ba4dabd7
Xavi León <xavi.leon@midokura.com>
2016-06-14 06:56:46 +0200

    FlowState: add flow state message streams
    This patch introduces the flow state input and output streams. They use
    the underlying snappy streams and byte buffer streams to write flow
    state data compressed and stored in blocks. These streams make sure to
    write the size of the message and the message itself in a single chunk
    to avoid being split into separate blocks. For the flow state
    encoding/decondig, we make use of the SbeEncoder, a wrapper class that
    contains methods to encode/decode information into flow state SBE
    messages and provides access the underlying byte array holding the data.
    
    This patch only provides a heap byte buffer allocator so the underlying
    storage is not baked by a file yet. This feature will be introduced in
    the following patch.
    
    Ref.: MI-985
    Change-Id: I136a677c17f22d533fbc1065f967d8d7d5ea3f9e
    Signed-off-by: Xavi León <xavi.leon@midokura.com>

---
+++ midolman/src/main/scala/org/midonet/services/flowstate/stream/FlowStateInputStream.scala
@@ -0,0 +1,113 @@
+/*
+ * Copyright 2016 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.services.flowstate.stream
+
+import java.io.IOException
+import java.nio.ByteBuffer
+import java.util.UUID
+
+import scala.collection.mutable
+import scala.util.control.NonFatal
+
+import com.typesafe.scalalogging.Logger
+
+import org.slf4j.LoggerFactory
+
+import org.midonet.midolman.config.FlowStateConfig
+import org.midonet.packets.SbeEncoder
+import org.midonet.services.flowstate.stream.snappy.SnappyInputStream
+import org.midonet.util.io.stream.ByteBufferBlockInputStream
+
+object FlowStateInputStream {
+
+    val LengthSize = 4
+
+    def apply(config: FlowStateConfig,
+              buffers: mutable.Queue[ByteBuffer])
+    : FlowStateInputStream = {
+        val blockInput = new ByteBufferBlockInputStream(
+            FlowStateBlock, buffers)
+        val compressedInput = new SnappyInputStream(blockInput)
+        new FlowStateInputStream(compressedInput)
+    }
+
+    def apply(config: FlowStateConfig, portId: UUID): FlowStateInputStream = {
+        // TODO: read from disk
+        // If (file exists) -> build the byte buffers queue
+        // else -> fail
+        ???
+    }
+
+}
+
+/**
+  * Input stream that decodes flow state messages from the input stream. Each
+  * port should have its own input stream. Use the [[FlowStateInputStream#apply]]
+  * constructors to create an input stream for a given port. This class is
+  * not thread safe.
+  *
+  * @param in
+  */
+protected class FlowStateInputStream(var in: SnappyInputStream) {
+
+    import FlowStateInputStream._
+
+    val log = Logger(
+        LoggerFactory.getLogger("org.midonet.services.flowstatestream"))
+
+    /**
+      * Read the flow state messages from a given stream of flow state messages.
+      * It decodes the message and returns the encoder used. The encoder already
+      * contains the methods to read the contents of the message.
+      *
+      * @throws IOException
+      * @return The [[SbeEncoder]] The encoder used to decode the message
+      */
+    def read(): Option[SbeEncoder] = {
+        try {
+            val length = new Array[Byte](LengthSize)
+            if (in.read(length) != -1) {
+                val buff = new Array[Byte](ByteBuffer.wrap(length).getInt)
+                log.debug("Before reading content.")
+                in.read(buff)
+
+                val encoder = new SbeEncoder()
+                encoder.decodeFrom(buff)
+                Option(encoder)
+            } else {
+                log.debug("Unexpected failure reading size of flow state " +
+                          "message. It was possibly truncated during a write " +
+                          "failure. Ignoring and returning EOF. ")
+                None // EOF
+            }
+        } catch {
+            case NonFatal(e) =>
+                log.debug(s"Failed to parse data while reading. EOF.", e)
+                None // EOF
+        }
+    }
+
+    /**
+      * Refer to InputStream.reset() for the general contract.
+      */
+    def reset(): Unit = {
+        // Snappy input stream does not support reset, allocate a new stream
+        in.input.reset()
+        in = new SnappyInputStream(in.input)
+    }
+
+}
