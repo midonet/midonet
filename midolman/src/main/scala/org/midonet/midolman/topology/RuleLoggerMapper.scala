0f4b57cc57d57ca56d2020031442c4001e6e93ba
Brandon Berg <bberg@midokura.com>
2016-06-14 21:17:06 +0900

    FWLOG: Implement agent-side LoggingResource
    -Add RuleLogger simulation object.
    -Add RuleLoggerMapper.
    -Add list of RuleLogger references to Chain simulation object and modify
     ChainMapper to load it.
    -In simulation, log result of chain application to all the chain's RuleLoggers
    
    Signed-off-by: Brandon Berg <bberg@midokura.com>
    Change-Id: I85aa2b17b5d688925ec69b3e7c0cd6d45df97989

---
+++ midolman/src/main/scala/org/midonet/midolman/topology/RuleLoggerMapper.scala
@@ -0,0 +1,99 @@
+/*
+ * Copyright 2015 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.midolman.topology
+
+import java.util.UUID
+
+import com.google.protobuf.GeneratedMessage
+
+import rx.Observable
+
+import org.midonet.cluster.models.Commons.LogEvent
+import org.midonet.cluster.models.Topology.LoggingResource.Type
+import org.midonet.cluster.models.Topology.{LoggingResource, RuleLogger}
+import org.midonet.cluster.util.UUIDUtil.asRichProtoUuid
+import org.midonet.midolman.logging.MidolmanLogging
+import org.midonet.midolman.simulation.{FileRuleLogger, RuleLogger => SimRuleLogger}
+import org.midonet.util.functors.{makeAction0, makeAction1, makeFunc1}
+
+class RuleLoggerMapper(id: UUID, vt: VirtualTopology)
+    extends VirtualDeviceMapper(classOf[SimRuleLogger], id, vt)
+            with MidolmanLogging {
+
+    override def logSource: String = "org.midonet.devices.rule-logger"
+    override def logMark: String = s"rule-logger:$id"
+
+    private val logResTracker =
+        new StoreObjectReferenceTracker(vt, classOf[LoggingResource], log)
+
+    private var ruleLogger: RuleLogger = null
+    private val ruleLoggerObservable =
+        vt.store.observable(classOf[RuleLogger], id)
+            .observeOn(vt.vtScheduler)
+            .doOnCompleted(makeAction0(ruleLoggerDeleted()))
+            .doOnNext(makeAction1(ruleLoggerUpdated))
+
+    private var simRuleLogger: SimRuleLogger = null
+
+    // NB: The order of merged observables is important. LogResTracker's
+    // observable must be merged before ruleLoggerObservable is merged, so that
+    // the LogResource update isn't dropped.
+    override def observable: Observable[SimRuleLogger] =
+        Observable.merge(logResTracker.refsObservable, ruleLoggerObservable)
+            .observeOn(vt.vtScheduler)
+            .filter(makeFunc1(isReady))
+            .map[SimRuleLogger](makeFunc1(build))
+            .distinctUntilChanged()
+
+    private def build(gm: GeneratedMessage): SimRuleLogger = {
+        assertThread()
+        val lr = logResTracker.currentRefs.head._2
+        val logAcceptEvents =
+            lr.getEnabled && (ruleLogger.getEvent == LogEvent.ACCEPT ||
+                              ruleLogger.getEvent == LogEvent.ALL)
+        val logDropEvents =
+            lr.getEnabled && (ruleLogger.getEvent == LogEvent.DROP ||
+                              ruleLogger.getEvent == LogEvent.ALL)
+
+        simRuleLogger = lr.getType match {
+            case Type.FILE =>
+                new FileRuleLogger(
+                    id, ruleLogger.getFileName, logAcceptEvents, logDropEvents)(
+                    simRuleLogger.asInstanceOf[FileRuleLogger])
+        }
+
+        log.debug(s"Emitting $simRuleLogger")
+        simRuleLogger
+    }
+
+    private def isReady(gm: GeneratedMessage): Boolean = {
+        assertThread()
+        logResTracker.areRefsReady
+    }
+
+    private def ruleLoggerDeleted(): Unit = {
+        assertThread()
+        log.debug(s"RuleLogger $id deleted")
+    }
+
+    private def ruleLoggerUpdated(rl: RuleLogger): Unit = {
+        assertThread()
+        log.debug(s"RuleLogger $id updated")
+        logResTracker.requestRefs(rl.getLoggingResourceId.asJava)
+        ruleLogger = rl
+    }
+}
---
+++ midolman/src/test/scala/org/midonet/midolman/topology/RuleLoggerMapperTest.scala
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2015 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.midolman.topology
+
+import java.util.UUID
+
+import scala.collection.mutable
+import scala.concurrent.duration.{Duration, DurationInt}
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+import rx.{Observable, Subscription}
+
+import org.midonet.cluster.topology.{TopologyBuilder, TopologyMatchers}
+import org.midonet.cluster.util.UUIDUtil.asRichProtoUuid
+import org.midonet.midolman.simulation.{FileRuleLogger, Chain => SimChain, RuleLogger => SimRuleLogger}
+import org.midonet.midolman.util.MidolmanSpec
+import org.midonet.util.MidonetEventually
+import org.midonet.util.reactivex.TestAwaitableObserver
+
+@RunWith(classOf[JUnitRunner])
+class RuleLoggerMapperTest extends MidolmanSpec with TopologyBuilder
+                                   with TopologyMatchers with MidonetEventually {
+    private val timeout: Duration = 1 second
+    private var vt: VirtualTopology = _
+
+    override protected def beforeTest(): Unit = {
+        super.beforeTest()
+        vt = injector.getInstance(classOf[VirtualTopology])
+    }
+
+    feature("RuleLoggerMapper") {
+        scenario("Publishes existing RuleLogger on startup.") {
+            val ch = createChain(name = Some("chain"))
+            vt.store.create(ch)
+
+            val lr = createLoggingResource()
+            vt.store.create(lr)
+
+            val rl = createRuleLogger(lr.getId.asJava, ch.getId.asJava,
+                                      fileName = Some("log.log"))
+            vt.store.create(rl)
+
+            val obs = createChainMapperAndObserver(ch.getId.asJava)
+            obs.awaitOnNext(1, timeout)
+            val simChain = obs.getOnNextEvents.get(0)
+            simChain.ruleLoggers.size shouldBe 1
+            simChain.ruleLoggers.head match {
+                case frl: FileRuleLogger =>
+                    frl.id shouldBe rl.getId.asJava
+                    frl.logAcceptEvents shouldBe true
+                    frl.logDropEvents shouldBe true
+                    frl.fileName shouldBe "log.log"
+                    frl.logDir shouldBe FileRuleLogger.DefaultLogDir
+            }
+        }
+
+        scenario("Publishes updates to LoggingResource's 'enabled' property") {
+            val ch = createChain()
+            vt.store.create(ch)
+
+            val lr = createLoggingResource()
+            vt.store.create(lr)
+
+            val rl = createRuleLogger(lr.getId.asJava, ch.getId.asJava,
+                                      fileName = Some("log.log"))
+            vt.store.create(rl)
+        }
+    }
+
+    private def createChainMapperAndObserver(chainId: UUID)
+    : TestAwaitableObserver[SimChain] = {
+        val mapper = new ChainMapper(chainId, vt, mutable.Map())
+        val obs = new TestAwaitableObserver[SimChain]
+        Observable.create(mapper).subscribe(obs)
+        obs
+    }
+}
