53faa099610c161981ca8403e4841c98551577f0
Xavi León <xavi.leon@midokura.com>
2016-06-14 06:48:13 +0200

    FlowState: introduce snappy block reader and writer
    This patch introduces compressed blocks with the snappy algorithm.
    The streaming classes provided by the snappy library are not fully
    convenient to us for two reaons:
    - it writes a magic number at the beginning of the stream and it's
      verified at reading time. We will expire old data from the stream so
      the head will be eventually removed which will make the stream
      unreadable.
    - it writes the size of the compressed block and the actual data in two
      different chunks of data. With this approach, our underlying block
      storage would potentially store metadata and data of a compressed
      chunks in different blocks. Again, as old blocks can expire, the
      metadatada for a given block might be removed without the data.
    
    Both issues prevents us to start reading compressed data at the
    beginning of each block.
    
    This patch introduces the snappy input/output streams for storing
    compressed flow state data. It ensures that each compressed chunk is
    written atomically with its metadata (size of the compressed block) and
    does not write any custom header. This allows us to expire any old
    block and start reading from the stream at any given block.
    
    Ref.: MI-985
    Change-Id: If257562e7c8197029d98725043cab8f64d9e5062
    Signed-off-by: Xavi León <xavi.leon@midokura.com>

---
+++ midolman/src/main/scala/org/midonet/services/flowstate/stream/snappy/SnappyBlockWriter.scala
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2016 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.services.flowstate.stream.snappy
+
+import java.nio.ByteBuffer
+
+import org.xerial.snappy.Snappy
+
+import org.midonet.util.io.stream.{ByteBufferBlockWriter, TimedBlockHeader}
+
+/**
+  * Compressed output stream. Loosely based on
+  * [[org.xerial.snappy.SnappyOutputStream]] but with the following caveats:
+  * - We don't write any header at the beginning for verification.
+  * - The size and compressed block are written in a single chunk of data into
+  *   the underlying output stream.
+  * We also follow a similar contract to an OutputStream altough we don't
+  * extend that class because we don't support writting just one byte but
+  * a chunk (array of bytes).
+  *
+  * @param out underlying output stream.
+  * @param blockSize Size of the uncompressed block.
+  */
+class SnappyBlockWriter(val out: ByteBufferBlockWriter[TimedBlockHeader],
+                        blockSize: Int) {
+
+    private var pointer: Int = 0
+
+    private val compressedSize = new Array[Byte](4)
+    private val uncompressed = new Array[Byte](blockSize)
+    private val compressed = new Array[Byte](Snappy.maxCompressedLength(blockSize))
+
+    /**
+      * Compress the raw byte array data. If the whole chunk does not fit
+      * into the uncompressed buffer, then the current raw data is compressed
+      * and a new compressed chunk is created.
+      *
+      * @param array array data of any type (e.g., byte[], float[], long[], ...)
+      * @param offset
+      * @param length
+      * @see java.io.OutputStream@write(byte[] array, int offset, int length)
+      */
+    def write(array: Array[Byte], offset: Int, length: Int): Unit = {
+        if (length > blockSize - pointer) {
+            // Not enough space for the chunk in uncompressed, compress first
+            dump()
+        }
+        Snappy.arrayCopy(array, offset, length, uncompressed, pointer)
+        pointer += length
+    }
+
+    /**
+      * @see java.io.OutputStream#flush()
+      */
+    def flush(): Unit = {
+        dump()
+    }
+
+    /**
+      * @see java.io.OutputStream#close()
+      */
+    def close(): Unit = {
+        flush()
+        out.close()
+    }
+
+    protected def dump(): Unit = {
+        if (pointer > 0) {
+            // Compress and dump the buffer content
+            val compressedSize = Snappy.compress(
+                uncompressed, 0, pointer, compressed, 4)
+            // Write compressed size to compressed buffer
+            ByteBuffer.wrap(compressed, 0, 4).putInt(compressedSize)
+            out.write(compressed, 0, compressedSize + 4)
+            pointer = 0
+        }
+    }
+}
