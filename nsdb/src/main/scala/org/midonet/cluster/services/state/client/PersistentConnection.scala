11a8157461a70c8c1b15cf3021c38939deeb3cd0
Adrian Serrano <adrisr83@gmail.com>
2016-06-14 14:34:48 +0200

    10kp: protobuf socket client
    This introduces the helpers for the nsdb connection
    to the State Proxy Service
    
    Ref: MI-1000
    
    Change-Id: I5fb1449954963a707522c829dc0816b49339a218

---
+++ nsdb/src/main/scala/org/midonet/cluster/services/state/client/PersistentConnection.scala
@@ -0,0 +1,245 @@
+/*
+ * Copyright 2016 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.cluster.services.state.client
+
+import java.nio.channels.ClosedChannelException
+import java.util.concurrent.atomic.AtomicReference
+import java.util.concurrent.ScheduledExecutorService
+
+import scala.util.{Failure, Success}
+import scala.PartialFunction.cond
+import scala.concurrent.duration._
+import scala.concurrent.{CancellationException, ExecutionContext}
+
+import com.google.protobuf.Message
+import com.typesafe.scalalogging.Logger
+
+import io.netty.channel.nio.NioEventLoopGroup
+
+import org.slf4j.LoggerFactory
+
+import rx.Observer
+
+/**
+  * The PersistentConnection abstract class will maintain a persistent
+  * connection to a remote server by means of the [[Connection]] class.
+  * When the connection is dropped (due to a close by the remote server,
+  * or an error) it will try to reconnect indefinitely.
+  *
+  * Type parameters S and R stand for Send and Receive types, respectively. See
+  * [[Connection]].
+  *
+  * @param host the remote server host
+  * @param port the remote port to connect to
+  * @param executor ScheduledExecutorService used to schedule reconnects
+  * @param retryTimeout the time to wait between reconnect attempts.
+  *                     The default is 3 seconds.
+  * @param context the executionContext
+  * @param eventLoopGroup is the [[NioEventLoopGroup]] to be used for Netty's
+  *                       event loop.
+  * Usage
+  *
+  * Derive your class from PersistentConnection.
+  * Implement the missing Observer[Message] method: onNext(msg)
+  * More advanced behavior can be attained by overriding [[onConnect()]] and
+  * [[onDisconnect()]]
+  *
+  * Call [[start()]] when you want the Connection to be initiated.
+  * Call [[stop()]] to close the current connection and disable the reconnection
+  * mechanism.
+  */
+abstract class PersistentConnection[S <: Message, R <: Message]
+                                   (name: String,
+                                    host: String,
+                                    port: Int,
+                                    executor: ScheduledExecutorService,
+                                    retryTimeout: FiniteDuration
+                                        = PersistentConnection.DefaultRetryDelay)
+                                   (implicit context: ExecutionContext,
+                                    eventLoopGroup: NioEventLoopGroup)
+        extends Observer[R] {
+
+    import PersistentConnection._
+
+    private type ConnectionType = Connection[S, R]
+
+    private case class Connected(connection: ConnectionType) extends State
+
+    private val log = Logger(LoggerFactory.getLogger("PersistentConnection"))
+    private val state = new AtomicReference(Init : State)
+
+    def isConnected: Boolean = cond(state.get) { case Connected(_) => true }
+    def isConnecting: Boolean = cond(state.get) {
+        case AwaitingReconnect | Connecting => true
+    }
+
+    /** Implement this method to provide the prototype message for decoding.
+      * Usually [[R]].getDefaultInstance
+      *
+      * @return The prototype message
+      */
+    protected def getMessagePrototype: R
+
+    /**
+      * Implement this method to add custom behavior when the connection
+      * is established (for the first time or after a connection retry)
+      */
+    protected def onConnect(): Unit
+
+    /**
+      * Implement this method to add custom behavior when the connection
+      * is closed (due to stop(), remote close(), or an error)
+      */
+    protected def onDisconnect(cause: Throwable): Unit
+
+    /**
+      * Initiates the connection to the remote server. It will retry
+      * indefinitely until successful
+      *
+      * @throws StoppedException
+      * @throws AlreadyStartedException
+      */
+    @throws[StoppedException]
+    @throws[AlreadyStartedException]
+    def start(): Unit = {
+        state.get match {
+            case Init =>
+                if (state.compareAndSet(Init, AwaitingReconnect)) {
+                    connect()
+                } else {
+                    // match over new state and return appropriate error
+                    start()
+                }
+            case Dead =>
+                throw new StoppedException(toString)
+
+            case _ =>
+                throw new AlreadyStartedException(toString)
+        }
+    }
+
+    def stop(): Boolean = {
+        log.info(s"$this disconnecting")
+
+        state.getAndSet(Dead) match {
+            case Dead => false
+            case Connected(conn) =>
+                conn.close()
+                onDisconnect(new StoppedException(toString))
+                true
+            case _ => true
+        }
+    }
+
+    def write(msg: S, flush: Boolean = true): Boolean = state.get match {
+        case Connected(connection) => connection.write(msg, flush)
+        case _ => false
+    }
+
+    def flush(): Boolean = state.get match {
+        case Connected(connection) => connection.flush()
+        case _ => false
+    }
+
+    final protected def onCompleted(): Unit =
+        handleDisconnect(new ClosedChannelException())
+
+    final protected def onError(cause: Throwable): Unit =
+        handleDisconnect(cause)
+
+    private def handleDisconnect(cause: Throwable) = {
+        log.warn(s"$this closed: $cause")
+
+        val current = state.get
+        current match {
+            case Connected(_) =>
+                onDisconnect(cause)
+                delayedStart(current)
+
+            case Dead =>
+
+            case _ =>
+                throw new UnexpectedStateException(current, toString)
+        }
+    }
+
+    @throws[StoppedException]
+    @throws[UnexpectedStateException]
+    private def connect(): Unit = {
+        log.info(s"$this connecting")
+
+        if (state.compareAndSet(AwaitingReconnect, Connecting)) {
+            val connection = new ConnectionType(host,
+                                                port,
+                                                this,
+                                                getMessagePrototype)
+            connection.connect() onComplete {
+                case Success(_)   =>
+                    if (state.compareAndSet(Connecting, Connected(connection))) {
+                        onConnect()
+                    } else {
+                        connection.close()
+                    }
+                case Failure(err) => delayedStart(Connecting)
+            }
+        } else {
+            state.get match {
+                case Dead => throw new StoppedException(toString)
+                case s: State => throw new UnexpectedStateException(s, toString)
+            }
+        }
+    }
+
+    private def delayedStart(currentState: State): Unit = {
+        if (state.compareAndSet(currentState, AwaitingReconnect)) {
+            executor.schedule(new Runnable {
+                def run() = {
+                    try {
+                        connect()
+                    } catch {
+                        case err: Throwable =>
+                            log.info(s"$this reconnect cancelled: $err")
+                    }
+                }
+            }, retryTimeout.toMillis, MILLISECONDS)
+        } else {
+            log.info(s"$this reconnect cancelled.")
+        }
+    }
+
+    override def toString: String = s"[$name to $host:$port]"
+}
+
+object PersistentConnection {
+
+    private sealed trait State
+    private case object Init extends State
+    private case object AwaitingReconnect extends State
+    private case object Connecting extends State
+    private case object Dead extends State
+
+    private val DefaultRetryDelay = 3 seconds
+
+    class UnexpectedStateException(s: State, desc: String)
+        extends IllegalStateException(s"$desc got caught in an unexpected state: $s")
+
+    class AlreadyStartedException(desc: String)
+        extends IllegalStateException(s"$desc has already been started")
+
+    class StoppedException(desc: String)
+        extends CancellationException(s"$desc has been stopped")
+}
---
+++ nsdb/src/test/scala/org/midonet/cluster/services/state/client/PersistentConnectionTest.scala
@@ -0,0 +1,363 @@
+/*
+ * Copyright 2016 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.cluster.services.state.client
+
+import java.util.concurrent.ScheduledThreadPoolExecutor
+import java.util.concurrent.atomic.AtomicInteger
+
+import scala.util.{Success, Failure}
+import scala.concurrent.duration._
+import scala.concurrent.{ExecutionContext, Future}
+
+import com.google.protobuf.Message
+
+import io.netty.channel.nio.NioEventLoopGroup
+
+import org.junit.runner.RunWith
+import org.mockito.Mockito
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{FeatureSpec, GivenWhenThen, Matchers}
+
+import rx.Observer
+
+import org.midonet.cluster.rpc.State.{ProxyRequest, ProxyResponse}
+import org.midonet.util.MidonetEventually
+
+@RunWith(classOf[JUnitRunner])
+class PersistentConnectionTest  extends FeatureSpec
+                                        with Matchers
+                                        with MidonetEventually
+                                        with GivenWhenThen {
+
+    val executor = new ScheduledThreadPoolExecutor(4)
+    implicit val exctx = ExecutionContext.fromExecutor(executor)
+
+    val numThreads = 4
+    val reconnectTimeout = 100 milliseconds
+    implicit val loopGroup = new NioEventLoopGroup(numThreads)
+    val message = msgPing(1)
+
+    private def msgPing(id: Long) = ProxyRequest.newBuilder()
+        .setRequestId(id)
+        .setPing(ProxyRequest.Ping.getDefaultInstance)
+        .build()
+
+    class TestConnection(port: Int)
+        extends PersistentConnection[ProxyRequest, ProxyResponse](
+            "Test Connection",
+            "localhost",
+            port,
+            executor,
+            reconnectTimeout) {
+
+        var numMessages = 0
+        var numConnects = 0
+        var numDisconnects = 0
+
+        override protected def getMessagePrototype = ProxyResponse
+            .getDefaultInstance
+
+        override protected def onNext(msg: ProxyResponse): Unit = {
+            numMessages += 1
+        }
+
+        override protected def onConnect(): Unit = {
+            numConnects += 1
+        }
+
+        override protected def onDisconnect(cause: Throwable): Unit = {
+            numDisconnects += 1
+        }
+    }
+
+    feature("no activity until started") {
+
+        Given("A stopped connection")
+        val c = new TestConnection(0)
+
+        scenario("I/O without start") {
+
+            When("output is attempted")
+            val writeFlushResult = c.write(message, flush = true)
+            val writeNoFlushResult = c.write(message, flush = false)
+            val flushResult = c.flush()
+
+            Then("It had no effect")
+            writeFlushResult shouldBe false
+            writeNoFlushResult shouldBe false
+            flushResult shouldBe false
+            c.numConnects shouldBe 0
+            c.numMessages shouldBe 0
+            c.numDisconnects shouldBe 0
+        }
+    }
+
+    feature("not usable after stop") {
+
+        Given("A stopped connection")
+        val c = new TestConnection(0)
+
+        scenario("Start after stop") {
+            When("it is stopped")
+            c.stop()
+            Then("When start is called, it throws StoppedException")
+            an[PersistentConnection.StoppedException] shouldBe thrownBy {
+                c.start()
+            }
+            And("It can't be used")
+            c.write(message, flush = true) shouldBe false
+            c.write(message, flush = false) shouldBe false
+            c.flush() shouldBe false
+            c.numConnects shouldBe 0
+            c.numMessages shouldBe 0
+            c.numDisconnects shouldBe 0
+        }
+    }
+
+    feature("Can be connected to a server") {
+
+        Given("A connection and a server")
+        val server = new TestServer(ProxyRequest.getDefaultInstance)
+        val c = new TestConnection(server.port)
+
+        scenario("connect using start") {
+
+            When("The connection is started")
+            c.start()
+
+            Then("It connects to the server")
+            eventually {
+                c.isConnected shouldBe true
+                server.hasClient shouldBe true
+            }
+        }
+
+        scenario("server close") {
+
+            When("The server closes its end")
+            server.close()
+
+            Then("The close is received at the local end")
+            eventually {
+                c.isConnected shouldBe false
+            }
+
+            And("No action until the timeout is completed")
+            Thread.sleep(reconnectTimeout.toMillis / 2)
+            c.isConnected shouldBe false
+
+            And("It reconnects when the timeout expires")
+            Thread.sleep(reconnectTimeout.toMillis)
+            c.isConnected shouldBe true
+            c.numConnects shouldBe 2
+            c.numDisconnects shouldBe 1
+            c.numMessages shouldBe 0
+
+            c.stop()
+            eventually {
+                c.isConnected shouldBe false
+            }
+            eventually {
+                server.hasClient shouldBe false
+            }
+        }
+    }
+
+    feature("reconnects on error") {
+        val server = new TestServer(ProxyRequest.getDefaultInstance)
+        val c = new TestConnection(server.port) {
+            override def onNext(msg: ProxyResponse): Unit = {
+                throw new Exception
+            }
+        }
+        c.start()
+
+        scenario("disconnect on error") { for (i <- 1 to 1) {
+
+            eventually {
+                server.hasClient shouldBe true
+                c.isConnected shouldBe true
+            }
+            server.write(message)
+            eventually {
+                c.isConnected shouldBe false
+            }
+            c.numDisconnects shouldBe i
+            eventually { server.hasClient shouldBe false }
+        } }
+
+    }
+    feature("reconnection is aborted by close") {
+
+        val server = new TestServer(ProxyRequest.getDefaultInstance)
+
+        scenario("close while reconnecting") {
+
+            Given("A connection to a server")
+            val c = new TestConnection(server.port)
+
+            When("It is connected")
+            c.start()
+            eventually {
+                c.isConnected shouldBe true
+            }
+            eventually {
+                server.hasClient shouldBe true
+            }
+
+            And("Forced to reconnect")
+            server.close()
+            Thread.sleep(reconnectTimeout.toMillis / 2)
+            c.isConnected shouldBe false
+
+            And("it is stopped in the middle of a reconnect")
+            c.stop()
+
+            Then("It will not reconnect")
+            Thread.sleep(2 * reconnectTimeout.toMillis)
+            c.numDisconnects shouldBe 1
+            c.numConnects shouldBe 1
+            eventually {
+                server.hasClient shouldBe false
+            }
+        }
+
+        scenario("failure to connect") {
+            for (i <- 1 to 1) {
+                val c = new TestConnection(0)
+                c.start()
+                eventually {
+                    c.isConnecting shouldBe true
+                }
+                c.stop()
+                eventually {
+                    c.isConnecting shouldBe false
+                }
+                Thread.sleep(reconnectTimeout.toMillis)
+                c.isConnecting shouldBe false
+                c.isConnected shouldBe false
+            }
+        }
+    }
+
+    feature("Behavior is deterministic when run in parallel") {
+
+        val server = new TestServer(ProxyRequest.getDefaultInstance)
+
+        scenario("multiple starts in parallel") {
+
+            Given("A disconnected client")
+            val c = new TestConnection(server.port)
+
+            When("Start is called in parallel")
+            val tries = 50
+            val succeeded = new AtomicInteger(0)
+            val failed = new AtomicInteger(0)
+            for (i <- 1 to tries) {
+                Future {
+                    c.start()
+                } onComplete {
+                    case Success(_) => succeeded.incrementAndGet()
+                    case Failure(err) =>
+                        failed.incrementAndGet()
+                        err shouldBe
+                        an[PersistentConnection.AlreadyStartedException]
+                }
+            }
+
+            Then("Only one call succeeds")
+            eventually {
+                (succeeded.get + failed.get) shouldBe tries
+            }
+            assert(succeeded.get == 1)
+        }
+    }
+
+    feature("I/O works") {
+
+        val server = new TestServer(ProxyRequest.getDefaultInstance)
+        val c = new TestConnection(server.port)
+
+        val serverObserverMock = Mockito
+            .mock(classOf[Observer[Message]])
+        server.attachedObserver = serverObserverMock
+
+        c.start()
+        eventually {
+            c.isConnected shouldBe true
+            server.hasClient shouldBe true
+        }
+
+        scenario("send message") {
+            for (count <- 1 to 1) {
+                assert(c.write(message))
+                Mockito.verify(serverObserverMock,
+                               Mockito.timeout(1000)).onNext(message)
+            }
+        }
+
+        scenario("receive message") {
+            for (count <- 1 to 1) {
+                server.write(message)
+                eventually {
+                    c.numMessages shouldBe count
+                }
+            }
+        }
+    }
+
+    feature("observer events are issued") {
+
+        val server = new TestServer(ProxyRequest.getDefaultInstance)
+
+        scenario("onConnect/onDisconnect") {
+            for (count <- 1 to 1) {
+
+                Given("A persistent connection")
+                val c = new TestConnection(server.port)
+
+                When("It is established")
+                c.start()
+
+                Then("connect and disconnect callbacks are fired")
+                val iters = 5
+                for (i <- 1 until iters) {
+                    eventually {
+                        c.numConnects shouldBe i
+                        server.hasClient shouldBe true
+                    }
+                    server.close()
+                    eventually {
+                        c.numDisconnects shouldBe i
+                        server.hasClient shouldBe false
+                    }
+                }
+                eventually {
+                    c.numConnects shouldBe iters
+                    server.hasClient shouldBe true
+                }
+
+                And("When stopped manually, disconnect not fired")
+                c.stop()
+                eventually {
+                    c.numDisconnects shouldBe iters
+                    server.hasClient shouldBe false
+                }
+            }
+        }
+    }
+}
