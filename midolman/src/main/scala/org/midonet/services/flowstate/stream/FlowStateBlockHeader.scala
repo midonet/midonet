cd5d882f35aa423b89aa68235b58d90be3c14e66
Xavi León <xavi.leon@midokura.com>
2016-06-14 06:56:46 +0200

    FlowState: add flow state message streams
    This patch introduces the flow state input and output streams. They use
    the underlying snappy streams and byte buffer streams to write flow
    state data compressed and stored in blocks. These streams make sure to
    write the size of the message and the message itself in a single chunk
    to avoid being split into separate blocks. For the flow state
    encoding/decondig, we use of the SbeEncoder, a wrapper class that
    contains methods to encode/decode information into flow state SBE
    messages and provides access the underlying byte array holding the data.
    
    This patch also provides factories to create memory mapped byte buffers
    to provide persitent storage for flow state entries for a given port id.
    
    Ref.: MI-985
    Change-Id: I136a677c17f22d533fbc1065f967d8d7d5ea3f9e
    Signed-off-by: Xavi León <xavi.leon@midokura.com>

---
+++ midolman/src/main/scala/org/midonet/services/flowstate/stream/FlowStateBlockHeader.scala
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2016 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.services.flowstate.stream
+
+import java.nio.ByteBuffer
+import java.util
+
+import org.midonet.util.concurrent.NanoClock
+import org.midonet.util.io.stream.{BlockHeaderBuilder, TimedBlockHeader}
+
+case class FlowStateBlockHeader(val magic: Array[Byte],
+                                override val blockLength: Int,
+                                override val lastEntryTime: Long)
+    extends TimedBlockHeader {
+
+
+    override def toString: String = {
+        s"${classOf[FlowStateBlockHeader].toString}[" +
+        s"magic: ${new String(magic)}, " +
+        s"last: $lastEntryTime, " +
+        s"length: $blockLength]"
+    }
+
+    def isValid: Boolean = {
+        util.Arrays.equals(magic, FlowStateBlock.MagicNumber) &&
+        lastEntryTime != -1
+    }
+}
+
+object FlowStateBlock extends BlockHeaderBuilder[TimedBlockHeader] {
+
+    /**
+      * Frame format for flow state storage blocks:
+      * A message timestamp of -1 or without the
+      * magic number means an invalid block.
+      *
+      * +---------------------------------------+
+      * | Magic number (10 bytes if SHA-1)      |
+      * +---------------------------------------+
+      * | Length of compressed data (4 bytes)   |
+      * +---------------------------------------+
+      * | Last message timestamp (8 bytes)      |
+      * +---------------------------------------+
+      * | Data                                  |
+      * |                                       |
+      * |                                       |
+      * +---------------------------------------+
+      */
+
+    val MagicNumber: Array[Byte] =
+        Array[Byte]('X', 'F', 'l', 'o', 'w', 'S', 't', 'a', 't', 'e')
+    val EmptyMagicNumber = new Array[Byte](MagicNumber.array.length)
+    //val MagicNumber =
+    //    ByteBuffer.wrap(Array[Byte]('X', 'F', 'l', 'o', 'w', 'S', 't', 'a', 't', 'e'))
+    val MagicNumberSize = MagicNumber.array.length
+    val LengthOffset = MagicNumberSize
+    val LastTimeOffset = MagicNumberSize + 4
+
+    val headerSize = MagicNumberSize + 12
+
+    override def init(buffer: ByteBuffer, reset: Boolean): Unit = {
+        val header = FlowStateBlock(buffer)
+        if (reset || !header.isValid) {
+            buffer.put(MagicNumber.array, 0, MagicNumberSize)
+            buffer.putInt(LengthOffset, 0)
+            buffer.putLong(LastTimeOffset, -1)
+            buffer.position(headerSize)
+        } else {
+            buffer.position(header.blockLength + headerSize)
+        }
+    }
+
+    override def update(buffer: ByteBuffer, params: AnyVal*): Unit = {
+        buffer.putInt(LengthOffset, buffer.position - headerSize)
+        buffer.putLong(LastTimeOffset, NanoClock.DEFAULT.tick)
+    }
+
+    def apply(buffer: ByteBuffer): FlowStateBlockHeader = {
+        val magic = new Array[Byte](MagicNumberSize)
+        for (i <- 0 until MagicNumberSize) {
+            magic(i) = buffer.get(i)
+        }
+        FlowStateBlockHeader(magic,
+                             buffer.getInt(LengthOffset),
+                             buffer.getLong(LastTimeOffset))
+    }
+}
