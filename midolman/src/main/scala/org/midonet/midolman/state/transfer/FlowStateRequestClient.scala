36b732586247ba2e6dcedcb1ec7a7baa1abff2d9
Mateo Hermida <mateo@midokura.com>
2016-06-20 18:06:55 +0200

    FlowState: Add TCP client for intercommunication
    Adds a new TCP client that will be used to request Flow State from the
    Flow State Minions exposing it. The same client can be used for the
    three new type of requests (raw, internal and remote).
    
    Also added a mechanism to allow for pipelined encoding of the flow state
    that is being read for performance.
    
    Ref: MI-987
    
    Change-Id: I8fd18ecc420ea52d34a5f7b0fa0494a33db5d02f
    Signed-off-by: Mateo Hermida <mateo@midokura.com>

---
+++ midolman/src/main/scala/org/midonet/midolman/state/transfer/FlowStateRequestClient.scala
@@ -0,0 +1,181 @@
+/*
+ * Copyright 2016 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.midolman.state.transfer
+
+import java.io.{DataInputStream, IOException}
+import java.net.Socket
+import java.util.UUID
+
+import com.typesafe.scalalogging.Logger
+import org.midonet.cluster.flowstate.FlowStateTransfer.{StateRequest, StateResponse}
+import org.midonet.packets.SbeEncoder
+import org.midonet.services.flowstate.transfer.StateTransferProtocolBuilder._
+import org.midonet.services.flowstate.transfer.StateTransferProtocolParser._
+import org.midonet.services.flowstate.transfer.internal._
+import org.midonet.util.io.stream.ByteBufferBlockWriter
+import org.slf4j.LoggerFactory.getLogger
+
+import scala.util.control.NonFatal
+
+class FlowStateRequestClient(socketTimeout: Int) {
+
+    private val Log = Logger(getLogger(getClass))
+    private val Retries = 3
+
+    private var socket: Socket = _
+    private var dis: DataInputStream = _
+    private var host: String = _
+
+    def remoteFlowStateFrom(host: String, tcpPort: Int, portId: UUID,
+                            translator: PipelinedSbeEncoderTranslator): Unit = {
+        retryClosing {
+            initSocket(tcpPort)
+
+            val request = buildStateRequestRemote(portId, host)
+            val response = sendRequest(request)
+
+            response match {
+                case StateAckRemote(port) =>
+                    Log debug s"Remote Ack received from previous owner of $port"
+                    pipelinedReadTranslatedState(translator)
+                case StateError(code, description) =>
+                    Log warn s"Ignoring response: $code error received from" +
+                        s" previous owner: $description"
+                case _ =>
+                    Log warn "Ignoring response: received a malformed/illegal" +
+                        s" response from $host"
+            }
+        }
+    }
+
+    def internalFlowStateFrom(tcpPort: Int, portId: UUID,
+                              translator: PipelinedSbeEncoderTranslator): Unit = {
+        retryClosing {
+            initSocket(tcpPort)
+
+            val request = buildStateRequestInternal(portId)
+            val response = sendRequest(request)
+
+            response match {
+                case StateAckInternal(port) =>
+                    Log debug s"Internal Ack received from previous owner of $port"
+                    pipelinedReadTranslatedState(translator)
+                case StateError(code, description) =>
+                    Log warn s"Ignoring response: $code error received from" +
+                        s" previous owner: $description"
+                case _ =>
+                    Log warn "Ignoring response: received a malformed/illegal" +
+                        s" response from $host"
+            }
+        }
+    }
+
+    def rawPipelinedFlowStateFrom(host: String, tcpPort: Int, portId: UUID,
+                                  writer: ByteBufferBlockWriter[_]) = {
+        retryClosing {
+            initSocket(tcpPort, host)
+
+            val request = buildStateRequestRaw(portId)
+            val response = sendRequest(request)
+
+            response match {
+                case StateAckRaw(port) =>
+                    Log debug s"Raw Ack received from previous owner of $port"
+                    pipelinedReadWriteRawState(writer)
+                case StateError(code, description) =>
+                    Log warn s"Ignoring response: $code error received from" +
+                        s" previous owner: $description"
+                case _ =>
+                    Log warn "Ignoring response: received a malformed/illegal" +
+                        s" response from $host"
+            }
+        }
+    }
+
+    private def pipelinedReadTranslatedState(translator: PipelinedSbeEncoderTranslator): Unit = {
+        var next = dis.readInt()
+        while (next > 0) {
+            val rawEncoder = readBytes(next)
+            val sbe = decodeFromBytes(rawEncoder)
+            translator.pushToPipeline(sbe)
+            next = dis.readInt()
+        }
+    }
+
+    private def pipelinedReadWriteRawState(writer: ByteBufferBlockWriter[_]): Unit = {
+        var next = dis.readInt()
+        while (next > 0) {
+            val buffer = readBytes(next)
+            writer.write(buffer)
+            next = dis.readInt()
+        }
+    }
+
+    private def decodeFromBytes(bytes: Array[Byte]) = {
+        val encoder = new SbeEncoder
+        encoder.decodeFrom(bytes)
+        encoder
+    }
+
+    private def sendRequest(stateRequest: StateRequest) = {
+        try {
+            socket.getOutputStream().write(stateRequest.toByteArray)
+
+            val protobufSize = dis.readInt()
+
+            val rawResponse = readBytes(protobufSize)
+            val response = StateResponse.parseFrom(rawResponse)
+
+            parseStateResponse(response)
+        } catch {
+            case e: IOException =>
+                Log warn (s"Unable to get requested flow from $host", e)
+                throw e
+        }
+    }
+
+    private def initSocket(tcpPort: Int, clientHost: String = "0.0.0.0"): Unit = {
+        host = clientHost
+        socket = new Socket(host, tcpPort)
+        socket.setSoTimeout(socketTimeout)
+        dis = new DataInputStream(socket.getInputStream)
+    }
+
+    private def readBytes(size: Int): Array[Byte] = {
+        val buffer = new Array[Byte](size)
+        dis.readFully(buffer)
+        buffer
+    }
+
+    private def retryClosing(retriable: => Unit): Unit = {
+        try {
+            retry(Retries) { retriable }
+        } finally {
+            if (socket ne null) socket.close()
+        }
+    }
+
+    private def retry(retries: Int) (retriable: => Unit): Unit = {
+        try {
+            retriable
+        } catch {
+            case NonFatal(e) if retries > 0 => retry (retries - 1) (retriable)
+            case NonFatal(_) => // Won't have the requested flow state, but we
+                                // shouldn't stop
+        }
+    }
+}
---
+++ midolman/src/main/scala/org/midonet/midolman/state/transfer/PipelinedSbeEncoderTranslator.scala
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2016 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.midolman.state.transfer
+
+import java.util.{HashMap => JHashMap, HashSet => JHashSet}
+
+import org.midonet.midolman.HostRequestProxy.FlowStateBatch
+import org.midonet.midolman.state.ConnTrackState.ConnTrackKey
+import org.midonet.midolman.state.FlowStateAgentPackets._
+import org.midonet.midolman.state.NatState._
+import org.midonet.packets.NatState.NatBinding
+import org.midonet.packets.SbeEncoder
+
+/**
+  * Translates flow state SbeEncoder responses to internally used FlowStateBatch
+  * objects. For performance the object is constructed in a pipeline, and since
+  * many keys could be repeated, we can also get better memory usage.
+  */
+class PipelinedSbeEncoderTranslator {
+
+    val strongConnTrack = new JHashSet[ConnTrackKey]()
+    val weakConnTrack = new JHashSet[ConnTrackKey]()
+    val strongNat = new JHashMap[NatKey, NatBinding]()
+    val weakNat = new JHashMap[NatKey, NatBinding]()
+
+    // FIXME[mateo]: we don't make a difference between strong and weak connTrack
+    // and nats when we use them, we should just keep them together in the batch
+    def pushToPipeline(sbe: SbeEncoder): Unit = {
+        val message = sbe.flowStateMessage
+
+        val connTrackIter = message.conntrack()
+        while (connTrackIter.hasNext) {
+            val next = connTrackIter.next()
+            val connTrack = connTrackKeyFromSbe(next, ConnTrackKey)
+
+            strongConnTrack.add(connTrack)
+        }
+
+        val natIter = message.nat()
+        while (natIter.hasNext) {
+            val next = natIter.next()
+            val natKey = natKeyFromSbe(next, NatKey)
+            val natBinding = natBindingFromSbe(next)
+
+            strongNat.put(natKey, natBinding)
+        }
+
+        FlowStateBatch(strongConnTrack, weakConnTrack, strongNat, weakNat)
+    }
+
+    def toFlowStateBatch() = {
+        FlowStateBatch(strongConnTrack, weakConnTrack, strongNat, weakNat)
+    }
+}
