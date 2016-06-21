e1cf0266489feefd029cc3efab99df305767ab0c
Alex Bikfalvi <alex.bikfalvi@midokura.com>
2016-06-21 12:43:43 +0200

    Logging: Add logger with marker wrapper
    This adds a Logger wrapper that includes an optional marker to
    each log message. This will be used to substitute individual
    loggers in the agent/cluster per device or ZK node with common
    loggers that include a custom marker.
    
    The rationale for this is to prevent caching of an increasing
    number of loggers in the logback LoggingContext while minimizing
    the number of code changes: we shall use the new Logger instead
    and each class can add its own mark prefix.
    
    The wrapper uses logging markers rather than simply prefixing a
    string, to prevent the generation of additional string garbage
    for each logging messages, as well as to allow the operator to
    filter the messages by marker, and modify the wrapper formatting
    as needed.
    
    Ref: MI-1075
    
    Change-Id: I55f4939f5c47f849b939c54ee82a8a2769961e00
    Signed-off-by: Alex Bikfalvi <alex.bikfalvi@midokura.com>

---
+++ midonet-util/src/main/scala/org/midonet/util/logging/Logger.scala
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2016 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.util.logging
+
+import scala.language.experimental.macros
+
+import com.typesafe.scalalogging.{Logger => Wrapper}
+
+import org.apache.commons.lang.StringUtils
+import org.slf4j.helpers.LogstashBasicMarker
+import org.slf4j.{Marker, Logger => Underlying}
+
+/**
+  * Companion for [[Logger]], providing a factory for [[Logger]]s.
+  */
+object Logger {
+
+    /**
+      * Create a [[Logger]] wrapping the given underlying
+      * [[com.typesafe.scalalogging.Logger]].
+      */
+    def apply(underlying: Underlying): Logger =
+        new Logger(underlying, null)
+
+    /**
+      * Create a [[Logger]] wrapping the given underlying
+      * [[com.typesafe.scalalogging.Logger]] with a specified marker.
+      */
+    def apply(underlying: Underlying, mark: String): Logger =
+        new Logger(underlying, mark)
+
+}
+
+final class Logger private (val underlying: Underlying, mark: String) {
+
+    val wrapper = Wrapper(underlying)
+    private[logging] val marker: Marker =
+        if (!StringUtils.isBlank(mark)) new LogstashBasicMarker(s"[$mark] ")
+        else null
+
+    // Error
+
+    @inline def error(message: String): Unit =
+        wrapper.error(marker, message)
+    @inline def error(message: String, cause: Throwable): Unit =
+        wrapper.error(marker, message, cause)
+    @inline def error(message: String, args: AnyRef*): Unit =
+        wrapper.error(marker, message, args)
+
+    // Warn
+
+    @inline def warn(message: String): Unit =
+        wrapper.warn(marker, message)
+    @inline def warn(message: String, cause: Throwable): Unit =
+        wrapper.warn(marker, message, cause)
+    @inline def warn(message: String, args: AnyRef*): Unit =
+        wrapper.warn(marker, message, args)
+
+    // Info
+
+    @inline def info(message: String): Unit =
+        wrapper.info(marker, message)
+    @inline def info(message: String, cause: Throwable): Unit =
+        wrapper.info(marker, message, cause)
+    @inline def info(message: String, args: AnyRef*): Unit =
+        wrapper.info(marker, message, args)
+
+    // Debug
+
+    @inline def debug(message: String): Unit =
+        wrapper.debug(marker, message)
+    @inline def debug(message: String, cause: Throwable): Unit =
+        wrapper.debug(marker, message, cause)
+    @inline def debug(message: String, args: AnyRef*): Unit =
+        wrapper.debug(marker, message, args)
+
+    // Trace
+
+    @inline def trace(message: String): Unit =
+        wrapper.trace(marker, message)
+    @inline def trace(message: String, cause: Throwable): Unit =
+        wrapper.trace(marker, message, cause)
+    @inline def trace(message: String, args: AnyRef*): Unit =
+        wrapper.trace(marker, message, args)
+
+}
