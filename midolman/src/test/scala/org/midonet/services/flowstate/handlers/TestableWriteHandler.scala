48eb11554265ad9b0115272b564c415cd072bc8b
Xavi León <xavi.leon@midokura.com>
2016-06-21 18:36:07 +0200

    FlowState: send incoming flow state to minion
    This patch enables the agent to send incoming flow state from other
    Midonet Agents to the flow state minion. This is only necessary when
    storing flow state messages locally. In this case, we need to know which
    ports are owned by the agent in order to store the message with the
    corresponding writer indexed by port ID.
    
    To support this use case, we extend the minion UDP interface to also
    receive a message with the set of ports currently bound locally, and
    intersect this set with the port ids present in the message.
    
    Another possibility would be to extend the flow state SBE to include
    the currently owned ports but it was discarded because it meant
    regenerating the whole message to include the owned ports field,
    increasing the number of memory allocations in the fast path.
    
    Change-Id: I5c60d48c1eceb69654e50541873aafc0186f2c4e
    Signed-off-by: Xavi León <xavi.leon@midokura.com>

--- midolman/src/test/scala/org/midonet/services/flowstate/handlers/TestableWriteHandler.scala
+++ midolman/src/test/scala/org/midonet/services/flowstate/handlers/TestableWriteHandler.scala
@@ -15,12 +15,15 @@
  */
 package org.midonet.services.flowstate.handlers
 
+import java.util
 import java.util.UUID
 
 import org.midonet.cluster.storage.FlowStateStorageWriter
 import org.midonet.midolman.config.FlowStateConfig
 import org.midonet.packets.SbeEncoder
 import org.mockito.Mockito._
+
+import org.midonet.services.flowstate.stream.FlowStateWriter
 
 class TestableWriteHandler (config: FlowStateConfig)
     extends FlowStateWriteHandler(config, null) {
@@ -37,8 +40,22 @@ class TestableWriteHandler (config: FlowStateConfig)
         legacyStorage
     }
 
-    override def writeInLocalStorage(portId: UUID, encoder: SbeEncoder): Unit = {
-        super.writeInLocalStorage(portId, encoder)
+    override def getFlowStateWriter(portId: UUID): FlowStateWriter =
+        portWriters.synchronized {
+            if (portWriters.containsKey(portId)) {
+                portWriters.get(portId)
+            } else {
+                val writer = mock(classOf[FlowStateWriter])
+                portWriters.put(portId, writer)
+                writer
+            }
+        }
+
+
+    override def writeInLocalStorage(ingressPortId: UUID,
+                                     egressPortIds: util.ArrayList[UUID],
+                                     encoder: SbeEncoder): Unit = {
+        super.writeInLocalStorage(ingressPortId, egressPortIds, encoder)
         writes += 1
     }
 
