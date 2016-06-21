94cb09adc3177c7163de4c8427e42d5c1ea90329
Adrian Serrano <adrisr83@gmail.com>
2016-06-21 13:26:44 +0200

    10kp: A StateTableClient trait for the client side
    This patch introduces a StateTableClient trait to
    be used by replicated maps wishing to subscribe
    to updates from the State Proxy service
    
    Change-Id: Ie641cc72f75df2190b4c4abbec86a79acdbf7b48
    Ref: MI-1000

---
+++ nsdb/src/main/scala/org/midonet/cluster/services/state/client/StateSubscriptionKey.scala
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2016 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.cluster.services.state.client
+
+import java.util.UUID
+
+import org.midonet.cluster.rpc.State.ProxyRequest
+import org.midonet.cluster.models.Commons
+
+class StateSubscriptionKey(val objectClass: Class[_],
+                           val objectId: UUID,
+                           val keyClass: Class[_],
+                           val valueClass: Class[_],
+                           val tableName: String,
+                           val tableArguments: List[String],
+                           val lastVersion: Long) {
+
+    private[client] def toSubscribeMessage()
+        : ProxyRequest.Subscribe = {
+
+        val msg = ProxyRequest.Subscribe.newBuilder()
+
+        tableArguments foreach (arg => msg.addTableArguments(arg))
+
+        msg.setObjectId(Commons.UUID.newBuilder()
+                            .setMsb(objectId.getMostSignificantBits)
+                            .setLsb(objectId.getLeastSignificantBits))
+            .setObjectClass(objectClass.toString())
+            .setKeyClass(keyClass.toString())
+            .setValueClass(valueClass.toString())
+            .setTableName(tableName)
+            .setLastVersion(lastVersion)
+            .build()
+    }
+}
+
+object StateSubscriptionKey {
+    private[client] implicit def toSubscribeMessage(key: StateSubscriptionKey)
+        : ProxyRequest.Subscribe = key.toSubscribeMessage()
+}
