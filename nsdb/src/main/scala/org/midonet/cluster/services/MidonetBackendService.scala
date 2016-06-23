35e4a3bde74de38e28fd568b513d951560f8f7db
Adrian Serrano <adrisr83@gmail.com>
2016-06-23 11:44:55 +0200

    10kp: State proxy client uses service discovery
    This patch modifies the State Proxy client to obtain the remote
    address using service discovery.
    
    Some required fixes to the server's service discovery are also
    included.
    
    Ref: MI-1117
    
    Change-Id: Ib5bd1d30965efde3332676fd02220ecbe6423f98

--- nsdb/src/main/scala/org/midonet/cluster/services/MidonetBackendService.scala
+++ nsdb/src/main/scala/org/midonet/cluster/services/MidonetBackendService.scala
@@ -16,7 +16,6 @@
 
 package org.midonet.cluster.services
 
-import java.net.URI
 import java.util.UUID
 import java.util.concurrent.ExecutorService
 
@@ -42,7 +41,8 @@ import org.midonet.cluster.data.{ZoomInit, ZoomInitializer}
 import org.midonet.cluster.models.Neutron._
 import org.midonet.cluster.models.Topology._
 import org.midonet.cluster.services.c3po.C3POState
-import org.midonet.cluster.services.discovery.{MidonetDiscovery, MidonetDiscoveryClient, MidonetDiscoveryImpl, MidonetServiceURI}
+import org.midonet.cluster.services.discovery.{MidonetDiscovery, MidonetDiscoveryClient, MidonetDiscoveryImpl, MidonetServiceHostAndPort}
+import org.midonet.cluster.services.state.StateProxyService
 import org.midonet.cluster.storage.{CuratorZkConnection, MidonetBackendConfig}
 import org.midonet.cluster.util.ConnectionObservable
 import org.midonet.conf.HostIdGenerator
@@ -374,8 +374,6 @@ class MidonetBackendService(config: MidonetBackendConfig,
 
     private var discoveryServiceExecutor: ExecutorService = null
     private var discoveryService: MidonetDiscovery = null
-    private var stateProxyDiscoveryClient:
-        MidonetDiscoveryClient[MidonetServiceURI] = null
 
     override val reactor = new TryCatchReactor("nsdb", 1)
     override val connection = new CuratorZkConnection(curator, reactor)
@@ -409,8 +407,6 @@ class MidonetBackendService(config: MidonetBackendConfig,
             discoveryService = new MidonetDiscoveryImpl(curator,
                                                         discoveryServiceExecutor,
                                                         config)
-            stateProxyDiscoveryClient =
-                discoveryService.getClient[MidonetServiceURI]("stateProxy")
 
             notifyStarted()
             log.info("Setting up storage bindings")
@@ -431,16 +427,7 @@ class MidonetBackendService(config: MidonetBackendConfig,
         curator.close()
         failFastCurator.close()
         notifyStopped()
-        stateProxyDiscoveryClient.stop()
         discoveryService.stop()
         discoveryServiceExecutor.shutdown()
-    }
-
-    private def getStateProxyServiceInstanceUri(): URI = {
-        if(isRunning) {
-            stateProxyDiscoveryClient.instances.get(0).uri
-        } else {
-            throw new IllegalStateException("The service is not running")
-        }
     }
 }
