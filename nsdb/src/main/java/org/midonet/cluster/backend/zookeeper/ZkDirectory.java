e7385baa794ff3db2ffcf5a735edc0987f0d0662
Alex Bikfalvi <alex.bikfalvi@midokura.com>
2016-07-05 18:45:32 +0200

    10kp: ZkDirectory does not require ZkConnection
    With this change the ZkDirectory no longer requires a ZkConnection
    instance but rather the ZooKeeper client. This allows us to
    remove this unnecessary wrapper class, and simply expose the
    ZooKeeper client or Curator where neeeded.
    
    Ref: MI-1003
    
    Change-Id: Id0dfe4ba179f3f0b45d21565dff591b43dd1eedc
    Signed-off-by: Alex Bikfalvi <alex.bikfalvi@midokura.com>

--- nsdb/src/main/java/org/midonet/cluster/backend/zookeeper/ZkDirectory.java
+++ nsdb/src/main/java/org/midonet/cluster/backend/zookeeper/ZkDirectory.java
@@ -29,6 +29,7 @@ import org.apache.zookeeper.OpResult;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooDefs.Ids;
+import org.apache.zookeeper.ZooKeeper;
 import org.apache.zookeeper.data.ACL;
 import org.apache.zookeeper.data.Stat;
 import org.slf4j.Logger;
@@ -41,15 +42,20 @@ import org.midonet.util.eventloop.Reactor;
 public class ZkDirectory implements Directory {
     private static final Logger log = LoggerFactory.getLogger(ZkDirectory.class);
 
-    public ZkConnection zk;
+    public ZooKeeper zk;
     private String basePath;
     private final List<ACL> acl = Ids.OPEN_ACL_UNSAFE;
     private Reactor reactor;
 
-    public ZkDirectory(ZkConnection zk, String basePath, Reactor reactor) {
+    public ZkDirectory(ZooKeeper zk, String basePath, Reactor reactor) {
         this.zk = zk;
         this.basePath = basePath;
         this.reactor = reactor;
+    }
+
+    public ZkDirectory(ZkConnection connection, String basePath,
+                       Reactor reactor) {
+        this(connection.getZooKeeper(), basePath, reactor);
     }
 
     @Override
@@ -66,7 +72,7 @@ public class ZkDirectory implements Directory {
     public String add(String relativePath, byte[] data, CreateMode mode)
             throws KeeperException, InterruptedException {
         String absPath = getAbsolutePath(relativePath);
-        String path = zk.getZooKeeper().create(absPath, data, acl, mode);
+        String path = zk.create(absPath, data, acl, mode);
         return path.substring(basePath.length());
     }
 
@@ -87,7 +93,7 @@ public class ZkDirectory implements Directory {
 
         final String absPath = getAbsolutePath(relativePath);
 
-        zk.getZooKeeper().create(
+        zk.create(
             absPath, data, acl, mode,
             new AsyncCallback.StringCallback() {
                 @Override
@@ -115,7 +121,7 @@ public class ZkDirectory implements Directory {
     public void update(String relativePath, byte[] data, int version)
             throws KeeperException, InterruptedException {
         String absPath = getAbsolutePath(relativePath);
-        zk.getZooKeeper().setData(absPath, data, version);
+        zk.setData(absPath, data, version);
     }
 
     private Watcher wrapCallback(Runnable runnable) {
@@ -197,14 +203,14 @@ public class ZkDirectory implements Directory {
     public byte[] get(String relativePath, Runnable watcher)
         throws KeeperException, InterruptedException {
         String absPath = getAbsolutePath(relativePath);
-        return zk.getZooKeeper().getData(absPath, wrapCallback(watcher), null);
+        return zk.getData(absPath, wrapCallback(watcher), null);
     }
 
     @Override
     public void asyncGet(String relativePath,
                          DirectoryCallback<byte[]> dataCallback,
                          Watcher watcher, Object context) {
-        zk.getZooKeeper().getData(
+        zk.getData(
             getAbsolutePath(relativePath), watcher,
             new AsyncCallback.DataCallback() {
                 @Override
@@ -231,8 +237,7 @@ public class ZkDirectory implements Directory {
         }
 
         return
-            new HashSet<>(
-                zk.getZooKeeper().getChildren(absPath, wrapCallback(watcher)));
+            new HashSet<>(zk.getChildren(absPath, wrapCallback(watcher)));
     }
 
     @Override
@@ -240,7 +245,7 @@ public class ZkDirectory implements Directory {
                                  DirectoryCallback<Collection<String>> callback,
                                  Watcher watcher, Object context) {
         String absPath = getAbsolutePath(relativePath);
-        zk.getZooKeeper().getChildren(
+        zk.getChildren(
             absPath, watcher, new AsyncCallback.Children2Callback() {
                 @Override
                 public void processResult(int rc, String path, Object ctx,
@@ -259,14 +264,14 @@ public class ZkDirectory implements Directory {
     public boolean exists(String path, Watcher watcher)
             throws KeeperException, InterruptedException {
         String absPath = getAbsolutePath(path);
-        return zk.getZooKeeper().exists(absPath, watcher) != null;
+        return zk.exists(absPath, watcher) != null;
     }
 
     @Override
     public boolean exists(String relativePath) throws KeeperException,
                                                    InterruptedException {
         String absPath = getAbsolutePath(relativePath);
-        return zk.getZooKeeper().exists(absPath, null) != null;
+        return zk.exists(absPath, null) != null;
     }
 
     @Override
@@ -274,7 +279,7 @@ public class ZkDirectory implements Directory {
                             DirectoryCallback<Boolean> callback,
                             Object context) {
         String absPath = getAbsolutePath(relativePath);
-        zk.getZooKeeper().exists(
+        zk.exists(
             absPath, null, new AsyncCallback.StatCallback() {
                 @Override
                 public void processResult(int rc, String path, Object ctx,
@@ -295,7 +300,7 @@ public class ZkDirectory implements Directory {
     public void delete(String relativePath) throws KeeperException,
                                                    InterruptedException {
         String absPath = getAbsolutePath(relativePath);
-        zk.getZooKeeper().delete(absPath, -1);
+        zk.delete(absPath, -1);
     }
 
     @Override
@@ -303,7 +308,7 @@ public class ZkDirectory implements Directory {
                             DirectoryCallback<Void> callback,
                             Object context) {
         String absPath = getAbsolutePath(relativePath);
-        zk.getZooKeeper().delete(
+        zk.delete(
             absPath, version, new AsyncCallback.VoidCallback() {
                 @Override
                 public void processResult(int rc, String path, Object ctx) {
@@ -333,6 +338,6 @@ public class ZkDirectory implements Directory {
     @Override
     public List<OpResult> multi(List<Op> ops)
         throws InterruptedException, KeeperException {
-        return zk.getZooKeeper().multi(ops);
+        return zk.multi(ops);
     }
 }
