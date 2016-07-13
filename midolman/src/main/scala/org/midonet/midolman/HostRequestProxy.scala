d47d3a9fff8ca2d36a42801d33d86d1364f7632e
Mateo Hermida <mateo@midokura.com>
2016-07-13 12:38:30 +0200

    FlowState: Don't wait for Cassandra connection
    The request proxy was waiting for an established connection with the
    cassandra cluster before starting the flow state recovery. This was done
    even if the flags to read from cassandra were off. Now if the flags are
    off and the Agent is insctructed to go to local storage or to request
    flow state from a different Agent then it doesn't wait for cassandra.
    
    Ref: MI-1202
    
    Change-Id: Ica76b34f9a9bc307b8f576c676213bc705343c33
    Signed-off-by: Mateo Hermida <mateo@midokura.com>
    (cherry picked from commit 26d8f5f4b94e56aaaf9bfadd3dd7765d2f4789cd)

--- midolman/src/main/scala/org/midonet/midolman/HostRequestProxy.scala
+++ midolman/src/main/scala/org/midonet/midolman/HostRequestProxy.scala
@@ -21,6 +21,7 @@ import java.util.concurrent.Executors.newSingleThreadExecutor
 import java.util.{UUID, HashMap => JHashMap, HashSet => JHashSet, Map => JMap, Set => JSet}
 
 import akka.actor.ActorRef
+
 import org.midonet.cluster.storage.FlowStateStorage
 import org.midonet.midolman.SimulationBackChannel.{BackChannelMessage, Broadcast}
 import org.midonet.midolman.config.FlowStateConfig
@@ -38,7 +39,6 @@ import org.midonet.services.flowstate.{FlowStateInternalMessageHeaderSize, FlowS
 import org.midonet.util.concurrent.ReactiveActor.{OnCompleted, OnError}
 import org.midonet.util.concurrent._
 import rx.Subscription
-
 import scala.collection.mutable
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Success}
@@ -133,36 +133,23 @@ class HostRequestProxy(hostId: UUID,
                            InetAddress.getLoopbackAddress, flowStateConfig.port)
     private val flowStateBuffer = ByteBuffer.allocate(MaxMessageSize)
 
+    private def requestLegacyStateForPort(portInfo: (UUID, UUID)): Future[FlowStateBatch] =
+        storageFuture.flatMap { storage =>
+            val (port, _) = portInfo
 
-    private def stateForPort(storage: FlowStateStorage[ConnTrackKey, NatKey],
-                             portInfo: (UUID, UUID)): Future[FlowStateBatch] = {
-        val (portId, previousOwnerId) = portInfo
+            val scf = storage.fetchStrongConnTrackRefs(port)
+            val wcf = storage.fetchWeakConnTrackRefs(port)
+            val snf = storage.fetchStrongNatRefs(port)
+            val wnf = storage.fetchWeakNatRefs(port)
 
-        if (flowStateConfig.legacyReadState) {
-            legacyStateRequestForPort(storage, portId)
-        } else if (flowStateConfig.localReadState) {
-            stateRequestForPort(portId, previousOwnerId)
-        } else {
-            log warn "Flow state not being recovered from local nor legacy " +
-                "storage. Check the agent's configuration to turn on one of " +
-                "the flags for flow state recovery"
-            Future.successful(EmptyFlowStateBatch)
+            ((scf zip wcf) zip (snf zip wnf)) map {
+                case ((sc, wc), (sn, wn)) => FlowStateBatch(sc, wc, sn, wn)
+            }
         }
-    }
 
-    private def legacyStateRequestForPort(storage: FlowStateStorage[ConnTrackKey, NatKey],
-                                  port: UUID): Future[FlowStateBatch] = {
-        val scf = storage.fetchStrongConnTrackRefs(port)
-        val wcf = storage.fetchWeakConnTrackRefs(port)
-        val snf = storage.fetchStrongNatRefs(port)
-        val wnf = storage.fetchWeakNatRefs(port)
+    private def requestStateForPort(portInfo: (UUID, UUID)): Future[FlowStateBatch] = {
+        val (port, previousOwnerId) = portInfo
 
-        ((scf zip wcf) zip (snf zip wnf)) map {
-            case ((sc, wc), (sn, wn)) => FlowStateBatch(sc, wc, sn, wn)
-        }
-    }
-
-    private def stateRequestForPort(port: UUID, previousOwnerId: UUID): Future[FlowStateBatch] = {
         Future {
             if (previousOwnerId == null || previousOwnerId == hostId) {
                 log debug s"Requesting local flow state for port: $port"
@@ -184,10 +171,21 @@ class HostRequestProxy(hostId: UUID,
         }(tcpClientExecutionContext)
     }
 
-    private def stateForPorts(storage: FlowStateStorage[ConnTrackKey, NatKey],
-                              bindings: Map[UUID, UUID]): Future[FlowStateBatch] =
-        Future.fold(bindings map (stateForPort(storage, _)))(EmptyFlowStateBatch) {
-            (batch: FlowStateBatch, v: FlowStateBatch) => batch.merge(v)
+    private def stateForPorts(bindings: Map[UUID, UUID]): Future[FlowStateBatch] =
+        if (flowStateConfig.legacyReadState) {
+            mergedBatches(bindings map requestLegacyStateForPort)
+        } else if (flowStateConfig.localReadState) {
+            mergedBatches(bindings map requestStateForPort)
+        } else {
+            log warn "Flow state not being recovered from local nor legacy " +
+                     "storage. Check the agent's configuration to turn on " +
+                     "one of the flags for flow state recovery"
+            Future.successful(EmptyFlowStateBatch)
+        }
+
+    private def mergedBatches(batches: Iterable[Future[FlowStateBatch]]) =
+        Future.fold(batches)(EmptyFlowStateBatch) {
+            (left: FlowStateBatch, right: FlowStateBatch) => left.merge(right)
         }
 
     private def resolveHostIp(id: UUID) =
@@ -263,7 +261,7 @@ class HostRequestProxy(hostId: UUID,
 
                 lastPorts = h.portBindings.keySet
 
-                storageFuture.flatMap(stateForPorts(_, ports)).andThen {
+                stateForPorts(ports).andThen {
                     case Success(stateBatch) =>
                         log.debug(s"Fetched ${stateBatch.size()} pieces of " +
                                   s"flow state for ports ${ports.keySet}")
