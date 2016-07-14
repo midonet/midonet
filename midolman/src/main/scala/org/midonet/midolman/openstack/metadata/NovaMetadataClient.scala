d0ba40fbb9460e59d1a0b17208b0b48096366f80
Alex Bikfalvi <alex.bikfalvi@midokura.com>
2016-07-14 16:13:04 +0200

    Metadata service uses Scala logger
    This replaces the Java with the Scala logger in the metadata
    service, which uses macros.
    
    Change-Id: Ibef39a464d3e18b322962ff76168029175aa1430
    Signed-off-by: Alex Bikfalvi <alex.bikfalvi@midokura.com>

--- midolman/src/main/scala/org/midonet/midolman/openstack/metadata/NovaMetadataClient.scala
+++ midolman/src/main/scala/org/midonet/midolman/openstack/metadata/NovaMetadataClient.scala
@@ -19,7 +19,6 @@ package org.midonet.midolman.openstack.metadata
 import com.sun.jersey.api.client.{Client, ClientResponse, UniformInterfaceException}
 
 import org.apache.commons.io.IOUtils
-import org.slf4j.Logger
 
 object Conv {
     implicit def toHexstring(bytes: Array[Byte]): String =
@@ -55,7 +54,6 @@ class UnknownRemoteAddressException(remoteAddr: String)
     extends RuntimeException(s"Unknown remote address $remoteAddr")
 
 object NovaMetadataClient {
-    private val log: Logger = MetadataService.getLogger
 
     import Conv._
 
@@ -66,17 +64,17 @@ object NovaMetadataClient {
     def getMetadata(path: String, remoteAddr: String,
                     novaMetadataUrl: String,
                     sharedSecret: String): String = {
-        log debug s"Request from $remoteAddr for path $path"
+        Log debug s"Request from $remoteAddr for path $path"
         InstanceInfoMap getByAddr remoteAddr match {
             case Some(info) =>
-                log debug s"Request matches instance $info"
+                Log debug s"Request matches instance $info"
                 getMetadata(path, info, novaMetadataUrl, sharedSecret)
             case None =>
                 /*
                  * This shouldn't happen normally as datapath flows are
                  * installed using InstanceInfo.
                  */
-                log warn s"Received request from unknown address $remoteAddr"
+                Log warn s"Received request from unknown address $remoteAddr"
                 throw new UnknownRemoteAddressException(remoteAddr)
         }
     }
@@ -87,7 +85,7 @@ object NovaMetadataClient {
         val instanceIdSig = signInstanceId(sharedSecret, info.instanceId)
         val client = new Client
         val url = novaMetadataUrl + path
-        log debug s"Request from instance:${info.instanceId} to $url"
+        Log debug s"Request from instance:${info.instanceId} to $url"
 
         val resource = client.resource(url)
             .header("X-Tenant-ID", info.tenantId)
@@ -100,7 +98,7 @@ object NovaMetadataClient {
                 response.getType.getParameters.getOrDefault("charset", "UTF-8")
             val data = IOUtils.toString(response.getEntityInputStream,
                                         encoding)
-            log debug s"Response for instance:${info.instanceId} " +
+            Log debug s"Response for instance:${info.instanceId} " +
                       s"status:${response.getStatus} " +
                       s"length:${response.getLength} " +
                       s"media-type:${response.getType} " +
@@ -108,7 +106,7 @@ object NovaMetadataClient {
             data
         } catch {
             case e: UniformInterfaceException =>
-                log error s"Unexpected HTTP response: $e for request: " +
+                Log error s"Unexpected HTTP response: $e for request: " +
                           s"$url $info"
                 throw e
         }
