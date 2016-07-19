e59a0f3c6c85f46233a271fb251843044404c54d
Alex Bikfalvi <alex.bikfalvi@midokura.com>
2016-07-19 13:46:41 +0200

    Close wdog pipe after stopping services
    With this change the agent closes the watchdog process pipe after
    shutting down the agent services, such that watchdog does not
    retry to restart the agent, while it is still shutting down.
    
    Change-Id: I053e468ad03dcf20680c8329f51057cd28129bba
    Signed-off-by: Alex Bikfalvi <alex.bikfalvi@midokura.com>
    (cherry picked from commit ce2fc279bf7b0dbaf4a935e383b155e7007e4adc)

--- midolman/src/main/scala/org/midonet/midolman/WatchedProcess.scala
+++ midolman/src/main/scala/org/midonet/midolman/WatchedProcess.scala
@@ -18,35 +18,27 @@ package org.midonet.midolman
 import java.io.IOException
 import java.nio.ByteBuffer
 import java.nio.channels.FileChannel
-import java.nio.file.FileSystems
-import java.nio.file.Path
-import java.nio.file.StandardOpenOption
-import java.util.Timer
-import java.util.TimerTask
+import java.nio.file.{FileSystems, Path, StandardOpenOption}
+import java.util.{Timer, TimerTask}
 import java.util.concurrent.TimeoutException
 
-import com.typesafe.scalalogging.Logger
-import org.midonet.util.concurrent.CallingThreadExecutionContext
-import org.slf4j.LoggerFactory
 import scala.concurrent.Promise
+
+import com.typesafe.scalalogging.Logger
+
+import org.slf4j.LoggerFactory
+
+import org.midonet.util.concurrent.CallingThreadExecutionContext
 
 class WatchedProcess {
     val log = Logger(LoggerFactory.getLogger("org.midonet.midolman.watchdog"))
 
-    @volatile
-    private var running = false
+    private val sync = new Object
+    @volatile private var running = false
     private var intervalMillis: Int = 0
     private var pipePath: Path = null
     private var pipe: FileChannel = null
     private var timer: Timer = null
-
-    private val shutdownHook = new Thread() {
-        override def run() {
-            log.info("Stopping watchdog thread")
-            timer.cancel()
-            running = false
-        }
-    }
 
     private def initializationTimeout(promise: Promise[_]) = {
         new TimerTask() {
@@ -57,7 +49,7 @@ class WatchedProcess {
     }
 
     private val tick = new TimerTask() {
-        val buf = ByteBuffer.allocate(1);
+        val buf = ByteBuffer.allocate(1)
         buf.put(57.toByte)
         buf.flip()
 
@@ -93,7 +85,7 @@ class WatchedProcess {
                 log.warn(s"Disabling watchdog: Invalid WDOG_TIMEOUT value: $timeoutStr")
                 false
             } else {
-                pipePath = FileSystems.getDefault().getPath(pipeStr);
+                pipePath = FileSystems.getDefault.getPath(pipeStr)
                 true
             }
         } catch {
@@ -115,7 +107,7 @@ class WatchedProcess {
         }
     }
 
-    def start(initializationWitness: Promise[_], timeout: Int) {
+    def start(initializationWitness: Promise[_], timeout: Int): Unit = sync.synchronized {
         if (running)
             return
         if (!readConfig())
@@ -125,16 +117,24 @@ class WatchedProcess {
 
         log.info("Starting watchdog thread")
         running = true
-        Runtime.getRuntime.addShutdownHook(shutdownHook)
         timer = new Timer("watchdog", true)
         timer.scheduleAtFixedRate(tick, 0, intervalMillis)
+        timer.schedule(initializationTimeout(initializationWitness),
+                       timeout * 1000)
 
-        timer.schedule(initializationTimeout(initializationWitness), timeout * 1000)
         initializationWitness.future.onFailure {
             case e =>
                 Midolman.dumpStacks()
                 timer.cancel()
                 pipe.close()
         }(CallingThreadExecutionContext)
+    }
+
+    def close(): Unit = sync.synchronized {
+        log.info("Stopping watchdog thread")
+        if (timer ne null) {
+            timer.cancel()
+        }
+        running = false
     }
 }
