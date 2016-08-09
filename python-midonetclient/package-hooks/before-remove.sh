07257021fd566b55fb8a1c6d1a478142bdc4c6a9
Ivan Kelly <ivan@midokura.com>
2016-08-09 19:11:22 +0100

    Don't assume all systems use dpkg for python client
    Change-Id: I09c2d4d458b3edcdc6db0b562371c340457691cf
    Signed-off-by: Ivan Kelly <ivan@midokura.com>

--- python-midonetclient/package-hooks/before-remove.sh
+++ python-midonetclient/package-hooks/before-remove.sh
@@ -1,8 +1,13 @@
 if command -v pyclean >/dev/null 2>&1; then
     pyclean -p python-midonetclient
 else
-    dpkg -L python-midonetclient | grep \.py$ | while read file
-    do
+    PYFILES=""
+    if [ -f /etc/debian_version ]; then
+        PYFILES=$(dpkg -L python-midonetclient | grep \.py$)
+    elif [ -f /etc/redhat-release ]; then
+        PYFILES=$(rpm -ql python-midonetclient | grep \.py$)
+    fi
+    for file in $PYFILES; do
         rm -f "${file}"[co] >/dev/null
     done
 fi
