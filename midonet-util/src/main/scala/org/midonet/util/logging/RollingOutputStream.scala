8fc75a625df241e6ac3a0c3548d32601c0efd270
Brandon Berg <bberg@midokura.com>
2016-08-12 18:52:04 +0900

    FWLOG: RollingOutputStream
    Create RollingOutputStream, an OutputStream implementation that rotates the
    destination file according to the specified rolling strategy.
    
    Change-Id: Ib07786a09d8f6b083a5f1da95f69c1c7c8464e83
    Signed-off-by: Brandon Berg <bberg@midokura.com>

---
+++ midonet-util/src/main/scala/org/midonet/util/logging/RollingOutputStream.scala
@@ -0,0 +1,271 @@
+/*
+ * Copyright 2016 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.util.logging
+
+import java.io._
+import java.util.UUID
+
+import scala.sys.process._
+
+import org.slf4j.LoggerFactory
+
+import org.midonet.util.UnixClock
+
+/** Creates underlying OutputStream for RollingOutputStream. */
+trait OutputStreamFactory {
+    protected def createOutputStream(path: String): OutputStream
+}
+
+/**
+  * Creates BufferedOutputStream as underlying OutputStream for
+  * RollingOutputStream.
+  */
+trait BufferedOutputStreamFactory extends OutputStreamFactory {
+    override protected def createOutputStream(path: String): OutputStream = {
+        new BufferedOutputStream(new FileOutputStream(path, true))
+    }
+}
+
+/**
+  * Rolling strategy for RollingOutputStream.
+  */
+trait RollingStrategy {
+
+    /**
+      * Invoked on each write to determine whether the logs should be rotated
+      * before writing, so should be fast and allocation-free.
+      */
+    protected def shouldRollOver: Boolean
+
+    /**
+      * Invoked on the first write and after each rollover to schedule the next
+      * rollover. Implementation is optional, but may be helpful to simplify
+      * shouldRollover implementation.
+      */
+    protected def scheduleNextRollover(): Unit = {}
+}
+
+private object DailyRollingStrategy {
+    val msPerDay = 24 * 60 * 60 * 1000
+}
+
+/**
+  * Implementation of RollingStrategy that rotates logs once per day, at
+  * midnight.
+  */
+trait DailyRollingStrategy extends RollingStrategy {
+    import DailyRollingStrategy._
+
+    protected[logging] val clock = UnixClock()
+
+    protected var nextRollover = 0L
+
+    override protected def shouldRollOver: Boolean = {
+        if (nextRollover == 0L) {
+            scheduleNextRollover()
+            false
+        } else {
+            clock.time >= nextRollover
+        }
+    }
+
+    override protected def scheduleNextRollover(): Unit = {
+        nextRollover = ((clock.time / msPerDay) + 1) * msPerDay
+    }
+}
+
+/**
+  * Used by RollingOutputStream to check headers on existing files and write
+  * headers as needed.
+  */
+trait HeaderManager {
+    /** Output stream for writing the header. Provided by RollingOutputStream */
+    protected var out: OutputStream
+
+    /**
+      * Returns true if file has a valid header and false if it's empty or
+      * doesn't exist. ThrowIllegalArgumentException if file is non-empty but
+      * does not have a valid header.
+      */
+    protected def hasHeader(file: File): Boolean
+
+    /** Write the header to file. */
+    protected def writeHeader(): Unit
+
+    protected def throwInvalidHeaderError(file: File): Unit = {
+        throw new IllegalArgumentException(
+            s"File ${file.getAbsolutePath} exists and is not empty, but " +
+            "does not have a valid header.")
+    }
+}
+
+/** HeaderManager that writes a header consisting of a fixed array of bytes. */
+trait StaticHeaderManager extends HeaderManager {
+    // Provided by RollingOutputStream implementation.
+    protected val header: Array[Byte]
+
+    override protected def hasHeader(file: File): Boolean = {
+        if (!file.exists() || file.length() == 0) {
+            false
+        } else if (file.length() < header.length) {
+            throwInvalidHeaderError(file)
+            false // Unreachable, but the type checker doesn't know that.
+        } else {
+            val buf = new Array[Byte](header.length)
+            val in = new FileInputStream(file)
+            in.read(buf)
+            if (!buf.sameElements(header)) {
+                throwInvalidHeaderError(file)
+            }
+            true
+        }
+    }
+
+    override protected def writeHeader(): Unit = {
+        out.write(header)
+        out.flush()
+    }
+}
+
+/**
+  * OutputStream that periodically rolls over and compresses the destination
+  * file according to the specified RollingStrategy. A concrete implementation
+  * of RollingOutputStream must mix in concrete implementations of the
+  * following three abstract traits:
+  *
+  * OutputStreamFactory: Creates the underlying OutputStream.
+  *
+  * RollingStrategy: Determines when the destination file will be rolled over.
+  *
+  * HeaderManager: Handles writing headers to newly-created files if needed.
+  *
+  * A concrete implementation of RollingOutputStream must mix in concrete
+  * implementations of these traits or implement their abstract methods
+  * directly.
+  *
+  * Requires the external Linux utility 'logrotate'.
+  *
+  * @param path The live version of the destination file, e.g.
+  *             /var/log/example.log. Older versions will be rotated to
+  *             /var/log/example.log.1.gz, /var/log/example.log.2.gz, etc.
+  *
+  * @param maxFiles Maximum number of rotated files to keep. If there are
+  *                 already maxFiles files retained and the RollingStrategy
+  *                 calls for a rollover, the oldest file will be deleted.
+  */
+abstract class RollingOutputStream(val path: String, val maxFiles: Int)
+    extends OutputStream {
+    this: OutputStreamFactory with RollingStrategy with HeaderManager =>
+
+    protected val log = LoggerFactory.getLogger(getClass)
+
+    private val tmpDir = System.getProperty("java.io.tmpdir")
+
+    // logrotate conf and status files. Required to run logrotate.
+    private val confFile = new File(s"$tmpDir/${UUID.randomUUID()}.conf")
+    private val statusFile = new File(confFile.getAbsolutePath + ".status")
+
+    private val destFile = new File(path)
+
+    protected var out: OutputStream = createOutputStream(path)
+    if (!hasHeader(destFile)) {
+        writeHeader()
+    }
+
+    override def write(b: Array[Byte]): Unit = {
+        checkNotClosed()
+        if (shouldRollOver)
+            rollOver()
+        out.write(b)
+    }
+
+    override def write(b: Int): Unit = {
+        checkNotClosed()
+        if (shouldRollOver)
+            rollOver()
+        out.write(b)
+    }
+
+    override def write(b: Array[Byte], off: Int, len: Int): Unit = {
+        checkNotClosed()
+        if (shouldRollOver)
+            rollOver()
+        out.write(b, off, len)
+    }
+
+    override def flush(): Unit = {
+        checkNotClosed()
+        out.flush()
+    }
+
+    override def close(): Unit = {
+        checkNotClosed()
+        out.close()
+        out = null
+    }
+
+    def rollOver(): Unit = {
+        checkNotClosed()
+
+        // Create the logrotate config file.
+        val confFileOs = new FileOutputStream(confFile, false)
+        confFileOs.write(
+            s"""
+               |$path {
+               |    rotate $maxFiles
+               |    compress
+               |}
+            """.stripMargin.getBytes)
+        confFileOs.close()
+
+        // Close stream before rotating.
+        out.close()
+
+        // Run logrotate, capturing stderr in case it fails.
+        val errBldr = new StringBuilder
+        val plog = ProcessLogger(o => Unit, e => errBldr.append(e).append('\n'))
+        val p = (s"logrotate -f ${confFile.getAbsolutePath} " +
+                s"--state $statusFile").run(plog)
+        if (p.exitValue() != 0) {
+            log.error("Log rotate failed with error code {}: {}",
+                      Array(p.exitValue().toString,
+                            errBldr.toString()))
+        }
+
+        confFile.delete()
+        statusFile.delete()
+
+        scheduleNextRollover()
+
+        // Reopen output stream with new file.
+        out = createOutputStream(path)
+        writeHeader()
+    }
+
+    private def checkNotClosed(): Unit = {
+        if (out == null)
+            throw new IllegalStateException("Stream is closed.")
+    }
+}
+
+class DailyRollingStaticHeaderBufferedOutputStream(
+        path: String, maxFiles: Int,
+        protected val header: Array[Byte])
+    extends RollingOutputStream(path, maxFiles)
+            with BufferedOutputStreamFactory
+            with DailyRollingStrategy
+            with StaticHeaderManager
---
+++ midonet-util/src/test/scala/org/midonet/util/logging/RollingOutputStreamTest.scala
@@ -0,0 +1,238 @@
+/*
+ * Copyright 2016 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.util.logging
+
+import java.io.{File, FileInputStream, FileOutputStream}
+import java.util.{Properties, UUID}
+import java.util.zip.GZIPInputStream
+
+import org.apache.commons.io.FileUtils
+import org.scalatest.{BeforeAndAfterAll, FeatureSpec, Matchers}
+
+import org.midonet.util.logging.DailyRollingStrategy.msPerDay
+import org.midonet.util.{MockUnixClock, UnixClock}
+
+class RollingOutputStreamTest extends FeatureSpec
+                                      with Matchers
+                                      with BeforeAndAfterAll {
+
+    val tmpDir = new File("/tmp/rolling-output-stream-test")
+
+    var savedProperties: Properties = null
+
+    override protected def beforeAll(): Unit = {
+        tmpDir.mkdir()
+        savedProperties = System.getProperties
+        System.setProperty(UnixClock.USE_MOCK_CLOCK_PROPERTY, "yes")
+    }
+
+    override protected def afterAll(): Unit = {
+        FileUtils.deleteDirectory(tmpDir)
+        System.setProperties(savedProperties)
+    }
+
+    private def genFilePath: String = {
+        s"${tmpDir.getAbsolutePath}/rollover-${UUID.randomUUID()}.log"
+    }
+
+    feature("RollingOutputStream") {
+        scenario("Performs rollover when rollOver() called") {
+            val header = "header"
+            val path = genFilePath
+            val os = new DailyRollingStaticHeaderBufferedOutputStream(
+                path, 5, header.getBytes)
+
+            val log1Text = "First log's text."
+            os.write(log1Text.getBytes())
+            os.rollOver()
+
+            val log2Text = "Second log's text."
+            os.write(log2Text.getBytes())
+            os.close()
+
+            checkFile(path + ".1.gz", header, log1Text)
+            checkFile(path, header, log2Text)
+        }
+
+        scenario("Performs scheduled rollover") {
+            val header = "[header]\n"
+            val path = genFilePath
+            val os = new DailyRollingStaticHeaderBufferedOutputStream(
+                path, 5, header.getBytes)
+
+            val log1Text = "First log's text"
+            os.write(log1Text.getBytes)
+
+            // Trigger rollover
+            advanceToNextMidnight(os.clock)
+            val log2Text = "Second log text"
+            os.write(log2Text.getBytes)
+
+            // Trigger another rollover.
+            advanceToNextMidnight(os.clock)
+            val log3Text = "Third log text"
+            os.write(log3Text.getBytes)
+            os.close()
+
+            checkFile(path + ".2.gz", header, log1Text)
+            checkFile(path + ".1.gz", header, log2Text)
+            checkFile(path, header, log3Text)
+        }
+
+        scenario("Throws exception on corrupt header") {
+            val header = "Header\n"
+            val path = genFilePath
+
+            val fos = new FileOutputStream(path)
+            fos.write("Blah blah blah".getBytes)
+            fos.close()
+
+            an [IllegalArgumentException] should be thrownBy
+                new DailyRollingStaticHeaderBufferedOutputStream(
+                    path, 5, header.getBytes)
+            val f = new File(path)
+            f.delete()
+        }
+
+        scenario("Throws exception on too-short header.") {
+            val header = "Header\n"
+            val path = genFilePath
+
+            val fos = new FileOutputStream(path)
+            fos.write("Header".getBytes) // No \n
+            fos.close()
+
+            an [IllegalArgumentException] should be thrownBy
+                new DailyRollingStaticHeaderBufferedOutputStream(
+                    path, 5, header.getBytes)
+            val f = new File(path)
+            f.delete()
+        }
+
+        scenario("Throws exceptions when using closed stream.") {
+            val header = "[Header\n]"
+            val path = genFilePath
+            val os = new DailyRollingStaticHeaderBufferedOutputStream(
+                path, 5, header.getBytes)
+            os.close()
+
+            an [IllegalStateException] should be thrownBy
+                os.write("Blah".getBytes)
+
+            an [IllegalStateException] should be thrownBy os.flush()
+            an [IllegalStateException] should be thrownBy os.close()
+            an [IllegalStateException] should be thrownBy os.rollOver()
+        }
+
+        scenario("Resumes writing to file with valid header") {
+            val header = "[Header\n]"
+            val path = genFilePath
+            val os = new DailyRollingStaticHeaderBufferedOutputStream(
+                path, 5, header.getBytes)
+            val line1 = "Body line 1\n"
+            os.write(line1.getBytes)
+            os.close()
+
+            val os2 = new DailyRollingStaticHeaderBufferedOutputStream(
+                path, 5, header.getBytes)
+            val line2 = "Body line 2\n"
+            os2.write(line2.getBytes)
+            os2.rollOver()
+
+            val line3 = "Body line 3\n"
+            os2.write(line3.getBytes)
+            os2.close()
+
+            checkFile(path + ".1.gz", header, line1 + line2)
+            checkFile(path, header, line3)
+        }
+
+        scenario("Supports flush()") {
+            val header = "[Header\n]"
+            val path = genFilePath
+            val os = new DailyRollingStaticHeaderBufferedOutputStream(
+                path, 5, header.getBytes)
+            val body = "Body text"
+            os.write(body.getBytes)
+
+            // Not flushed yet, should only have header.
+            checkFile(path, header, "", delete = false)
+
+            // Flush and try again.
+            os.flush()
+            checkFile(path, header, body, delete = false)
+
+            os.close()
+            checkFile(path, header, body)
+        }
+
+        scenario("Supports other write() overloads") {
+            val header = "[Header\n]"
+            val path = genFilePath
+            val os = new DailyRollingStaticHeaderBufferedOutputStream(
+                path, 5, header.getBytes)
+            os.write('a'.toByte)
+            os.write('b'.toByte)
+            os.write('c'.toByte)
+            advanceToNextMidnight(os.clock)
+            Thread.sleep(200)
+
+            os.write('d'.toByte)
+            os.write("abcdefghi".getBytes, 4, 2)
+            advanceToNextMidnight(os.clock)
+            Thread.sleep(200)
+
+            os.write("abcdefghi".getBytes, 6, 3)
+            os.close()
+
+            checkFile(path + ".2.gz", header, "abc")
+            checkFile(path + ".1.gz", header, "def")
+            checkFile(path, header, "ghi")
+        }
+    }
+
+    private def advanceToNextMidnight(clock: UnixClock): Unit = {
+        val mockClock = clock.asInstanceOf[MockUnixClock]
+        mockClock.time = (((mockClock.time + 1) / msPerDay) + 1) * msPerDay
+    }
+
+    private def checkFile(path: String, header: String, body: String,
+                          delete: Boolean = true): Unit = {
+        val file = new File(path)
+        file.exists shouldBe true
+
+        val in = if (path.endsWith(".gz")) {
+            new GZIPInputStream(new FileInputStream(path))
+        } else {
+            new FileInputStream(path)
+        }
+
+        val buf = new Array[Byte](file.length.toInt)
+
+        // Check header.
+        val headerLen = header.getBytes.length
+        in.read(buf, 0, headerLen)
+        new String(buf.take(headerLen)) shouldBe header
+
+        // Check body.
+        val bodyLen = in.read(buf)
+        new String(buf.take(bodyLen)) shouldBe body
+
+        if (delete)
+            file.delete()
+    }
+}
