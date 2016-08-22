450c3cdc486c04173def64901590ab58e876a5ff
Miguel Ángel Herranz Trillo <miguel@midokura.com>
2016-08-22 17:42:27 +0200

    Updating ARP entries related to FIPs on GW port MAC change
    In case the MAC is change in a port that is acting as gateway for
    FIPs, the entries in the associated network ARP tables has to be
    updated, deleting entry of old MAC and creating one for new MAC.
    
    This is done in this commit by implementing a new method in the
    PortTranslator that find impacted FIPs by checking the NAT rules of
    the port in case there is a MAC update.
    
    A unit test is also included, that check the create/delete
    operations are added by the PortTranslator.
    
    Ref: MI-1210
    
    Change-Id: Ie1bbf56e36caf11092745b22fdda5422c60944ed
    (cherry picked from commit 5e64ca4de461a3b882b9b3815bbb3209e8e5426e)

--- midonet-cluster/src/test/scala/org/midonet/cluster/services/c3po/translators/TranslatorTestBase.scala
+++ midonet-cluster/src/test/scala/org/midonet/cluster/services/c3po/translators/TranslatorTestBase.scala
@@ -15,10 +15,15 @@
  */
 package org.midonet.cluster.services.c3po.translators
 
-import scala.concurrent.Promise
+import java.util
+
+import scala.concurrent.{Future, Promise}
 import java.util.{UUID => JUUID}
 
+import scala.util.Try
+
 import org.mockito.Mockito.{mock, when}
+import org.mockito.ArgumentMatcher
 import org.mockito.Matchers.any
 import org.mockito.invocation.InvocationOnMock
 import org.mockito.stubbing.Answer
@@ -136,6 +141,36 @@ abstract class TranslatorTestBase  extends FlatSpec
         }
         when(storage.getAll(classOfM, ids))
                     .thenReturn(Promise.successful(msgs).future)
+    }
+
+    /* Return a matcher that match if a sequence (thatSeq) have all the elements
+     * (and only that elements) of the sequence given as argument (thisSeq) */
+    protected def seqInAnyOrder[T](thisSeq: Seq[T]) = new ArgumentMatcher[Seq[T]] {
+        override def matches(thatSeq: scala.Any): Boolean =
+            thisSeq.toSet.equals(thatSeq.asInstanceOf[Seq[T]].toSet)
+    }
+
+    /* Mock exists and getAll on instances of M with its ID in "ids" in any
+     * order.
+     * ids and msgs must NOT be null
+     * ids and msgs must have the same number of elements */
+    protected def bindAllInAnyOrder[K, M](
+            ids: Seq[UUID], msgs: Seq[M], clazz: Class[M] = null): Unit = {
+        assert(ids != null)
+        assert(msgs != null)
+        assert(ids.size == msgs.size)
+        when(storage.getAll(org.mockito.Matchers.eq(clazz),
+                            org.mockito.Matchers.argThat(seqInAnyOrder(ids))))
+        .thenAnswer(
+            new Answer[Future[Seq[M]]] {
+                val boundIds = (ids zip msgs).toMap
+                override def answer(invocation: InvocationOnMock) = {
+                    val args = invocation.getArguments()
+                    val answerIds = args(1).asInstanceOf[Seq[UUID]]
+                    Promise.successful(answerIds map boundIds).future
+                }
+            }
+        )
     }
 
     /* Finds an operation on Chain with the specified chain ID, and returns a
