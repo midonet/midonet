0f8c2db5636e138de3628d095b24851b832cf04d
Adrian Serrano <adrian@midokura.com>
2016-08-22 16:25:15 +0200

    10kp: Improve performance of table entry decoding
    This patch reduces the number of allocations needed to process
    each state table entry, the main bottleneck in state table
    processing at the cluster. It also features more efficient
    decoding methods for the common types.
    
    Change-Id: I131daf3955a0812f0b371fccce13c4163da6afe7

--- midonet-cluster/src/main/scala/org/midonet/cluster/services/state/StateEntryDecoder.scala
+++ midonet-cluster/src/main/scala/org/midonet/cluster/services/state/StateEntryDecoder.scala
@@ -39,36 +39,85 @@ object StateEntryDecoder {
         }
     }
 
+    private class FastDefaultDecoder extends StateEntryDecoder {
+        private val builder = KeyValue.newBuilder()
+        override def decode(string: String): KeyValue = {
+            builder
+                .setDataVariable(ByteString.copyFromUtf8(string))
+                .build()
+        }
+    }
+
     private object MacDecoder extends StateEntryDecoder {
         @throws[InvalidMacException]
         override def decode(string: String): KeyValue = {
             KeyValue.newBuilder()
-                    .setData64(MAC.stringToLong(string))
-                    .build()
+                .setData64(MAC.stringToLong(string))
+                .build()
+        }
+    }
+
+    private class FastMacDecoder extends StateEntryDecoder {
+        private val builder = KeyValue.newBuilder()
+        @throws[IllegalArgumentException]
+        override def decode(string: String): KeyValue = {
+            builder
+                .setData64(FastDecoders.macStringToLong(string))
+                .build()
         }
     }
 
     private object Ip4Decoder extends StateEntryDecoder {
+
         @throws[IllegalArgumentException]
         override def decode(string: String): KeyValue = {
             KeyValue.newBuilder()
-                    .setData32(IPv4Addr.stringToInt(string))
-                    .build()
+                .setData32(IPv4Addr.stringToInt(string))
+                .build()
+        }
+    }
+
+    private class FastIp4Decoder extends StateEntryDecoder {
+        private val builder = KeyValue.newBuilder()
+
+        @throws[IllegalArgumentException]
+        override def decode(string: String): KeyValue = {
+            builder
+                .setData32(IPv4Addr.stringToInt(string))
+                .build()
         }
     }
 
     private object UuidDecoder extends StateEntryDecoder {
+
         @throws[IllegalArgumentException]
         override def decode(string: String): KeyValue = {
             val id = UUID.fromString(string)
             KeyValue.newBuilder()
-                    .setDataVariable(ByteString.copyFrom(
-                        ByteBuffer.allocate(16)
-                            .order(ByteOrder.BIG_ENDIAN)
-                            .putLong(id.getMostSignificantBits)
-                            .putLong(id.getLeastSignificantBits)
-                            .rewind().asInstanceOf[ByteBuffer]))
-                    .build()
+                .setDataVariable(ByteString.copyFrom(
+                    ByteBuffer.allocate(16)
+                        .order(ByteOrder.BIG_ENDIAN)
+                        .putLong(id.getMostSignificantBits)
+                        .putLong(id.getLeastSignificantBits)
+                        .rewind().asInstanceOf[ByteBuffer]))
+                .build()
+        }
+    }
+
+    private class FastUuidDecoder extends StateEntryDecoder {
+        private val builder = KeyValue.newBuilder()
+        private val buffer = ByteBuffer.allocate(16).order(ByteOrder.BIG_ENDIAN)
+        private val uuid = new FastDecoders.MutableUUID
+
+        @throws[IllegalArgumentException]
+        override def decode(string: String): KeyValue = {
+            uuid.fillFromString(string)
+            buffer.clear()
+            builder.setDataVariable(ByteString.copyFrom(
+                    buffer.putLong(uuid.getMostSignificantBits)
+                          .putLong(uuid.getLeastSignificantBits)
+                          .rewind().asInstanceOf[ByteBuffer]))
+                .build()
         }
     }
 
@@ -84,11 +133,22 @@ object StateEntryDecoder {
         }
     }
 
+    /**
+      * @return A [[StateEntryDecoder]] optimised to reduce allocations
+      *         WARNING: Not thread-safe!
+      */
+    def getUnsafe(clazz: Class[_]): StateEntryDecoder = {
+        clazz match {
+            case MacClass => new FastMacDecoder
+            case Ip4Class => new FastIp4Decoder
+            case UuidClass => new FastUuidDecoder
+            case _ => new FastDefaultDecoder
+        }
+    }
 }
 
 trait StateEntryDecoder {
 
     @throws[Exception]
     def decode(string: String): KeyValue
-
 }
