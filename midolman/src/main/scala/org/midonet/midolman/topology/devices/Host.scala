c5db609e473d5fe924a29819ecb54cb7395d830a
Alex Bikfalvi <alex.bikfalvi@midokura.com>
2016-09-05 09:12:45 +0200

    Simplify the Host simulation device
    This removes some overhead from the Host simulation device by
    replacing the ZOOM conversions with arguments passed by
    constructor.
    
    In addition to removing the overhead, this also ensures that
    setting the host fields is thread-safe.
    
    Change-Id: Id52af1b74a0939fc02fbb5d8007dbb1e909c927b
    Signed-off-by: Alex Bikfalvi <alex.bikfalvi@midokura.com>
    (cherry picked from commit ed3d9c4df5a6ef013c17c46e195a1ec7dd97c4d9)

--- midolman/src/main/scala/org/midonet/midolman/topology/devices/Host.scala
+++ midolman/src/main/scala/org/midonet/midolman/topology/devices/Host.scala
@@ -16,74 +16,18 @@
 
 package org.midonet.midolman.topology.devices
 
-import java.util.{Objects, UUID}
+import java.util.UUID
 
-import org.midonet.cluster.data.{ZoomClass, ZoomField, ZoomObject}
-import org.midonet.cluster.models.Topology
 import org.midonet.midolman.topology.VirtualTopology.Device
 import org.midonet.packets.IPAddr
 
-@ZoomClass(clazz = classOf[Topology.Host])
-class Host extends ZoomObject with Device {
-
-    def this(host: Host) = {
-        this()
-        id = host.id
-        portIds = host.portIds
-        tunnelZoneIds = host.tunnelZoneIds
-        tunnelZones = host.tunnelZones
-        alive = host.alive
-    }
-
-    def this(hostId: UUID,
-             alive: Boolean,
-             portBindings: Map[UUID, (String, UUID)],
-             tunnelZones: Map[UUID, IPAddr]) = {
-        this()
-        id = hostId
-        this.portIds = portBindings.keySet
-        this.portBindings = portBindings
-        this.tunnelZoneIds = tunnelZones.keySet
-        this.tunnelZones = tunnelZones
-        this.alive = alive
-    }
-
-    @ZoomField(name = "id")
-    var id: UUID = _
-    @ZoomField(name = "port_ids")
-    var portIds = Set.empty[UUID]
-    @ZoomField(name = "tunnel_zone_ids")
-    var tunnelZoneIds = Set.empty[UUID]
-
-    // To be filled by the HostMapper.
-    // The IP address of the host in each one of the tunnel zones.
-    var tunnelZones = Map.empty[UUID, IPAddr]
-
-    // To be filled by the HostMapper.
-    // The interface to which each port is bound, and the previous owner of the
-    // flowstate for the port
-    var portBindings = Map.empty[UUID, (String, UUID)]
-
-    // The alive status of the host is stored outside of the host proto.
-    var alive: Boolean = false
+case class Host(id: UUID,
+                alive: Boolean,
+                tunnelZones: Map[UUID, IPAddr],
+                portBindings: Map[UUID, (String, UUID)]) extends Device {
 
     override def toString =
-        s"Host [id=$id alive=$alive " +
-        s"boundPortIds=$portIds portBindings=$portBindings " +
-        s"tunnelZoneIds=$tunnelZoneIds tunnelZones=$tunnelZones]"
+        s"Host [id=$id alive=$alive portBindings=$portBindings " +
+        s"tunnelZones=$tunnelZones]"
 
-    override def equals(o: Any): Boolean = o match {
-        case host: Host =>
-            host.alive == alive &&
-            host.id == id &&
-            host.portIds == portIds &&
-            host.portBindings == portBindings &&
-            host.tunnelZoneIds == tunnelZoneIds &&
-            host.tunnelZones == tunnelZones
-        case _ => false
-    }
-
-    override def hashCode: Int =
-        Objects.hash(id, portIds, portBindings,
-                     tunnelZoneIds, tunnelZones)
 }
