4438d1e388662d59beba522c41a8f251a36d8652
Mateo Hermida <mateo@midokura.com>
2016-09-05 14:55:32 +0200

    Make ci scripts sh-compatible
    The ci scripts are using the bash built-in source, this patch updates it
    to use the 'dot' command.
    
    Change-Id: Ied6d2df87be61a1ae6f653409db46cdab1eb6165
    Signed-off-by: Mateo Hermida <mateo@midokura.com>

--- tests/ci-perf.sh
+++ tests/ci-perf.sh
@@ -24,13 +24,13 @@ if [ -z "$BUILD" ]; then
     BUILD="unknown"
 fi
 
-source venv/bin/activate
+. venv/bin/activate
 
 # disable debug logging
 export MIDOLMAN1=$(docker ps -q -a -f=name=midolman1)
 echo "agent.loggers.root=INFO" | docker exec -i $MIDOLMAN1 mn-conf set -t default
 
-pushd tests/mdts/tests/performance_tests/
+cd tests/mdts/tests/performance_tests/
 ./run_tests.sh -r $WORKSPACE/tests -l logs
 if [ $? = 0 ]; then
     export JMXTRANS=$(docker ps -q -a -f=name=jmxtrans)
@@ -44,8 +44,8 @@ if [ $? = 0 ]; then
 else
     echo "Tests failed, skipping stats upload"
 fi
-popd
+cd -
 
-pushd /tmp/jfr
+cd /tmp/jfr
 tar -cvzf $WORKSPACE/perf_jfr.tar.gz *
-popd
+cd -
