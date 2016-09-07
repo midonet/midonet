b19db96dcc1153bb8e8aa6bd9ae00b51e3b34315
Xavi León <xavi.leon@midokura.com>
2016-09-07 16:03:24 +0200

    Introduce a free port provider for testing
    Factor out into an utility class the method to get a free port from the
    OS to bind a server to. Tests based on the ConfResourceTest still used
    randomness to get a port which could be flakey on the CI
    
    Change-Id: I78cccffeda38533fb1795784060f4d91d03e9d19
    Signed-off-by: Xavi León <xavi.leon@midokura.com>

---
+++ midonet-cluster/src/test/java/org/midonet/cluster/util/PortProvider.java
@@ -0,0 +1,46 @@
+package org.midonet.cluster.util;/*
+ * Copyright 2016 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class PortProvider {
+
+    /**
+     * Binding a socket to port 0 makes the OS returns us a free ephemeral
+     * port. Return this port so a server can binds to it.
+     *
+     * @return port A free port to bind a server to.
+     */
+    public static int getPort() {
+        while (true) {
+            ServerSocket ss = null;
+            try {
+                ss = new ServerSocket(0);
+                ss.setReuseAddress(true);
+                return ss.getLocalPort();
+            } catch (IOException e) {
+            } finally {
+                if (ss != null) {
+                    try {
+                        ss.close();
+                    } catch (IOException ioe) {}
+                }
+            }
+        }
+    }
+
+}
