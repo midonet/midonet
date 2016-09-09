a4f98a3c68b7e0c23296507ed71f52a11cd3efbf
Xavi León <xavi.leon@midokura.com>
2016-09-09 13:26:19 +0200

    Utils: Introduce monitored processes
    This patch factors out the handling of failures on subprocess so it can
    be reused in later patches. It also fixes an issue with the restart
    logic: if a process reaches the maximum number of restarts even if it is
    in a long period of time, it will shutdown the Agent completely. This
    patch improves the restart logic by only considering a time window for
    failures to be accounted for. In other words, the monitored process
    will only be restarted if it reaches the maximum the number of failures
    within a specific time window.
    
    Change-Id: I35d5796f58e2a58dbb3f9678fcfe13ae5f2211fc
    Signed-off-by: Xavi León <xavi.leon@midokura.com>

---
+++ midonet-util/src/main/java/org/midonet/util/process/MonitoredDaemonProcess.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2016 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.midonet.util.process;
+
+import java.util.LinkedList;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.util.concurrent.AbstractService;
+
+import org.slf4j.Logger;
+
+import org.midonet.util.UnixClock;
+import org.midonet.util.UnixClock$;
+
+/**
+ * A monitored daemon process is a subprocess being monitored by the parent
+ * process. It will restart the subprocess for a number of times during a
+ * specific time interval. If the number of failures during an interval exceeds
+ * the provided limit, the parent process will exit with the specified exit
+ * error code.
+ */
+public class MonitoredDaemonProcess extends AbstractService {
+
+    final private String cmd;
+    final private Logger log;
+    final private String prefix;
+    final private int retries;
+    final private long period;
+    final private Runnable exitAction;
+    final protected UnixClock clock = UnixClock$.MODULE$.apply();
+    final protected LinkedList<Long> startEvents;
+
+    volatile protected Process process;
+
+    public MonitoredDaemonProcess(String cmd, Logger log, String prefix,
+                                  int retries, long period, Runnable exitAction) {
+        this.cmd = cmd;
+        this.log = log;
+        this.prefix = prefix;
+        this.retries = retries;
+        this.period = period;
+        this.exitAction = exitAction;
+        this.startEvents = new LinkedList<>();
+    }
+
+    @Override
+    protected void doStart() {
+        synchronized (startEvents) {
+            long startWindow = clock.time() - period;
+            while (startEvents.size() > 0 && startEvents.peek() < startWindow) {
+                startEvents.poll();
+            }
+
+            if (startEvents.size() < retries) {
+                startEvents.offer(clock.time());
+                createProcess();
+                if (!isRunning()) {
+                    notifyStarted();
+                }
+            } else {
+                notifyFailed(new Exception(
+                    "Process ``" + cmd + "`` failed after " + retries +
+                    " retries in a period of " + period + " ms: " +
+                    "notifying failure."));
+                exitAction.run();
+            }
+        }
+    }
+
+    @Override
+    protected void doStop() {
+        if (process != null) {
+            process.destroy();
+            log.info("Process ``{}`` stopped", cmd);
+        }
+        notifyStopped();
+    }
+
+    @VisibleForTesting
+    protected void createProcess() {
+        process = ProcessHelper.newDaemonProcess(cmd, log, prefix)
+            .addExitHandler(exitHandler)
+            .run();
+        log.info("Process ``{}`` starting with pid {} at time {}",
+                 cmd, ProcessHelper.getProcessPid(process), clock.time());
+    }
+
+    final Runnable exitHandler = new Runnable() {
+        @Override
+        public void run() {
+            if (process != null) {
+                log.warn("Process ``{}`` exited with error code {}",
+                         cmd, process.exitValue());
+            } else {
+                log.warn("Process ``{}`` exited", cmd);
+            }
+            if (isRunning()) {
+                doStart();
+            }
+        }
+    };
+}
--- midonet-util/src/test/java/org/midonet/util/process/TestProcessHelper.java
+++ midonet-util/src/test/java/org/midonet/util/process/TestProcessHelper.java
@@ -80,4 +80,6 @@ public class TestProcessHelper {
                 .runAndWait(),
             equalTo(code));
     }
+
+
 }
