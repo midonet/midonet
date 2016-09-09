42a12cdb1bdb6151541d0e2fdb9f40fcc822a3cf
Xavi León <xavi.leon@midokura.com>
2016-09-09 18:51:53 +0200

    VPP: start vpp process during Agent startup
    This patch starts and monitors the VPP subprocess and includes the new
    package dependency when building the midolman package.
    
    Ref.: MI-1318
    Signed-off-by: Xavi León <xavi.leon@midokura.com>
    
    Change-Id: I761012487091a327fa9aa6f15ed10bdcc4a7cfef

--- midolman/src/main/java/org/midonet/midolman/Midolman.java
+++ midolman/src/main/java/org/midonet/midolman/Midolman.java
@@ -61,7 +61,6 @@ import org.midonet.midolman.services.MidolmanService;
 import org.midonet.midolman.simulation.PacketContext$;
 import org.midonet.util.cLibrary;
 import org.midonet.util.process.MonitoredDaemonProcess;
-import org.midonet.util.process.ProcessHelper;
 
 public class Midolman {
 
@@ -72,10 +71,13 @@ public class Midolman {
     public static final int MIDOLMAN_ERROR_CODE_UNHANDLED_EXCEPTION = 6001;
     public static final int MIDOLMAN_ERROR_CODE_PACKET_WORKER_DIED = 6002;
     public static final int MIDOLMAN_ERROR_CODE_MINION_PROCESS_DIED = 6003;
+    public static final int MIDOLMAN_ERROR_CODE_VPP_PROCESS_DIED = 6004;
 
     private static final long MIDOLMAN_EXIT_TIMEOUT_MILLIS = 30000;
     private static final int MINION_PROCESS_MAXIMUM_STARTS = 3;
     private static final int MINION_PROCESS_FAILING_PERIOD = 180000;
+    private static final int VPP_PROCESS_MAXIMUM_STARTS = 3;
+    private static final int VPP_PROCESS_MAXIMUM_PERIOD = 30000;
 
     private Injector injector;
 
@@ -83,6 +85,7 @@ public class Midolman {
 
     private volatile boolean shuttingDown = false;
     private volatile MonitoredDaemonProcess minionProcess = null;
+    private volatile MonitoredDaemonProcess vppProcess = null;
 
     private Midolman() {
     }
@@ -196,6 +199,10 @@ public class Midolman {
             "/usr/share/midolman/minions-start", log, "org.midonet.services",
             MINION_PROCESS_MAXIMUM_STARTS, MINION_PROCESS_FAILING_PERIOD,
             MIDOLMAN_ERROR_CODE_MINION_PROCESS_DIED);
+        vppProcess = new MonitoredDaemonProcess(
+            "vpp -c /etc/vpp/startup.conf", log, "org.midonet.vpp",
+            VPP_PROCESS_MAXIMUM_STARTS, VPP_PROCESS_MAXIMUM_PERIOD,
+            MIDOLMAN_ERROR_CODE_VPP_PROCESS_DIED);
 
         Options options = new Options();
         options.addOption("c", "configFile", true, "config file path");
@@ -280,6 +287,7 @@ public class Midolman {
 
         shuttingDown = true;
         minionProcess.shutdown();
+        vppProcess.shutdown();
 
         if (injector == null)
             return;
