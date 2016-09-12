59ac3bf35bd6d3f31b07b3be57549634311bfcf1
Alex Bikfalvi <alex.bikfalvi@midokura.com>
2016-09-12 23:22:56 +0200

    Perform egress port mirroring on VLAN-tagged frames
    This ensures that egress port mirroring is applied to VLAN-tagged
    frames, even if those frames skip the usual egress filters.
    
    Fixes: MI-758
    
    Change-Id: Id7d671c5e7bdebe1bbfab59e29ab3b2368f4a3f9
    Signed-off-by: Alex Bikfalvi <alex.bikfalvi@midokura.com>
    (cherry picked from commit 7424ffa11a1ed423205952fa6f07b04a7a5ee665)

--- midolman/src/main/scala/org/midonet/midolman/simulation/Bridge.scala
+++ midolman/src/main/scala/org/midonet/midolman/simulation/Bridge.scala
@@ -168,10 +168,7 @@ class Bridge(val id: UUID,
             else
                 handleL2Unicast() // including ARP replies
 
-        if (areChainsApplicable())
-            doPostBridging(context, action)
-        else
-            action
+        doPostBridging(context, action)
     }
 
     val normalProcess = ContinueWith((context) => {
@@ -199,8 +196,9 @@ class Bridge(val id: UUID,
      * Tells if chains should be executed for the current frames. So far, all
      * will be, except those with a VLAN tag.
      */
-    private def areChainsApplicable()(implicit context: PacketContext) =
+    private def areChainsApplicable()(implicit context: PacketContext) = {
         !context.wcmatch.isVlanTagged
+    }
 
     /**
       * Used by normalProcess to deal with L2 Unicast frames, this just decides
@@ -491,7 +489,10 @@ class Bridge(val id: UUID,
             case _ =>
         }
 
-        val result = filterOut(context, act.simStep)
+        val result =
+            if (areChainsApplicable()) filterOut(context, act.simStep)
+            else act
+
         if (result eq act)
             mirroringOutbound(context, result)
         else
