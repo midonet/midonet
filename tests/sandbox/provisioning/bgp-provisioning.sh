1506136d40bfb9ac9045744218831c9dc5a70464
Miguel Ángel Herranz Trillo <miguel@midokura.com>
2016-09-19 19:16:54 +0200

    Setup IPv6 interfaces in sandbox quagga containers
    Changes in BGP provisioning script to add IPv6 addresses to all the
    quagga containers interfaces and enable IPv6 forwarding on them.
    
    Change-Id: I6df8fc1a9f0d697ef8928cdc7911a26ffee2b6ac
    Signed-off-by: Miguel Ángel Herranz Trillo <miguel@midokura.com>

--- tests/sandbox/provisioning/bgp-provisioning.sh
+++ tests/sandbox/provisioning/bgp-provisioning.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
+set -x
+
+function setup_interface {
+    BRIDGE=$1
+    IFACE=$2
+    CONTAINER=$3
+    IPV4_ADDRESS=$4
+    IPV6_ADDRESS="$5"
+
+    sudo pipework/pipework $BRIDGE -i $IFACE $CONTAINER $IPV4_ADDRESS
+    if [[ ! -z $IPV6_ADDRESS ]]; then
+        sudo docker exec -it $CONTAINER ip a add $IPV6_ADDRESS dev $IFACE
+        sudo docker exec $CONTAINER bash -c "echo 1 > /proc/sys/net/ipv6/conf/$IFACE/forwarding"
+    fi
+}
 
 echo "Provisioning bgp interfaces..."
 
-# Cleaning previous bridge if exists
+# Cleaning previous bridges
 sudo ip link set dev brbgp0_${SANDBOX_NAME} down
 sudo ip link set dev brbgp1_${SANDBOX_NAME} down
 sudo ip link set dev brbgp2_${SANDBOX_NAME} down
@@ -12,22 +27,22 @@ sudo ip link del brbgp2_${SANDBOX_NAME}
 
 # One quagga (master) advertising "inet" networks connected to two
 # quaggas (peers) which will do the peering with midolman
-sudo pipework/pipework brbgp0_${SANDBOX_NAME} -i bgp0 mnsandbox${SANDBOX_NAME}_quagga0_1 10.10.0.1/24
-sudo pipework/pipework brbgp0_${SANDBOX_NAME} -i bgp0 mnsandbox${SANDBOX_NAME}_quagga1_1 10.10.0.2/24
-sudo pipework/pipework brbgp0_${SANDBOX_NAME} -i bgp0 mnsandbox${SANDBOX_NAME}_quagga2_1 10.10.0.3/24
+setup_interface brbgp0_${SANDBOX_NAME} bgp0 mnsandbox${SANDBOX_NAME}_quagga0_1 10.10.0.1/24 2001:10::1/72
+setup_interface brbgp0_${SANDBOX_NAME} bgp0 mnsandbox${SANDBOX_NAME}_quagga1_1 10.10.0.2/24 2001:10::2/72
+setup_interface brbgp0_${SANDBOX_NAME} bgp0 mnsandbox${SANDBOX_NAME}_quagga2_1 10.10.0.3/24 2001:10::3/72
 
 # Two interfaces per quagga peer (for single and multiple sessions)
 # BGP peer1
-sudo pipework/pipework brbgp1_${SANDBOX_NAME} -i bgp1 mnsandbox${SANDBOX_NAME}_quagga1_1 10.1.0.240/24
-sudo pipework/pipework brbgp1_${SANDBOX_NAME} -i bgp2 mnsandbox${SANDBOX_NAME}_quagga2_1 10.1.0.241/24
+setup_interface brbgp1_${SANDBOX_NAME} bgp1 mnsandbox${SANDBOX_NAME}_quagga1_1 10.1.0.240/24 2001:1::240/72
+setup_interface brbgp1_${SANDBOX_NAME} bgp2 mnsandbox${SANDBOX_NAME}_quagga2_1 10.1.0.241/24 2001:1::241/72
 
 # BGP peer2
-sudo pipework/pipework brbgp2_${SANDBOX_NAME} -i bgp1 mnsandbox${SANDBOX_NAME}_quagga2_1 10.2.0.240/24
-sudo pipework/pipework brbgp2_${SANDBOX_NAME} -i bgp2 mnsandbox${SANDBOX_NAME}_quagga1_1 10.2.0.241/24
+setup_interface brbgp2_${SANDBOX_NAME} bgp1 mnsandbox${SANDBOX_NAME}_quagga2_1 10.2.0.240/24 2001:2::240/72
+setup_interface brbgp2_${SANDBOX_NAME} bgp2 mnsandbox${SANDBOX_NAME}_quagga1_1 10.2.0.241/24 2001:2::241/72
 
 # An interface per midolman peer (for single and multiple sessions)
 # MM peer1
-sudo pipework/pipework brbgp1_${SANDBOX_NAME} -i bgp0 mnsandbox${SANDBOX_NAME}_midolman1_1 10.1.0.1/24
+setup_interface brbgp1_${SANDBOX_NAME} bgp0 mnsandbox${SANDBOX_NAME}_midolman1_1 10.1.0.1/24
 
 # MM peer2
-sudo pipework/pipework brbgp2_${SANDBOX_NAME} -i bgp0 mnsandbox${SANDBOX_NAME}_midolman2_1 10.2.0.1/24
+setup_interface brbgp2_${SANDBOX_NAME} bgp0 mnsandbox${SANDBOX_NAME}_midolman2_1 10.2.0.1/24
