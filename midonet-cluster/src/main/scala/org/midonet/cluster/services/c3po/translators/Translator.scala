d863d44ebe01718255de49cfa4e7267ea7f694c0
Alex Bikfalvi <alex.bikfalvi@midokura.com>
2016-09-19 17:36:41 +0200

    Translators: Cleanup translator interface
    This cleans up the methods from the Neutron translator interface
    and adds the corresponding ScalaDoc in preparation for including
    transactions.
    
    Ref: MI-1447
    
    Change-Id: I9086c6cbad2805dafec7c4ce880b24706555c2c7
    Signed-off-by: Alex Bikfalvi <alex.bikfalvi@midokura.com>

--- midonet-cluster/src/main/scala/org/midonet/cluster/services/c3po/translators/Translator.scala
+++ midonet-cluster/src/main/scala/org/midonet/cluster/services/c3po/translators/Translator.scala
@@ -30,7 +30,8 @@ import org.midonet.cluster.models.Commons.UUID
 import org.midonet.cluster.services.c3po.NeutronTranslatorManager.{Create, Delete, Operation, Update}
 import org.midonet.util.concurrent.toFutureOps
 
-/** Defines a class that is able to translate from an operation from a high
+/**
+  * Defines a class that is able to translate from an operation from a high
   * level model to a set of operations on the MidoNet model.
   */
 abstract class Translator[HighLevelModel <: Message](
@@ -42,19 +43,40 @@ abstract class Translator[HighLevelModel <: Message](
     protected def storage: ReadOnlyStorage
 
     /**
-     * Translate the operation on a high level model to a list of
-     * operations on translated models that:
-     *
-     * - maintain (if necessary) the original model,
-     * - and translate the model into a corresponding representation made up of
-     *   various derived models.
-     */
+      * Translates the operation on a high level model to a list of operations
+      * on translated models that:
+      * - maintain (if necessary) the original model,
+      * - and translate the model into a corresponding representation made up of
+      *   various derived models.
+      */
     @throws[TranslationException]
-    def translateOp(op: Operation[HighLevelModel]): OperationList =
+    def translateOp(op: Operation[HighLevelModel]): OperationList = {
         retainHighLevelModel(op) ++ translate(op)
+    }
 
-    /* Keep the original model as is by default. Override if the model does not
-     * need to be maintained, or need some special handling. */
+    /**
+      * Translates the operation on a high level model to a list of operations
+      * on translated models, without maintaining the representation of the
+      * original model.
+      */
+    @throws[TranslationException]
+    def translate(op: Operation[HighLevelModel]): OperationList = {
+        try {
+            op match {
+                case Create(nm) => translateCreate(nm)
+                case Update(nm, _) => translateUpdate(nm)
+                case Delete(_, id) => translateDelete(id)
+            }
+        } catch {
+            case NonFatal(ex) =>
+                throw new TranslationException(op, ex, ex.getMessage)
+        }
+    }
+
+    /**
+      * Keep the original model as is by default. Override if the model does not
+      * need to be maintained, or need some special handling.
+      */
     protected def retainHighLevelModel(op: Operation[HighLevelModel])
     : List[Operation[HighLevelModel]] = {
         op match {
@@ -64,20 +86,21 @@ abstract class Translator[HighLevelModel <: Message](
         }
     }
 
-    def translate(op: Operation[HighLevelModel]): OperationList = try {
-        op match {
-            case Create(nm) => translateCreate(nm)
-            case Update(nm, _) => translateUpdate(nm)
-            case Delete(_, id) => translateDelete(id)
-        }
-    } catch {
-        case NonFatal(ex) =>
-            throw new TranslationException(op, ex, ex.getMessage)
-    }
-
-    /* Implement the following for CREATE/UPDATE/DELETE of the model */
+    /**
+      * Translates a [[Create]] operation on the high-level model.
+      */
     protected def translateCreate(nm: HighLevelModel): OperationList
+
+    /**
+      * Translates an [[Update]] operation on the high-level model.
+      */
     protected def translateUpdate(nm: HighLevelModel): OperationList
+
+    /**
+      * Translates a [[Delete]] operation on the high-level model, with a given
+      * object identifier. The default implementation uses idempotent deletion,
+      * where the operation is ignored if the object does not exist.
+      */
     protected def translateDelete(id: UUID): OperationList = {
         val nm = try storage.get(ct.runtimeClass, id).await() catch {
             case ex: NotFoundException =>
@@ -90,6 +113,9 @@ abstract class Translator[HighLevelModel <: Message](
         translateDelete(nm.asInstanceOf[HighLevelModel])
     }
 
+    /**
+      * Translates a [[Delete]] operation on the high-level model.
+      */
     protected def translateDelete(nm: HighLevelModel): OperationList = List()
 }
 
