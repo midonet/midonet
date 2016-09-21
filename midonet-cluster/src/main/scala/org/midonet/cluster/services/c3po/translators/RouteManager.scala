0104efab776f3e751fb7e4d6aacfa72029e85d7b
Alex Bikfalvi <alex.bikfalvi@midokura.com>
2016-09-21 13:05:33 +0200

    Remove unused method from the route manager
    Ref: MI-1462
    
    Change-Id: Ie3ba326de3d459d1e596295a8ab7b0e3d05701af
    Signed-off-by: Alex Bikfalvi <alex.bikfalvi@midokura.com>

--- midonet-cluster/src/main/scala/org/midonet/cluster/services/c3po/translators/RouteManager.scala
+++ midonet-cluster/src/main/scala/org/midonet/cluster/services/c3po/translators/RouteManager.scala
@@ -16,27 +16,22 @@
 
 package org.midonet.cluster.services.c3po.translators
 
-import scala.collection.JavaConverters._
-
-import org.midonet.cluster.data.storage.ReadOnlyStorage
 import org.midonet.cluster.models.Commons.{IPAddress, IPSubnet, UUID}
 import org.midonet.cluster.models.Neutron.NeutronRoute
 import org.midonet.cluster.models.Topology.Dhcp.Opt121RouteOrBuilder
 import org.midonet.cluster.models.Topology.Route.NextHop
 import org.midonet.cluster.models.Topology._
 import org.midonet.cluster.rest_api.neutron.models.MetaDataService
-import org.midonet.cluster.services.c3po.NeutronTranslatorManager.{Create, Update}
+import org.midonet.cluster.services.c3po.NeutronTranslatorManager.Create
 import org.midonet.cluster.util.IPSubnetUtil.univSubnet4
 import org.midonet.cluster.util.UUIDUtil.asRichProtoUuid
 import org.midonet.cluster.util.{IPAddressUtil, IPSubnetUtil, UUIDUtil}
 import org.midonet.packets.IPv4Addr
-import org.midonet.util.concurrent.toFutureOps
 
 
 trait RouteManager {
     import RouteManager._
 
-    protected def storage: ReadOnlyStorage
     /**
      * Tests if the route is to Meta Data Server.
      * @param nextHopGw A next hop gateway to Meta Data Server.
@@ -104,23 +99,6 @@ trait RouteManager {
     protected def addLocalRouteToRouter(rPort: PortOrBuilder): OperationList = {
         List(Create(newLocalRoute(rPort.getId,
                                   rPort.getPortAddress)))
-    }
-
-    /** Set or clear the next_hop_gateway field on all routes with a gateway
-      * via the specified subnet */
-    protected def updateGatewayRoutesOps(gatewayIp: IPAddress,
-                                         subnetId: UUID): OperationList = {
-        val dhcp = storage.get(classOf[Dhcp], subnetId).await()
-        if (dhcp.getGatewayRouteIdsCount == 0) return List()
-
-        val routes = storage.getAll(classOf[Route],
-                                    dhcp.getGatewayRouteIdsList.asScala).await()
-        for (rt <- routes.toList) yield {
-            val bldr = rt.toBuilder
-            if (gatewayIp == null) bldr.clearNextHopGateway()
-            else bldr.setNextHopGateway(gatewayIp)
-            Update(bldr.build())
-        }
     }
 
     /**
