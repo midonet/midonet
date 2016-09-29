7d3d9709d970ab33e31fdd416f980cb50d61910f
Mateo Hermida <mateo@midokura.com>
2016-09-29 14:38:16 +0200

    Add jenkins version option to ci-start script
    This patch adds an option to the ci-start script so that we can define
    different start preparations for both enviroments. This is needed
    because we use two dfferent environments and slave images that have
    different requirements.
    
    Change-Id: I1154ebc13fdb5c1a51dd46118e92c69d306cf87e
    Signed-off-by: Mateo Hermida <mateo@midokura.com>
    (cherry picked from commit 04f88f1490748cb24aa080e4b1cd788c3d767807)

--- tests/ci-start.sh
+++ tests/ci-start.sh
@@ -4,8 +4,9 @@ SANDBOX_NAME="mdts"
 SANDBOX_FLAVOUR="default_v2_neutron+kilo"
 OVERRIDE="sandbox/override_v2"
 PROVISIONING="sandbox/provisioning/all-provisioning.sh"
+JENKINS_VERSION="v1"
 
-while getopts ":f:o:p:h" opt; do
+while getopts ":f:o:p:hu" opt; do
     case $opt in
     f)
         SANDBOX_FLAVOUR=$OPTARG
@@ -21,6 +22,9 @@ while getopts ":f:o:p:h" opt; do
              " [-p PROVISIONING_SCRIPT]"
         exit 1
         ;;
+    u)
+        JENKINS_VERSION="v2"
+        ;;
     esac
 done
 
@@ -35,9 +39,11 @@ sudo modprobe 8021q
 sudo apt-get install --no-install-recommends -y libssl-dev libffi-dev
 
 # Upgrade docker daemon to latest version and configure to use v2 registry in artifactory
-sudo apt-get install -qy -o Dpkg::Options::="--force-confnew" --only-upgrade docker-engine=1.12.1-0~trusty
-sudo sed -i 's/^#DOCKER_OPTS=.*/DOCKER_OPTS="--insecure-registry artifactory-v2.bcn.midokura.com"/' /etc/default/docker
-sudo service docker restart
+if [ "$JENKINS_VERSION" == "v1" ]; then
+    sudo apt-get install -qy -o Dpkg::Options::="--force-confnew" --only-upgrade docker-engine=1.12.1-0~trusty
+    sudo sed -i 's/^#DOCKER_OPTS=.*/DOCKER_OPTS="--insecure-registry artifactory-v2.bcn.midokura.com"/' /etc/default/docker
+    sudo service docker restart
+fi
 
 # create virtualenv for sandbox and mdts
 sudo pip install --upgrade pip setuptools virtualenv
