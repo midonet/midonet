2126579b1bac5bdc34d1c87df2c8208d0c6d7e68
Joe Mills <joe@midokura.com>
2016-10-05 17:00:31 +0900

    Add TC request handler
    This handler creates a new thread that will process
    TC requests and convert them to Netlink messages, and
    send those messages to the linux kernel to implement
    the traffic control configuration.
    
    Change-Id: I720f91e8483180370ef60e7da0677b6b64497c27
    Signed-off-by: Joe Mills <joe@midokura.com>

---
+++ midolman/src/test/java/org/midonet/midolman/host/services/TcRequestHandlerTest.scala
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2016 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.midolman.host.services
+
+import java.nio.ByteBuffer
+import java.util
+import java.util.concurrent.LinkedBlockingQueue
+
+import org.junit.runner.RunWith
+import org.midonet.midolman.util.MockNetlinkChannelFactory
+import org.midonet.netlink.NetlinkMessage
+import org.midonet.netlink.rtnetlink.Rtnetlink
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{BeforeAndAfterAll, FeatureSpec, Matchers}
+import org.scalatest.concurrent._
+import org.scalatest.concurrent.Eventually._
+
+case class TR(msg: Int, ifi: Int)
+
+@RunWith(classOf[JUnitRunner])
+class TcRequestHandlerTest extends FeatureSpec
+                    with BeforeAndAfterAll
+                    with Matchers
+                    with ScalaFutures {
+
+    val add = TcRequestOps.ADDFILTER
+    val rem = TcRequestOps.REMQDISC
+
+    class TestableTcRequestHandler(q: LinkedBlockingQueue[TcRequest])
+        extends TcRequestHandler(new MockNetlinkChannelFactory(), q) {
+
+        val reqs = new util.ArrayList[Int]()
+
+        override def writeRead(buf: ByteBuffer): Unit = {
+            val msgType = buf.getShort(NetlinkMessage.NLMSG_TYPE_OFFSET)
+            val offset = NetlinkMessage.NLMSG_PID_OFFSET +
+                         NetlinkMessage.NLMSG_PID_SIZE + 4
+            val ifindex = buf.getInt(offset)
+            reqs.add(msgType)
+            reqs.add(ifindex)
+            buf.clear()
+        }
+
+        def opsMatchReqs(reqList: List[TR]): Boolean = {
+            val expected = new util.ArrayList[Int]()
+            reqList foreach { tr =>
+                tr.msg match {
+                    case TcRequestOps.ADDFILTER =>
+                        expected.add(Rtnetlink.Type.NEWQDISC)
+                        expected.add(tr.ifi)
+                        expected.add(Rtnetlink.Type.NEWTFILTER)
+                        expected.add(tr.ifi)
+                    case TcRequestOps.REMQDISC =>
+                        expected.add(Rtnetlink.Type.DELQDISC)
+                        expected.add(tr.ifi)
+                }
+            }
+
+            expected equals reqs
+        }
+    }
+
+
+    feature("Handler processes requests") {
+        scenario("random requests") {
+
+            val q = new LinkedBlockingQueue[TcRequest]()
+            val handler = new TestableTcRequestHandler(q)
+
+            handler.start()
+
+            val reqs = List(
+                TR(add, 1), TR(add, 2), TR(rem, 1), TR(add, 3), TR(add, 4),
+                TR(rem, 4), TR(add, 100), TR(rem, 2))
+
+            reqs foreach { tr =>
+                q.add(new TcRequest(tr.msg, tr.ifi, 300, 200))
+            }
+
+            eventually {
+                handler.opsMatchReqs(reqs) shouldBe true
+            }
+        }
+    }
+}
+
