ef39f8c731b5ad0beb71a744af898619ce1131bb
Alex Bikfalvi <alex.bikfalvi@midokura.com>
2016-10-05 13:44:19 +0200

    Fix bridge mapper loses mirrors
    This fixes an issue in the bridge mapper, where tracking of
    referenced mirrors was not done correctly, because the outbound
    mirrors were overwriting the tracked references for the inbound
    mirrors.
    
    The patch also adds a mirrors unit test.
    
    Change-Id: I5fc1d812c35fc5caa43af2c3aa373b18571385f3
    Signed-off-by: Alex Bikfalvi <alex.bikfalvi@midokura.com>
    (cherry picked from commit 888062c9e02a4ace2b757561114e7c5bc036d840)

--- midolman/src/main/scala/org/midonet/midolman/topology/BridgeMapper.scala
+++ midolman/src/main/scala/org/midonet/midolman/topology/BridgeMapper.scala
@@ -496,8 +496,10 @@ final class BridgeMapper(bridgeId: UUID, implicit override val vt: VirtualTopolo
             if (bridge.hasInboundFilterId) bridge.getInboundFilterId else null,
             if (bridge.hasOutboundFilterId) bridge.getOutboundFilterId else null)
 
-        mirrorsTracker.requestRefs(bridge.getInboundMirrorIdsList.asScala map (_.asJava) :_*)
-        mirrorsTracker.requestRefs(bridge.getOutboundMirrorIdsList.asScala map (_.asJava) :_*)
+        // Request the mirrors for this bridge.
+        mirrorsTracker.requestRefs(
+            bridge.getInboundMirrorIdsList.asScala.map(_.asJava) ++
+            bridge.getOutboundMirrorIdsList.asScala.map(_.asJava) :_*)
     }
 
     /**
