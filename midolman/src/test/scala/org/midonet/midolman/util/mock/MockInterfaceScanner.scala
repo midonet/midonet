b82de3e202563f04eabec64c9d628b35f054ae80
Joe Mills <joe@midokura.com>
2016-10-12 12:01:55 +0900

    Add QoS Service
    This is a new host service that runs on all midolman hosts
    and translates QoS configurations to linux TC netlink requests.
    
    It works by listening to InterfaceScanner for the information
    on currently UP interfaces on the local host. It combines this
    information with the topology information about the ports that
    are currently bound to interfaces on the local host. If a match
    is found (i.e. there is a port bound to a local interface that is
    UP and has QoS policy information configured) it will pass on the
    request to a thread that passes messages on to the kernel.
    
    Signed-off-by: Joe Mills <joe@midokura.com>
    Change-Id: Id4acbd1b45cf7f5fc9b5180f5a0635555c8fec0e

--- midolman/src/test/scala/org/midonet/midolman/util/mock/MockInterfaceScanner.scala
+++ midolman/src/test/scala/org/midonet/midolman/util/mock/MockInterfaceScanner.scala
@@ -20,7 +20,7 @@ import java.nio.ByteBuffer
 import scala.collection.mutable
 
 import rx.subjects.BehaviorSubject
-import rx.{Observable, Observer, Subscription}
+import rx.{Observable, Observer, Scheduler, Subscription}
 
 import org.midonet.midolman.host.interfaces.InterfaceDescription
 import org.midonet.midolman.host.scanner.InterfaceScanner
@@ -52,7 +52,8 @@ class MockInterfaceScanner extends InterfaceScanner {
             _ => interfaceDescriptions.values.toSet))
 
     override
-    def subscribe(obs: Observer[Set[InterfaceDescription]]): Subscription = {
+    def subscribe(obs: Observer[Set[InterfaceDescription]],
+                  scheduler: Option[Scheduler] = None): Subscription = {
         val subscription: Subscription = notifications.subscribe(obs)
         obs.onNext(interfaceDescriptions.values.toSet)
         subscription
