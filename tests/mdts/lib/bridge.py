d659704f7f94e47d92708eed26f542a55563aed1
Michael Micucci <micucci@midokura.com>
2016-10-20 12:53:14 +0900

    Added QoS MDTS lib and tests
    Added QOS policy to VTM and as a model object for MDTS to use.
    Added various functions to parse the QOS policy info from a
    topology YAML file as well as add the obejcts programatically.
    
    Joe Mills <joe@midokura.com> supplied the original code base for
    the MDTS lib changes.
    
    Verify bandwiodth function is just a stub for now, to be filled
    out in a subsequent patch.
    
    For QOS policy tests, used test plan:
    
    https://docs.google.com/a/midokura.com/document/d/1f9_I2tdKTN9XWzUh4qKjlolr4vww7gJAAU3g1zH4HXg/edit?usp=sharing
    
    Change-Id: I421dc9952c867acb8ba732121da4709960c9eae4

--- tests/mdts/lib/bridge.py
+++ tests/mdts/lib/bridge.py
@@ -38,7 +38,7 @@ class Bridge(ResourceBase):
         """ Builds a bridge MidoNet resource.
 
         On building, if filter names are specified, it looks up Chain data via
-        VirtualTopologyManager and assign them to this bridge. 
+        VirtualTopologyManager and assign them to this bridge.
         """
         tenant_id = self._get_tenant_id()
         self._mn_resource = self._api.add_bridge()
@@ -53,6 +53,11 @@ class Bridge(ResourceBase):
             if filter_field in self._data:
                 self._context.look_up_resource(
                         self._mn_resource, filter_field, self._data[filter_field])
+
+        if 'qos_policy' in self._data:
+            qp_name = self._data['qos_policy']
+            qp_id = self._context._qos_policies[qp_name].get_id()
+            self._mn_resource.qos_policy_id(qp_id)
 
         self._mn_resource.create()
 
@@ -104,3 +109,11 @@ class Bridge(ResourceBase):
 
     def get_outbound_filter(self):
         return self._outbound_filter
+
+    def set_qos_policy(self, qos_policy):
+        self._mn_resource.qos_policy_id(qos_policy.get_id())
+        self.update()
+
+    def clear_qos_policy(self):
+        self._mn_resource.qos_policy_id(None)
+        self.update()
