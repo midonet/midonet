5f7a1558365e3e33a02d851dd2e447f4b8f945b1
Joe Mills <joe@midokura.com>
2016-11-11 18:05:20 +0900

    Start of haproxy-helper script
    The rest of the functionality and tests need to
    filled out.
    
    Change-Id: I2307130c6f9325d4b6baea033517089114322537
    Signed-off-by: Joe Mills <joe@midokura.com>

---
+++ midolman/src/main/scala/org/midonet/midolman/haproxy/HaproxyHelper.scala
@@ -0,0 +1,20 @@
+package org.midonet.midolman.haproxy
+
+import scala.sys.process._
+
+class HaproxyHelper(haproxyScript: String) {
+
+    def makens(name: String, iface: String, mac: String, ip: String,
+               routerIp: String) = {
+        s"$haproxyScript makens $name $iface $mac $ip $routerIp".!!
+    }
+
+    def cleanns(name: String, iface: String) = {
+        try {
+            s"$haproxyScript cleanns $name $iface".!!
+        } catch {
+            case e: RuntimeException =>
+            // expected if the namespace is already cleaned
+        }
+    }
+}
