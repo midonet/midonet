c7f4bb89598a3d266ea1fbb8fcefdce3712fc1e8
Michael Micucci <micucci@midokura.com>
2016-11-15 14:54:09 +0900

    LBaaS V2 Neutron API endpoints
    Add top-level endpoints and models for each neutron resource.
    
    Change-Id: I5476bcd56af426f13dafc27da469542173598f6e

---
+++ midonet-cluster/src/test/java/org/midonet/cluster/rest_api/neutron/TestPoolV2Resource.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2015 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.cluster.rest_api.neutron;
+
+import java.util.UUID;
+
+import javax.ws.rs.core.Response;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import org.midonet.cluster.rest_api.ConflictHttpException;
+import org.midonet.cluster.rest_api.NotFoundHttpException;
+import org.midonet.cluster.rest_api.neutron.models.PoolV2;
+import org.midonet.cluster.rest_api.neutron.resources.PoolV2Resource;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TestPoolV2Resource extends LoadBalancerV2TestBase {
+
+    private PoolV2Resource testPoolObject;
+
+    @Before
+    public void setUp() throws Exception {
+
+        super.setUp();
+
+        testPoolObject = new PoolV2Resource(uriInfo, plugin);
+    }
+
+    @Test
+    public void testPoolCreate() throws Exception {
+
+        PoolV2 input = pool();
+        PoolV2 output = pool(input.id);
+
+        doReturn(output).when(plugin).createPoolV2(input);
+
+        Response resp = testPoolObject.create(input);
+
+        assertCreate(resp, output,
+                NeutronUriBuilder.getPoolV2(BASE_URI, input.id));
+    }
+
+    @Test(expected = ConflictHttpException.class)
+    public void testPoolCreateConflict() throws Exception {
+
+        doThrow(ConflictHttpException.class).when(plugin).createPoolV2(
+                any(PoolV2.class));
+
+        testPoolObject.create(new PoolV2());
+    }
+
+    @Test(expected = NotFoundHttpException.class)
+    public void testPoolGetNotFound() throws Exception {
+
+        doReturn(null).when(plugin).getPoolV2(any(UUID.class));
+
+        testPoolObject.get(UUID.randomUUID());
+    }
+
+    @Test
+    public void testPoolUpdate() throws Exception {
+
+        PoolV2 input = pool();
+        PoolV2 output = pool(input.id);
+
+        doReturn(output).when(plugin).updatePoolV2(input.id, input);
+
+        Response resp = testPoolObject.update(input.id, input);
+
+        assertUpdate(resp, output);
+    }
+
+    @Test(expected = NotFoundHttpException.class)
+    public void testPoolUpdateNotFound() throws Exception {
+
+        doThrow(NotFoundHttpException.class).when(plugin).updatePoolV2(
+                any(UUID.class), any(PoolV2.class));
+
+        testPoolObject.update(any(UUID.class), any(PoolV2.class));
+    }
+}
