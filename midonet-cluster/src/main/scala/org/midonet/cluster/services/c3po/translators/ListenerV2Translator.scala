f243ef0756d598167f46e8138f19035ad816d55b
Michael Micucci <micucci@midokura.com>
2016-11-29 17:14:25 +0900

    Add translations for ListenerV2 object
    Signed-off-by: Michael Micucci <micucci@midokura.com>
    Change-Id: Ied38a645f7f1518b96cd7b1f650af900c16fbac9

---
+++ midonet-cluster/src/main/scala/org/midonet/cluster/services/c3po/translators/ListenerV2Translator.scala
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2016 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.cluster.services.c3po.translators
+
+import scala.collection.JavaConverters._
+
+import org.midonet.cluster.data.storage.Transaction
+import org.midonet.cluster.models.Commons.UUID
+import org.midonet.cluster.models.Neutron.{NeutronLoadBalancerV2Listener, NeutronPort}
+import org.midonet.cluster.models.Topology._
+import org.midonet.cluster.services.c3po.NeutronTranslatorManager.Operation
+
+/** Provides a Neutron model translator for NeutronLoadBalancerV2Listener. */
+class ListenerV2Translator
+    extends Translator[NeutronLoadBalancerV2Listener]
+            with LoadBalancerManager
+            with PortManager {
+
+    import PortManager.routerInterfacePortPeerId
+
+    override protected def retainHighLevelModel(tx: Transaction,
+                                                op: Operation[NeutronLoadBalancerV2Listener])
+    : List[Operation[NeutronLoadBalancerV2Listener]] = List()
+
+
+    override protected def translateCreate(tx: Transaction,
+                                           nL: NeutronLoadBalancerV2Listener)
+    : Unit = {
+        if (nL.getLoadbalancersCount != 1) {
+            throw new IllegalArgumentException(
+                "Listener must have one and only one load balancer ID")
+        }
+
+        val bldr = Vip.newBuilder
+        bldr.setAdminStateUp(nL.getAdminStateUp)
+        bldr.setId(nL.getId)
+        if (nL.hasDefaultPoolId) {
+            bldr.setPoolId(nL.getDefaultPoolId)
+        }
+        bldr.setProtocolPort(nL.getProtocolPort)
+
+        val lbRouter = tx.get(classOf[Router], lbV2RouterId(nL.getLoadbalancers(0)))
+
+        val vipPeerPortId = lbRouter.getPortIdsList.asScala.find { id =>
+            tx.exists(classOf[NeutronPort], routerInterfacePortPeerId(id))
+        }.getOrElse {
+            throw new IllegalStateException(
+                "LB router has no router interface port which links " +
+                "to the VIP port")
+        }
+
+        bldr.setAddress(tx.get(classOf[Port], vipPeerPortId).getPortAddress)
+        tx.create(bldr.build())
+    }
+
+    override protected def translateDelete(tx: Transaction,
+                                           id: UUID)
+    : Unit = {
+        tx.delete(classOf[Vip], id, ignoresNeo = true)
+    }
+
+    override protected def translateUpdate(tx: Transaction,
+                                           nL: NeutronLoadBalancerV2Listener)
+    : Unit = {
+        val oldBldr = tx.get(classOf[Vip], nL.getId).toBuilder
+        oldBldr.setAdminStateUp(nL.getAdminStateUp)
+        oldBldr.setPoolId(nL.getDefaultPoolId)
+        tx.update(oldBldr.build())
+    }
+
+}
