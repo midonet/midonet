f067b4280491116e84889d83d59d9fc343397e17
Joe Mills <joe@midokura.com>
2016-12-01 16:28:38 +0900

    Add more complex haproxy config ability
    The current "PoolConfig" class only has the ability
    to write a config for a single pool with a single vip.
    
    For LBaaSv2, we need the ability to support multiple
    pools, vips, and health monitors.
    
    Change-Id: I373e17d6c8deea6c30347af6ffbf13d80aebe7d8
    Signed-off-by: Joe Mills <joe@midokura.com>

--- midolman/src/integration/scala/org/midonet/midolman/haproxy/HaproxyHelperTest.scala
+++ midolman/src/integration/scala/org/midonet/midolman/haproxy/HaproxyHelperTest.scala
@@ -19,8 +19,7 @@ package org.midonet.midolman.haproxy
 import java.util.UUID
 
 import org.junit.runner.RunWith
-import org.midonet.midolman.l4lb.{HealthMonitorConfig, PoolConfig, PoolMemberConfig, VipConfig}
-import org.midonet.midolman.state.l4lb.SessionPersistence
+import org.midonet.midolman.l4lb._
 import org.scalatest.concurrent.Eventually
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.{FeatureSpec, ShouldMatchers}
@@ -63,43 +62,65 @@ class HaproxyHelperTest extends FeatureSpec
         }
     }
 
-    def verifyConfFile(poolConfig: PoolConfig, confFile: String): Unit = {
+    def verifyConfFile(lbConfig: LoadBalancerV2Config,
+                       confFile: String): Unit = {
         eventually {
             val contents = s"cat $confFile".!!
-            for (member <- poolConfig.members) {
-                contents should include (member.address)
+            for (pool <- lbConfig.pools) {
+                for (member <- pool.members) {
+                    contents should include (member.address)
+                }
             }
-            contents should include (poolConfig.vip.id.toString)
+
+            for (vip <- lbConfig.vips) {
+                contents should include (vip.id.toString)
+            }
         }
     }
 
-    def verifyStatus(haproxy: HaproxyHelper, poolConfig: PoolConfig): Unit = {
+    def verifyStatus(haproxy: HaproxyHelper,
+                     lbConfig: LoadBalancerV2Config): Unit = {
         eventually {
             val (upNodes, downNodes) = haproxy.getStatus
-            val memberIds = poolConfig.members map {m => m.id}
-            (upNodes ++ downNodes) should contain theSameElementsAs memberIds
+            val memberIds = lbConfig.pools.flatMap(p => p.members)
+                                          .map (_.id.toString)
+            val nodes = (upNodes++downNodes) map (_.toString)
+            nodes should contain theSameElementsAs memberIds
         }
     }
 
-    val vipConfig = new VipConfig(true, UUID.randomUUID(), "10.0.0.10", 80,
-                                  SessionPersistence.SOURCE_IP)
-    val healthMonitor = new HealthMonitorConfig(true, 1, 1, 1)
-    val member1 = new PoolMemberConfig(true, UUID.randomUUID(), 100,
-                                       "10.0.0.1", 80)
-    val member2 = new PoolMemberConfig(true, UUID.randomUUID(), 100,
-                                       "10.0.0.2", 80)
-    val member3 = new PoolMemberConfig(true, UUID.randomUUID(), 100,
-                                       "10.0.0.3", 80)
-    val pool1 = new PoolConfig(UUID.randomUUID(), UUID.randomUUID(),
-                              Set(vipConfig), Set(member1, member2),
-                              healthMonitor, true, "", "")
-    val pool2 = new PoolConfig(UUID.randomUUID(), UUID.randomUUID(),
-                               Set(vipConfig), Set(member1, member2),
-                               healthMonitor, true, "", "")
-    val pool1Updated = new PoolConfig(pool1.id, pool1.loadBalancerId,
-                                     Set(vipConfig),
-                                     Set(member1, member2, member3),
-                                     healthMonitor, true, "", "")
+    val healthMonitor = HealthMonitorV2Config(UUID.randomUUID(), true, 1, 1, 1)
+
+    val member1 = MemberV2Config(UUID.randomUUID(), true, "10.0.0.1", 80)
+    val member2 = MemberV2Config(UUID.randomUUID(), true, "10.0.0.2", 80)
+    val member3 = MemberV2Config(UUID.randomUUID(), true, "10.0.0.3", 80)
+    val member4 = MemberV2Config(UUID.randomUUID(), true, "10.0.0.4", 80)
+    val member5 = MemberV2Config(UUID.randomUUID(), true, "10.0.0.5", 80)
+
+    val pool1 = PoolV2Config(UUID.randomUUID(), true, Set(member1, member2),
+                             healthMonitor)
+    val pool2 = PoolV2Config(UUID.randomUUID(), true, Set(member4, member5),
+                             healthMonitor)
+
+    val pool1Updated = PoolV2Config(pool1.id, true,
+                                    Set(member1, member2, member3),
+                                    healthMonitor)
+
+    val listener1 = ListenerV2Config(UUID.randomUUID(), true, 80, pool1.id)
+    val listener2 = ListenerV2Config(UUID.randomUUID(), true, 80, pool2.id)
+
+    val lb1 = new LoadBalancerV2Config(UUID.randomUUID(), Set(listener1),
+                                       Set(pool1), true)
+    val lb2 = new LoadBalancerV2Config(UUID.randomUUID(),
+                                       Set(listener1, listener2),
+                                       Set(pool1, pool2), true)
+
+    val lb1Updated = new LoadBalancerV2Config(lb1.id, Set(listener1),
+                                              Set(pool1Updated), true)
+
+    val lbMultiPool = new LoadBalancerV2Config(lb1.id,
+                                               Set(listener1, listener2),
+                                               Set(pool1, pool2), true)
 
     val haproxyScript = "../midolman/src/lib/midolman/service_containers/haproxy/haproxy-helper"
 
@@ -107,21 +128,21 @@ class HaproxyHelperTest extends FeatureSpec
     feature("deploys haproxy") {
         scenario("haproxy is started and restarted in a namespace") {
             val ifaceName = "iface"
-            val nsName = namespaceName(pool1.id.toString)
+            val nsName = namespaceName(lb1.id.toString)
             val haproxy = new HaproxyHelper(haproxyScript)
             try {
-                haproxy.deploy(pool1, ifaceName, "50:46:5d:a3:6d:f4",
+                haproxy.deploy(lb1, ifaceName, "50:46:5d:a3:6d:f4",
                                "20.0.0.1", "20.0.0.2")
                 verifyIpNetns(nsName, ifaceName)
                 verifyHaproxyRunning(nsName)
-                verifyConfFile(pool1, haproxy.confLoc)
-                verifyStatus(haproxy, pool1)
+                verifyConfFile(lb1, haproxy.confLoc)
+                verifyStatus(haproxy, lb1)
 
-                haproxy.restart(pool1Updated)
+                haproxy.restart(lb1Updated)
                 verifyIpNetns(nsName, ifaceName)
                 verifyHaproxyRunning(nsName)
-                verifyConfFile(pool1Updated, haproxy.confLoc)
-                verifyStatus(haproxy, pool1Updated)
+                verifyConfFile(lb1Updated, haproxy.confLoc)
+                verifyStatus(haproxy, lb1Updated)
             } finally {
                 haproxy.undeploy(nsName, ifaceName)
                 verifyNoIpNetns(nsName, ifaceName)
@@ -129,42 +150,59 @@ class HaproxyHelperTest extends FeatureSpec
         }
 
         scenario("two separate deployments do not interfere with each other") {
-            val name1 = namespaceName(pool1.id.toString)
+            val name1 = namespaceName(lb1.id.toString)
             val ifaceName1 = "iface1"
-            val name2 = namespaceName(pool2.id.toString)
+            val name2 = namespaceName(lb2.id.toString)
             val ifaceName2 = "iface2"
             val haproxy1 = new HaproxyHelper(haproxyScript)
             val haproxy2 = new HaproxyHelper(haproxyScript)
             try {
-                haproxy1.deploy(pool1, ifaceName1, "50:46:5d:a3:6d:f4",
+                haproxy1.deploy(lb1, ifaceName1, "50:46:5d:a3:6d:f4",
                                 "20.0.0.1", "20.0.0.2")
-                haproxy2.deploy(pool2, ifaceName2, "50:46:5d:a3:6d:f4",
+                haproxy2.deploy(lb2, ifaceName2, "50:46:5d:a3:6d:f4",
                                 "20.0.0.1", "20.0.0.2")
                 verifyIpNetns(name1, ifaceName1)
                 verifyIpNetns(name2, ifaceName2)
                 verifyHaproxyRunning(name1)
                 verifyHaproxyRunning(name2)
-                verifyConfFile(pool1, haproxy1.confLoc)
-                verifyConfFile(pool2, haproxy2.confLoc)
-                verifyStatus(haproxy1, pool1)
-                verifyStatus(haproxy2, pool2)
+                verifyConfFile(lb1, haproxy1.confLoc)
+                verifyConfFile(lb2, haproxy2.confLoc)
+                verifyStatus(haproxy1, lb1)
+                verifyStatus(haproxy2, lb2)
 
-                haproxy1.restart(pool1Updated)
+                haproxy1.restart(lb1Updated)
 
                 verifyIpNetns(name1, ifaceName1)
                 verifyIpNetns(name2, ifaceName2)
                 verifyHaproxyRunning(name1)
                 verifyHaproxyRunning(name2)
-                verifyConfFile(pool1Updated, haproxy1.confLoc)
-                verifyConfFile(pool2, haproxy2.confLoc)
-                verifyStatus(haproxy1, pool1Updated)
-                verifyStatus(haproxy2, pool2)
+                verifyConfFile(lb1Updated, haproxy1.confLoc)
+                verifyConfFile(lb2, haproxy2.confLoc)
+                verifyStatus(haproxy1, lb1Updated)
+                verifyStatus(haproxy2, lb2)
             } finally {
                 haproxy1.undeploy(name1, ifaceName1)
                 verifyNoIpNetns(name1, ifaceName1)
 
                 haproxy2.undeploy(name2, ifaceName2)
                 verifyNoIpNetns(name2, ifaceName2)
+            }
+        }
+
+        scenario("multiple pools per load balancer") {
+            val name = namespaceName(lb1.id.toString)
+            val ifaceName = "iface1"
+            val haproxy = new HaproxyHelper(haproxyScript)
+            try {
+                haproxy.deploy(lbMultiPool, ifaceName, "50:46:5d:a3:6d:f4",
+                               "20.0.0.1", "20.0.0.2")
+                verifyIpNetns(name, ifaceName)
+                verifyHaproxyRunning(name)
+                verifyConfFile(lbMultiPool, haproxy.confLoc)
+                verifyStatus(haproxy, lbMultiPool)
+            } finally {
+                haproxy.undeploy(name, ifaceName)
+                verifyNoIpNetns(name, ifaceName)
             }
         }
     }
---
+++ midolman/src/main/scala/org/midonet/midolman/l4lb/LoadBalancerV2Config.scala
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2014 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.midonet.midolman.l4lb
+
+import java.util.UUID
+
+import scala.collection.immutable.Set
+
+case class ListenerV2Config(id: UUID, adminStateUp: Boolean, port: Int,
+                            poolId: UUID)
+
+case class MemberV2Config(id: UUID, adminStateUp: Boolean, address: String,
+                          port: Int)
+
+case class HealthMonitorV2Config(id: UUID, adminStateUp: Boolean, delay: Int,
+                                 timeout: Int, maxRetries: Int)
+
+case class PoolV2Config(id: UUID, adminStateUp: Boolean,
+                        members: Set[MemberV2Config],
+                        healthMonitor: HealthMonitorV2Config)
+
+class LoadBalancerV2Config(val id: UUID,
+                           val vips: Set[ListenerV2Config],
+                           val pools: Set[PoolV2Config],
+                           val adminStateUp: Boolean) {
+
+    def generateConfigFile(sockFile: String): String = {
+        val conf = new StringBuilder()
+        conf append
+            s"""
+                |global
+                |    daemon
+                |    user nobody
+                |    group daemon
+                |    log /dev/log local0
+                |    log /dev/log local1 notice
+                |    stats socket $sockFile mode 0666 level user
+                |defaults
+                |    log global
+                |    retries 3
+                |    timeout connect 5000
+                |    timeout client 5000
+                |    timeout server 5000
+                |""".stripMargin
+
+        vips filter (_.adminStateUp) foreach { v =>
+            conf append
+                s"""
+                    |frontend ${v.id}
+                    |    option tcplog
+                    |    bind *:${v.port}
+                    |    mode tcp
+                    |    default_backend ${v.poolId}
+                    |""".stripMargin
+        }
+        pools filter (_.adminStateUp) foreach { p =>
+            conf append
+                s"""
+                    |backend ${p.id}
+                    |    timeout check ${p.healthMonitor.timeout}
+                    |""".stripMargin
+
+            p.members filter (_.adminStateUp) foreach { m =>
+                val hm = p.healthMonitor
+                conf append
+                    s"""
+                       |    server ${m.id} ${m.address}:${m.port} check inter ${hm.delay}s fall ${hm.maxRetries}
+                       |""".stripMargin
+            }
+        }
+        conf.toString()
+    }
+}
