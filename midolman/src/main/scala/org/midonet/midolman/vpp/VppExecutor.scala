2635478f081d3b3fbfbbc4f3995d47c3b42465e4
Alex Bikfalvi <alex.bikfalvi@midokura.com>
2016-12-02 17:24:55 +0100

    FIP64: Add a VPP executor base class
    This is a first step towards making the VPP controller a service
    using a single thread executor as opposed to being an actor.
    
    Change-Id: I57096a6238d7e7a5f7664e241097a193b4cbdf7b
    Signed-off-by: Alex Bikfalvi <alex.bikfalvi@midokura.com>

---
+++ midolman/src/main/scala/org/midonet/midolman/vpp/VppExecutor.scala
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2016 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.midolman.vpp
+
+import java.util.concurrent.ExecutorService
+
+import scala.concurrent.{ExecutionContext, Future, Promise}
+import scala.util.control.NonFatal
+
+import com.google.common.util.concurrent.AbstractService
+
+import org.midonet.midolman.vpp.VppExecutor.Receive
+import org.midonet.util.concurrent.{ConveyorBelt, Executors}
+import org.midonet.util.functors._
+import org.midonet.util.logging.Logging
+
+object VppExecutor {
+
+    type Receive = PartialFunction[Any, Future[Any]]
+
+    object Nothing extends Receive {
+        def isDefinedAt(x: Any): Boolean = false
+        def apply(x: Any): Future[Any] =
+            throw new UnsupportedOperationException("Not supported")
+    }
+
+}
+
+abstract class VppExecutor extends AbstractService with Logging {
+
+    protected val executor = newExecutor
+    protected val ec = ExecutionContext.fromExecutor(executor)
+
+    private val belt = new ConveyorBelt(t => {
+        log.error("Error on conveyor belt", t)
+    })
+
+    protected def newExecutor: ExecutorService = {
+        Executors.singleThreadScheduledExecutor(
+            "vpp-controller", isDaemon = true, Executors.CallerRunsPolicy)
+    }
+
+    /**
+      * Handles messages on the service executor. An overriding class must
+      * implement a partial function handling the received message, and return
+      * a future that completes when the handling the message has finished.
+      */
+    protected def receive: Receive
+
+    /**
+      * Sends a message to execute on the VPP executor thread. The message
+      * must be handled by the [[receive]] method, which will return a
+      * future that completes when the handling of the message has finished.
+      *
+      * The handling of messages is serialized such that a new message will not
+      * be processed until the future returned by a previous one has completed.
+      */
+    protected def send(message: Any): Future[Any] = {
+        if (!isRunning) {
+            return Future.failed(new IllegalStateException("Service not started"))
+        }
+        val promise = Promise[Any]
+        executor.execute(makeRunnable {
+            if (receive.isDefinedAt(message)) {
+                belt.handle(() => {
+                    val future =
+                        try receive.apply(message)
+                        catch { case NonFatal(e) => Future.failed(e) }
+                    promise tryCompleteWith future
+                    future
+                })
+            } else {
+                val error = s"Unhandled message $message"
+                log warn error
+                promise.tryFailure(new UnsupportedOperationException(error))
+            }
+        })
+        promise.future
+    }
+
+    override def doStop(): Unit = {
+        Executors.shutdown(executor) { _ =>
+            log warn s"Exception while stopping VPP controller executor"
+        }
+    }
+
+}
---
+++ midolman/src/test/scala/org/midonet/midolman/vpp/VppExecutorTest.scala
@@ -0,0 +1,170 @@
+/*
+ * Copyright 2016 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.midolman.vpp
+
+import scala.async.Async
+import scala.concurrent.Future
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{FeatureSpec, GivenWhenThen, Matchers}
+
+import org.midonet.midolman.vpp.VppExecutor.Receive
+import org.midonet.util.concurrent._
+
+@RunWith(classOf[JUnitRunner])
+class VppExecutorTest extends FeatureSpec with Matchers with GivenWhenThen {
+
+    private class TestableVppExecutor(override val receive: Receive)
+        extends VppExecutor {
+
+        def !(message: Any): Future[Any] = send(message)
+
+        override def doStart(): Unit = {
+            notifyStarted()
+        }
+
+        override def doStop(): Unit = {
+            super.doStop()
+            notifyStopped()
+        }
+    }
+
+    feature("VPP executor handles messages") {
+        scenario("VPP executor starts and stops") {
+            Given("A VPP executor")
+            val vpp = new TestableVppExecutor(VppExecutor.Nothing)
+
+            When("Starting the VPP executor")
+            vpp.startAsync().awaitRunning()
+
+            Then("The VPP executor should be running")
+            vpp.isRunning shouldBe true
+
+            When("Stopping the VPP executor")
+            vpp.stopAsync().awaitTerminated()
+
+            Then("The VPP executor should be stopped")
+            vpp.isRunning shouldBe false
+        }
+
+        scenario("VPP executor manages unhandled messages") {
+            Given("A VPP executor")
+            val vpp = new TestableVppExecutor(VppExecutor.Nothing)
+            vpp.startAsync().awaitRunning()
+
+            When("Sending a message")
+            val future = vpp ! 1
+
+            Then("The message should execute")
+            intercept[UnsupportedOperationException] { future.await() }
+
+            vpp.stopAsync().awaitTerminated()
+        }
+
+        scenario("VPP executor manages handled messages") {
+            Given("A message handler")
+            val handler: Receive = {
+                case n: Int => Future.successful(2 * n)
+                case s: String => Future.successful(s.reverse)
+            }
+
+            And("A VPP executor")
+            val vpp = new TestableVppExecutor(handler)
+            vpp.startAsync().awaitRunning()
+
+            When("Sending an integer")
+            val future1 = vpp ! 11
+
+            Then("The message should execute")
+            future1.await() shouldBe 22
+
+            When("Sending a string")
+            val future2 = vpp ! "test"
+
+            Then("The message should execute")
+            future2.await() shouldBe "tset"
+
+            When("Sending an unhandled message")
+            val future3 = vpp ! new Object
+
+            Then("The message should execute")
+            intercept[UnsupportedOperationException] { future3.await() }
+
+            vpp.stopAsync().awaitTerminated()
+        }
+
+        scenario("VPP executor does not handle messages if not started") {
+            Given("A VPP executor")
+            val vpp = new TestableVppExecutor(VppExecutor.Nothing)
+
+            When("Sending a message")
+            val future = vpp ! 1
+
+            Then("The message should fail")
+            intercept[IllegalStateException] { future.await() }
+        }
+
+        scenario("Message execution should be serialized") {
+            Given("A message handler where messages complete on demand")
+            import scala.concurrent.ExecutionContext.Implicits.global
+
+            @volatile var result = Seq[Int]()
+            val handler: Receive = {
+                case value: Int =>
+                    Async.async {
+                        result = result :+ value
+                        value
+                    }
+            }
+
+            And("A VPP executor")
+            val vpp = new TestableVppExecutor(handler)
+            vpp.startAsync().awaitRunning()
+
+            When("Sending several messages")
+            val futures = for (index <- 1 until 10) yield {
+                vpp ! index
+            }
+
+            Then("The futures should complete")
+            Future.sequence(futures).await()
+
+            And("The messages should execute in sequence")
+            result shouldBe (1 until 10)
+        }
+
+        scenario("VPP executor manages mis-behaving handlers") {
+            Given("A message handler")
+            val e = new Exception()
+            val handler: Receive = {
+                case _ => throw e
+            }
+
+            And("A VPP executor")
+            val vpp = new TestableVppExecutor(handler)
+            vpp.startAsync().awaitRunning()
+
+            When("Sending a message")
+            val future = vpp ! 0
+
+            Then("The executor should handle the exception")
+            intercept[Exception] { future.await() } shouldBe e
+        }
+    }
+
+}
