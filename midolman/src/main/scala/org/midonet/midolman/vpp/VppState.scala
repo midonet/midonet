420b5e2d6dea9da2aa8353f0c9f4ba0fa5b3b178
Alex Bikfalvi <alex.bikfalvi@midokura.com>
2016-12-12 10:49:14 +0100

    FIP64: Allocate the NAT pool between multiple uplinks
    This modifies the VPP state trait to track the group ports for
    every uplink port, and use this information to allocate a partition
    of a NAT pool whenever configuring a FIP64. The partition
    allocation is deterministic, and therefore for the same uplink
    topology, the trait returns the same allocated NAT pool for a
    given FIP.
    
    This implementation performs a crude mapping between the tenant
    router port where a FIP64 is configured and the uplink ports,
    under the following assumptions:
    
    * For a given tenant router port where a FIP64 is associated,
    that port is able to reach any of the configured uplink ports,
    and therefore the NAT pool allocation should consider that port.
    
    * We support only one uplink port per physical gateway, and the
    NAT pool allocation algorithm considers the stateful port groups
    of that port alone. The NAT pool will be deterministically divided
    between all the ports in the stateful port groups of that uplink
    port.
    
    * If there is no uplink port, or if there are more than one, we
    don't support the NAT pool allocation.
    
    Ref: MI-1783
    
    Change-Id: I5a010b413feea86b71494a082ccff756d85f484c
    Signed-off-by: Alex Bikfalvi <alex.bikfalvi@midokura.com>

--- midolman/src/main/scala/org/midonet/midolman/vpp/VppState.scala
+++ midolman/src/main/scala/org/midonet/midolman/vpp/VppState.scala
@@ -16,26 +16,95 @@
 
 package org.midonet.midolman.vpp
 
-import java.util.UUID
+import java.util
+import java.util.{Collections, UUID, List => JList}
 
+import javax.annotation.concurrent.NotThreadSafe
+
+import com.google.common.collect.ImmutableList
+
+import org.midonet.cluster.util.UUIDUtil
 import org.midonet.midolman.rules.NatTarget
+import org.midonet.midolman.vpp.VppState.UplinkState
 import org.midonet.packets.IPv4Addr
+import org.midonet.util.logging.Logging
 
-private[vpp] trait VppState {
+object VppState {
+
+    case class UplinkState(groupPortIds: JList[UUID])
+
+}
+
+private[vpp] trait VppState extends Logging {
+
+    private val uplinks = new util.HashMap[UUID, UplinkState]
+
+    /**
+      * Adds an uplink port.
+      */
+    @NotThreadSafe
+    protected def addUplink(portId: UUID, groupPortIds: JList[UUID]): Unit = {
+        uplinks.put(portId, UplinkState(groupPortIds))
+    }
+
+    /**
+      * Removes an uplink port.
+      */
+    @NotThreadSafe
+    protected def removeUplink(portId: UUID): Unit = {
+        uplinks.remove(portId)
+    }
+
+    /**
+      * Returns the NAT64 pool for the specified tenant router port with the
+      * given NAT pool. The method splits the original NAT pool between all
+      * uplink ports that are reachable from the given tenant router port.
+      * If none of the uplinks are reachable from this tenant router port,
+      * the method returns [[None]].
+      */
+    @NotThreadSafe
+    protected def poolFor(portId: UUID, natPool: NatTarget)
+    : Option[NatTarget] = {
+        // TODO: Assume that there is only one uplink port per physical
+        // TODO: gateway and the uplink is reachable from this tenant router
+        // TODO: port. Further work should observer the virtual topology between
+        // TODO: the uplink and the tenant router and update reachablity.
+
+        if (uplinks.isEmpty) {
+            log warn s"No uplink ports: ignoring FIP64 for port $portId"
+            None
+        } else if (uplinks.size() > 1) {
+            log warn "Multiple uplinks per physical gateway not supported: " +
+                     s"ignoring FIP64 for port $portId"
+            None
+        } else {
+            val uplinkEntry = uplinks.entrySet().iterator().next()
+            val uplinkPortId = uplinkEntry.getKey
+            val groupPortIds = uplinkEntry.getValue.groupPortIds
+            Some(splitPool(natPool, uplinkPortId, groupPortIds))
+        }
+    }
 
     /**
       * Deterministically splits the given NAT pool across the given number of
       * uplink ports, and returns the NAT pool corresponding to the current
       * uplink port.
       */
-    def splitPool(natPool: NatTarget, portId: UUID, portIds: Seq[UUID])
-    : NatTarget = {
+    protected def splitPool(natPool: NatTarget, portId: UUID,
+                            portIds: JList[UUID]): NatTarget = {
         val poolStart = natPool.nwStart.toInt
         val poolEnd = natPool.nwEnd.toInt + 1
         val poolRange = poolEnd - poolStart
 
-        val index = portIds.sorted.indexOf(portId)
-        val count = portIds.size
+        // For performance, do not sort if the array is already ordered.
+        val orderedPortIds = if (!UUIDUtil.Ordering.isOrdered(portIds)) {
+            val array = portIds.toArray(new Array[UUID](portIds.size()))
+            util.Arrays.sort(array, UUIDUtil.Comparator)
+            ImmutableList.copyOf(array)
+        } else portIds
+
+        val index = Collections.binarySearch(orderedPortIds, portId)
+        val count = orderedPortIds.size
 
         val rangeStart = poolStart + poolRange * index / count
         val rangeEnd = poolStart + (poolRange * (index + 1) / count) - 1
