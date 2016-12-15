4b247f1b5cef978124e5a89cda5f13f1c0d54a36
Alex Bikfalvi <alex.bikfalvi@midokura.com>
2016-12-15 12:23:30 +0100

    Make cLibrary a class, move to jna package
    This changes the cLibrary from an interface to a class, using
    the native keyword to define the external methods as such. The
    patch also adds Javadoc for the external methods, and removes
    redundant modifiers.
    
    This patch is in preparation for adding new methods.
    
    Change-Id: Ia44302aaa8cb1f61a602ad2d02bb3ff2edbe6bfe
    Signed-off-by: Alex Bikfalvi <alex.bikfalvi@midokura.com>

--- midonet-util/src/main/java/org/midonet/jna/CLibrary.java
+++ midonet-util/src/main/java/org/midonet/jna/CLibrary.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Midokura SARL
+ * Copyright 2016 Midokura SARL
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,25 +13,41 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.midonet.util;
+
+package org.midonet.jna;
 
 import java.nio.ByteBuffer;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import com.sun.jna.LastErrorException;
-import com.sun.jna.Library;
 import com.sun.jna.Native;
 import com.sun.jna.Structure;
 import com.sun.jna.Structure.ByValue;
 import com.sun.jna.ptr.IntByReference;
 
-/**
-* JNA based library wrapper for a bunch of function calls inside the C library.
-*/
-public interface cLibrary extends Library {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-    public static cLibrary lib = (cLibrary) Native.loadLibrary("c", cLibrary.class);
+/**
+ * JNA based library wrapper for a bunch of function calls inside the C library.
+ */
+@SuppressWarnings("unused")
+public final class CLibrary {
+
+    private static final Logger log = LoggerFactory.getLogger("org.midonet.jna");
+
+    static {
+        try {
+            Native.register("c");
+        } catch (NoClassDefFoundError | UnsatisfiedLinkError | NoSuchMethodError e) {
+            log.error("Native method calls are not available");
+            System.exit(-1);
+        }
+    }
+
+    private CLibrary() {}
 
     public static class NetlinkSockAddress extends Structure {
         public short nl_family;
@@ -50,12 +66,11 @@ public interface cLibrary extends Library {
 
         @Override
         protected List getFieldOrder() {
-            return Arrays.asList("chars");
+            return Collections.singletonList("chars");
         }
     }
 
     public static class UnixPathByVal extends UnixPath implements ByValue {}
-
 
     public static class UnixDomainSockAddress extends Structure {
         public short sun_family;
@@ -83,7 +98,7 @@ public interface cLibrary extends Library {
     public static final int SOCK_NONBLOCK = 0x800;
 
     // this is the default page size for an amd64 linux kernel
-    public static int PAGE_SIZE = 0x1000;
+    public static final int PAGE_SIZE = 0x1000;
 
     public static final int SOL_IP = 0;
     public static final int SOL_SOCKET = 1;
@@ -108,40 +123,41 @@ public interface cLibrary extends Library {
 
     public static final int STDOUT_FILENO = 1;
 
-    int mlockall(int flags) throws LastErrorException;
+    public static native int mlockall(int flags) throws LastErrorException;
 
-    int munlockall() throws LastErrorException;
+    public static native int munlockall() throws LastErrorException;
 
-    int socket(int domain, int type, int protocol);
+    public static native int socket(int domain, int type, int protocol);
 
-    int connect(int fd, NetlinkSockAddress addrSockAddress, int size);
+    public static native int connect(int fd, NetlinkSockAddress addrSockAddress, int size);
 
-    int connect(int fd, UnixDomainSockAddress addrSockAddress, int size);
+    public static native int connect(int fd, UnixDomainSockAddress addrSockAddress, int size);
 
-    int bind(int fd, NetlinkSockAddress addrSockAddress, int size);
+    public static native int bind(int fd, NetlinkSockAddress addrSockAddress, int size);
 
-    int bind(int fd, UnixDomainSockAddress addrSockAddress, int size);
+    public static native int bind(int fd, UnixDomainSockAddress addrSockAddress, int size);
 
-    int accept(int fd, UnixDomainSockAddress clientSockAddress, IntByReference size);
+    public static native int accept(int fd, UnixDomainSockAddress clientSockAddress, IntByReference size);
 
-    int listen(int fd, int backlog);
+    public static native int listen(int fd, int backlog);
 
-    int getsockname(int fd, NetlinkSockAddress addrSockAddress, IntByReference size);
+    public static native int getsockname(int fd, NetlinkSockAddress addrSockAddress, IntByReference size);
 
-    int getsockname(int fd, UnixDomainSockAddress addrSockAddress, IntByReference size);
+    public static native int getsockname(int fd, UnixDomainSockAddress addrSockAddress, IntByReference size);
 
-    int setsockopt(int fd, int level, int optname, ByteBuffer buf, int buflen);
-    int getsockopt(int fd, int level, int optname, ByteBuffer buf, ByteBuffer buflen);
+    public static native int setsockopt(int fd, int level, int optname, ByteBuffer buf, int buflen);
 
-    int send(int fd, ByteBuffer buf, int len, int flags);
+    public static native int getsockopt(int fd, int level, int optname, ByteBuffer buf, ByteBuffer buflen);
 
-    int recv(int fd, ByteBuffer buf, int len, int flags);
+    public static native int send(int fd, ByteBuffer buf, int len, int flags);
 
-    int getpagesize();
+    public static native int recv(int fd, ByteBuffer buf, int len, int flags);
 
-    int close(long l);
+    public static native int getpagesize();
 
-    String strerror(int errno);
+    public static native int close(long l);
 
-    int isatty(int fd);
+    public static native String strerror(int errno);
+
+    public static native int isatty(int fd);
 }
