6077146ffabbeca470399030b1430f2b202c7ed0
Alex Bikfalvi <alex.bikfalvi@midokura.com>
2016-12-27 12:19:44 +0100

    Introduce sockaddr JNA structure
    This is used to update the interface configuration using ioctl
    calls for settings that are not modifiable via Netlink.
    
    Ref: MI-1858
    
    Change-Id: I318dde6c0fb681e8d70aa17f92be383da605d3df
    Signed-off-by: Alex Bikfalvi <alex.bikfalvi@midokura.com>
    (cherry picked from commit 58f1165fd5d4c885ec6d9fc938ca0b534c8b0306)

---
+++ midonet-util/src/main/java/org/midonet/jna/Socket.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2016 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.midonet.jna;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.sun.jna.Pointer;
+import com.sun.jna.Structure;
+
+/**
+ * JNA implementation of type and function definitions from the linux/socket.h
+ * header.
+ */
+@SuppressWarnings("unused")
+public interface Socket {
+
+    /**
+     * struct sockaddr {
+     *    sa_family_t     sa_family;      // address family, AF_xxx
+     *    char            sa_data[14];    // 14 bytes of protocol address
+     * };
+     */
+    class SockAddr extends Structure {
+
+        private static final List FIELDS = Arrays.asList("saFamily", "saData");
+
+        public short saFamily;
+        public byte[] saData = new byte[14];
+
+        public SockAddr() { }
+
+        public SockAddr(Pointer ptr) {
+            super(ptr);
+        }
+
+        protected List getFieldOrder() {
+            return FIELDS;
+        }
+
+        public static class ByReference
+            extends SockAddr
+            implements Structure.ByReference { }
+
+        public static class ByValue
+            extends SockAddr
+            implements Structure.ByValue { }
+    }
+
+}
