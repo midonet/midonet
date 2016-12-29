c0012d695681af6aaa1c0a648d0b62c9bdde6e16
Alex Bikfalvi <alex.bikfalvi@midokura.com>
2016-12-29 23:11:28 +0100

    Remove parentheses for DisruptorBackChannel#shouldProcess
    According to the scala guidelines, getter methods with no side
    effects should not have parentheses.
    
    Change-Id: I345cd5e4a3c52a56833d1cc9e32c71bc45232cf2
    Signed-off-by: Alex Bikfalvi <alex.bikfalvi@midokura.com>

--- midolman/src/main/scala/org/midonet/midolman/PacketWorkflow.scala
+++ midolman/src/main/scala/org/midonet/midolman/PacketWorkflow.scala
@@ -306,8 +306,8 @@ class PacketWorkflow(
             process()
         }
 
-    override def shouldProcess(): Boolean =
-        super.shouldProcess() ||
+    override def shouldProcess: Boolean =
+        super.shouldProcess ||
         backChannel.hasMessages ||
         arpBroker.shouldProcess() ||
         shouldExpire
