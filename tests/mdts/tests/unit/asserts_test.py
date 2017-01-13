b1a38aaa0aa1ec848e806993dfa885ac69ca8181
Joe Mills <joe@midokura.com>
2017-01-13 16:33:21 +0900

    Fix H234 errors in mdts
    Change-Id: I43be3c3ae3003f0d68b8a68a0423c515cbffe5d7
    Signed-off-by: Joe Mills <joe@midokura.com>

--- tests/mdts/tests/unit/asserts_test.py
+++ tests/mdts/tests/unit/asserts_test.py
@@ -25,13 +25,13 @@ class AssertsTest(unittest.TestCase):
         ifExpects = receives_icmp_unreachable_for_udp('172.16.1.1',
                                                       '100.100.100.100',
                                                       9, 9, 5)
-        self.assertEquals('icmp and src host 100.100.100.100 and '
-                          'icmp[20:4] = 2886729985 and '
-                          'icmp[24:4] = 1684300900 and '
-                          'icmp[28:2] = 9 and icmp[30:2] = 9',
-                          ifExpects._filter)
-        self.assertEquals(5,
-                          ifExpects._timeout)
+        self.assertEqual('icmp and src host 100.100.100.100 and '
+                         'icmp[20:4] = 2886729985 and '
+                         'icmp[24:4] = 1684300900 and '
+                         'icmp[28:2] = 9 and icmp[30:2] = 9',
+                         ifExpects._filter)
+        self.assertEqual(5,
+                         ifExpects._timeout)
 
 
 if __name__ == "__main__":
