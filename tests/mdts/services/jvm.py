af583499dd4532c8a6917ef528f70afb7cb90378
Joe Mills <joe@midokura.com>
2017-01-13 15:10:00 +0900

    fix F403 errors in mdts
    Change-Id: I1e7a2f20fab6ba195244ee645b637b05c15c3f6b
    Signed-off-by: Joe Mills <joe@midokura.com>

--- tests/mdts/services/jvm.py
+++ tests/mdts/services/jvm.py
@@ -13,7 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-from jpype import *
+from jpype import getDefaultJVMPath
+from jpype import java
+from jpype import startJVM
 import logging
 
 LOG = logging.getLogger(__name__)
