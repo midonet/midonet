1c1a8796431df120dd4dbb599e7ae855119e7ec8
Dmitry Bufistov <dmitry@midokura.com>
2017-01-25 18:35:22 +0100

    Create Vpn SeriveContainer on first siteconnection
    If VpnService container is created without site connections
    it state changes from "STARTING" to "STOPPED", so container scheduler
    kills it within 10 seconds and tries to reschedule same container on
    next host within 15 seconds. This is done in a loop until first site
    connection is created.
    
    This patch avoid this resource wasting by creating
    vpn service container when first connection is created and deleting
    it when last connection of all vpn services that use this container
    is deleted. The containerId field of VpnService objects is managed by
    SiteConnection translator.
    
    Ref: MI-1230
    Change-Id: I42b30b106bb392279f1f00c1dec4ccc333244b02
    Signed-off-by: Dmitry Bufistov <dmitry@midokura.com>

--- midonet-cluster/src/test/scala/org/midonet/cluster/services/c3po/translators/VPNaaSTranslatorIT.scala
+++ midonet-cluster/src/test/scala/org/midonet/cluster/services/c3po/translators/VPNaaSTranslatorIT.scala
@@ -19,6 +19,7 @@ package org.midonet.cluster.services.c3po.translators
 import java.util.UUID
 
 import scala.collection.JavaConversions._
+import scala.concurrent.duration._
 
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
@@ -30,8 +31,8 @@ import org.midonet.cluster.models.Neutron.{IPSecSiteConnection, VpnService}
 import org.midonet.cluster.models.Topology.Route.NextHop
 import org.midonet.cluster.models.Topology._
 import org.midonet.cluster.services.c3po.translators.RouteManager.localRouteId
-import org.midonet.cluster.services.c3po.translators.VpnServiceTranslator._
-import org.midonet.cluster.util.UUIDUtil.{asRichProtoUuid, toProto}
+import org.midonet.cluster.services.c3po.translators.IPSecSiteConnectionTranslator.vpnContainerRouteId
+import org.midonet.cluster.util.UUIDUtil._
 import org.midonet.cluster.util.{IPAddressUtil, IPSubnetUtil}
 import org.midonet.util.concurrent.toFutureOps
 
@@ -59,17 +60,22 @@ class VPNaaSTranslatorIT extends C3POMinionTestBase {
         val vpn1Json = vpnServiceJson(vpn1Id, router1Id,
                                       externalV4Ip = Some(router1GatewayIp))
         insertCreateTask(30, VpnServiceType, vpn1Json, vpn1Id)
-        eventually(verifyContainer(router1Id, Seq(vpn1Id), router1GatewayIp))
+        eventually(verifyVpnServices(router1Id, Seq(vpn1Id), router1GatewayIp))
+        verifyNoContainerAndRedirectRoute(router1Id)
 
         // setup second vpn service on router
         val vpn2Json = vpnServiceJson(vpn2Id, router1Id,
                                       externalV4Ip = Some(router1GatewayIp))
         insertCreateTask(40, VpnServiceType, vpn2Json, vpn2Id)
-        eventually(verifyContainer(router1Id, Seq(vpn1Id, vpn2Id), router1GatewayIp))
+        eventually(verifyVpnServices(router1Id, Seq(vpn1Id, vpn2Id), router1GatewayIp))
+        verifyNoContainerAndRedirectRoute(router1Id)
+        var router = storage.get(classOf[Router], router1Id).await(60 seconds)
+        router.getVpnServiceIdsCount shouldBe 2
 
         // delete the first vpn service
         insertDeleteTask(50, VpnServiceType, vpn1Id)
-        eventually(verifyContainer(router1Id, Seq(vpn2Id), router1GatewayIp))
+        eventually(verifyVpnServices(router1Id, Seq(vpn2Id), router1GatewayIp))
+        verifyNoContainerAndRedirectRoute(router1Id)
 
         // update the second vpn
         val vpn2JsonV2 = vpnServiceJson(vpn2Id, router1Id,
@@ -91,7 +97,10 @@ class VPNaaSTranslatorIT extends C3POMinionTestBase {
         }
 
         insertDeleteTask(70, VpnServiceType, vpn2Id)
-        eventually(verifyNoContainer(router1Id))
+        verifyNoContainerAndRedirectRoute(router1Id)
+        eventually (storage.get(classOf[Router], router1Id).await()
+                        .getVpnServiceIdsCount shouldBe 0)
+        eventually (storage.getAll(classOf[VpnService]).await().size shouldBe 0)
     }
 
     "IPSecSiteConnectionTranslator" should "handle IPSecSiteConnection CRUD" in {
@@ -102,38 +111,25 @@ class VPNaaSTranslatorIT extends C3POMinionTestBase {
                                       externalV4Ip = Some(router1GatewayIp))
         insertCreateTask(30, VpnServiceType, vpn1Json, vpn1Id)
 
-        // Create and verify a connection.
-        val cnxn1Id = UUID.randomUUID()
         var cnxn1LocalCidrs = Seq("10.0.1.0/24")
         var cnxn1PeerCidrs = Seq("20.0.1.0/24", "20.0.2.0/24")
-        val cnxn1IkeJson = ikePolicyJson()
-        val cnxn1IpSecJson = ipSecPolicyJson()
-        val cnxn1Json = ipSecSiteConnectionJson(
-            cnxn1Id, vpn1Id, cnxn1LocalCidrs, cnxn1PeerCidrs,
-            cnxn1IkeJson, cnxn1IpSecJson)
-        insertCreateTask(40, IpSecSiteConType, cnxn1Json, cnxn1Id)
-        val cnxn1 = eventually {
-            // We create two routes when we create the VpnService, hence
-            // numOtherRoutes = 2
-            verifyCnxn(cnxn1Id, router1Id, cnxn1LocalCidrs, cnxn1PeerCidrs,
-                       numOtherRoutes = 2)
-        }
+        // Create and verify a connection.
+        // We create two routes when we create the VpnService container, hence
+        // numOtherRoutes = 2
+        val cnxn1 = createAndVerifyConnection(vpn1Id, router1Id, cnxn1LocalCidrs,
+                                              cnxn1PeerCidrs,
+                                              expectedRoutes = 2)
+        val cnxn1Id = cnxn1.getId
 
+        var cnxn2LocalCidrs = Seq("10.0.1.0/24", "10.0.2.0/24")
+        var cnxn2PeerCidrs = Seq("40.0.1.0/24", "40.0.2.0/24", "40.0.3.0/24")
         // Create a second connection on the same VPN and verify that they
         // coexist.
-        val cnxn2Id = UUID.randomUUID()
-        val cnxn2LocalCidrs = Seq("10.0.1.0/24", "10.0.2.0/24")
-        val cnxn2PeerCidrs = Seq("40.0.1.0/24", "40.0.2.0/24", "40.0.3.0/24")
-        val cnxn2IkeJson = ikePolicyJson()
-        val cnxn2IpSecJson = ipSecPolicyJson()
-        val cnxn2Json = ipSecSiteConnectionJson(
-            cnxn2Id, vpn1Id, cnxn2LocalCidrs, cnxn2PeerCidrs,
-            cnxn2IkeJson, cnxn2IpSecJson)
-        insertCreateTask(50, IpSecSiteConType, cnxn2Json, cnxn2Id)
-        val cnxn2 = eventually {
-            verifyCnxn(cnxn2Id, router1Id, cnxn2LocalCidrs, cnxn2PeerCidrs,
-                       numOtherRoutes = 2 + cnxn1.getRouteIdsCount)
-        }
+        val cnxn2 = createAndVerifyConnection(vpn1Id, router1Id,
+                                              cnxn2LocalCidrs,
+                                              cnxn2PeerCidrs,
+                                              2 + cnxn1.getRouteIdsCount)
+        val cnxn2Id = cnxn2.getId()
         verifyCnxn(cnxn1Id, router1Id, cnxn1LocalCidrs, cnxn1PeerCidrs,
                    numOtherRoutes = 2 + cnxn2.getRouteIdsCount)
 
@@ -141,8 +137,8 @@ class VPNaaSTranslatorIT extends C3POMinionTestBase {
         cnxn1LocalCidrs = Seq("10.0.1.0/24")
         cnxn1PeerCidrs = Seq("20.0.1.0/24", "20.0.3.0/24")
         val cnxn1V2Json = ipSecSiteConnectionJson(
-            cnxn1Id, vpn1Id, cnxn1LocalCidrs, cnxn1PeerCidrs,
-            cnxn1IkeJson, cnxn1IpSecJson, adminStateUp = false,
+            cnxn1.getId, vpn1Id, cnxn1LocalCidrs, cnxn1PeerCidrs,
+            ikePolicyJson(), ipSecPolicyJson(), adminStateUp = false,
             name = Some("renamed-cnxn1"),
             description = Some("Description for cnxn1"))
         insertUpdateTask(60, IpSecSiteConType, cnxn1V2Json, cnxn1Id)
@@ -173,6 +169,7 @@ class VPNaaSTranslatorIT extends C3POMinionTestBase {
         eventually {
             verifyCnxnGone(cnxn2Id, cnxn2.getRouteIdsList)
         }
+        eventually(verifyNoContainerAndRedirectRoute(router1Id))
     }
 
     "IPSecSiteConnectionTranslator" should "not delete preexisting local redirect chain" in {
@@ -192,25 +189,30 @@ class VPNaaSTranslatorIT extends C3POMinionTestBase {
             .setAction(Rule.Action.DROP)
             .build()
         storage.create(rule)
-        storage.update(router.toBuilder
-                           .setLocalRedirectChainId(chain.getId).build)
+        storage.update(router.toBuilder.setLocalRedirectChainId(
+            chain.getId).build)
 
         val vpn1Json = vpnServiceJson(vpn1Id, router1Id,
                                       externalV4Ip = Some(router1GatewayIp))
         insertCreateTask(30, VpnServiceType, vpn1Json, vpn1Id)
-        eventually(verifyContainer(router1Id, Seq(vpn1Id),
-                                   router1GatewayIp))
-        insertDeleteTask(40, VpnServiceType, vpn1Id)
+
+        var cnxn1LocalCidrs = Seq("10.0.1.0/24")
+        var cnxn1PeerCidrs = Seq("20.0.1.0/24", "20.0.2.0/24")
+        var cnxn1 = createAndVerifyConnection(vpn1Id, router1Id, cnxn1LocalCidrs,
+                                              cnxn1PeerCidrs,
+                                              expectedRoutes = 2)
+
+
+        insertDeleteTask(50, IpSecSiteConType, cnxn1.getId)
         storage.exists(classOf[Chain], chain.getId).await() shouldBe true
 
         storage.delete(classOf[Rule], rule.getId)
-
         storage.exists(classOf[Chain], chain.getId).await() shouldBe true
 
-        insertCreateTask(50, VpnServiceType, vpn1Json, vpn1Id)
-        eventually(verifyContainer(router1Id, Seq(vpn1Id),
-                                   router1GatewayIp))
-        insertDeleteTask(60, VpnServiceType, vpn1Id)
+        cnxn1 = createAndVerifyConnection(vpn1Id, router1Id, cnxn1LocalCidrs,
+                                          cnxn1PeerCidrs,
+                                          expectedRoutes = 2)
+        insertDeleteTask(60, IpSecSiteConType, cnxn1.getId)
         eventually {
             storage.exists(classOf[Chain], chain.getId).await() shouldBe false
         }
@@ -267,7 +269,7 @@ class VPNaaSTranslatorIT extends C3POMinionTestBase {
                                   router1Id).await() shouldBe true)
     }
 
-    private def verifyNoContainer(routerId: UUID): Unit = {
+    private def verifyNoContainer(routerId: UUID): Router = {
         storage.getAll(classOf[ServiceContainerGroup]).await().size shouldBe 0
         storage.getAll(classOf[ServiceContainer]).await().size shouldBe 0
 
@@ -275,13 +277,17 @@ class VPNaaSTranslatorIT extends C3POMinionTestBase {
         router.getPortIdsCount shouldBe 1 // only gw left
 
         val rules = storage.getAll(classOf[Rule]).await()
-            .filter(VpnServiceTranslator.isRedirectRule)
-        rules.count(VpnServiceTranslator.isESPRule) shouldBe 0
-        rules.count(VpnServiceTranslator.isUDP500Rule) shouldBe 0
-        rules.count(VpnServiceTranslator.isUDP4500Rule) shouldBe 0
+            .filter(IPSecSiteConnectionTranslator.isRedirectRule)
+        rules.count(IPSecSiteConnectionTranslator.isESPRule) shouldBe 0
+        rules.count(IPSecSiteConnectionTranslator.isUDP500Rule) shouldBe 0
+        rules.count(IPSecSiteConnectionTranslator.isUDP4500Rule) shouldBe 0
+        router
+    }
 
+    private def verifyNoContainerAndRedirectRoute(routerId: UUID): Unit = {
+
+        val router = verifyNoContainer(routerId)
         router.hasLocalRedirectChainId shouldBe false
-        router.getVpnServiceIdsCount shouldBe 0
     }
 
     private def verifyContainer(rtrId: UUID, vpnIds: Seq[UUID],
@@ -302,12 +308,12 @@ class VPNaaSTranslatorIT extends C3POMinionTestBase {
         val rules = storage.getAll(classOf[Rule],
                                    redirectChain.getRuleIdsList).await()
             .filter((r: Rule) => {
-                        VpnServiceTranslator.isRedirectForEndpointRule(r, localEndpointIp) &&
+                IPSecSiteConnectionTranslator.isRedirectForEndpointRule(r, localEndpointIp) &&
                             r.getTransformRuleData.getTargetPortId == rtrPort.getId
                     })
-        rules.count(VpnServiceTranslator.isESPRule) shouldBe 1
-        rules.count(VpnServiceTranslator.isUDP500Rule) shouldBe 1
-        rules.count(VpnServiceTranslator.isUDP4500Rule) shouldBe 1
+        rules.count(IPSecSiteConnectionTranslator.isESPRule) shouldBe 1
+        rules.count(IPSecSiteConnectionTranslator.isUDP500Rule) shouldBe 1
+        rules.count(IPSecSiteConnectionTranslator.isUDP4500Rule) shouldBe 1
 
         val localRt = storage.get(classOf[Route],
                                   localRouteId(container.getPortId, rtrPort.getPortAddress)).await()
@@ -325,7 +331,7 @@ class VPNaaSTranslatorIT extends C3POMinionTestBase {
         container.getServiceGroupId shouldBe scg.getId
         container.getServiceType shouldBe "IPSEC"
 
-        router.getVpnServiceIdsList.map(_.asJava) should
+        router.getVpnServiceIdsList should
             contain theSameElementsAs vpnIds
         router.getPortIdsList should contain(rtrPort.getId)
         router.getLocalRedirectChainId shouldBe redirectChain.getId
@@ -351,5 +357,43 @@ class VPNaaSTranslatorIT extends C3POMinionTestBase {
         vpnRt.hasNextHopGateway shouldBe false
         vpnRt.getSrcSubnet shouldBe IPSubnetUtil.AnyIPv4Subnet
         vpnRt.getDstSubnet shouldBe IPSubnetUtil.toProto("169.254.0.0/30")
+    }
+
+    private def verifyVpnServices(rtrId: UUID, vpnIds: Seq[UUID],
+                                  rtrGwIp: String): Unit = {
+        val vpnServices = storage.getAll(classOf[VpnService], vpnIds)
+            .await(60 seconds)
+        vpnServices.size shouldBe vpnIds.size
+
+        val router = storage.get(classOf[Router], rtrId).await()
+        router.hasLocalRedirectChainId shouldBe false
+
+        val containerIds = vpnServices.map(_.hasContainerId)
+        all (containerIds) shouldBe false
+
+        all (vpnServices.map(_.getExternalIp.getAddress)) shouldBe rtrGwIp
+        storage.exists(classOf[Port], rtrId).await(60 seconds) shouldBe false
+
+        storage.exists(classOf[Chain], router.getLocalRedirectChainId)
+            .await() shouldBe false
+
+        val localEndpointIp = IPSubnetUtil.fromAddress(rtrGwIp)
+    }
+
+    private def createAndVerifyConnection(vpnServiceId: UUID, routerId: UUID,
+                                          localCidrs: Seq[String],
+                                          peerCidrs: Seq[String],
+                                          expectedRoutes: Int): IPSecSiteConnection = {
+        val cnxn1Id = UUID.randomUUID()
+        val cnxn1IkeJson = ikePolicyJson()
+        val cnxn1IpSecJson = ipSecPolicyJson()
+        val cnxn1Json = ipSecSiteConnectionJson(
+            cnxn1Id, vpnServiceId, localCidrs, peerCidrs,
+            cnxn1IkeJson, cnxn1IpSecJson)
+        insertCreateTask(40, IpSecSiteConType, cnxn1Json, cnxn1Id)
+        eventually {
+            verifyCnxn(cnxn1Id, router1Id, localCidrs, peerCidrs,
+                       expectedRoutes)
+        }
     }
 }
