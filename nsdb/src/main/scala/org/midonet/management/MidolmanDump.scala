4eaef5172940a6f062e32023b934c4003578d37c
Adrian Serrano <adrian@midokura.com>
2017-01-26 10:26:57 +0100

    mm-dump: Tool to dump the heap of a running VM
    This patch adds a new tool to midonet-tools: mm-dump. It connects
    via JMX to a running JVM and dumps the heap to a file.
    
    Supports the following arguments:
    
    -h, --host: Host were the target VM resides (default: localhost)
    
    -p, --port: Port number used by JMX (default: 7200)
    
    -o, --output: Path for the output file in the target host. If it's not
                  an absolute path, it's taken relative to the current
    	      working directory of the target VM.
    
    -a, --all: Dump all objects. By default, only live (reachable) objects
               are dumped.
    
    Ref: MI-1994
    Change-Id: I9e06755b28dbb6cfaaa9ee815571a1978f6772b2
    Signed-off-by: Adrian Serrano <adrian@midokura.com>

---
+++ nsdb/src/main/scala/org/midonet/management/MidolmanDump.scala
@@ -0,0 +1,138 @@
+/*
+ * Copyright 2016 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.midonet.management
+
+import java.io.IOException
+import java.lang.management.{ManagementFactory, RuntimeMXBean}
+
+import javax.management.openmbean.{CompositeData, TabularDataSupport}
+import javax.management.remote.{JMXConnectorFactory, JMXServiceURL}
+import javax.management.{JMX, MBeanServerConnection, ObjectName}
+
+import scala.util.control.NonFatal
+
+import com.sun.management.HotSpotDiagnosticMXBean
+
+import org.rogach.scallop._
+
+object MidolmanDump extends App {
+
+    System.setProperty("logback.configurationFile", "logback-disabled.xml")
+
+    val DefaultHost = "localhost"
+    val JMXDefaultPort = 7200
+    val DefaultDumpFileName = "dump.hprof"
+    val DefaultDumpAllObjects = false
+
+    val opts = new ScallopConf(args) {
+        val port = opt[Int]("port", short = 'p', default = Option(JMXDefaultPort),
+                            descr = "JMX port",
+                            required = true)
+        val host = opt[String]("host", short = 'h', default = Option(DefaultHost),
+                               descr = "Host")
+        val output = opt[String]("output", short = 'o', default = None,
+                                 descr = "Output file")
+
+        val dumpAll = opt[Boolean]("all", short = 'a', default = Some(DefaultDumpAllObjects),
+                                   descr = "Include unreachable objects")
+
+        printedName = "mm-dump"
+
+        footer("Copyright (c) 2016 Midokura SARL, All Rights Reserved.")
+    }
+
+    val ERROR = s"[\033[31m${opts.printedName}\033[0m]"
+    val WARNING = s"[\033[33m${opts.printedName}\033[0m]"
+    val INFO = s"[\033[32m${opts.printedName}\033[0m]"
+
+    private def connect(host: String, port: Int): MBeanServerConnection = {
+        val url = new JMXServiceURL(
+            s"service:jmx:rmi:///jndi/rmi://$host:$port/jmxrmi")
+        val jmxc = JMXConnectorFactory.connect(url, null)
+
+        jmxc.getMBeanServerConnection
+    }
+
+    private def getRemoteCwd(conn: MBeanServerConnection): String = {
+        val cwdProp = Array[String]("user.dir")
+        try {
+            val objName = new ObjectName(ManagementFactory.RUNTIME_MXBEAN_NAME)
+            val runtime = JMX.newMBeanProxy(conn,
+                                            objName,
+                                            classOf[RuntimeMXBean],
+                                            true)
+            val props = runtime.getSystemProperties
+                        .asInstanceOf[TabularDataSupport]
+
+            props.get(cwdProp).asInstanceOf[CompositeData]
+                .get("value").asInstanceOf[String]
+
+        } catch {
+            case NonFatal(err) =>
+                System.err.println("$WARNING Unable to get remote working directory.")
+                "(unknown)"
+        }
+    }
+
+    private def getExceptionMessage(e: Throwable): String = {
+        var t = e
+        while ((t.getCause ne null) && (t.getMessage eq null))
+            t = e.getCause
+        t.getMessage
+    }
+
+    try {
+        val Slash = "/"
+        val DiagnosticsBeanName = new ObjectName("com.sun.management:type=HotSpotDiagnostic")
+
+        val host = opts.host.get.get
+        val conn = connect(host, opts.port.get.get)
+        val bean = JMX.newMBeanProxy(conn,
+                                     DiagnosticsBeanName,
+                                     classOf[HotSpotDiagnosticMXBean],
+                                     true)
+
+        val destinationFile = opts.output.get match {
+            case Some(name) => name
+            case None => DefaultDumpFileName
+        }
+
+        val fullPath = if (destinationFile.startsWith(Slash)) {
+            destinationFile
+        } else {
+            val cwd = getRemoteCwd(conn)
+            val separator = if (cwd.endsWith(Slash)) "" else Slash
+            s"$cwd$separator$destinationFile"
+        }
+
+        System.err.println(s"$INFO Dumping heap to $host:$fullPath ...")
+
+        val onlyLiveObjects = ! opts.dumpAll.get.get
+        bean.dumpHeap(destinationFile, onlyLiveObjects)
+
+    } catch {
+        case e: IOException =>
+            val msg = getExceptionMessage(e)
+            System.err.println(s"$ERROR I/O error: $msg")
+            System.exit(2)
+
+        case e: Throwable =>
+            e.printStackTrace()
+            val msg = getExceptionMessage(e)
+            System.err.println(s"$ERROR Unexpected error: $msg")
+            System.exit(1)
+    }
+}
