0d73ba2a6c3440ca0e9f1466792d44e1f8765f88
Dmitry Bufistov <dmitry@midokura.com>
2017-01-30 16:16:04 +0100

    Added stack trace dump option to mm-dump
    If -t option is specified, then istead of heap we dump stack trace and exit.
    
    Ref: MI-2003
    Change-Id: I9c9d00e298254848d3f6f97c22e753eb596f27c1
    Signed-off-by: Dmitry Bufistov <dmitry@midokura.com>

--- nsdb/src/main/scala/org/midonet/management/MidolmanDump.scala
+++ nsdb/src/main/scala/org/midonet/management/MidolmanDump.scala
@@ -15,8 +15,11 @@
  */
 package org.midonet.management
 
-import java.io.IOException
-import java.lang.management.{ManagementFactory, RuntimeMXBean}
+import java.io.{BufferedWriter, File, FileWriter, IOException}
+import java.lang.management.{ManagementFactory, RuntimeMXBean, ThreadMXBean}
+import java.lang.management.ManagementFactory.THREAD_MXBEAN_NAME
+import java.lang.management.ManagementFactory.getThreadMXBean
+import java.lang.management.ManagementFactory.newPlatformMXBeanProxy
 import java.nio.file.Paths
 
 import javax.management.openmbean.{CompositeData, TabularDataSupport}
@@ -45,10 +48,21 @@ object MidolmanDump extends App {
         val host = opt[String]("host", short = 'h', default = Option(DefaultHost),
                                descr = "Host")
         val output = opt[String]("output", short = 'o', default = None,
-                                 descr = "Output file")
+                                 descr = "Output file. It will be created on process" +
+                                         " machine for heap dump and on current" +
+                                         " machine for stack trace dump")
 
-        val dumpAll = opt[Boolean]("all", short = 'a', default = Some(DefaultDumpAllObjects),
+        val dumpHeap = opt[Boolean]("heap", short = 'm',
+                                    default = Some(false),
+                                    descr = "Dump memory heap")
+
+        val dumpAll = opt[Boolean]("all", short = 'a',
+                                   default = Some(DefaultDumpAllObjects),
                                    descr = "Include unreachable objects")
+
+        val dumpStackTrace = opt[Boolean]("stack-trace", short = 's',
+            default = Some(false),
+                descr = "Dump stack trace of all threads")
 
         printedName = "mm-dump"
 
@@ -95,7 +109,14 @@ object MidolmanDump extends App {
         t.getMessage
     }
 
+    var bufferWriter: BufferedWriter = null
     try {
+        if (opts.dumpHeap.get.get ==
+            opts.dumpStackTrace.get.get) {
+            System.err.println(s"$ERROR Either heap dump or stack trace dump" +
+                " action must be specified")
+            System.exit(3)
+        }
         val DiagnosticsBeanName = new ObjectName("com.sun.management:type=HotSpotDiagnostic")
 
         val host = opts.host.get.get
@@ -118,10 +139,30 @@ object MidolmanDump extends App {
             Paths.get(getVMWorkingDirectory(conn)).resolve(destinationPath)
         }
 
-        System.err.println(s"$INFO Dumping heap to $host:$fullPath ...")
+        if (opts.dumpStackTrace.get.get) {
+            val file = new File(destinationFile)
+            if (file.exists()) {
+                System.err.println(s"$ERROR output file $destinationFile" +
+                    " already exists")
+            } else {
+                System.err.println(s"$INFO Dumping stack trace to $destinationPath ...")
+                val tmbean = newPlatformMXBeanProxy(conn,
+                                                    THREAD_MXBEAN_NAME,
+                                                    classOf[ThreadMXBean])
 
-        val onlyLiveObjects = ! opts.dumpAll.get.get
-        bean.dumpHeap(destinationFile, onlyLiveObjects)
+                bufferWriter = new BufferedWriter(new FileWriter(file))
+                bufferWriter.write(s"Total threads: " + tmbean.getThreadCount +
+                    "\n\n")
+                tmbean.dumpAllThreads(tmbean.isObjectMonitorUsageSupported,
+                    tmbean.isSynchronizerUsageSupported)
+                        .map(_.toString) foreach (bufferWriter.write(_))
+            }
+        } else {
+            System.err.println(s"$INFO Dumping heap to $host:$fullPath ...")
+
+            val onlyLiveObjects = ! opts.dumpAll.get.get
+            bean.dumpHeap(destinationFile, onlyLiveObjects)
+        }
 
     } catch {
         case e: IOException =>
@@ -134,5 +175,9 @@ object MidolmanDump extends App {
             val msg = getExceptionMessage(e)
             System.err.println(s"$ERROR Unexpected error: $msg")
             System.exit(1)
+    } finally {
+        if (bufferWriter ne null) {
+            bufferWriter.close()
+        }
     }
 }
