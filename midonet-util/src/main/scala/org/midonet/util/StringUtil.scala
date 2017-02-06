ffdd2abbb1c660a87c3d6506c930a613c8339b4b
Miguel Herranz <miguel@midokura.com>
2017-02-06 15:19:45 +0100

    Serialize the metrics with minimal allocation
    The Metering.toTextTable() method has been added so it can be used
    to generate the metrics in a text form. This method only generates
    1 allocation of 56 bytes per registry, needed to iterate it.
    
    Change-Id: I308ebd4fe0743cc3958f1e952c69e51868077c76
    Ref: MI-2017

--- midonet-util/src/main/scala/org/midonet/util/StringUtil.scala
+++ midonet-util/src/main/scala/org/midonet/util/StringUtil.scala
@@ -16,6 +16,8 @@
 
 package org.midonet.util
 
+import java.io.BufferedWriter
+
 object StringUtil {
 
     /**
@@ -32,4 +34,72 @@ object StringUtil {
 
     /** Returns null if o is null, otherwise o.toString. */
     def toStringOrNull(o: AnyRef) = if (o == null) null else o.toString
+
+    val LONG_MIN_VALUE_STRING = Long.MinValue.toString
+    val BASE = 10
+    val DIGITS = "0123456789".toCharArray()
+    val MAX_NUMBER_OF_DIGITS = 19
+    val LEFTMOST_DIGIT_UNITARY_VALUE = 1000000000000000000L
+
+    // Assert DIGITS, MAX_NUMBER_OF_DIGITS and LEFTMOST_DIGIT_UNITARY_VALUE
+    // values, by comparing them with calculated ones, but avoid the
+    // calculation cost during initialization for production code.
+    assert( {for (i <- 0 to BASE - 1) { assert(DIGITS(i) == i.toString.charAt(0)) }; true })
+    assert(MAX_NUMBER_OF_DIGITS == Math.ceil(Math.log(Math.max(
+               Math.abs(Long.MinValue.asInstanceOf[Double]),
+               Math.abs(Long.MaxValue.asInstanceOf[Double])
+           ))/Math.log(BASE)).asInstanceOf[Int])
+    assert(LEFTMOST_DIGIT_UNITARY_VALUE ==
+           ("1" + ("0" * (MAX_NUMBER_OF_DIGITS - 1))).toLong)
+
+    /**
+      * Append the decimal representation of a long to a StringBuilder without
+      * allocate objects
+      *
+      * @param b the buffered writer
+      * @param i the long
+      */
+    def append(b: BufferedWriter, i: Long): BufferedWriter = {
+        var x = i
+        // special case, as sign cannot be change for Long.MIN_VALUE
+        if (x == Long.MinValue) {
+            b.append(LONG_MIN_VALUE_STRING)
+            return b
+        }
+        // special case, as zero has "one leading zero" special case and this
+        // way we can avoid to check "to go too far" in the loop skip leading
+        // zeros loop
+        if (x == 0) {
+            b.append('0')
+            return b
+        }
+        // for negative, just print and change sign
+        if (x < 0) {
+            b.append('-')
+            x = -x
+        }
+        var currentDigitUnitaryValue = LEFTMOST_DIGIT_UNITARY_VALUE
+        var digit = (x / currentDigitUnitaryValue).asInstanceOf[Int]
+        // loop to skip leading zeros
+        while (digit == 0) {
+            // append nothing
+            // get next digit
+            x %= currentDigitUnitaryValue
+            currentDigitUnitaryValue /= BASE
+            digit = (x / currentDigitUnitaryValue).asInstanceOf[Int]
+        }
+        // loop for digits at positions greater than unit
+        while (currentDigitUnitaryValue > 1) {
+            // append digit
+            b.append(DIGITS(digit))
+            // get next digit
+            x %= currentDigitUnitaryValue
+            currentDigitUnitaryValue /= BASE
+            digit = (x / currentDigitUnitaryValue).asInstanceOf[Int]
+        }
+        // print digit at units position as a special case out of the loop to
+        // avoid further unneeded divisions
+        b.append(DIGITS(digit))
+        b
+    }
 }
