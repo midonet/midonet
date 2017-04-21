b88bfe6126c553917b28e241bee9a3834ffd923c
YAMAMOTO Takashi <yamamoto@midokura.com>
2017-04-21 14:57:30 +0900

    tests: Use the correct version of the provisioning script for newton
    Ref: MI-2128
    Signed-off-by: YAMAMOTO Takashi <yamamoto@midokura.com>
    Change-Id: Ie12f920a6b9f91ea54a8e46af0be1670d39a5601

---
+++ tests/sandbox/provisioning/all-newton-provisioning.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+$PWD/sandbox/provisioning/bgp-provisioning.sh
+$PWD/sandbox/provisioning/l2gw-provisioning.sh
+$PWD/sandbox/provisioning/vtep-provisioning.sh
+$PWD/sandbox/provisioning/keystone-newton-provisioning.sh
