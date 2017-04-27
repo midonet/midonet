017efc7ae81281c3e9b3cbf12d715c556bda9f7a
Ivan Kelly <ivan@midokura.com>
2017-04-27 16:23:23 +0200

    Build system for native timed expiration map
    GTest tests run as a dependency of the JVM tests.
    The shared library is built as a dependency of the jar task.
    
    Change-Id: I4364a48d561b39bd9851078aa94d2fc406724f84
    Signed-off-by: Ivan Kelly <ivan@midokura.com>

---
+++ midonet-util/src/nativeTimedExpirationMapTest/cpp/test_main.cpp
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2017 Midokura SARL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "gtest/gtest.h"
+
+#include "nativeTimedExpirationMap.h"
+
+using namespace testing;
+
+TEST(FoobarTests, test_foobar) {
+  auto a = 1 + 2; // make sure we're using c++11
+  ASSERT_TRUE(0 == 0);
+}
+
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
