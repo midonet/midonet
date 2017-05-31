cd140d41fdb294bd1026c2a8703db2ec1b6dd079
Xavi León <xavi.leon@midokura.com>
2017-05-31 18:29:58 +0200

    MDTS: install quagga 1.1.1 in midolman2
    ... so we can test zebra protocol v3 on the BGP tests.
    
    Change-Id: I07c9f518e6c0f842af182452f60b4686a9d0ce7d
    Ref: MI-2341
    Signed-off-by: Xavi León <xavi.leon@midokura.com>

--- tests/sandbox/override_v2/midolman/override.sh
+++ tests/sandbox/override_v2/midolman/override.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Include the misc testing repos for quagga 1.1.1
+MISC_TESTING_REPO_FILE=/etc/apt/sources.list.d/midonet-misc-testing.list
+echo "deb http://builds.midonet.org/misc testing main" > $MISC_TESTING_REPO_FILE
+
 # Install the latest packages from the local repository
 LOCAL_REPO_FILE=/etc/apt/sources.list.d/midonet-local.list
 echo "deb file:/packages /" > $LOCAL_REPO_FILE
@@ -29,5 +33,15 @@ MIDOLMAN_ENV_FILE='/etc/midolman/midolman-env.sh'
 sudo sed -i 's/\(MAX_HEAP_SIZE=\).*$/\1256M/' $MIDOLMAN_ENV_FILE
 MINIONS_ENV_FILE='/etc/midolman/minions-env.sh'
 sudo sed -i 's/\(MAX_HEAP_SIZE=\).*$/\1128M/' $MINIONS_ENV_FILE
+
+# Force quagga 0.99.23 (v2 zebra protocol) on midolman1
+if [ $(hostname) = "midolman1" ]; then
+    apt-get install -qy --force-yes quagga=0.99.23.1-0midokura
+fi
+
+# Install quagga 1.1.1 (v3 zebra protocol) on midolman2
+if [ $(hostname) = "midolman2" ]; then
+    apt-get install -qy --force-yes quagga=1.1.1-3
+fi
 
 exec /run-midolman.sh
