2b8cd3473b5a85faf8ef9f36e7de862328a0d6fd
Mateo Hermida <mateo@midokura.com>
2017-09-27 16:22:21 +0200

    Turn on performance flags
    This patch enables by default the flags for off-heap
    tables, topology snapshoting and reclaiming the
    existing OVS datapath.
    
    Ref: MI-2644
    
    Change-Id: Iaf9d9346c242c75d6a7a4ca38c1bf99422402883
    Signed-off-by: Mateo Hermida <mateo@midokura.com>

--- tests/sandbox/override_v2/midolman/override.sh
+++ tests/sandbox/override_v2/midolman/override.sh
@@ -45,18 +45,18 @@ fi
 
 sed -i -e 's/zookeeper_hosts = .*$/zookeeper_hosts = '"$MIDO_ZOOKEEPER_HOSTS"'/' /etc/midolman/midolman.conf
 
-# Enable offheap tables for midolman1
 # Force quagga 0.99.23 (v2 zebra protocol) on midolman1
 if [ $(hostname) = "midolman1" ]; then
-    mn-conf set -h local <<EOF
-agent.midolman.off_heap_tables=true
-agent.midolman.reclaim_datapath=true
-EOF
     apt-get install -qy --force-yes quagga=0.99.23.1-0midokura
 fi
 
 # Install quagga 1.1.1 (v3 zebra protocol) on midolman2
+# Disable offheap tables for midolman2
 if [ $(hostname) = "midolman2" ]; then
+    mn-conf set -h local <<EOF
+agent.midolman.off_heap_tables=false
+agent.midolman.reclaim_datapath=false
+EOF
     apt-get install -qy --force-yes quagga=1.1.1-3
 fi
 
