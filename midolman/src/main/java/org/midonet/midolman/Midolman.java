248b22c71f108e0f1d81664187cfe30f02801c54
Xavi León <xavi.leon@midokura.com>
2017-10-02 16:06:28 +0200

    Fast reboot: coordinate stop/start of new agent
    This patch introduces a new mechaism to reboot the
    agent that allows Midonet to provide close to
    zero down-time during a reboot. The mechanism is
    based on starting a new agent while the previous one
    is still processing traffic. The shutdown of the
    old agent and the startup of the new agent is
    coordinated as to reduce to the minimum the
    downtime by sending the SIGUSR1 signal to the
    watchdog daemon.
    
    To start the fast reboot mechanism, the operator
    just needs to issue the command:
    
    $ mm-reboot
    
    Change-Id: Ic7dbbec022c61f52213feeb362f783709499ef25
    Ref: MI-2007
    Signed-off-by: Xavi León <xavi.leon@midokura.com>

--- midolman/src/main/java/org/midonet/midolman/Midolman.java
+++ midolman/src/main/java/org/midonet/midolman/Midolman.java
@@ -15,6 +15,7 @@
  */
 package org.midonet.midolman;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.management.ManagementFactory;
@@ -28,6 +29,7 @@ import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
 
+import org.midonet.cluster.storage.MidonetBackendConfig;
 import org.midonet.midolman.datapath.FlowExpirator;
 import scala.concurrent.Promise;
 import scala.concurrent.Promise$;
@@ -61,9 +63,14 @@ import org.midonet.conf.MidoNodeConfigurator;
 import org.midonet.jna.CLibrary;
 import org.midonet.midolman.cluster.zookeeper.ZookeeperConnectionModule;
 import org.midonet.midolman.config.MidolmanConfig;
+import org.midonet.midolman.host.services.HostService;
 import org.midonet.midolman.logging.FlowTracingAppender;
+import org.midonet.midolman.management.JmxConnectorServer;
+import org.midonet.midolman.management.SimpleHTTPServerService;
+import org.midonet.midolman.services.MidolmanActorsService;
 import org.midonet.midolman.services.MidolmanService;
 import org.midonet.midolman.simulation.PacketContext$;
+import org.midonet.midolman.topology.VirtualToPhysicalMapper;
 import org.midonet.midolman.topology.VirtualTopology;
 import org.midonet.util.process.MonitoredDaemonProcess;
 
@@ -87,6 +94,9 @@ public class Midolman {
         Midolman.class.getPackage().getImplementationVersion();
     public static final String VENDOR =
         Midolman.class.getPackage().getImplementationVendor();
+
+    private static final String FAST_REBOOT_FILE =
+        "/var/run/midolman/fast_reboot.file";
 
     private Injector injector;
 
@@ -179,9 +189,18 @@ public class Midolman {
         Runtime.getRuntime().addShutdownHook(new Thread("shutdown") {
             @Override
             public void run() {
-                doServicesCleanup();
+                log.debug("Fast reboot - isFastReboot? {}", isFastReboot());
+                if (isFastReboot()) {
+                    doServicesCleanupOnFastReboot();
+                } else {
+                    doServicesCleanup();
+                }
             }
         });
+    }
+
+    private boolean isFastReboot() {
+        return new File(FAST_REBOOT_FILE).exists();
     }
 
     private void setUncaughtExceptionHandler() {
@@ -237,6 +256,9 @@ public class Midolman {
 
         MidolmanConfig config = createConfig(configurator);
 
+        if (config.lockMemory())
+            lockMemory();
+
         FlowTablePreallocation preallocation;
         if (config.offHeapTables()) {
             preallocation = new FlowTableNoPreallocation();
@@ -268,18 +290,31 @@ public class Midolman {
         );
 
         // start the services
-        injector.getInstance(MidonetBackend.class)
-            .startAsync()
-            .awaitRunning();
+        MidonetBackend backend = injector.getInstance(MidonetBackend.class);
+        backend.startAsync().awaitRunning();
+
+        RebootBarriers barriers = new RebootBarriers(
+            backend.curator(), config.zookeeper());
+
+        if (isFastReboot()) {
+            log.info("Fast reboot (backup): initialization finished, remove "
+                     + "stage1 barrier and wait (5s) on stage2 barrier.");
+            barriers.stage2().setBarrier();
+            barriers.stage1().removeBarrier();
+            barriers.stage2().waitOnBarrier(5, TimeUnit.SECONDS);
+        }
+
         injector.getInstance(MidolmanService.class)
             .startAsync()
             .awaitRunning();
 
+        if (isFastReboot()) {
+            log.info("Fast reboot (backup): services started.");
+            new File(FAST_REBOOT_FILE).delete();
+        }
+
         enableFlowTracingAppender(
                 injector.getInstance(FlowTracingAppender.class));
-
-        if (config.lockMemory())
-            lockMemory();
 
         if (!initializationPromise.trySuccess(true)) {
             log.error("MidoNet Agent failed to initialize in {} seconds and " +
@@ -326,8 +361,10 @@ public class Midolman {
         log.info("MidoNet Agent shutting down...");
 
         try {
-            minionProcess.stopAsync()
-                .awaitTerminated(MINION_PROCESS_WAIT_TIMEOUT, TimeUnit.SECONDS);
+            if (minionProcess != null)
+                minionProcess.stopAsync()
+                    .awaitTerminated(MINION_PROCESS_WAIT_TIMEOUT,
+                                     TimeUnit.SECONDS);
         } catch (Exception e) {
             log.error("Minion process failed while stopping.", e);
         }
@@ -351,6 +388,41 @@ public class Midolman {
             watchedProcess.close();
             log.info("MidoNet Agent exiting. Bye!");
         }
+    }
+
+    private void doServicesCleanupOnFastReboot() {
+        log.info("Fast reboot (main) Midonet Agent coordinated shutdown started...");
+
+        MidonetBackend backend = injector
+            .getInstance(MidonetBackend.class);
+        MidonetBackendConfig backendConfig = injector
+            .getInstance(MidonetBackendConfig.class);
+        RebootBarriers barriers = new RebootBarriers(
+            backend.curator(), backendConfig);
+
+        barriers.clear();
+
+        try {
+            minionProcess.stopAsync()
+                .awaitTerminated(MINION_PROCESS_WAIT_TIMEOUT, TimeUnit.SECONDS);
+            minionProcess = null;
+        } catch (Exception e) {
+            log.error("Minion process failed while stopping.", e);
+        }
+
+        log.info("Minion process stopped.");
+
+        barriers.stage1().setBarrier();
+        log.info("Fast reboot (main): minion stopped, set barrier 1 and "
+                 + "wait (10s) on stage1 barrier.");
+        barriers.stage1().waitOnBarrier(10, TimeUnit.SECONDS);
+
+        // Stop all agent's services.
+        doServicesCleanup();
+
+        barriers.stage2().removeBarrier();
+        log.info("Fast reboot (main): agent services stopped, " +
+                 "remove stage 2 barrier and finish shutdown.");
     }
 
     @VisibleForTesting
