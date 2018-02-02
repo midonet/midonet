6280c60a2d54f1d71917f8b20703069c8f9f63d1
Alex Bikfalvi <alex@bikfalvi.com>
2018-02-02 20:50:47 +0100

    Initialize logging before everything else
    This reverses the change done during the fast-reboot feature,
    where logging was initialized after the rest of the agent. This
    ensures that logging conforms with the current mn-conf(1)
    setup.
    
    Ref: MNA-1208
    
    Change-Id: I8a3a13800e823897e5b8d3089a047cb6634e95ce
    Signed-off-by: Alex Bikfalvi <alex@bikfalvi.com>

--- midolman/src/main/java/org/midonet/midolman/Midolman.java
+++ midolman/src/main/java/org/midonet/midolman/Midolman.java
@@ -232,15 +232,6 @@ public class Midolman {
             throw t;
         }
 
-        minionProcess = new MonitoredDaemonProcess(
-            "/usr/share/midolman/minions-start", log, "org.midonet.services",
-            MINION_PROCESS_MAXIMUM_STARTS, MINION_PROCESS_FAILING_PERIOD,
-            (Exception e) -> {
-                log.debug(e.getMessage());
-                Midolman.exitAsync(MIDOLMAN_ERROR_CODE_MINION_PROCESS_DIED);
-            });
-        minionProcess.startAsync();
-
         Options options = new Options();
         options.addOption("c", "configFile", true, "config file path");
 
@@ -256,9 +247,39 @@ public class Midolman {
         MidoNodeConfigurator configurator =
             MidoNodeConfigurator.apply(configFilePath);
 
+        MidolmanConfig config = createConfig(configurator);
+
         MetricRegistry metricRegistry = new MetricRegistry();
 
-        MidolmanConfig config = createConfig(configurator);
+        Observable<Config> configObservable = configurator
+            .observableRuntimeConfig(HostIdGenerator.getHostId(), false);
+
+        configObservable
+            .subscribe(new LoggerLevelWatcher(scala.Option.apply("agent")));
+
+        configObservable
+            .subscribe(new LoggerMetricsWatcher("agent", metricRegistry));
+
+        ConfigRenderOptions renderOpts = ConfigRenderOptions.defaults()
+            .setJson(true)
+            .setComments(false)
+            .setOriginComments(false)
+            .setFormatted(true);
+        Config conf = config.conf();
+        Boolean showConfigPasswords =
+            System.getProperties().containsKey("midonet.show_config_passwords");
+        log.info("Loaded configuration: {}",
+                 configurator.dropSchema(conf, showConfigPasswords).root()
+                     .render(renderOpts));
+
+        minionProcess = new MonitoredDaemonProcess(
+            "/usr/share/midolman/minions-start", log, "org.midonet.services",
+            MINION_PROCESS_MAXIMUM_STARTS, MINION_PROCESS_FAILING_PERIOD,
+            (Exception e) -> {
+                log.debug(e.getMessage());
+                Midolman.exitAsync(MIDOLMAN_ERROR_CODE_MINION_PROCESS_DIED);
+            });
+        minionProcess.startAsync();
 
         if (config.lockMemory())
             lockMemory();
@@ -334,29 +355,6 @@ public class Midolman {
             FlowExpirator expirator = injector.getInstance(FlowExpirator.class);
             new Thread(expirator::expireAllFlows, "flow-expirator").start();
         }
-
-        Scheduler scheduler = injector.getInstance(VirtualTopology.class).vtScheduler();
-        Observable<Config> configObservable = configurator
-            .observableRuntimeConfig(HostIdGenerator.getHostId(), false)
-            .observeOn(scheduler);
-
-        configObservable
-            .subscribe(new LoggerLevelWatcher(scala.Option.apply("agent")));
-
-        configObservable.
-            subscribe(new LoggerMetricsWatcher("agent", metricRegistry));
-
-        ConfigRenderOptions renderOpts = ConfigRenderOptions.defaults()
-            .setJson(true)
-            .setComments(false)
-            .setOriginComments(false)
-            .setFormatted(true);
-        Config conf = injector.getInstance(MidolmanConfig.class).conf();
-        Boolean showConfigPasswords =
-            System.getProperties().containsKey("midonet.show_config_passwords");
-        log.info("Loaded configuration: {}",
-                 configurator.dropSchema(conf, showConfigPasswords).root()
-                             .render(renderOpts));
 
         injector.getInstance(MidolmanService.class).awaitTerminated();
     }
