07af9414eba5269c9687dba63fa6ede21d931654
Dmitry Bufistov <dmitry@midokura.com>
2018-03-15 15:30:16 +0100

    Split midonet-cluster:test to 3 groups
    So no test worker fail with exit code 29.
    
    Change-Id: Idb66b8854c82e6142b74f21794a2be1eea650d0d
    Ref: MI-2710
    Signed-off-by: Dmitry Bufistov <dmitry@midokura.com>

---
+++ tests/ci-gate-unit.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+./gradlew clean midolman:test nsdb:test midonet-util:test netlink:test\
+  midonet-cluster:test1 midonet-cluster:test2 midonet-cluster:test3 \
+  midonet-tools:test python-midonetclient:test cobertura -x integration \
+ -x midonet-cluster:test
